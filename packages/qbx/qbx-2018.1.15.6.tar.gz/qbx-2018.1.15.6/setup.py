41142d96c341b0ecc638148f019a913c1a2f3615
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:08 +0000

    Add package qbx/2018.1.15.6/qbx-2018.1.15.6.tar.gz

---
+++ packages/qbx/qbx-2018.1.15.6.tar.gz/qbx-2018.1.15.6/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(name='qbx',
+      author='qbtrade',
+      url='https://github.com/qbtrade/qbx',
+      author_email='admin@qbtrade.org',
+      packages=['qbx'],
+      version='2018.01.15.6',
+      install_requires=[
+          'docopt',
+          'requests',
+          'flask',
+          'flask_restful'
+      ],
+      zip_safe=False,
+      entry_points={'console_scripts': ['qbx=qbx:run']}
+      )
