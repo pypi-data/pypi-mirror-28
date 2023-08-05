126ee87a1a33d093663314c8681625dabaaf1c18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:26 +0000

    Add package django-wscounter/0.1.2.2/django-wscounter-0.1.2.2.tar.gz

---
+++ packages/django-wscounter/django-wscounter-0.1.2.2.tar.gz/django-wscounter-0.1.2.2/wscounter/views.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import authentication, permissions
+
+
+class WSCounterAPIView(APIView):
+    authentication_classes = (authentication.CSRFCheck,)
+    permission_classes = (permissions.AllowAny,)
+
+    def get(self, request, format=None):
+        summary = {}
+        summary["enterpriseCustomer"] = ""
+
+        return Response(summary)
