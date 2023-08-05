f543b3be924c519e9b2d0d986d90246e00f5a969
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:42 +0000

    Add package xy-ocrtext/1.0.0/xy_ocrtext-1.0.0.tar.gz

---
+++ packages/xy-ocrtext/xy_ocrtext-1.0.0.tar.gz/xy_ocrtext-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+  name = 'xy_ocrtext',
+  packages = ['xy_ocrtext'],
+  version = '1.0.0',
+  description = 'Get constellation',
+  long_description='Get constellation Info From juhe API',
+  author = 'KingHS',
+  author_email = '382771946@qq.com',
+  keywords = ['pip3', 'ocrtext', 'python3','python','ocr'],
+  license='MIT',
+)
