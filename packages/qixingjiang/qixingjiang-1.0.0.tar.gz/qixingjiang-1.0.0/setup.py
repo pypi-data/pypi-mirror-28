ef40c7bde4bf18406bed1bcb2b0e23cece72cc64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:57 +0000

    Add package qixingjiang/1.0.0/qixingjiang-1.0.0.tar.gz

---
+++ packages/qixingjiang/qixingjiang-1.0.0.tar.gz/qixingjiang-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name='qixingjiang',
+    version='1.0.0',
+    py_modules=['qixingjiang'],
+    author='sevenstar',
+    author_email='584212325@qq.com',
+    url='http://www.headfirstlabs.com',
+    description='A simple printer of nested lists',
+    )
