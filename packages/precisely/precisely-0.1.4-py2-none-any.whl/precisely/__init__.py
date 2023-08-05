ba2bd7821aa2904460a9a3efa8bf8dd3855e43c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:27 +0000

    Add package precisely/0.1.4/precisely-0.1.4-py2-none-any.whl

---
+++ packages/precisely/precisely-0.1.4-py2-none-any.whl/precisely/__init__.py
@@ -0,0 +1,33 @@
+from .base import Matcher, is_matcher
+from .core_matchers import equal_to, anything, all_of, any_of, not_
+from .object_matchers import has_attr, has_attrs, instance_of
+from .iterable_matchers import contains_exactly, is_sequence
+from .feature_matchers import has_feature
+
+
+__all__ = [
+    "assert_that",
+    "Matcher",
+    "is_matcher",
+    "equal_to",
+    "anything",
+    "all_of",
+    "any_of",
+    "has_attr",
+    "has_attrs",
+    "instance_of",
+    "contains_exactly",
+    "is_same_sequence",
+    "is_sequence",
+    "has_feature",
+    "not_",
+]
+
+# Deprecated
+is_same_sequence = is_sequence
+
+
+def assert_that(value, matcher):
+    result = matcher.match(value)
+    if not result.is_match:
+        raise AssertionError("\nExpected: {0}\nbut: {1}".format(matcher.describe(), result.explanation))
