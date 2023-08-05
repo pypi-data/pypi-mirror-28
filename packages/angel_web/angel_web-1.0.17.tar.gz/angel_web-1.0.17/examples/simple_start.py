ad4b531fa3e454656871524c9181652c4d891d7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:14 +0000

    Add package angel_web/1.0.17/angel_web-1.0.17.tar.gz

---
+++ packages/angel_web/angel_web-1.0.17.tar.gz/angel_web-1.0.17/examples/simple_start.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time         2017-12-20
+# @Email        2273337844@qq.com
+# @Copyright    © 2017 Lafite93
+
+
+if __name__ == "__main__":
+    import sys
+    sys.path.append("../angel_web")
+    from angel_web import app
+    app.serve_forever(debug=True)
