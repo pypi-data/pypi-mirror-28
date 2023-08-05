4f65a9e8eeed18e221925fbbae3af495e796bfee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:58 +0000

    Add package pyrocko/2018.1.16/pyrocko-2018.1.16.tar.gz

---
+++ packages/pyrocko/pyrocko-2018.1.16.tar.gz/pyrocko-2018.1.16/src/info.py
@@ -0,0 +1,6 @@
+# This module is automatically created from setup.py
+git_sha1 = '25fb4649fdd2dbc93a8e57e601d60180229224ae'
+local_modifications = False
+version = '2017.11.22'
+long_version = 'pyrocko-2017.11.22-25fb4649fdd2dbc93a8e57e601d60180229224ae-2018-01-15_15:42:40'  # noqa
+installed_date = '2018-01-15_15:42:40'
