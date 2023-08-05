085d22131a74400f1ab0676265b3cd5e882a7512
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:40 +0000

    Add package meansort/0.2/meansort-0.2.tar.gz

---
+++ packages/meansort/meansort-0.2.tar.gz/meansort-0.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='meansort',
+      version='0.2',
+      description='Sorting algorithm by splitting a list by its mean/average.',
+      url='https://github.com/dibsonthis/meansort',
+      author='Adib Attie',
+      author_email='adib.attie33@gmail.com',
+      license='MIT',
+      packages=['meansort'],
+      install_requires=[
+        
+      ],
+      zip_safe=False)
