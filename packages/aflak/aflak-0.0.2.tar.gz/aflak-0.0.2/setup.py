e0713a4bd2f2a7ded18f498726fa4f7c44c06a75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:06 +0000

    Add package aflak/0.0.2/aflak-0.0.2.tar.gz

---
+++ packages/aflak/aflak-0.0.2.tar.gz/aflak-0.0.2/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+
+
+setup(name='aflak',
+      version='0.0.2',
+      description='Advanced Framework for Learning Astrophysical Knowledge',
+      author='Malik Olivier Boussejra',
+      author_email='malik@boussejra.com',
+      url='https://github.com/malikolivier/aflak',
+      license='GPLv3',
+      packages=['aflak'],
+      entry_points={
+          'console_scripts': [
+              'aflak = aflak.aflak:_main',
+          ]
+      },
+      install_requires=[
+          'astropy',
+          'pyqtgraph',
+          'PyQt5',
+      ],
+      classifiers=[
+          'Development Status :: 1 - Planning',
+          'Environment :: Console',
+          'Environment :: X11 Applications :: Qt',
+          'Intended Audience :: End Users/Desktop',
+          'Intended Audience :: Education',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+          'Operating System :: POSIX :: Linux',
+          'Programming Language :: Python :: 3.6',
+          'Topic :: Multimedia :: Graphics',
+          'Topic :: Scientific/Engineering :: Astronomy',
+          'Topic :: Scientific/Engineering :: Physics',
+          'Topic :: Scientific/Engineering :: Visualization'
+      ])
