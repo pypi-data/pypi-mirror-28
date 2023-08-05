a189442c70ebdd7041778b1b31c8caccb074c155
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:17 +0000

    Add package delimited/0.0.5/delimited-0.0.5.tar.gz

---
+++ packages/delimited/delimited-0.0.5.tar.gz/delimited-0.0.5/delimited/__init__.py
@@ -0,0 +1,12 @@
+__version__ = "0.0.5"
+__title__ = "delimited"
+__summary__ = "Defines types that allow for accessing and modifying nested data"
+__url__ = "https://github.com/chrisantonellis/delimited"
+__author__ = "Christopher Antonellis"
+__email__ = "christopher.antonellis@gmail.com"
+__license__ = "MIT License"
+
+from delimited.container import NestedDict
+from delimited.container import NestedOrderedDict
+from delimited.container import DelimitedDict
+from delimited.container import DelimitedOrderedDict
