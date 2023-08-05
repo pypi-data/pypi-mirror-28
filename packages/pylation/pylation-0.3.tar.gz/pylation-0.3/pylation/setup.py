1f6ab89b7119b405e5db61edad6418996c38e925
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:59 +0000

    Add package pylation/0.3/pylation-0.3.tar.gz

---
+++ packages/pylation/pylation-0.3.tar.gz/pylation-0.3/pylation/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(name='pylation',
+      version='0.2',
+      description='alpha testing this package at the moment, more coming soon',
+      url='http://github.com/bheff88/pylation',
+      author='Braden Heffernan',
+      author_email='baheffer@ucalgary.ca',
+      license='MIT',
+      packages=['pylation'],
+#      install_requires=['numpy>=1.9.1',
+#                        'scipy>=0.14',
+#                        'six>=1.9.0',],
+      zip_safe=False)
+# python setup.py sdist upload -r pypi
