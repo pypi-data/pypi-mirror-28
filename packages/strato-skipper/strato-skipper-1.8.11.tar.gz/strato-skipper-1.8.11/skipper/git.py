05abb13d4cea0694dbc2a10b2c3a68e9f5faea3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:10 +0000

    Add package strato-skipper/1.8.11/strato-skipper-1.8.11.tar.gz

---
+++ packages/strato-skipper/strato-skipper-1.8.11.tar.gz/strato-skipper-1.8.11/skipper/git.py
@@ -0,0 +1,19 @@
+import logging
+import subprocess
+
+
+def get_hash(short=False):
+    git_command = ['git', 'rev-parse']
+    if short:
+        git_command += ['--short']
+    git_command += ['HEAD']
+
+    if uncommitted_changes():
+        logging.warning("*** Uncommitted changes present - Build container version might be outdated ***")
+
+    return subprocess.check_output(git_command).decode().strip()
+
+
+def uncommitted_changes():
+    """Return True is there are uncommitted changes."""
+    return subprocess.call(['git', 'diff-index', '--quiet', 'HEAD', '--']) != 0
