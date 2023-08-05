1cbe08a17cdd7d0ab0e061f2ae8385404a0dd5f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:50 +0000

    Add package brotz/0.1.0/brotz-0.1.0.tar.gz

---
+++ packages/brotz/brotz-0.1.0.tar.gz/brotz-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='brotz',
+      version='0.1.0',
+      description='Lightweight, chiller way to compose html in python',
+      url='http://github.com/leontrolski/brotz',
+      author='leontrolski',
+      author_email='ojhrussell@gmail.com',
+      license='GNU General Public License v3.0',
+      packages=['brotz'],
+      zip_safe=False)
