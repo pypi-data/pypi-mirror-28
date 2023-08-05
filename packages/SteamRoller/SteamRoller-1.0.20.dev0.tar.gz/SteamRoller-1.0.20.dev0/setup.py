cd725ee3c1c09887511ee58e472293f98416ae57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:58 +0000

    Add package SteamRoller/1.0.20.dev0/SteamRoller-1.0.20.dev0.tar.gz

---
+++ packages/SteamRoller/SteamRoller-1.0.20.dev0.tar.gz/SteamRoller-1.0.20.dev0/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name="SteamRoller",
+      version="1.0.20-dev",
+      description="An experimental framework for text classification experiments",
+      author="Tom Lippincott",
+      author_email="tom@cs.jhu.edu",
+      url="http://hltcoe.jhu.edu",
+      maintainer="Tom Lippincott",
+      maintainer_email="tom@cs.jhu.edu",
+      packages=["steamroller",
+                "steamroller.tools",
+                "steamroller.tasks",
+                "steamroller.models",
+                "steamroller.metrics",
+                "steamroller.plots",
+                "steamroller.scons"],
+      package_dir={"steamroller" : "src"},
+      scripts=["scripts/steamroller"],
+      install_requires=["concrete", "scikit-learn>=0.18.1", "scons>=3.0.0", "plotnine", "flask", "valid", "tqdm", "tensorflow>=1.0.0", "nltk"],
+      package_data={"" : ["data/SConstruct", "data/steamroller_config.json"]},
+     )
