82031b4abfc33ecce5a29deced6ab675ef2d6822
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:51 +0000

    Add package dsomesh/1.1.0/dsomesh-1.1.0.tar.gz

---
+++ packages/dsomesh/dsomesh-1.1.0.tar.gz/dsomesh-1.1.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+name = 'dsomesh',
+version = '1.1.0',
+py_modules = ['dsomesh'],
+author = 'dsomesh',
+author_email = 'meetsomesh01@gmail.com',
+url = 'http://www.headfirstlabs.com',
+description = 'A simple printer of nested lists with new changes',
+keywords = ['testing', 'logging', 'example'],
+classifiers = [],
+)
