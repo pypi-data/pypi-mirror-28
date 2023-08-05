1b74c07c3aaaa6b30a9be22e966d3c7d33622588
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:15 +0000

    Add package tqs-client/0.5/tqs_client-0.5.tar.gz

---
+++ packages/tqs-client/tqs_client-0.5.tar.gz/tqs_client-0.5/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(
+    name = "tqs_client",
+    version = "0.5",
+    py_modules = ["tqs"],
+    setup_requires = ["wheel"],
+    install_requires = ["requests"],
+    python_requires = ">=3",
+    description = "Tiny Queue Service Client",
+    author = "Stefan Arentz",
+    author_email = "stefan@arentz.ca",
+    url = "https://github.com/st3fan/tqs-client",
+)
