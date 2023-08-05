c06a110a9707d4072a2c28f521769c58b670c776
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:16 +0000

    Add package icon/0.0.1/icon-0.0.1.tar.gz

---
+++ packages/icon/icon-0.0.1.tar.gz/icon-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='icon',
+    version="0.0.1",                 
+    description="Random choice from Google Material Icons in Jupyter notebook",
+    long_description="""\
+Random choice from Google Material Icons in Jupyter notebook
+See: https://github.com/google/material-design-icons""",
+    author='Saito Tsutomu',
+    license='PSFL',
+    classifiers=[
+        "Development Status :: 1 - Planning"
+    ],
+    keywords='icon',
+)
