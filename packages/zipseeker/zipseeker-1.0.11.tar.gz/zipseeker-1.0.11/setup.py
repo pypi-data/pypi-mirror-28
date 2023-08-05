6e49f04b2e6364f8711a55914724aedd3a8ecd26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:25 +0000

    Add package zipseeker/1.0.11/zipseeker-1.0.11.tar.gz

---
+++ packages/zipseeker/zipseeker-1.0.11.tar.gz/zipseeker-1.0.11/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+import os
+
+ROOT = os.path.abspath(os.path.dirname(__file__))
+
+VERSION='1.0.11'
+
+setup(name='zipseeker',
+      packages=['zipseeker'],
+      version=VERSION,
+      description='Create a streamable and (somewhat) seekable .ZIP file',
+      long_description=open(ROOT + '/README.rst').read(),
+      author='Ayke van Laethem',
+      author_email='aykevanlaethem@gmail.com',
+      url='https://github.com/aykevl/python-zipseeker',
+      keywords=['zip', 'http', 'streaming'],
+      download_url='https://github.com/aykevl/python-zipseeker/archive/v%s.zip' % VERSION,
+     )
