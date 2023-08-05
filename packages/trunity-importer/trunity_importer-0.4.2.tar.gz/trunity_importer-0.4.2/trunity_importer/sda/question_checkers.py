39f4bbc7983fa5166a96c637283639e35c165cbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:16 +0000

    Add package trunity-importer/0.4.2/trunity_importer-0.4.2.tar.gz

---
+++ packages/trunity-importer/trunity_importer-0.4.2.tar.gz/trunity_importer-0.4.2/trunity_importer/sda/question_checkers.py
@@ -0,0 +1,64 @@
+"""
+Some items in XMl are "dirty". So we need to check questions for correctness.
+"""
+from warnings import warn
+from trunity_importer.sda.question_containers import (
+    MultipleChoice,
+    MultipleAnswer,
+    QuestionType,
+)
+
+
+def _all_answers_has_one_true(question: MultipleChoice) -> bool:
+    """
+    Return True if there are only one answer with True. False otherwise.
+    """
+    count_true = 0
+
+    for answer in question.answers:
+        if answer.correct is True:
+            count_true += 1
+
+    if count_true == 1:
+        return True
+    else:
+        warn(
+            "Question with id={} has not exactly one True answer!".format(
+                question.item_id)
+        )
+        return False
+
+
+def _all_answers_are_not_false(question: MultipleAnswer) -> bool:
+    """
+    Return False if there all answers with True. False otherwise.
+    """
+    count_true = 0
+
+    for answer in question.answers:
+        if answer.correct is True:
+            count_true += 1
+
+    if count_true >= 1:
+        return True
+    else:
+        warn(
+            "Question with id={} has all False answers!".format(
+                question.item_id)
+        )
+        return False
+
+
+def correct_question(question: MultipleChoice) -> bool:
+    """
+    Check question is correct or not.
+    """
+    if question.type == QuestionType.MULTIPLE_CHOICE:
+        return _all_answers_has_one_true(question)
+
+    elif question.type == QuestionType.MULTIPLE_ANSWER:
+        return _all_answers_are_not_false(question)
+
+    else:
+        # for all other questions we assume they are correct:
+        return True
