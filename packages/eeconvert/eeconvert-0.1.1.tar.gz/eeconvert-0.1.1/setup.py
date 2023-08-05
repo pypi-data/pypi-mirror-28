6a2672df9c3730dffc619367d8b5678e6d9df192
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:31 +0000

    Add package eeconvert/0.1.1/eeconvert-0.1.1.tar.gz

---
+++ packages/eeconvert/eeconvert-0.1.1.tar.gz/eeconvert-0.1.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='eeconvert',
+      version='0.1.1',
+      description='earth engine conversion for jupyter notebook use',
+      url='http://github.com/rutgerhofste/eeconvert',
+      author='Rutger Hofste',
+      author_email='rutgerhofste@gmail.com',
+      license='MIT',
+      packages=['eeconvert'],
+      install_requires=[
+          'folium','geopandas'
+      ],
+      zip_safe=False)
