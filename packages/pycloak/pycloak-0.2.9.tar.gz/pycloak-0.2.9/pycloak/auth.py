51592350c600ae71bccd74756538c89110f75907
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:29 +0000

    Add package pycloak/0.2.9/pycloak-0.2.9.tar.gz

---
+++ packages/pycloak/pycloak-0.2.9.tar.gz/pycloak-0.2.9/pycloak/auth.py
@@ -0,0 +1,109 @@
+
+import json
+import jwt
+import logging
+import requests
+import time
+import urllib.parse
+import urllib.request
+
+logger = logging.getLogger('pycloak.auth')
+
+
+def direct_access_grant_token(username, password, host="http://localhost:8080", realm="master", client_id="admin-cli", include_offline=False):
+    """
+    Makes a request against the Keycloak openid-connect/token endpoint using a direct access grant to get a token.
+
+    Returned JSON objects will look like this:
+    {'access_token': 'ey...2w', 'expires_in': 60, 'refresh_expires_in': 1800, 'refresh_token': 'ey..Og', 'not-before-policy': 0, 'session_state': '5ca9ccf7-b425-4fe8-81a1-8bbd770ee312', 'token_type': 'bearer'}
+
+    @param username: user for which the token is to be granted
+    @param password: user's password
+    @param host: full hostname with protocol of the keycloak auth server
+    @param realm: realm against which the direct access grant will be made
+    @param client_id: client argument for the direct access grant request
+    @param include_offline: denotes whether or not to include the parameter for an offline token request.
+    @return: json object representing token Response
+    @raise: AuthException if a non-200 response is returned by the token endpoint
+    """
+    token_request_data = {'grant_type': 'password', 'client_id': client_id, 'username': username, 'password': password}
+    if include_offline:
+        token_request_data['scope'] = 'offline_access'
+    token_request_url = "{0}/auth/realms/{1}/protocol/openid-connect/token".format(host, realm)
+    token_response = requests.post(token_request_url, data=token_request_data)
+
+    if token_response.status_code != 200:
+        raise AuthException('Non-200 Response returned from direct access grant attempt: {}'.format(token_response.status_code))
+
+    return json.loads(token_response.text)
+
+def offline_token(offline_token , host="http://localhost:8080", realm="master", client_id="admin-cli", include_offline=False):
+    token_request_data = {'grant_type': 'refresh_token', 'client_id': client_id, 'refresh_token': offline_token}
+
+    if include_offline:
+        token_request_data['scope'] = 'offline_access'
+    token_request_url = "{0}/auth/realms/{1}/protocol/openid-connect/token".format(host, realm)
+    token_response = requests.post(token_request_url, data=token_request_data)
+
+    if token_response.status_code == 400:
+        raise AuthException('400/Bad Request returned.  Validate that the client allows this kind of grant and the calling user has the offline_access role (and it is effective for the client)')
+    if token_response.status_code != 200:
+        raise AuthException('Non-200 Response returned from offline token refresh attempt: {}'.format(token_response.status_code))
+
+    return json.loads(token_response.text)
+
+def is_expired(token):
+    """
+    Takes a base-64 encoded representation of a JSON web token and determines if it is expired.  Note that this is the *only* check it performs.
+
+    @param token: base-64 encoded representation of JWT
+    @return: true if expired, false if non-expired
+    """
+    decoded_jwt = jwt.decode(token, verify=False)
+    try:
+        exp = int(decoded_jwt['exp'])
+    except ValueError:
+        logging.error('Could not determine token expiry using "exp" claim.  Pleave validate that valid tokens are being returned from the endpoint.')
+        raise AuthException("Could not determine token expiry")
+
+    return exp < int(time.time())
+
+class AuthSession:
+
+    __access_token = None
+
+    def __init__(self, username=None, password=None, offline_token=None, host="http://localhost:8080", realm="master", client_id="admin-cli"):
+        if offline_token is None and (username is None or password is None):
+            raise AuthException("Unable to authenticate, no form of credentials provided.  Please supply either a username/password pair or offline token.")
+
+        self.username = username
+        self.password = password
+        self.token = offline_token
+        self.host = host
+        self.realm = realm
+        self.client_id = client_id
+
+    #TODO should be using refresh tokens instead of direct access grants for re-auth every time
+    def get_access_token(self):
+        if self.__access_token is None or is_expired(self.__access_token):
+            if self.token is not None:
+                logging.debug('Using offline token grant to acquire access token.')
+                token_response = offline_token(self.token, self.host, self.realm, self.client_id)
+            else:
+                logging.debug('Using direct access grant to acquire access token.')
+                token_response = direct_access_grant_token(self.username, self.password, self.host, self.realm, self.client_id)
+
+            self.__access_token = token_response['access_token']
+
+        return self.__access_token
+
+    def bearer_header(self):
+        return {'Authorization': "Bearer {}".format(self.get_access_token())}
+
+    access_token = property(get_access_token)
+    bearer_header = property(bearer_header)
+
+class AuthException(Exception):
+
+    def __init__(self, *args, **kwargs):
+        Exception.__init__(self, *args, **kwargs)
---
+++ packages/pycloak/pycloak-0.2.9.tar.gz/pycloak-0.2.9/pycloak/flow.py
@@ -0,0 +1,148 @@
+
+import requests
+import json
+from pycloak import execution
+
+class Flow:
+
+    def __init__(self, realm, json_rep=None, dict_rep=None):
+        """
+        Instantiates a representation of a Keycloak authentication flow.
+
+        :param realm: pycloak realm object to which this flow belongs.
+        :param json_rep: json representation of the authentication flow.  Given priority over dict_rep if both are provided.
+        :param dict_rep: dictionary representation of the authentication flow.  Discarded if json_rep is provided.
+        """
+        if not json_rep and dict_rep:
+            json_rep = json.loads(json.dumps(dict_rep))
+
+        self.realm = realm
+        self.id = json_rep.get('id')
+        self.json = json_rep
+
+    # TODO this just occurred to me... the '$host/auth' path can be changed.  should be parameterized!
+    # Might be useful to make static url-maker methods in all the respective classes for this?
+    def executions(self, provider=None):
+        """
+        Retrieves list of all executions for this authentication flow
+
+        :return: json list of executions associated with this authentication flow
+        """
+        # TODO see if the ID can be used instead, no idea why KC console decided to use alias for REST path for executions
+        # http://localhost:8080/auth/admin/realms/master/authentication/flows/Test Flow/executions
+        executions_url = "{0}/auth/admin/realms/{1}/authentication/flows/{2}/executions".format(self.realm.auth_session.host, self.realm.id, self.json['alias'])
+        response = requests.get(executions_url, headers=self.realm.auth_session.bearer_header)
+
+        if response.status_code != 200:
+            raise AuthFlowException("could not retrieve executions for auth flow")
+
+        executions = json.loads(response.text)
+        if provider is None:
+            return executions
+        else:
+            return list(filter(lambda execution: execution.get('providerId') == provider, executions))
+
+    def execution(self, id=None):
+        """
+        Retrieves a single execution by ID.
+
+        :param id: GUID of the execution.  I.E. 2cd14612-4901-4d94-81ca-a86729d63fab
+        :return: pycloak object of the matching execution.  None if not found or no id is provided.
+        """
+        executions = self.executions()
+        if id is not None:
+            execu = next(filter(lambda execu: execu['id'] == id, self.executions()), None)
+            return execution.Execution(self, json_rep=execu)
+
+        return None
+
+
+    def create_execution(self, new_execution):
+        """
+        Creates an execution in this flow.  Note that the execution will be added according to default Keycloak behavior and
+        further customization is likely to be necessary.
+
+        :param execution: json object representing the execution to be added to the flow.  I.E. {'provider': 'auth-username-password-form'}
+        :return: arbitrary execution that shares the same provider type.  Most commonly, there will only be one execution of each type,
+        so this will be reliable.  However, since Keycloak does not return a Location header on 204, there is no deterministic
+        way to validate the correct execution is retrieved.  If something crazy happens None might be returned.  Just warning you.
+
+        Will PR for this soon.
+        """
+        # TODO validate required "provider" field present
+
+        url = "{0}/auth/admin/realms/{1}/authentication/flows/{2}/executions/execution".format(self.realm.auth_session.host, self.realm.id, self.json['alias'])
+        response = requests.post(url, json=new_execution, headers=self.realm.auth_session.bearer_header)
+
+        # TODO keycloak PR for this, return at least a UID or something...
+        if response.status_code != 204:
+            raise AuthFlowException("Error attempting to create new execution: {0}/{1}".format(response.status_code, response.text))
+
+        best_guess = next(iter(self.executions(provider=new_execution['provider'])), None)
+        if best_guess is None:
+            return None
+        else:
+            return execution.Execution(self, json_rep=best_guess)
+
+    def update_execution(self, execution):
+        """
+        Performs an update operation for an execution, matched by ID
+
+        :return: updated execution object, as retrieved from Keycloak
+        """
+
+        url = "{0}/auth/admin/realms/{1}/authentication/flows/{2}/executions".format(self.realm.auth_session.host, self.realm.id, self.json['alias'])
+        response = requests.put(url, json=execution, headers=self.realm.auth_session.bearer_header)
+
+        if response.status_code != 204:
+            raise AuthFlowException("Error attempting to update execution")
+
+        return self.execution(id=execution['id'])
+
+    def delete_execution(self, id):
+        """
+        Deletes execution specified by ID.  If not found, no error is raised.
+        """
+        url = "{0}/auth/admin/realms/{1}/authentication/executions/{2}".format(self.realm.auth_session.host, self.realm.id, id)
+        response = requests.delete(url, headers=self.realm.auth_session.bearer_header)
+
+        if response.status_code not in [404, 204]:
+            raise AuthFlowException("Error attempting to delete execution")
+
+        #TODO feels wrong - return something else here
+        return response
+
+    def delete_all_executions(self):
+        """
+        Convenience method for remove all executions associated with this flow
+
+        :return: fresh flow object with executions removed.
+        """
+        for execution in self.executions():
+            self.delete_execution(execution['id'])
+
+        return self.realm.auth_flow(id=self.id)
+
+
+    def create_child_flow(self, child_flow):
+        """
+        Creates child flow, with the current flow as parent
+
+        :param child_flow: json representation of the flow to create.  I.E. {'alias': 'test-sub-flow', 'type': 'basic-flow', 'description': 'Demonstrates how to configure a sub-flow', 'provider': 'registration-page-form'}
+        """
+        url = "{0}/auth/admin/realms/{1}/authentication/flows/{2}/executions/flow".format(self.realm.auth_session.host, self.realm.id, self.json['alias'])
+        response = requests.post(url, json=child_flow, headers=self.realm.auth_session.bearer_header)
+
+        if response.status_code != 204:
+            raise AuthFlowException("Error attempting to create new execution")
+
+        return next(iter(self.executions(provider=execution['provider'])))
+
+    def update_child_flow(self, flow):
+        """
+        Just invokes update_execution.  Since both child flows and executions share the same structure, they use the same REST invocations
+        """
+        self.update_execution(flow)
+
+class AuthFlowException(Exception):
+    pass
