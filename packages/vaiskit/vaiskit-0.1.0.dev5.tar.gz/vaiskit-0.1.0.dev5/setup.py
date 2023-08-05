111503a36b9d60be3bb2ac60f16240dc9d25af29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:42 +0000

    Add package vaiskit/0.1.0.dev5/vaiskit-0.1.0.dev5.tar.gz

---
+++ packages/vaiskit/vaiskit-0.1.0.dev5.tar.gz/vaiskit-0.1.0.dev5/setup.py
@@ -0,0 +1,16 @@
+import os
+from setuptools import setup
+
+if __name__ == "__main__":
+    setup(
+        name="vaiskit",
+        author='VAIS',
+        packages=['vaiskit'],
+        package_data={'': ['./vaiskit/nlp', './vaiskit/speech']},
+        author_email='support@vais.vn',
+        url='https://vais.vn',
+        include_package_data=True,
+        install_requires=["grpcio==1.4.0", "pyaudio==0.2.11"],
+        version="0.1.0.dev5",
+        python_requires='>=2.6, !=3.0.*, !=3.1.*, !=3.2.*, <4',
+    )
