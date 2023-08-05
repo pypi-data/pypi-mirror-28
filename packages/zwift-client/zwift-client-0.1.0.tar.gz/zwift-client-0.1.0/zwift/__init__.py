2e9a1c3d3bf36957da037875a0fdabe8fa5de2ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:25 +0000

    Add package zwift-client/0.1.0/zwift-client-0.1.0.tar.gz

---
+++ packages/zwift-client/zwift-client-0.1.0.tar.gz/zwift-client-0.1.0/zwift/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""Top-level package for Zwift Mobile API client."""
+from .client import Client
+
+
+__author__ = """Sander Smits"""
+__email__ = 'jhmsmits@gmail.com'
+__version__ = '0.1.0'
+
+__all__ = [Client]
