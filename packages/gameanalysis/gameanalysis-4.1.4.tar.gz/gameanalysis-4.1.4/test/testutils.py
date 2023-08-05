add1ce47e64747dcfc07d2089a64b036d8913976
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:07 +0000

    Add package gameanalysis/4.1.4/gameanalysis-4.1.4.tar.gz

---
+++ packages/gameanalysis/gameanalysis-4.1.4.tar.gz/gameanalysis-4.1.4/test/testutils.py
@@ -0,0 +1,33 @@
+import functools
+import warnings
+
+
+games = [
+    ([1], [1]),
+    ([1], [2]),
+    ([2], [1]),
+    ([1, 1], [1, 1]),
+    ([1, 1], [2, 2]),
+    ([2, 2], [1, 1]),
+    ([1, 2], [2, 1]),
+    ([2, 1], [1, 2]),
+    ([3, 4], [2, 3]),
+    ([2, 3, 4], [4, 3, 2]),
+]
+
+
+def warnings_filter(category=Warning, status='ignore'):
+    """decorator to apply warning filter to a function
+
+    Arguments
+    ---------
+    status : str, optional
+        The status to treat all warnings as. Default ignores all wanrings."""
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapped(*args, **kwargs):
+            with warnings.catch_warnings():
+                warnings.simplefilter('ignore', category)
+                return func(*args, **kwargs)
+        return wrapped
+    return decorator
