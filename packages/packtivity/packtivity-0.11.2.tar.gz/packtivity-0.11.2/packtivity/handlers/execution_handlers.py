a803f1bb4368d7606326c3172aaa32307e68491d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:56 +0000

    Add package packtivity/0.11.2/packtivity-0.11.2.tar.gz

---
+++ packages/packtivity/packtivity-0.11.2.tar.gz/packtivity-0.11.2/packtivity/handlers/execution_handlers.py
@@ -0,0 +1,314 @@
+import os
+import subprocess
+import sys
+import time
+import psutil
+import shlex
+import pipes
+
+import click
+import yaml
+
+import packtivity.utils as utils
+import packtivity.logutils as logutils
+
+handlers,executor = utils.handler_decorator()
+
+def sourcepath(path):
+    if 'PACKTIVITY_WORKDIR_LOCATION' in os.environ:
+        old,new = os.environ['PACKTIVITY_WORKDIR_LOCATION'].split(':')
+        dockerpath = new+path.rsplit(old,1)[1]
+        return dockerpath
+    else:
+        return path
+
+def state_context_to_mounts(state):
+    readwrites  = state.readwrite
+    readonlies = state.readonly
+    mounts = ''
+    for rw in readwrites:
+        mounts += '-v {}:{}:rw'.format(sourcepath(os.path.abspath(rw)),rw)
+    for ro in readonlies:
+        mounts += ' -v {}:{}:ro'.format(sourcepath(ro),ro)
+    return mounts
+
+def prepare_par_mounts(parmounts,state):
+    mounts = []
+    for i,x in enumerate(parmounts):
+        parmountfile = os.path.join(state.readwrite[0],'_yadage_parmount_{}.txt'.format(i))
+        with open(parmountfile,'w') as f:
+            f.write(x['mountcontent'])
+
+        mounts.append(' -v {}:{}'.format(
+            sourcepath(os.path.abspath(parmountfile)),
+            x['mountpath']
+        ))
+
+    return mounts
+
+def cvmfs_from_volume_plugin(cvmfs_repos = None):
+    if not cvmfs_repos:
+        cvmfs_repos = yaml.load(os.environ.get('PACKTIVITY_CVMFS_REPOS','null'))
+    if not cvmfs_repos:
+        cvmfs_repos  = ['atlas.cern.ch','atlas-condb.cern.ch','sft.cern.ch']
+    command_line = ' --security-opt label:disable'
+    for repo in cvmfs_repos:
+        command_line += ' --volume-driver cvmfs -v {cvmfs_repo}:/cvmfs/{cvmfs_repo}'.format(cvmfs_repo = repo)
+    return command_line
+
+def cvmfs_from_external_mount():
+    return ' -v {}:/cvmfs'.format(os.environ.get('PACKTIVITY_CVMFS_LOCATION','/cvmfs'))
+
+def cvmfs_mount():
+    cvmfs_source = os.environ.get('PACKTIVITY_CVMFS_SOURCE','external')
+    if cvmfs_source == 'external':
+        return cvmfs_from_external_mount()
+    elif cvmfs_source == 'voldriver':
+        return cvmfs_from_volume_plugin()
+    else:
+        raise RuntimeError('unknown CVMFS location requested')
+
+def auth_mount():
+    if 'PACKTIVITY_AUTH_LOCATION' not in os.environ:
+        return ' -v /home/recast/recast_auth:/recast_auth'
+    else:
+        return ' -v {}:/recast_auth'.format(os.environ['PACKTIVITY_AUTH_LOCATION'])
+
+def resource_mounts(state,environment,log):
+    report = '''\n\
+--------------
+run in docker container image: {image}
+with env: {env}
+resources: {resources}
+--------------
+    '''.format(image = environment['image'],
+               env = environment['envscript'] if environment['envscript'] else 'default env',
+               resources = environment['resources']
+              )
+    log.debug(report)
+
+    do_cvmfs = 'CVMFS' in environment['resources']
+    do_auth  = ('GRIDProxy'  in environment['resources']) or ('KRB5Auth' in environment['resources'])
+    log.debug('do_auth: %s do_cvmfs: %s',do_auth,do_cvmfs)
+
+
+    resource_mounts = ''
+    if do_cvmfs:
+        resource_mounts+=cvmfs_mount()
+
+    if do_auth:
+        resource_mounts+=auth_mount()
+
+    return resource_mounts
+
+def docker_execution_cmdline(state,environment,log,metadata,stdin,cmd_argv):
+    quoted_string = ' '.join(map(pipes.quote,cmd_argv))
+
+    image = environment['image']
+    imagetag = environment['imagetag']
+
+    # generic non-volume mount flags
+    workdir_flag =  '-w {}'.format(environment['workdir']) if environment['workdir'] is not None else ''
+
+    cidfile = '{}/{}.cid'.format(state.metadir,metadata['name'])
+    if os.path.exists(cidfile):
+        log.warning('cid file %s seems to exist, container execution will crash',cidfile)
+    cid_file = '--cidfile {}'.format(cidfile)
+
+    custom_mod = ' {}'.format(os.environ.get('PACKTIVITY_DOCKER_CMD_MOD',''))
+
+    # volume mounts (resources, parameter mounts and state mounts)
+    state_mounts = state_context_to_mounts(state)
+    rsrcs_mounts = resource_mounts(state,environment,log)
+    par_mounts = ' '.join(prepare_par_mounts(environment['par_mounts'], state))
+
+    return 'docker run --rm {stdin} {cid} {workdir} {custom} {state_mounts} {rsrcs} {par_mounts} {img}:{tag} {command}'.format(
+        stdin = '-i' if stdin else '',
+        cid = cid_file,
+        workdir = workdir_flag,
+        custom = custom_mod,
+        state_mounts = state_mounts,
+        rsrcs = rsrcs_mounts,
+        par_mounts = par_mounts,
+        img = image,
+        tag = imagetag,
+        command = quoted_string
+    )
+
+def run_docker_with_script(state,environment,job,log):
+    script = job['script']
+    interpreter = job['interpreter']
+
+    log.debug('script is:')
+    log.debug('\n--------------\n'+script+'\n--------------')
+    if 'PACKTIVITY_DRYRUN' in os.environ:
+        return
+
+    indocker = interpreter
+    envmod = 'source {} && '.format(environment['envscript']) if environment['envscript'] else ''
+    in_docker_cmd = envmod+indocker
+    return in_docker_cmd, script
+
+def run_docker_with_oneliner(state,environment,command,log):
+    log.debug('''\n\
+--------------
+running one liner in container.
+command: {command}
+--------------
+    '''.format(command = command))
+
+    envmod = 'source {} &&'.format(environment['envscript']) if environment['envscript'] else ''
+    in_docker_cmd = '{envmodifier} {command}'.format(envmodifier = envmod, command = command)
+    return in_docker_cmd, None
+
+def execute_docker(metadata,state,log,docker_run_cmd_str,stdin_content = None):
+    log.debug('container execution command: \n%s',docker_run_cmd_str)
+    log.debug('stdin if any: %s', stdin_content)
+    if 'PACKTIVITY_DRYRUN' in os.environ:
+        return
+    try:
+        with logutils.setup_logging_topic(metadata,state,'run', return_logger = True) as runlog:
+
+            proc = None
+            if stdin_content:
+                log.debug('stdin: \n%s',stdin_content)
+                proc = subprocess.Popen(shlex.split(docker_run_cmd_str),
+                                        stdin = subprocess.PIPE,
+                                        stderr = subprocess.STDOUT,
+                                        stdout = subprocess.PIPE,
+                                        bufsize=1,
+                                        close_fds = True)
+                proc.stdin.write(stdin_content.encode('utf-8'))
+                proc.stdin.close()
+            else:
+                proc = subprocess.Popen(shlex.split(docker_run_cmd_str), stderr = subprocess.STDOUT, stdout = subprocess.PIPE, bufsize=1, close_fds = True)
+
+            log.debug('started run subprocess with pid %s. now wait to finish',proc.pid)
+            time.sleep(0.5)
+            log.debug('process children: %s',[x for x in psutil.Process(proc.pid).children(recursive = True)])
+
+            for line in iter(proc.stdout.readline, b''):
+                runlog.info(line.strip())
+            while proc.poll() is None:
+                pass
+            proc.stdout.close()
+        log.debug('container execution subprocess finished. return code: %s',proc.returncode)
+        if proc.returncode:
+            log.error('non-zero return code raising exception')
+            raise subprocess.CalledProcessError(returncode =  proc.returncode, cmd = docker_run_cmd_str)
+        log.debug('moving on from run')
+    except subprocess.CalledProcessError as exc:
+        log.exception('subprocess failed. code: %s,  command %s',exc.returncode,exc.cmd)
+        raise RuntimeError('failed container execution subprocess.')
+    except:
+        log.exception("Unexpected error: %s",sys.exc_info())
+        raise
+    finally:
+        log.debug('finally for run')
+
+def docker_pull(docker_pull_cmd,log,state,metadata):
+    log.debug('container image pull command: \n  %s',docker_pull_cmd)
+    if 'PACKTIVITY_DRYRUN' in os.environ:
+        return
+    try:
+        with logutils.setup_logging_topic(metadata,state,'pull', return_logger = True) as pulllog:
+            proc = subprocess.Popen(shlex.split(docker_pull_cmd), stderr = subprocess.STDOUT, stdout = subprocess.PIPE, bufsize=1, close_fds = True)
+            log.debug('started pull subprocess with pid %s. now wait to finish',proc.pid)
+            time.sleep(0.5)
+            log.debug('process children: %s',[x for x in psutil.Process(proc.pid).children(recursive = True)])
+
+            for line in iter(proc.stdout.readline, b''):
+                pulllog.info(line.strip())
+            while proc.poll() is None:
+                pass
+
+            proc.stdout.close()
+
+        log.debug('pull subprocess finished. return code: %s',proc.returncode)
+        if proc.returncode:
+            log.error('non-zero return code raising exception')
+            raise subprocess.CalledProcessError(returncode =  proc.returncode, cmd = docker_pull_cmd)
+        log.debug('moving on from pull')
+    except RuntimeError as e:
+        log.exception('caught RuntimeError')
+        raise e
+    except subprocess.CalledProcessError as exc:
+        log.exception('subprocess failed. code: %s,  command %s',exc.returncode,exc.cmd)
+        raise RuntimeError('failed container image pull subprocess in docker_enc_handler.')
+    except:
+        log.exception("Unexpected error: %s",sys.exc_info())
+        raise
+    finally:
+        log.debug('finally for pull')
+
+
+@executor('docker-encapsulated')
+def docker_enc_handler(environment,state,job,metadata):
+    with logutils.setup_logging_topic(metadata,state,'step',return_logger = True) as log:
+        if 'command' in job:
+            stdin = False
+            in_docker_cmd, stdin = run_docker_with_oneliner(state,environment,job['command'],log)
+        elif 'script' in job:
+            stdin = True
+            in_docker_cmd, stdin = run_docker_with_script(state,environment,job,log)
+        else:
+            raise RuntimeError('do not know yet how to run this...')
+
+        container_argv  = ['sh', '-c', in_docker_cmd]
+        container_stdin = stdin
+
+        cmdline = docker_execution_cmdline(
+            state,environment,log,metadata,
+            stdin = stdin,
+            cmd_argv = container_argv
+        )
+
+        if 'PACKTIVITY_DOCKER_NOPULL' not in os.environ:
+            log.info('prepare pull')
+            docker_pull_cmd = 'docker pull {container}:{tag}'.format(
+                container = environment['image'],
+                tag = environment['imagetag']
+            )
+            docker_pull(docker_pull_cmd,log,state,metadata)
+        execute_docker(metadata,state,log,cmdline, stdin_content = container_stdin)
+
+@executor('noop-env')
+def noop_env(environment,state,job,metadata):
+    with logutils.setup_logging_topic(metadata,state,'step',return_logger = True) as log:
+        log.info('state is: %s',state)
+        log.info('would be running this job: %s',job)
+
+@executor('localproc-env')
+def localproc_env(environment,state,job,metadata):
+    with logutils.setup_logging_topic(metadata,state,'step',return_logger = True) as log:
+        olddir = os.path.realpath(os.curdir)
+        workdir = state.readwrite[0]
+        try:
+            log.info('changing to workdirectory %s',workdir)
+            utils.mkdir_p(workdir)
+            os.chdir(workdir)
+            shell = ['sh', '-c', str(job['command'])]
+            # shell = ['sh','-c','echo hello world']
+            log.info('running %s', shell)
+            subprocess.check_call(shell)
+        except:
+            log.exception('local job failed. job: %s',job)
+            raise RuntimeError('failed')
+        finally:
+            log.info('changing back to original directory %s',olddir)
+            os.chdir(olddir)
+
+@executor('manual-env')
+def manual_env(environment,state,job,metadata):
+    instructions = environment['instructions']
+    ctx = yaml.safe_dump(state,default_flow_style = False)
+    click.secho(instructions, fg = 'blue')
+    click.secho(ctx, fg = 'cyan')
+
+@executor('test-env')
+def test_process(environment,state,job,metadata):
+    with logutils.setup_logging_topic(metadata,state,'step',return_logger = True) as log:
+        log.info('a complicated test environment')
+        log.info('job:  {}'.format(job))
+        log.info('env:  {}'.format(environment))
+        log.info('state {}'.format(state))
---
+++ packages/packtivity/packtivity-0.11.2.tar.gz/packtivity-0.11.2/packtivity/syncbackends.py
@@ -0,0 +1,110 @@
+import yaml
+import os
+
+import packtivity.logutils as logutils
+from packtivity.typedleafs import TypedLeafs
+
+class packconfig(object):
+    def __init__(self,**kwargs):
+        self.handler_selection = kwargs
+        fromenv = os.environ.get('PACKTIVITY_SYNCCONFIGFILE',None)
+        if fromenv:
+            override = yaml.load(open(fromenv))
+            self.handler_selection.update(**override)
+
+    def get_impl(self,category,handler):
+        try:
+            return self.handler_selection[category][handler]
+        except KeyError:
+            return 'default'
+
+def build_job(process,parameters,state,pack_config):
+    '''
+    takes a process template and builds a job out of it using a handler.
+    '''
+    proc_type =  process['process_type']
+    impl = pack_config.get_impl('process',proc_type)
+    from .handlers.process_handlers import handlers as proc_handlers
+    handler = proc_handlers[proc_type][impl]
+    return handler(process,parameters, state)
+
+def build_env(environment,parameters,state,pack_config):
+    '''
+    builds an environment template description and builds a fully-defined env
+    this will use a handler in the future (just as build_job)
+    '''
+
+    env_type =  environment['environment_type']
+    impl = pack_config.get_impl('environment',env_type)
+    from .handlers.environment_handlers import handlers as env_handlers
+    try:
+        handler = env_handlers[env_type][impl]
+    except KeyError:
+        handler = env_handlers['default']['default']
+    return handler(environment,parameters,state)
+
+def run_in_env(environment,job,state,metadata,pack_config):
+    '''
+    takes a job and an environment and executes with the state context attached
+    '''
+    env_type = environment['environment_type']
+    impl = pack_config.get_impl('executor',env_type)
+    from .handlers.execution_handlers import handlers as exec_handlers
+    handler = exec_handlers[env_type][impl]
+    return handler(environment,state,job,metadata)
+
+def publish(publisher,parameters,state, pack_config):
+    pub_type   = publisher['publisher_type']
+    impl = pack_config.get_impl('publisher',pub_type)
+    from .handlers.publisher_handlers import handlers as pub_handlers
+    handler = pub_handlers[pub_type][impl]
+    pubdata = handler(publisher,parameters,state)
+    return TypedLeafs(pubdata, state.datamodel if state else None)
+
+def model_parameters(parameters, state):
+    parameters = TypedLeafs(parameters,state.datamodel if state else None)
+    if not state: return parameters
+    return state.model(parameters)
+
+def prepublish(spec, parameters, state, pack_config):
+    '''
+    attempts to prepublish output data, returns None if not possible
+    '''
+    parameters = model_parameters(parameters, state)
+    pub = spec['publisher']
+
+    if pub['publisher_type'] in ['frompar-pub','constant-pub']:
+        return publish(pub,parameters,state,pack_config)
+    if pub['publisher_type'] in ['interpolated-pub', 'fromparjq-pub']:
+        from .statecontexts.posixfs_context import LocalFSState
+        if not state:
+            return publish(pub,parameters,state,pack_config)
+        if type(state) == LocalFSState:
+            if pub['glob'] == False or len(state.readwrite)==0:
+                return publish(pub,parameters,state,pack_config)
+    return None
+
+def run_packtivity(spec, parameters,state,metadata,config):
+    with logutils.setup_logging_topic(metadata,state,'step',return_logger = True) as log:
+        try:
+            parameters = model_parameters(parameters, state)
+            if spec['process'] and spec['environment']:
+                job = build_job(spec['process'], parameters, state, config)
+                env = build_env(spec['environment'], parameters, state, config)
+                run_in_env(env,job,state,metadata,config)
+            pubdata = publish(spec['publisher'], parameters,state, config)
+            log.info('publishing data: %s',pubdata)
+            return pubdata
+        except:
+            log.exception('%s raised exception',metadata)
+            raise
+
+class defaultsyncbackend(object):
+    def __init__(self,packconfig_spec = None):
+        self.config = packconfig(**packconfig_spec) if packconfig_spec else packconfig()
+
+    def prepublish(self,spec, parameters, state):
+        return prepublish(spec, parameters, state, self.config)
+
+    def run(self,spec,parameters,state, metadata = {'name': 'packtivity_syncbackend'}):
+        return run_packtivity(spec,parameters,state,metadata,self.config)
