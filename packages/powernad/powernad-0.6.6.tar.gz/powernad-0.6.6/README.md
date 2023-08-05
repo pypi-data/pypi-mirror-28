47ca68eca966394ed884bc8162c39bc3363bcf94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:36 +0000

    Add package powernad/0.6.6/powernad-0.6.6.tar.gz

---
+++ packages/powernad/powernad-0.6.6.tar.gz/powernad-0.6.6/README.md
@@ -0,0 +1,6 @@
+# adCloud-NaverSearchAdAPI
+
+
+* 네이버 광고 API Reference
+  - https://github.com/naver/searchad-apidoc
+
---
+++ packages/powernad/powernad-0.6.6.tar.gz/powernad-0.6.6/powernad/Object/Target/RequestObject/UpdateTargetObject.py
@@ -0,0 +1,7 @@
+class UpdateTargetObject:
+    def __init__(self, nccTargetId, ownerId, target, targetTp):
+        
+        self.nccTargetId = nccTargetId
+        self.ownerId = ownerId
+        self.target = target
+        self.targetTp = targetTp
