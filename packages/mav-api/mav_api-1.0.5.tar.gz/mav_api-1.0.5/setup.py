f36669bf6c385632ec5b891e8cda088efcdbfa61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:40 +0000

    Add package mav-api/1.0.5/mav_api-1.0.5.tar.gz

---
+++ packages/mav-api/mav_api-1.0.5.tar.gz/mav_api-1.0.5/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+import mavapi.api
+
+setup(
+    name='mav_api',
+    version=mavapi.api.__version__,
+    packages=['mavapi'],
+    url='https://github.com/Ar4ikov/mavapi',
+    license='Apache 2.0',
+    author='Nikita Archikov',
+    author_email='bizy18588@gmail.com',
+    description='A simple MAV API wrapper for python (3.5 or newer)'
+)
