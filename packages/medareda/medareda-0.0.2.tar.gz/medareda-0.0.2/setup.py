e0f24f2314110751e002c1d148ff2683a865cf40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:02 +0000

    Add package medareda/0.0.2/medareda-0.0.2.tar.gz

---
+++ packages/medareda/medareda-0.0.2.tar.gz/medareda-0.0.2/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+  name = 'medareda',
+  py_modules = ['medareda'],
+  packages = ['medareda'], # this must be the same as the name above
+  version = '0.0.2',
+  description = 'MedaReda Test package',
+  author = 'Mark Hallett',
+  author_email = 'pp@markhallett.co.uk',
+  url = 'https://github.com/dev-area/medareda',
+  download_url = 'https://github.com/dev-area/medareda/tarball/0.0.2',
+  keywords = ['test', 'medareda', 'version0.0.2' ], 
+  classifiers = [],
+)
