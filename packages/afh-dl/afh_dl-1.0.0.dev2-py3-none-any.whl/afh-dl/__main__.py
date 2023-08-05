dfa4ae190209c4d4800b0cb1c44731b28e28c927
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:51 +0000

    Add package afh-dl/1.0.0.dev2/afh_dl-1.0.0.dev2-py3-none-any.whl

---
+++ packages/afh-dl/afh_dl-1.0.0.dev2-py3-none-any.whl/afh-dl/__main__.py
@@ -0,0 +1,4 @@
+import afh_dl
+
+if __name__ == '__main__':
+    afh_dl.main()
---
+++ packages/afh-dl/afh_dl-1.0.0.dev2-py3-none-any.whl/afh_dl/__main__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import afh_dl
+
+if __name__ == '__main__':
+    afh_dl.main()
