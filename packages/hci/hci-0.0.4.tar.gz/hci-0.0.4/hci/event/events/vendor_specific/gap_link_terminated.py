e428e737bc8ddc57fa05277ff537db2d1c5347e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:14 +0000

    Add package hci/0.0.4/hci-0.0.4.tar.gz

---
+++ packages/hci/hci-0.0.4.tar.gz/hci-0.0.4/hci/event/events/vendor_specific/gap_link_terminated.py
@@ -0,0 +1,34 @@
+from struct import unpack_from
+
+from .. import VendorSpecificEvent
+
+
+class GAP_LinkTerminated(VendorSpecificEvent):
+    REASON = {0x08: 'Supervisor Timeout',
+              0x13: 'Peer Requested',
+              0x16: 'Host Requested',
+              0x22: 'Control Packet Timeout',
+              0x28: 'Control Packet Instant Passed',
+              0x3B: 'LSTO Violation',
+              0x3D: 'MIC Failure'}
+
+    @property
+    def conn_handle(self):
+        OFFSET, SIZE_OCTETS = 6, 2
+        conn_handle = self._get_data(OFFSET, SIZE_OCTETS)
+        return unpack_from('<H', conn_handle)[0]
+
+    @property
+    def reason(self):
+        OFFSET, SIZE_OCTETS = 8, 1
+        reason = self._get_data(OFFSET, SIZE_OCTETS)
+        return unpack_from('<B', reason)[0]
+
+    def __str__(self):
+        return super().__str__() + '\n' + '\n'.join([
+            'Connection Handle: {} ({})',
+            'Reason {} ({})']).format(
+            hex(self.conn_handle),
+            int(self.conn_handle),
+            hex(self.reason),
+            GAP_LinkTerminated.REASON[self.reason])
