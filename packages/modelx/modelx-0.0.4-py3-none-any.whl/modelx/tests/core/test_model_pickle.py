ac2c53e2a71cd87e81d30b12d5e1ad50ed35a6f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:56 +0000

    Add package modelx/0.0.4/modelx-0.0.4-py3-none-any.whl

---
+++ packages/modelx/modelx-0.0.4-py3-none-any.whl/modelx/tests/core/test_model_pickle.py
@@ -0,0 +1,69 @@
+from textwrap import dedent
+import pytest
+import pickle
+
+from modelx.core.api import *
+from modelx.core.cells import CellArgs
+from modelx.core.base import get_interfaces
+
+
+# ---- Test impl ----
+
+@pytest.fixture
+def pickletest():
+
+    model = new_model('TestModel')
+    space = model.new_space()
+
+    func1 = dedent("""\
+    def single_value(x):
+        return 5 * x
+    """)
+
+    func2 = dedent("""\
+    def mult_single_value(x):
+        return 2 * single_value(x)
+    """)
+
+    func1 = space.new_cells(func=func1)
+    func2 = space.new_cells(func=func2)
+
+    func2(5)
+
+    byte_obj = pickle.dumps(model._impl)
+    unpickled = pickle.loads(byte_obj)
+
+    return [model._impl, unpickled]
+
+
+def test_unpickled_model(pickletest):
+
+    model, unpickeld = pickletest
+
+    errors = []
+
+    if not model.name == unpickeld.name:
+        errors.append("name did not match")
+
+    if not hasattr(model, 'interface'):
+        errors.append("no interface")
+
+    if not hasattr(model, 'cellgraph'):
+        errors.append("no cellgraph")
+
+    assert not errors, "errors:\n{}".format("\n".join(errors))
+
+
+
+def x_test_model_load(simplemodel):
+
+    simplemodel.save('data\simplemodel')
+
+    model = load_model('data\simplemodel')
+
+    # print(model.spaces)
+    # space = simplemodel.currentspace
+    # simplemodel.save('data\simplemodel')
+    # print(space.fibo[10])
+
+
