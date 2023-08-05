f4b5535b50a918aa92a27d31eefa4e460680f690
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:49 +0000

    Add package seek-well/0.1.0/seek-well-0.1.0.tar.gz

---
+++ packages/seek-well/seek-well-0.1.0.tar.gz/seek-well-0.1.0/setup.py
@@ -0,0 +1,22 @@
+import os
+
+from setuptools import setup
+
+import scripts
+
+project_base_url = 'https://github.com/lycantropos/seek-well/'
+setup(name='seek-well',
+      scripts=[os.path.join('scripts', 'seek-well')],
+      version=scripts.__version__,
+      description=scripts.__doc__,
+      long_description=open('README.rst').read(),
+      author='Azat Ibrakov',
+      author_email='azatibrakov@gmail.com',
+      url=project_base_url,
+      download_url=project_base_url + 'archive/master.zip',
+      keywords=['sql'],
+      install_requires=[
+          'sqlparse>=0.2.4',  # parsing SQL statements
+          'graphviz>=0.8.2',
+          'click>=6.7',  # parsing command-line arguments
+      ])
