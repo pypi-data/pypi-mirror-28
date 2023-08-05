0fc49146c91448792c9dd3e08c8ad7cc4d26661d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:21 +0000

    Add package dallar-subsidy/1.2/dallar_subsidy-1.2.zip

---
+++ packages/dallar-subsidy/dallar_subsidy-1.2.zip/dallar_subsidy-1.2/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup, Extension
+
+dallar_subsidy_module = Extension('dallar_subsidy', sources = ['dallar_subsidy.cpp'])
+
+setup (name = 'dallar_subsidy',
+    version = '1.2',
+    description = 'Subsidy function for Dallar',
+    maintainer = 'Allar',
+    maintainer_email = 'allar@gamemak.in',
+    url = 'https://github.com/dallar-project/dallar-subsidy-python',
+    keywords = ['dallar', 'dal', 'throestl'],
+    ext_modules = [dallar_subsidy_module])
