b05e2cb1a258a72d67f15313d3b5043e99068d3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:52 +0000

    Add package nn/0.0.0/nn-0.0.0.tar.gz

---
+++ packages/nn/nn-0.0.0.tar.gz/nn-0.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='nn',
+    version='0.0.0',
+    description='Neural Networks',
+    author='Ravindra Marella',
+    author_email='mv.ravindra007@gmail.com',
+    license='MIT',
+    packages=['nn'],
+    install_requires=['tensorflow'],
+    zip_safe=False
+)
