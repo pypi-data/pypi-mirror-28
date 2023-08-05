113d09b0030dde132bd8165e02e84ec3e2138695
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:53 +0000

    Add package allsortalgo/0.0.1/allsortalgo-0.0.1.tar.gz

---
+++ packages/allsortalgo/allsortalgo-0.0.1.tar.gz/allsortalgo-0.0.1/README.md
@@ -0,0 +1,18 @@
+[![Build Status](https://travis-ci.org/sannykr/allsortalgo.svg?branch=master)](https://travis-ci.org/sannykr/allsortalgo)
+
+Sorting Algorithms
+========================
+
+This project includes implementations of the following sorting algorithms:
+
+1. Bubble sort: In bubble sort, we repeatedly swap the adjancent elements if they are in wrong order. It's time complexity is O(N*N)
+
+2. Insertion sort: In insertion sort, we start with a sub-list of size 2, and insert the right-most elements to the proper location 
+   in such a way that the list remains sorted. It's time complexity is O(N*N)
+
+3. merge sort: In merge sort, we use divide and conquer strategy and sort the sublists and merge them recursively. It's time complexity
+   is O(N*logN)
+
+I will be adding more sorting algorithms to this project.    
+
+---------------
---
+++ packages/allsortalgo/allsortalgo-0.0.1.tar.gz/allsortalgo-0.0.1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+CLASSIFIERS = [
+    'Development Status :: 1',
+    'Operating System :: OS Independent',
+    'Intended Audience :: Science/Research',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Topic :: Scientific/Engineering']
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='allsortalgo',
+    version='0.0.1',
+    description='Python package containing implementation of all sorting algorithms',
+    long_description=readme,
+    author='Sanny Kumar',
+    author_email='sanny.iitkgp@gmail.com',
+    url='https://github.com/sannykr/allsortalgo',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs')),
+    classifiers=CLASSIFIERS
+)
+
