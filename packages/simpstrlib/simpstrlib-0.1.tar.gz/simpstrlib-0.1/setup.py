602e4e803fb75ea5a7e60b1184eed4f4ea82a77c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:05 +0000

    Add package simpstrlib/0.1/simpstrlib-0.1.tar.gz

---
+++ packages/simpstrlib/simpstrlib-0.1.tar.gz/simpstrlib-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'simpstrlib',
+  packages = ['simpstrlib'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Simple String library',
+  author = 'Liran BH',
+  author_email = 'liran@gmail.com',
+  url = 'https://github.com/dev-area/simpstrlib',
+  download_url = 'https://github.com/dev-area/simpstrlib/tarball/0.1',
+  keywords = ['string', 'reverse'], 
+  classifiers = [],
+)
