3576216fbdd677388bb1a1513f12306b0a7e5e99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:56 +0000

    Add package sandwichcloud-deli/0.0.11/sandwichcloud_deli-0.0.11-py3-none-any.whl

---
+++ packages/sandwichcloud-deli/sandwichcloud_deli-0.0.11-py3-none-any.whl/deli/kubernetes/resources/const.py
@@ -0,0 +1,14 @@
+GROUP = "sandwichcloud.com"
+
+ID_LABEL = GROUP + '/id'
+NAME_LABEL = GROUP + '/name'
+PROJECT_LABEL = GROUP + '/project'
+REGION_LABEL = GROUP + '/region'
+ZONE_LABEL = GROUP + '/zone'
+IMAGE_LABEL = GROUP + '/image'
+NETWORK_LABEL = GROUP + '/network'
+NETWORK_PORT_LABEL = GROUP + '/network_port'
+SERVICE_ACCOUNT_LABEL = GROUP + '/service_account'
+TAG_LABEL = 'tag.' + GROUP
+IMAGE_VISIBILITY_LABEL = GROUP + '/visibility'
+IMAGE_MEMBER_LABEL = 'image-member.' + GROUP
