f885e99857db62c02137cd315074406e109f886c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:42 +0000

    Add package fladm/1.2/fladm-1.2-py2.7.egg

---
+++ packages/fladm/fladm-1.2-py2.7.egg/fladm/fl3/__init__.py
@@ -0,0 +1,147 @@
+
+
+import urllib
+import urllib2
+import requests
+from requests_toolbelt.multipart.encoder import MultipartEncoder
+import json
+
+
+'''
+f = fl3.File(domain='flamingo.exem-oss.org')
+f.login('user', 'pw')
+f.delete('/user/hive/password/README.md')
+f.upload_file('README.md', '/user/hive/password/README.md')
+
+# TO BE
+f.chmod('/user/hive/password/README.md', '755')
+f.chown('/user/hive/password/README.md', 'hdfs:hdfs')
+'''
+
+def build_headers(raw):
+    headers = {}
+
+    for line in raw.split('\n'):
+        line = line.strip()
+        if len(line) > 0:
+            t = line.lstrip().split(':')
+            headers[t[0]] = t[1].lstrip()
+
+    return headers
+
+def get_filename(filename):
+    return filename.split('/')[-1]
+
+class File:
+    def __init__(self, domain):
+        self.domain = domain
+
+    domain = 'flamingo.exem-oss.org'
+    cookie = None
+
+    def login(self, username, password):
+        headers = {
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+            # 'Accept-Encoding': 'gzip, deflate',
+            'Accept-Language': 'ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7',
+            'Cache-Control': 'no-cache',
+            'Connection': 'keep-alive',
+            # 'Content-Length': '28',
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Host': 'flamingo.exem-oss.org',
+            'Origin': 'http://flamingo.exem-oss.org',
+            'Pragma': 'no-cache',
+            'Referer': 'http://flamingo.exem-oss.org/login',
+            # 'Upgrade-Insecure-Requests': '1',
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36'
+        }
+
+        url = 'http://%s/login' % self.domain
+
+        data = urllib.urlencode({
+            'username': username,
+            'password': password
+        })
+
+        s = requests.session()
+        s.headers.update(headers)
+
+        s.get(url)
+        r = s.post(url, data=data, allow_redirects=False)
+
+        cookie = 'JSESSIONID=%s' % r.cookies.get('JSESSIONID')
+
+        # set cookie to server
+        headers['Cookie'] = cookie
+        s.headers.update(headers)
+        s.get(url)
+
+        self.cookie = cookie
+
+    def get_headers(self):
+        header_raw = '''
+            Host: flamingo.exem-oss.org
+            Connection: keep-alive
+            Content-Length: 19542
+            X-CSRF-Token:
+            Origin: http://%s
+            X-Requested-With: XMLHttpRequest
+            User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36
+            Content-Type: application/json; charset=UTF-8;
+            Accept: */*
+            Referer: http://flamingo.exem-oss.org/
+            Accept-Encoding: gzip, deflate
+            Accept-Language: ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7
+            Cookie: %s
+        ''' % (self.domain, self.cookie)
+
+        return build_headers(header_raw)
+
+    def delete(self, filename):
+        headers = self.get_headers()
+        url = 'http://%s/fs/hdfs/delete.json' % self.domain
+
+        data = json.dumps({
+            'currentPath': '/',
+            'deleteList': [{
+                'fullyQualifiedPath': filename,
+                'directory': False
+            }]
+        })
+
+        r = requests.post(url, data=data, headers=headers)
+        return json.loads(r.content)['success']
+
+    def permission(self, filename, group="hdfs", owner="hdfs", permission="644"):
+        headers = self.get_headers()
+        url = 'http://%s/fs/hdfs/setPermission' % self.domain
+
+        data = json.dumps({
+            'currentPath': filename,
+            'fileName': get_filename(filename),
+            'fileStatus': 'FILE'
+        })
+
+        r = requests.post(url, data=data, headers=headers)
+        return json.loads(r.content)['success']
+
+    def upload_file(self, filename, dest_filename, overwrite=False):
+        if overwrite:
+            self.delete(dest_filename)
+
+        headers = self.get_headers()
+        url = 'http://%s/fs/hdfs/upload.json' % self.domain
+
+        dstPath = '/'.join(dest_filename.split('/')[0:-1])
+        fileName = get_filename(dest_filename)
+
+        form_data = MultipartEncoder(fields={
+            'dstPath': dstPath,
+            'fileName': (fileName, open(filename, 'rb'))
+        })
+
+        headers['Content-Type'] = form_data.content_type
+
+        r = requests.post(url, data=form_data, headers=headers)
+
+        return json.loads(r.content)['success']
---
+++ packages/fladm/fladm-1.2-py2.7.egg/fladm/upload.py
@@ -0,0 +1,68 @@
+import os
+import fl3
+import sys
+import argparse
+
+summary = 'Upload file to HDFS (using Flamingo REST API)'
+desc = '''
+    %s
+    $ fladm upload username:password@domain [local filename] [dest filename]
+''' % summary
+
+def print_help():
+    print ('*' * 80)
+    print (' ' * 25 + 'upload')
+    print ('*' * 80)
+    print (desc)
+    print ('*' * 80)
+    print ('*' * 80)
+
+    print ('ex)')
+    for usage in get_usage_examples():
+        print ('\t' + usage)
+
+def get_usage_examples():
+    return [
+        '$ fladm upload exem:1234@flamingo-exem.org workflow.xml /flamingo/oozie/test-wf/workflow.xml'
+    ]
+
+def main_upload(argv):
+    sys.argv = argv
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-username", help="user name")
+    parser.add_argument("-password", help="input password")
+    parser.add_argument("-domain", help="domain")
+    parser.add_argument("-source", help="Local filename")
+    parser.add_argument("-dest", help="HDFS filename")
+
+    parser.add_argument("-own", help="Owner")
+    parser.add_argument("-group", help="Group")
+
+    args = parser.parse_args()
+
+    print args
+
+    # if len(argv) >= 4:
+    #     connect_info = argv[2]
+    #     local_filename = argv[3]
+    #     dest_filename = argv[4]
+    #
+    #     username = connect_info.split(':')[0]
+    #     password = connect_info.split(':')[1].split('@')[0]
+    #     domain = connect_info.split('@')[1]
+    #
+    #     # verify local file
+    #     if os.path.isfile(local_filename) == False:
+    #         print "Not exists file '%s'" % local_filename
+    #         exit(1)
+    #
+    #     f = fl3.File(domain)
+    #     f.login(username, password)
+    #     r = f.upload_file(local_filename, dest_filename, True)
+    #
+    #     if r:
+    #         print "Upload Success. Local file '%s' -> HDFS file '%s' " % (local_filename, dest_filename)
+    #
+    # else:
+    #   print_help()
