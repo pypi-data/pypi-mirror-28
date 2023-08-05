6c499b819494e7d1f7e3fbc5cc593f4912f80413
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:38 +0000

    Add package juspayp3/1.0.0/juspayp3-1.0.0.tar.gz

---
+++ packages/juspayp3/juspayp3-1.0.0.tar.gz/juspayp3-1.0.0/README.md
@@ -0,0 +1,26 @@
+# juspay-expresscheckout-python3-client
+
+This is version is ported from existing expresscheckout-python-client.
+
+## Reference
+
+[expresscheckout-python-client](https://bitbucket.org/juspay/expresscheckout-python-client/)
+[juspay docs](https://juspay.in/docs/advanced/ec/)
+
+## tools used
+
+2to3
+
+## requirements
+
+certifi==2017.11.5
+
+chardet==3.0.4
+
+idna==2.6
+
+juspay==1.1.2
+
+requests==2.18.4
+
+urllib3==1.22
---
+++ packages/juspayp3/juspayp3-1.0.0.tar.gz/juspayp3-1.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(name='juspayp3',
+      version='1.0.0',
+      description='Python 3 version of expresscheckout-python-client - JusPay Express Checkout API',
+      url='https://github.com/dhavalsavalia/juspay-expresscheckout-python3-client',
+      author='Dhaval Savalia',
+      author_email='dhaval.savalia6@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      keywords='juspay expresscheckout payments',
+      install_requires = ['requests'],
+      )
