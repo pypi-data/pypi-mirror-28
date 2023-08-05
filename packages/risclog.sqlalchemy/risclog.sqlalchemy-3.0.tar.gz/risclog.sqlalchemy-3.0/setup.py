d6ca0723c08e29d1667292f5fbc315dbfa1bd73c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:20 +0000

    Add package risclog.sqlalchemy/3.0/risclog.sqlalchemy-3.0.tar.gz

---
+++ packages/risclog.sqlalchemy/risclog.sqlalchemy-3.0.tar.gz/risclog.sqlalchemy-3.0/setup.py
@@ -0,0 +1,84 @@
+# This should be only one line. If it must be multi-line, indent the second
+# line onwards to keep the PKG-INFO file format intact.
+"""Encapsulate sqlalchemy modelling infrastructure.
+"""
+
+from setuptools import setup, find_packages
+import glob
+
+
+setup(
+    name='risclog.sqlalchemy',
+    version='3.0',
+
+    install_requires=[
+        'SQLAlchemy >= 1.0',
+        'alembic >= 0.7',
+        'pytz',
+        'setuptools',
+        'zope.component >= 4.0.1, <= 4.4.1',
+        'zope.interface',
+        'zope.sqlalchemy',
+    ],
+
+    extras_require={
+        'test': [
+            'gocept.testdb',
+        ],
+        'self-test': [
+            'mock',
+            'pyramid',
+            'pytest',
+        ],
+        'python2': [
+            'plone.testing[test]'
+        ],
+        'pyramid': [
+            'pyramid'
+        ],
+    },
+
+    entry_points={},
+
+    author='gocept <mail@gocept.com>',
+    author_email='mail@gocept.com',
+    license='ZPL 2.1',
+    url='https://bitbucket.org/gocept/risclog.sqlalchemy/',
+
+    keywords='sqlalchemy alembic model infrastructure utility',
+    classifiers="""\
+Development Status :: 5 - Production/Stable
+Framework :: Pyramid
+Intended Audience :: Developers
+License :: OSI Approved
+License :: OSI Approved :: Zope Public License
+Natural Language :: English
+Operating System :: OS Independent
+Programming Language :: Python
+Programming Language :: Python :: 2
+Programming Language :: Python :: 2.7
+Programming Language :: Python :: 3
+Programming Language :: Python :: 3.4
+Programming Language :: Python :: 3.5
+Programming Language :: Python :: 3.6
+Programming Language :: Python :: Implementation
+Programming Language :: Python :: Implementation :: CPython
+Programming Language :: Python :: Implementation :: PyPy
+Topic :: Database
+Topic :: Software Development :: Libraries
+Topic :: Software Development :: Libraries :: Python Modules
+"""[:-1].split('\n'),
+    description=__doc__.strip(),
+    long_description=(
+        open('README.rst').read() +
+        '\n\n' +
+        open('HACKING.rst').read() +
+        '\n\n' +
+        open('CHANGES.rst').read()),
+    namespace_packages=['risclog'],
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    data_files=[('', glob.glob('*.rst'))],
+    zip_safe=False,
+)
