7476bb2bfd4f6f63cbb47c4cdc6452074fb3efbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:38 +0000

    Add package conx/3.5.4/conx-3.5.4.tar.gz

---
+++ packages/conx/conx-3.5.4.tar.gz/conx-3.5.4/conx/datasets/_cifar10.py
@@ -0,0 +1,13 @@
+import numpy as np
+from keras.utils import to_categorical
+
+def cifar10(dataset):
+    from keras.datasets import cifar10
+    (x_train, y_train), (x_test, y_test) = cifar10.load_data()
+    inputs = np.concatenate((x_train, x_test))
+    labels = np.concatenate((y_train, y_test))
+    targets = to_categorical(labels, 10)
+    labels = np.array([str(label[0]) for label in labels], dtype=str)
+    inputs = inputs.astype('float32')
+    inputs /= 255
+    dataset.load_direct([inputs], [targets], [labels])
