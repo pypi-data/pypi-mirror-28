0623993a898f2687e95eccfe1113ac24b1331993
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:49 +0000

    Add package HermesCache/0.7.2/HermesCache-0.7.2.tar.gz

---
+++ packages/HermesCache/HermesCache-0.7.2.tar.gz/HermesCache-0.7.2/setup.py
@@ -0,0 +1,32 @@
+try:
+  from setuptools import setup
+except ImportError:
+  from distutils.core import setup
+
+
+setup(
+  name         = 'HermesCache',
+  version      = '0.7.2',
+  author       = 'saaj',
+  author_email = 'mail@saaj.me',
+  packages     = ['hermes', 'hermes.backend', 'hermes.test'],
+  test_suite   = 'hermes.test',
+  url          = 'https://bitbucket.org/saaj/hermes',
+  license      = 'LGPL-2.1+',
+  description  = 'Python caching library with tag-based invalidation '
+    'and dogpile effect prevention',
+  long_description = open('README.txt', 'rb').read().decode('utf-8'),
+  platforms        = ['Any'],
+  keywords         = 'python cache tagging redis memcached',
+  classifiers      = [
+    'Topic :: Software Development :: Libraries',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Intended Audience :: Developers'
+  ]
+)
+
