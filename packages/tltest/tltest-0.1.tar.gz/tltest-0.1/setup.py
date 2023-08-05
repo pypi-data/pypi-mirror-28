fafe83912da403113539421eeab945e9eab89a65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:11 +0000

    Add package tltest/0.1/tltest-0.1.tar.gz

---
+++ packages/tltest/tltest-0.1.tar.gz/tltest-0.1/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+#coding=utf-8
+
+from setuptools import setup, find_packages
+setup(
+name = "tltest",
+version = "0.1",
+packages = find_packages(),
+# scripts = [''],
+
+# Project uses reStructuredText, so ensure that the docutils get
+# installed or upgraded on the target machine
+install_requires = [''],
+
+package_data = {},
+
+# metadata for upload to PyPI
+author = "admin",
+author_email = "admin@yjadd.com",
+description = "test",
+license = "Apache2",
+keywords = "tltest",
+url = "http://www.yjadd.com/",
+)
---
+++ packages/tltest/tltest-0.1.tar.gz/tltest-0.1/tltest/test.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+#coding=utf-8
+
+def sum(n):
+    sum = 0
+    for i in range(n+1):
+        sum += i
+    return sum
