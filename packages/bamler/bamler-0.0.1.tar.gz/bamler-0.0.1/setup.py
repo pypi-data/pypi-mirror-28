913fe93a7edebfa456eb739e9938323f5e6d9dae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:57 +0000

    Add package bamler/0.0.1/bamler-0.0.1.tar.gz

---
+++ packages/bamler/bamler-0.0.1.tar.gz/bamler-0.0.1/setup.py
@@ -0,0 +1,16 @@
+# flake8: noqa
+from distutils.core import setup
+
+setup(
+    name = "bamler",
+    packages = ["bamler"],
+    version = "0.0.1",
+    description = "Machine Learning Lib",
+    author = "Darshan Chaudhary",
+    author_email = "deathbullet@gmail.com",
+    url = "https://github.com/baml-meetup/bamler",
+    download_url = "https://github.com/baml-meetup/bamler/archive/0.0.1.tar.gz",
+    keywords = ["neural networks", "machine learning"],
+    license = "LICENSE.txt",
+    classifiers = [],
+)
