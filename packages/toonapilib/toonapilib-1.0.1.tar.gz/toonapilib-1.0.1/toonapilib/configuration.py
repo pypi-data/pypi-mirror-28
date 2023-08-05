18618535e1fc01bb314ab496dcd75c45a0e20317
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:32 +0000

    Add package toonapilib/1.0.1/toonapilib-1.0.1.tar.gz

---
+++ packages/toonapilib/toonapilib-1.0.1.tar.gz/toonapilib-1.0.1/toonapilib/configuration.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+# File: configuration.py
+"""A place to store the configuration."""
+
+
+STATE_CACHING_SECONDS = 30
+REQUEST_TIMEOUT = 9
+
+STATES = {0: 'Comfort',
+          1: 'Home',
+          2: 'Sleep',
+          3: 'Away',
+          4: 'Unknown',
+          5: 'Unknown'}
