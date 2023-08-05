ed96d6e9759426167284c0a0b2aab94f61d99531
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:23 +0000

    Add package mypackage9645/0.1/mypackage9645-0.1.tar.gz

---
+++ packages/mypackage9645/mypackage9645-0.1.tar.gz/mypackage9645-0.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(
+  name = 'mypackage9645',
+  packages = ['mypackage9645'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A random test lib',
+  author = 'Peter Downs',
+  author_email = 'anonimatanonimus@gmail.com',
+  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  classifiers = [],
+)
