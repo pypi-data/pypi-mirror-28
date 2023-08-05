956988c1230b0bf7e82d3f87931a6a2969fec74f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:32 +0000

    Add package m-lib.full/1.0.1/m_lib.full-1.0.1-py3.4.egg

---
+++ packages/m-lib.full/m_lib.full-1.0.1-py3.4.egg/m_lib/__init__.py
@@ -0,0 +1,5 @@
+"""Broytman Library for Python, Copyright (C) 1996-2018 PhiloSoft Design"""
+
+from pkgutil import extend_path
+__path__ = extend_path(__path__, __name__)
+__import__('pkg_resources').declare_namespace(__name__)
