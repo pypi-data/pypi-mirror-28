d443c3eb5b601ac898ad5c717999f81c7019b64a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:14 +0000

    Add package easytrader/0.12.4/easytrader-0.12.4.tar.gz

---
+++ packages/easytrader/easytrader-0.12.4.tar.gz/easytrader-0.12.4/easytrader/__init__.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+from .api import *
+from .webtrader import WebTrader
+from .gftrader import GFTrader
+from .joinquant_follower import JoinQuantFollower
+from .ricequant_follower import RiceQuantFollower
+from . import exceptions
+
+__version__ = '0.12.4'
+__author__ = 'shidenggui'
