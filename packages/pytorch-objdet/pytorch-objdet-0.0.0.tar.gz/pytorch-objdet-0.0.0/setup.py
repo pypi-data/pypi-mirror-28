ed86d8516a8091278968f280b5e25985aa0baa64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:33 +0000

    Add package pytorch-objdet/0.0.0/pytorch-objdet-0.0.0.tar.gz

---
+++ packages/pytorch-objdet/pytorch-objdet-0.0.0.tar.gz/pytorch-objdet-0.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(name='pytorch-objdet',
+      version='0.0.0',
+      description='Object Detection using PyTorch',
+      author='Khushhall Chandra Mahajan',
+      author_email='kcm00000@gmail.com',
+      url='https://github.com/khushhallchandra/pytorch-objdet',
+      license='MIT',
+      install_requires=['numpy'],
+      packages=find_packages())
