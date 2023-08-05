9dad45d2af509345ed6b006074cb51fdcf8b6d01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:12 +0000

    Add package jsonapi-requests/0.6.0/jsonapi-requests-0.6.0.tar.gz

---
+++ packages/jsonapi-requests/jsonapi-requests-0.6.0.tar.gz/jsonapi-requests-0.6.0/setup.py
@@ -0,0 +1,33 @@
+from pip.req import parse_requirements
+from setuptools import find_packages
+from setuptools import setup
+
+
+setup(
+    name='jsonapi-requests',
+    version='0.6.0',
+    description='Python client implementation for json api. http://jsonapi.org/',
+    author='Social WiFi',
+    author_email='it@socialwifi.com',
+    url='https://github.com/socialwifi/jsonapi-requests',
+    packages=find_packages(exclude=['tests']),
+    install_requires=[str(ir.req) for ir in parse_requirements('base_requirements.txt', session=False)],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest', 'flask'],
+    extras_require={
+        'flask': ['flask']
+    },
+    license='BSD',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ]
+)
