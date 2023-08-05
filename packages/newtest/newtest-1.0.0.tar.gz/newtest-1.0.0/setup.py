fdbf6274aaa4dcdad9b82b62ccfc6d0dbce61944
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:31 +0000

    Add package newtest/1.0.0/newtest-1.0.0.tar.gz

---
+++ packages/newtest/newtest-1.0.0.tar.gz/newtest-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='newtest',
+    version='1.0.0',
+    py_modules=['newtest'],
+    author='topwoo',
+    author_email='88215070@qq.com',
+    url='http://www.headfirstlabs.com',
+    description='A simple printer of nested lists',
+    )
