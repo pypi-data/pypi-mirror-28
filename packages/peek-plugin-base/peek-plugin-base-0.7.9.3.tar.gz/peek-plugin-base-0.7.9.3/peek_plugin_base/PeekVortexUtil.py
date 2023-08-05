b31d1628634f7c9de2aef16c5e4f9a0b16d45658
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:50 +0000

    Add package peek-plugin-base/0.7.9.3/peek-plugin-base-0.7.9.3.tar.gz

---
+++ packages/peek-plugin-base/peek-plugin-base-0.7.9.3.tar.gz/peek-plugin-base-0.7.9.3/peek_plugin_base/PeekVortexUtil.py
@@ -0,0 +1,19 @@
+
+
+#: The vortex name for the Server service
+peekServerName = "peek-server"
+
+#: The vortex name for the Client service
+peekClientName = "peek-client"
+
+#: The vortex name for the Agent service
+peekAgentName = "peek-agent"
+
+#: The vortex name for the Worker service
+peekWorkerName = "peek-worker"
+
+# #: The vortex name for the Mobile service
+# peekWorkerName = "peek-mobile"
+#
+# #: The vortex name for the Admin service
+# peekWorkerName = "peek-admin"
