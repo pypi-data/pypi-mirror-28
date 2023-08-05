41731ffe3146b10f2b7513e8c015f3343468b0ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:22 +0000

    Add package poloniex/0.0.13/poloniex-0.0.13.tar.gz

---
+++ packages/poloniex/poloniex-0.0.13.tar.gz/poloniex-0.0.13/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+with open('README.rst') as README:
+    long_description = README.read()
+    long_description = long_description[long_description.index('Description'):]
+
+setup(name='poloniex',
+      version='0.0.13',
+      description='Poloniex API client for humans',
+      long_description=long_description,
+      url='http://github.com/Aula13/poloniex',
+      author='Enrico Bacis, Daniele Ciriello',
+      author_email='enrico.bacis@gmail.com, ciriello.daniele@gmail.com',
+      license='MIT',
+      packages=['poloniex'],
+      setup_requires=['pytest-runner'],
+      install_requires=['requests', 'six'],
+      tests_require=['pytest', 'responses'],
+      keywords='poloniex cryptocurrency cryptocurrencies api client bitcoin'
+      )
