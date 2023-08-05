6c999293f829100dace81432f3d268e84ad43e3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:27 +0000

    Add package formly/0.13.0/formly-0.13.0.tar.gz

---
+++ packages/formly/formly-0.13.0.tar.gz/formly-0.13.0/setup.py
@@ -0,0 +1,43 @@
+import codecs
+
+from os import path
+from setuptools import find_packages, setup
+
+
+def read(*parts):
+    filename = path.join(path.dirname(__file__), *parts)
+    with codecs.open(filename, encoding="utf-8") as fp:
+        return fp.read()
+
+setup(
+    author="Patrick Altman",
+    author_email="paltman@eldarion.com",
+    description="a dynamic form generator",
+    name="formly",
+    long_description=read("README.rst"),
+    version="0.13.0",
+    url="https://github.com/eldarion/formly",
+    license="BSD",
+    packages=find_packages(),
+    package_data={
+        "formly": []
+    },
+    test_suite="runtests.runtests",
+    tests_require=[],
+    install_requires=[
+        "jsonfield>=1.0.3",
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    zip_safe=False
+)
