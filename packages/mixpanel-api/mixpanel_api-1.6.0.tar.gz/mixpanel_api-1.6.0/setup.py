278cc755efc09cba1cbeba3292a550ab5f95a348
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:22 +0000

    Add package mixpanel-api/1.6.0/mixpanel_api-1.6.0.tar.gz

---
+++ packages/mixpanel-api/mixpanel_api-1.6.0.tar.gz/mixpanel_api-1.6.0/setup.py
@@ -0,0 +1,12 @@
+#from distutils.core import setup
+from setuptools import setup
+setup(
+    name = 'mixpanel_api',
+    packages = ['mixpanel_api'],
+    version = '1.6.0',
+    description = 'tools for exporting and importing data into Mixpanel',
+    author='Jared McFarland',
+    author_email='support@mixpanel.com',
+    url='http://www.mixpanel.com'
+    
+)
