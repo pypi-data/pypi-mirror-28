14da0d6cb52ef3313fea12e34025ab906cee8a44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:54 +0000

    Add package propyte/2018.1.9.1816/propyte-2018.1.9.1816.tar.gz

---
+++ packages/propyte/propyte-2018.1.9.1816.tar.gz/propyte-2018.1.9.1816/setup.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+
+import setuptools
+
+def main():
+
+    setuptools.setup(
+        name             = "propyte",
+        version          = "2018.01.09.1816",
+        description      = "template Python program",
+        long_description = long_description(),
+        url              = "https://github.com/wdbm/propyte",
+        author           = "Will Breaden Madden",
+        author_email     = "wbm@protonmail.ch",
+        license          = "GPLv3",
+        py_modules       = [
+                           "propyte"
+                           ],
+        install_requires = [
+                           "docopt",
+                           "pushbullet.py",
+                           "pyprel",
+                           "pytg",
+                           "shijian",
+                           "technicolor"
+                           ],
+        scripts          = [
+                           "propyte_alert.py"
+                           ],
+        entry_points     = """
+                           [console_scripts]
+                           propyte = propyte:propyte
+                           """
+    )
+
+def long_description(
+    filename = "README.md"
+    ):
+
+    if os.path.isfile(os.path.expandvars(filename)):
+        try:
+            import pypandoc
+            long_description = pypandoc.convert_file(filename, "rst")
+        except ImportError:
+            long_description = open(filename).read()
+    else:
+        long_description = ""
+    return long_description
+
+if __name__ == "__main__":
+    main()
