dd9855d605937ec3c3626f4f74920a47dca95e2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:25 +0000

    Add package tree123/1.1.1/tree123-1.1.1.tar.gz

---
+++ packages/tree123/tree123-1.1.1.tar.gz/tree123-1.1.1/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name='tree123',
+    version='1.1.1',
+    py_modules=['tree1234'],
+    author='Slightbo',
+    author_email='18835385@qq.com',
+    url='https://upload.pypi.org/legacy/',
+    description='there is a small christmas tree'
+)
---
+++ packages/tree123/tree123-1.1.1.tar.gz/tree123-1.1.1/tree1234.py
@@ -0,0 +1,4 @@
+for i in range(1,18,2):       #7个*一行加两个 1 3 5 7 9 11 13 15 17
+    print(('*'*i).center(18)) #输出*在7个*长度中间
+for i in range(1,6,2):#531
+    print(('*'*(6-i)).center(18)) 
