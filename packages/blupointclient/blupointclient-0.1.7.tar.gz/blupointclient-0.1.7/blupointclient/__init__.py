672f50dae3da6b51a3f3f159b0fda219f6796c46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:10 +0000

    Add package blupointclient/0.1.7/blupointclient-0.1.7.tar.gz

---
+++ packages/blupointclient/blupointclient-0.1.7.tar.gz/blupointclient-0.1.7/blupointclient/__init__.py
@@ -0,0 +1,8 @@
+#from cls import demo_test
+
+class cms():
+    def __init__(self):
+        print("Demo Intialized")
+
+
+
---
+++ packages/blupointclient/blupointclient-0.1.7.tar.gz/blupointclient-0.1.7/blupointclient/cls.py
@@ -0,0 +1,4 @@
+class demo_test:
+
+    def test(self):
+        print("Test Done!")
