837dc0e68e09252d433c607c6672ffa36e0876a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:33 +0000

    Add package adsquery/0.1.0/adsquery-0.1.0.tar.gz

---
+++ packages/adsquery/adsquery-0.1.0.tar.gz/adsquery-0.1.0/adsquery/definitions.py
@@ -0,0 +1,17 @@
+journalAbbrevs = {
+    "Astronomy and Astrophysics": "A&A",
+    "Monthly Notices of the Royal Astronomical Society": "MNRAS",
+    "The Astrophysical Journal": "ApJ"
+}
+
+class bcolors:
+    '''Character code for colors in terminal'''
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
