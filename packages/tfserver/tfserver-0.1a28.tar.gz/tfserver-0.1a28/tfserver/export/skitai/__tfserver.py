6f5e3072ed71e92e470de77176354d2647027f27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:32 +0000

    Add package tfserver/0.1a28/tfserver-0.1a28.tar.gz

---
+++ packages/tfserver/tfserver-0.1a28.tar.gz/tfserver-0.1a28/tfserver/export/skitai/__tfserver.py
@@ -0,0 +1,59 @@
+from skitai.saddle import Saddle
+import tfserver
+import tensorflow as tf
+from tfserver import prediction_service_pb2, predict_pb2
+from tensorflow.python.framework import tensor_util
+import numpy as np
+import os
+
+app = Saddle (__name__)
+app.debug = True
+app.use_reloader = True
+
+app.access_control_allow_origin = ["*"]
+	
+@app.shutdown
+def shutdown (wasc):
+	# loading TF checkpoint
+	tfserver.close ()
+	
+@app.startup
+def startup (wasc):
+	tfserver.load_model (app.config.tf_model_dir, app.config.get ("tf_config"))
+	
+@app.route ("/tensorflow.serving.PredictionService/Predict")
+def Predict (was, request, timeout = 10):
+	feed_dict = {}
+	for k, v in request.inputs.items ():
+		tensor_name, dtype, shape = tfserver.tfsess.input_map [k]
+		feed_dict [tensor_name] = tensor_util.MakeNdarray (v)
+	outputs = []
+	for k, v in tfserver.tfsess.output_map.items ():
+		outputs.append ((k, v))	
+	predict_results = tfserver.tfsess.run ([tensor [0] for name, tensor in outputs], feed_dict = feed_dict)
+	
+	response = predict_pb2.PredictResponse()
+	for i, result in enumerate (predict_results):	
+		predict_result = predict_results [i]
+		response.outputs [outputs [i][0]].CopyFrom (
+			tensor_util.make_tensor_proto(predict_result.astype("float32"), shape=predict_result.shape)
+		)
+	return response
+	
+@app.route ("/predict")
+def predict (was, **inputs):
+	feed_dict = {}
+	for k, v in inputs.items ():
+		tensor_name, dtype, shape = tfserver.tfsess.input_map [k]
+		feed_dict [tensor_name] = np.array (v)
+	outputs = []
+	for k, v in tfserver.tfsess.output_map.items ():
+		outputs.append ((k, v))	
+	predict_results = tfserver.tfsess.run ([tensor [0] for name, tensor in outputs], feed_dict = feed_dict)
+	
+	response = {}
+	for i, result in enumerate (predict_results):	
+		predict_result = predict_results [i]		
+		response [outputs [i][0]] = predict_result.astype ("float32").tolist ()		
+	return was.response.api (response)
+	
