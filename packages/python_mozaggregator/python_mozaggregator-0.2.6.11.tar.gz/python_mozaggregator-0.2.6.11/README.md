563c89b594cc01db9da20d93f58bd95be8b60846
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:56 +0000

    Add package python_mozaggregator/0.2.6.11/python_mozaggregator-0.2.6.11.tar.gz

---
+++ packages/python_mozaggregator/python_mozaggregator-0.2.6.11.tar.gz/python_mozaggregator-0.2.6.11/README.md
@@ -0,0 +1,99 @@
+# python_mozaggregator
+Aggregator job for Telemetry. See this [blog](http://robertovitillo.com/2015/07/02/telemetry-metrics-roll-ups/) post for details. 
+
+[![Build Status](https://travis-ci.org/mozilla/python_mozaggregator.svg?branch=master)](https://travis-ci.org/mozilla/python_mozaggregator)
+
+## Development and deployment
+
+Note: if you are on Windows you might want to disable `core.autocrlf` to avoid git converting LF endings to CRLF when checking out.
+
+To start hacking on your local machine:
+```bash
+vagrant up
+vagrant ssh
+```
+
+If vagrant fails to download a box on Windows 10 then follow the workaround mentioned [here](https://github.com/mitchellh/vagrant/issues/6754).
+
+To run tests within the Vagrant VM:
+```bash
+cd /vagrant
+./run-tests.sh
+```
+
+To deploy a new version of the aggregation service to the cloud:
+```bash
+ansible-playbook ansible/deploy.yml -e '@ansible/envs/dev.yml' -i ansible/inventory
+```
+
+To connect to the database on the host, when running it inside the Vagrant VM, using something like [pgAdmin](http://pgadmin.org/), use `localhost` as the host, `5432` as the port, `vagrant` as the username, and a blank password.
+
+## API
+Aggregates are made available through a HTTP API. There are two kinds of aggregates: per submission date (date a ping is received by the server) and per build-id (date the submitting product was built).
+
+To access the aggregates use the ```aggregates_by/build_id/``` and ```aggregates_by/submission_date/``` prefix respectively.
+
+In the URLs below, replace `SERVICE` with the origin of this service's instance. The official service is `http://aggregates.telemetry.mozilla.org`.
+
+The following examples are based on build-id aggregates. Replace `build_id` with `submission_date` to use aggregates per submission date instead.
+
+##### Get available channels:
+```bash
+curl -X GET http://SERVICE/aggregates_by/build_id/channels/
+["nightly","beta","aurora"]
+```
+
+##### Get a list of options for the available dimensions on a given channel and version:
+```bash
+curl -X GET "http://SERVICE/filters/?channel=nightly&version=42"
+{"metric":["A11Y_CONSUMERS","A11Y_IATABLE_USAGE_FLAG",...], 
+ "application":["Fennec","Firefox"],
+ ...}
+```
+
+##### Get a list of available build-ids for a given channel:
+```bash
+curl -X GET "http://SERVICE/aggregates_by/build_id/channels/nightly/dates/"
+[{"date":"20150630","version":"42"}, {"date":"20150629","version":"42"}]
+```
+
+##### Given a set of build-ids, retrieve for each of build-id the aggregated histogram that complies with the requested filters:
+```bash
+curl -X GET "http://SERVICE/aggregates_by/build_id/channels/nightly/?version=41&dates=20150615,20150616&metric=GC_MS&os=Windows_NT"
+{"buckets":[0, ..., 10000],
+ "data":[{"date":"20150615",
+          "count":239459,
+          "sum": 412346123,
+          "histogram":[309, ..., 5047],
+          "label":""},
+         {"date":"20150616",
+          "count":233688,
+          "sum": 402241121,
+          "histogram":[306, ..., 7875],
+          "label":""}],
+ "kind":"exponential",
+ "description":"Time spent running JS GC (ms)"}
+```
+
+The available filters are:
+- `metric`, e.g. JS_TELEMETRY_ADDON_EXCEPTIONS
+- `application`, e.g. Firefox
+- `architecture`, e.g. x86
+- `os`, e.g. Windows_NT
+- `osVersion`, e.g. 6.1
+- `e10sEnabled`, e.g. true
+- `label`, e.g Adblock-Plus
+- `child`, e.g. true, meaningful only if e10s is enabled
+
+A reply has the following attributes:
+- `buckets`, which represents the bucket labels of the histogram
+- `kind`, the kind of histogram (e.g. exponential)
+- `data`, which is an array of metric objects with the following attributes:
+  - `date`: a build-id
+  - `count`: number of metrics aggregated
+  - `sum`: sum of accumulated values
+  - `histogram`: bucket values
+  - `description`: histogram description
+  - `label`: for keyed histograms, the key the entry belongs to, or otherwise a blank string
+
+Keyed histograms have the same format as unkeyed histograms, but there can possibly be multiple metric objects with the same date, each with a different key (`label`).
---
+++ packages/python_mozaggregator/python_mozaggregator-0.2.6.11.tar.gz/python_mozaggregator-0.2.6.11/mozaggregator/db.py
@@ -0,0 +1,212 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import psycopg2
+import pandas as pd
+import ujson as json
+import boto.rds2
+import os
+import string
+import sql
+import config
+
+from moztelemetry.spark import Histogram
+from boto.s3.connection import S3Connection
+from cStringIO import StringIO
+from mozaggregator.aggregator import SIMPLE_MEASURES_LABELS, COUNT_HISTOGRAM_LABELS, NUMERIC_SCALARS_LABELS, \
+                                    SIMPLE_MEASURES_PREFIX, COUNT_HISTOGRAM_PREFIX, NUMERIC_SCALARS_PREFIX, \
+                                    SCALAR_MEASURE_MAP
+
+# Use latest revision, we don't really care about histograms that have
+# been removed. This only works though if histogram definitions are
+# immutable, which has been the case so far.
+histogram_revision_map = {"nightly": "https://hg.mozilla.org/mozilla-central/rev/tip",
+                          "aurora": "https://hg.mozilla.org/releases/mozilla-aurora/rev/tip",
+                          "beta": "https://hg.mozilla.org/releases/mozilla-beta/rev/tip",
+                          "release": "https://hg.mozilla.org/releases/mozilla-release/rev/tip"}
+
+_metric_printable = set(string.ascii_uppercase + string.ascii_lowercase + string.digits + "_-[].")
+
+def get_db_connection_string(read_only=False):
+    if os.getenv("DB_TEST_URL"):
+        return os.getenv("DB_TEST_URL")
+    elif config.USE_PRODUCTION_DB:
+        s3 = S3Connection(host="s3-us-west-2.amazonaws.com")
+        secret = json.loads(s3.get_bucket(config.BUCKET).get_key(config.SECRET).get_contents_as_string())["password"]
+        rds = boto.rds2.connect_to_region(config.REGION)
+        rds_name = config.READ_RDS if read_only else config.WRITE_RDS
+        db = rds.describe_db_instances(rds_name)["DescribeDBInstancesResponse"]["DescribeDBInstancesResult"]["DBInstances"][0]
+        return "dbname={} user={} password={} host={}".format(db["DBName"], db["MasterUsername"], secret, db["Endpoint"]["Address"])
+    else:
+        return "dbname={} user={} password={} host={}".format(config.DBNAME, config.DBUSER, config.DBPASS, config.DBHOST)
+
+def _create_connection(autocommit=True, connection_string_override=None):
+    if connection_string_override:
+        conn = psycopg2.connect(connection_string_override)
+    else:
+        conn = psycopg2.connect(get_db_connection_string(False))
+
+    if autocommit:
+        conn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
+
+    return conn
+
+
+def submit_aggregates(aggregates, dry_run=False):
+    _preparedb()
+
+    connection_string = get_db_connection_string(False)
+
+    build_id_count = aggregates[0].\
+                     map(lambda x: (x[0][:4], _aggregate_to_sql(x))).\
+                     filter(lambda x: x[1]).\
+                     reduceByKey(lambda x, y: x + y).\
+                     map(lambda x: _upsert_build_id_aggregates(x[0], x[1], connection_string, dry_run=dry_run)).\
+                     count()
+
+    submission_date_count = aggregates[1].\
+                            map(lambda x: (x[0][:3], _aggregate_to_sql(x))).\
+                            filter(lambda x: x[1]).\
+                            reduceByKey(lambda x, y: x + y).\
+                            map(lambda x: _upsert_submission_date_aggregates(x[0], x[1], connection_string, dry_run=dry_run)).\
+                            count()
+
+    # TODO: Auto-vacuuming might be sufficient. Re-enable if needed.
+    # _vacuumdb()
+    return build_id_count, submission_date_count
+
+
+def _preparedb():
+    conn = _create_connection()
+    cursor = conn.cursor()
+    cursor.execute(sql.query)
+
+
+def _get_complete_histogram(channel, metric, values):
+    revision = histogram_revision_map.get(channel, "nightly")  # Use nightly revision if the channel is unknown
+
+    for prefix, labels in SCALAR_MEASURE_MAP.iteritems():
+        if metric.startswith(prefix):
+            histogram = pd.Series({int(k): v for k, v in values.iteritems()}, index=labels).fillna(0).values
+            break
+    else:
+        histogram = Histogram(metric, {"values": values}, revision=revision).get_value(autocast=False).values
+
+    return list(histogram)
+
+
+def _aggregate_to_sql(aggregate):
+    result = StringIO()
+    key, metrics = aggregate
+    submission_date, channel, version, application, architecture, os, os_version, e10s = key[:3] + key[-5:]
+    dimensions = {"application": application,
+                  "architecture": architecture,
+                  "os": os,
+                  "osVersion": os_version,
+                  "e10sEnabled": e10s}
+
+    for metric, payload in metrics.iteritems():
+        metric, label, process_type = metric
+
+        if not set(metric).issubset(_metric_printable):
+            continue  # Ignore metrics with non printable characters...
+
+        if u'\u0000' in label:
+            continue # Ignore labels with null character
+
+        try:
+            # Make sure values fit within a pgsql bigint
+            # TODO: we should probably log this event
+            if payload["sum"] > (1 << 63) - 1:
+                continue
+
+            histogram = _get_complete_histogram(channel, metric, payload["histogram"]) + [payload["sum"], payload["count"]]
+            histogram = [str(long(x)) for x in histogram]
+        except KeyError as e:
+            # Should eventually log errors
+            continue
+
+        dimensions["metric"] = metric
+        dimensions["label"] = label
+        # Have to special-case content and parent here to maintain
+        # backwards compatibility.
+        dimensions["child"] = "true" if process_type == "content" \
+                              else "false" if process_type == "parent" \
+                              else process_type
+
+        json_dimensions = json.dumps(dimensions)
+        # json.dumps takes care of properly escaping the text but a SQL command
+        # will first be interpreted as a string literal before being executed.
+        # This doubles the number of backslashes we need.
+        json_dimensions = json_dimensions.replace("\\", "\\\\")
+
+        result.write("{}\t{}\n".format(json_dimensions, "{" + ",".join(histogram) + "}"))
+    return result.getvalue()
+
+
+def _upsert_build_id_aggregates(key, stage_table, connection_string, dry_run=False):
+    conn = _create_connection(autocommit=False, connection_string_override=connection_string)
+    cursor = conn.cursor()
+    submission_date, channel, version, build_id = key
+
+    # Aggregates with different submisssion_dates write to the same tables, we need a lock
+    cursor.execute("select lock_transaction(%s, %s, %s, %s)", ("build_id", channel, version, build_id))
+
+    cursor.execute("select was_processed(%s, %s, %s, %s, %s)", ("build_id", channel, version, build_id, submission_date))
+    if cursor.fetchone()[0]:
+        # This aggregate has already been processed
+        conn.rollback()
+        return
+
+    cursor.execute("select create_temporary_table(%s, %s, %s, %s)", ("build_id", channel, version, build_id))
+    stage_table_name = cursor.fetchone()[0]
+
+    cursor.copy_from(StringIO(stage_table), stage_table_name, columns=("dimensions", "histogram"))
+    cursor.execute("select merge_table(%s, %s, %s, %s, %s)", ('build_id', channel, version, build_id, stage_table_name))
+
+    if dry_run:
+        conn.rollback()
+    else:
+        conn.commit()
+
+    cursor.close()
+    conn.close()
+
+
+def _upsert_submission_date_aggregates(key, stage_table, connection_string, dry_run=False):
+    conn = _create_connection(autocommit=False, connection_string_override=connection_string)
+    cursor = conn.cursor()
+    submission_date, channel, version = key
+
+    cursor.execute("select was_processed(%s, %s, %s, %s, %s)", ("submission_date", channel, version, submission_date, submission_date))
+    if cursor.fetchone()[0]:
+        # This aggregate has already been processed
+        conn.rollback()
+        return
+
+    cursor.execute("select create_temporary_table(%s, %s, %s, %s)", ("submission_date", channel, version, submission_date))
+    stage_table_name = cursor.fetchone()[0]
+
+    cursor.copy_from(StringIO(stage_table), stage_table_name, columns=("dimensions", "histogram"))
+    cursor.execute("select merge_table(%s, %s, %s, %s, %s)", ("submission_date", channel, version, submission_date, stage_table_name))
+
+    if dry_run:
+        conn.rollback()
+    else:
+        conn.commit()
+
+    cursor.close()
+    conn.close()
+
+
+def _vacuumdb():
+    conn = _create_connection()
+    conn.set_isolation_level(0)
+    cursor = conn.cursor()
+    cursor.execute("vacuum")
+    cursor.close()
+    conn.close()
