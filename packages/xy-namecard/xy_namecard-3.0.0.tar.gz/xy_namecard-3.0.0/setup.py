0d8eca5463af3cd5339ef57c3f633cde9eaffc1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:37 +0000

    Add package xy-namecard/3.0.0/xy_namecard-3.0.0.tar.gz

---
+++ packages/xy-namecard/xy_namecard-3.0.0.tar.gz/xy_namecard-3.0.0/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+setup(
+  name = 'xy_namecard',
+  packages = ['xy_namecard'],
+  version = '3.0.0',
+  description = 'namecard Recognition',
+  long_description='namecard Recognition,age,beauty,gender',
+  author = 'KingHS',
+  author_email = '382771946@qq.com',
+  keywords = ['pip3', 'namecard', 'python3','python','namecard Recognition'],
+  license='MIT',
+  install_requires=[
+        'qcloud_image'
+    ],
+)
