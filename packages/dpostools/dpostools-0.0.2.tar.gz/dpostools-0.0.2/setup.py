0cc6ba47809aee71bb7874dafb6a745798b292f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:30 +0000

    Add package dpostools/0.0.2/dpostools-0.0.2.tar.gz

---
+++ packages/dpostools/dpostools-0.0.2.tar.gz/dpostools-0.0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='dpostools',
+    version='0.0.2',
+    packages=[''],
+    ext_package='arkdbtools',
+    url='https://github.com/BlockHub/blockhubdpostools.git',
+    license='MIT',
+    author='Charles',
+    author_email='karel@blockhub.nl',
+    description='generic toolkit for interacting with DPOS chains'
+)
