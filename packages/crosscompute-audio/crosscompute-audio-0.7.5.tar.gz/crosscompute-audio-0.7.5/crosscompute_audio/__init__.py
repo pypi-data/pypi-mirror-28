94638c8be2ad192e558332f7bfc06946e53aea72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:30 +0000

    Add package crosscompute-audio/0.7.5/crosscompute-audio-0.7.5.tar.gz

---
+++ packages/crosscompute-audio/crosscompute-audio-0.7.5.tar.gz/crosscompute-audio-0.7.5/crosscompute_audio/__init__.py
@@ -0,0 +1,17 @@
+from crosscompute.scripts.serve import get_file_url
+from crosscompute.types import DataType
+
+
+class AudioType(DataType):
+
+    suffixes = 'audio',
+    formats = 'mp3', 'ogg'
+    template = 'crosscompute_audio:type.jinja2'
+
+    @classmethod
+    def load(Class, path):
+        return path
+
+    @classmethod
+    def render(Class, path):
+        return get_file_url(path)
