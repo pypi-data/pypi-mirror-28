37a64d1214974ddbf53b9b0b0e4e171b22626556
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:03:45 +0000

    Add package sed/0.3.1/sed-0.3.1.tar.gz

---
+++ packages/sed/sed-0.3.1.tar.gz/sed-0.3.1/setup.py
@@ -0,0 +1,37 @@
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+
+setup(
+    name='sed',
+    version='0.3.1',
+    description='Streaming editor toolkit',
+    author='Hugh Brown',
+    author_email='hughdbrown@yahoo.com',
+    url='http://iwebthereforeiam.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    install_requires=[
+    ],
+    tests_require=[
+        'nose',
+    ],
+    setup_requires=[],
+    packages=[
+        'sed',
+        'sed.engine',
+    ],
+    test_suite='nose.collector',
+    zip_safe=False,
+)
