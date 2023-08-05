6b20ff9ebdec1d5571c215dc69258c1b571f6cab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:35 +0000

    Add package dynamorm/0.5.3/dynamorm-0.5.3-py2.py3-none-any.whl

---
+++ packages/dynamorm/dynamorm-0.5.3-py2.py3-none-any.whl/dynamorm/types/_marshmallow.py
@@ -0,0 +1,36 @@
+from marshmallow import Schema as MarshmallowSchema
+from marshmallow import fields
+
+from .base import DynamORMSchema
+from ..exceptions import ValidationError
+
+
+class Schema(MarshmallowSchema, DynamORMSchema):
+    """This is the base class for marshmallow based schemas"""
+
+    @staticmethod
+    def field_to_dynamo_type(field):
+        """Given a marshmallow field object return the appropriate Dynamo type character"""
+        if isinstance(field, fields.Raw):
+            return 'B'
+        if isinstance(field, fields.Number):
+            return 'N'
+        return 'S'
+
+    @classmethod
+    def dynamorm_fields(cls):
+        return cls().fields
+
+    @classmethod
+    def dynamorm_validate(cls, obj, partial=False, native=False):
+        if native:
+            data, errors = cls().load(obj, partial=partial)
+        else:
+            data, errors = cls(partial=partial).dump(obj)
+        if errors:
+            raise ValidationError(obj, cls.__name__, errors)
+        return data
+
+    @staticmethod
+    def base_field_type():
+        return fields.Field
