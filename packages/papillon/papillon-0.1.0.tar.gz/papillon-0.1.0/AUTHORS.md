992bf1d1749555f2cef44d0e3d1aeb1c706e0ae1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:12 +0000

    Add package papillon/0.1.0/papillon-0.1.0.tar.gz

---
+++ packages/papillon/papillon-0.1.0.tar.gz/papillon-0.1.0/AUTHORS.md
@@ -0,0 +1,8 @@
+Papillon main developers:
+
+* Domenico Somma: domenico.somma@glasgow.ac.uk
+
+Contributors:
+
+* Emma Smith: e.smith.4@research.gla.ac.uk
+  * for suggestions and Beta testing
---
+++ packages/papillon/papillon-0.1.0.tar.gz/papillon-0.1.0/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: domenico.somma@glasgow.ac.uk
+"""
+
+from setuptools import setup
+
+setup(
+    name='papillon',
+    version='0.1.0',
+    py_modules=['papillon'],
+    description='A Python module to read and plot Galaxy RNA-seq data',
+    author='Domenico Somma',
+    author_email='domenico.somma@glasgow.ac.uk',
+    license='Mozilla Public License 2.0',
+    url='https://github.com/domenico-somma/Papillon/',
+    install_requires=[
+        "pandas >= 0.17.1",
+        "Seaborn >= 0.8.1",
+    ],
+)
