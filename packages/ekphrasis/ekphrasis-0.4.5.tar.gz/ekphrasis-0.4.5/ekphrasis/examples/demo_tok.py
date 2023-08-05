52798f03d708aa18b57cf8a7c90dad1ec7ae424c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:51 +0000

    Add package ekphrasis/0.4.5/ekphrasis-0.4.5.tar.gz

---
+++ packages/ekphrasis/ekphrasis-0.4.5.tar.gz/ekphrasis-0.4.5/ekphrasis/examples/demo_tok.py
@@ -0,0 +1,30 @@
+"""
+Created by Christos Baziotis.
+"""
+import nltk
+
+from ekphrasis.classes.tokenizer import SocialTokenizer, Tokenizer
+
+
+def wsp_tokenizer(text):
+    return text.split(" ")
+
+
+puncttok = nltk.WordPunctTokenizer().tokenize
+
+social_tokenizer = SocialTokenizer(lowercase=False).tokenize
+mytokenizer = Tokenizer(lowercase=False).tokenize
+
+sents = [
+    "CANT WAIT for the new season of #TwinPeaks ＼(^o^)／ yaaaay!!! #davidlynch #tvseries :)))",
+    "I saw the new #johndoe movie and it suuuuucks!!! WAISTED $10... #badmovies 3:/",
+    "@SentimentSymp:  can't wait for the Nov 9 #Sentiment talks!  YAAAAAAY !!! >:-D http://sentimentsymposium.com/.",
+]
+
+for s in sents:
+    print()
+    print("ORG: ", s)  # original sentence
+    # print("WSP : ", wsp_tokenizer(s))  # whitespace tokenizer
+    # print("WPU : ", puncttok(s))  # WordPunct tokenizer
+    print("SC : ", social_tokenizer(s))  # social tokenizer
+    print("SC : ", mytokenizer(s))  # social tokenizer
