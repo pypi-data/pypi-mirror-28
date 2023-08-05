b986b9aef1d9dcd58f4593437fbda7cb4e1377aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:44 +0000

    Add package django-wscounter/0.1.4.7.9.4/django-wscounter-0.1.4.7.9.4.tar.gz

---
+++ packages/django-wscounter/django-wscounter-0.1.4.7.9.4.tar.gz/django-wscounter-0.1.4.7.9.4/setup.py
@@ -0,0 +1,22 @@
+import os
+from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
+
+setup(
+    name='django-wscounter',
+    version='0.1.4.7.9.4',
+    packages=['wscounter'],
+    description='A dynamic web-socket',
+    long_description=README,
+    author='Firas Al Kafri',
+    author_email='firas.alkafri@gmail.com',
+    url='https://github.com/salalem/django-wscounter/',
+    license='GPLv2',
+    install_requires=[
+        'Django>=1.8.18, <=1.9.2',
+        'djangorestframework>=3.2.3, <=3.6.2',
+        'cerberus>=1.1'
+    ]
+)
