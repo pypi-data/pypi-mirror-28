57ff084f0ca8e9206781222c1cdb41040ecc485d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:31 +0000

    Add package waspy/0.22.0/waspy-0.22.0.tar.gz

---
+++ packages/waspy/waspy-0.22.0.tar.gz/waspy-0.22.0/waspy/transports/__init__.py
@@ -0,0 +1,7 @@
+from .httptransport import HTTPTransport, HTTPClientTransport
+from .httptoolstransport import HTTPTransport as HTTPToolsTransport
+from .rabbitmqtransport import RabbitMQTransport, RabbitMQClientTransport
+from .transportabc import TransportABC
+from .testtransport import TestTransport
+
+
