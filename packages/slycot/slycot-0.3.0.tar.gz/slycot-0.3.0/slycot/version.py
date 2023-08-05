d36d4b62c414051b2d27c6dfd24fd1ca8c856525
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:06 +0000

    Add package slycot/0.3.0/slycot-0.3.0.tar.gz

---
+++ packages/slycot/slycot-0.3.0.tar.gz/slycot-0.3.0/slycot/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM SLYCOT SETUP.PY
+short_version = '0.3.0'
+version = '0.3.0'
+full_version = '0.3.0'
+git_revision = '3c7b91b5dcd3664b9f9acec184b86d246310dc76'
+release = True
+
+if not release:
+    version = full_version
