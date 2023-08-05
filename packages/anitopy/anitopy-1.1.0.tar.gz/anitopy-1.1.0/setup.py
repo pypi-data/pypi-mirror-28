dbd55d32a6a80cc024f0842f3ecc69312f124080
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:04 +0000

    Add package anitopy/1.1.0/anitopy-1.1.0.tar.gz

---
+++ packages/anitopy/anitopy-1.1.0.tar.gz/anitopy-1.1.0/setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+
+setup(
+    name='anitopy',
+    packages=['anitopy'],
+    version='1.1.0',
+    description='An anime video filename parser',
+    author='Igor Cescon de Moura',
+    author_email='igorcesconm@gmail.com',
+    url='https://github.com/igorcmoura/anitopy',
+    python_requires='>=2.7',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ]
+)
