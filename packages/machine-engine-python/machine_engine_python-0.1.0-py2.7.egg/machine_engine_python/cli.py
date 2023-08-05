dbde1e71021c39167397bf27a3267bbe8d824fb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:13 +0000

    Add package machine-engine-python/0.1.0/machine_engine_python-0.1.0-py2.7.egg

---
+++ packages/machine-engine-python/machine_engine_python-0.1.0-py2.7.egg/machine_engine_python/cli.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+"""Console script for machine_engine_python."""
+
+import click
+
+
+@click.command()
+def main(args=None):
+    """Console script for machine_engine_python."""
+    click.echo("Replace this message by putting your code into "
+               "machine_engine_python.cli.main")
+    click.echo("See click documentation at http://click.pocoo.org/")
+
+
+if __name__ == "__main__":
+    main()
