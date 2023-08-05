37621fe38677a79bcfff351520a70846423ddf9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:16 +0000

    Add package linken/1.3.0/linken-1.3.0.tar.gz

---
+++ packages/linken/linken-1.3.0.tar.gz/linken-1.3.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+
+
+setup(
+        name            ='linken',
+        version         ='1.3.0',
+        py_modules      =['linken'],
+        author          ='dollar',
+        author_email    ='951410206@qq.com',
+        url             ='http://www.headdirstlabs.com',
+        description     ='A simple printer of nested lists',
+     )
