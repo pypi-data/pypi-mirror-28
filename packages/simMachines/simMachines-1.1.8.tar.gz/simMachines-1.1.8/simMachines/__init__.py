ad76377a9785246fb6e5d8934f6ca8942cb32271
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:03 +0000

    Add package simMachines/1.1.8/simMachines-1.1.8.tar.gz

---
+++ packages/simMachines/simMachines-1.1.8.tar.gz/simMachines-1.1.8/simMachines/__init__.py
@@ -0,0 +1,7 @@
+from simMachines.Authenticate import Authenticate
+from simMachines.Leliel import Leliel
+from simMachines.LelielAljunied import LelielAljunied
+from simMachines.Ramiel import Ramiel
+from simMachines.Gaghiel_G import Gaghiel_G
+
+__all__ = ['Authenticate','Leliel','LelielAljunied','Ramiel','Gaghiel_G']
