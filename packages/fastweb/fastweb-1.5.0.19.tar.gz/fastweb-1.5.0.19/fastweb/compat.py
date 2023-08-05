5b55b3dc7e1634e69fd026b796cbbe2ae9ebc1c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:56 +0000

    Add package fastweb/1.5.0.19/fastweb-1.5.0.19.tar.gz

---
+++ packages/fastweb/fastweb-1.5.0.19.tar.gz/fastweb-1.5.0.19/fastweb/compat.py
@@ -0,0 +1,20 @@
+# coding:utf8
+
+"""兼容模块"""
+
+import os
+import sys
+
+
+if sys.version_info < (3, 0):
+    import configparser as cParser
+else:
+    import configparser as cParser
+
+if os.name == 'posix' and sys.version_info < (3, 0):
+    import subprocess32 as subprocess
+else:
+    import subprocess
+
+
+
