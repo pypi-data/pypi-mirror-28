9c38d611ffcde8106bfecb578ecfde749ea08ab6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:20 +0000

    Add package yadage/0.14.1/yadage-0.14.1.tar.gz

---
+++ packages/yadage/yadage-0.14.1.tar.gz/yadage-0.14.1/yadage/manualutils.py
@@ -0,0 +1,58 @@
+import jsonpointer
+from .wflowstate import make_deserializer
+from .steering_object import YadageSteering
+from .utils import setupbackend_fromstring
+
+def connect(metadir, accept_metadir, ctrlstring, ctrlopts, modelsetup, modelopts, backendstring):
+    ys = YadageSteering.connect(
+        accept_metadir = accept_metadir,
+        metadir = metadir,
+        ctrlstring = ctrlstring,
+        ctrlopts = ctrlopts,
+        modelsetup = modelsetup,
+        modelopts = modelopts
+    )
+    if backendstring:
+        ys.controller.backend = setupbackend_fromstring(backendstring)
+    return ys
+
+
+def rule_steps_indices(workflow):
+    rule_to_steps_index = {}
+    steps_to_rule_index = {}
+    for rule in workflow.rules + workflow.applied_rules:
+        path = '/'.join([rule.offset, rule.rule.name])
+        p = jsonpointer.JsonPointer(path)
+        try:
+            steps_of_rule = [x['_nodeid'] for x in p.resolve(workflow.stepsbystage) if '_nodeid' in x]
+        except jsonpointer.JsonPointerException:
+            steps_of_rule = []
+        rule_to_steps_index[rule.identifier] = steps_of_rule
+        for step in steps_of_rule:
+            steps_to_rule_index[step] = rule.identifier
+    return rule_to_steps_index, steps_to_rule_index
+
+def preview_rule(wflow, name = None, identifier=None):
+    stateopts = {}
+    wflowmaker = make_deserializer(stateopts)
+    newflow = wflowmaker(wflow.json())
+
+    if identifier:
+        rule = newflow.view().getRule(identifier=identifier)
+    else:
+        offset, name = name.split('/')
+        rule = newflow.view(offset).getRule(name)
+
+    if not rule.applicable(newflow):
+        return
+
+    rule.apply(newflow)
+    newflow.rules.remove(rule)
+    newflow.applied_rules.append(rule)
+
+    existing_rules = [x.identifier for x in (wflow.rules + wflow.applied_rules)]
+    existing_nodes = wflow.dag.nodes()
+
+    new_rules = [{'name': x.rule.name, 'offset': x.offset} for x in newflow.rules if x.identifier not in existing_rules]
+    new_nodes = [{'name': newflow.dag.getNode(n).name, 'parents': newflow.dag.predecessors(n)} for n in newflow.dag.nodes() if n not in existing_nodes]
+    return new_rules, new_nodes
---
+++ packages/yadage/yadage-0.14.1.tar.gz/yadage-0.14.1/yadage/reset.py
@@ -0,0 +1,27 @@
+import networkx as nx
+
+def reset_node_state(node):
+    node.submit_time = None
+    node.ready_by_time = None
+    node.resultproxy = None
+    node.backend = None
+
+def reset_step(workflow, step):
+    s = workflow.dag.getNode(step)
+    reset_node_state(s)
+    try:
+        s.task.state.reset()
+    except AttributeError:
+        pass
+
+def reset_steps(workflow, steps):
+    for s in steps:
+        reset_step(workflow, s)
+
+def collective_downstream(workflow, steps):
+    downstream = set()
+    for step in steps:
+        for x in nx.descendants(workflow.dag, step):
+            downstream.add(x)
+    return list(downstream)
+
