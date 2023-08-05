7b37f37f3a0a880110cf98f7217f2eaad731c017
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:52 +0000

    Add package wechat-auto-reply2/1.1.1/wechat_auto_reply2-1.1.1.tar.gz

---
+++ packages/wechat-auto-reply2/wechat_auto_reply2-1.1.1.tar.gz/wechat_auto_reply2-1.1.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'wechat_auto_reply2',      
+    version = '1.1.1',
+    py_modules = ['wechat_auto_reply2'],
+    author = 'lsc',        
+    author_email = '123456@qq.com',
+    url = 'https://upload.pypi.org/legacy/',
+    description = 'wechat auto reply'   
+    )
---
+++ packages/wechat-auto-reply2/wechat_auto_reply2-1.1.1.tar.gz/wechat_auto_reply2-1.1.1/wechat_auto_reply2.py
@@ -0,0 +1,21 @@
+import itchat
+import time
+def auto_reply():
+    # 自动回复
+    # 封装好的装饰器，当接收到的消息是Text，即文字消息
+    @itchat.msg_register('Text')
+    def text_reply(msg):
+        # 当消息不是由自己发出的时候
+        if not msg['FromUserName'] == myUserName:
+            # 发送一条提示给文件助手
+            itchat.send_msg(u"[%s]收到好友@%s 的信息：%s\n" %
+                            (time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(msg['CreateTime'])),
+                            msg['User']['NickName'],
+                            msg['Text']), 'filehelper')
+            # 回复给好友
+            return u'[自动回复]您好，我现在有事不在，一会再和您联系。'
+
+    itchat.auto_login()
+    # 获取自己的UserName
+    myUserName = itchat.get_friends(update=True)[0]["UserName"]
+    itchat.run()
