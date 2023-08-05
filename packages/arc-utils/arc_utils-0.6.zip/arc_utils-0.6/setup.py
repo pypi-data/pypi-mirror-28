6b5d1f83920768b6b9f316f77d120e9f72141776
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:23 +0000

    Add package arc-utils/0.6/arc_utils-0.6.zip

---
+++ packages/arc-utils/arc_utils-0.6.zip/arc_utils-0.6/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+# Utility function to read the README file.
+# Used for the long_description.
+def read(fname):
+    return open(path.join(path.dirname(__file__), fname)).read()
+
+setup(
+    name='arc_utils',
+    version='0.6',
+    description="Python utilities for use with Esri ArcGIS Desktop software",
+    long_description=read('README.md'),
+    url='https://github.com/gdherbert/arc_utils',
+    author='Grant Herbert',
+    author_email='gdherbert@gmail.com',
+    keywords='esri arcpy arcgis',
+    download_url = 'https://github.com/gdherbert/arc_utils/tree/0.6',
+    packages=find_packages()
+    )
