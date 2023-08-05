9ca99ba450662ae4d8be829ee7d320d809954919
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:59 +0000

    Add package opengrid/0.5.5/opengrid-0.5.5-py2.py3-none-any.whl

---
+++ packages/opengrid/opengrid-0.5.5-py2.py3-none-any.whl/opengrid/__init__.py
@@ -0,0 +1,6 @@
+from opengrid.__about__ import __version__
+import opengrid.library.analysis as analysis
+import opengrid.library.exceptions as exceptions
+from opengrid.library.regression import MultiVarLinReg
+import opengrid.datasets as datasets
+from opengrid.library.plotting import plot_style
