62ea4ecdaf5d32231b6ba2885225ada1916388f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:30 +0000

    Add package snbl-toolbox/2018.1.16/snbl-toolbox-2018.1.16.tar.gz

---
+++ packages/snbl-toolbox/snbl-toolbox-2018.1.16.tar.gz/snbl-toolbox-2018.1.16/SNBLToolBox/frozen.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+hg_hash = "7b5973dbceb9"
+version = "2018.1.16"
+url = "https://soft.snbl.eu/_sources/toolbox.rst.txt"
+token = ".. SNBLToolBox Versions"
+text = "A new version of SNBL Toolbox is available at <a href=https://soft.snbl.eu/toolbox.html#download>soft.snbl.eu</a>"
