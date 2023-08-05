870352baa5dfd58621619daaf8177dda65d6ca02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:00 +0000

    Add package kerasparalleltext/0.16/kerasparalleltext-0.16.tar.gz

---
+++ packages/kerasparalleltext/kerasparalleltext-0.16.tar.gz/kerasparalleltext-0.16/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup
+from setuptools import find_packages
+
+
+setup(
+    name='kerasparalleltext',
+    packages=find_packages(),
+    version='0.16',
+    description='Pre-processing text in parallel for Keras in python.',
+    author='Hamel Husain',
+    author_email='hamel.husain@gmail.com',
+    url='https://github.com/hamelsmu/KerasParallelText',
+    license='MIT',
+    install_requires=['numpy',
+                      'scipy',
+                      'six',
+                      'pandas',
+                      'pyyaml',
+                      'pathos',
+                      'msgpack',
+                      'msgpack_numpy',
+                      'dask',
+                      'pyarrow',
+                      'more_itertools',
+                      'textacy',
+                      'gensim',
+                      'spacy',
+                      'keras'],
+    extras_require={
+        'h5py': ['h5py'],
+        'visualize': ['pydot'],
+        'tests': ['pytest',
+                  'pytest-pep8',
+                  'pytest-xdist',
+                  'pytest-cov',
+                  'pandas'],
+    },
+    download_url='https://github.com/hamelsmu/KerasParallelText/archive/0.16.tar.gz',
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
