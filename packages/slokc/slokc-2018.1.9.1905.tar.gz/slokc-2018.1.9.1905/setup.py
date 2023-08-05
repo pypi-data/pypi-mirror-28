371c8a47deba5ca2532b8121760703ed6b60451c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:10 +0000

    Add package slokc/2018.1.9.1905/slokc-2018.1.9.1905.tar.gz

---
+++ packages/slokc/slokc-2018.1.9.1905.tar.gz/slokc-2018.1.9.1905/setup.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+
+import setuptools
+
+def main():
+
+    setuptools.setup(
+        name             = "slokc",
+        version          = "2018.01.09.1905",
+        description      = "Kelvin Building automated Wi-Fi login program",
+        long_description = long_description(),
+        url              = "https://github.com/wdbm/suck_lord_kelvin_cock",
+        author           = "Will Breaden Madden",
+        author_email     = "wbm@protonmail.ch",
+        license          = "GPLv3",
+        py_modules       = [
+                           "suck_lord_kelvin_cock"
+                           ],
+        install_requires = [
+                           "docopt"
+                           ],
+        scripts          = [
+                           "suck_lord_kelvin_cock.py"
+                           ],
+        entry_points     = """
+                           [console_scripts]
+                           suck_lord_kelvin_cock = suck_lord_kelvin_cock:suck_lord_kelvin_cock
+                           """
+    )
+
+def long_description(
+    filename = "README.md"
+    ):
+
+    if os.path.isfile(os.path.expandvars(filename)):
+        try:
+            import pypandoc
+            long_description = pypandoc.convert_file(filename, "rst")
+        except ImportError:
+            long_description = open(filename).read()
+    else:
+        long_description = ""
+    return long_description
+
+if __name__ == "__main__":
+    main()
