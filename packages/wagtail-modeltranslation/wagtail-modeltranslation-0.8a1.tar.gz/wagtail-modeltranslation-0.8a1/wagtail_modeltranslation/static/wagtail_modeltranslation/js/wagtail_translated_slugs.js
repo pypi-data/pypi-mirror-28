595cb6b807e32cde3d63fc06993baa28d38827fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:51 +0000

    Add package wagtail-modeltranslation/0.8a1/wagtail-modeltranslation-0.8a1.tar.gz

---
+++ packages/wagtail-modeltranslation/wagtail-modeltranslation-0.8a1.tar.gz/wagtail-modeltranslation-0.8a1/wagtail_modeltranslation/static/wagtail_modeltranslation/js/wagtail_translated_slugs.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+    /* Only non-live pages should auto-populate the slug from the title */
+    if (!$('body').hasClass('page-is-live')) {
+        var slugFollowsTitle = false;
+
+        $.each(langs, function (idx, lang_code) {
+            $('#id_title_' + lang_code).on('focus', function () {
+                /* slug should only follow the title field if its value matched the title's value at the time of focus */
+                var currentSlug = $('#id_slug_' + lang_code).val();
+                var slugifiedTitle = cleanForSlug(this.value);
+                slugFollowsTitle = (currentSlug == slugifiedTitle);
+            });
+
+            $('#id_title_' + lang_code).on('keyup keydown keypress blur', function () {
+                if (slugFollowsTitle) {
+                    var slugifiedTitle = cleanForSlug(this.value);
+                    $('#id_slug_' + lang_code).val(slugifiedTitle);
+                }
+            });
+        });
+    }
+});
---
+++ packages/wagtail-modeltranslation/wagtail-modeltranslation-0.8a1.tar.gz/wagtail-modeltranslation-0.8a1/wagtail_modeltranslation/translation.py
@@ -0,0 +1,16 @@
+# coding utf-8
+
+from modeltranslation.decorators import register
+from modeltranslation.translator import TranslationOptions
+from wagtail.wagtailcore.models import Page
+
+
+@register(Page)
+class PageTR(TranslationOptions):
+    fields = (
+        'title',
+        'slug',
+        'seo_title',
+        'search_description',
+        'url_path',
+    )
