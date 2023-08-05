c1807f1130aaf48a7503dbc03382827b1b0a364e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:00 +0000

    Add package python-treasury/1.0a1/python-treasury-1.0a1.tar.gz

---
+++ packages/python-treasury/python-treasury-1.0a1.tar.gz/python-treasury-1.0a1/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+from setuptools import setup
+
+
+setup(
+    name='python-treasury',
+    version='1.0a1',
+    description='IBR Cold wallet library',
+    author='Cochise Ruhulessin',
+    author_email='c.ruhulessin@ibrb.org',
+    url='https://www.ibrb.org',
+    project_name='Cold wallet',
+    install_requires=[],
+    packages=[
+        'treasury',
+    ]
+)
