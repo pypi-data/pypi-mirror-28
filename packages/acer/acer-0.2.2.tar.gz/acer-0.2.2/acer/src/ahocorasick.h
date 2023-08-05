acc6ff3c441a4b2d486930fb97fc861cb1c6a5f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:23 +0000

    Add package acer/0.2.2/acer-0.2.2.tar.gz

---
+++ packages/acer/acer-0.2.2.tar.gz/acer-0.2.2/acer/src/ahocorasick.h
@@ -0,0 +1,21 @@
+#ifndef AHOCORASICK
+#define AHOCORASICK
+
+#include <iostream>
+#include "trie_tree.h"
+
+using namespace std;
+
+class AhoCorasick: public TrieTree {
+    unordered_map <int, Node*> failure;
+    time_t start,stop;
+    public:
+    void insert(string word, bool if_reverse=false);
+    void checkout(Node* &root, unordered_map<string, string> &result,const int &stop);
+    void search_(vector<string> &segments);
+    //void search(string text);
+    void search(string text, bool if_reverse=false);
+    void search(vector<string> segments);
+    void make_ac();
+};
+#endif
