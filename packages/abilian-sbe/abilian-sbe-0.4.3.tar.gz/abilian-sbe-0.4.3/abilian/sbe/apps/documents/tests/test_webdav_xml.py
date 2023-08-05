c97aa6dfdf26c033655b9d9d1bbcacf7c8d15e93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:50 +0000

    Add package abilian-sbe/0.4.3/abilian-sbe-0.4.3.tar.gz

---
+++ packages/abilian-sbe/abilian-sbe-0.4.3.tar.gz/abilian-sbe-0.4.3/abilian/sbe/apps/documents/tests/test_webdav_xml.py
@@ -0,0 +1,50 @@
+from __future__ import absolute_import, print_function, unicode_literals
+
+from os.path import dirname
+
+from lxml import etree
+
+from ..webdav.constants import DAV_PROPS
+from ..webdav.xml import MultiStatus, Propfind
+
+
+def test_propfind_sample1():
+    xml = open(dirname(__file__) + "/data/propfind1.xml", 'rb').read()
+    propfind = Propfind(xml)
+    assert propfind.mode == 'prop'
+
+
+def test_propfind_sample2():
+    xml = open(dirname(__file__) + "/data/propfind2.xml", 'rb').read()
+    propfind = Propfind(xml)
+    assert propfind.mode == 'prop'
+
+
+def test_propfind_sample3():
+    xml = open(dirname(__file__) + "/data/propfind3.xml", 'rb').read()
+    propfind = Propfind(xml)
+    assert propfind.mode == 'allprop'
+
+
+def test_empty_multistatus():
+    m = MultiStatus()
+    result = m.to_string()
+
+    # Check XML is weel-formed
+    etree.fromstring(result)
+
+
+def test_multistatus():
+
+    class Obj(object):
+        pass
+
+    obj = Obj()
+    obj.name = "some name"
+    obj.is_folder = True
+    m = MultiStatus()
+    m.add_response_for("http://example.com/", obj, DAV_PROPS)
+    result = m.to_string()
+
+    # Check XML is weel-formed
+    etree.fromstring(result)
