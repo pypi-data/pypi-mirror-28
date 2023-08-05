9202e10734c57ecf85fa02a263511d1deac4ec2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:53 +0000

    Add package xy-xingzuo/1.0.0/xy_xingzuo-1.0.0.tar.gz

---
+++ packages/xy-xingzuo/xy_xingzuo-1.0.0.tar.gz/xy_xingzuo-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+  name = 'xy_xingzuo',
+  packages = ['xy_xingzuo'],
+  version = '1.0.0',
+  description = 'Get constellation',
+  long_description='Get constellation Info From juhe API',
+  author = 'KingHS',
+  author_email = '382771946@qq.com',
+  keywords = ['pip3', 'xingzuo', 'python3','python','constellation'],
+  license='MIT',
+)
