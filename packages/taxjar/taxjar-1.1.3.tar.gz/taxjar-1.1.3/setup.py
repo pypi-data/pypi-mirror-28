872fd59e2a83707906c9f95e6241ec7f3d54d984
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:35 +0000

    Add package taxjar/1.1.3/taxjar-1.1.3.tar.gz

---
+++ packages/taxjar/taxjar-1.1.3.tar.gz/taxjar-1.1.3/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='taxjar',
+    version='1.1.3',
+    description='Sales tax API client for Python',
+    author='TaxJar',
+    author_email='support@taxjar.com',
+    url='https://github.com/taxjar/taxjar-python',
+    download_url='https://github.com/taxjar/taxjar-python/archive/v1.1.3.zip',
+    packages=['taxjar', 'taxjar.data'],
+    install_requires=[
+        'requests >= 2.13.0',
+        'jsonobject >= 0.7.1'
+    ],
+    tests_require=[
+        'mock'
+    ]
+)
+
