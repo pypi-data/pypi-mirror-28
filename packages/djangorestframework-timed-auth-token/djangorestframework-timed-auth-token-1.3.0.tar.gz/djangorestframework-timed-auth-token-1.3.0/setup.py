f1a832e8a0ecbd8d09483256839f2f85fe23de9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:11 +0000

    Add package djangorestframework-timed-auth-token/1.3.0/djangorestframework-timed-auth-token-1.3.0.tar.gz

---
+++ packages/djangorestframework-timed-auth-token/djangorestframework-timed-auth-token-1.3.0.tar.gz/djangorestframework-timed-auth-token-1.3.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+
+def read(filename):
+    with open(filename) as f:
+        return f.read()
+
+
+setup(
+    name='djangorestframework-timed-auth-token',
+    version='1.3.0',
+    description=('An authentication token for djangorestframework that has an expiration date.'),
+    long_description=read('README.rst'),
+    author='Ryan Pineo',
+    author_email='ryanpineo@gmail.com',
+    license='MIT',
+    url='https://github.com/silverlogic/djangorestframework-timed-auth-token',
+    packages=['timed_auth_token', 'timed_auth_token.migrations'],
+    install_requires=['djangorestframework>=3.1'],
+    classifiers=[
+        'Framework :: Django',
+        'Programming Language :: Python :: 3'
+    ]
+)
