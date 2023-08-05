d90b296a572b276c91c77f77d77b7e4e830ca470
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:06 +0000

    Add package py_html/2.12/py_html-2.12.zip

---
+++ packages/py_html/py_html-2.12.zip/py_html-2.12/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'py_html',
+  packages = ['py_html'],
+  version = '2.12',
+  description = 'Generate HTML Report ',
+  author = 'Ankit Singh',
+  author_email = 'ankitsingh1152@gmail.com',
+  #url = '',
+  #download_url = '',
+  keywords = ['testing', 'logging', 'pytest'], # arbitrary keywords
+  classifiers = [],
+)
