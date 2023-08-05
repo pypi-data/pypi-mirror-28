2edca164a730fdcc197c5a9993d543d425bb3a98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:47 +0000

    Add package yugioh-bot/0.4.4/yugioh_bot-0.4.4-py3-none-any.whl

---
+++ packages/yugioh-bot/yugioh_bot-0.4.4-py3-none-any.whl/bot/providers/__init__.py
@@ -0,0 +1,26 @@
+from .provider import Provider
+from bot.providers.nox import Nox
+from .steam import Steam
+import logging
+
+available_providers = [Nox, Steam]
+
+
+def get_provider(name):
+    if name.lower() == 'nox':
+        return Nox
+    elif name.lower() == 'steam':
+        return Steam
+    else:
+        raise Exception("Provider {} does not exist, choose from [{}]".format(name, ','.join(
+            [x.__str__() for x in available_providers])))
+
+
+root = logging.getLogger("bot.providers")
+root.setLevel(logging.DEBUG)
+formatter = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+ch.setFormatter(formatter)
+root.addHandler(ch)
