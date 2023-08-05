e4542df53ca4829429bf3a514e2d9094eeacce20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:06 +0000

    Add package fluentcms-filer/1.0.3/fluentcms-filer-1.0.3.tar.gz

---
+++ packages/fluentcms-filer/fluentcms-filer-1.0.3.tar.gz/fluentcms-filer-1.0.3/fluentcms_filer/file/content_plugins.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+from django.utils.translation import ugettext_lazy as _
+
+from fluent_contents.extensions import ContentPlugin, plugin_pool
+
+from .models import FilerFileItem
+
+
+@plugin_pool.register
+class FilerFilePlugin(ContentPlugin):
+    model = FilerFileItem
+    category = _('Filer')
+    render_template = "fluentcms_filer/file.html"
+
+    class Media:
+        css = {
+            'screen': (
+                'fluentcms_filer/file/admin.css',
+            )
+        }    
---
+++ packages/fluentcms-filer/fluentcms-filer-1.0.3.tar.gz/fluentcms-filer-1.0.3/fluentcms_filer/teaser/content_plugins.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+from django.utils.translation import ugettext_lazy as _
+
+from fluent_contents.extensions import ContentPlugin, plugin_pool
+
+from .models import FilerTeaserItem
+
+
+@plugin_pool.register
+class FilerTeaserPlugin(ContentPlugin):
+    model = FilerTeaserItem
+    category = _('Filer')
+    render_template = "fluentcms_filer/teaser.html"
+
+    class Media:
+        css = {
+            'screen': (
+                'fluentcms_filer/teaser/admin.css',
+            )
+        }    
