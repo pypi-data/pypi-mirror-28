b92141d48fe1b1ba56eb93e4a561b903a58fd5f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:11 +0000

    Add package zerophone-hw/0.3.0/zerophone_hw-0.3.0.tar.gz

---
+++ packages/zerophone-hw/zerophone_hw-0.3.0.tar.gz/zerophone_hw-0.3.0/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+version = "0.3.0"
+
+setup(
+    name='zerophone_hw',
+    py_modules=['zerophone_hw'],
+    version=version,
+    description='ZeroPhone API library',
+    author='CRImier',
+    author_email='crimier@yandex.ru',
+    install_requires=[
+        "gpio"
+    ],
+    url = 'https://github.com/ZeroPhone/Zerophone-API-Python',
+    download_url = 'https://github.com/ZeroPhone/zerophone-api-py/archive/{}.tar.gz'.format(version)
+)
