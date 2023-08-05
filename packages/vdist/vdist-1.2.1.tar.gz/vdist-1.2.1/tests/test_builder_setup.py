026f0192e9fc058dd5e1b88d598023a8aef65eff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:37 +0000

    Add package vdist/1.2.1/vdist-1.2.1.tar.gz

---
+++ packages/vdist/vdist-1.2.1.tar.gz/vdist-1.2.1/tests/test_builder_setup.py
@@ -0,0 +1,21 @@
+import pytest
+
+from vdist.builder import Builder, NoBuildsFoundException
+
+
+def test_builder_nobuilds():
+    b = Builder()
+
+    with pytest.raises(NoBuildsFoundException):
+        b.start_build()
+
+
+def test_internal_profile_loads():
+    b = Builder()
+
+    profiles = b.get_available_profiles()
+
+    internal_profile_ids = ['ubuntu-trusty', 'centos6', 'debian-wheezy']
+
+    for profile_id in internal_profile_ids:
+        assert profile_id in profiles
