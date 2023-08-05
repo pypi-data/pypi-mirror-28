bd20766cd601d80a3e505af5f4d02e7e234a59c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:33 +0000

    Add package socos/0.2/socos-0.2-py2.py3-none-any.whl

---
+++ packages/socos/socos-0.2-py2.py3-none-any.whl/socos/__init__.py
@@ -0,0 +1,12 @@
+"""socos is a commandline tool for controlling Sonos speakers"""
+
+from .core import SoCos
+
+# Will be parsed by setup.py to determine package metadata
+__author__ = 'SoCo team <python-soco@googlegroups.com>'
+__version__ = '0.2'
+__website__ = 'https://github.com/SoCo/socos'
+__license__ = 'MIT License'
+
+
+__all__ = ['SoCos']
