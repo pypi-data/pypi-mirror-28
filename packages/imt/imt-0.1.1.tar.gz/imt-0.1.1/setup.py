9ac70634128bdca41d812a6415bfba19841edfaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:28 +0000

    Add package imt/0.1.1/imt-0.1.1.tar.gz

---
+++ packages/imt/imt-0.1.1.tar.gz/imt-0.1.1/setup.py
@@ -0,0 +1,14 @@
+
+from distutils.core import setup
+setup(
+    name='imt',
+    packages=['imt'],
+    version='0.1.1',
+    description='a package for conducting matched market experimental design and analysis',
+    author='Andrew Mueller',
+    author_email='muelleau1917@gmail.com',
+    url='https://github.com/Muelleau/MatchMarketExperiments',
+    download_url='https://github.com/Muelleau/MatchMarketExperiments.git',
+    keywords=['experiment', 'match', 'market'],
+    classifiers=[]
+)
