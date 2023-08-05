1535eea3839077b77d001b7860f70361815510ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:37 +0000

    Add package twitter-ads/2.0.1/twitter_ads-2.0.1-py2.py3-none-any.whl

---
+++ packages/twitter-ads/twitter_ads-2.0.1-py2.py3-none-any.whl/twitter_ads/__init__.py
@@ -0,0 +1,8 @@
+# Copyright (C) 2015 Twitter, Inc.
+
+VERSION = (2, 0, 1)
+API_VERSION = '2'
+
+from twitter_ads.utils import get_version
+
+__version__ = get_version()
---
+++ packages/twitter-ads/twitter_ads-2.0.1-py2.py3-none-any.whl/twitter_ads/creative.py
@@ -0,0 +1,389 @@
+# Copyright (C) 2015 Twitter, Inc.
+
+"""Container for all creative management logic used by the Ads API SDK."""
+
+from requests.exceptions import HTTPError
+
+from twitter_ads import API_VERSION
+from twitter_ads.enum import TRANSFORM
+from twitter_ads.http import Request
+from twitter_ads.resource import resource_property, Resource, Persistence, Analytics
+
+
+class PromotedAccount(Resource, Persistence):
+
+    PROPERTIES = {}
+
+    RESOURCE_COLLECTION = '/' + API_VERSION + '/accounts/{account_id}/promoted_accounts'
+    RESOURCE = '/' + API_VERSION + '/accounts/{account_id}/promoted_accounts/{id}'
+
+
+# promoted account properties
+# read-only
+resource_property(PromotedAccount, 'approval_status', readonly=True)
+resource_property(PromotedAccount, 'created_at', readonly=True, transform=TRANSFORM.TIME)
+resource_property(PromotedAccount, 'deleted', readonly=True, transform=TRANSFORM.BOOL)
+resource_property(PromotedAccount, 'entity_status', readonly=True)
+resource_property(PromotedAccount, 'id', readonly=True)
+resource_property(PromotedAccount, 'updated_at', readonly=True, transform=TRANSFORM.TIME)
+# writable
+resource_property(PromotedAccount, 'line_item_id')
+resource_property(PromotedAccount, 'user_id')
+
+
+class PromotedTweet(Resource, Persistence, Analytics):
+
+    PROPERTIES = {}
+
+    RESOURCE_COLLECTION = '/' + API_VERSION + '/accounts/{account_id}/promoted_tweets'
+    RESOURCE = '/' + API_VERSION + '/accounts/{account_id}/promoted_tweets/{id}'
+
+    def save(self):
+        """
+        Saves or updates the current object instance depending on the
+        presence of `object.id`.
+        """
+        params = self.to_params()
+        if 'tweet_id' in params:
+            params['tweet_ids'] = [params['tweet_id']]
+            del params['tweet_id']
+
+        if self.id:
+            raise HTTPError("Method PUT not allowed.")
+
+        resource = self.RESOURCE_COLLECTION.format(account_id=self.account.id)
+        response = Request(self.account.client, 'post', resource, params=params).perform()
+        return self.from_response(response.body['data'][0])
+
+
+# promoted tweet properties
+# read-only
+resource_property(PromotedTweet, 'approval_status', readonly=True)
+resource_property(PromotedTweet, 'created_at', readonly=True, transform=TRANSFORM.TIME)
+resource_property(PromotedTweet, 'deleted', readonly=True, transform=TRANSFORM.BOOL)
+resource_property(PromotedTweet, 'entity_status', readonly=True)
+resource_property(PromotedTweet, 'id', readonly=True)
+resource_property(PromotedTweet, 'updated_at', readonly=True, transform=TRANSFORM.TIME)
+# writable
+resource_property(PromotedTweet, 'line_item_id')
+resource_property(PromotedTweet, 'tweet_id')  # SDK limitation
+
+
+class Video(Resource, Persistence):
+
+    PROPERTIES = {}
+
+    RESOURCE_COLLECTION = '/' + API_VERSION + '/accounts/{account_id}/videos'
+    RESOURCE = '/' + API_VERSION + '/accounts/{account_id}/videos/{id}'
+
+
+# video properties
+# read-only
+resource_property(Video, 'aspect_ratio', readonly=True)
+resource_property(Video, 'created_at', readonly=True, transform=TRANSFORM.TIME)
+resource_property(Video, 'deleted', readonly=True, transform=TRANSFORM.BOOL)
+resource_property(Video, 'duration', readonly=True, transform=TRANSFORM.INT)
+resource_property(Video, 'id', readonly=True)
+resource_property(Video, 'media_key', readonly=True)
+resource_property(Video, 'poster_url', readonly=True)
+resource_property(Video, 'preview_url', readonly=True)
+resource_property(Video, 'ready_to_tweet', readonly=True, transform=TRANSFORM.BOOL)
+resource_property(Video, 'reasons_not_servable', readonly=True, transform=TRANSFORM.LIST)
+resource_property(Video, 'tweeted', readonly=True, transform=TRANSFORM.BOOL)
+resource_property(Video, 'updated_at', readonly=True, transform=TRANSFORM.TIME)
+# writable
+resource_property(Video, 'description')
+resource_property(Video, 'poster_image_media_id')
+resource_property(Video, 'title')
+resource_property(Video, 'video_media_id')
+
+
+class AccountMedia(Resource, Persistence):
+
+    PROPERTIES = {}
+
+    RESOURCE_COLLECTION = '/' + API_VERSION + '/accounts/{account_id}/account_media'
+    RESOURCE = '/' + API_VERSION + '/accounts/{account_id}/account_media/{id}'
+
+
+# Account Media properties
+# read-only
+resource_property(AccountMedia, 'created_at', readonly=True, transform=TRANSFORM.TIME)
+resource_property(AccountMedia, 'deleted', readonly=True, transform=TRANSFORM.BOOL)
+resource_property(AccountMedia, 'id', readonly=True)
+resource_property(AccountMedia, 'media_url', readonly=True)
+resource_property(AccountMedia, 'updated_at', readonly=True, transform=TRANSFORM.TIME)
+# writable
+resource_property(AccountMedia, 'creative_type')
+resource_property(AccountMedia, 'media_id')
+resource_property(AccountMedia, 'video_id')
+
+
+class MediaCreative(Resource, Persistence):
+
+    PROPERTIES = {}
+
+    RESOURCE_COLLECTION = '/' + API_VERSION + '/accounts/{account_id}/media_creatives'
+    RESOURCE = '/' + API_VERSION + '/accounts/{account_id}/media_creatives/{id}'
+
+
+# Media Creative properties
+# read-only
+
+resource_property(MediaCreative, 'approval_status', readonly=True)
+resource_property(MediaCreative, 'created_at', readonly=True, transform=TRANSFORM.TIME)
+resource_property(MediaCreative, 'deleted', readonly=True, transform=TRANSFORM.BOOL)
+resource_property(MediaCreative, 'id', readonly=True)
+resource_property(MediaCreative, 'serving_status', readonly=True)
+resource_property(MediaCreative, 'updated_at', readonly=True, transform=TRANSFORM.TIME)
+# writable
+resource_property(MediaCreative, 'account_media_id')
+resource_property(MediaCreative, 'landing_url')
+resource_property(MediaCreative, 'line_item_id')
+
+
+class WebsiteCard(Resource, Persistence):
+
+    PROPERTIES = {}
+
+    RESOURCE_COLLECTION = '/' + API_VERSION + '/accounts/{account_id}/cards/website'
+    RESOURCE = '/' + API_VERSION + '/accounts/{account_id}/cards/website/{id}'
+
+
+# website card properties
+# read-only
+resource_property(WebsiteCard, 'card_type', readonly=True)
+resource_property(WebsiteCard, 'card_uri', readonly=True)
+resource_property(WebsiteCard, 'created_at', readonly=True, transform=TRANSFORM.TIME)
+resource_property(WebsiteCard, 'id', readonly=True)
+resource_property(WebsiteCard, 'image', readonly=True)
+resource_property(WebsiteCard, 'image_display_height', readonly=True)
+resource_property(WebsiteCard, 'image_display_width', readonly=True)
+resource_property(WebsiteCard, 'deleted', readonly=True, transform=TRANSFORM.BOOL)
+resource_property(WebsiteCard, 'preview_url', readonly=True)
+resource_property(WebsiteCard, 'website_dest_url', readonly=True)
+resource_property(WebsiteCard, 'website_display_url', readonly=True)
+resource_property(WebsiteCard, 'updated_at', readonly=True, transform=TRANSFORM.TIME)
+# writable
+resource_property(WebsiteCard, 'image_media_id')
+resource_property(WebsiteCard, 'name')
+resource_property(WebsiteCard, 'website_title')
+resource_property(WebsiteCard, 'website_url')
+
+
+class VideoWebsiteCard(Resource, Persistence):
+
+    PROPERTIES = {}
+
+    RESOURCE_COLLECTION = '/' + API_VERSION + '/accounts/{account_id}/cards/video_website'
+    RESOURCE = '/' + API_VERSION + '/accounts/{account_id}/cards/video_website/{id}'
+
+
+# video website card properties
+# read-only
+resource_property(VideoWebsiteCard, 'account_id', readonly=True)
+resource_property(VideoWebsiteCard, 'card_type', readonly=True)
+resource_property(VideoWebsiteCard, 'card_uri', readonly=True)
+resource_property(VideoWebsiteCard, 'created_at', readonly=True, transform=TRANSFORM.TIME)
+resource_property(VideoWebsiteCard, 'deleted', readonly=True, transform=TRANSFORM.BOOL)
+resource_property(VideoWebsiteCard, 'id', readonly=True)
+resource_property(VideoWebsiteCard, 'preview_url', readonly=True)
+resource_property(VideoWebsiteCard, 'updated_at', readonly=True, transform=TRANSFORM.TIME)
+resource_property(VideoWebsiteCard, 'video_content_id', readonly=True)
+resource_property(VideoWebsiteCard, 'video_height', readonly=True)
+resource_property(VideoWebsiteCard, 'video_hls_url', readonly=True)
+resource_property(VideoWebsiteCard, 'video_owner_id', readonly=True)
+resource_property(VideoWebsiteCard, 'video_poster_height', readonly=True)
+resource_property(VideoWebsiteCard, 'video_poster_url', readonly=True)
+resource_property(VideoWebsiteCard, 'video_poster_width', readonly=True)
+resource_property(VideoWebsiteCard, 'video_url', readonly=True)
+resource_property(VideoWebsiteCard, 'video_width', readonly=True)
+resource_property(VideoWebsiteCard, 'website_dest_url', readonly=True)
+resource_property(VideoWebsiteCard, 'website_display_url', readonly=True)
+# writable
+resource_property(VideoWebsiteCard, 'name')
+resource_property(VideoWebsiteCard, 'title')
+resource_property(VideoWebsiteCard, 'video_id')
+resource_property(VideoWebsiteCard, 'website_url')
+
+
+class ImageAppDownloadCard(Resource, Persistence):
+
+    PROPERTIES = {}
+
+    RESOURCE_COLLECTION = '/' + API_VERSION + '/accounts/{account_id}/cards/image_app_download'
+    RESOURCE = '/' + API_VERSION + '/accounts/{account_id}/cards/image_app_download/{id}'
+
+
+# image app download card properties
+# read-only
+resource_property(ImageAppDownloadCard, 'id', readonly=True)
+resource_property(ImageAppDownloadCard, 'image_display_height', readonly=True)
+resource_property(ImageAppDownloadCard, 'image_display_width', readonly=True)
+resource_property(ImageAppDownloadCard, 'wide_app_image', readonly=True)
+resource_property(ImageAppDownloadCard, 'card_uri', readonly=True)
+resource_property(ImageAppDownloadCard, 'card_type', readonly=True)
+resource_property(ImageAppDownloadCard, 'preview_url', readonly=True)
+resource_property(ImageAppDownloadCard, 'created_at', readonly=True, transform=TRANSFORM.TIME)
+resource_property(ImageAppDownloadCard, 'updated_at', readonly=True, transform=TRANSFORM.TIME)
+resource_property(ImageAppDownloadCard, 'deleted', readonly=True, transform=TRANSFORM.BOOL)
+# writable
+resource_property(ImageAppDownloadCard, 'app_country_code')
+resource_property(ImageAppDownloadCard, 'app_cta')
+resource_property(ImageAppDownloadCard, 'iphone_app_id')
+resource_property(ImageAppDownloadCard, 'iphone_deep_link')
+resource_property(ImageAppDownloadCard, 'ipad_app_id')
+resource_property(ImageAppDownloadCard, 'ipad_deep_link')
+resource_property(ImageAppDownloadCard, 'googleplay_app_id')
+resource_property(ImageAppDownloadCard, 'googleplay_deep_link')
+resource_property(ImageAppDownloadCard, 'name')
+resource_property(ImageAppDownloadCard, 'wide_app_image_media_id')
+
+
+class VideoAppDownloadCard(Resource, Persistence):
+
+    PROPERTIES = {}
+
+    RESOURCE_COLLECTION = '/' + API_VERSION + '/accounts/{account_id}/cards/video_app_download'
+    RESOURCE = '/' + API_VERSION + '/accounts/{account_id}/cards/video_app_download/{id}'
+
+
+# video app download card properties
+# read-only
+resource_property(VideoAppDownloadCard, 'card_uri', readonly=True)
+resource_property(VideoAppDownloadCard, 'card_type', readonly=True)
+resource_property(VideoAppDownloadCard, 'created_at', readonly=True, transform=TRANSFORM.TIME)
+resource_property(VideoAppDownloadCard, 'deleted', readonly=True, transform=TRANSFORM.BOOL)
+resource_property(VideoAppDownloadCard, 'id', readonly=True)
+resource_property(VideoAppDownloadCard, 'preview_url', readonly=True)
+resource_property(VideoAppDownloadCard, 'updated_at', readonly=True, transform=TRANSFORM.TIME)
+resource_property(VideoAppDownloadCard, 'video_content_id', readonly=True)
+resource_property(VideoAppDownloadCard, 'video_hls_url', readonly=True)
+resource_property(VideoAppDownloadCard, 'video_owner_id', readonly=True)
+resource_property(VideoAppDownloadCard, 'video_poster_url', readonly=True)
+resource_property(VideoAppDownloadCard, 'video_url', readonly=True)
+# writable
+resource_property(VideoAppDownloadCard, 'app_country_code')
+resource_property(VideoAppDownloadCard, 'app_cta')
+resource_property(VideoAppDownloadCard, 'image_media_id')
+resource_property(VideoAppDownloadCard, 'ipad_app_id')
+resource_property(VideoAppDownloadCard, 'ipad_deep_link')
+resource_property(VideoAppDownloadCard, 'iphone_app_id')
+resource_property(VideoAppDownloadCard, 'iphone_deep_link')
+resource_property(VideoAppDownloadCard, 'googleplay_app_id')
+resource_property(VideoAppDownloadCard, 'googleplay_deep_link')
+resource_property(VideoAppDownloadCard, 'name')
+resource_property(VideoAppDownloadCard, 'video_id')
+
+
+class ImageConversationCard(Resource, Persistence):
+
+    PROPERTIES = {}
+
+    RESOURCE_COLLECTION = '/' + API_VERSION + '/accounts/{account_id}/cards/image_conversation'
+    RESOURCE = '/' + API_VERSION + '/accounts/{account_id}/cards/image_conversation/{id}'
+
+
+# image conversation card properties
+# read-only
+resource_property(ImageConversationCard, 'card_type', readonly=True)
+resource_property(ImageConversationCard, 'card_uri', readonly=True)
+resource_property(ImageConversationCard, 'created_at', readonly=True, transform=TRANSFORM.TIME)
+resource_property(ImageConversationCard, 'deleted', readonly=True, transform=TRANSFORM.BOOL)
+resource_property(ImageConversationCard, 'id', readonly=True)
+resource_property(ImageConversationCard, 'image', readonly=True)
+resource_property(ImageConversationCard, 'preview_url', readonly=True)
+resource_property(ImageConversationCard, 'updated_at', readonly=True, transform=TRANSFORM.TIME)
+# writable
+resource_property(ImageConversationCard, 'cover_image_id')
+resource_property(ImageConversationCard, 'fouth_cta')
+resource_property(ImageConversationCard, 'fouth_cta_tweet')
+resource_property(ImageConversationCard, 'image_media_id')
+resource_property(ImageConversationCard, 'first_cta')
+resource_property(ImageConversationCard, 'first_cta_tweet')
+resource_property(ImageConversationCard, 'name')
+resource_property(ImageConversationCard, 'second_cta')
+resource_property(ImageConversationCard, 'second_cta_tweet')
+resource_property(ImageConversationCard, 'thank_you_text')
+resource_property(ImageConversationCard, 'thank_you_url')
+resource_property(ImageConversationCard, 'third_cta')
+resource_property(ImageConversationCard, 'third_cta_tweet')
+resource_property(ImageConversationCard, 'title')
+
+
+class VideoConversationCard(Resource, Persistence):
+
+    PROPERTIES = {}
+
+    RESOURCE_COLLECTION = '/' + API_VERSION + '/accounts/{account_id}/cards/video_conversation'
+    RESOURCE = '/' + API_VERSION + '/accounts/{account_id}/cards/video_conversation/{id}'
+
+
+# video conversation card properties
+# read-only
+
+resource_property(VideoConversationCard, 'card_uri', readonly=True)
+resource_property(VideoConversationCard, 'card_type', readonly=True)
+resource_property(VideoConversationCard, 'created_at', readonly=True, transform=TRANSFORM.TIME)
+resource_property(VideoConversationCard, 'deleted', readonly=True, transform=TRANSFORM.BOOL)
+resource_property(VideoConversationCard, 'id', readonly=True)
+resource_property(VideoConversationCard, 'preview_url', readonly=True)
+resource_property(VideoConversationCard, 'video_url', readonly=True)
+resource_property(VideoConversationCard, 'video_poster_url', readonly=True)
+resource_property(VideoConversationCard, 'updated_at', readonly=True, transform=TRANSFORM.TIME)
+# writable
+resource_property(ImageConversationCard, 'cover_image_id')
+resource_property(ImageConversationCard, 'cover_video_id')
+resource_property(ImageConversationCard, 'fouth_cta')
+resource_property(ImageConversationCard, 'fouth_cta_tweet')
+resource_property(ImageConversationCard, 'image_media_id')
+resource_property(ImageConversationCard, 'first_cta')
+resource_property(ImageConversationCard, 'first_cta_tweet')
+resource_property(ImageConversationCard, 'name')
+resource_property(ImageConversationCard, 'second_cta')
+resource_property(ImageConversationCard, 'second_cta_tweet')
+resource_property(ImageConversationCard, 'thank_you_text')
+resource_property(ImageConversationCard, 'thank_you_url')
+resource_property(ImageConversationCard, 'third_cta')
+resource_property(ImageConversationCard, 'third_cta_tweet')
+resource_property(ImageConversationCard, 'title')
+resource_property(ImageConversationCard, 'video_id')
+
+
+class ScheduledTweet(Resource, Persistence):
+
+    PROPERTIES = {}
+
+    RESOURCE_COLLECTION = '/' + API_VERSION + '/accounts/{account_id}/scheduled_tweets'
+    RESOURCE = '/' + API_VERSION + '/accounts/{account_id}/scheduled_tweets/{id}'
+    PREVIEW = '/' + API_VERSION + '/accounts/{account_id}/scheduled_tweets/preview/{id}'
+
+    def preview(self):
+        """
+        Returns an HTML preview for a Scheduled Tweet.
+        """
+        if self.id:
+            resource = self.PREVIEW
+            resource = resource.format(account_id=self.account.id, id=self.id)
+            response = Request(self.account.client, 'get', resource).perform()
+            return response.body['data']
+
+
+# scheduled tweet properties
+# read-only
+resource_property(ScheduledTweet, 'created_at', readonly=True, transform=TRANSFORM.TIME)
+resource_property(ScheduledTweet, 'completed_at', read_only=True, transform=TRANSFORM.TIME)
+resource_property(ScheduledTweet, 'id', read_only=True)
+resource_property(ScheduledTweet, 'id_str', read_only=True)
+resource_property(ScheduledTweet, 'media_keys', readonly=True, transform=TRANSFORM.LIST)
+resource_property(ScheduledTweet, 'scheduled_status', read_only=True)
+resource_property(ScheduledTweet, 'tweet_id', readonly=True)
+resource_property(ScheduledTweet, 'updated_at', readonly=True, transform=TRANSFORM.TIME)
+resource_property(ScheduledTweet, 'user_id', read_only=True)
+# writable
+resource_property(ScheduledTweet, 'as_user_id')
+resource_property(ScheduledTweet, 'card_uri')
+resource_property(ScheduledTweet, 'media_ids', transform=TRANSFORM.LIST)
+resource_property(ScheduledTweet, 'nullcast', transform=TRANSFORM.BOOL)
+resource_property(ScheduledTweet, 'scheduled_at', transform=TRANSFORM.TIME)
+resource_property(ScheduledTweet, 'text')
