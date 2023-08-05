7b1f9d23eb5b65da78951f5270ac7a1c25408db3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:07 +0000

    Add package aetros/0.13.0/aetros-0.13.0.tar.gz

---
+++ packages/aetros/aetros-0.13.0.tar.gz/aetros-0.13.0/aetros/commands/IdCommand.py
@@ -0,0 +1,34 @@
+from __future__ import absolute_import
+from __future__ import print_function
+import argparse
+
+import six
+from aetros import api
+from aetros.utils import read_home_config
+
+
+class IdCommand:
+    def __init__(self, logger):
+        self.logger = logger
+        self.client = None
+        self.registered = False
+        self.active = True
+
+    def main(self, args):
+        import aetros.const
+
+        parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter,
+                                         prog=aetros.const.__prog__ + ' run')
+
+        parsed_args = parser.parse_args(args)
+        config = read_home_config()
+
+        user = api.user()
+
+        print("Logged in as %s (%s) on %s" % (user['username'], user['name'], config['host']))
+
+        if len(user['accounts']) > 0:
+            for orga in six.itervalues(user['accounts']):
+                print("  %s of organisation %s (%s)." % ("Owner" if orga['memberType'] == 1 else "Member", orga['username'], orga['name']))
+        else:
+            print("  Without membership to an organisation.")
---
+++ packages/aetros/aetros-0.13.0.tar.gz/aetros-0.13.0/aetros/commands/InitCommand.py
@@ -0,0 +1,55 @@
+from __future__ import absolute_import
+from __future__ import print_function
+import argparse
+
+import os
+
+import sys
+import ruamel.yaml as yaml
+
+from aetros import api
+from aetros.utils import read_home_config
+
+
+class InitCommand:
+    def __init__(self, logger):
+        self.logger = logger
+        self.client = None
+        self.registered = False
+        self.active = True
+
+    def main(self, args):
+        import aetros.const
+
+        parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter,
+            prog=aetros.const.__prog__ + ' run')
+        parser.add_argument('name', nargs='?', help="Model name")
+        parser.add_argument('--private', action='store_true', help="Make the model private. Example: aetros init my-model --private")
+
+        home_config = read_home_config()
+        parsed_args = parser.parse_args(args)
+        if not parsed_args.name:
+            parser.print_help()
+            sys.exit(1)
+
+        if os.path.exists('aetros.yml'):
+            config = yaml.safe_load(open('aetros.yml', 'r'))
+            if isinstance(config, dict) and 'model' in config:
+                print("failed: aetros.yml already exists with a linked model to " + config['model'])
+                sys.exit(1)
+
+        if not parsed_args.private:
+            print("Warning: creating public model. Use --private to create private models.")
+
+        try:
+            name = api.create_model(parsed_args.name or (os.path.basename(os.getcwd())), parsed_args.private)
+        except api.ApiError as e:
+            if e.error != 'already_exists':
+                raise e
+            name = parsed_args.name
+
+        with open('aetros.yml', 'w') as f:
+            f.write('model: ' + name)
+
+        print("aetros.yml created linked with model " + name + ' in ' + os.getcwd())
+        print("Open AETROS Trainer to see the model at https://" + home_config['host'] + '/model/' + name)
