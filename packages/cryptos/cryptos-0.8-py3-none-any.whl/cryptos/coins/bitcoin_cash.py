704d85c2104893ea6eb81ca8e23de1e34f004940
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:20 +0000

    Add package cryptos/0.8/cryptos-0.8-py3-none-any.whl

---
+++ packages/cryptos/cryptos-0.8-py3-none-any.whl/cryptos/coins/bitcoin_cash.py
@@ -0,0 +1,31 @@
+from .bitcoin import Bitcoin
+from ..transaction import SIGHASH_ALL, SIGHASH_FORKID
+from ..explorers import blockdozer
+
+class BitcoinCash(Bitcoin):
+    display_name = "Bitcoin Cash"
+    coin_symbol = "bcc"
+    magicbyte = 0
+    hashcode = SIGHASH_ALL + SIGHASH_FORKID
+
+    def __init__(self, testnet=False, **kwargs):
+        super(BitcoinCash, self).__init__(testnet, **kwargs)
+        if self.is_testnet:
+            self.display_name = "Bitcoin Cash Testnet"
+            self.coin_symbol = "tbcc"
+            self.magicbyte = 111
+
+    def unspent(self, *addrs):
+        return blockdozer.unspent(*addrs, coin_symbol=self.coin_symbol)
+
+    def history(self, *addrs):
+        return blockdozer.history(*addrs, coin_symbol=self.coin_symbol)
+
+    def fetchtx(self, tx):
+        return blockdozer.fetchtx(tx, coin_symbol=self.coin_symbol)
+
+    def txinputs(self, tx):
+        return blockdozer.txinputs(tx, coin_symbol=self.coin_symbol)
+
+    def pushtx(self, tx):
+        return blockdozer.pushtx(tx, coin_symbol=self.coin_symbol)
