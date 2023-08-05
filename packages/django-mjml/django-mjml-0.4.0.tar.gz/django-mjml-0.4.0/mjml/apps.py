fa46c22ad5c85bc2aab93a080ea99426fcbb5504
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:02 +0000

    Add package django-mjml/0.4.0/django-mjml-0.4.0.tar.gz

---
+++ packages/django-mjml/django-mjml-0.4.0.tar.gz/django-mjml-0.4.0/mjml/apps.py
@@ -0,0 +1,27 @@
+from django.apps import AppConfig
+from django.core.exceptions import ImproperlyConfigured
+from .tools import mjml_render
+from . import settings as mjml_settings
+
+
+def check_mjml_command():
+    test_mjml = '<mjml><mj-body><mj-container></mj-container></mj-body></mjml>'
+    test_result_fragment = '<html '
+    try:
+        html = mjml_render(test_mjml)
+    except RuntimeError as e:
+        raise ImproperlyConfigured(e)
+    if test_result_fragment not in html:
+        raise ImproperlyConfigured(
+            'mjml command returns wrong result.\n'
+            'Check installation mjml. See https://github.com/mjmlio/mjml#installation'
+        )
+
+
+class MJMLConfig(AppConfig):
+    name = 'mjml'
+    verbose_name = 'Use MJML in Django templates'
+
+    def ready(self):
+        if mjml_settings.MJML_BACKEND_MODE == 'cmd':
+            check_mjml_command()
---
+++ packages/django-mjml/django-mjml-0.4.0.tar.gz/django-mjml-0.4.0/mjml/node/tcpserver.js
@@ -0,0 +1,73 @@
+'use strict';
+
+
+var host = '127.0.0.1',
+    port = '28101',
+    touchstop_fn = null,
+    argv = process.argv.slice(2);
+
+
+switch (argv.length) {
+    case 0:
+        break;
+    case 1:
+        port = argv[0];
+        break;
+    case 2:
+        port = argv[0];
+        host = argv[1];
+        break;
+    case 3:
+        port = argv[0];
+        host = argv[1];
+        touchstop_fn = argv[2];
+        break;
+    default:
+        console.log('Run command: NODE_PATH=node_modules node tcpserver.js 28101 127.0.0.1 /tmp/mjmltcpserver.stop');
+}
+
+
+var mjml = require('mjml'),
+    net = require('net'),
+    fs = require('fs'),
+    server = net.createServer();
+
+
+function handleConnection(conn) {
+    conn.setEncoding('utf8');
+    conn.on('data', function(d) {
+        var result;
+        try {
+            result = mjml.mjml2html(d.toString());
+            if (typeof result === 'object') result = result.html;
+            conn.write('0');
+        } catch (err) {
+            result = err.message;
+            conn.write('1');
+        }
+        conn.write(('000000000' + Buffer.byteLength(result).toString()).slice(-9));
+        conn.write(result);
+    });
+    conn.once('close', function() {});
+    conn.on('error', function(err) {});
+}
+
+
+server.on('connection', handleConnection);
+server.listen(port, host, function () {
+    console.log('RUN SERVER %s:%s', host, port);
+});
+
+
+if (touchstop_fn) {
+    try {
+        fs.statSync(touchstop_fn);
+    } catch (e) {
+        fs.closeSync(fs.openSync(touchstop_fn, 'w'));
+    }
+
+    fs.watchFile(touchstop_fn, function() {
+        server.close();
+        process.exit();
+    });
+}
