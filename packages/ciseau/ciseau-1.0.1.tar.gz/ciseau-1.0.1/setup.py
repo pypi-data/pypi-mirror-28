3b7289a18aa48935309447a50a7e0900ed4ca77e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:05 +0000

    Add package ciseau/1.0.1/ciseau-1.0.1.tar.gz

---
+++ packages/ciseau/ciseau-1.0.1.tar.gz/ciseau-1.0.1/setup.py
@@ -0,0 +1,34 @@
+import os
+from setuptools import setup, find_packages
+
+def readfile(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='ciseau',
+    version='1.0.1',
+    description='Word and sentence tokenization.',
+    long_description=readfile('README.md'),
+    ext_modules=[],
+    packages=find_packages(),
+    py_modules = [],
+    author='Jonathan Raiman',
+    author_email='jonathanraiman@gmail.com',
+    url='https://github.com/JonathanRaiman/ciseau',
+    download_url='https://github.com/JonathanRaiman/ciseau',
+    keywords='XML, tokenization, NLP',
+    license='MIT',
+    platforms='any',
+    zip_safe=False,
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Text Processing :: Linguistic',
+    ],
+    setup_requires = [],
+    install_requires=[
+    ],
+    include_package_data=True,
+)
