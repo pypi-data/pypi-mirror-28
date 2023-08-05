683a77121acf416a2435d69413f7552fd451010a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:25 +0000

    Add package scbw/0.2a1/scbw-0.2a1-py3-none-any.whl

---
+++ packages/scbw/scbw-0.2a1-py3-none-any.whl/scbw/utils.py
@@ -0,0 +1,47 @@
+from random import choice
+from typing import Optional
+
+
+def levenshtein_dist(s1, s2):
+    if len(s1) < len(s2):
+        return levenshtein_dist(s2, s1)
+
+    # len(s1) >= len(s2)
+    if len(s2) == 0:
+        return len(s1)
+
+    previous_row = range(len(s2) + 1)
+    for i, c1 in enumerate(s1):
+        current_row = [i + 1]
+        for j, c2 in enumerate(s2):
+            insertions = previous_row[
+                             j + 1] + 1  # j+1 instead of j since previous_row and current_row are one character longer
+            deletions = current_row[j] + 1  # than s2
+            substitutions = previous_row[j] + (c1 != c2)
+            current_row.append(min(insertions, deletions, substitutions))
+        previous_row = current_row
+
+    return previous_row[-1]
+
+
+def random_string(len: int = 8) -> str:
+    return "".join(choice("0123456789ABCDEF") for _ in range(len))
+
+
+def which(program: str) -> Optional[str]:
+    import os
+
+    def is_exe(fpath: str):
+        return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
+
+    fpath, fname = os.path.split(program)
+    if fpath:
+        if is_exe(program):
+            return program
+    else:
+        for path in os.environ["PATH"].split(os.pathsep):
+            exe_file = os.path.join(path, program)
+            if is_exe(exe_file):
+                return exe_file
+
+    return None
