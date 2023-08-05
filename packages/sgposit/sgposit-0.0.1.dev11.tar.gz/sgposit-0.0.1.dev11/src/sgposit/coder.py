c83b96aed599a4013a45d7966a9bb1ca97a3f3e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:06 +0000

    Add package sgposit/0.0.1.dev11/sgposit-0.0.1.dev11.tar.gz

---
+++ packages/sgposit/sgposit-0.0.1.dev11.tar.gz/sgposit-0.0.1.dev11/src/sgposit/coder.py
@@ -0,0 +1,291 @@
+# MIT License
+#
+# Copyright (c) 2018 SpeedGo Computing
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+
+import copy
+import numbers
+import sys
+
+if sys.version_info.major >= 3:
+    from math import gcd
+else:
+    from fractions import gcd
+
+from sgposit import bitops
+
+
+def create_positrep(nbits=32, es=2, s=0, k=0, e=0, f=0, h=0, t='n'):
+    return { 's': s, 'k': k, 'e': e, 'f': f, 'h': h, 'nbits': nbits, 'es': es, 't': t }
+
+
+def create_zero_positrep(nbits=32, es=2):
+    return create_positrep(nbits=nbits, es=es, s=0, k=0, e=0, f=0, h=0, t='z')
+
+
+def create_cinf_positrep(nbits=32, es=2):
+    return create_positrep(nbits=nbits, es=es, s=0, k=0, e=0, f=0, h=0, t='c')
+
+
+def copy_positrep(rep):
+    return copy.deepcopy(rep)
+
+
+def decode_posit_binary(bits, nbits, es):
+    assert nbits >= 2 and es >= 0
+
+    rep = create_positrep(nbits=nbits, es=es)
+
+    if bits == 0:
+        rep['t'] = 'z'
+        return rep
+    elif bits == (1 << (nbits-1)):
+        rep['t'] = 'c'
+        return rep
+
+    assert rep['t'] == 'n'
+
+    rep['s'] = bitops.get_int_bits(bits, nbits-1)
+
+    if rep['s'] == 1: bits = -bits
+
+    regime = bitops.get_int_bits(bits, nbits-2)
+    nleads = bitops.count_leading_bits(bits, regime, nbits-2)
+
+    if regime == 0:
+        assert 1 <= nleads and nleads <= nbits-2
+        rep['k'] = -nleads
+    else:
+        assert 1 <= nleads and nleads <= nbits-1
+        rep['k'] = nleads - 1
+
+    i = nbits - 1 - nleads - 1 - 1      # n - signbit - rs - rbar - nextbit
+
+    e = 0
+    nebits = 0
+    while i >= 0 and nebits < es:
+        e = 2*e | bitops.get_int_bits(bits, i)
+        i -= 1
+        nebits += 1
+    e <<= es - nebits
+    rep['e'] = e
+
+    rep['h'] = max(0, i+1)      # Remaining bits, and +1 since i is 0-based.
+    if rep['h'] > 0:
+        rep['f'] = bitops.get_int_bits(bits, 0, i)
+
+    return rep
+
+
+def encode_posit_binary(rep):
+    assert isinstance(rep['nbits'], numbers.Integral) and rep['nbits'] >= 2
+    assert isinstance(rep['es'   ], numbers.Integral) and rep['es'] >= 0
+    assert isinstance(rep['s'    ], numbers.Integral) and (rep['s'] == 0 or rep['s'] == 1)
+    assert isinstance(rep['e'    ], numbers.Integral) and rep['e'] >= 0 and rep['e'] <= 2**rep['es'] - 1
+    assert isinstance(rep['h'    ], numbers.Integral) and rep['h'] >= 0
+    assert isinstance(rep['f'    ], numbers.Integral) and rep['f'] >= 0 and rep['f'] <= 2**rep['h'] - 1
+    assert rep['t'] in ['c', 'n', 'z']
+
+    if rep['t'] == 'z':
+        return 0
+    elif rep['t'] == 'c':
+        return 1 << (rep['nbits']-1)
+
+    assert rep['t'] == 'n'
+
+    maxpos_bits = 2**(rep['nbits']-1) - 1
+
+    n = rep['nbits'] - 1    # Remaining number of bits after reserving 1 for sign bit.
+    bits = 0
+    rounded = False
+
+    if rep['k'] >= 0:
+        if n >= rep['k'] + 1:
+            bits = bitops.create_mask(rep['k'] + 1)
+            n -= rep['k'] + 1
+
+            assert n >= 0
+
+            if n > 0:
+                bits <<= 1
+                n -= 1
+        else:
+            bits = bitops.create_mask(n)
+            n = 0
+            rounded = True
+    else:
+        bits = 1
+        if n < -rep['k'] + 1:
+            rounded = True
+        n -= min(n, -rep['k'] + 1)
+
+    assert n >= 0
+
+    if n >= rep['es']:
+        bits <<= rep['es']
+        bits |= rep['e']
+        n -= rep['es']
+    elif rounded == False:
+        assert rep['es'] > 0
+
+        m = rep['es'] - n       # Number of exponent bits to truncate.
+        te = rep['e'] >> m
+        te <<= m                # Rounded down exponent bits.
+
+        value = rep['e'] * 2**rep['h'] + rep['f']
+        represented_value = te * 2**rep['h']
+        truncation = value - represented_value
+        tie = 2**(m + rep['h'] - 1)
+
+        te >>= m        # Truncated exponent bits.
+        bits <<= n
+        bits |= te
+        n = 0
+
+        if truncation == tie:
+            if bits & 0x01 and bits < maxpos_bits:
+                bits += 1
+        elif truncation > tie:
+            if bits < maxpos_bits:
+                bits += 1
+
+        rounded = True
+
+    assert n >= 0
+
+    if n > rep['h']:
+        bits <<= rep['h']
+        bits |= rep['f']
+        n -= rep['h']
+
+        bits <<= n
+        n = 0
+
+    elif rounded == False:
+        m = rep['h'] - n    # Number of fractional bits to truncate.
+        tf = rep['f'] >> m
+        tf <<= m            # Rounded down fractional bits.
+
+        value = rep['f']
+        represented_value = tf
+        truncation = value - represented_value
+        tie = 2**(m-1)
+
+        tf >>= m    # Truncated fractional bits.
+        bits <<= n
+        bits |= tf
+        n = 0
+
+        if truncation == tie:
+            if bits & 0x01 and bits < maxpos_bits:
+                bits += 1
+        elif truncation > tie:
+            if bits < maxpos_bits:
+                bits += 1
+
+        rounded = True
+
+    assert n == 0
+
+    if rep['s'] == 1:
+        bits = -bits
+        mask = bitops.create_mask(rep['nbits'])
+        bits &= mask
+
+    assert isinstance(bits, numbers.Integral) and bits >= 0 and bits <= bitops.create_mask(rep['nbits'])
+
+    return bits
+
+
+# rep: normal posit representation,
+# return (sign, intpart, num, den) where number = sign*(intpart + num/den)
+def positrep_normal_to_rational(rep):
+    assert rep['t'] == 'n'
+
+    E = (2**rep['es'] * rep['k']) + rep['e']
+    V = (1 << rep['h']) + rep['f']
+    D = rep['h']
+
+    if E >= 0:
+        V <<= E
+    else:
+        D += -E
+
+    mask = bitops.create_mask(D)
+    num = V & mask
+    den = 1 << D
+    intpart = V >> D
+
+    g = gcd(num, den)
+    num //= g
+    den //= g
+    sign = 1 if rep['s'] == 0 else -1
+
+    assert sign == -1 or sign == 1
+    assert intpart >= 0
+    assert num >= 0
+    assert den >= 1
+
+    return (sign, intpart, num, den)
+
+
+# rational: (sign, intpart, num, den) where number = sign*(intpart + num/den)
+def rational_to_str(rational, separate_intpart=False):
+
+    (sign, intpart, num, den) = rational
+
+    assert sign == -1 or sign == 1
+    assert intpart >= 0
+    assert num >= 0
+    assert den >= 1
+
+    out = '' if sign > 0 else '-'
+
+    if num == 0:
+        out += str(intpart)
+    else:
+        if separate_intpart:
+            if intpart != 0:
+                out += str(intpart)
+                out += '+' if sign > 0 else '-'
+        else:
+            num += den*intpart
+
+        out += str(num) + '/' + str(den)
+
+    return out
+
+
+def positrep_to_rational_str(rep, separate_intpart=False):
+    if rep['t'] == 'z':
+        return '0'
+    elif rep['t'] == 'c':
+        return 'cinf'
+
+    assert rep['t'] == 'n'
+
+    rational = positrep_normal_to_rational(rep)
+
+    return rational_to_str(rational, separate_intpart=separate_intpart)
+
+
+def positrep_to_str(rep):
+    return positrep_to_rational_str(rep, separate_intpart=True)
---
+++ packages/sgposit/sgposit-0.0.1.dev11.tar.gz/sgposit-0.0.1.dev11/src/sgposit/pcposit.py
@@ -0,0 +1,278 @@
+# MIT License
+#
+# Copyright (c) 2018 SpeedGo Computing
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+
+import numbers
+import operator
+
+from sgposit import coder
+
+
+"""
+Provably correct posit number arithmetic.
+"""
+class PCPosit:
+
+    def __init__(self, v=None, mode=None, nbits=None, es=None):
+        nbits_given = True
+        es_given = True
+        if nbits is None:
+            nbits_given = False
+            nbits = 32
+        if es is None:
+            es_given = False
+            es = 2
+
+        if v is None:
+            self.rep = coder.create_zero_positrep(nbits=nbits, es=es)
+            return
+        elif isinstance(v, PCPosit):
+            if nbits_given and v.rep['nbits'] != nbits:
+                raise NotImplementedError('Mismatched nbits posit conversion is not implemented.')
+            if es_given and v.rep['es'] != es:
+                raise NotImplementedError('Mismatched es posit conversion is not implemented.')
+            self.rep = coder.copy_positrep(v.rep)
+            return
+        elif mode == 'bits':
+            if isinstance(v, numbers.Integral):
+                self.rep = coder.decode_posit_binary(v, nbits=nbits, es=es)
+                return
+            elif isinstance(v, str):
+                raise NotImplementedError('Binary bit string posit conversion is not implemented.')
+        elif isinstance(v, str):
+            if v == 'cinf':
+                self.rep = coder.create_cinf_positrep(nbits=nbits, es=es)
+            elif v == '0':
+                self.rep = coder.create_zero_positrep(nbits=nbits, es=es)
+            else:
+                raise ValueError('Expect 0 or cinf posit consntant from the input string.')
+
+            return
+
+        raise ValueError('Input is not supported.')
+
+
+    def __add__(self, other):
+        if self.rep['t'] == 'z':
+            return PCPosit(other)
+        elif other.rep['t'] == 'z':
+            return PCPosit(self)
+        elif self.rep['t'] == 'c' or other.rep['t'] == 'c':
+            return PCPosit('cinf', nbits=self.rep['nbits'], es=self.rep['es'])
+
+        assert self.rep['t'] == 'n' and other.rep['t'] == 'n'
+
+        (xa,ma) = self._fixedpoint()
+        (xb,mb) = other._fixedpoint()
+
+        m = max(ma, mb)
+        xc = xa*2**(m-mb) + xb*2**(m-ma)
+        mc = ma + mb - m
+
+        return self._fixedpoint_to_posit(xc, mc, nbits=self.rep['nbits'], es=self.rep['es'])
+
+
+    def __sub__(self, other):
+        p = -PCPosit(other)
+        return self + p
+
+
+    def __neg__(self):
+        p = PCPosit(self)
+        if p.rep['t'] == 'n':
+            p.rep['s'] = p.rep['s'] ^ 1
+        return p
+
+
+    def __mul__(self, other):
+        if self.rep['t'] == 'c' or other.rep['t'] == 'c':
+            return PCPosit('cinf', nbits=self.rep['nbits'], es=self.rep['es'])
+        elif self.rep['t'] == 'z' or other.rep['t'] == 'z':
+            return PCPosit('0', nbits=self.rep['nbits'], es=self.rep['es'])
+
+        assert self.rep['t'] == 'n' and other.rep['t'] == 'n'
+
+        (xa,ma) = self._fixedpoint()
+        (xb,mb) = other._fixedpoint()
+
+        xc = xa * xb
+        mc = ma + mb
+
+        return self._fixedpoint_to_posit(xc, mc, nbits=self.rep['nbits'], es=self.rep['es'])
+
+
+    def __div__(self, other):
+        return self.__truediv__(other)
+
+
+    def __truediv__(self, other):
+        if self.rep['t'] == 'c' or other.rep['t'] == 'z':
+            return PCPosit('cinf', nbits=self.rep['nbits'], es=self.rep['es'])
+        elif self.rep['t'] == 'z' or other.rep['t'] == 'c':
+            return PCPosit('0', nbits=self.rep['nbits'], es=self.rep['es'])
+
+        assert self.rep['t'] == 'n' and other.rep['t'] == 'n'
+
+        (xa,ma) = self._fixedpoint()
+        (xb,mb) = other._fixedpoint()
+
+        nbits = self.rep['nbits']
+        es = self.rep['es']
+        sign = 1
+        if (xa < 0)^(xb < 0): sign = -1
+        if xa < 0: xa = -xa
+        if xb < 0: xb = -xb
+
+        g = ma - mb + (2**es)*(nbits-2) + nbits - 1
+        g = max(0, g)
+        xc = (xa * 2**g) // xb
+        mc = ma - mb - g
+
+        xc = max(xc, 1)     # Posit never round to 0.
+        xc *= sign
+
+        return self._fixedpoint_to_posit(xc, mc, nbits=nbits, es=es)
+
+
+    def __floordiv__(self, other):
+        raise NotImplementedError
+
+
+    def __eq__(self, other):
+        a = self.rep
+        b = other.rep
+
+        if a['t'] == 'c' or b['t'] == 'c':
+            return False
+
+        elif a['t'] == 'z' or b['t'] == 'z':
+            if a['t'] == b['t']:
+                return True
+            else:
+                return False
+
+        else:
+            assert a['t'] == 'n'
+            assert b['t'] == 'n'
+
+            return a == b
+
+
+    def __ne__(self, other):
+        return not(self == other)
+
+
+    def __lt__(self, other):
+        return self._cmp_op(other, operator.lt)
+
+
+    def __le__(self, other):
+        return self._cmp_op(other, operator.le)
+
+
+    def __gt__(self, other):
+        return self._cmp_op(other, operator.gt)
+
+
+    def __ge__(self, other):
+        return self._cmp_op(other, operator.ge)
+
+
+    def __str__(self):
+        return coder.positrep_to_str(self.rep)
+
+
+    def _cmp_op(self, other, op):
+        a = self.rep
+        b = other.rep
+
+        if a['t'] == 'c' or b['t'] == 'c':
+            return False
+
+        (xa,ma) = self._fixedpoint()
+        (xb,mb) = other._fixedpoint()
+
+        m = max(ma, mb)
+        xa <<= m - mb
+        xb <<= m - ma
+
+        return op(xa, xb)
+
+
+    # Return (x,m) representing number = x * 2^m
+    def _fixedpoint(self):
+        rep = self.rep
+
+        assert rep['t'] != 'c'
+
+        if rep['t'] == 'z':
+            x = 0
+            m = 0
+        else:
+            assert rep['t'] == 'n'
+
+            x = (-1)**rep['s'] * (2**rep['h'] + rep['f'])
+            m = 2**rep['es'] * rep['k'] + rep['e'] - rep['h']
+
+        return (x,m)
+
+
+    @classmethod
+    def _fixedpoint_to_posit(cls, x, m, nbits=None, es=None):
+        assert nbits is not None
+        assert es is not None
+
+        if x == 0:
+            return PCPosit('0', nbits=nbits, es=es)
+
+        p = PCPosit(nbits=nbits, es=es)
+        p.rep['t'] = 'n'
+        p.rep['s'] = 0
+
+        if x < 0:
+            x = -x
+            p.rep['s'] = 1
+
+        assert x != 0
+
+        while x != 0 and x % 2 == 0:
+            x >>= 1
+            m += 1
+
+        g = 0
+        y = x
+        while y >= 2:
+            y >>= 1
+            g -= 1
+
+        assert y >= 1 and y < 2, "y={}".format(y)
+
+        p.rep['e'] = (m - g) % 2**es
+        p.rep['k'] = (m - g) // 2**es
+
+        p.rep['h'] = -g
+        p.rep['f'] = x - 2**p.rep['h']
+
+        bits = coder.encode_posit_binary(p.rep)
+        p.rep = coder.decode_posit_binary(bits, nbits=nbits, es=es)
+
+        return p
