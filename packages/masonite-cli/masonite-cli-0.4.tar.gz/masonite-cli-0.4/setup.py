7e3d5a299e594bc884781e86108231fb68d5c3fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:22 +0000

    Add package masonite-cli/0.4/masonite-cli-0.4.tar.gz

---
+++ packages/masonite-cli/masonite-cli-0.4.tar.gz/masonite-cli-0.4/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="masonite-cli",
+    version='0.4',
+    py_modules=['craft'],
+    install_requires=[
+        'Click',
+    ],
+    entry_points='''
+        [console_scripts]
+        craft=craft:cli
+    ''',
+)
