00b68ba37e5d15c496c10db671c43c399024d338
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:22 +0000

    Add package pyliupeng/1.1.0/pyliupeng-1.1.0.tar.gz

---
+++ packages/pyliupeng/pyliupeng-1.1.0.tar.gz/pyliupeng-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'pyliupeng',      
+    version = '1.1.0',
+    py_modules = ['test'],
+    author = 'beihan',        
+    author_email = '1070263027@qq.com',
+    url = 'https://www.cnblogs.com/bywallance/p/6886519.html',
+    description = 'xingxing',  
+)
