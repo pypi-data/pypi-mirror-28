a2e859b4a6b5c1e712414fb646f5a412d2808110
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:14 +0000

    Add package nester-er-1/1.1.0/nester-er-1-1.1.0.tar.gz

---
+++ packages/nester-er-1/nester-er-1-1.1.0.tar.gz/nester-er-1-1.1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name = 'nester-er-1',
+    version = '1.1.0',
+    py_modules =['nester-er-1'],
+    author ='ermeiyao',
+    author_email = 'ermeiyao@aliyun.com',
+    url= 'http://github.com/storborg/funniest',
+    description   = 'A simple printer of nested lists',
+    )
