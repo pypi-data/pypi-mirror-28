986359702470b33d696e4480b6a26dc53ea428dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:45 +0000

    Add package skopos/0.2/skopos-0.2.tar.gz

---
+++ packages/skopos/skopos-0.2.tar.gz/skopos-0.2/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+from setuptools import setup, find_packages
+
+setup(
+  name = 'skopos',
+  # packages = ['skopos'],
+  version = '0.2',
+  description = 'Deep Reinforcement Learning Library',
+  author = 'Skopos-team',
+  packages=find_packages(),
+  # packages=[package for package in find_packages() if package.startswith('skopos')],
+  install_requires=['numpy>=1.13.1', 'tensorflow>=1.4', 'matplotlib>=2.0.2','scipy>=1.0.0', 'matplotlib>=2.0.2'],
+  author_email = 'skopos.library@gmail.com',
+  url = 'https://github.com/Skopos-team/Skopos', 
+  download_url = 'https://github.com/Skopos-team/Skopos/archive/0.2.tar.gz',
+  keywords = ['testing', 'logging', 'example'],
+  classifiers = [],
+)
