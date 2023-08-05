df614d1c699d05970a6fffa21a1f1d84fba90ffb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:06 +0000

    Add package tfserver/0.1a27/tfserver-0.1a27.tar.gz

---
+++ packages/tfserver/tfserver-0.1a27.tar.gz/tfserver-0.1a27/tfserver/__init__.py
@@ -0,0 +1,45 @@
+import os
+import tensorflow as tf
+import time
+
+__version__ = "0.1a27"
+
+class Session:
+	def __init__ (self, model_dir, tfconfig = None):
+		self.model_dir = model_dir
+		self.tfconfig = tfconfig
+		
+		self.tfsess = tf.Session (config = tfconfig)
+		self.tfsess.run (tf.global_variables_initializer())	
+		meta = tf.saved_model.loader.load (
+			self.tfsess, 
+			[tf.saved_model.tag_constants.SERVING], 
+			model_dir
+		)
+		
+		self.signature_def_name, signature_def = meta.signature_def.popitem ()	
+		self.input_map = {}
+		self.output_map = {}
+
+		for k, v in signature_def.inputs.items ():
+			self.input_map [k] = v.name
+		for k, v in signature_def.outputs.items ():
+			self.output_map [k] = v.name
+	
+	def run (self, *args, **kargs):
+		return self.tfsess.run (*args, **kargs)
+		
+	def close (self):
+		self.tfsess.close ()
+	
+tfsess = None
+			
+def load_model (model_dir, tfconfig = None):
+	global tfsess	
+	tfsess = Session (model_dir, tfconfig)
+	
+def close ():
+	global tfsess	
+	tfsess.close ()
+	tfsess = None
+	
