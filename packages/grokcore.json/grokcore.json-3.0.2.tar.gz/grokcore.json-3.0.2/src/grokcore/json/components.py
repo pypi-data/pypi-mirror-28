5ead561e9dcd9a4f1c657bea770c201df8204914
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:59 +0000

    Add package grokcore.json/3.0.2/grokcore.json-3.0.2.tar.gz

---
+++ packages/grokcore.json/grokcore.json-3.0.2.tar.gz/grokcore.json-3.0.2/src/grokcore/json/components.py
@@ -0,0 +1,38 @@
+##############################################################################
+#
+# Copyright (c) 2006-2007 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Base classes for Grok application components.
+
+When an application developer builds a Grok-based application, the
+classes they define each typically inherit from one of the base classes
+provided here.
+
+"""
+
+from zope.publisher.browser import BrowserPage
+from zope.publisher.publish import mapply
+import json
+import grokcore.view
+from zope.interface import implementer
+
+
+@implementer(grokcore.view.IGrokSecurityView)
+class JSON(BrowserPage, grokcore.view.ViewSupport):
+    """Base class for JSON views in Grok applications."""
+
+    def __call__(self):
+        view_name = self.__view_name__
+        method = getattr(self, view_name)
+        method_result = mapply(method, (), self.request)
+        self.request.response.setHeader('Content-Type', 'application/json')
+        return json.dumps(method_result)
