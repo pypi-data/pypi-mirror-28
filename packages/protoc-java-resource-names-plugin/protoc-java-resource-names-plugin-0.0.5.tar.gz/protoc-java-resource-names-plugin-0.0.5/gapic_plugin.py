2e57ea7debb66a5be4fb77b4a6ceddef26cec7f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:23 +0000

    Add package protoc-java-resource-names-plugin/0.0.5/protoc-java-resource-names-plugin-0.0.5.tar.gz

---
+++ packages/protoc-java-resource-names-plugin/protoc-java-resource-names-plugin-0.0.5.tar.gz/protoc-java-resource-names-plugin-0.0.5/gapic_plugin.py
@@ -0,0 +1,173 @@
+#!/usr/bin/env python
+
+# Copyright 2016, Google Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+#     * Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above
+# copyright notice, this list of conditions and the following disclaimer
+# in the documentation and/or other materials provided with the
+# distribution.
+#     * Neither the name of Google Inc. nor the names of its
+# contributors may be used to endorse or promote products derived from
+# this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import os
+import sys
+import pystache
+
+from plugin.compiler import plugin_pb2 as plugin
+from google.protobuf.descriptor_pb2 import FieldDescriptorProto
+
+from plugin.templates import resource_name, insertion_points
+from plugin.utils import proto_utils, gapic_utils
+
+
+TEMPLATE_LOCATION = os.path.join('plugin', 'templates')
+
+def render_new_file(renderer, response, resource):
+  f = response.file.add()
+  f.name = resource.filename()
+  f.content = renderer.render(resource)
+
+
+def generate_resource_name_types(response, gapic_config, java_package):
+  renderer = pystache.Renderer(search_dirs=TEMPLATE_LOCATION)
+  for collection_config in gapic_config.collection_configs.values():
+    resource = resource_name.ResourceName(collection_config, java_package)
+    resource_type = resource_name.ResourceNameType(resource.className(), java_package)
+    render_new_file(renderer, response, resource)
+    render_new_file(renderer, response, resource_type)
+
+  for fixed_config in gapic_config.fixed_collections.values():
+    resource = resource_name.ResourceNameFixed(fixed_config, java_package)
+    resource_type = resource_name.ResourceNameType(resource.className(), java_package)
+    render_new_file(renderer, response, resource)
+    render_new_file(renderer, response, resource_type)
+
+  for oneof_config in gapic_config.collection_oneofs.values():
+    resource_oneof = resource_name.ResourceNameOneof(oneof_config, java_package)
+    render_new_file(renderer, response, resource_oneof)
+
+
+def generate_get_set_injection(response, gapic_config, request, java_package):
+  renderer = pystache.Renderer(search_dirs=TEMPLATE_LOCATION)
+  for pf in get_protos_to_generate_for(request):
+    for item, package in proto_utils.traverse(pf):
+      filename = os.path.join(java_package.replace('.', os.path.sep), item.name + '.java')
+      for field in item.field:
+        entity_name = gapic_config.get_entity_name_for_message_field(
+            item.name, field.name)
+        if entity_name:
+          concrete_resource = None
+          if entity_name == gapic_utils.GAPIC_CONFIG_ANY:
+            resource = resource_name.ResourceNameAny()
+            concrete_resource = resource_name.ResourceNameUntyped()
+          elif entity_name in gapic_config.collection_configs:
+            collection = gapic_config.collection_configs.get(entity_name)
+            resource = resource_name.ResourceName(collection, java_package)
+          elif entity_name in gapic_config.collection_oneofs:
+            collection = gapic_config.collection_oneofs.get(entity_name)
+            resource = resource_name.ResourceNameOneof(collection, java_package)
+          else:
+            raise ValueError('entity name not found: ' + entity_name)
+
+          f = response.file.add()
+          f.name = filename
+          f.insertion_point = 'builder_scope:' + package + '.' + item.name
+          f.content = renderer.render(get_builder_view(field)(resource, field, concrete_resource))
+
+          f = response.file.add()
+          f.name = filename
+          f.insertion_point = 'class_scope:' + package + '.' + item.name
+          f.content = renderer.render(get_class_view(field)(resource, field, concrete_resource))
+
+
+def get_builder_view(field):
+  if field.label == FieldDescriptorProto.LABEL_REPEATED:
+    return insertion_points.InsertBuilderList
+  else:
+    return insertion_points.InsertBuilder
+
+
+def get_class_view(field):
+  if field.label == FieldDescriptorProto.LABEL_REPEATED:
+    return insertion_points.InsertClassList
+  else:
+    return insertion_points.InsertClass
+
+
+def get_protos_to_generate_for(request):
+  proto_files = dict((pf.name, pf) for pf in request.proto_file)
+  for pf_name in request.file_to_generate:
+    if pf_name in proto_files:
+      yield proto_files[pf_name]
+
+
+def resolve_java_package_name(request):
+  java_package = None
+  for pf in get_protos_to_generate_for(request):
+    for item, package in proto_utils.traverse(pf):
+      for opt in proto_utils.get_named_options(pf, 'java_package'):
+        if java_package is not None and java_package != opt[1]:
+          raise ValueError('got conflicting java packages: '
+                           + str(java_package) + ' and '
+                           + str(opt[1]))
+        java_package = opt[1]
+        break
+  if java_package is None:
+    raise ValueError('java package not defined')
+  return java_package
+
+
+def main(data):
+  # Parse request
+  request = plugin.CodeGeneratorRequest()
+  request.ParseFromString(data)
+
+  java_package = resolve_java_package_name(request)
+  gapic_config = gapic_utils.read_from_gapic_yaml(request.parameter)
+
+  # Generate output
+  response = plugin.CodeGeneratorResponse()
+
+  generate_resource_name_types(response, gapic_config, java_package)
+  generate_get_set_injection(response, gapic_config, request, java_package)
+
+  # Serialise response message
+  output = response.SerializeToString()
+
+  return output
+
+if __name__ == '__main__':
+  try:
+    source = sys.stdin.buffer
+    dest = sys.stdout.buffer
+  except AttributeError:
+    source = sys.stdin
+    dest = sys.stdout
+
+  # Read request message from stdin
+  data = source.read()
+
+  output = main(data)
+
+  # Write to stdout
+  dest.write(output)
---
+++ packages/protoc-java-resource-names-plugin/protoc-java-resource-names-plugin-0.0.5.tar.gz/protoc-java-resource-names-plugin-0.0.5/test/test_plugin_baseline.py
@@ -0,0 +1,129 @@
+# Copyright 2016 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import pytest
+import subprocess
+import shutil
+import difflib
+
+from plugin.utils import casing_utils
+
+
+TEST_DIR = os.path.join('test', 'testdata')
+TEST_OUTPUT_DIR = os.path.join(TEST_DIR, 'test_output')
+
+
+def check_output(output_class, output_path, baseline):
+    baseline_file = os.path.join(TEST_DIR, baseline + '.baseline')
+    with open(baseline_file) as f:
+        expected_output = f.readlines()
+
+    actual_output_file = os.path.join(TEST_OUTPUT_DIR,
+                                      output_path,
+                                      output_class + '.java')
+    with open(actual_output_file) as f:
+        actual_output = f.readlines()
+
+    assert expected_output == actual_output, 'Baseline error. File "' \
+        + baseline_file + '" did not match "' \
+        + actual_output_file + '"\nDiff:\n' \
+        + diff(expected_output,
+               actual_output,
+               baseline_file,
+               actual_output_file)
+
+
+def diff(expected_output, actual_output, fromfile, tofile):
+    difflines = list(difflib.context_diff(expected_output,
+                                          actual_output,
+                                          fromfile=fromfile,
+                                          tofile=tofile))
+    if len(difflines) > 50:
+        difflines = difflines[:50]
+        difflines.append('*** diff too long, truncating ***\n')
+    return "".join(difflines)
+
+
+def run_protoc_gapic_plugin(output_dir, gapic_yaml, include_dirs, proto_files,
+                            lang_out=None):
+    def format_output_arg(name, output_dir, extra_arg=None):
+        if extra_arg:
+            return '--{}_out={}:{}'.format(name, extra_arg, output_dir)
+        else:
+            return '--{}_out={}'.format(name, output_dir)
+
+    args = ['protoc']
+    if lang_out is not None:
+        args.append(format_output_arg(lang_out, output_dir))
+    args += [format_output_arg('gapic', output_dir, gapic_yaml),
+             '--plugin=protoc-gen-gapic=gapic_plugin.py']
+    args += ['-I' + path for path in include_dirs]
+    args += proto_files
+    subprocess.check_call(args)
+
+
+def clean_test_output():
+    shutil.rmtree(TEST_OUTPUT_DIR, True)
+    os.mkdir(TEST_OUTPUT_DIR)
+
+
+@pytest.fixture(scope='class')
+def run_protoc():
+    clean_test_output()
+    gapic_yaml = os.path.join(TEST_DIR, 'library_gapic.yaml')
+    # TODO: make this path configurable
+    include_dirs = ['.', './googleapis']
+    proto_files = ['library_simple.proto', 'archive.proto']
+    run_protoc_gapic_plugin(TEST_OUTPUT_DIR,
+                            gapic_yaml,
+                            include_dirs,
+                            [os.path.join(TEST_DIR, x) for x in proto_files],
+                            'java')
+
+
+RESOURCE_NAMES_TO_GENERATE = ['book_name', 'shelf_name', 'archived_book_name',
+                              'deleted_book']
+ONEOFS_TO_GENERATE = ['book_name_oneof']
+MESSAGE_CLASSES_TO_EXTEND = ['book', 'shelf', 'list_books_response',
+                             'book_from_anywhere']
+
+PROTOC_OUTPUT_DIR = os.path.join('com', 'google', 'example', 'library', 'v1')
+
+
+class TestProtocGapicPlugin(object):
+
+    @pytest.mark.parametrize('resource', RESOURCE_NAMES_TO_GENERATE)
+    def test_resource_name_generation(self, run_protoc, resource):
+        generated_class = casing_utils.lower_underscore_to_upper_camel(
+            resource)
+        check_output(generated_class, PROTOC_OUTPUT_DIR, 'java_' + resource)
+
+    @pytest.mark.parametrize('resource', RESOURCE_NAMES_TO_GENERATE)
+    def test_resource_name_type_generation(self, run_protoc, resource):
+        generated_type = \
+            casing_utils.lower_underscore_to_upper_camel(resource) + 'Type'
+        check_output(generated_type, PROTOC_OUTPUT_DIR,
+                     'java_' + resource + '_type')
+
+    @pytest.mark.parametrize('oneof', ONEOFS_TO_GENERATE)
+    def test_resource_name_oneof_generation(self, run_protoc, oneof):
+        generated_oneof = casing_utils.lower_underscore_to_upper_camel(oneof)
+        check_output(generated_oneof, PROTOC_OUTPUT_DIR, 'java_' + oneof)
+
+    @pytest.mark.parametrize('message', MESSAGE_CLASSES_TO_EXTEND)
+    def test_get_set_insertion(self, run_protoc, message):
+        proto_class = casing_utils.lower_underscore_to_upper_camel(message)
+        check_output(proto_class, PROTOC_OUTPUT_DIR,
+                     'java_' + message + '_insert')
