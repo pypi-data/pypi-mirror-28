8760e000bfae74b1409586745c56ca9f0cae378a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:38 +0000

    Add package flaskify/0.1.2/flaskify-0.1.2-py2.py3-none-any.whl

---
+++ packages/flaskify/flaskify-0.1.2-py2.py3-none-any.whl/flaskify.py
@@ -0,0 +1,117 @@
+import click
+import os
+from json import load,dump
+from strings import *
+
+cfg = {
+	"FLASK_APP" : "run.py",
+	"VENV_PATH" : "venv/bin/",
+	"ROUTES"    : "app/routes.py"
+}
+
+@click.group()
+def cli():
+	pass
+
+@cli.command()
+def run():
+	if os.path.isfile(cfg['FLASK_APP']):
+		os.system(
+			'export FLASK_APP=%s\n%sflask run'
+			% (cfg['FLASK_APP'], cfg['VENV_PATH']))
+	else:
+		click.echo('Cannot find \'%s\'' % cfg['FLASK_APP'])
+
+@cli.group()
+def generate():
+	pass
+
+@generate.command()
+@click.option('--name', '-n',
+	          prompt='project folder name',
+			  help='Project folder name: No spaces')
+def project(name):
+	click.echo('Generating project...')
+	os.makedirs(name)
+	os.system('python3 -m venv %s/venv' % name)
+	os.system('%s/venv/bin/pip3 install wheel' % name)
+	os.system('%s/venv/bin/pip3 install flask' % name)
+	os.makedirs(name + '/app')
+	os.makedirs(name + '/app/templates')
+	os.makedirs(name + '/app/static')
+	os.makedirs(name + '/app/static/css')
+	os.makedirs(name + '/app/static/scss')
+	routes = open(name + "/app/routes.py", "w+")
+	config = open(name + "/config.py", "w+")
+	run = open(name + "/run.py", "w+")
+	init = open(name + "/app/__init__.py", "w+")
+	base_html = open(name + "/app/templates/base.html", "w+")
+	index_html = open(name + "/app/templates/index.html", "w+")
+	base_css = open(name + "/app/static/css/base.css", "w+")
+	index_css = open(name + "/app/static/css/index.css", "w+")
+
+
+	init.write(
+		"from flask import Flask\n\n"
+		"app = Flask(__name__)\n"
+		"app.config.from_object('config')\n"
+		"from app import routes"
+	)
+
+	run.write(
+		"#!venv/bin/python3\n\n"
+		"from app import app\n"
+		"app.run(debug=True)"
+	)
+	
+	config.write(
+		"SECRET_KEY = %s" % os.urandom(24)
+	)
+
+	routes.write(
+		"from app import app\n"
+		"from flask import render_template\n\n"
+		"@app.route('/')\n"
+		"@app.route('/index')\n"
+		"def index():\n"
+		"\treturn render_template('index.html')"
+	)
+
+	base_html.write(base_html_string)
+	index_html.write("{% extends 'base.html' %}")
+	base_css.write(base_css_string)
+
+	routes.close()
+	config.close()
+	run.close()
+	init.close()
+	base_html.close()
+	index_html.close()
+	base_css.close()
+	index_css.close()
+
+@generate.command()
+@click.option('--name', prompt='route name')
+@click.option('--template_name', prompt='html template name')
+def route(name, template_name):
+	string = "\n\n@app.route('/%s')\n" % name
+	string += "def %s():\n\treturn render_template('%s.html')\n" % (name,name)
+	if os.path.isfile(cfg['ROUTES']):
+		f = open(cfg['ROUTES'], 'a+')
+		html = open('app/templates/' + template_name + '.html', "w+")
+		css = open('app/static/css/' + template_name + '.css', "w+")
+		
+		f.write(string)
+		html.write(
+			"<head>\n"
+			"\t<title>Placeholder title</title>\n"
+			'\t<meta name="viewport" content="width=device-width, initial-scale=1.0">\n'
+			"\t<link href=\"{{ url_for(\'static\', filename=\'css/" + template_name + ".css\') }}\" rel=\"stylesheet\">\n"
+			"</head>"
+		)
+
+		f.close()
+		html.close()
+		css.close()
+	else:
+		click.echo('cannot find \'app/routes.py\'')
