701f357ebda8cf6c42f59ffcf50c877d8356f011
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:04 +0000

    Add package pdp/0.2/pdp-0.2.tar.gz

---
+++ packages/pdp/pdp-0.2.tar.gz/pdp-0.2/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+
+with open('README.rst') as file:
+    long_description = file.read()
+
+setup(
+    name='pdp',
+    version='v0.2',
+
+    description='Build fast data processing pipelines easily',
+    long_description=long_description,
+
+    url='https://github.com/Egor-Krivov/pdp',
+    download_url='https://github.com/Egor-Krivov/pdp/archive/v0.2.tar.gz',
+
+    author='Egor-Krivov',
+    author_email='e.a.krivov@gmail.com',
+    license='MIT',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+
+        'Intended Audience :: Developers',
+
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+
+    python_requires='>=3.5',
+    keywords='pipeline parallel thread data processing augmentation',
+
+    packages=find_packages(exclude=['examples', 'tests', 'prototypes']),
+    include_package_data=True,
+
+    install_requires=[]
+)
