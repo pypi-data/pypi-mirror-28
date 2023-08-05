0822b6f32fc403b4587edd657082adc2d9777bc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:46 +0000

    Add package swampyer/1.1/swampyer-1.1.tar.gz

---
+++ packages/swampyer/swampyer-1.1.tar.gz/swampyer-1.1/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+from setuptools import setup, find_packages
+
+setup(name='swampyer',
+      version='1.1',
+      description='Simple WAMP library with minimal external dependencies',
+      url = 'https://github.com/zabertech/python-swampyer',
+      download_url = 'https://github.com/zabertech/python-swampyer/archive/1.1.tar.gz',
+      author='Aki Mimoto',
+      author_email='aki+swampyer@zaber.com',
+      license='MIT',
+      packages=['swampyer'],
+      scripts=[],
+      test_suite='tests',
+      install_requires=[
+          'websocket-client',
+          'six',
+          'setuptools',
+      ],
+      dependency_links=[],
+      zip_safe=False)
+
