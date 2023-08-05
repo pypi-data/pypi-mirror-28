18f7838e5be43f4bcea0efba23aa0ade9be291c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:01 +0000

    Add package scikit-fem/0.1.2/scikit-fem-0.1.2.tar.gz

---
+++ packages/scikit-fem/scikit-fem-0.1.2.tar.gz/scikit-fem-0.1.2/skfem/__init__.py
@@ -0,0 +1,29 @@
+from skfem.mesh import *
+from skfem.assembly import *
+from skfem.element import *
+from skfem.utils import *
+
+__all__ = ['MeshTri',
+           'MeshTet',
+           'MeshQuad',
+           'MeshLine',
+           'MeshLineMortar',
+           'AssemblerLocalMortar',
+           'AssemblerLocal',
+           'AssemblerGlobal',
+           'ElementLocalTriP1',
+           'ElementLocalTetP1',
+           'ElementGlobalTriPp',
+           'ElementGlobalTriDG',
+           'ElementGlobalLineHermite',
+           'ElementGlobalLineP1',
+           'ElementGlobalArgyris',
+           'ElementGlobalMorley',
+           'ElementLocalTriDG',
+           'ElementLocalTetDG',
+           'ElementLocalQ1',
+           'ElementLocalH1Vec',
+           'ElementLocalQ2',
+           'direct',
+           'cg',
+           'build_ilu_pc']
