e12f3b5937a68f4984320b12866cf743298e2fbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:02 +0000

    Add package funniest_mawe/0.1/funniest_mawe-0.1.tar.gz

---
+++ packages/funniest_mawe/funniest_mawe-0.1.tar.gz/funniest_mawe-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name="funniest_mawe",
+      version="0.1",
+      description="Funny Python packaging test",
+      url="https://github.com/MaWeffm/funniest_mawe",
+      author="Martin Wegner",
+      author_email="ma.wegner@em.uni-frankfurt.de",
+      license="MIT???",
+      packages=["funniest_mawe"],
+      zip_safe=False)
