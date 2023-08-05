cdc91e3aad4cbc2bc2551a1c5007a01345932233
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:38 +0000

    Add package shijian/2018.1.9.1/shijian-2018.1.9.1.tar.gz

---
+++ packages/shijian/shijian-2018.1.9.1.tar.gz/shijian-2018.1.9.1/setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+
+import setuptools
+
+def main():
+
+    setuptools.setup(
+        name             = "shijian",
+        version          = "2018.01.09.0001",
+        description      = "change, time, file, list, statistics, language and other utilities",
+        long_description = long_description(),
+        url              = "https://github.com/wdbm/shijian",
+        author           = "Will Breaden Madden",
+        author_email     = "wbm@protonmail.ch",
+        license          = "GPLv3",
+        py_modules       = [
+                           "shijian"
+                           ],
+        install_requires = [
+                           "python-dateutil",
+                           "numpy",
+                           "pyprel",
+                           "scipy",
+                           "shijian"
+                           ],
+        entry_points     = """
+                           [console_scripts]
+                           shijian = shijian:shijian
+                           """
+    )
+
+def long_description(
+    filename = "README.md"
+    ):
+
+    if os.path.isfile(os.path.expandvars(filename)):
+        try:
+            import pypandoc
+            long_description = pypandoc.convert_file(filename, "rst")
+        except ImportError:
+            long_description = open(filename).read()
+    else:
+        long_description = ""
+    return long_description
+
+if __name__ == "__main__":
+    main()
