0a55419bf5c3e1daf5ff4aacea3381adfc810059
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:33 +0000

    Add package guillotina/2.3.11/guillotina-2.3.11-py3-none-any.whl

---
+++ packages/guillotina/guillotina-2.3.11-py3-none-any.whl/guillotina/__init__.py
@@ -0,0 +1,14 @@
+# load the patch before anything else.
+from guillotina import glogging
+from guillotina import patch  # noqa
+from guillotina._cache import BEHAVIOR_CACHE  # noqa
+from guillotina._cache import FACTORY_CACHE  # noqa
+from guillotina._cache import PERMISSIONS_CACHE  # noqa
+from guillotina._cache import SCHEMA_CACHE  # noqa
+from guillotina._settings import app_settings  # noqa
+from guillotina.i18n import default_message_factory as _  # noqa
+from zope.interface import Interface  # noqa
+
+
+# create logging
+logger = glogging.getLogger('guillotina')
