39e849ba0f5b17dec21501fe732db56c7e946faf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:14 +0000

    Add package grokcore.site/3.0.2/grokcore.site-3.0.2.tar.gz

---
+++ packages/grokcore.site/grokcore.site-3.0.2.tar.gz/grokcore.site-3.0.2/setup.py
@@ -0,0 +1,59 @@
+from setuptools import setup, find_packages
+import os
+
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+long_description = (
+    read('README.txt')
+    + '\n' +
+    read('CHANGES.txt')
+    )
+
+tests_require = [
+    'zope.app.appsetup',
+    'zope.component',
+    'zope.configuration',
+    'zope.testing',
+    'grokcore.content',
+    ]
+
+setup(
+    name='grokcore.site',
+    version='3.0.2',
+    author='Grok Team',
+    author_email='grok-dev@zope.org',
+    url='http://grok.zope.org',
+    download_url='http://pypi.python.org/pypi/grokcore.site',
+    description='Grok-like configuration for Zope local site and utilities',
+    long_description=long_description,
+    license='ZPL',
+    classifiers=['Environment :: Web Environment',
+                 'Intended Audience :: Developers',
+                 'License :: OSI Approved :: Zope Public License',
+                 'Programming Language :: Python',
+                 'Framework :: Zope3',
+                 ],
+
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    namespace_packages=['grokcore'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=['setuptools',
+                      'grokcore.component >= 2.1',
+                      'martian >= 0.13',
+                      'persistent',
+                      'zope.annotation',
+                      'zope.component',
+                      'zope.container',
+                      'zope.event',
+                      'zope.interface',
+                      'zope.lifecycleevent',
+                      'zope.schema',
+                      'zope.site',
+                      ],
+    tests_require=tests_require,
+    extras_require={'test': tests_require},
+)
