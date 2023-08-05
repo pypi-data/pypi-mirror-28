cfad7a254559891f90283d280b9892b93d43aba2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:59 +0000

    Add package pypowerworld/0.3.5/pypowerworld-0.3.5.tar.gz

---
+++ packages/pypowerworld/pypowerworld-0.3.5.tar.gz/pypowerworld-0.3.5/setup.py
@@ -0,0 +1,64 @@
+﻿#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Note: To use the 'upload' functionality of this file, you must:
+#   $ pip install twine
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+    
+# Package meta-data.
+NAME = 'pypowerworld'
+DESCRIPTION = 'Powerworld COM wrapper for Python'
+URL = 'https://github.com/jessemarkboyd/pypowerworld'
+EMAIL = 'jessemarkboyd@gmail.com'
+AUTHOR = 'Jesse Boyd'
+REQUIRED = [
+    'pandas','numpy','pypiwin32','pywin32'
+]
+
+
+# Load the package's __version__.py module as a dictionary.
+about = {}
+with open(path.join(here, NAME, '__version__.py')) as f:
+    exec(f.read(), about)
+
+# Where the magic happens:
+setup(
+    name=NAME,
+    version=about['__version__'],
+    description=DESCRIPTION,
+    long_description=long_description,
+    author=AUTHOR,
+    author_email=EMAIL,
+    url=URL,
+    packages=find_packages(exclude=('tests',)),
+    install_requires=REQUIRED,
+    include_package_data=True,
+    license='MIT',
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ],
+)
