6820ebcaab50836d1d6e89bd088389fcf80e674d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:37 +0000

    Add package python-ta/1.3.2/python-ta-1.3.2.tar.gz

---
+++ packages/python-ta/python-ta-1.3.2.tar.gz/python-ta-1.3.2/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup
+
+
+def readme():
+    try:
+        import pypandoc
+    except ImportError:
+        return ''
+
+    return pypandoc.convert('README.md', 'rst')
+
+setup(
+    name='python-ta',
+    version='1.3.2',
+    description='Code checking tool for teaching Python',
+    long_description=readme(),
+    url='http://github.com/pyta-uoft/pyta',
+    author='David Liu',
+    author_email='david@cs.toronto.edu',
+    license='MIT',
+    packages=['python_ta', 'python_ta.reporters', 'python_ta.checkers',
+              'python_ta.docstring', 'python_ta.patches', 'python_ta.parser',
+              'python_ta.transforms', 'python_ta.typecheck'],
+    install_requires=[
+        'astroid>=1.6',
+        'funcparserlib',
+        'hypothesis',
+        'pycodestyle',
+        'pylint>=1.8',
+        'nose',
+        'colorama',
+        'six',
+        'jinja2',
+        'pygments'
+    ],
+    include_package_data=True,
+    zip_safe=False)
