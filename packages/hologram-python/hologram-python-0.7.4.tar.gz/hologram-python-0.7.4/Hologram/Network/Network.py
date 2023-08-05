a0faa43374283faff08c2aeecf14515782364ebd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:08 +0000

    Add package hologram-python/0.7.4/hologram-python-0.7.4.tar.gz

---
+++ packages/hologram-python/hologram-python-0.7.4.tar.gz/hologram-python-0.7.4/Hologram/Network/Network.py
@@ -0,0 +1,48 @@
+# Network.py - Hologram Python SDK Network interface
+#
+# Author: Hologram <support@hologram.io>
+#
+# Copyright 2016 - Hologram (Konekt, Inc.)
+#
+#
+# LICENSE: Distributed under the terms of the MIT License
+#
+
+from Hologram.Event import Event
+import os
+import logging
+from logging import NullHandler
+
+class Network(object):
+
+    def __repr__(self):
+        return type(self).__name__
+
+    def __init__(self, event=Event()):
+        self.event = event
+        # Logging setup.
+        self.logger = logging.getLogger(__name__)
+        self.logger.addHandler(NullHandler())
+
+    def connect(self):
+        self.event.broadcast('network.connected')
+
+    def disconnect(self):
+        self.event.broadcast('network.disconnected')
+
+    def reconnect(self):
+        raise NotImplementedError('Must instantiate a defined Network type')
+
+    def getConnectionStatus(self):
+        raise NotImplementedError('Must instantiate a defined Network type')
+
+    def is_connected(self):
+        return False
+
+    @property
+    def interfaceName(self):
+        return self._interfaceName
+
+    @interfaceName.setter
+    def interfaceName(self, name):
+        self._interfaceName = name
---
+++ packages/hologram-python/hologram-python-0.7.4.tar.gz/hologram-python-0.7.4/scripts/hologram_spacebridge.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+# hologram_spacebridge.py - Hologram Python SDK command line interface (CLI) for
+#                           spacebridge interfaces
+#
+# Author: Hologram <support@hologram.io>
+#
+# Copyright 2016 - Hologram (Konekt, Inc.)
+#
+#
+# LICENSE: Distributed under the terms of the MIT License
+
+
+from scripts.hologram_receive import run_hologram_receive
+from scripts.hologram_receive import parse_hologram_receive_args
+
+def parse_hologram_spacebridge_args(parser):
+    parse_hologram_receive_args(parser)
+    parser.set_defaults(command_selected='spacebridge')
+
+def run_hologram_spacebridge(args):
+    args['command_selected'] = 'receive_data'
+    run_hologram_receive(args)
