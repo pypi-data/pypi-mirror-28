00c6058dcc521167a767031114dee56b03e1c5cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:25 +0000

    Add package xy-trans/2.0.0/xy_trans-2.0.0.tar.gz

---
+++ packages/xy-trans/xy_trans-2.0.0.tar.gz/xy_trans-2.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+  name = 'xy_trans',
+  packages = ['xy_trans'],
+  version = '2.0.0',
+  description = 'translate dictionary',
+  long_description='translate dictionary zh to en ,en to zh',
+  author = 'KingHS',
+  author_email = '382771946@qq.com',
+  keywords = ['pip3', 'trans', 'python3','python','translate'],
+  license='MIT',
+)
