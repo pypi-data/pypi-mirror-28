585734d139b6267e779d5b4f8933fe32cc75a63f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:38 +0000

    Add package xy-qq/1.0.0/xy_qq-1.0.0.tar.gz

---
+++ packages/xy-qq/xy_qq-1.0.0.tar.gz/xy_qq-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+  name = 'xy_qq',
+  packages = ['xy_qq'],
+  version = '1.0.0',
+  description = 'Get The QQ Luck',
+  long_description='Get The QQ Luck From JiSu API',
+  author = 'KingHS',
+  author_email = '382771946@qq.com',
+  keywords = ['pip3', 'qqluck', 'python3','python','qq'],
+  license='MIT',
+)
---
+++ packages/xy-qq/xy_qq-1.0.0.tar.gz/xy_qq-1.0.0/xy_qq/xy_qq.py
@@ -0,0 +1,32 @@
+import urllib.request
+import urllib.parse
+import json
+APPKEY='631c5a5b9992bd74'
+APPSECRET='eS0iNTaMdI5mgRew8JkrVqUB7mG1MytS'
+API_URL='http://api.jisuapi.com/qqluck/query'
+
+'''qq号码测试'''
+def luck(qq=''):
+    if qq == '':
+        return -1
+    data = {}
+    data['appkey'] = APPKEY
+    data['qq'] = qq
+    url_values = urllib.parse.urlencode(data)
+    url = API_URL + '?' + url_values
+    result = urllib.request.urlopen(url)
+    jsonarr = json.loads(result.read())
+    res = jsonarr['result']
+    if res:
+        res_dict = {}
+        res_dict['qq']=res['qq']
+        res_dict['luck']=res['luck']
+        res_dict['socre']=res['score']
+        res_dict['content']=res['content']
+        res_dict['character']=res['character']
+        res_dict['detail']=res['characterdetail']
+        return res_dict
+    else:
+        return -1
+
+print(luck('382771946'))
