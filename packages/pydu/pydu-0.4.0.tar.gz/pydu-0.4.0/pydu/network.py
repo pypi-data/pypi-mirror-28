3ce651ce7555eb80152ca867c8c246a6e032fe7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:38 +0000

    Add package pydu/0.4.0/pydu-0.4.0.tar.gz

---
+++ packages/pydu/pydu-0.4.0.tar.gz/pydu-0.4.0/pydu/network.py
@@ -0,0 +1,71 @@
+import socket
+import struct
+import ctypes
+from pydu.platform import WINDOWS
+from pydu.string import safeencode
+
+
+# https://github.com/hickeroar/win_inet_pton/blob/master/win_inet_pton.py
+class _sockaddr(ctypes.Structure):
+    _fields_ = [("sa_family", ctypes.c_short),
+                ("__pad1", ctypes.c_ushort),
+                ("ipv4_addr", ctypes.c_byte * 4),
+                ("ipv6_addr", ctypes.c_byte * 16),
+                ("__pad2", ctypes.c_ulong)]
+
+
+# https://github.com/kennethreitz/requests/blob/master/requests/utils.py
+def dotted_netmask(mask):
+    """
+    Converts mask from /xx format to xxx.xxx.xxx.xxx
+    Example: if mask is 24 function returns 255.255.255.0
+    """
+    mask = int(mask)
+    bits = 0xffffffff ^ (1 << 32 - mask) - 1
+    return socket.inet_ntoa(struct.pack('>I', bits))
+
+
+# https://github.com/kennethreitz/requests/blob/master/requests/utils.py
+def is_ipv4(ip):
+    """
+    Returns True if the IPv4 address ia valid, otherwise returns False.
+    """
+    try:
+        socket.inet_aton(ip)
+    except socket.error:
+        return False
+    return True
+
+
+def is_ipv6(ip):
+    """
+    Returns True if the IPv6 address ia valid, otherwise returns False.
+    """
+    if WINDOWS:
+        ip = safeencode(ip)
+        addr = _sockaddr()
+        addr_size = ctypes.c_int(ctypes.sizeof(addr))
+
+        if ctypes.windll.ws2_32.WSAStringToAddressA(
+            ip,
+            socket.AF_INET6,
+            None,
+            ctypes.byref(addr),
+            ctypes.byref(addr_size)
+        ) != 0:
+            return False
+        return True
+    else:
+        try:
+            socket.inet_pton(socket.AF_INET6, ip)
+        except socket.error:
+            return False
+        return True
+
+
+def get_free_port():
+    s = socket.socket(socket.AF_INET, type=socket.SOCK_STREAM)
+    s.bind(('127.0.0.1', 0))
+    _, port = s.getsockname()
+    s.close()
+    return port
---
+++ packages/pydu/pydu-0.4.0.tar.gz/pydu-0.4.0/tests/test_network.py
@@ -0,0 +1,38 @@
+import pytest
+from pydu.network import dotted_netmask, is_ipv4, is_ipv6, get_free_port
+
+
+@pytest.mark.parametrize(
+    'mask, expected', (
+        (8, '255.0.0.0'),
+        (24, '255.255.255.0'),
+        (25, '255.255.255.128'),
+    ))
+def test_dotted_netmask(mask, expected):
+    assert dotted_netmask(mask) == expected
+
+
+class TestIsIPv4Address:
+
+    def test_valid(self):
+        assert is_ipv4('8.8.8.8')
+
+    @pytest.mark.parametrize('value', ('8.8.8.8.8', 'localhost.localdomain'))
+    def test_invalid(self, value):
+        assert not is_ipv4(value)
+
+
+class TestIsIPv6Address:
+
+    def test_valid(self):
+        assert is_ipv6('fe80::9e5b:b149:e187:1a18')
+
+    @pytest.mark.parametrize('value', ('fe80::9e5b:b149:e187::', 'localhost.localdomain'))
+    def test_invalid(self, value):
+        assert not is_ipv6(value)
+
+
+def test_get_free_port():
+    port = get_free_port()
+    assert isinstance(port, int)
+    assert 65536 > port > 0
