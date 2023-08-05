599ff86458ab3573120fcbacf275ef61091d7f2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:02 +0000

    Add package django-request-logging/0.5.5/django-request-logging-0.5.5.tar.gz

---
+++ packages/django-request-logging/django-request-logging-0.5.5.tar.gz/django-request-logging-0.5.5/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='django-request-logging',
+      version='0.5.5',
+      description='Django middleware that logs http request body.',
+      url='https://github.com/Rhumbix/django-request-logging.git',
+      author='Kenneth Jiang',
+      author_email='kenneth@rhumbix.com',
+      license='MIT',
+      packages=['request_logging'],
+      install_requires=[
+          'Django',
+      ],
+      zip_safe=False)
