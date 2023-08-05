56e98560f0a9839adae08245b099a80eb990824d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:04 +0000

    Add package ioamdoit/0.0.2/ioamdoit-0.0.2.tar.gz

---
+++ packages/ioamdoit/ioamdoit-0.0.2.tar.gz/ioamdoit-0.0.2/setup.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python
+
+from setuptools import setup
+
+import versioneer
+
+setup(name = 'ioamdoit',
+      description = 'common ioam doit tasks',
+      version = versioneer.get_version(),
+      cmdclass = versioneer.get_cmdclass(),
+      license = 'MIT',
+      url = 'http://github.com/ioam/ioamdoit',
+      py_modules=['ioamdoit'],
+      )
