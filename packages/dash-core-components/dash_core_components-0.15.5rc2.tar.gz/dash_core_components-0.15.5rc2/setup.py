273a3894835b731ebfadb8c23cd3c2d3f57458af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:12 +0000

    Add package dash-core-components/0.15.5rc2/dash_core_components-0.15.5rc2.tar.gz

---
+++ packages/dash-core-components/dash_core_components-0.15.5rc2.tar.gz/dash_core_components-0.15.5rc2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+exec(open('dash_core_components/version.py').read())
+
+setup(
+    name='dash_core_components',
+    version=__version__,
+    author='Chris Parmer',
+    author_email='chris@plot.ly',
+    packages=['dash_core_components'],
+    include_package_data=True,
+    license='MIT',
+    description='Dash UI core component suite',
+    install_requires=['dash']
+)
