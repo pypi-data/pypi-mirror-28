9775c434a32caf35317ecc1c0a81e4cb05d42460
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:00 +0000

    Add package lylogger/0.0.2/lylogger-0.0.2.tar.gz

---
+++ packages/lylogger/lylogger-0.0.2.tar.gz/lylogger-0.0.2/lylogger/__init__.py
@@ -0,0 +1,1 @@
+from lylogger.core import tolog
---
+++ packages/lylogger/lylogger-0.0.2.tar.gz/lylogger-0.0.2/lylogger/core.py
@@ -0,0 +1,36 @@
+import logging
+from logging.handlers import RotatingFileHandler
+
+class Lylogger:
+    def __init__(self,file_path=None,silent=False,mode='w',
+                 setlevel='debug',formatter=None,rotate=False,maxsize=50,maxcount=10):
+        if formatter ==None:
+            formatter = logging.Formatter('[Time:%(asctime)s][File:%(filename)s][Function: %(funcName)s][Line:%(lineno)d][%(levelname)s]::%(message)s')
+        self.pprint = logging.getLogger('')
+        __level =  {'debug':logging.DEBUG,'info':logging.INFO,'warning':logging.WARNING,'critical':logging.CRITICAL}
+        self.pprint.setLevel(__level[setlevel])
+        if silent == False:
+            console = logging.StreamHandler()
+            console.setFormatter(formatter)
+            self.pprint.addHandler(console)
+
+        if file_path != None and rotate is False:
+            file = logging.FileHandler(file_path, mode=mode, encoding='utf8')
+            file.setFormatter(formatter)
+            self.pprint.addHandler(file)
+        elif file_path != None and rotate is True:
+            Rthandler = RotatingFileHandler('myapp.log', maxBytes=maxsize * 1024 * 1024, backupCount=maxcount)
+            Rthandler.setFormatter(formatter)
+            self.pprint.addHandler(Rthandler)
+
+
+def tolog(file_path=None,silent=False,mode='w',setlevel='debug',formatter=None,maxsize=50,maxcount=10):
+    return Lylogger(file_path=file_path,
+                    silent=silent,
+                    mode=mode,
+                    setlevel=setlevel,
+                    formatter=formatter,
+                    maxcount=maxcount,
+                    maxsize=maxsize)
+
+
