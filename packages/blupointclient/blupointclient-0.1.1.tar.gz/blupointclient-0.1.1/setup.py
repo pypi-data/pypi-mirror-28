69cb492ae41b8a5a9aaaf03e01e76d17cf7d58ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:00 +0000

    Add package blupointclient/0.1.1/blupointclient-0.1.1.tar.gz

---
+++ packages/blupointclient/blupointclient-0.1.1.tar.gz/blupointclient-0.1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='blupointclient',  # This is the name of your PyPI-package.
+    version='0.1.001',  # Update the version number for new releases
+    description='BluPoint Client',
+    py_modules=['demo']
+    #scripts=['demo']  # The name of your scipt, and also the command you'll be using for calling it
+)
