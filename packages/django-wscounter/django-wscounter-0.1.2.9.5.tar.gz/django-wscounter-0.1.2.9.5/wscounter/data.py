dce23cc067825869a3d3f7c580cbcd52dec2240a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:20 +0000

    Add package django-wscounter/0.1.2.9.5/django-wscounter-0.1.2.9.5.tar.gz

---
+++ packages/django-wscounter/django-wscounter-0.1.2.9.5.tar.gz/django-wscounter-0.1.2.9.5/wscounter/data.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+
+
+def get_from_to(model, date_from, date_to):
+    return model.objects.filter(created__range=[date_from, date_to]).count()
+
+
+def get(model):
+    return model.objects.all().count()
+
+
+def is_date_valid(date_text):
+    try:
+        datetime.strptime(date_text, "%Y/%m/%d")
+        return True
+    except ValueError:
+        return False
+
+
+def get_date_from_string(date_text):
+    return datetime.strptime(date_text, "%Y/%m/%d")
