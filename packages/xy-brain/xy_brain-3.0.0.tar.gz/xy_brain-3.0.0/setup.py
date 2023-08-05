c23b21c558cff4c3e1e668d816a4258009f1112e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:51 +0000

    Add package xy-brain/3.0.0/xy_brain-3.0.0.tar.gz

---
+++ packages/xy-brain/xy_brain-3.0.0.tar.gz/xy_brain-3.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+  name = 'xy_brain',
+  packages = ['xy_brain'],
+  version = '3.0.0',
+  description = 'Get The Brain Test',
+  long_description='Get The brain test',
+  author = 'KingHS',
+  author_email = '382771946@qq.com',
+  keywords = ['pip3', 'python3','python','brain'],
+  license='MIT',
+)
