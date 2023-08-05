135744dc43afeb125ca6f7a21e733616ecf7e776
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:14 +0000

    Add package nester-UeRS/1.1.1/nester__UeRS-1.1.1.tar.gz

---
+++ packages/nester-UeRS/nester__UeRS-1.1.1.tar.gz/nester__UeRS-1.1.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+  name = 'nester__UeRS',
+  version = '1.1.1',
+  py_modules = ['nester__UeRS'],
+  author = 'uendelrocha',
+  author_email = 'uendelrocha@gmail.com',
+  url = 'https://www.facebook.com/uendelrocha',
+  description = 'Uma simples impressora de listas aninhadas',
+  )
