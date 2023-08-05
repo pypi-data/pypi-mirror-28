546145c4d8410ebe8eda02597524374054894944
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:56 +0000

    Add package ckandr/0.1/ckandr-0.1.tar.gz

---
+++ packages/ckandr/ckandr-0.1.tar.gz/ckandr-0.1/ckandr/koinex.py
@@ -0,0 +1,35 @@
+import os
+import requests
+from terminaltables import DoubleTable
+
+class Koinex(object):
+    """Main Koinex Class"""
+
+    def __init__(self):
+        self.URL = "https://koinex.in/api/ticker"
+        self.title = "\033[94mKoinex CryptoCurrency Rates\033[0m"
+        self.cryptos = {
+            "BTC": "BitCoin",
+            "ETH": "Ethereum",
+            "XRP": "Ripple",
+            "BCH": "Bitcoin Cash",
+            "LTC": "LiteCoin"
+        }
+
+
+    def get_koinex_rates(self):
+        os.system('clear')
+        try:
+            response = requests.get(self.URL)
+        except Exception as e:
+            print(type(e).__name__)
+        if response and response.status_code == 200:
+            koinex_data = response.json()
+            koinex_list = []
+            koinex_list.append(['CryptoCurrency Name', 'Symbol', 'Price'] )
+            for curr in self.cryptos:
+                koinex_list.append([self.cryptos[curr], curr, koinex_data['prices'][curr]])
+            table = DoubleTable(koinex_list)
+            table.title = self.title
+            table.inner_row_border = True
+            print (table.table)
