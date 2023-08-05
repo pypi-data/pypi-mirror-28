eb114040cb11e572bb4a6a3efcfe704706de5912
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:46 +0000

    Add package ref/0.0.2.2/ref-0.0.2.2.tar.gz

---
+++ packages/ref/ref-0.0.2.2.tar.gz/ref-0.0.2.2/setup.py
@@ -0,0 +1,40 @@
+#DIST:distutils,setup.py 
+
+#!/usr/bin/env python
+
+#modules = map(__import__, moduleNames)
+
+
+
+from distutils.core import setup
+
+import json;
+
+
+
+settings = json.loads(open("config.json").read()) 
+
+
+
+
+package_name = str(settings["package"])
+
+setup(
+
+    name=package_name,
+
+    version="0.0.2.2",
+
+    author="thinkersilver",
+
+    author_email="myonlineprofile2012@gmail.com",
+
+    packages=[package_name],
+
+    scripts=['bin/%s' % package_name],
+
+    package_data={'':["./config.json"]},
+
+    description="References library"
+
+) 
