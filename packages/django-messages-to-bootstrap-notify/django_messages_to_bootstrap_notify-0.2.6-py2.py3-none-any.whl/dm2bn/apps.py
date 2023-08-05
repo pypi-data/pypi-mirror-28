1873bf5eeca35bf6a6296bd0c5ce39fded7d6240
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:32 +0000

    Add package django-messages-to-bootstrap-notify/0.2.6/django_messages_to_bootstrap_notify-0.2.6-py2.py3-none-any.whl

---
+++ packages/django-messages-to-bootstrap-notify/django_messages_to_bootstrap_notify-0.2.6-py2.py3-none-any.whl/dm2bn/apps.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+# django-messages-to-bootstrap-notify
+# dm2bn/apps.py
+
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+__all__ = [
+    "Config",
+]
+
+
+class Config(AppConfig):
+
+    name = "dm2bn"
+    verbose_name = "Django messages to bootstrap-notify"
