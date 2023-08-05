5a23f3024e001fccca30151a2f31713f52231748
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:40 +0000

    Add package wr-attrs/3.0.0/wr-attrs-3.0.0.tar.gz

---
+++ packages/wr-attrs/wr-attrs-3.0.0.tar.gz/wr-attrs-3.0.0/wr_attrs/__init__.py
@@ -0,0 +1,13 @@
+__version__ = '3.0.0'
+
+
+from .attrs2 import Attr, Attrs, BoundAttr, NotSet, Required, container
+
+__all__ = [
+    'Attr',
+    'Attrs',
+    'BoundAttr',
+    'NotSet',
+    'Required',
+    'container',
+]
