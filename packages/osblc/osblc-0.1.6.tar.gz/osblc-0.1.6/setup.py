20dc7c0803dcb980c611d7ce557f5e392008e085
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:16 +0000

    Add package osblc/0.1.6/osblc-0.1.6.tar.gz

---
+++ packages/osblc/osblc-0.1.6.tar.gz/osblc-0.1.6/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+__version__ = '0.1.6'
+
+setup(
+    name='osblc',
+    version=__version__,
+    description='On s\'en bat les couilles !',
+    author='Noel Martignoni',
+    author_email='noel@martignoni.fr',
+    url='https://gitlab.com/nmartignoni/osblc',
+    scripts=['osblc'],
+    install_requires=['pygame', 'termcolor'],
+    include_package_data=True,
+    packages=find_packages(exclude=['tests*']),
+)
