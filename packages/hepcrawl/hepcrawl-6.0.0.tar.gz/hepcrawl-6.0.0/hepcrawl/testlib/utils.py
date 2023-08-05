95fc730e1326bbbe7294ec9e8a5950f4361efa15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:52 +0000

    Add package hepcrawl/6.0.0/hepcrawl-6.0.0.tar.gz

---
+++ packages/hepcrawl/hepcrawl-6.0.0.tar.gz/hepcrawl-6.0.0/hepcrawl/testlib/utils.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of hepcrawl.
+# Copyright (C) 2017 CERN.
+#
+# hepcrawl is a free software; you can redistribute it and/or modify it
+# under the terms of the Revised BSD License; see LICENSE file for
+# more details.
+
+from __future__ import absolute_import, division, print_function
+
+from scrapyd_api import ScrapydAPI
+
+
+def get_crawler_instance(crawler_host, *args, **kwargs):
+    """Get current crawler instance.
+
+    Args:
+        crawler_host(str): the crawler's host name.
+
+    Returns:
+        ScrapydAPI: current crawler instance.
+    """
+    return ScrapydAPI(
+        crawler_host,
+        *args,
+        **kwargs
+    )
+
+
+def deep_sort(element):
+    """Dummy deep-sort json dicts (lists, dicts, stirngs, bools and integers).
+    """
+    if isinstance(element, str):
+        return element
+
+    if isinstance(element, dict):
+        for key, value in element.items():
+            element[key] = deep_sort(value)
+
+        return element
+
+    if isinstance(element, list):
+        new_element = []
+        for item in sorted(element):
+            new_element.append(deep_sort(item))
+
+        return new_element
+
+    return element
