3657cbe421a4c05367f985781f5ea4140b9fbecd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:23 +0000

    Add package wellcometohello/1.1.0/wellcometohello-1.1.0.zip

---
+++ packages/wellcometohello/wellcometohello-1.1.0.zip/wellcometohello-1.1.0/setup.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(
+    name = 'wellcometohello',      
+    version = '1.1.0',
+    py_modules = ['d3'],
+    author = 'windfly357',        
+    author_email = '584784844@qq.com',
+    url = 'https://upload.pypi.org/legacy/',
+    description = 'hello world'   
+    )
