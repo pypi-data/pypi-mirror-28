dba038fa50bc73a8813580f54d68a5bd09af6316
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:24 +0000

    Add package nhl-api/0.0.1/nhl_api-0.0.1.tar.gz

---
+++ packages/nhl-api/nhl_api-0.0.1.tar.gz/nhl_api-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='nhl_api',
+    packages=['nhl_api'],  # this must be the same as the name above
+    version='0.0.1',
+    description='An api wrapper for the nhl stats api',
+    author='Joe Williams',
+    author_email='joewill@gmail.com',
+    url='https://github.com/joewill/nhl_api',
+    download_url='https://github.com/peterldowns/mypackage/archive/0.1.tar.gz',
+    keywords=['nhl api', 'nhl', 'hockey'],  # arbitrary keywords
+    classifiers=[],
+)
