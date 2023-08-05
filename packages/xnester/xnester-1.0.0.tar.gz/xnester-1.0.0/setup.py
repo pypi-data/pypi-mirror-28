108b9b5bbde22f042cc76b9124b0abe237e86767
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:19 +0000

    Add package xnester/1.0.0/xnester-1.0.0.tar.gz

---
+++ packages/xnester/xnester-1.0.0.tar.gz/xnester-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name='xnester',
+    version='1.0.0',
+    py_modules=['xnester'],
+    author='xrypython',
+    author_email='xingruyi68@163.com',
+    url='http://www.hebeu.edu.cn',
+    description='A simple printer of xnested lists',
+    )
