18ba2ded45313c2fde5a81e4c00e5f45c59c7a2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:32 +0000

    Add package samp-server-cli/0.1.7/samp-server-cli-0.1.7.tar.gz

---
+++ packages/samp-server-cli/samp-server-cli-0.1.7.tar.gz/samp-server-cli-0.1.7/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+  name='samp-server-cli',
+  version='0.1.7',
+  author='Zeex',
+  author_email='zeex@rocketmail.com',
+  url='https://github.com/Zeex/samp-server-cli',
+  description='A command line interface for SA-MP server',
+  license='BSD',
+  py_modules = ['samp_server_cli'],
+  entry_points = {
+    'console_scripts': ['samp-server-cli=samp_server_cli:main']
+  },
+)
