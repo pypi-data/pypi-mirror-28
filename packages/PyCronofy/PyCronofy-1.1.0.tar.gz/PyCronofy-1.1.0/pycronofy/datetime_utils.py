9045bdb292a7e5dfff03ce06636547172b4e5f06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:53 +0000

    Add package PyCronofy/1.1.0/PyCronofy-1.1.0.tar.gz

---
+++ packages/PyCronofy/PyCronofy-1.1.0.tar.gz/PyCronofy-1.1.0/pycronofy/datetime_utils.py
@@ -0,0 +1,42 @@
+import datetime
+import pytz
+
+from pycronofy.exceptions import PyCronofyDateTimeError
+
+ISO_8601_DATE_FORMAT = '%Y-%m-%d'
+ISO_8601_DATETIME_FORMAT = '%Y-%m-%dT%H:%M:%SZ'  # UTC
+
+
+def get_iso8601_string(date_time):
+    """
+        Accepts either an ISO 8601 string OR a datetime object.
+        Must be in UTC format:
+
+        2016-01-31T12:33:00Z
+        2016-01-31T12:33:00UTC
+        2016-01-31T12:33:00+00:00
+
+        https://en.wikipedia.org/wiki/ISO_8601
+
+        :param datetime.datetime date_time: ``datetime.datetime`` or ``string``.
+        :return: ISO 8601 formatted datetime string.
+        :rtype: ``string``
+    """
+    if not date_time:
+        # Return None if passed None
+        return date_time
+    date_time_type = type(date_time)
+    if date_time_type in (type(''), type(u'')):
+        # If passed a string, return the string.
+        return date_time
+    elif date_time_type == datetime.date:
+        # If passed a date, return an iso8601 formatted date string.
+        return date_time.strftime(ISO_8601_DATE_FORMAT)
+    elif date_time_type != datetime.datetime:
+        # If passed anything other than a datetime, date, string, or None, raise an Exception.
+        error_message = 'Unsupported type: ``%s``.\nSupported types: ``<datetime.datetime>``, ``<datetime.date>``, or ``<str>``.'
+        raise PyCronofyDateTimeError(
+            error_message % (repr(type(date_time))), date_time)
+    if date_time.tzinfo and date_time.tzinfo != pytz.utc:
+        date_time = date_time.astimezone(pytz.utc)
+    return date_time.strftime(ISO_8601_DATETIME_FORMAT)
---
+++ packages/PyCronofy/PyCronofy-1.1.0.tar.gz/PyCronofy-1.1.0/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from distutils.core import setup
+setup(
+    name='PyCronofy',
+    version='1.1.0',
+    description='Python library for Cronofy',
+    author='VenueBook',
+    author_email='dev@venuebook.com',
+    url='https://github.com/cronofy/pycronofy',
+    packages=['pycronofy']
+)
