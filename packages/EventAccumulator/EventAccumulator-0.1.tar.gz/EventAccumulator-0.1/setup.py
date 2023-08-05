9eab3eae6a3bc0994ad461882e3d870c28b1161a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:53 +0000

    Add package EventAccumulator/0.1/EventAccumulator-0.1.tar.gz

---
+++ packages/EventAccumulator/EventAccumulator-0.1.tar.gz/EventAccumulator-0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='EventAccumulator',
+    url='https://bitbucket.org/ottokruse/event-accumulator',
+    author='Otto Kruse',
+    author_email='ottokruse@gmail.com',
+    version='0.1',
+    packages=['accumulator',],
+    license='MIT',
+    long_description=open('README.md').read(),
+)
