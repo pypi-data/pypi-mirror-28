088226d98b4076cf687eb89f621ffe65b4e16291
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:18 +0000

    Add package nimbus-scrapy/0.3.3/nimbus_scrapy-0.3.3-py2.py3-none-any.whl

---
+++ packages/nimbus-scrapy/nimbus_scrapy-0.3.3-py2.py3-none-any.whl/nimbus_scrapy/spiders/mixin.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+import os
+import sys
+import json
+import datetime
+import time
+import urlparse
+import random
+import platform
+import copy
+import scrapy
+from scrapy import signals
+from scrapy.spiders import Spider, CrawlSpider, CSVFeedSpider, SitemapSpider, XMLFeedSpider
+from scrapy.spiders import Rule, BaseSpider
+from scrapy.exceptions import DropItem
+from scrapy.exceptions import NotConfigured, IgnoreRequest
+from scrapy.http.request.form import FormRequest, Request
+from scrapy.http.response.html import HtmlResponse
+from scrapy.selector import Selector
+from scrapy.utils.response import get_base_url
+
+__all__ = ["BaseMixin", ]
+
+
+class BaseMixin(object):
+    config = None
+    spider_id = None
+    spider_config = None
+
+    def setup(self, config=None):
+        if config is None:
+            self.config = None
+        elif isinstance(config, basestring):
+            self.config = json.loads(config)
+        elif isinstance(config, dict):
+            self.config = copy.deepcopy(config)
+        else:
+            raise NotImplementedError
+        if self.config is None:
+            return
+        self.spider_id = self.config.pop("id", None) or self.config.pop("spider_id", "default")
+        for key, value in self.config.iteritems():
+            prifunc_key = u"_setup_{}".format(key)
+            pubfunc_key = u"setup_{}".format(key)
+            func = getattr(self, prifunc_key, None) or getattr(self, pubfunc_key, None)
+            if func and callable(func):
+                v = func(self.spider_id, value)
+                setattr(self, key, v)
+            elif value:
+                setattr(self, key, value)
+
+    def _setup_db(self, spider_id=None, config=None):
+        from ..data import dbclient
+        if not isinstance(config, dict):
+            raise ValueError(u"ERROR db:{}".format(config))
+        dbclient.init(name=spider_id, **config)
+        return dbclient.get_session(name=spider_id)
+
+    def _setup_redis(self, spider_id=None, config=None):
+        from ..data import redisclient
+        if not isinstance(config, dict):
+            raise ValueError(u"ERROR redis:{}".format(config))
+        connection = redisclient.init(name=spider_id, **config)
+        return connection
+
+    def _setup_spider(self, spider_id=None, config=None):
+        if not isinstance(config, dict):
+            raise ValueError(u"ERROR spider:{}".format(config))
+        self.spider_config = copy.deepcopy(config)
+        return self.spider_config
+
+    def _setup_spider_config(self, spider_id=None, config=None):
+        if not isinstance(config, dict):
+            raise ValueError(u"ERROR spider_config:{}".format(config))
+        self.spider_config = copy.deepcopy(config)
+        return self.spider_config
+
+    def _setup_proxy(self, spider_id=None, config=None):
+        if config is None:
+            raise ValueError(u"ERROR proxy:{}".format(config))
+        return copy.deepcopy(config)
+
+    def get_db(self):
+        return getattr(self, "db", None)
+
+    def get_redis(self):
+        return getattr(self, "redis", None)
+
+    def get_proxy(self):
+        return getattr(self, "proxy", None)
+
+    def process_item(self, item, spider, **kwargs):
+        raise NotImplementedError
+
+
