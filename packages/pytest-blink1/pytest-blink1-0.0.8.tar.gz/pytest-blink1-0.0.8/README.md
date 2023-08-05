be6b619f653eec1cb84e149faa815d6c85285ee3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:45 +0000

    Add package pytest-blink1/0.0.8/pytest-blink1-0.0.8.tar.gz

---
+++ packages/pytest-blink1/pytest-blink1-0.0.8.tar.gz/pytest-blink1-0.0.8/README.md
@@ -0,0 +1,21 @@
+# pytest-blink1
+
+![Build Status](https://secure.travis-ci.org/vmalloe/pytest-blink1.png?branch=master )
+
+A pytest plugin to emit notifications via the [Blink(1) RGB LED](https://blink1.thingm.com/)
+
+
+## Installation
+
+You can install "pytest-blink1" via pip from PyPI:
+
+```
+$ pip install pytest-blink1
+```
+
+After this, make sure you're running the API server via the blink1control utility.
+
+
+## License
+
+Distributed under the terms of the MIT license, "pytest-blink1" is free and open source software
---
+++ packages/pytest-blink1/pytest-blink1-0.0.8.tar.gz/pytest-blink1-0.0.8/pytest_blink1/__init__.py
@@ -0,0 +1,27 @@
+from urllib.request import urlopen, URLError
+
+def pytest_terminal_summary(terminalreporter, exitstatus=None): # pylint: disable=unused-argument
+    _add_patterns()
+
+    tr = terminalreporter
+    passes = len(tr.stats.get('passed', []))
+    fails = len(tr.stats.get('failed', []))
+    skips = len(tr.stats.get('deselected', []))
+    errors = len(tr.stats.get('error', []))
+    if (passes or skips) and not (fails or errors):
+        _pattern('pytest-success')
+    else:
+        _pattern('pytest-failure')
+
+def _add_patterns():
+    _blink('pattern/add?pname=pytest-success&pattern=3,%2300ff00,1.5,%23000000,0.5')
+    _blink('pattern/add?pname=pytest-failure&pattern=1,%23ff0000,0.3,1,%23ff0000,0.3,2,%23000000,0.1,0,%23ff0000,0.3,2,%23ff0000,0.3,1,%23000000,0.1,0')
+
+def _pattern(name):
+    _blink('pattern/play?pname={}'.format(name))
+
+def _blink(command):
+    try:
+        urlopen('http://localhost:8934/blink1/{}'.format(command))
+    except URLError as e:
+        pass
