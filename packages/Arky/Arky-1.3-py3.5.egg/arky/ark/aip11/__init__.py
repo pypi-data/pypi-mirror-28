f9b4bd2e90dc89df49efa40f3fa04bb67787bb82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:05 +0000

    Add package Arky/1.3/Arky-1.3-py3.5.egg

---
+++ packages/Arky/Arky-1.3-py3.5.egg/arky/ark/aip11/__init__.py
@@ -0,0 +1,199 @@
+# -*- encoding: utf8 -*-
+# © Toons
+
+from .. import __PY3__
+from .. import crypto
+from .. import slots
+from .. import init
+from .. import rest
+from .. import cfg
+
+import struct
+
+
+C = 0.0001*100000000
+rest.POST.createEndpoint(rest.POST, rest.post, "/peer/transactions/v1")
+
+
+class Payload:
+	
+	@staticmethod
+	def setArkPerByteFees(value):
+		global C
+		C = value
+
+	@staticmethod
+	def get(typ, **kw):
+		return crypto.hexlify(getattr(Payload, "type%d"%typ)(**kw))
+
+	@staticmethod
+	def type0(**kw):
+		try:
+			recipientId = crypto.base58.b58decode_check(kw["recipientId"])
+		except:
+			raise Exception("no recipientId defined")
+		return struct.pack("<QI21s" if __PY3__ else ("<QI"+21*"c"),
+			kw.get("amount", 0),
+			kw.get("expiration", 0),
+			recipientId
+		)
+
+	@staticmethod
+	def type1(**kw):
+		if "secondSecret" in kw:
+			secondPublicKey = crypto.getKeys(kw["secondSecret"])["publicKey"]
+		elif "secondPublicKey" in kw:
+			secondPublicKey = kw["secondPublicKey"]
+		else:
+			raise Exception("no secondSecret or secondPublicKey given")
+		return struct.pack("<33s", crypto.unhexlify(secondPublicKey)) if __PY3__ else \
+	           struct.pack(33*"c", secondPublicKey)
+
+	@staticmethod
+	def type2(**kw):
+		username = kw.get("username", False)
+		if username:
+			length = len(username)
+			if 3 <= length <= 255:
+				return struct.pack("<B%ds"%length, length, username.encode()) if __PY3__ else \
+				       struct.pack("<B" + length*"c", length, username)
+			else:
+				raise Exception("bad username length [3-255]: %s" % username)
+		else:
+			raise Exception("no username defined")
+
+	@staticmethod
+	def type3(**kw):
+		pass
+
+
+def getHeaders(**kw):
+	if "secret" in kw:
+		publicKey = crypto.getKeys(kw["secret"])["publicKey"]
+	elif "publicKey" in kw:
+		publicKey = kw["publicKey"]
+	else:
+		raise Exception("Can not initialize transaction (no secret or publicKey given)")
+
+	header = struct.pack("<BBBBI",
+		kw.get("head", 0xff),
+		kw.get("version", 0x02),
+		kw.get("network", int(cfg.marker, base=16)),
+		kw.get("type", 0),
+		int(slots.getTime())
+	)
+
+	header += struct.pack("<33s", crypto.unhexlify(publicKey)) if __PY3__ else \
+	          struct.pack(33*"c", publicKey)
+
+	header += struct.pack("<Q", kw.get("fees", 0))
+
+	vendorField = kw.get("vendorField", "")
+	n = min(255, len(vendorField))
+	header += struct.pack("<B", n)
+	if n > 0:
+		header += struct.pack("<%ss"%n, crypto.unhexlify(publicKey[:n])) if __PY3__ else \
+		          struct.pack(n*"c", publicKey[:n])
+
+	return crypto.hexlify(header)
+
+
+def bakePayload(**kw):
+	if "publicKey" in kw and "privateKey" in kw:
+		keys = {}
+		keys["publicKey"] = kw["publicKey"]
+		keys["privateKey"] = kw["privateKey"]
+	elif "secret" in kw:
+		keys = crypto.getKeys(kw["secret"])
+	else:
+		raise Exception("Can not initialize transaction (no secret or keys given)")
+
+	payload = Payload.get(kw.get("type", 0), **kw)
+	kw["fees"] = int((kw.get("type", 0) + len(payload) + 47) * C)
+	header = getHeaders(**kw)
+	payload = header + payload
+
+	payload += crypto.getSignatureFromBytes(crypto.unhexlify(payload), keys["privateKey"])
+	if kw.get("secondSecret", False):
+		secondKeys = crypto.getKeys(kw["secondSecret"])
+		payload += crypto.getSignatureFromBytes(crypto.unhexlify(payload), secondKeys["privateKey"])
+	elif kw.get("secondPrivateKey", False):
+		payload += crypto.getSignatureFromBytes(crypto.unhexlify(payload), kw["secondPrivateKey"])
+	# identify payload
+	payload += crypto.getIdFromBytes(crypto.unhexlify(payload))
+
+	return payload
+
+# This function is a high-level broadcasting for a single tx
+def sendTransaction(**kw):
+	tx = crypto.bakeTransaction(**dict([k,v] for k,v in kw.items() if v))
+	result = rest.POST.peer.transactions.v1(peer=cfg.peers[0], transactions=[tx])
+	success = 1 if result["success"] else 0
+	for peer in cfg.peers[1:]:
+		if rest.POST.peer.transactions.v1(peer=peer, transactions=[tx])["success"]:
+			success += 1
+	result["broadcast"] = "%.1f%%" % (100.*success/len(cfg.peers))
+	return result
+
+#######################
+## basic transaction ##
+#######################
+
+# def sendToken(amount, recipientId, vendorField, secret, secondSecret=None):
+# 	return sendTransaction(
+# 		amount=amount,
+# 		recipientId=recipientId,
+# 		vendorField=VendorField,
+# 		secret=secret,
+# 		secondSecret=secondSecret
+# 	)
+
+# def registerSecondPublicKey(secondPublicKey, secret, secondSecret=None):
+# 	keys = crypto.getKeys(secret)
+# 	return sendTransaction(
+# 		type=1,
+# 		publicKey=keys["publicKey"],
+# 		privateKey=keys["privateKey"],
+# 		secondSecret=secondSecret,
+# 		asset={"signature":{"publicKey":secondPublicKey}}
+# 	)
+
+# def registerSecondPassphrase(secondPassphrase, secret, secondSecret=None):
+# 	secondKeys = crypto.getKeys(secondPassphrase)
+# 	return registerSecondPublicKey(secondKeys["publicKey"], secret, secondSecret)
+
+# def registerDelegate(username, secret, secondSecret=None):
+# 	keys = crypto.getKeys(secret)
+# 	return sendTransaction(
+# 		type=2,
+# 		publicKey=keys["publicKey"],
+# 		privateKey=keys["privateKey"],
+# 		secondSecret=secondSecret,
+# 		asset={"delegate":{"username":username, "publicKey":keys["publicKey"]}}
+# 	)
+
+# def upVoteDelegate(usernames, secret, secondSecret=None):
+# 	keys = crypto.getKeys(secret)
+# 	req = rest.GET.api.delegates.get(username=usernames[-1])
+# 	if req["success"]:
+# 		return sendTransaction(
+# 			type=3,
+# 			publicKey=keys["publicKey"],
+# 			recipientId=crypto.getAddress(keys["publicKey"]),
+# 			privateKey=keys["privateKey"],
+# 			secondSecret=secondSecret,
+# 			asset={"votes":["+%s"%req["delegate"]["publicKey"]]}
+# 		)
+
+# def downVoteDelegate(usernames, secret, secondSecret=None):
+# 	keys = crypto.getKeys(secret)
+# 	req = rest.GET.api.delegates.get(username=usernames[-1])
+# 	if req["success"]:
+# 		return sendTransaction(
+# 			type=3,
+# 			publicKey=keys["publicKey"],
+# 			recipientId=crypto.getAddress(keys["publicKey"]),
+# 			privateKey=keys["privateKey"],
+# 			secondSecret=secondSecret,
+# 			asset={"votes":["-%s"%req["delegate"]["publicKey"]]}
+# 		)
---
+++ packages/Arky/Arky-1.3-py3.5.egg/arky/cli/ledger.py
@@ -0,0 +1,199 @@
+# -*- encoding: utf8 -*-
+# © Toons
+
+"""
+Usage: 
+    ledger link [-i <index> -r <rank>]
+    ledger unlink
+    ledger status
+    ledger send <amount> <address> [<message>]
+    ledger vote [-ud] [<delegates>]
+
+Options:
+-i <index> --account-index <index>  ledger account index  [default: 0]
+-r <rank> --address-rank <rank>     ledger address rank   [default: 0]
+-u --up                             up vote delegate name folowing
+-d --down                           down vote delegate name folowing
+
+Subcommands:
+    link     : link to ledger account.
+    status   : show information about linked account.
+    send     : send token amount to <address>. You can set a 64-char message.
+    vote     : up or down vote delegate(s). <delegates> can be a coma-separated list
+               or a valid new-line-separated file list conaining delegate names.
+"""
+    # ledger validate <registry>
+    # validate : validate transaction from registry.
+
+from .. import HOME
+from .. import rest
+from .. import cfg
+from .. import util
+from .. import ldgr
+from .. import slots
+
+from . import __PY3__
+from . import PROMPT
+from . import DATA
+from . import parse
+from . import input
+from . import __name__ as __root_name__
+from . import floatAmount
+from . import askYesOrNo
+
+from .account import _send
+from .account import _getVoteList
+
+import traceback
+import arky
+import sys
+import os
+
+
+def _sign(tx, derivation_path):
+	try:
+		tx = ldgr.signTx(tx, derivation_path, debug=True)
+	except Exception as e:
+		if not len(e.__dict__):
+			sys.stdout.write("%r\n" % e)
+		elif e.sw == 28416:
+			sys.stdout.write("Ledger key is not ready, try again...\n")
+		elif e.sw == 27013:
+			sys.stdout.write("Transaction canceled\n")
+		else:
+			sys.stdout.write("".join(traceback.format_tb(e.__traceback__)).rstrip() + "\n")
+			sys.stdout.write("%r\n" % e)
+	else:
+		return tx
+
+	return False
+
+
+def _return():
+	sys.stdout.write("Ledger not available on %s network\n" % cfg.network)
+	PROMPT.module = sys.modules[__root_name__]
+	parse(["ledger", ".."])
+
+
+def _whereami():
+	if hasattr(cfg, "slip44"):
+		if DATA.ledger:
+			return "ledger[%s]" % util.shortAddress(DATA.ledger["address"])
+		else:
+			return "ledger"
+	else:
+		_return()
+		return ""
+
+
+def link(param):
+	if hasattr(cfg, "slip44"):
+
+		ledger_dpath = "44'/"+cfg.slip44+"'/%(--account-index)s'/0/%(--address-rank)s" % param
+		try:
+			publicKey = ldgr.getPublicKey(ldgr.parseBip32Path(ledger_dpath))
+			address = arky.core.crypto.getAddress(publicKey)
+			DATA.ledger = rest.GET.api.accounts(address=address).get("account", {})
+		except:
+			sys.stdout.write("Ledger key is not ready, try again...\n")
+		else:
+			if not DATA.ledger:
+				sys.stdout.write("    %s account does not exixts in %s blockchain...\n" % (address, cfg.network))
+			else:
+				DATA.ledger["path"] = ledger_dpath
+
+	else:
+		_return()
+
+
+def status(param):
+	if DATA.ledger:
+		data = rest.GET.api.accounts(address=DATA.ledger["address"], returnKey="account")
+		data["derivationPath"] = DATA.ledger["path"]
+		util.prettyPrint(data)
+
+
+def unlink(param):
+	DATA.ledger.clear()
+
+
+def send(param):
+
+	if DATA.ledger:
+		amount = floatAmount(param["<amount>"])
+		if amount:
+			sys.stdout.write("Use ledger key to confirm or or cancel :\n")
+			sys.stdout.write("    Send %(amount).8f %(token)s to %(recipientId)s ?\n" % \
+		                    {"token": cfg.token, "amount": amount, "recipientId": param["<address>"]})
+			tx = dict(
+				type=0,
+				timestamp=int(slots.getTime()),
+				fee=cfg.fees["send"],
+				amount=amount*100000000,
+				recipientId=param["<address>"],
+				vendorField=param["<message>"],
+			)
+			
+			if _sign(tx, DATA.ledger["path"]): _send(tx)
+
+
+def vote(param):
+
+	lst, verb, to_vote = _getVoteList(param)
+
+	if len(lst):
+		sys.stdout.write("Use ledger key to confirm or or cancel :\n")
+		sys.stdout.write("    %s %s ?\n" % (verb, ", ".join(to_vote)))
+		tx = dict(
+			type=3,
+			timestamp=int(slots.getTime()),
+			fee=cfg.fees["vote"],
+			amount=0,
+			recipientId=DATA.ledger["address"],
+			asset={"votes": lst}
+		)
+
+		if _sign(tx, DATA.ledger["path"]): _send(tx)
+
+
+# def validate(param):
+# 	unlink(param)
+# 	if param["<registry>"]:
+# 		folder = os.path.join(HOME, ".escrow", cfg.network)
+# 		registry = util.loadJson(param["<registry>"], folder)
+
+# 		if len(registry):
+
+# 			derivation_path = input("Enter the derivation path: ")
+# 			try:
+# 				public_key = ldgr.getPublicKey(ldgr.parseBip32Path(derivation_path))
+# 			except Exception as e:
+# 				public_key = ""
+# 				sys.stdout.write("%r\n" % e)
+# 				return False
+
+# 			if registry["secondPublicKey"] == public_key:
+# 				items = []
+# 				for tx in registry["transactions"]:
+# 					if tx.get("asset", False):
+# 						items.append("type=%(type)d, asset=%(asset)s" % tx)
+# 					else:
+# 						items.append("type=%(type)d, amount=%(amount)d, recipientId=%(recipientId)s" % tx)
+# 				if not len(items):
+# 					sys.stdout.write("    No transaction found in registry\n")
+# 					return
+# 				choices = util.chooseMultipleItem("Transactions(s) found:", *items)
+# 				if askYesOrNo("Validate transactions %s ?" % ",".join([str(i) for i in choices])):
+# 					for idx in list(choices):
+# 						tx = registry["transactions"][idx-1]
+# 						if _sign(tx, derivation_path):
+# 							_send(tx)
+# 							registry["transactions"].pop(idx-1)
+# 					util.dumpJson(registry, param["<registry>"], folder)
+# 				else:
+# 					sys.stdout.write("    Validation canceled\n")
+# 			else:
+# 				sys.stdout.write("    Not the valid thirdparty passphrase\n")
+# 		else:
+# 			sys.stdout.write("    Transaction registry not found\n")
+
