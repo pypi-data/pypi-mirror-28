c83cedea646c1b0e47a44c090c052a0986df6e48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:35 +0000

    Add package anyconfig-ion-backend/0.0.2/anyconfig-ion-backend-0.0.2.tar.gz

---
+++ packages/anyconfig-ion-backend/anyconfig-ion-backend-0.0.2.tar.gz/anyconfig-ion-backend-0.0.2/anyconfig_ion_backend/__init__.py
@@ -0,0 +1,8 @@
+"""anyconfig backend to load and dump Amazon Ion
+(https://http://amzn.github.io) data.
+"""
+from __future__ import absolute_import
+from .ion import Parser
+
+__version__ = "0.0.2"
+__all__ = ["Parser"]
