fd16d343ea7818ab07d770bf5a5bb86a6b70c6d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:19 +0000

    Add package django-fake-database-backends/0.0.2/django-fake-database-backends-0.0.2.tar.gz

---
+++ packages/django-fake-database-backends/django-fake-database-backends-0.0.2.tar.gz/django-fake-database-backends-0.0.2/django_fake_database_backends/backends/mysql/schema.py
@@ -0,0 +1,31 @@
+from django.db.backends.mysql.schema import DatabaseSchemaEditor \
+    as BaseDatabaseSchemaEditor
+
+
+class DatabaseSchemaEditor(BaseDatabaseSchemaEditor):
+    def execute(self, sql, params=()):
+        sql = str(sql)
+        if self.collect_sql:
+            ending = "" if sql.endswith(";") else ";"
+            if params is not None:
+                self.collected_sql.append(
+                    (sql % tuple(map(self.quote_value, params))) + ending)
+            else:
+                self.collected_sql.append(sql + ending)
+        # If not collecting the sql, do not execute
+
+    def quote_value(self, value):
+        if type(value) == bool:
+            return str(int(value))
+        # TODO escape correctly all values for mysql
+        # Preferably without having the mysql client as dep
+        return value
+
+    def _field_should_be_indexed(self, model, field):
+        create_index = super(
+            DatabaseSchemaEditor, self)._field_should_be_indexed(model, field)
+        if (create_index and
+           field.get_internal_type() == 'ForeignKey' and
+           field.db_constraint):
+            return False
+        return create_index
---
+++ packages/django-fake-database-backends/django-fake-database-backends-0.0.2.tar.gz/django-fake-database-backends-0.0.2/setup.py
@@ -0,0 +1,57 @@
+from codecs import open
+from os import path
+from setuptools import setup, find_packages
+
+
+PROJECT_DIR = path.abspath(path.dirname(__file__))
+
+with open(path.join(PROJECT_DIR, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+install_requirements = [
+    'django>=1.9'
+]
+
+test_requirements = (
+    'pytest',
+    'tox',
+    'pillow'
+)
+
+setup(
+    name='django-fake-database-backends',
+    version='0.0.2',
+
+    description='Fake django database backends to generate sql without real database',
+    long_description=long_description,
+    url='https://github.com/David-Wobrock/django-fake-database-backends',
+    author='David Wobrock',
+    author_email='david.wobrock@gmail.com',
+    license='MIT',
+
+    packages=find_packages(exclude=['tests/']),
+    install_requires=install_requirements,
+    extras_require={
+        'test': test_requirements
+    },
+
+    keywords='django database backend',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.9',
+        'Framework :: Django :: 1.10',
+        'Framework :: Django :: 1.11',
+        'Framework :: Django :: 2.0',
+
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ]
+)
