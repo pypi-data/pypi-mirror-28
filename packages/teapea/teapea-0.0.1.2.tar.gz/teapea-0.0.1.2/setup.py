716ada9662794601d061432a003e4af03594c074
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:03:45 +0000

    Add package teapea/0.0.1.2/teapea-0.0.1.2.tar.gz

---
+++ packages/teapea/teapea-0.0.1.2.tar.gz/teapea-0.0.1.2/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name="teapea",
+    version="0.0.1.2",
+    author="thinkersilver",
+    author_email="myonlineprofile2012@gmail.com",
+    packages=["teapea"],
+    description="Data Driven Document library"
+) 
