1b2428af8bf1e89de6277ce3ee0b063ae086dde5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:38 +0000

    Add package fakturownia-python/0.1.1/fakturownia-python-0.1.1.tar.gz

---
+++ packages/fakturownia-python/fakturownia-python-0.1.1.tar.gz/fakturownia-python-0.1.1/tests/test_fakturownia.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests for `fakturownia-python` package."""
+import logging
+
+import pytest
+from envparse import env
+from faker import Faker
+
+import fakturownia
+from fakturownia.core import get_default_client
+
+fake = Faker()
+log = logging.getLogger(__name__)
+
+
+@pytest.fixture
+def response():
+    """Sample pytest fixture.
+
+    See more at: http://doc.pytest.org/en/latest/fixture.html
+    """
+    # import requests
+    # return requests.get('https://github.com/wooyek/cookiecutter-pylib')
+
+
+def test_content(response):
+    """Sample pytest test function with the pytest fixture as an argument."""
+    # from bs4 import BeautifulSoup
+    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+
+
+def test_version_exists():
+    """This is a stupid test dummy validating import of fakturownia"""
+    assert fakturownia.__version__
+
+
+@pytest.fixture
+def settings():
+    env.read_envfile()
+
+
+def test_client_factory(settings):
+    client = get_default_client()
+    assert client is not None
+    assert client.api_token
+    log.debug("client.base_url: %s", client.base_url)
+    assert client.base_url
