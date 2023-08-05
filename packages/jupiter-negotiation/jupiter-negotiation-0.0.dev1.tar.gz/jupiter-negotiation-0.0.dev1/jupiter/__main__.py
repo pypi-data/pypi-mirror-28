5eec87edf01b7c04629479dc3fbc12b3c979f0cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:05 +0000

    Add package jupiter-negotiation/0.0.dev1/jupiter-negotiation-0.0.dev1.tar.gz

---
+++ packages/jupiter-negotiation/jupiter-negotiation-0.0.dev1.tar.gz/jupiter-negotiation-0.0.dev1/jupiter/__main__.py
@@ -0,0 +1,35 @@
+import sys
+import traceback
+from .cli import parser
+from .jupiter.jupiter import Jupiter
+from .jupiter.jupiter import test
+
+def execute(args):
+    print(args)
+    pass
+
+
+def main(argv=sys.argv):
+    if len(argv) < 2:
+        parser.parse_args(["-h"])
+        sys.exit(0)
+
+    # 自動でコマンドライン引数を読み取る 引数必要なし
+    args = parser.parse_args()
+
+    try:
+        exit_code = execute(args)
+        sys.exit(exit_code)
+
+    except Exception as e:
+        error_type = type(e).__name__
+        stack_trace = traceback.format_exc()
+
+        if args.stacktrace:
+            print("{:=^30}".format(" STACK TRACE "))
+            print(stack_trace.strip())
+
+        else:
+            sys.stderr.write(
+                "{0}: {1}\n".format(e_type, e.message))
+            sys.exit(1)
