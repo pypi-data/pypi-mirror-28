859fb6625c19632b2cbac2bf76149911087c0e10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:19 +0000

    Add package huanzinester/1.2.0/huanzinester-1.2.0.tar.gz

---
+++ packages/huanzinester/huanzinester-1.2.0.tar.gz/huanzinester-1.2.0/huanzinester.py
@@ -0,0 +1,12 @@
+"""递归打印列表"""
+
+
+def print_lol(the_list, level):
+        # the_list 列表（或者嵌套列表），递归打印所有项.
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol(each_item, level + 1)
+        else:
+            for tab_stop in range(level):
+                print("\t", end='')
+            print(each_item)
