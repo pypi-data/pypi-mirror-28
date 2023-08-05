4ac646252e24e481769f39ebbdd88335af054d80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:46 +0000

    Add package quicktracer/1.0.0/quicktracer-1.0.0.zip

---
+++ packages/quicktracer/quicktracer-1.0.0.zip/quicktracer-1.0.0/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+import os
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+  name = 'quicktracer',
+  packages = ['quicktracer'],
+  version = '1.0.0',
+  description = 'A simple function to do realtime tracing/monitoring/plotting',
+  author = 'DomNomNom',
+  author_email = 'dominikschmid93+quicktracer@gmail.com',
+  url = 'https://github.com/DomNomNom/quicktracer',
+  # download_url = 'https://github.com/DomNomNom/quicktracer/archive/0.1.tar.gz',
+  long_description=read('README'),
+  license = "MIT",
+  keywords = ['testing', 'tracing', 'plot', 'RLBot', 'quicktracer', 'realtime', 'monitoring'],
+  classifiers = [],
+)
+
