c5adc497c2966e12a0d50643997f3061b271c17e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:27 +0000

    Add package emailpgp/0.2.1/emailpgp-0.2.1.tar.gz

---
+++ packages/emailpgp/emailpgp-0.2.1.tar.gz/emailpgp-0.2.1/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:ts=4:sw=4:expandtab
+# Copyright 2017 juga (juga at riseup dot net), MIT license.
+
+"""Setup."""
+from setuptools import find_packages, setup
+
+
+setup(
+    name='emailpgp',
+    version='0.2.1',
+    description='Extend Python email.mime classes to add MIME'
+    ' multipart/pgp-encrypted type messages.',
+    long_description='',
+    author='juga',
+    author_email='juga@riseup.net',
+    license='MIT',
+    url='https://github.com/juga0/emailpgp',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    extras_require={
+        'dev': ['ipython', 'pyflakes', 'pep8'],
+        'test': ['tox', 'pytest'],
+        'doc': ['sphinx', 'pylint']
+    },
+    python_requires=">=3.5",
+    zip_safe=False,
+    include_package_data=True,
+    keywords='python mime OpenPGP',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        "Environment :: Console",
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: System :: Networking',
+    ],
+)
