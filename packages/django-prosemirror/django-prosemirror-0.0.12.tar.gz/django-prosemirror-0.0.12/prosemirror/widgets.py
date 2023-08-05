8357050ac98bdad3dadd10700fd9a4683fd582b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:15 +0000

    Add package django-prosemirror/0.0.12/django-prosemirror-0.0.12.tar.gz

---
+++ packages/django-prosemirror/django-prosemirror-0.0.12.tar.gz/django-prosemirror-0.0.12/prosemirror/widgets.py
@@ -0,0 +1,84 @@
+"""
+Fields
+"""
+
+import json
+
+try:
+    from django.urls import reverse_lazy
+except ImportError:
+    # Django <1.10 compliance
+    from django.core.urlresolvers import reverse_lazy
+
+from django.conf import settings
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django import forms
+from django.forms import widgets
+
+MAXI = {
+    "type": "bar",  # can be bar or tooltip
+    "schema": "maxi",  # can be maxi or micro
+}
+
+MICRO = {
+    "type": "tooltip",  # can be bar or tooltip
+    "schema": "micro",  # can be maxi or micro
+}
+
+DEFAULTS = {
+    "GLOBAL": {
+        "inline_code": False,  # allows inline code
+        "rule": False,  # Allows rule
+    },
+    "PROFILES": {
+        "default": MAXI,
+        "maxi": MAXI,
+        "micro": MICRO,
+    },
+}
+CONFIG = getattr(settings, "PROSEMIRROR", {})
+
+
+class ProseMirrorWidget(widgets.Textarea):
+    """
+    ProseMirror widget
+    """
+    def __init__(self, *args, **kwargs):
+        """
+        Constructor
+        """
+        self.profile = kwargs.pop("prosemirror_profile", "default")
+        attrs = kwargs.get("attrs", {})
+        attrs["data-prosemirror-options"] = json.dumps(self.options)
+        attrs["class"] = (
+            "%s prosemirror-box" % attrs.get("class", "")).strip()
+        kwargs["attrs"] = attrs
+        super(ProseMirrorWidget, self).__init__(*args, **kwargs)
+
+    @property
+    def options(self):
+        """
+        Get options
+        """
+        config = DEFAULTS.copy()
+        config.update(CONFIG)
+        options = config["GLOBAL"].copy()
+        options.update(config["PROFILES"][self.profile])
+        return options
+
+    def _media(self):
+        """
+        Include media
+        """
+        js = (
+            staticfiles_storage.url("prosemirror/widget.min.js"),
+        )
+
+        css = {
+            "all": (
+                staticfiles_storage.url(
+                    "prosemirror/widget.min.css"),
+            )
+        }
+        return forms.Media(css=css, js=js)
+    media = property(_media)
---
+++ packages/django-prosemirror/django-prosemirror-0.0.12.tar.gz/django-prosemirror-0.0.12/setup.py
@@ -0,0 +1,51 @@
+"""
+Install script
+"""
+
+import codecs
+import os
+import re
+
+from setuptools import setup, find_packages
+
+import versioneer
+
+
+###############################################################################
+
+NAME = "django-prosemirror"
+AUTHOR = "Tom Van Damme"
+EMAIL = "t_o_mvandamme@hotmail.com"
+URL = "https://github.com/zakdoek/django-prosemirror/"
+LICENSE = "MIT"
+DESCRIPTION = "Prosemirror field for django"
+PACKAGES = find_packages()
+CLASSIFIERS = [
+    "Framework :: Django",
+]
+INSTALL_REQUIRES = [
+    "commonmark>=0.7.0",
+]
+
+###############################################################################
+
+README = open(os.path.join(os.path.dirname(__file__), "README.md")).read()
+
+
+if __name__ == "__main__":
+    print(PACKAGES)
+    setup(
+        name=NAME,
+        author=AUTHOR,
+        author_email=EMAIL,
+        url=URL,
+        license=LICENSE,
+        description=DESCRIPTION,
+        long_description=README,
+        version=versioneer.get_version(),
+        cmdclass=versioneer.get_cmdclass(),
+        packages=PACKAGES,
+        include_package_data=True,
+        classifiers=CLASSIFIERS,
+        install_requires=INSTALL_REQUIRES,
+    )
