9d174144ba10d5b08d87cc310ce5d75bbc6d9f38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:35 +0000

    Add package mythril/0.8.27/mythril-0.8.27.tar.gz

---
+++ packages/mythril/mythril-0.8.27.tar.gz/mythril-0.8.27/mythril/analysis/security.py
@@ -0,0 +1,28 @@
+from mythril.analysis.report import Report
+from mythril.analysis import modules
+import pkgutil
+
+
+def fire_lasers(statespace):
+
+    issues = []
+    _modules = []
+    
+    for loader, name, is_pkg in pkgutil.walk_packages(modules.__path__):
+        _modules.append(loader.find_module(name).load_module(name))
+
+    for module in _modules:
+        issues += module.execute(statespace)
+
+    report = Report()
+
+    if (len(issues)):
+
+        for i in range(0, len(issues)):
+            report.append_issue(issues[i])
+
+        print(report.as_text())
+
+    else:
+
+        print("The analysis was completed successfully. No issues were detected.")
