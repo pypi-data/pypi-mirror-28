a47799f196bd8d5d074c557707e3936ef7ec1b53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:29 +0000

    Add package stars-zs/1.1.0/stars_zs-1.1.0.tar.gz

---
+++ packages/stars-zs/stars_zs-1.1.0.tar.gz/stars_zs-1.1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name = 'stars_zs',
+    version = '1.1.0',
+    py_modules = ['stars_zs'],
+    author = 'shuibihuajia',
+    author_email = 'shuibihuajia@163.com',
+    url = 'https://upload.pypi.org/legacy/',
+    description = 'generate starts'
+    )
---
+++ packages/stars-zs/stars_zs-1.1.0.tar.gz/stars_zs-1.1.0/stars_zs.py
@@ -0,0 +1,4 @@
+def star(i):
+    for i in range(10):
+        i+=1
+        print(i*"*")
