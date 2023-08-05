662b0826f5bcd7f6db2cd272cac79924ff51ad83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:15 +0000

    Add package stellaaa/1.1.0/stellaaa-1.1.0.tar.gz

---
+++ packages/stellaaa/stellaaa-1.1.0.tar.gz/stellaaa-1.1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup (
+        name = "stellaaa",
+        version = "1.1.0",
+        py_modules = ["stellaaa"],
+        author = "jason",
+        author_email = "397105246@qq.com",
+        url = "https://www.qq.com",
+        description = "A simple printer of nested lists",
+    )
