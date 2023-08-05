905729fec94b57cdfc499baad743ff82eb6f56e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:50 +0000

    Add package mylibjay2/0.0.1/mylibjay2-0.0.1.zip

---
+++ packages/mylibjay2/mylibjay2-0.0.1.zip/mylibjay2-0.0.1/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+from setuptools import setup, find_packages
+
+setup(
+    name = 'mylibjay2',
+    version = '0.0.1',
+    keywords = ('simple', 'test'),
+    description = 'just a simple test',
+    license = 'MIT License',
+	packages=['mylibjay2'],
+
+    author = 'ljx',
+    author_email = '261958374@qq.com',
+    platforms = 'any',
+)
