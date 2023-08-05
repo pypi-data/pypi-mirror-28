2bb0922053296ad7ecc84626ea35b40b20249bf6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:03 +0000

    Add package procmem/0.1.0/procmem-0.1.0.tar.gz

---
+++ packages/procmem/procmem-0.1.0.tar.gz/procmem-0.1.0/procmem/main_write.py
@@ -0,0 +1,31 @@
+# procmem - A process memory inspection tool
+# Copyright (C) 2018 Ingo Ruhnke <grumbel@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+from procmem.memory import Memory
+from procmem.pack import text2bytes
+
+
+def main_write(pid, args):
+    address = int(args.address, 16)
+
+    data = text2bytes(args.DATA, args.type)
+
+    with Memory.from_pid(pid, mode='wb') as mem:
+        mem.write(address, data)
+
+
+# EOF #
