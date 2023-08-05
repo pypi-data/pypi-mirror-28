3e9ed8521a70a704a46df4afb05b051d1201a28e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:41 +0000

    Add package edapy/0.2.0/edapy-0.2.0.tar.gz

---
+++ packages/edapy/edapy-0.2.0.tar.gz/edapy-0.2.0/setup.py
@@ -0,0 +1,43 @@
+# core modules
+from setuptools import find_packages
+from setuptools import setup
+
+config = {
+    'name': 'edapy',
+    'version': '0.2.0',
+    'author': 'Martin Thoma',
+    'author_email': 'info@martin-thoma.de',
+    'maintainer': 'Martin Thoma',
+    'maintainer_email': 'info@martin-thoma.de',
+    'packages': find_packages(),
+    'scripts': ['bin/edapy'],
+    # 'package_data': {'hwrt': ['templates/*', 'misc/*']},
+    'platforms': ['Linux'],
+    'url': 'https://github.com/MartinThoma/edapy',
+    'license': 'MIT',
+    'description': 'Exploratory Data Analysis',
+    'long_description': ("A tookit for exploratoriy data analysis."),
+    'install_requires': [
+        'argparse',
+        'click',
+        'pandas',
+        'PyPDF2',
+        'PyYAML',
+    ],
+    'keywords': ['EDA', 'Data Science'],
+    'download_url': 'https://github.com/MartinThoma/edapy',
+    'classifiers': ['Development Status :: 1 - Planning',
+                    'Environment :: Console',
+                    'Intended Audience :: Developers',
+                    'Intended Audience :: Science/Research',
+                    'Intended Audience :: Information Technology',
+                    'License :: OSI Approved :: MIT License',
+                    'Natural Language :: English',
+                    'Programming Language :: Python :: 3.5',
+                    'Topic :: Scientific/Engineering :: Information Analysis',
+                    'Topic :: Software Development',
+                    'Topic :: Utilities'],
+    'zip_safe': False,
+}
+
+setup(**config)
