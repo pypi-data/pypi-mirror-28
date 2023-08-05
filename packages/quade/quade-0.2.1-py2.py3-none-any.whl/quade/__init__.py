d5fe66ee2e86ccf25009a702e802f7d908e0919a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:35 +0000

    Add package quade/0.2.1/quade-0.2.1-py2.py3-none-any.whl

---
+++ packages/quade/quade-0.2.1-py2.py3-none-any.whl/quade/__init__.py
@@ -0,0 +1,14 @@
+from __future__ import unicode_literals
+
+from .settings import AllEnvs, DebugEnvs, Settings
+
+
+__all__ = [
+    'AllEnvs',
+    'DebugEnvs',
+    'Settings',
+]
+
+
+__version__ = '0.2.1'
+default_app_config = 'quade.apps.QuadeConfig'
