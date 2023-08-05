3d9a517d194ca97f57666625b6812a33ecb39730
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:26 +0000

    Add package Arky/1.3.1/Arky-1.3.1.tar.gz

---
+++ packages/Arky/Arky-1.3.1.tar.gz/Arky-1.3.1/test/test_util.py
@@ -0,0 +1,23 @@
+# -*- encoding: utf8 -*-
+# © Toons
+
+import unittest
+
+from arky import rest, cli
+
+class TestUtilDef(unittest.TestCase):
+
+	@classmethod
+	def setUpClass(cls):
+		rest.use("dark")
+
+	def test_unlockAcount(self):
+		# function doesn't exist so we can't test it
+		pass
+
+	def test_floatAmount(self):
+		# can' test because `floatAmount` function still seems to be WIP
+		pass
+
+if __name__ == '__main__':
+    unittest.main()
