80c5e23e7287e0f002cab0b8d01ff1451147c85b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:58 +0000

    Add package slack_log_handler/0.3.0/slack_log_handler-0.3.0.tar.gz

---
+++ packages/slack_log_handler/slack_log_handler-0.3.0.tar.gz/slack_log_handler-0.3.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='slack_log_handler',
+    version='0.3.0',
+    author='Daniel Perez',
+    author_email='daniel@claudetech.com',
+    packages=['slack_log_handler'],
+    url='https://github.com/claudetech/python-slack-log',
+    license='LICENSE',
+    description='Python Slack log handler using webhook',
+    long_description=open('README.md').read(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries'
+    ],
+)
