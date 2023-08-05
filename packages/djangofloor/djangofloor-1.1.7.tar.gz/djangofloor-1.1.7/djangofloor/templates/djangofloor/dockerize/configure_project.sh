3179e5419b5044bc9d8516495c76fd1601ff51aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:38 +0000

    Add package djangofloor/1.1.7/djangofloor-1.1.7.tar.gz

---
+++ packages/djangofloor/djangofloor-1.1.7.tar.gz/djangofloor-1.1.7/djangofloor/templates/djangofloor/dockerize/configure_project.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+{{ process.control.command_line }} collectstatic --noinput
---
+++ packages/djangofloor/djangofloor-1.1.7.tar.gz/djangofloor-1.1.7/djangofloor/utils.py
@@ -0,0 +1,145 @@
+"""Utility functions
+=================
+
+Define some utility functions like warning or walking through modules.
+
+"""
+
+import argparse
+import os
+from argparse import ArgumentParser
+from importlib import import_module
+
+import pkg_resources
+from django.core.exceptions import ImproperlyConfigured
+from django.utils.module_loading import import_string
+
+__author__ = 'Matthieu Gallet'
+
+
+class RemovedInDjangoFloor110Warning(DeprecationWarning):
+    """Used for displaying functions or modules that will be removed in a near future."""
+    pass
+
+
+class RemovedInDjangoFloor200Warning(DeprecationWarning):
+    """Used for displaying functions or modules that will be removed in a near future."""
+    pass
+
+
+def is_package_present(package_name):
+    """Return True is the `package_name` package is presend in your current Python environment."""
+    try:
+        import_module(package_name)
+        return True
+    except ImportError:
+        return False
+
+
+def ensure_dir(path, parent=True):
+    """Ensure that the given directory exists
+
+    :param path: the path to check
+    :param parent: only ensure the existence of the parent directory
+
+    """
+    dirname = os.path.dirname(path) if parent else path
+    if not os.path.isdir(dirname):
+        os.makedirs(dirname)
+    return path
+
+
+def walk(module_name, dirname, topdown=True):
+    """
+    Copy of :func:`os.walk`, please refer to its doc. The only difference is that we walk in a package_resource
+    instead of a plain directory.
+    :type module_name: basestring
+    :param module_name: module to search in
+    :type dirname: basestring
+    :param dirname: base directory
+    :type topdown: bool
+    :param topdown: if True, perform a topdown search.
+    """
+
+    def rec_walk(root):
+        """
+        Recursively list subdirectories and filenames from the root.
+        :param root: the root path
+        :type root: basestring
+        """
+        dirnames = []
+        filenames = []
+        for name in pkg_resources.resource_listdir(module_name, root):
+            # noinspection PyUnresolvedReferences
+            fullname = root + '/' + name
+            isdir = pkg_resources.resource_isdir(module_name, fullname)
+            if isdir:
+                dirnames.append(name)
+                if not topdown:
+                    rec_walk(fullname)
+            else:
+                filenames.append(name)
+        yield root, dirnames, filenames
+        if topdown:
+            for name in dirnames:
+                # noinspection PyUnresolvedReferences
+                for values in rec_walk(root + '/' + name):
+                    yield values
+
+    return rec_walk(dirname)
+
+
+def _resolve_name(name, package, level):
+    """Return the absolute name of the module to be imported."""
+    # noinspection PyTypeChecker
+    if not hasattr(package, 'rindex'):
+        raise ValueError("'package' not set to a string")
+    dot = len(package)
+    for x in range(level, 1, -1):
+        try:
+            dot = package.rindex('.', 0, dot)
+        except ValueError:
+            raise ValueError("attempted relative import beyond top-level package")
+    return "%s.%s" % (package[:dot], name)
+
+
+def guess_version(defined_settings):
+    """Guesss the project version.
+    Expect an installed version (findable with pkg_resources) or __version__ in `your_project/__init__.py`.
+    If not found
+
+    :param defined_settings: all already defined settings (dict)
+    :type defined_settings: :class:`dict`
+    :return: should be something like `"1.2.3"`
+    :rtype: :class:`str`
+    """
+    try:
+        project_distribution = pkg_resources.get_distribution(defined_settings['DF_MODULE_NAME'])
+        return project_distribution.version
+    except pkg_resources.DistributionNotFound:
+        pass
+    try:
+        return import_string('%s.__version__' % defined_settings['DF_MODULE_NAME'])
+    except ImportError:
+        return '1.0.0'
+
+
+def get_view_from_string(view_as_str):
+    try:
+        view = import_string(view_as_str)
+    except ImportError:
+        raise ImproperlyConfigured('Unable to import %s' % view_as_str)
+    if hasattr(view, 'as_view') and callable(view.as_view):
+        return view.as_view()
+    elif callable(view):
+        return view
+    raise ImproperlyConfigured('%s is not callabled and does not have an "as_view" attribute')
+
+
+def remove_arguments_from_help(parser, arguments):
+    assert isinstance(parser, ArgumentParser)
+    assert isinstance(arguments, set)
+    # noinspection PyProtectedMember
+    for action in parser._actions:
+        if arguments & set(action.option_strings):
+            action.help = argparse.SUPPRESS
