e0ef3eb067f9521d40ce5ea0acd2cd3b048bd467
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:41 +0000

    Add package pygeoprocessing/0.6.0/pygeoprocessing-0.6.0.tar.gz

---
+++ packages/pygeoprocessing/pygeoprocessing-0.6.0.tar.gz/pygeoprocessing-0.6.0/src/pygeoprocessing/__init__.py
@@ -0,0 +1,24 @@
+"""__init__ module for pygeprocessing, imports all the geoprocessing functions
+    into the pygeoprocessing namespace"""
+from __future__ import absolute_import
+
+import natcap.versioner
+__version__ = natcap.versioner.get_version('pygeoprocessing')
+
+import logging
+import types
+import sys
+
+from . import geoprocessing
+from .geoprocessing_core import calculate_slope
+
+LOGGER = logging.getLogger('pygeoprocessing')
+LOGGER.setLevel(logging.DEBUG)
+LOGGER.addHandler(logging.NullHandler())
+
+__all__ = ['calculate_slope']
+for attrname in dir(geoprocessing):
+    attribute = getattr(geoprocessing, attrname)
+    if isinstance(attribute, types.FunctionType):
+        __all__.append(attrname)
+        setattr(sys.modules['pygeoprocessing'], attrname, attribute)
