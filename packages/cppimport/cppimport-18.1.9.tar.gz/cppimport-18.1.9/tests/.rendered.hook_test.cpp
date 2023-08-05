9bfe7f9d022c9c781e95f01268cc6b328a817b8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:17 +0000

    Add package cppimport/18.1.9/cppimport-18.1.9.tar.gz

---
+++ packages/cppimport/cppimport-18.1.9.tar.gz/cppimport-18.1.9/tests/.rendered.hook_test.cpp
@@ -0,0 +1,9 @@
+/*cppimport*/
+
+#include <pybind11/pybind11.h>
+
+PYBIND11_PLUGIN(hook_test) {
+    pybind11::module m("hook_test", "auto-compiled c++ extension");
+    m.def("sub", [] (int i, int j) { return i - j; } );
+    return m.ptr();
+}
