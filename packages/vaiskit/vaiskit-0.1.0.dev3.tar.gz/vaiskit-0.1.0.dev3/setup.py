b81d90b879840036047e2998aac8df532f7b8882
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:41 +0000

    Add package vaiskit/0.1.0.dev3/vaiskit-0.1.0.dev3.tar.gz

---
+++ packages/vaiskit/vaiskit-0.1.0.dev3.tar.gz/vaiskit-0.1.0.dev3/setup.py
@@ -0,0 +1,15 @@
+import os
+from setuptools import setup
+
+if __name__ == "__main__":
+    setup(
+        name="vaiskit",
+        author='VAIS',
+        packages=['vaiskit'],
+        author_email='support@vais.vn',
+        url='https://vais.vn',
+        include_package_data=True,
+        install_requires=["grpcio==1.4.0", "pyaudio==0.2.11"],
+        version="0.1.0-dev3",
+        python_requires='>=2.6, !=3.0.*, !=3.1.*, !=3.2.*, <4',
+    )
