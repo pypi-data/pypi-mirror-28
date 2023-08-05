725dc978857ccfab9ae2237d09d55086149a4a7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:06 +0000

    Add package cvbase/0.3.6/cvbase-0.3.6-py2.py3-none-any.whl

---
+++ packages/cvbase/cvbase-0.3.6-py2.py3-none-any.whl/cvbase/__init__.py
@@ -0,0 +1,14 @@
+from .conversion import *
+from .det import *
+from .io import *
+from .progress import *
+from .timer import *
+from .version import __version__
+# require opencv
+try:
+    from .image import *
+    from .opencv import *
+    from .video import *
+    from .visualize import *
+except ImportError:
+    pass
