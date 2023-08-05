789eb72f6e1813c7b8cd91b1aedaa113af58a334
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:26 +0000

    Add package triggers/0.0.2/triggers-0.0.2.tar.gz

---
+++ packages/triggers/triggers-0.0.2.tar.gz/triggers-0.0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="triggers",
+    version='0.0.2',
+    packages=['triggers'],
+    install_requires=[
+        'masonite',
+    ],
+    include_package_data=True,
+)
