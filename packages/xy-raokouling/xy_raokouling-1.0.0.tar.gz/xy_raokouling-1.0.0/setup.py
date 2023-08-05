c73309f4e648cc3b5fac8e0802fe528e6a7b0e1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:51 +0000

    Add package xy-raokouling/1.0.0/xy_raokouling-1.0.0.tar.gz

---
+++ packages/xy-raokouling/xy_raokouling-1.0.0.tar.gz/xy_raokouling-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+  name = 'xy_raokouling',
+  packages = ['xy_raokouling'],
+  version = '1.0.0',
+  description = 'Get The rao kou ling',
+  long_description='Get The rao kou ling',
+  author = 'KingHS',
+  author_email = '382771946@qq.com',
+  keywords = ['pip3', 'python3','python','raokouling'],
+  license='MIT',
+)
---
+++ packages/xy-raokouling/xy_raokouling-1.0.0.tar.gz/xy_raokouling-1.0.0/xy_raokouling/xy_raokouling.py
@@ -0,0 +1,37 @@
+import urllib.request
+import urllib.parse
+import json
+import os
+
+
+APPKEY='631c5a5b9992bd74'
+APPSECRET='eS0iNTaMdI5mgRew8JkrVqUB7mG1MytS'
+API_URL='http://api.jisuapi.com/rkl/search'
+
+'''绕口令'''
+def search(keyword=''):
+    data = {}
+    data['appkey'] = APPKEY
+    data['pagesize'] = 1
+    data['pagenum'] = 1
+    data['keyword'] = keyword
+    url_values = urllib.parse.urlencode(data)
+    url = API_URL + '?' + url_values
+    result = urllib.request.urlopen(url)
+    jsonarr = json.loads(result.read())
+    res = jsonarr['result']
+    if res:
+        res_dict = {}
+        res_dict['title']=res['list'][0]['title']
+        res_dict['content']=res['list'][0]['content'].replace('<br />',os.linesep)
+        return res_dict
+    else:
+        return -1
+
+
+
+def main():
+    print(search())
+
+if __name__ == '__main__':
+    main()
