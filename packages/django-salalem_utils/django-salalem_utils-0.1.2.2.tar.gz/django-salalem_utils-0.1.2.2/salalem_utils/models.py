d4a2ab1d8e3f118c9a190a334dae34df03bb1fc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:15 +0000

    Add package django-salalem_utils/0.1.2.2/django-salalem_utils-0.1.2.2.tar.gz

---
+++ packages/django-salalem_utils/django-salalem_utils-0.1.2.2.tar.gz/django-salalem_utils-0.1.2.2/salalem_utils/models.py
@@ -0,0 +1,12 @@
+from django_extensions.db.fields import CreationDateTimeField, ModificationDateTimeField
+from django.utils.translation import ugettext_lazy as _
+
+
+class TimeStampMixin(object):
+    class Meta:
+        get_latest_by = 'modified'
+        ordering = ('-modified', '-created',)
+        abstract = True
+
+    created = CreationDateTimeField(_('created'))
+    modified = ModificationDateTimeField(_('modified'))
