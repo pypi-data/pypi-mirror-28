9560c5b3dd4ad2fd542432ef76351dd7f0e33adb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:30 +0000

    Add package nimbus-scrapy/0.1.0/nimbus_scrapy-0.1.0-py2.py3-none-any.whl

---
+++ packages/nimbus-scrapy/nimbus_scrapy-0.1.0-py2.py3-none-any.whl/nimbus_scrapy/spiders/base.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+import os
+import sys
+import json
+import datetime
+import time
+import urlparse
+import random
+import platform
+import scrapy
+from scrapy import signals
+from scrapy.spiders import Spider, CrawlSpider, CSVFeedSpider, SitemapSpider, XMLFeedSpider
+from scrapy.spiders import Rule
+from scrapy.http.request.form import FormRequest, Request
+from scrapy.http.response.html import HtmlResponse
+from scrapy.selector import Selector
+from scrapy.utils.response import get_base_url
+from .mixin import BaseMixin
+
+
+class AbstractSpider(BaseMixin, Spider):
+    name = None
+    allowed_domains = []
+    start_urls = []
+
+    def __init__(self, name=None, **kwargs):
+        super(AbstractSpider, self).__init__(name, **kwargs)
+
+    def start_requests(self):
+        return super(AbstractSpider, self).start_requests()
+
+    def parse(self, response):
+        raise NotImplementedError
+
+    @classmethod
+    def from_crawler(cls, crawler, *args, **kwargs):
+        config = kwargs.pop("config", None)
+        pipelines = crawler.settings["ITEM_PIPELINES"]
+        if not isinstance(pipelines, dict):
+            crawler.settings["ITEM_PIPELINES"] = {
+                "nimbus_scrapy.pipelines.NimbusPipeline": 200,
+            }
+        elif isinstance(pipelines, dict):
+            crawler.settings["ITEM_PIPELINES"]["nimbus_scrapy.pipelines.NimbusPipeline"] = 200
+
+        spider = super(AbstractSpider, cls).from_crawler(crawler, *args, **kwargs)
+        try:
+            spider.setup(config)
+        except Exception as e:
+            spider.log(e)
+        return spider
+
+
+
---
+++ packages/nimbus-scrapy/nimbus_scrapy-0.1.0-py2.py3-none-any.whl/nimbus_scrapy/spiders/mixin.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+import os
+import sys
+import json
+import datetime
+import time
+import urlparse
+import random
+import platform
+import copy
+import scrapy
+from scrapy import signals
+from scrapy.spiders import Spider, CrawlSpider, CSVFeedSpider, SitemapSpider, XMLFeedSpider
+from scrapy.spiders import Rule, BaseSpider
+from scrapy.exceptions import DropItem
+from scrapy.exceptions import NotConfigured, IgnoreRequest
+from scrapy.http.request.form import FormRequest, Request
+from scrapy.http.response.html import HtmlResponse
+from scrapy.selector import Selector
+from scrapy.utils.response import get_base_url
+
+__all__ = ["BaseMixin", ]
+
+
+class BaseMixin(object):
+    config = None
+    spider_id = None
+    spider_config = None
+
+    def setup(self, config=None):
+        if config is None:
+            self.config = None
+        elif isinstance(config, basestring):
+            self.config = json.loads(config)
+        elif isinstance(config, dict):
+            self.config = copy.deepcopy(config)
+        else:
+            raise NotImplementedError
+
+        self.spider_id = self.config.get("id", None)
+        for key, value in self.config.iteritems():
+            func_key = u"setup_{}".format(key)
+            func = getattr(self, func_key, None)
+            if callable(func):
+                client = func(self.spider_id, value)
+            else:
+                client = None
+            setattr(self, key, client)
+
+    def setup_db(self, spider_id=None, config=None):
+        from ..data import dbclient
+        dbclient.init(name=spider_id, **config)
+        return dbclient.get_session(name=spider_id)
+
+    def setup_redis(self, spider_id=None, config=None):
+        from ..data import redisclient
+        connection = redisclient.init(name=spider_id, **config)
+        return connection
+
+    def setup_spider(self, spider_id=None, config=None):
+        self.spider_config = copy.deepcopy(config)
+        return self.spider_config
+
+    def process_item(self, item, spider, **kwargs):
+        raise NotImplementedError
+
+
