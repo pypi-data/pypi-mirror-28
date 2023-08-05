618ae0f46247a849bf1c4466f23a2571dfb9b3c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:50 +0000

    Add package simdict/1.1.4/simdict-1.1.4.tar.gz

---
+++ packages/simdict/simdict-1.1.4.tar.gz/simdict-1.1.4/demo.py
@@ -0,0 +1,2 @@
+def echotest(test):
+	print(test)
---
+++ packages/simdict/simdict-1.1.4.tar.gz/simdict-1.1.4/setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+setup(
+	name = 'simdict',
+	version = '1.1.4',
+	py_modules = ['demo'],
+	author = 'overcat',
+	)
