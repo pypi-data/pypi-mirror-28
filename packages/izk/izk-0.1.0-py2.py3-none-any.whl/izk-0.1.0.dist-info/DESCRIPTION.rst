8185977ec4f4c33869e36d53b6ea1495d646eab3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:37 +0000

    Add package izk/0.1.0/izk-0.1.0-py2.py3-none-any.whl

---
+++ packages/izk/izk-0.1.0-py2.py3-none-any.whl/izk-0.1.0.dist-info/DESCRIPTION.rst
@@ -0,0 +1,36 @@
+# IZK: interactive zookeeper
+
+`izk` is a modern and simple zookeeper shell, with autoccompletion, history search, vi bindings, syntax highlighting and pretty-printing.
+
+## Installation
+
+To install `izk`, simply run
+
+```shell
+$ pip install izk
+```
+
+## Usage
+
+```shell
+$ izk --help
+usage: izk [-h]
+           [--style {default,emacs,...}]
+           [zk_url]
+
+CLI for zookeeper with syntax-highlighting and auto-completion
+
+positional arguments:
+  zk_url                URL of the zookeeper node. Default: localhost:2181
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --write               Authorize write operations (update/insert/remove)
+  --style {default,emacs,...}
+                        The color style to adopt. Default: monokai
+```
+
+## Demonstration
+
+Have a look at this [demonstration](https://www.youtube.com/embed/9Sp4R2prqHg), showing off a few features of `izk`.
+
---
+++ packages/izk/izk-0.1.0-py2.py3-none-any.whl/izk/formatting.py
@@ -0,0 +1,33 @@
+import json
+import collections
+import functools
+
+from pygments import highlight, lexers, formatters
+from pygments import styles
+
+
+STYLE_NAMES = list(styles.get_all_styles())
+
+
+def colorize(f):
+    @functools.wraps(f)
+    def wrapper(*args, **kwargs):
+        """Pretty-print container types and syntax-highlight command results."""
+        from .prompt import g
+        out = f(*args, **kwargs)
+        printable = repr(out)
+        lexer = lexers.PythonLexer()
+        try:
+            serialized = json.loads(out)
+        except ValueError:
+            pass
+        else:
+            if isinstance(serialized, collections.abc.Container):
+                printable = json.dumps(serialized, indent=2)
+                lexer = lexers.JsonLexer()
+        printable = highlight(
+            printable,
+            lexer,
+            formatters.Terminal256Formatter(style=g.style))
+        return printable
+    return wrapper
