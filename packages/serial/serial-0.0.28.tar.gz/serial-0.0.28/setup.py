95ee1c074143246508cd061471ac32f311903a4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:12 +0000

    Add package serial/0.0.28/serial-0.0.28.tar.gz

---
+++ packages/serial/serial-0.0.28.tar.gz/serial-0.0.28/setup.py
@@ -0,0 +1,53 @@
+from setuptools import setup, find_packages
+
+version = '0.0.28'
+
+setup(
+    name='serial',
+
+    version=version,
+
+    description='A library for serializing python objects as JSON/YAML/XML, and deserializing JSON/YAML/XML.',
+
+    # The project's main homepage.
+    url='https://bitbucket.com/davebelais/serial.git',
+
+    # Author details
+    author='David Belais',
+    author_email='david@belais.me',
+
+    # Choose your license
+    license='MIT',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        # 'Development Status :: 2 - Pre-Alpha',
+        # 'Development Status :: 3 - Alpha',
+        # 'Development Status :: 4 - Beta',
+        # 'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        # 'Programming Language :: Python :: 2.7',
+        # 'Programming Language :: Python :: 3',
+        # 'Programming Language :: Python :: 3.2',
+        # 'Programming Language :: Python :: 3.3',
+        # 'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+
+    keywords='rest api serialization serialize',
+
+    packages=find_packages(),
+    # packages=[], # explicitly set packages
+    # py_modules=[], # Single-file module names
+
+    # dependencies
+    # See https://packaging.python.org/en/latest/requirements.html
+    install_requires=[
+        'future>=0.16.0',
+        'pyyaml>=3.12',
+        'iso8601>=0.1.12',
+    ],
+)
