3aee2b630ce216eaee07052438b0187fd0f25dce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:02 +0000

    Add package docCLI/0.0.1/docCLI-0.0.1.tar.gz

---
+++ packages/docCLI/docCLI-0.0.1.tar.gz/docCLI-0.0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+	name='docCLI',
+	author='Alex Soderman',
+	author_email='asoderman.as@gmail.com',
+	version='0.0.1',
+	install_requires=[
+		'Click',
+		'google-api-python-client'
+		],
+	packages=find_packages(),
+	entry_points='''
+	[console_scripts]
+	docs=docCLI.docs:open_docs
+	drive=docCLI.drive:open_drive
+	sheets=docCLI.sheets:open_sheets
+	'''
+	)
