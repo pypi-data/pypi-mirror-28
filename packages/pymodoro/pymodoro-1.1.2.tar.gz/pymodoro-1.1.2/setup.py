338adb0b34d99e8e5883e216fe6e2f729d1d06ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:05 +0000

    Add package pymodoro/1.1.2/pymodoro-1.1.2.tar.gz

---
+++ packages/pymodoro/pymodoro-1.1.2.tar.gz/pymodoro-1.1.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pymodoro',
+    version='1.1.2',
+    packages=find_packages(),
+    install_requires=['Click', 'tqdm'],
+    entry_points='''
+        [console_scripts]
+        pymodoro=pymodoro:cli
+    ''',
+    author='Roger Almeida',
+    author_email="roger.eduardo@gmail.com",
+    description="It's a command line pomodoro timer",
+    license="MIT",
+    keywords="pomodoro technique timer application cli command line tool",
+    url="https://github.com/rogeralmeida/pomodoro",
+    project_urls={
+        "Bug Tracker": "https://github.com/rogeralmeida/pomodoro/issues",
+        "Documentation": "https://github.com/rogeralmeida/pomodoro",
+        "Source Code": "https://github.com/rogeralmeida/pomodoro",
+    }
+)
