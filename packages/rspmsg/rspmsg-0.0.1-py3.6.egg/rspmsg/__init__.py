0925978a4cfde99c41ae178cb109f1947cebeede
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:04 +0000

    Add package rspmsg/0.0.1/rspmsg-0.0.1-py3.6.egg

---
+++ packages/rspmsg/rspmsg-0.0.1-py3.6.egg/rspmsg/__init__.py
@@ -0,0 +1,16 @@
+from .rspmsg import Message, make_successful_message, make_failed_message, s, f
+
+__author__ = 'darkdarkfruit'
+
+VERSION_TUPLE = (0, 0, 1)
+VERSION_TUPLE_IN_STR = [str(i) for i in VERSION_TUPLE]
+
+
+
+def get_version():
+    return '.'.join(VERSION_TUPLE_IN_STR)
+
+__version__ = get_version()
+
+if __name__ == '__main__':
+    print(__version__)
