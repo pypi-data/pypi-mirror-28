c2618a6ad4f09eed029fde883552405dba0db97c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:14 +0000

    Add package backend.ai-common/1.1.2/backend.ai-common-1.1.2.tar.gz

---
+++ packages/backend.ai-common/backend.ai-common-1.1.2.tar.gz/backend.ai-common-1.1.2/setup.py
@@ -0,0 +1,74 @@
+from setuptools import setup
+from pathlib import Path
+
+requires = [
+    'pyzmq',
+    'aiozmq',
+    'aiohttp~=2.2.0',
+    'aiodns',
+    'async_timeout',
+    'etcd3~=0.7.0',
+    'msgpack-python~=0.4.8',
+    'python-json-logger',
+]
+build_requires = [
+    'wheel',
+    'twine',
+]
+test_requires = [
+    'pytest>=3.1',
+    'pytest-cov',
+    'pytest-asyncio',
+    'pytest-mock',
+    'asynctest',
+    'codecov',
+    'flake8',
+    'aioresponses',
+]
+dev_requires = build_requires + test_requires + [
+    'pytest-sugar',
+]
+ci_requires = []
+monitor_requires = [
+    'datadog>=0.16.0',
+    'raven>=6.1',
+]
+
+
+setup(
+    name='backend.ai-common',
+
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
+    version='1.1.2',
+    description='Backend.AI commons library',
+    long_description=Path('README.rst').read_text(),
+    url='https://github.com/lablup/backend.ai-common',
+    author='Lablup Inc.',
+    author_email='joongi@lablup.com',
+    license='LGPLv3',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Operating System :: POSIX',
+        'Operating System :: MacOS :: MacOS X',
+        'Environment :: No Input/Output (Daemon)',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Software Development',
+    ],
+    packages=['ai.backend.common'],
+    python_requires='>=3.6',
+    install_requires=requires,
+    extras_require={
+        'build': build_requires,
+        'test': test_requires,
+        'dev': dev_requires,
+        'ci': ci_requires,
+        'monitor': monitor_requires,
+    },
+)
