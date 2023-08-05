a86d43b071de2db9f1257441e29e51edfe9ef36c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:26 +0000

    Add package aimmo/v0.1.1-alpha.post.dev3/aimmo-v0.1.1-alpha.post.dev3.tar.gz

---
+++ packages/aimmo/aimmo-v0.1.1-alpha.post.dev3.tar.gz/aimmo-v0.1.1-alpha.post.dev3/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from setuptools import find_packages, setup
+
+import versioneer
+
+setup(
+    name='aimmo',
+    cmdclass=versioneer.get_cmdclass(),
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'django >= 1.8.3, < 1.9.0',
+        'django-autoconfig >= 0.3.6, < 1.0.0',
+        'django-forms-bootstrap',
+        'django-js-reverse',
+        'eventlet',
+        'flask',
+        'flask-socketio',
+        'requests',
+        'six',
+        'pykube',
+        'hypothesis',
+        'flask-cors'
+    ],
+    tests_require=[
+        'django-setuptest',
+        'httmock',
+    ],
+    test_suite='setuptest.setuptest.SetupTestSuite',
+    version=versioneer.get_version(),
+    zip_safe=False,
+)
