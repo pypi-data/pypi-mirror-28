35c4d05ea72f4310425f7ed54cc91dcc2322b878
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:27 +0000

    Add package vali/0.0.1/vali-0.0.1.tar.gz

---
+++ packages/vali/vali-0.0.1.tar.gz/vali-0.0.1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'readme.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='vali',
+    version='0.0.1',
+    description='Easy Type Checking',
+    long_description=long_description,
+    url='https://github.com/llamicron/vali',
+    author='Luke Sweeney',
+    author_email='llamicron@gmail.com',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        # 'Programming Language :: Python :: 2',
+        # 'Programming Language :: Python :: 2.7',
+        # 'Programming Language :: Python :: 3',
+        # 'Programming Language :: Python :: 3.4',
+        # 'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords='validation type-checking type',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),  # Required
+    # install_requires=[],
+)
