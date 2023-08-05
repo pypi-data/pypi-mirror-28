7c78efc44191755a06a41a56f44078e9bc0cd6a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:42 +0000

    Add package xy-news/2.0.0/xy_news-2.0.0.tar.gz

---
+++ packages/xy-news/xy_news-2.0.0.tar.gz/xy_news-2.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+  name = 'xy_news',
+  packages = ['xy_news'],
+  version = '2.0.0',
+  description = 'Get News From juhe API',
+  long_description='Get News From juhe API, Contain All Type News',
+  author = 'KingHS',
+  author_email = '382771946@qq.com',
+  keywords = ['pip3', 'news', 'python3','python'],
+  license='MIT',
+)
