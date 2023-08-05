b2b1737191d27791364005b934635eaf56861b37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:31 +0000

    Add package autogit-client/0.1.3/autogit-client-0.1.3.tar.gz

---
+++ packages/autogit-client/autogit-client-0.1.3.tar.gz/autogit-client-0.1.3/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+
+from setuptools import find_packages
+
+setup(
+    name='autogit-client',
+    packages=find_packages(),
+    scripts=['autogit_daemon/autogit_client.py', 'autogit_daemon/daemon.py', 'autogit_daemon/utils.py'],
+    version='0.1.3',
+    package_data={
+        # If any package contains *.txt or *.rst files, include them:
+        '': ['*.txt', '*.rst'],
+    },
+    description='A Daemon for Autogit Client',
+    author='Vimlabs',
+    author_email='neiell@vimlabs.com',
+    url='https://gitlab.vimlabs.com/vimlabs/autogit.io-daemon',
+    download_url='https://gitlab.vimlabs.com/vimlabs/autogit.io-daemon/repository/master/archive.tar.gz',
+    keywords=['autogit', 'autogit-daemon', 'autogit-client', 'vimlabs', 'auto-deploy', 'deploy automation'],  # arbitrary keywords
+    classifiers=[],
+    entry_points={
+        'console_scripts': [
+            'autogit-client=autogit_daemon.autogit_client:main'
+        ],
+    }
+)
