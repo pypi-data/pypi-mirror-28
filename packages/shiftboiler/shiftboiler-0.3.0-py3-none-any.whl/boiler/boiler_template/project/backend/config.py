e06d310324d67be6c2c758ddddfce58ab38955cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:29 +0000

    Add package shiftboiler/0.3.0/shiftboiler-0.3.0-py3-none-any.whl

---
+++ packages/shiftboiler/shiftboiler-0.3.0-py3-none-any.whl/boiler/boiler_template/project/backend/config.py
@@ -0,0 +1,35 @@
+from boiler.config import default_config as config
+import os
+"""
+Local config
+This file configures installation of this specific environment.
+It extends global application config and should not be committed to repository
+"""
+
+
+class DefaultConfig(config.DefaultConfig):
+    """ Local development config """
+
+    # set this for offline mode
+    SERVER_NAME = None
+    SECRET_KEY = None
+
+    ASSETS_VERSION = 1
+    ASSETS_PATH = '/'
+    FLASK_STATIC_PATH = os.path.realpath(os.getcwd() + '/web')
+
+
+class DevConfig(DefaultConfig, config.DevConfig):
+    """ Local development config """
+    pass
+
+
+class TestingConfig(DefaultConfig, config.TestingConfig):
+    """ Local testing config """
+    pass
+
+
+
+
+
+
---
+++ packages/shiftboiler/shiftboiler-0.3.0-py3-none-any.whl/boiler/feature/error_pages.py
@@ -0,0 +1,27 @@
+from werkzeug import exceptions as e
+from flask import render_template
+
+def error_pages_feature(app):
+    """
+    Error pages feature
+    Adds custom error pages to flask app
+    """
+    # create generic exception handler
+    def error_page(exception):
+        http_exception = isinstance(exception, e.HTTPException)
+        code = exception.code if http_exception else 500
+        template = 'errors/{}.html'.format(code)
+
+        # log exceptions only (app debug should be off)
+        if code == 500:
+            app.logger.error(exception)
+
+        return render_template(template, error=exception), code
+
+    # attach handler to every exception
+    for code in e.default_exceptions.keys():
+        app.register_error_handler(code, error_page)
+
+
+
+
