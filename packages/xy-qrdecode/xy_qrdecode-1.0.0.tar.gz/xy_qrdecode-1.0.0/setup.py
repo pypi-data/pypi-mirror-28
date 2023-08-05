77e6454ab72bbb406699291e3701aaf7c391d8fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:41 +0000

    Add package xy-qrdecode/1.0.0/xy_qrdecode-1.0.0.tar.gz

---
+++ packages/xy-qrdecode/xy_qrdecode-1.0.0.tar.gz/xy_qrdecode-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+  name = 'xy_qrdecode',
+  packages = ['xy_qrdecode'],
+  version = '1.0.0',
+  description = 'Get constellation',
+  long_description='Get constellation Info From juhe API',
+  author = 'KingHS',
+  author_email = '382771946@qq.com',
+  keywords = ['pip3', 'qrdecode', 'python3','python','qrcode'],
+  license='MIT',
+)
