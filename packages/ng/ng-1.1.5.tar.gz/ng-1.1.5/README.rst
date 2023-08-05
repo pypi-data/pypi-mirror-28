23f7463d81f6a7b1f921d07bf75231e4798ae650
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:24 +0000

    Add package ng/1.1.5/ng-1.1.5.tar.gz

---
+++ packages/ng/ng-1.1.5.tar.gz/ng-1.1.5/README.rst
@@ -0,0 +1,65 @@
+ng
+==
+
+.. image:: https://img.shields.io/pypi/l/ng.svg
+    :target: https://pypi.python.org/pypi/ng
+
+.. image:: https://img.shields.io/pypi/v/ng.svg
+    :target: https://pypi.python.org/pypi/ng
+
+.. image:: https://img.shields.io/pypi/pyversions/ng.svg
+    :target: https://pypi.python.org/pypi/ng
+
+.. image:: https://travis-ci.org/cls1991/ng.svg?branch=master
+    :target: https://travis-ci.org/cls1991/ng
+
+Get password of the wifi you're connected, and your current ip address, supports linux, macos, windows.
+
+☤ Quickstart
+------------
+
+Get your ip address:
+
+::
+
+    $ ng ip
+    `
+    local_ip:192.168.1.114
+    public_ip:49.4.160.250
+    `
+
+Get wifi password:
+
+::
+
+    $ ng wp
+    $ ng wp flyfish_5g
+    `
+    flyfish_5g:hitflyfish123456
+    `
+
+☤ Installation
+--------------
+
+You can install "ng" via pip from `PyPI <https://pypi.python.org/pypi/ng>`_:
+
+::
+
+    $ pip install ng
+	
+☤ Usage
+-------
+
+::
+
+    $ ng --help
+    Usage: ng [OPTIONS] COMMAND [ARGS]...
+
+      Get password of the wifi you're connected, and your current ip address.
+
+    Options:
+      --help  Show this message and exit.
+
+    Commands:
+      ip  Show ip address.
+      wp  Show wifi password.
---
+++ packages/ng/ng-1.1.5.tar.gz/ng-1.1.5/setup.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# coding: utf8
+
+import codecs
+import os
+
+from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, 'README.rst'), 'r', encoding='utf8') as f:
+    readme = f.read()
+
+setup(
+    name='ng',
+    version='1.1.5',
+    keywords=['wifi', 'password', 'ip'],
+    description="Get password of the wifi you're connected, and your current ip address.",
+    long_description=readme,
+    author='cls1991',
+    author_email='tzk19910406@hotmail.com',
+    url='https://github.com/cls1991/ng',
+    py_modules=['ng'],
+    install_requires=[
+        'click>=6.7',
+        'requests>=2.18.4',
+        'pytest>=3.3.1'
+    ],
+    license='Apache License 2.0',
+    zip_safe=False,
+    platforms='any',
+    entry_points={
+        'console_scripts': ['ng = ng:cli']
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Operating System :: OS Independent'
+    ]
+)
