fa47e7574f520c8a1349a70661375763494e9130
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:04 +0000

    Add package guake/3.0.0.0b2/Guake-3.0.0.0b2.tar.gz

---
+++ packages/guake/Guake-3.0.0.0b2.tar.gz/Guake-3.0.0.0b2/bootstrap-debian.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Install needed development packages on a Debian/Ubuntu system"
+
+sudo apt install -y \
+    aspell-fr \
+    dconf-editor \
+    gettext \
+    gir1.2-keybinder-3.0 \
+    gsettings-desktop-schemas \
+    libkeybinder-3.0-0 \
+    make \
+    pandoc \
+    python3 \
+    python3-gi \
+    python3-pip \
