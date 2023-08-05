bf1907dc8d47c6a0ef713c520aada32b87b1e032
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:53 +0000

    Add package fleep/0.4.3/fleep-0.4.3.tar.gz

---
+++ packages/fleep/fleep-0.4.3.tar.gz/fleep-0.4.3/setup.py
@@ -0,0 +1,26 @@
+import os
+import setuptools
+
+
+setuptools.setup(
+    name="fleep",
+    version="0.4.3",
+    description="File format determination library",
+    long_description=open(os.path.join(os.path.abspath(os.path.dirname(__file__)), "README.rst"), "r").read(),
+    url="https://github.com/floyernick/fleep",
+    author="Mykyta Paliienko",
+    author_email="floyernick@gmail.com",
+    license="MIT",
+    packages=["fleep"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development",
+    ],
+    python_requires=">=3",
+    include_package_data=True,
+    zip_safe=False
+)
