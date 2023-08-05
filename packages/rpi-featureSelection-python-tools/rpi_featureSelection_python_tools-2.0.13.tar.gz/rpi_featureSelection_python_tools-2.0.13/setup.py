3f4b36241c8d6f152d695887ec06302921845410
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:02 +0000

    Add package rpi-featureSelection-python-tools/2.0.13/rpi_featureSelection_python_tools-2.0.13.tar.gz

---
+++ packages/rpi-featureSelection-python-tools/rpi_featureSelection_python_tools-2.0.13.tar.gz/rpi_featureSelection_python_tools-2.0.13/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+	name='rpi_featureSelection_python_tools',  # This is the name of your PyPI-package. 
+	version= '2.0.13',  # Update the version number for new releases
+	author= 'Keyi Liu',
+	entry_points = {
+		'd3m.primitives': [
+			'rpi_featureSelection_python_tools.IPCMBplus_Selector = rpi_featureSelection_python_tools.feature_selector:IPCMBplus_Selector'
+		],
+	},
+	packages=['rpi_featureSelection_python_tools']
+)
