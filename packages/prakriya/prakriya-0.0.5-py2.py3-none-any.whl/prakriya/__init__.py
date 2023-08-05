a96f1b001e786f5f78bf0f44d4cb79d9834a0b10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:00 +0000

    Add package prakriya/0.0.5/prakriya-0.0.5-py2.py3-none-any.whl

---
+++ packages/prakriya/prakriya-0.0.5-py2.py3-none-any.whl/prakriya/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for prakriya."""
+
+__author__ = """Dr. Dhaval Patel"""
+__email__ = 'drdhaval2785@gmail.com'
+__version__ = '0.0.4'
+__all__ = ['Prakriya', 'Generate']
+
+
+from .verbforms import Prakriya
+from .generate import Generate
