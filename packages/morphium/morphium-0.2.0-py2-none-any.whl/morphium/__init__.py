543a1440258b668cdfc157385555bc46c17327ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:05 +0000

    Add package morphium/0.2.0/morphium-0.2.0-py2-none-any.whl

---
+++ packages/morphium/morphium-0.2.0-py2-none-any.whl/morphium/__init__.py
@@ -0,0 +1,14 @@
+import logging
+import warnings
+from morphium.archive import env, Archive
+from morphium.ia import InternetArchive
+
+warnings.simplefilter("ignore")
+
+fmt = '[%(levelname)s] %(name)s: %(message)s'
+logging.basicConfig(level=logging.INFO, format=fmt)
+logging.getLogger('requests').setLevel(logging.WARNING)
+logging.getLogger('botocore').setLevel(logging.WARNING)
+logging.getLogger('alembic').setLevel(logging.WARNING)
+
+__all__ = [env, Archive, InternetArchive]
