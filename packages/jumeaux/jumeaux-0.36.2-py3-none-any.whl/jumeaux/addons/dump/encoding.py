7d53fd3672f15042637a22b2480e8025b55fa92e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:53 +0000

    Add package jumeaux/0.36.2/jumeaux-0.36.2-py3-none-any.whl

---
+++ packages/jumeaux/jumeaux-0.36.2-py3-none-any.whl/jumeaux/addons/dump/encoding.py
@@ -0,0 +1,26 @@
+# -*- coding:utf-8 -*-
+
+import logging
+
+from owlmixin import OwlMixin
+
+from jumeaux.addons.dump import DumpExecutor
+from jumeaux.models import DumpAddOnPayload
+
+logger = logging.getLogger(__name__)
+
+
+class Config(OwlMixin):
+    encoding: str
+
+
+class Executor(DumpExecutor):
+    def __init__(self, config: dict):
+        self.config: Config = Config.from_dict(config or {})
+
+    def exec(self, payload: DumpAddOnPayload) -> DumpAddOnPayload:
+        return DumpAddOnPayload.from_dict({
+            "response": payload.response,
+            "body": payload.body.decode(payload.encoding.get()).encode(self.config.encoding),
+            "encoding": self.config.encoding
+        })
