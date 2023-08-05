ce6dc798032f34a31540c57eeca6ec4eca250c52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:35 +0000

    Add package ubuntui/0.1.9/ubuntui-0.1.9.tar.gz

---
+++ packages/ubuntui/ubuntui-0.1.9.tar.gz/ubuntui-0.1.9/setup.py
@@ -0,0 +1,19 @@
+"""
+urwid-ubuntu
+============
+
+Ubuntu styled widgets for urwid.
+"""
+
+from setuptools import setup, find_packages
+
+setup(name='ubuntui',
+      version="0.1.9",
+      description="Urwid widgets used in Ubuntu console apps",
+      long_description=__doc__,
+      author="Canonical Solutions Engineering",
+      author_email='ubuntu-dev@lists.ubuntu.com',
+      url="https://github.com/canonicalltd/urwid-ubuntu",
+      license="LGPL-3.0",
+      packages=find_packages(exclude=['test']),
+      install_requires=['urwid'])
