c42f4e4096942d48259a391520b6edd83264d667
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:52 +0000

    Add package zeep/2.5.0/zeep-2.5.0.tar.gz

---
+++ packages/zeep/zeep-2.5.0.tar.gz/zeep-2.5.0/tests/test_loader.py
@@ -0,0 +1,15 @@
+from tests.utils import DummyTransport
+from zeep.loader import parse_xml
+
+
+def test_huge_text():
+    # libxml2>=2.7.3 has XML_MAX_TEXT_LENGTH 10000000 without XML_PARSE_HUGE
+    tree = parse_xml(u"""
+        <s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/">
+         <s:Body>
+          <HugeText xmlns="http://hugetext">%s</HugeText>
+         </s:Body>
+        </s:Envelope>
+    """ % (u'\u00e5' * 10000001), DummyTransport(), xml_huge_tree=True)
+
+    assert tree[0][0].text == u'\u00e5' * 10000001
