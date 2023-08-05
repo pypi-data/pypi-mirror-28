23f34aa0171cdc52ad31cff652204696f62a7f1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:30 +0000

    Add package grokcore.viewlet/3.0.1/grokcore.viewlet-3.0.1.tar.gz

---
+++ packages/grokcore.viewlet/grokcore.viewlet-3.0.1.tar.gz/grokcore.viewlet-3.0.1/setup.py
@@ -0,0 +1,78 @@
+import os
+from setuptools import setup, find_packages
+
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+
+long_description = (
+    read('README.txt')
+    + '\n' +
+    read('CHANGES.txt')
+    )
+
+tests_require = [
+    'zope.annotation',
+    'zope.app.appsetup',
+    'zope.app.publication',
+    'zope.app.wsgi[test]',
+    'zope.configuration',
+    'zope.container',
+    'zope.principalregistry',
+    'zope.securitypolicy',
+    'zope.site',
+    'zope.testbrowser',
+    'zope.testing',
+    'zope.traversing',
+    ]
+
+setup(
+    name='grokcore.viewlet',
+    version='3.0.1',
+    author='Grok Team',
+    author_email='grok-dev@zope.org',
+    url='http://grok.zope.org',
+    download_url='http://cheeseshop.python.org/pypi/grokcore.viewlet',
+    description='Grok-like configuration for zope viewlets',
+    long_description=long_description,
+    license='ZPL',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Zope Public License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Framework :: Zope3',
+        ],
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    namespace_packages=['grokcore'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'grokcore.component >= 2.5',
+        'grokcore.security >= 1.6',
+        'grokcore.view >= 2.7',
+        'martian >= 0.14',
+        'setuptools',
+        'zope.browserpage',
+        'zope.component',
+        'zope.contentprovider',
+        'zope.interface',
+        'zope.login',
+        'zope.publisher',
+        'zope.security',
+        'zope.viewlet',
+        ],
+    tests_require=tests_require,
+    extras_require={'test': tests_require},
+)
---
+++ packages/grokcore.viewlet/grokcore.viewlet-3.0.1.tar.gz/grokcore.viewlet-3.0.1/src/grokcore/viewlet/tests/functional/viewlet/viewlet_interface.py
@@ -0,0 +1,44 @@
+"""
+
+Verify that associating viewlets with an interface instead of with a
+viewlet manager class works as expected.
+
+Set up the model object to view::
+
+  >>> root = getRootFolder()
+  >>> root['cave'] = Cave()
+
+Viewing the cave object should result in the viewlet being displayed,
+as it is associated with the interface::
+
+  >>> from zope.testbrowser.wsgi import Browser
+  >>> browser = Browser()
+  >>> browser.handleErrors = False
+  >>> browser.open("http://localhost/cave")
+  >>> print(browser.contents)
+  Me Fred
+
+
+"""
+
+import grokcore.viewlet as grok
+from zope.interface import Interface, implementer
+
+class ICavemenViewletManager(Interface):
+    pass
+
+@implementer(ICavemenViewletManager)
+class CavemenViewletManager(grok.ViewletManager):
+    grok.name('manage.cavemen')
+
+class FredViewlet(grok.Viewlet):
+    grok.viewletmanager(ICavemenViewletManager)
+
+    def render(self):
+        return u"Me Fred"
+
+class Cave(grok.Context):
+    pass
+
+class Index(grok.View):
+    pass
