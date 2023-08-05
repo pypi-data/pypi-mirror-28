f0f713ec9504a428d3ff2c39ba810c4845c77780
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:14 +0000

    Add package othello-parser/1.0.4/othello_parser-1.0.4.tar.gz

---
+++ packages/othello-parser/othello_parser-1.0.4.tar.gz/othello_parser-1.0.4/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+setup(
+    name='othello_parser',
+    packages=['othello_parser'],
+    version='1.0.4',
+    description='Othello Parser for TJHSST Labs',
+    author='Omkar Kulkarni',
+    author_email='okulkarni@tjhsst.edu',
+    url='https://github.com/ovkulkarni/othello-parser',
+    download_url='https://github.com/ovkulkarni/othello-parser/archive/1.0.4.tar.gz',
+    keywords=['othello', 'argparse', 'tjhsst', 'gabor', 'parser'],
+    classifiers=[],
+)
