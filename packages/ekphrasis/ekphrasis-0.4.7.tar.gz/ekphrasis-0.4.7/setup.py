0bb70b2f8bffd74c46f9c103acd3d7cbf6a4b3da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:17 +0000

    Add package ekphrasis/0.4.7/ekphrasis-0.4.7.tar.gz

---
+++ packages/ekphrasis/ekphrasis-0.4.7.tar.gz/ekphrasis-0.4.7/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(name='ekphrasis',
+      version='0.4.7',
+      description='Text processing tool, geared towards text from '
+                  'social networks, such as Twitter or Facebook. '
+                  'Ekphrasis performs tokenization, word normalization, '
+                  'word segmentation (for splitting hashtags) '
+                  'and spell correction.',
+      url='https://github.com/cbaziotis/ekphrasis',
+      author='Christos Baziotis',
+      author_email='christos.baziotis@gmail.com',
+      license='MIT',
+      packages=find_packages(exclude=['docs', 'tests*', 'analysis']),
+      include_package_data=True
+      )
