44d0920a8fa766d61d3628a9b68d5535376a3d44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:41 +0000

    Add package mabandit/1.3/mabandit-1.3-py3-none-any.whl

---
+++ packages/mabandit/mabandit-1.3-py3-none-any.whl/mabandit/Kernals/RBF.py
@@ -0,0 +1,26 @@
+# Radial bias kernel
+
+import tensorflow as tf
+import numpy as np
+
+#This was a huge bottelneck see why
+def RBF_tf(a, b, zigma = 1, variance_weight = 1, datatype = "float64"):
+    v1 = tf.Variable(a, dtype=tf.float32)
+    v2 = tf.Variable(b, dtype=tf.float32)
+
+    euclidian_dist = tf.reduce_sum(tf.square(tf.subtract(v1,v2)))
+    ans = tf.exp(-0.5*euclidian_dist/zigma)
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        answer = variance_weight*sess.run(ans)
+
+    return answer
+
+def RBF(a, b, zigma=1, variance_weight =1):
+    v1 = np.array(a)
+    v2 = np.array(b)
+
+    euclidian_distance = np.sum(np.square(np.subtract(v1,v2)))
+    ans = np.exp(-0.5 *euclidian_distance/zigma)
+
+    return ans
