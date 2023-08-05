a364fe373d0a4321cac60a094298d8f366773335
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:54 +0000

    Add package nio-cli/0.4.4/nio-cli-0.4.4.tar.gz

---
+++ packages/nio-cli/nio-cli-0.4.4.tar.gz/nio-cli-0.4.4/nio_cli/commands/new.py
@@ -0,0 +1,27 @@
+import subprocess
+
+from .base import Base
+
+
+class New(Base):
+
+    def __init__(self, options, *args, **kwargs):
+        super().__init__(options, *args, **kwargs)
+        self._name = self.options['<project-name>']
+
+    def run(self):
+        clone = (
+            "git clone --depth=1 git://github.com/{}/{}.git {}"
+        ).format('niolabs', 'project_template', self._name)
+        submodule_update = (
+            'cd ./{} '
+            '&& git submodule update --init --recursive'
+        ).format(self._name)
+        reinit_repo = (
+            'cd ./{} '
+            '&& git remote remove origin '
+            '&& git commit --amend --reset-author -m "Initial commit"'
+        ).format(self._name)
+        subprocess.call(clone, shell=True)
+        subprocess.call(submodule_update, shell=True)
+        subprocess.call(reinit_repo, shell=True)
