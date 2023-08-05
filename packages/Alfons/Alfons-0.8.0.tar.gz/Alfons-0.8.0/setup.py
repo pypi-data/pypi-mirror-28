94d1319b2577d0b5f2dcb603fbcef39d17117af4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:56 +0000

    Add package Alfons/0.8.0/Alfons-0.8.0.tar.gz

---
+++ packages/Alfons/Alfons-0.8.0.tar.gz/Alfons-0.8.0/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name = "Alfons",
+  packages = ["Alfons"],
+  version = "0.8.0",
+  description = "A library for using the Alfons system",
+  author = "Anton Lindroth",
+  author_email = "ntoonio@gmail.com",
+  url = "https://github.com/ntoonio/Alfons-Python/",
+  download_url = "https://github.com/ntoonio/Alfons-Python/archive/0.6.tar.gz",
+  keywords = ["home", "automation", "iot"],
+  classifiers = [],
+)
