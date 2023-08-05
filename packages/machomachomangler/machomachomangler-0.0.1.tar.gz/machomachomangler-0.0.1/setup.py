a36eb628bbebb790b5e22bb7fb35562d236abffb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:34 +0000

    Add package machomachomangler/0.0.1/machomachomangler-0.0.1.tar.gz

---
+++ packages/machomachomangler/machomachomangler-0.0.1.tar.gz/machomachomangler-0.0.1/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+from machomachomangler import __version__
+
+setup(
+    name="machomachomangler",
+    version='0.0.1',
+    description=
+        "Tools for mangling Mach-O and PE binaries",
+    long_description=open("README.rst").read(),
+    author="Nathaniel J. Smith",
+    author_email="njs@pobox.com",
+    license="MIT",
+    packages=find_packages(),
+    install_requires=[
+        "attrs",
+    ],
+    url="https://github.com/njsmith/machomachomangler",
+    # This means, just install *everything* you see under the package
+    # directory, even if it doesn't look like a source file, so long as it
+    # appears in MANIFEST.in:
+    include_package_data=True,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Operating System :: Microsoft",
+        "Operating System :: MacOS :: MacOS X",
+        "Topic :: Software Development :: Build Tools",
+        "Topic :: Software Development :: Compilers",
+        ],
+)
