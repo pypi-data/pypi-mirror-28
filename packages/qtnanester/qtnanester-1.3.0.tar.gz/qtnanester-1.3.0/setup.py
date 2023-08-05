47119da87049bd29c0be4474e81b5b0f111239e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:22 +0000

    Add package qtnanester/1.3.0/qtnanester-1.3.0.tar.gz

---
+++ packages/qtnanester/qtnanester-1.3.0.tar.gz/qtnanester-1.3.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name='qtnanester',
+    version='1.3.0',
+    py_modules=['qtnanester'],
+    author='hfpython',
+    author_email='hfpython@headfirst.com',
+    url='http://www.headfirstlabs.com',
+    description='A simple printer ',
+    )
