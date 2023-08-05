739ca5df494bf84fc06c93299f15105c0ed62d94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:21 +0000

    Add package grokcore.error/3.0.0/grokcore.error-3.0.0.tar.gz

---
+++ packages/grokcore.error/grokcore.error-3.0.0.tar.gz/grokcore.error-3.0.0/setup.py
@@ -0,0 +1,62 @@
+from setuptools import setup, find_packages
+import os
+
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+long_description = (
+    read('README.txt')
+    + '\n' +
+    read('CHANGES.txt')
+    )
+
+tests_require = [
+    'zope.testing',
+    'mock',
+    ]
+
+setup(
+    name='grokcore.error',
+    version='3.0.0',
+    author='Grok Team',
+    author_email='grok-dev@zope.org',
+    url='http://grok.zope.org',
+    download_url='http://pypi.python.org/pypi/grokcore.startup',
+    description='Paster support for Grok projects.',
+    long_description=long_description,
+    license='ZPL',
+    keywords='zope zope3 grok grokproject WSGI Paste paster',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Zope Public License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Framework :: Zope3',
+        ],    
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    namespace_packages=['grokcore'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'grokcore.component',
+        'setuptools',
+        'zope.component',
+        'zope.configuration',
+        'zope.error',
+        'zope.interface',
+        'zope.publisher',
+        ],
+    tests_require=tests_require,
+    extras_require={'test': tests_require},
+)
