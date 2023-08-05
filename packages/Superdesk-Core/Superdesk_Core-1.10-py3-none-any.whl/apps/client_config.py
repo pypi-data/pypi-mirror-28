e263f561b13f1a692026b948abeb2592a74c1307
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:45 +0000

    Add package Superdesk-Core/1.10/Superdesk_Core-1.10-py3-none-any.whl

---
+++ packages/Superdesk-Core/Superdesk_Core-1.10-py3-none-any.whl/apps/client_config.py
@@ -0,0 +1,29 @@
+
+import superdesk
+
+from flask import current_app as app
+from superdesk.utils import ListCursor
+
+
+class ClientConfigResource(superdesk.Resource):
+    item_methods = []
+    public_methods = ['GET']
+    resource_methods = ['GET']
+
+
+class ClientConfigService(superdesk.Service):
+
+    def get(self, req, lookup):
+        return ListCursor()
+
+    def on_fetched(self, docs):
+        docs['config'] = getattr(app, 'client_config', {})
+
+
+def init_app(app):
+    superdesk.register_resource('client_config', ClientConfigResource, ClientConfigService, _app=app)
+    app.client_config.update({
+        'schema': app.config.get('SCHEMA'),
+        'editor': app.config.get('EDITOR'),
+        'feedback_url': app.config.get('FEEDBACK_URL'),
+    })
