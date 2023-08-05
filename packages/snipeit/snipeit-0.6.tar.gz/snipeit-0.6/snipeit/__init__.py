d8241efab26547e4e1fd8b3a458e00c7e9d49460
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:31 +0000

    Add package snipeit/0.6/snipeit-0.6.tar.gz

---
+++ packages/snipeit/snipeit-0.6.tar.gz/snipeit-0.6/snipeit/__init__.py
@@ -0,0 +1,6 @@
+from Assets import Assets
+from Company import Company
+from Locations import Locations
+from Accessories import Accessories
+from Consumables import Consumables
+from Components import Components
