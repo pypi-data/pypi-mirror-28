4cee880e6906440756657695d672f0b9c27dcf8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:36 +0000

    Add package django-formidable/1.2.1/django_formidable-1.2.1-py2-none-any.whl

---
+++ packages/django-formidable/django_formidable-1.2.1-py2-none-any.whl/formidable/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import
+
+from .json_migrations import latest_version
+
+
+default_app_config = 'formidable.app.FormidableConfig'
+version = '1.2.1'
+json_version = latest_version
