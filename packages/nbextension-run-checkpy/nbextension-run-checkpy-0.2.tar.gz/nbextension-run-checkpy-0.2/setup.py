f1f0d402eacfa89880d12a98cbc5e3b93aa73de2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:35 +0000

    Add package nbextension-run-checkpy/0.2/nbextension-run-checkpy-0.2.tar.gz

---
+++ packages/nbextension-run-checkpy/nbextension-run-checkpy-0.2.tar.gz/nbextension-run-checkpy-0.2/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+try:
+    from jupyterpip import cmdclass
+except:
+    import pip, importlib
+    pip.main(['install', 'jupyter-pip'])
+    cmdclass = importlib.import_module('jupyterpip').cmdclass
+
+VERSION = '0.2'
+
+setup(
+    name='nbextension-run-checkpy',
+    version=VERSION,
+    description='An extension of the Jupyter Notebook that runs checkPy.',
+    author='Tom Kooij',
+    author_email='tom.kooij@gmail.com',
+    license='BSD',
+    classifiers=[
+        'Intended Audience :: Education',
+        'Topic :: Education :: Testing',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python'],
+    url='https://github.com/tomkooij/nbextension-run-checkpy',
+    package=['run-checkpy'],
+    cmdclass=cmdclass('run-checkpy', 'run-checkpy/run-checkpy'),
+    install_requires=['jupyter-pip'],
+)
