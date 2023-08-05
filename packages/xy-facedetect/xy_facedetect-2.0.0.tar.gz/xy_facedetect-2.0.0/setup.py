449236a138327421982432a36e406f21f59034db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:32 +0000

    Add package xy-facedetect/2.0.0/xy_facedetect-2.0.0.tar.gz

---
+++ packages/xy-facedetect/xy_facedetect-2.0.0.tar.gz/xy_facedetect-2.0.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'xy_facedetect',
+  packages = ['xy_facedetect'],
+  version = '2.0.0',
+  description = 'Get The Face Detect',
+  long_description='Get The Face Detect',
+  author = 'KingHS',
+  author_email = '382771946@qq.com',
+  keywords = ['pip3', 'python3','python','face detect','face','detect'],
+  license='MIT',
+  install_requires=['opencv-python']
+)
