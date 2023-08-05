274c8e41ba9ca8c524d1539d7276a5bef29dd4f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:38 +0000

    Add package heizidafirst/1.0.0/heizidafirst-1.0.0.tar.gz

---
+++ packages/heizidafirst/heizidafirst-1.0.0.tar.gz/heizidafirst-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils. core import setup
+
+
+setup(
+        name         = 'heizidafirst',
+        version      = '1.0.0',
+        py_modules   = ['nester'],
+        author       = '0lddriver',
+        author_email = 'heizida@weison86.com',
+        url          = 'http://www.0lddriver.com',
+        description  = 'A simple printer of nested lists',
+      )
