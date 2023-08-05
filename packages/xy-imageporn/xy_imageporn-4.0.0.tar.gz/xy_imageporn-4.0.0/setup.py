35fba38da28683894d440f6b407d9a571617cdd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:38 +0000

    Add package xy-imageporn/4.0.0/xy_imageporn-4.0.0.tar.gz

---
+++ packages/xy-imageporn/xy_imageporn-4.0.0.tar.gz/xy_imageporn-4.0.0/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+setup(
+  name = 'xy_imageporn',
+  packages = ['xy_imageporn'],
+  version = '4.0.0',
+  description = 'Image Porn Recognition',
+  long_description='Image Porn Recognition',
+  author = 'KingHS',
+  author_email = '382771946@qq.com',
+  keywords = ['pip3', 'imageporn', 'python3','python','Image Pron Recognition'],
+  license='MIT',
+  install_requires=[
+        'qcloud_image'
+    ],
+)
---
+++ packages/xy-imageporn/xy_imageporn-4.0.0.tar.gz/xy_imageporn-4.0.0/xy_imageporn/xy_imageporn.py
@@ -0,0 +1,24 @@
+from qcloud_image import Client
+from qcloud_image import CIUrl, CIFile, CIBuffer, CIUrls, CIFiles, CIBuffers
+
+appid = '1251185167'
+secret_id = 'AKIDiFYdT5XeaIl04utt2waz87sOfiY9ucV9'
+secret_key = 'XrwmMgkY6OQFvMYVEixxiDSRAdqnUw84'
+bucket = 'ocr'
+client = Client(appid, secret_id, secret_key, bucket)
+client.use_http()
+client.set_timeout(30)
+'''
+    {'result_list': [{'code': 0, 'message': 'success', 'filename': './king.jpg', 'data': {'result': 0, 'forbid_status': 0, 'confidence': 3.755, 'hot_score': 11.973, 'normal_score': 88.027, 'porn_score': 0.0}}], 'httpcode': 200}
+'''
+def porn_info(filename=''):
+    if filename == '':
+        return -1
+    info = client.porn_detect(CIFiles([filename,]))
+
+    res = info['result_list']
+    if res[0]['code'] == 0:
+        del res[0]['data']['forbid_status']
+        return res[0]['data']
+    else:
+        return -1
