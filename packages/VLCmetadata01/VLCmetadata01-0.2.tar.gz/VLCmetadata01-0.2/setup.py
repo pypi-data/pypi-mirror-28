92dce1ce9ded02979db9bf0ff4f0937b63b26c60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:31 +0000

    Add package VLCmetadata01/0.2/VLCmetadata01-0.2.tar.gz

---
+++ packages/VLCmetadata01/VLCmetadata01-0.2.tar.gz/VLCmetadata01-0.2/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+from setuptools import setup
+setup(
+    name='VLCmetadata01',
+    version='0.2',
+    packages=['testF',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.txt').read(),
+    author='Martin Barker',
+    author_email='martinbarker99@gmail.com',
+    url='https://github.com/MartinBarker/VLC-Radio',
+
+
+)
