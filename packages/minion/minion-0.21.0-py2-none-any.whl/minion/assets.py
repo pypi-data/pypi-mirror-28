067f604a6bd558cabcb2b4a4c4edfb20ce1390e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:26 +0000

    Add package minion/0.21.0/minion-0.21.0-py2-none-any.whl

---
+++ packages/minion/minion-0.21.0-py2-none-any.whl/minion/assets.py
@@ -0,0 +1,33 @@
+from pyrsistent import m
+import attr
+
+
+class DuplicateAsset(Exception):
+    pass
+
+
+class NoSuchAsset(LookupError):
+    pass
+
+
+@attr.s(hash=True, repr=False)
+class Bin(object):
+
+    _assets = attr.ib(default=m())
+
+    def __repr__(self):
+        return "<{}: {{{}}}>".format(
+            self.__class__.__name__,
+            ", ".join(repr(asset) for asset in sorted(self._assets)),
+        )
+
+    def add(self, **assets):
+        return attr.evolve(self, assets=self._assets.update(assets))
+
+    def provide(self, name):
+        try:
+            provider = self._assets[name]
+        except KeyError:
+            raise NoSuchAsset(name)
+        else:
+            return provider(bin=self)
