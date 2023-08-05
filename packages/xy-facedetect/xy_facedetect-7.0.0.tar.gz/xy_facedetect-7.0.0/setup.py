ca5eb1bd6857eba392d8fa2db1f18a7b87639dc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:41 +0000

    Add package xy-facedetect/7.0.0/xy_facedetect-7.0.0.tar.gz

---
+++ packages/xy-facedetect/xy_facedetect-7.0.0.tar.gz/xy_facedetect-7.0.0/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+setup(
+  name = 'xy_facedetect',
+  packages = ['xy_facedetect'],
+  package_data = {'xy_facedetect':['data/*']},
+  # package_dir = {'':'xy_facedetect'},
+  data_files = [('haarcascade_frontalface_default.xml')],
+  # include_package_data = True,
+  version = '7.0.0',
+  description = 'Get The Face Detect',
+  long_description='Get The Face Detect',
+  author = 'KingHS',
+  author_email = '382771946@qq.com',
+  keywords = ['pip3', 'python3','python','face detect','face','detect'],
+  license = 'MIT',
+  install_requires = ['opencv-python']
+)
