0d12508da163fbfc0211952820b0847f18f047f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:54 +0000

    Add package michal-314/1.0.0/michal_314-1.0.0.zip

---
+++ packages/michal-314/michal_314-1.0.0.zip/michal_314-1.0.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name         = "michal_314",
+    version      = "1.0.0",
+    py_modules   = ["michal_314"],
+    author       = "rocky_angela",
+    author_email = "michal314@163.com",
+    url          = "http://www.163.com",
+    description  = "A simple printer of michal_314 lists",
+    )
+    
+    
