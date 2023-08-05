a17e548b06ef7edec95d77428c3e3f073efe78b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:58 +0000

    Add package otree-core/0.0.0b1/otree-core-0.0.0b1.tar.gz

---
+++ packages/otree-core/otree-core-0.0.0b1.tar.gz/otree-core-0.0.0b1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+	name="otree-core",
+	version='0.0.0b1',
+	description="",
+    url='http://www.otree.org',
+	author='Chris',
+	author_email='chris@otree.org',
+	license='MIT',
+	packages=find_packages(),
+	zip_safe=False,
+)
