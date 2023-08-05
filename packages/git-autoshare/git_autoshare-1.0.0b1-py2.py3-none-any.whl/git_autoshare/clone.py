bca1d03383ab4b33031bde36f85277b0f08695d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:43 +0000

    Add package git-autoshare/1.0.0b1/git_autoshare-1.0.0b1-py2.py3-none-any.whl

---
+++ packages/git-autoshare/git_autoshare-1.0.0b1-py2.py3-none-any.whl/git_autoshare/clone.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+# Copyright © 2017 ACSONE SA/NV
+# License GPLv3 (http://www.gnu.org/licenses/gpl-3.0-standalone.html)
+
+from __future__ import print_function
+
+import os
+import subprocess
+import sys
+
+from .core import git_bin, prefetch_one, shared_urls
+
+
+def main():
+    cmd = [git_bin(), 'clone'] + sys.argv[1:]
+    skip = \
+        '--reference' in cmd or \
+        '--reference-if-able' in cmd or \
+        '-s' in cmd or \
+        '--share' in cmd
+    if not skip:
+        quiet = '-q' in cmd or '--quiet' in cmd
+        found = False
+        for repo_url, host, org, repo, repo_dir, private in shared_urls():
+            for i, arg in enumerate(cmd):
+                if arg.lower() == repo_url:
+                    found = True
+                    break
+            if found:
+                break
+        if found:
+            if not os.path.exists(repo_dir):
+                prefetch_one(host, [org], repo, repo_dir, private, quiet)
+            if not quiet:
+                print("git-autoshare clone added --reference", repo_dir)
+            cmd = cmd[:i] + ['--reference', repo_dir] + cmd[i:]
+    r = subprocess.call(cmd)
+    sys.exit(r)
---
+++ packages/git-autoshare/git_autoshare-1.0.0b1-py2.py3-none-any.whl/git_autoshare/prefetch.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# Copyright © 2017 ACSONE SA/NV
+# License GPLv3 (http://www.gnu.org/licenses/gpl-3.0-standalone.html)
+
+from __future__ import print_function
+
+import sys
+
+import click
+
+from .core import prefetch_all, prefetch_one, shared_urls
+
+
+@click.command()
+@click.option('--quiet', '-q', is_flag=True, default=False)
+@click.argument('repositories', metavar='<repository> ...', nargs=-1)
+def main(repositories, quiet):
+    exit = 0
+    if not repositories:
+        prefetch_all(quiet)
+    else:
+        for repository in repositories:
+            repository == repository.lower()
+            for repo_url, host, org, repo, repo_dir, private in shared_urls():
+                if repository == repo_url:
+                    prefetch_one(host, [org], repo, repo_dir, private, quiet)
+                    break
+            else:
+                print(repository, 'not found in repos.yml, not prefetched.')
+                exit += 1
+    sys.exit(exit)
