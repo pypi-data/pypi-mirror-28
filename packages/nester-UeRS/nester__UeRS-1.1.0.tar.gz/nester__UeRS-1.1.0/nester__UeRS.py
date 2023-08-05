c277953a4ff5265a843b2bcb364e65de6e6e74f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:14 +0000

    Add package nester-UeRS/1.1.0/nester__UeRS-1.1.0.tar.gz

---
+++ packages/nester-UeRS/nester__UeRS-1.1.0.tar.gz/nester__UeRS-1.1.0/nester__UeRS.py
@@ -0,0 +1,12 @@
+"""nester__UeRS.py: processa listas aninhadas"""
+def print_lol(the_list, level):
+  """Imprime os itens de uma lista na tela, inclusive os itens aninhados.
+  the_list: lista a ser impressa de forma recursiva."""
+  for each_item in the_list:
+    if isinstance(each_item, list):
+      print_lol(each_item, level+1)
+    else:
+      for num in range(level):
+        print("\t", end ='')
+        
+      print(each_item)
