02fa3327d5ad5459bb410dfe1502bbb26dab096c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:22 +0000

    Add package biwrap/0.1.5/biwrap-0.1.5.tar.gz

---
+++ packages/biwrap/biwrap-0.1.5.tar.gz/biwrap-0.1.5/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+from os.path import realpath, dirname, join
+from setuptools import setup, find_packages
+
+DISTNAME = 'biwrap'
+AUTHOR = 'Maxim Kochurov'
+AUTHOR_EMAIL = 'maxim.v.kochurov@gmail.com'
+VERSION = '0.1.5'
+PROJECT_ROOT = dirname(realpath(__file__))
+try:
+    LONG_DESCRIPTION = open(join(PROJECT_ROOT, 'README.rst')).read()
+except IOError:
+    LONG_DESCRIPTION = ''
+LICENSE = 'MIT License'
+
+if __name__ == "__main__":
+    setup(
+        author=AUTHOR,
+        author_email=AUTHOR_EMAIL,
+        description='Yet simple util to make wrapper with optional arguments',
+        long_description=LONG_DESCRIPTION,
+        name=DISTNAME,
+        version=VERSION,
+        license=LICENSE,
+        packages=find_packages(),
+        url='https://github.com/ferrine/biwrap',
+        download_url='https://github.com/ferrine/biwrap/archive/v0.1.5.zip'
+    )
