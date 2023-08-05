4636e750a4c3ca0c00fa36c54c1d3ff5beaaf082
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:38 +0000

    Add package angel_fs/1.0.1/angel_fs-1.0.1.tar.gz

---
+++ packages/angel_fs/angel_fs-1.0.1.tar.gz/angel_fs-1.0.1/angel_fs/utils/__init__.py
@@ -0,0 +1,6 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time         2018-01-12
+# @Email        2273337844@qq.com
+# @Copyright    © 2017 Lafite93
+
---
+++ packages/angel_fs/angel_fs-1.0.1.tar.gz/angel_fs-1.0.1/scripts/fs_daemon.py
@@ -0,0 +1,54 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time         2018-01-10
+# @Email        2273337844@qq.com
+# @Copyright    © 2017 Lafite93
+
+import os
+import sys
+import daemonize
+
+BASE_PATH = "/tmp/angel/fs"
+
+HELP = """
+This executable script is used to start/stop a angel_fs process as a daemon process.
+This requires specifying a pid file which is used to interact with the process.
+Usage examples:
+    {0} start
+    {0} stop
+    {0} restart
+    {0} status
+    {0} help
+\n
+"""
+
+pidfile = os.path.join(BASE_PATH, ".pid")
+stdout = os.path.join(BASE_PATH, "start.out")
+stderr = os.path.join(BASE_PATH, "start.err")
+
+def serve():
+    from angel_fs import filesystem
+    filesystem.serve_forever()
+
+def main():
+    if len(sys.argv) != 2:
+        sys.stderr.write("command error, more information:\n" + HELP)
+        exit(1)
+    if not os.path.isdir(BASE_PATH):
+        os.makedirs(BASE_PATH)
+    action = sys.argv[1]
+    if action == "start":
+        daemonize.start(serve, pidfile, stdout=stdout, stderr=stderr)
+    elif action == "stop":
+        daemonize.stop(pidfile)
+    elif action == "status":
+        daemonize.status(pidfile)
+    elif action == "restart":
+        daemonize.restart(serve, pidfile, stdout=stdout, stderr=stderr)
+    elif action == "help":
+        sys.stderr.write(HELP)
+    else:
+        sys.stderr.write("command error, more information:\n" + HELP)
+
+if __name__ == "__main__":
+    main()
