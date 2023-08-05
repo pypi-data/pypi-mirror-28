586d539f9990d5f829ae2ca7e530345b1f159488
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:16 +0000

    Add package tfsom/0.3/tfsom-0.3.tar.gz

---
+++ packages/tfsom/tfsom-0.3.tar.gz/tfsom-0.3/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+setup(name='tfsom',
+      packages=['tfsom'],
+      version='0.3',
+      description='A Self-organizing map implementation using the tensorflow library',
+      author='Dana Jomar, Marco Jungwirth, Jacco Oosterhuis',
+      author_email='marcojungwirth@hotmail.com',
+      url='https://pypi.python.org/pypi/tfsom',
+      classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Scientific/Engineering :: Visualization']
+      )
