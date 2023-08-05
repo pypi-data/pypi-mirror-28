d891bc867bd2bb0e11e57d7dc90fc92d9bb7361e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:02 +0000

    Add package pyneapple/0.2.1/pyneapple-0.2.1.tar.gz

---
+++ packages/pyneapple/pyneapple-0.2.1.tar.gz/pyneapple-0.2.1/pyneapple/__main__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import sys
+from pyneapple import Pyneapple
+
+if __name__ == '__main__':
+    if '__spec__' not in locals():
+        __spec__ = None #strange Windows workaround
+    p = Pyneapple()
+    p.run(sys.argv)
