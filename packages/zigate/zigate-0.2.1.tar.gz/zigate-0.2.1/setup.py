995aa054d8b3246aceb24d4b270d432184fe9f6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:28 +0000

    Add package zigate/0.2.1/zigate-0.2.1.tar.gz

---
+++ packages/zigate/zigate-0.2.1.tar.gz/zigate-0.2.1/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+import zigate
+
+setup(
+    name='zigate',
+    version=zigate.__version__,
+    description='python library for the zigate gateway',
+    long_description=open('README.md').read(),
+    author='Sébastien RAMAGE',
+    author_email='sebastien.ramage@gmail.com',
+    url='https://github.com/doudz/zigate',
+    packages=['zigate'],
+    keywords='zigate zigbee python3',
+    install_requires=['pyserial', 'pyudev'],
+    python_requires='>=3',
+    classifiers=(
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3.5',
+    ),
+)
