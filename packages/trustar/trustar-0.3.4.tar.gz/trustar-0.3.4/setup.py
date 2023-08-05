de76921f822067f7d843102bcc4c0b2215d7704e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:51 +0000

    Add package trustar/0.3.4/trustar-0.3.4.tar.gz

---
+++ packages/trustar/trustar-0.3.4.tar.gz/trustar-0.3.4/setup.py
@@ -0,0 +1,31 @@
+# git tag 0.3.4 -m "Bump version" && git push --tags origin master
+# python setup.py sdist
+# twine upload --skip-existing dist/*
+
+from distutils.core import setup
+
+version = '0.3.4'
+
+setup(
+    name='trustar',
+    packages=['trustar', 'trustar.models'],
+    version=version,
+    author='TruSTAR Technology, Inc.',
+    author_email='support@trustar.co',
+    url='https://github.com/trustar/trustar-python',
+    download_url='https://github.com/trustar/trustar-python/tarball/%s' % version,
+    description='Python SDK for the TruSTAR REST API',
+    license='MIT',
+    install_requires=['future',
+                      'python-dateutil',
+                      'pytz',
+                      'requests',
+                      'configparser',
+                      'unicodecsv',
+                      'tzlocal',
+                      'PyYAML',
+                      'six'
+                      ],
+
+    use_2to3=True
+)
---
+++ packages/trustar/trustar-0.3.4.tar.gz/trustar-0.3.4/trustar/__init__.py
@@ -0,0 +1,5 @@
+from __future__ import absolute_import
+
+from .trustar import TruStar
+from .models import *
+from .utils import *
