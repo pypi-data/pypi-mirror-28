e1af63e46de4a675307263e237d6476268bd0cd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:56 +0000

    Add package denny0515/1.0.0/denny0515-1.0.0.tar.gz

---
+++ packages/denny0515/denny0515-1.0.0.tar.gz/denny0515-1.0.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+
+setup(
+    name='denny0515',
+    version='1.0.0',
+    py_modules=['denny0515'],
+    author='DennyHan',
+    author_email='denny760515@gmail.com',
+    url='http://www.headfirstlabs.com',
+    description='A simple printer of nested lists',
+    )
