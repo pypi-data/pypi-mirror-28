abb273e5f09815281a741f7c1e2f1f61e8b33840
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:03:45 +0000

    Add package d3metafeatureextraction/0.1.6/d3metafeatureextraction-0.1.6.tar.gz

---
+++ packages/d3metafeatureextraction/d3metafeatureextraction-0.1.6.tar.gz/d3metafeatureextraction-0.1.6/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+from d3metafeatureextraction import __version__
+
+setup(
+    name="d3metafeatureextraction",
+    packages = find_packages(exclude=("data",)),
+    version = __version__,
+    description = "A DARPA D3M wrapper for extracting dataset metafeatures using the byu-dml metalearn package.",
+    author = "Roland Laboulaye, Brandon Schoenfeld",
+    author_email = "rlaboulaye@gmail.com, bjschoenfeld@gmail.com",
+    url = "https://github.com/byu-dml/d3metafeatureextraction",
+    keywords = ["metalearning", "metafeature", "machine learning", "metalearn"],
+    install_requires = [
+        "metalearn==0.2.0",
+        "numpy",
+        "pandas"
+    ],
+    dependency_links = [
+        "https://gitlab.com/datadrivendiscovery/metadata.git",
+        "https://gitlab.com/datadrivendiscovery/primitive-interfaces.git"
+    ],
+    entry_points = {
+        'd3m.primitives' : [
+            'd3metafeatureextraction = d3metafeatureextraction:D3MetafeatureExtraction',
+        ],
+    },
+    download_url = "https://github.com/byu-dml/d3metafeatureextraction/archive/{}.tar.gz".format(__version__)
+)
