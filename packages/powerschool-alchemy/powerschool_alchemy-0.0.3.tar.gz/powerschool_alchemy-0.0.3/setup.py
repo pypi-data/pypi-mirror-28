e5aeff4492a9783ba41c0bf6bf5dff6fba27ebf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:30 +0000

    Add package powerschool-alchemy/0.0.3/powerschool_alchemy-0.0.3.tar.gz

---
+++ packages/powerschool-alchemy/powerschool_alchemy-0.0.3.tar.gz/powerschool_alchemy-0.0.3/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name='powerschool_alchemy',
+      version='0.0.3',
+      description='This library allows you to interact with a PowerSchool Database with SQLALchemy, a Python ORM',
+      url='https://github.com/IronCountySchoolDistrict/powerschool-alchemy',
+      author='Iron County School District',
+      author_email='data@ironmail.org',
+      license='MIT',
+      packages=['powerschool_alchemy'],
+      install_requires=[
+          'python-dotenv',
+          'SQLAlchemy',
+          'cx-oracle',
+      ],
+      zip_safe=False)
