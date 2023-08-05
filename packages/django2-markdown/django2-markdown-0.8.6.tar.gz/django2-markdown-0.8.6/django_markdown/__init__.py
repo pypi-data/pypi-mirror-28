dab8311b14afead841b17183104a70e8a19b7f08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:34 +0000

    Add package django2-markdown/0.8.6/django2-markdown-0.8.6.tar.gz

---
+++ packages/django2-markdown/django2-markdown-0.8.6.tar.gz/django2-markdown-0.8.6/django_markdown/__init__.py
@@ -0,0 +1,6 @@
+""" Django-Markdown supports markdown in Django. """
+
+__version__ = "0.8.6"
+__project__ = "django2-markdown"
+__author__ = "Tomas Garzon <tomasgarzonhervas@gmail.com>"
+__license__ = "GNU LGPL"
