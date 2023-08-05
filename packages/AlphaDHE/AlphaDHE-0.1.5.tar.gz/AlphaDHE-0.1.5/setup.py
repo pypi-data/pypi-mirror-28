4eb1817c1c5a2a3f796321c8983eb78e33f03b08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:05 +0000

    Add package AlphaDHE/0.1.5/AlphaDHE-0.1.5.tar.gz

---
+++ packages/AlphaDHE/AlphaDHE-0.1.5.tar.gz/AlphaDHE-0.1.5/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+setup(
+  name = 'AlphaDHE',
+  packages = ['AlphaDHE'],
+  version = '0.1.5',
+  description = 'DiffieHellman Ephemeral for A-Z ciphers',
+  author = 'Karl Zander',
+  author_email = 'pvial@kryptomagik.com',
+  url = 'https://github.com/pvial00/MASH',
+  keywords = ['cryptography', 'encryption', 'security'],
+  classifiers = [],
+  install_requires = [
+      'MASHHASH',
+      'pycrypto',
+      ],
+)
