fae764e6a4575ddc4b566bd493401c7eefde7a25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:31 +0000

    Add package measurement/2.0.1/measurement-2.0.1.tar.gz

---
+++ packages/measurement/measurement-2.0.1.tar.gz/measurement-2.0.1/tests/test_utils.py
@@ -0,0 +1,32 @@
+from .base import MeasurementTestBase
+
+
+from measurement.measures import Mass, Distance, Temperature
+from measurement.utils import guess
+
+
+class UtilsTest(MeasurementTestBase):
+
+    def test_guess_weight(self):
+        result = guess(23, 'g')
+
+        self.assertEqual(
+            result,
+            Mass(g=23)
+        )
+
+    def test_guess_distance(self):
+        result = guess(30, 'mi')
+
+        self.assertEqual(
+            result,
+            Distance(mi=30)
+        )
+
+    def test_guess_temperature(self):
+        result = guess(98, 'f')
+
+        self.assertEqual(
+            result,
+            Temperature(f=98)
+        )
