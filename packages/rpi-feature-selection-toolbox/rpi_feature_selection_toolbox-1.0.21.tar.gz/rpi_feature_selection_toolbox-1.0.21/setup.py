b71bd7ef2439d248c66f88c703c793ce88c4b928
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:18 +0000

    Add package rpi-feature-selection-toolbox/1.0.21/rpi_feature_selection_toolbox-1.0.21.tar.gz

---
+++ packages/rpi-feature-selection-toolbox/rpi_feature_selection_toolbox-1.0.21.tar.gz/rpi_feature_selection_toolbox-1.0.21/setup.py
@@ -0,0 +1,35 @@
+# Copyright 2015-2017 The MathWorks, Inc.
+
+from setuptools import setup
+
+# from distutils.command.clean import clean
+# from distutils.command.install import install
+
+# class InstallRuntime(install):
+#     # Calls the default run command, then deletes the build area 
+#     # (equivalent to "setup clean --all").
+#     def run(self):
+#         install.run(self)
+#         c = clean(self.distribution)
+#         c.all = True
+#         c.finalize_options()
+#         c.run()
+
+if __name__ == '__main__':
+
+    setup(
+        name="rpi_feature_selection_toolbox",
+        version="1.0.21",
+        description='test matlab runtime with LD_LIBRARY_PATH turned off',
+        author='Keyi Liu, Zijun Cui, Qiang Ji',
+        url='https://www.ecse.rpi.edu/~cvrl/',
+        platforms=['Linux', 'Windows', 'MacOS'],
+        packages=[
+            'rpi_feature_selection_toolbox'
+        ],
+        package_data={'rpi_feature_selection_toolbox': ['*.ctf']},
+        # Executes the custom code above in order to delete the build area.
+        # cmdclass={'install': InstallRuntime}
+    )
+
+
