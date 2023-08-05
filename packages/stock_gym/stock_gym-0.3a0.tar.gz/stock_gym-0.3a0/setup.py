ae95be7e57a119a16af17f6e8f08a958b2f61f1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:06 +0000

    Add package stock_gym/0.3a0/stock_gym-0.3a0.tar.gz

---
+++ packages/stock_gym/stock_gym-0.3a0.tar.gz/stock_gym-0.3a0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+import sys, os
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
+NEWS = open(os.path.join(here, 'NEWS.txt')).read()
+
+
+version = '0.3a'
+
+
+setup(
+    name='stock_gym',
+    packages=['stock_gym', 'stock_gym.envs', 'stock_gym.envs.stocks'],
+    version=version,
+    description="An OpenAI Gym environment for stock market data",
+    long_description=README + '\n\n' + NEWS,
+    classifiers=[],
+    keywords='openai-gym-environments openai-gym openai',
+    author='Bobby',
+    author_email='karma0@gmail.com',
+    url='https://github.com/karma0/stock_gym.git',
+    license='MIT',
+    install_requires=['gym'],
+)
