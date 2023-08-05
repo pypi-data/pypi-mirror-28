fd998b034e17af4aff9e3d7d7f52c2dbcf632ea0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:00 +0000

    Add package pytouchline/0.7/pytouchline-0.7.tar.gz

---
+++ packages/pytouchline/pytouchline-0.7.tar.gz/pytouchline-0.7/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+setup(
+  name = 'pytouchline',
+  packages = ['pytouchline'],
+  version = '0.7',
+  description = 'A Roth Touchline interface library',
+  author = 'Aksel Bondoe',
+  author_email = 'aksel.bondoe@gmail.com',
+  license='MIT',
+  url = 'https://github.com/abondoe/pytouchline',
+  download_url = 'https://github.com/abondoe/pytouchline/archive/0.7.tar.gz',
+  keywords = ['Roth', 'Touchline', 'Home Assistant', 'hassio', "Heat pump"],
+  classifiers = [
+	'Development Status :: 3 - Alpha',
+	'Intended Audience :: Developers',
+	'License :: OSI Approved :: MIT License',
+	'Programming Language :: Python :: 3',
+  ],
+  install_requires=['httplib2']
+)
