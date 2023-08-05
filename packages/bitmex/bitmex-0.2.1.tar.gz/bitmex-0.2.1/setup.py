a93ca6e55553727b10b109a9a4a1cb7da0642f84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:26 +0000

    Add package bitmex/0.2.1/bitmex-0.2.1.tar.gz

---
+++ packages/bitmex/bitmex-0.2.1.tar.gz/bitmex-0.2.1/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+
+setup(name='bitmex',
+      version='0.2.1',
+      description='BitMEX API client.',
+      author='Samuel Reed',
+      author_email='sam@bitmex.com',
+      url='https://github.com/bitmex/apiConnectors',
+      install_requires=['bravado'],
+      packages=[''],
+      )
