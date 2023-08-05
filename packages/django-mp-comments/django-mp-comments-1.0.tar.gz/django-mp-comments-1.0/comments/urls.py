a4ccb0689135833bb625997a5b6aba12bc1d5643
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:15 +0000

    Add package django-mp-comments/1.0/django-mp-comments-1.0.tar.gz

---
+++ packages/django-mp-comments/django-mp-comments-1.0.tar.gz/django-mp-comments-1.0/comments/urls.py
@@ -0,0 +1,14 @@
+
+from django.conf.urls import url
+
+from comments import views
+
+
+urlpatterns = [
+
+    url(r'^create/$', views.create_comment, name='create'),
+
+    url(r'^list/(?P<app_label>\w+)/(?P<model>\w+)/(?P<object_id>\d+)/$',
+        views.get_comments, name='list'),
+
+]
