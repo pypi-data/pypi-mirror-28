3deaa166e5e1140d2a68cc61fb29750e562a94a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:04 +0000

    Add package toga-winforms/0.3.0.dev3/toga-winforms-0.3.0.dev3.tar.gz

---
+++ packages/toga-winforms/toga-winforms-0.3.0.dev3.tar.gz/toga-winforms-0.3.0.dev3/toga_winforms/widgets/table.py
@@ -0,0 +1,52 @@
+from travertino.size import at_least
+
+from toga_winforms.libs import *
+
+from .base import Widget
+
+
+class Table(Widget):
+    def create(self):
+        self._container = self
+        self.native = WinForms.ListView()
+
+        dataColumn = []
+        for heading in self.interface.headings:
+            col = WinForms.ColumnHeader()
+            col.Text = heading
+            dataColumn.append(col)
+
+        self.native.View = WinForms.View.Details
+        self.native.Columns.AddRange(dataColumn)
+
+    def change_source(self, source):
+        for row in self.interface.data:
+            row._impl = WinForms.ListViewItem(*[
+                getattr(row, attr) for attr in self.interface._accessors
+            ])
+            self.native.Items.Insert(index, row._impl)
+
+    def insert(self, index, item):
+        item._impl = WinForms.ListViewItem(*[
+            getattr(item, attr) for attr in self.interface._accessors
+        ])
+        self.native.Items.Insert(index, item._impl)
+
+    def change(self, item):
+        raise NotImplementedError()
+
+    def remove(self, item):
+        raise NotImplementedError()
+
+    def clear(self):
+        self.native.Clear()
+
+    def set_on_select(self, handler):
+        raise NotImplementedError()
+
+    def scroll_to_row(self, row):
+        raise NotImplementedError()
+
+    def rehint(self):
+        self.interface.intrinsic.width = at_least(self.interface.MIN_WIDTH)
+        self.interface.intrinsic.height = at_least(self.interface.MIN_HEIGHT)
