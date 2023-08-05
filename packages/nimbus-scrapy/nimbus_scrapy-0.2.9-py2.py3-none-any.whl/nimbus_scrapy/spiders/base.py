658bc1e9775a9fd2f0e4d5409c1fab126da19851
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:42 +0000

    Add package nimbus-scrapy/0.2.9/nimbus_scrapy-0.2.9-py2.py3-none-any.whl

---
+++ packages/nimbus-scrapy/nimbus_scrapy-0.2.9-py2.py3-none-any.whl/nimbus_scrapy/spiders/base.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+"""
+Package Description
+
+Configuration
+=================================================================================
+{
+    "id": "0000000000000000",
+    "db": {
+        "db_uri": "{dialect}{user}:{password}@{host}:{port}/{db}",
+        "dialect": "postgresql://",
+        "host": "xxx.xxx.xxx.xxx",
+        "port": "5432",
+        "db": "xxx",
+        "user": "xxx",
+        "password": "xxx"
+    },
+    "redis": {
+        "host": '127.0.0.1',
+        "port": 6379,
+        "db": 0,
+        "password": None
+    },
+    "spider_config": {
+        "time": "",
+        "keywords": [
+            "KEY1", "KEY2"
+        ]
+    }
+}
+
+"""
+import os
+import sys
+import json
+import logging
+import datetime
+import time
+import urlparse
+import random
+import platform
+import scrapy
+from scrapy import signals
+from scrapy.spiders import Spider, CrawlSpider, CSVFeedSpider, SitemapSpider, XMLFeedSpider
+from scrapy.spiders import Rule
+from scrapy.http.request.form import FormRequest, Request
+from scrapy.http.response.html import HtmlResponse
+from scrapy.selector import Selector
+from scrapy.utils.response import get_base_url
+from .mixin import BaseMixin
+
+
+class AbstractSpider(BaseMixin, Spider):
+    name = None
+    allowed_domains = []
+    start_urls = []
+
+    def start_requests(self):
+        return super(AbstractSpider, self).start_requests()
+
+    def parse(self, response):
+        raise NotImplementedError
+
+    @classmethod
+    def from_crawler(cls, crawler, *args, **kwargs):
+        config = kwargs.pop("config", None)
+        spider = super(AbstractSpider, cls).from_crawler(crawler, *args, **kwargs)
+        try:
+            if config is None:
+                spider.log(message="config is None", level=logging.ERROR)
+            else:
+                spider.log(message=config, level=logging.DEBUG)
+                spider.setup(config)
+                spider.log(u"CONFIG [{spider_id}]: {config}".format(spider_id=spider.spider_id, config=spider.config))
+                spider.log(u"SPIDER [{spider_id}]: {config}".format(spider_id=spider.spider_id, config=spider.spider_config))
+        except ValueError as e:
+            spider.log(e, level=logging.ERROR)
+            raise e
+        except Exception as e:
+            spider.log(e, level=logging.ERROR)
+        return spider
+
