b5686e76b551ad786a5cbc2cc925f4e4ec9a7816
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:56 +0000

    Add package lollipop-jsonschema/0.8/lollipop-jsonschema-0.8.tar.gz

---
+++ packages/lollipop-jsonschema/lollipop-jsonschema-0.8.tar.gz/lollipop-jsonschema-0.8/lollipop_jsonschema/__init__.py
@@ -0,0 +1,4 @@
+from .jsonschema import json_schema, Encoder, TypeEncoder, \
+    ModifierEncoder, AnyEncoder, StringEncoder, NumberEncoder, BooleanEncoder, \
+    ListEncoder, TupleEncoder, ObjectEncoder, DictEncoder, OneOfEncoder, \
+    ConstantEncoder
