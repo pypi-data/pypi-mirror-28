966ac25fec217c786d944b32dffc96cdce18ae64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:48 +0000

    Add package akad/0.11.1/akad-0.11.1.tar.gz

---
+++ packages/akad/akad-0.11.1.tar.gz/akad-0.11.1/setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+try:
+    from setuptools import setup
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup
+
+setup(
+    name='akad',
+    packages=['akad'],
+    version='0.11.1',
+    author='Fadhiil Rachman',
+    long_description='Akad stored LINE Protocols of all you need',
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Communications :: Chat',
+    ],
+    install_requires=['thrift'],
+)
