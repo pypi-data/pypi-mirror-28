96f2f7392cf2f3360aebc15c6539dcf3dd218634
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:34 +0000

    Add package pydomo/0.2.2/pydomo-0.2.2.tar.gz

---
+++ packages/pydomo/pydomo-0.2.2.tar.gz/pydomo-0.2.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='pydomo',
+    version='0.2.2',
+    description='The official Python3 Domo API SDK - Domo, Inc.',
+    long_description=long_description,
+    author='Bobby Swingler',
+    author_email='bobby.swingler@domo.com',
+    url='https://github.com/domoinc/domo-python-sdk',
+    download_url='https://github.com/domoinc/domo-python-sdk/tarball/0.2.2',
+    keywords='domo api sdk',
+    license='MIT',
+    packages=find_packages(exclude=['examples']),
+    install_requires=[
+        'requests',
+        'requests_toolbelt',
+    ],
+    python_requires='>=3',
+)
