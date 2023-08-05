86c6196dd34cee7b816cbf9933de922cadc5012d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:54 +0000

    Add package python-topic-model-preprocessor/0.0.2/python-topic-model-preprocessor-0.0.2.tar.gz

---
+++ packages/python-topic-model-preprocessor/python-topic-model-preprocessor-0.0.2.tar.gz/python-topic-model-preprocessor-0.0.2/tm_preprocessor/__init__.py
@@ -0,0 +1,7 @@
+__title__ = 'tm_preprocessor'
+__version__ = '0.0.2'
+__author__ = 'Zhiya Zuo'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2017-2018 Zhiya Zuo'
+
+from tm_preprocessor.preprocessor import Preprocessor
