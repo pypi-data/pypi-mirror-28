ca45887dfc3b830b7d408d6d211354c2d9be8b53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:44 +0000

    Add package codegenit/1.3/codegenit-1.3.tar.gz

---
+++ packages/codegenit/codegenit-1.3.tar.gz/codegenit-1.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(name='codegenit',
+      version='1.3',
+      description='Generate a python project from a swagger file.',
+      url='http://github.com/cidrblock/codegenit',
+      author='Bradley A. Thornton',
+      author_email='brad@thethorntons.net',
+      license='MIT',
+      packages=[
+        'codegenit'
+      ],
+      install_requires=[
+        'pygments',
+        'astor',
+        'colorama'
+      ],
+      entry_points={
+        'console_scripts': [
+            'codegenit = codegenit.codegenit:main',
+        ],
+      },
+      zip_safe=False)
