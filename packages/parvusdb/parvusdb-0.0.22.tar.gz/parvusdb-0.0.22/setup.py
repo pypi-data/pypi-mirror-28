a92adc2c6190102f4291a78d8f940d704027b432
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:00 +0000

    Add package parvusdb/0.0.22/parvusdb-0.0.22.tar.gz

---
+++ packages/parvusdb/parvusdb-0.0.22.tar.gz/parvusdb-0.0.22/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+from distutils.extension import Extension
+from Cython.Build import cythonize
+
+packages = ['parvusdb', 'parvusdb.utils']
+extensions = [Extension('parvusdb', ['parvusdb' + '/__init__.py']),
+              Extension('parvusdb.utils', ['parvusdb/utils/__init__.py',
+                                           'parvusdb/utils/aux.py',
+                                           'parvusdb/utils/code_container.py',
+                                           'parvusdb/utils/graph_builder.py',
+                                           'parvusdb/utils/graph_database.py',
+                                           'parvusdb/utils/node_matcher.py'
+                                           ])]
+
+setup(name='parvusdb',
+      version='0.0.22',
+      description='A lightweight in-memory graph database',
+      url='http://github.com/fractalego/parvusdb',
+      author='Alberto Cetoli',
+      author_email='alberto@nlulite.com',
+      license='MIT',
+      packages=packages,
+      install_requires=[
+          'python-igraph',
+          'hy',
+#          'Cython',
+      ],
+#      ext_modules=cythonize(extensions),
+      zip_safe=False)
---
+++ packages/parvusdb/parvusdb-0.0.22.tar.gz/parvusdb-0.0.22/test/test.py
@@ -0,0 +1,4 @@
+from parvusdb.utils import GraphDatabase
+
+if __name__ == '__main__':
+    print('Hello!')
