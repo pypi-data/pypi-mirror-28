190f54b72132dcc79b7bb1685dc6dba518616702
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:58 +0000

    Add package python3-logstash/0.4.72/python3-logstash-0.4.72.tar.gz

---
+++ packages/python3-logstash/python3-logstash-0.4.72.tar.gz/python3-logstash-0.4.72/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+setup(
+    name='python3-logstash',
+    packages=['logstash'],
+    version='0.4.72',
+    description='Python logging handler for Logstash.',
+    long_description=open('README.rst').read(),
+    author='Israel Flores',
+    author_email='jobs@israelfl.com',
+    url='https://github.com/israel-fl/python3-logstash',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: System :: Logging'
+    ]
+)
