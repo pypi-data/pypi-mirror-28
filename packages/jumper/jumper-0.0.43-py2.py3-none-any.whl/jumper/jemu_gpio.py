497f56848896a5c84386e0f22b6da1973183e67a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:03 +0000

    Add package jumper/0.0.43/jumper-0.0.43-py2.py3-none-any.whl

---
+++ packages/jumper/jumper-0.0.43-py2.py3-none-any.whl/jumper/jemu_gpio.py
@@ -0,0 +1,25 @@
+"""
+:copyright: (c) 2017 by Jumper Labs Ltd.
+:license: Apache 2.0, see LICENSE.txt for more details.
+"""
+from threading import Lock
+
+
+class JemuGpio:
+    _PIN_NUM = "pin_number"
+    _TRANSITION_TYPE = "transition_type"
+
+    def __init__(self, jemu_socket_manager):
+        self._pin_level_callbacks = []
+        self._jemu_socket_manager = jemu_socket_manager
+        self._jemu_socket_manager.register(self.receive_packet)
+        self._lock = Lock()
+
+    def on_pin_level_event(self, callbacks):
+        self._pin_level_callbacks += callbacks
+
+    def receive_packet(self, jemu_packet):
+        if jemu_packet["type"] == "pin_level_event":
+            for callback in self._pin_level_callbacks:
+                with self._lock:
+                    callback(jemu_packet[self._PIN_NUM], jemu_packet[self._TRANSITION_TYPE])
