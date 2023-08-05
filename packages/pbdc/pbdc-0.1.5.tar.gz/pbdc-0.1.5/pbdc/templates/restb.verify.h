401bfa6fc3fbabb4c7f0a05bb39afb4191ebb40b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:45 +0000

    Add package pbdc/0.1.5/pbdc-0.1.5.tar.gz

---
+++ packages/pbdc/pbdc-0.1.5.tar.gz/pbdc-0.1.5/pbdc/templates/restb.verify.h
@@ -0,0 +1,26 @@
+//This file is generated auto by pbdc library , dont edit it directly .
+//Any problem when using it , please contact mail:hex@gamesci.com.cn 
+
+#ifndef _RES_TAB_{{package}}_VERIFY_AUTO_GEN_H_
+#define _RES_TAB_{{package}}_VERIFY_AUTO_GEN_H_
+
+class ResTabIndexBase;
+class  ResTabVerifierBase {
+
+public:
+    void        Clean();
+    int         Verify();
+
+protected:
+    virtual     void        OnClean();
+    virtual     int         OnVerify();
+
+public:
+    ResTabVerifierBase(ResTabIndexBase * index):m_pIndex(index){}
+
+protected:
+    ResTabIndexBase *  m_pIndex {nullptr};
+};
+
+
+#endif
