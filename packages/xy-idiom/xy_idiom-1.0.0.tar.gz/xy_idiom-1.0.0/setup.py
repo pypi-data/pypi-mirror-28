85538ef9e02720cba93b7f4e99249e397b3e09b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:37 +0000

    Add package xy-idiom/1.0.0/xy_idiom-1.0.0.tar.gz

---
+++ packages/xy-idiom/xy_idiom-1.0.0.tar.gz/xy_idiom-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+  name = 'xy_idiom',
+  packages = ['xy_idiom'],
+  version = '1.0.0',
+  description = 'Get The idiom info',
+  long_description='Get The idiom info',
+  author = 'KingHS',
+  author_email = '382771946@qq.com',
+  keywords = ['pip3', 'idiom','python3','python'],
+  license='MIT',
+)
