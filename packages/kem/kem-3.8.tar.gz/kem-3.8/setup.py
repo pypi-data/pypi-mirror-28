2d24efed04515390f9f5da4a1688f015b1812e6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:25 +0000

    Add package kem/3.8/kem-3.8.tar.gz

---
+++ packages/kem/kem-3.8.tar.gz/kem-3.8/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+setup(
+    name = 'kem',
+    packages=['kem'],
+    package_dir={'kem':'kem'},
+    package_data={'kem':['management/commands/*']},
+    version = '3.8',
+    description = 'A django App for kem',
+    author = ['davidtnfsh', 'theshaneyu'],
+    author_email = 'davidtnfsh@gmail.com',
+    url = 'https://github.com/udicatnchu/kem',
+    download_url = 'https://github.com/udicatnchu/kem/archive/v3.8.tar.gz',
+    keywords = ['kem', 'gensim'],
+    classifiers = [],
+    license='GPL3.0',
+    install_requires=[
+        'simplejson',
+        'gensim',
+        'ngram'
+    ],
+    zip_safe=True,
+)
