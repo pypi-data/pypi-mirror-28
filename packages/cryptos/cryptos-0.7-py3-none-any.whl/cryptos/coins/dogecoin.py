3a352f7a377fab9f2a35361a651b3df8e57d710f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:20 +0000

    Add package cryptos/0.7/cryptos-0.7-py3-none-any.whl

---
+++ packages/cryptos/cryptos-0.7-py3-none-any.whl/cryptos/coins/dogecoin.py
@@ -0,0 +1,15 @@
+from .bitcoin import Bitcoin
+
+
+class Doge(Bitcoin):
+    display_name = "Dogecoin"
+    coin_symbol = "DOGE"
+    magicbyte = 30
+
+    def __init__(self, testnet=False, **kwargs):
+        super(Doge, self).__init__(testnet, **kwargs)
+        if self.is_testnet:
+            self.display_name = "Dogecoin Testnet"
+            self.coin_symbol = "DOGETEST"
+            self.magicbyte = 113
+            raise NotImplementedError("Due to explorer limitations, testnet support for this coin has not been implemented yet!")
