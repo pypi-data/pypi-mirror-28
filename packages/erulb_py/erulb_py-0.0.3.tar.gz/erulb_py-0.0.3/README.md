c83465805f46dcf051b03aec0f35898c8644abb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:38 +0000

    Add package erulb_py/0.0.3/erulb_py-0.0.3.tar.gz

---
+++ packages/erulb_py/erulb_py-0.0.3.tar.gz/erulb_py-0.0.3/README.md
@@ -0,0 +1,4 @@
+Eru load balance
+================
+
+ELB(Eru load balance)是基于 [openresty](https://openresty.org/en/) 开发的 load balance 插件，在 eru 体系中，它负责将流量分发到各个容器中。ELB 的优点在于它允许灵活复杂的分流条件。
---
+++ packages/erulb_py/erulb_py-0.0.3.tar.gz/erulb_py-0.0.3/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+
+setup(
+    name='erulb_py',
+    version='0.0.3',
+    author='timfeirg',
+    zip_safe=True,
+    author_email='timfeirg@icloud.com',
+    description='ELB 3 python client',
+    py_modules=['erulbpy', 'ruledata'],
+    install_requires=[
+        'redis',
+        'requests',
+        'setuptools',
+    ],
+)
