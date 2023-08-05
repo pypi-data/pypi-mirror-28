36c47f7a20bef1d65174a87aa7c49753818340df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:16 +0000

    Add package gcloud-rest/1.1.0/gcloud_rest-1.1.0-py2.py3-none-any.whl

---
+++ packages/gcloud-rest/gcloud_rest-1.1.0-py2.py3-none-any.whl/tests/integration/taskqueue/manager_test.py
@@ -0,0 +1,33 @@
+import json
+import os
+
+from gcloud.rest.taskqueue import encode
+from gcloud.rest.taskqueue import TaskManager
+
+
+def test_lifecycle(mocker):
+    project = os.environ['GCLOUD_PROJECT']
+    task_queue = 'test-pull'
+
+    tasks = [
+        {'test_idx': 1},
+        {'test_idx': 2},
+        {'test_idx': 3},
+        {'test_idx': 4},
+    ]
+
+    worker = mocker.Mock()
+    worker.return_value = ['ok' for _ in tasks]
+
+    tm = TaskManager(project, task_queue, worker, batch_size=len(tasks))
+
+    # drain old test tasks
+    drain = tm.tq.drain()
+
+    # insert new ones
+    for task in tasks:
+        tm.tq.insert(encode(json.dumps(task)),
+                     tag=encode('gcloud-rest-manager-test'))
+
+    tm.find_and_process_work()
+    assert worker.mock_calls == [mocker.call(tasks)]
