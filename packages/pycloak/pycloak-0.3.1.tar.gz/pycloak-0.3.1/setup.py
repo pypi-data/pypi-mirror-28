620113f037ca175051463909c44702cc57991323
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:40 +0000

    Add package pycloak/0.3.1/pycloak-0.3.1.tar.gz

---
+++ packages/pycloak/pycloak-0.3.1.tar.gz/pycloak-0.3.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='pycloak',
+    packages=['pycloak','pycloak.test'],
+    version='0.3.1',
+    description='API for Keycloak Admin Interface',
+    author='Josh Cain',
+    author_email='jcain@redhat.com',
+    license='GNU Lesser General Public License, V3',
+    long_description=open('README.txt').read(),
+)
