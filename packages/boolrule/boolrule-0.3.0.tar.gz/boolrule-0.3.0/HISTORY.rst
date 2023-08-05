1b4fe7396648a6ee95c4faa72a661c19ae022dbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:10 +0000

    Add package boolrule/0.3.0/boolrule-0.3.0.tar.gz

---
+++ packages/boolrule/boolrule-0.3.0.tar.gz/boolrule-0.3.0/HISTORY.rst
@@ -0,0 +1,27 @@
+=======
+History
+=======
+
+0.2.0 (2016-10-27)
+------------------
+
+* Fixed error caused by refactor from internal codebase that was preventing deep context level values from being
+  referenced in a substitution value
+
+0.1.2 (2016-09-30)
+------------------
+
+* Improved documentation
+
+
+0.1.1 (2016-09-30)
+------------------
+
+* Made ``context`` optional
+* Improved documentation
+
+
+0.1.0 (2016-09-30)
+------------------
+
+* First release on PyPI.
---
+++ packages/boolrule/boolrule-0.3.0.tar.gz/boolrule-0.3.0/tests/test_boolrule.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pytest
+
+from boolrule import BoolRule, MissingVariableException
+
+
+@pytest.mark.parametrize('s,expected', [
+    ('5 > 3', True),
+    ('5 < 3', False),
+    ('7 == true', False),
+    ('true == true', True),
+    ('None is None', True),
+])
+def test_simple_comparisons(s, expected):
+    boolrule = BoolRule(s)
+    assert boolrule.test() == expected
+
+
+@pytest.mark.parametrize('s,expected', [
+    ('5 > 3 and 3 > 1', True),
+    ('5 > 3 and 3 > 5', False),
+    ('5 > 3 or 3 > 5', True)
+])
+def test_logical_combinations(s, expected):
+    boolrule = BoolRule(s)
+    assert boolrule.test() == expected
+
+
+@pytest.mark.parametrize('s,expected', [
+    ('5 > 3 and (3 > 5 or 3 > 1)', True),
+    ('5 > 3 and (3 > 5 and 3 < 1)', False),
+])
+def test_nested_logical_combinations(s, expected):
+    boolrule = BoolRule(s)
+    assert boolrule.test() == expected
+
+
+
+@pytest.mark.parametrize('s,context,expected', [
+    ('foo = "bar" AND baz > 10', {'foo': 'bar', 'baz': 20}, True),
+    ('foo = "bar" AND baz > 10', {'foo': 'bar', 'baz': 9}, False),
+    ('foo = "bar" AND ("a" = "b" OR baz > 10)', {'foo': 'bar', 'baz': 11}, True),
+    ('foo.bar = "bar"', {'foo': {'bar': 'bar'}}, True),
+    ('foo.bar isnot none', {'foo': {'bar': 4}}, True),
+    ('foo.bar is none', {'foo': {'bar': 4}}, False),
+    ('foo.bar is none', {'foo': {'bar': None}}, True),
+])
+def test_subsitution_values(s, context, expected):
+    boolrule = BoolRule(s)
+    assert boolrule.test(context) == expected
+
+
+@pytest.mark.parametrize('s,context,expected', [
+    ('x in (5, 6, 7)', {'x': 5}, True),
+    ('x in (5, 6, 7)', {'x': 8}, False),
+    ('x in (5, 6, 7, y)', {'x': 99, 'y': 99}, True),
+])
+def test_list_membership(s, context, expected):
+    boolrule = BoolRule(s)
+    assert boolrule.test(context) == expected
+
+
+@pytest.mark.parametrize('s,context', [
+    ('foo < bar', None),
+    ('foo < bar', {}),
+    ('foo < bar', {'foo': 5}),
+])
+def test_missing_vars_raises_exception(s, context):
+    with pytest.raises(MissingVariableException):
+        boolrule = BoolRule(s)
+        boolrule.test(context)
+
+
+# malformed_queries = [
+#     # missing variable 'foo', should produce exception
+#     ('"foo" == "bar"', {}),
+#     ('foo is "bar"', {'foo': 'bar'}),
+#     # TODO unbalanced parentheses do not raise a parse error, investigate nestedExpr...
+#     # ('foo = "bar" or (', {'foo': 'bar'}),
+#     # ('5 > 4)', {}),
+# ]
+# for query in malformed_queries:
+#     with self.assertRaises(ParseException):
+#         rule = BoolRule(query[0])
+#         rule.test(query[1])
+
