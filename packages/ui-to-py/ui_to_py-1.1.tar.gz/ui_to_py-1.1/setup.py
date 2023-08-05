247e28072acde1e5ecd14408253c317f9570deda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:35 +0000

    Add package ui-to-py/1.1/ui_to_py-1.1.tar.gz

---
+++ packages/ui-to-py/ui_to_py-1.1.tar.gz/ui_to_py-1.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name = 'ui_to_py',
+    version ='1.1',
+    packages = find_packages(),
+    author = 'featTheB',
+    long_description=open(r'README.md').read(),
+    url = 'https://github.com/featTheB/ui-to-py-gui',
+    
+    )
