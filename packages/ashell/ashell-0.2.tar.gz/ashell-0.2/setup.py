e8bb2ea932b996428afa2cb5a2fdf06d479c3f23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:07 +0000

    Add package ashell/0.2/ashell-0.2.tar.gz

---
+++ packages/ashell/ashell-0.2.tar.gz/ashell-0.2/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup
+
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(
+    name='ashell',
+    version='0.2',
+    packages=['ashell'],
+    keywords='shell android ashell',
+    url='',
+    license='MIT',
+    author='Roi Meir',
+    description='Add bashrc like functionality to adb',
+    long_description=readme(),
+    entry_points={'console_scripts': ['ashell=ashell:main']},
+    install_requires=['pexpect'],
+    include_package_data=True,
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Development Status :: 5 - Production/Stable',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: System :: Shells',
+        'Topic :: Terminals'
+    ]
+)
