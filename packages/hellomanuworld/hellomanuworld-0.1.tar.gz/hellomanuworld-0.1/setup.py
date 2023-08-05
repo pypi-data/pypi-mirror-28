378702ea2cc79b835a8b38efd22ad412abcadf89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:14 +0000

    Add package hellomanuworld/0.1/hellomanuworld-0.1.tar.gz

---
+++ packages/hellomanuworld/hellomanuworld-0.1.tar.gz/hellomanuworld-0.1/setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+setup(
+	name='hellomanuworld',    # This is the name of your PyPI-package.
+     	version='0.1',                          # Update the version number for new releases
+	scripts=['hellomanuworld']                  # The name of your scipt, and also the command you'll be using for calling it
+)
