8c3ded9bf2883ac179495482ab6f4bf4a740f50f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:06 +0000

    Add package cibc/0.2.4/cibc-0.2.4.tar.gz

---
+++ packages/cibc/cibc-0.2.4.tar.gz/cibc-0.2.4/cibc/DELETE_ME.py
@@ -0,0 +1,11 @@
+import datetime
+from cibc import CIBC
+
+if __name__ == '__main__':
+    print(c.Accounts())
+    for account in c.accounts:
+        print(account.information)
+        transactions = account.aquireTransactions(dateFrom=datetime.datetime(year=2016, month=1,day=1),
+                                                  dateUntil= datetime.datetime(year=2016, month=4, day=30))
+        print(transactions)
+        account.tocsv(filename='{}.csv'.format(str(account)))
