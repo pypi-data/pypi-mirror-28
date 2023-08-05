cb2a643d696c523dac37109b65096260c1fc881e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:28 +0000

    Add package fsleyes/0.20.1/fsleyes-0.20.1-py2.py3-none-any.whl

---
+++ packages/fsleyes/fsleyes-0.20.1-py2.py3-none-any.whl/fsleyes/displaycontext/giftiopts.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+#
+# giftiopts.py - The GiftiOpts class.
+#
+# Author: Paul McCarthy <pauldmccarthy@gmail.com>
+#
+"""This module provides the :class:`GiftiOpts` class, which contains settings
+for displaying a :class:`.GiftiSurface` overlay.
+"""
+
+
+import fsl.data.gifti as fslgifti
+from . import            meshopts
+
+
+class GiftiOpts(meshopts.MeshOpts):
+    """The :class:`GiftiOpts` class, which contains settings for displaying
+    a :class:`.GiftiSurface` overlay.
+
+    Currently (as of FSLeyes |version|), the ``GiftiOpts`` class is identical
+    to the :class:`.MeshOpts` class (from which it derives), with the exception
+    that the initial value of the :attr:`.MeshOpts.coordSpace` property is
+    set to ``'affine'``, and the :attr:`.MeshOpts.vertexData` property is
+    populated with any GIFTI data files that are related to the surface file.
+    """
+
+
+    def __init__(self, overlay, *args, **kwargs):
+        """Create a ``GiftiOpts`` instance.
+
+        All arguments are passed to the :class:`.MeshOpts` constructor.
+        """
+
+        self.getProp('coordSpace').setAttribute(self, 'default', 'affine')
+        self.coordSpace = 'affine'
+
+        vdataFiles = [None] + fslgifti.relatedFiles(overlay.dataSource)
+        self.getProp('vertexData').setChoices(vdataFiles, instance=self)
+
+        meshopts.MeshOpts.__init__(self, overlay, *args, **kwargs)
