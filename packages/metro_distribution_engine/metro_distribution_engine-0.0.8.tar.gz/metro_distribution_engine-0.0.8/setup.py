b88f9b78386ad58369f3aebc9afc5e01fb5e8c8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:26 +0000

    Add package metro_distribution_engine/0.0.8/metro_distribution_engine-0.0.8.tar.gz

---
+++ packages/metro_distribution_engine/metro_distribution_engine-0.0.8.tar.gz/metro_distribution_engine-0.0.8/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name="metro_distribution_engine",
+    version="0.0.8",
+    url="https://metro.exchange",
+    author="Rory Byrne",
+    author_email="rory.byrne57@mail.dcu.ie",
+    packages=['metro_distribution_engine', 'metro_distribution_engine/sqs_engine']
+)
