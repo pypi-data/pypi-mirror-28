ba1f5a042e336606791119d72fa32d6e18a8b209
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:00 +0000

    Add package tfserver/0.1a9/tfserver-0.1a9.tar.gz

---
+++ packages/tfserver/tfserver-0.1a9.tar.gz/tfserver-0.1a9/tfserver/__init__.py
@@ -0,0 +1,23 @@
+import os
+import tensorflow as tf
+import time
+
+__version__ = "0.1a9"
+
+tfsess = None
+
+def load_model (model_dir, tfconfig = None):
+	global tfsess
+		
+	_tfsess = tf.Session (config = tfconfig)
+	_tfsess.run (tf.global_variables_initializer())	
+	tf.saved_model.loader.load (
+		_tfsess, [tf.saved_model.tag_constants.SERVING], model_dir
+	)		
+	tfsess = _tfsess
+	
+def close ():
+	global tfsess
+	
+	tfsess.close ()
+	
