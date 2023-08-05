5870d268e4c7b120fe6203d2defdc991d5a35e3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:06 +0000

    Add package django-salalem_utils/0.1.1/django-salalem_utils-0.1.1.tar.gz

---
+++ packages/django-salalem_utils/django-salalem_utils-0.1.1.tar.gz/django-salalem_utils-0.1.1/setup.py
@@ -0,0 +1,21 @@
+import os
+from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
+
+setup(
+    name='django-salalem_utils',
+    version='0.1.1',
+    packages=['salalem_utils'],
+    description='Salalem Django Utilities and Extensions',
+    long_description=README,
+    author='Firas Al Kafri',
+    author_email='firas.alkafri@gmail.com',
+    url='https://github.com/salalem/django-salalem_utils/',
+    license='GPLv2',
+    install_requires=[
+        'Django<2',
+        'django-extensions'
+    ]
+)
