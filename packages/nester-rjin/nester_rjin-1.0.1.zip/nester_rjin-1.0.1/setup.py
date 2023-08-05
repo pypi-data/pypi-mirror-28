e03598bdbdd22f8e92a2ac22f026c2c5b6d886c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:32 +0000

    Add package nester-rjin/1.0.1/nester_rjin-1.0.1.zip

---
+++ packages/nester-rjin/nester_rjin-1.0.1.zip/nester_rjin-1.0.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'nester_rjin',
+    version = '1.0.1',
+    py_modules = ['nester_rjin'],
+    author = 'hfpyhton',
+    author_email = 'hfpython@headfirstlabs.com',
+    url = 'http://www.headfirstlabs.com',
+    description = 'A simple printer of nested listes'
+    )
