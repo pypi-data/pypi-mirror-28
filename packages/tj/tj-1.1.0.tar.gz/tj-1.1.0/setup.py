8492afa741d262772d325ddf09997b338d6febf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:25 +0000

    Add package tj/1.1.0/tj-1.1.0.tar.gz

---
+++ packages/tj/tj-1.1.0.tar.gz/tj-1.1.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(  
+    name="tj",
+    version='1.1.0',
+    description="A module that have powerful and simple functions",
+    long_description="A module that have powerful and simple functions. Read README.txt for more info",
+    author="Tushar Jain",
+    author_email="tusharlock10@gmail.com",
+    py_modules=['tj'],
+    install_requires=[
+        'Crypto']
+)
