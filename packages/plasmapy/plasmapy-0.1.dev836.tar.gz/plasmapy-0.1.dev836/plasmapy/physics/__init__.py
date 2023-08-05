8a0be224a85da607fed4e6061bad663672993bd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:31 +0000

    Add package plasmapy/0.1.dev836/plasmapy-0.1.dev836.tar.gz

---
+++ packages/plasmapy/plasmapy-0.1.dev836.tar.gz/plasmapy-0.1.dev836/plasmapy/physics/__init__.py
@@ -0,0 +1,9 @@
+# 'physics' is a tentative name for this subpackage.  Another
+# possibility is 'plasma'.  The organization is to be decided by v0.1.
+
+from .parameters import *
+from .quantum import *
+from .relativity import *
+from .transport import *
+from .distribution import *
+from .dielectric import *
