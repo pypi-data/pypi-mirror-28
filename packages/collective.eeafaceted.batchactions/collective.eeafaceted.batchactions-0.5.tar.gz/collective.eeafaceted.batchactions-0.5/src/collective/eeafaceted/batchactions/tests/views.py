328491bb4cad20921d1543d304c5bfd527837799
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:38 +0000

    Add package collective.eeafaceted.batchactions/0.5/collective.eeafaceted.batchactions-0.5.tar.gz

---
+++ packages/collective.eeafaceted.batchactions/collective.eeafaceted.batchactions-0.5.tar.gz/collective.eeafaceted.batchactions-0.5/src/collective/eeafaceted/batchactions/tests/views.py
@@ -0,0 +1,15 @@
+from collective.eeafaceted.batchactions.browser.views import BaseBatchActionForm
+
+
+class TestingBatchActionForm(BaseBatchActionForm):
+
+    buttons = BaseBatchActionForm.buttons.copy()
+    label = (u"Testing form")
+    button_with_icon = True
+
+    def available(self):
+        """Available if 'hide_testing_action' not found in request."""
+        res = super(TestingBatchActionForm, self).available()
+        if res and not self.request.get('hide_testing_action'):
+            return True
+        return False
