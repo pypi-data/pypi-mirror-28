707dfa3cd334fc65b10501e1aaa248971beb16d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:26 +0000

    Add package jotting/0.1.1/jotting-0.1.1-py3-none-any.whl

---
+++ packages/jotting/jotting-0.1.1-py3-none-any.whl/jotting/_shelf_py35.py
@@ -0,0 +1,46 @@
+import inspect
+import asyncio
+import functools
+import threading
+
+from .utils import CallMap, infer_title
+
+
+class _book_compat(object):
+
+    @classmethod
+    def shelf(cls):
+        task = asyncio.Task.current_task() or threading.current_thread()
+        if task not in cls._shelves:
+            cls._shelves[task] = [{"title": "__main__"}]
+        return cls._shelves[task]
+
+    @classmethod
+    def mark(cls, title, *binding, **content):
+        def setup(function, title=title):
+            if title is None:
+                title = infer_title(function)
+            cm = CallMap(function)
+            if inspect.iscoroutinefunction(function):
+                @functools.wraps(function)
+                async def author(*args, **kwargs):
+                    book = kwargs.pop("__book__", None)
+                    intro = dict(content, **cm.map(args, kwargs))
+                    with book or cls(title, *binding, **intro):
+                        result = await function(*args, **kwargs)
+                        cls.close({"returned": result})
+                        return result
+            else:
+                @functools.wraps(function)
+                def author(*args, **kwargs):
+                    book = kwargs.pop("__book__", None)
+                    intro = dict(content, **cm.map(args, kwargs))
+                    with book or cls(title, *binding, **intro):
+                        result = function(*args, **kwargs)
+                        cls.close({"returned": result})
+                        return result
+            return author
+        if callable(title):
+            return setup(title, None)
+        else:
+            return setup
