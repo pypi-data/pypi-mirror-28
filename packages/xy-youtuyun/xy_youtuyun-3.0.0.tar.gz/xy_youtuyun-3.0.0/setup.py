cb4f9c1ce89a9d219ac4885ea57d773032dd9a42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:06 +0000

    Add package xy-youtuyun/3.0.0/xy_youtuyun-3.0.0.tar.gz

---
+++ packages/xy-youtuyun/xy_youtuyun-3.0.0.tar.gz/xy_youtuyun-3.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'xy_youtuyun',
+    version = '3.0.0',
+    description = 'Face AI Demo',
+    long_description='Face AI Demo',
+    license = 'MIT License',
+    packages = find_packages(),
+    platforms = 'any',
+    author = 'KingHS',
+    author_email = '382771946@qq.com',
+    keywords = ('xy_youtuyun', 'qcloud','pip3','face'),
+    install_requires=['requests']
+)
