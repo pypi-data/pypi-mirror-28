e408066682bf623da9639694192d5203f252f367
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:06 +0000

    Add package acer/0.2.1/acer-0.2.1.tar.gz

---
+++ packages/acer/acer-0.2.1.tar.gz/acer-0.2.1/acer/src/trie_tree.h
@@ -0,0 +1,33 @@
+#ifndef TRIETREE
+#define TRIETREE
+
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+typedef struct ChildNode{
+    public:
+    string word;
+    unordered_map<string, ChildNode*> child;
+    int state=0;
+    int depth;
+    bool is_end = false;
+    string segment;
+} Node;
+
+class TrieTree{
+    public:
+    TrieTree(){
+        proot = new Node();
+    }
+    Node* proot;
+    int node_nums=0;
+    unordered_map <string, int> vocab;
+    public:
+    void add_words(vector<string> &word, bool);
+    void add_word(string word, bool=false);
+};
+
+
+#endif
---
+++ packages/acer/acer-0.2.1.tar.gz/acer-0.2.1/acer/src/utils.h
@@ -0,0 +1,10 @@
+#ifndef TTA_UTILS
+#define TTA_UTILS
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<string> cut(const string &word, bool);
+vector<string> read_file(string);
+#endif
