fec18950b88f5401496a6e176659ff1772e9f024
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:50 +0000

    Add package guillotina/2.2.12/guillotina-2.2.12.tar.gz

---
+++ packages/guillotina/guillotina-2.2.12.tar.gz/guillotina-2.2.12/guillotina/tests/test_server.py
@@ -0,0 +1,6 @@
+from guillotina.traversal import TraversalRouter
+
+
+def test_make_app(dummy_guillotina):
+    assert dummy_guillotina is not None
+    assert type(dummy_guillotina.router) == TraversalRouter
