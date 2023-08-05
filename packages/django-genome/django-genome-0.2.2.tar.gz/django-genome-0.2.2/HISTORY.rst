f2bb8103cc4673927b61534e738a629269709812
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:33 +0000

    Add package django-genome/0.2.2/django-genome-0.2.2.tar.gz

---
+++ packages/django-genome/django-genome-0.2.2.tar.gz/django-genome-0.2.2/HISTORY.rst
@@ -0,0 +1,28 @@
+.. :changelog:
+
+History
+-------
+
+0.1.0 (2017-12-30)
+++++++++++++++++++
+
+* First release on PyPI.
+* Initial models and REST API
+* Syncs data from HGNC and UCSC to build database
+
+0.2.0 (2018-01-05)
+++++++++++++++++++
+
+* Improved REST API Filters
+* made Chromosomes and Gene Symbols save as uppercase to maintain consistency
+
+
+0.2.1 (2018-01-08)
+++++++++++++++++++
+
+* Fixed issues with migrations
+
+0.2.2 (2018-01-12)
+++++++++++++++++++
+
+* Fixed route names for SimpleRouter
---
+++ packages/django-genome/django-genome-0.2.2.tar.gz/django-genome-0.2.2/genome/urls.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from rest_framework import routers
+
+from . import viewsets
+
+
+router = routers.SimpleRouter()
+router.register(r'', viewsets.GenomeViewSet)
+router.register(r'genes', viewsets.GeneViewSet)
+router.register(r'transcripts', viewsets.TranscriptViewSet)
+router.register(r'exons', viewsets.ExonViewSet)
+
+default_router = routers.DefaultRouter()
+default_router.register(r'genomes', viewsets.GenomeViewSet)
+default_router.register(r'genes', viewsets.GeneViewSet)
+default_router.register(r'transcripts', viewsets.TranscriptViewSet)
+default_router.register(r'exons', viewsets.ExonViewSet)
+
+
+urlpatterns = default_router.urls
