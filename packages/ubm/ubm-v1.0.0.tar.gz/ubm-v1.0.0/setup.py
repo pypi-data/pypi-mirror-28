36a906129e61e2fe997ad0b41fadc03316716d9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:03:43 +0000

    Add package ubm/v1.0.0/ubm-v1.0.0.tar.gz

---
+++ packages/ubm/ubm-v1.0.0.tar.gz/ubm-v1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+  name='ubm',
+  packages=['ubm'],  # this must be the same as the name above
+  version='v1.0.0',
+  description='Library for UniBo Motorsport data analysis',
+  author='Aaron Russo',
+  author_email='axolo6@gmail.com',
+  url='https://github.com/peterldowns/mypackage',  # use the URL to the github repo
+  download_url='https://github.com/Ax6/ubm-python-libraries/archive/v1.0.0.tar.gz',
+  classifiers=[],
+)
