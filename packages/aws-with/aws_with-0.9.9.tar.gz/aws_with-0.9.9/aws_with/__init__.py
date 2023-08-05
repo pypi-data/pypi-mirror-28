c53bc66aeb833f7f3d776ff63ba81320e80c31d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:10 +0000

    Add package aws-with/0.9.9/aws_with-0.9.9.tar.gz

---
+++ packages/aws-with/aws_with-0.9.9.tar.gz/aws_with-0.9.9/aws_with/__init__.py
@@ -0,0 +1,22 @@
+""" aws_with module definition """
+
+#from . import utils
+#from . import regions
+#from . import organizations
+#from . import commands
+#from . import workplan
+#from . import output
+#from . import cli
+#from . import main
+
+VERSION = "0.9.9"
+BUILD_TIME = "2018-01-08 06:12:42"
+BUILD_COMMIT_HASH = "4542b1251b20d908dc706c05c98b70ed8dfc2710"
+
+__title__ = "aws_with"
+__version__ = VERSION
+__summary__ = "aws_with command line utility"
+__license__ = "APACHE2"
+__uri__ = "https://aws.amazon.com/"
+__author__ = "Mike Evans"
+__email__ = "miev@amazon.com"
