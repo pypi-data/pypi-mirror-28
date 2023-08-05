edf1e418751cce6b8731f90ecc9b0a7261218b52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:49 +0000

    Add package django-wscounter/0.1.3.9.9.2/django-wscounter-0.1.3.9.9.2.tar.gz

---
+++ packages/django-wscounter/django-wscounter-0.1.3.9.9.2.tar.gz/django-wscounter-0.1.3.9.9.2/wscounter/views.py
@@ -0,0 +1,103 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.core.exceptions import FieldError
+from django.db.models import Q
+from datetime import datetime
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import authentication, permissions
+import json
+from . import utils as data_utils
+
+
+class WSCounterAPIView(APIView):
+    authentication_classes = (authentication.CSRFCheck,)
+    permission_classes = (permissions.AllowAny,)
+
+    def get(self, request, format=None):
+        """
+        :param request:
+        :param format:
+                    models = [
+                      {
+                          "model": "ModelName",
+                          "from": "date",
+                          "to": "date"
+                      },
+                      .
+                      .
+                      .
+                    ]
+        :return:
+                      ModelName: {
+                          "from": "date",
+                          "to": "date",
+                          "data": "count"
+                      },
+                      .
+                      .
+                      .
+        """
+        response_json_object = {
+            "result": "success",
+            "models": {},
+            "message": None
+        }
+
+        error_messages = []
+        requested_data_array = None
+
+        try:  # Validate JSON object
+            requested_data_array = json.loads(request.GET.get("models", None))
+            try:  # Check if at least one model name exists
+                for requested_data in requested_data_array:
+                    try:  # Check if model exists
+                        model = data_utils.get_model(requested_data['model'])
+                        model_data = {}
+
+                        queryset = model.objects.all()
+
+                        try:  # Check if date parameters were sent
+                            try: # Check if date is valid
+                                date_from = datetime.strptime(requested_data['from'], "%d/%m/%Y")
+                                date_to = datetime.strptime(requested_data['to'], "%d/%m/%Y")
+                                try:
+                                    queryset = queryset.filter(created_at__range=[date_from, date_to])
+                                except FieldError:
+                                    queryset = queryset.filter(created__range=[date_from, date_to])
+
+                                model_data['from'] = date_from
+                                model_data['to'] = date_to
+                            except ValueError:
+                                error_messages.append(
+                                    "dates for model " + requested_data['model'] + " are not valid, should be DD/MM/YYYY")
+                        except KeyError:
+                            pass
+
+                        try:  # Check if custom Query
+                            query = requested_data['query']
+
+                            django_query = Q()
+                            for x in query:
+                                django_query.add(Q(**x), Q.AND)
+
+                            queryset = queryset.filter(django_query)
+                        except KeyError:
+                            pass
+
+                        model_data['data'] = queryset.count()
+                        response_json_object['models'][model.__name__] = model_data
+
+                    except KeyError:
+                        error_messages.append(requested_data['model'] + " does not exist")
+            except KeyError:
+                error_messages.append("At least one model name has to be provided")
+        except ValueError:
+            error_messages.append("Provided data is not valid")
+
+        if len(error_messages) != 0:
+            response_json_object['result'] = "error"
+            response_json_object['message'] = error_messages
+
+        return Response(response_json_object)
