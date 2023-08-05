785ef7a0e4a1ebe5f41883750b7791f3a0c4f3b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:31 +0000

    Add package django-wscounter/0.1.3.9/django-wscounter-0.1.3.9.tar.gz

---
+++ packages/django-wscounter/django-wscounter-0.1.3.9.tar.gz/django-wscounter-0.1.3.9/wscounter/utils.py
@@ -0,0 +1,7 @@
+def get_model(model_name):
+    try:
+        from django.apps import apps
+        return apps.get_model(model_name)
+    except ImportError:
+        from django.db.models.loading import get_model
+        return get_model(model_name)
