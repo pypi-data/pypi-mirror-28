c058a28ca3b5c36c6e81e23ed1c791a635cda912
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:48 +0000

    Add package eeconvert/0.1.4/eeconvert-0.1.4.tar.gz

---
+++ packages/eeconvert/eeconvert-0.1.4.tar.gz/eeconvert-0.1.4/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='eeconvert',
+      version='0.1.4',
+      description='earth engine conversion for jupyter notebook use',
+      url='http://github.com/rutgerhofste/eeconvert',
+      author='Rutger Hofste',
+      author_email='rutgerhofste@gmail.com',
+      license='MIT',
+      packages=['eeconvert'],
+      install_requires=['boto3','botocore','sqlalchemy','sqlalchemy2'],
+      zip_safe=False)
