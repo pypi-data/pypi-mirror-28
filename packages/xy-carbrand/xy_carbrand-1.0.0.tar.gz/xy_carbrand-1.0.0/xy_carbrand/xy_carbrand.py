85df3af2f24f88447bc96d703ef6f4ed5a301f79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:37 +0000

    Add package xy-carbrand/1.0.0/xy_carbrand-1.0.0.tar.gz

---
+++ packages/xy-carbrand/xy_carbrand-1.0.0.tar.gz/xy_carbrand-1.0.0/xy_carbrand/xy_carbrand.py
@@ -0,0 +1,40 @@
+import urllib.request
+import urllib.parse
+import json
+APPKEY='631c5a5b9992bd74'
+APPSECRET='eS0iNTaMdI5mgRew8JkrVqUB7mG1MytS'
+API_URL='http://api.jisuapi.com/car/brand'
+
+'''返回所有汽车品牌'''
+def brands():
+    data = {}
+    data['appkey'] = APPKEY
+    url_values = urllib.parse.urlencode(data)
+    url = API_URL + '?' + url_values
+    result = urllib.request.urlopen(url)
+    jsonarr = json.loads(result.read())
+    if jsonarr['status'] != '0':
+        print(jsonarr['msg'])
+        exit()
+    result = jsonarr['result']
+    cars = []
+    for val in result:
+        cars.append(val['name'])
+    return cars
+
+'''返回所有品牌的信息'''
+def info():
+    data = {}
+    data['appkey'] = APPKEY
+    url_values = urllib.parse.urlencode(data)
+    url = API_URL + '?' + url_values
+    result = urllib.request.urlopen(url)
+    jsonarr = json.loads(result.read())
+    if jsonarr['status'] != '0':
+        print(jsonarr['msg'])
+        exit()
+    result = jsonarr['result']
+    carsinfo = []
+    for val in result:
+        carsinfo.append([val['name'],val['initial'],val['logo']])
+    return carsinfo
