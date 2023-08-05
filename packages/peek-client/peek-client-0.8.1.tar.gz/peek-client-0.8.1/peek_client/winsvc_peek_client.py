33ff961d45b96a6962f2ff28737a4aefa736d463
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:07 +0000

    Add package peek-client/0.8.1/peek-client-0.8.1.tar.gz

---
+++ packages/peek-client/peek-client-0.8.1.tar.gz/peek-client-0.8.1/peek_client/winsvc_peek_client.py
@@ -0,0 +1,44 @@
+import platform
+
+try:
+    import win32serviceutil
+    import win32service
+    import win32event
+    import servicemanager
+    # import socket
+except ImportError as e:
+    if platform.system() is "Windows":
+        raise
+
+from twisted.internet import reactor
+
+from peek_client import run_peek_client
+
+
+class PeekSvc(win32serviceutil.ServiceFramework):
+    _svc_name_ = "peek_client"
+    _svc_display_name_ = "Peek Client"
+
+    def __init__(self, args):
+        win32serviceutil.ServiceFramework.__init__(self, args)
+        self.hWaitStop = win32event.CreateEvent(None, 0, 0, None)
+        # socket.setdefaulttimeout(120)
+
+    def SvcStop(self):
+        self.ReportServiceStatus(win32service.SERVICE_STOP_PENDING)
+        win32event.SetEvent(self.hWaitStop)
+        reactor.callLater(0, reactor.stop)
+
+    def SvcDoRun(self):
+        servicemanager.LogMsg(servicemanager.EVENTLOG_INFORMATION_TYPE,
+                              servicemanager.PYS_SERVICE_STARTED,
+                              (self._svc_name_, ''))
+        run_peek_client.main()
+
+
+def main():
+    win32serviceutil.HandleCommandLine(PeekSvc)
+
+
+if __name__ == '__main__':
+    main()
