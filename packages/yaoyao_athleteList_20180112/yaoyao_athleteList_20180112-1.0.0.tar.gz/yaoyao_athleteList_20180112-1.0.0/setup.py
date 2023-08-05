af5ee6bf605b0e4727550461ec2aef28aca85ff6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:14 +0000

    Add package yaoyao_athleteList_20180112/1.0.0/yaoyao_athleteList_20180112-1.0.0.tar.gz

---
+++ packages/yaoyao_athleteList_20180112/yaoyao_athleteList_20180112-1.0.0.tar.gz/yaoyao_athleteList_20180112-1.0.0/setup.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+		name = 'yaoyao_athleteList_20180112',
+		version = '1.0.0',
+		py_modules = ['yaoyao_athleteList_20180112'],
+		author = 'fushenmomo',
+		author_email = '1021051650@qq.com',
+		url = 'https://pypi.python.org/pypi',
+		description = 'a class definition about list',
+	)
