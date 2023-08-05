3af606615f1307acb54d53682bee8ffd09883fea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:57 +0000

    Add package cimgtrs/1.1.0/cimgtrs-1.1.0.tar.gz

---
+++ packages/cimgtrs/cimgtrs-1.1.0.tar.gz/cimgtrs-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'cimgtrs',      
+    version = '1.1.0',
+    py_modules = ['cimgtrs'],
+    author = 'yush1n',        
+    author_email = '471979570@qq.com',
+    url = 'https://pypi.python.org/pypi',
+    description = 'this is my first'   
+    )
