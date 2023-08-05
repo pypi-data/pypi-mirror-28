e5b0294609ad6f5ce4b797c6eb45c1dec77e3ba0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:37 +0000

    Add package psims/0.0.13/psims-0.0.13.tar.gz

---
+++ packages/psims/psims-0.0.13.tar.gz/psims-0.0.13/setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name='psims',
+    version='0.0.13',
+    description="Writers and controlled vocabulary manager for PSI-MS's mzML and mzIdentML standards",
+    maintainer='Joshua Klein',
+    maintainer_email="jaklein@bu.edu",
+    zip_safe=False,
+    packages=find_packages(),
+    url="https://github.com/mobiusklein/psims",
+    include_package_data=True,
+    install_requires=[
+        "lxml",
+        "six",
+        "sqlalchemy",
+        "numpy"
+    ]
+)
