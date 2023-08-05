03aa436f13f35cffaee29d91561b92642e20a173
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:24 +0000

    Add package httpimport/0.5.13/httpimport-0.5.13.tar.gz

---
+++ packages/httpimport/httpimport-0.5.13.tar.gz/httpimport-0.5.13/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup
+
+
+import httpimport
+
+
+# Get the long description from the README file
+# And also convert to reST
+# MD to RST Convert Line acquired from:
+# https://bons.ai/blog/markdown-for-pypi ->
+# https://github.com/BonsaiAI/bonsai-config/blob/0.3.1/setup.py#L9
+try:
+	from pypandoc import convert
+
+	def read_md(f): return convert(f, 'rst').replace("~",'^')	# Hack to pass the 'rst_lint.py' - PyPI
+
+except ImportError:
+	print("warning: pypandoc module not found, could not convert Markdown to RST")
+	def read_md(f): return open(f, 'r').read()
+
+
+setup(name='httpimport',
+	  version=httpimport.__version__,
+	  description='Module for remote in-memory Python package/module loading through HTTP',
+	  long_description=read_md('README.md'),
+	  author=httpimport.__author__,
+	  author_email='john.torakis@gmail.com',
+	  license='Apache2',
+	  url=httpimport.__github__,
+	  py_modules=['httpimport'],
+	  classifiers=[
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+ 		'Intended Audience :: Developers',
+	 	  ],
+  	  keywords = ['import',
+  	  	'loader',
+  	  	'memory',
+  	  	'http'],
+	  )
