af4bd67e65ba98a49d142908d1eda35f9f1740ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:28 +0000

    Add package digitick-client/0.1.0a3/digitick_client-0.1.0a3-py2.py3-none-any.whl

---
+++ packages/digitick-client/digitick_client-0.1.0a3-py2.py3-none-any.whl/digitick_client/apis/__init__.py
@@ -0,0 +1,10 @@
+from __future__ import absolute_import
+
+# import apis into api package
+from .authorization_api import AuthorizationApi
+from .catalog_api import CatalogApi
+from .distribution_api import DistributionApi
+from .document_api import DocumentApi
+from .order_api import OrderApi
+from .user_api import UserApi
+from .venue_api import VenueApi
