fd4dc2d699a400b251263a145f7c9a6d2813683e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:58 +0000

    Add package zeugma/0.29/zeugma-0.29.tar.gz

---
+++ packages/zeugma/zeugma-0.29.tar.gz/zeugma-0.29/zeugma/conf.py
@@ -0,0 +1,27 @@
+# -*- coding:utf-8 -*-
+"""
+Created on the 05/01/18
+@author: Nicolas Thiebaut
+@email: nkthiebaut@gmail.com
+"""
+import os
+from zeugma.logger import PACKAGE_LOGGER as logger
+
+MODELS_DIR = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'zeugma_embeddings')
+if not os.path.exists(MODELS_DIR):
+    logger.info(MODELS_DIR + ' not found, creating it.')
+    os.mkdir(MODELS_DIR)
+
+OOV_TAG = '<oov>'  # Out-Of-Vocabulary tag for rare words
+
+W2V_EMBEDDINGS_URL = "https://github.com/eyaler/word2vec-slim/raw/master/" +\
+                     "GoogleNews-vectors-negative300-SLIM.bin.gz"
+
+GLOVE_EMBEDDINGS_URL = "http://nlp.stanford.edu/data/glove.6B.zip"
+
+DEFAULT_PRETRAINED_EMBEDDINGS = {
+    'Word2Vec': {'url': W2V_EMBEDDINGS_URL,
+                 'filename': 'GoogleNews-vectors-negative300-SLIM.bin'},
+    'GloVe': {'url': GLOVE_EMBEDDINGS_URL,
+              'filename': 'glove.6B.300d.txt'}
+}
