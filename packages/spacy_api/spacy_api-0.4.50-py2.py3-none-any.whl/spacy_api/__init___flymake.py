58b5b04c695c735f9db6f2a16240e9e644b0c16f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:54 +0000

    Add package spacy_api/0.4.50/spacy_api-0.4.50-py2.py3-none-any.whl

---
+++ packages/spacy_api/spacy_api-0.4.50-py2.py3-none-any.whl/spacy_api/__init___flymake.py
@@ -0,0 +1,20 @@
+import sys
+
+__project__ = "spacy_api"
+__version__ = "0.4.42"
+__repo__ = "https://github.com/kootenpv/spacy_api"
+
+from spacy_api.client import Client
+from spacy_api.server import SpacyLocalServer as LocalClient
+
+
+def print_version():
+    sv = sys.version_info
+    py_version = "{}.{}.{}".format(sv.major, sv.minor, sv.micro)
+    version_parts = __version__.split(".")
+    s = "{} version: [{}], Python {}".format(__project__, __version__, py_version)
+    s += "\nMajor version: {}  (breaking changes)".format(version_parts[0])
+    s += "\nMinor version: {}  (extra feature)".format(version_parts[1])
+    s += "\nMicro version: {} (commit count)".format(version_parts[2])
+    s += "\nFind out the most recent version at {}".format(__repo__)
+    return s
