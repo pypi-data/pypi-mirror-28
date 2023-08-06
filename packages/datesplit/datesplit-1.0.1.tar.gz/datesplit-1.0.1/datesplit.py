f973899d0d5317e7b1e1a08a3710ea0ace9597b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:43 +0000

    Add package datesplit/1.0.1/datesplit-1.0.1.tar.gz

---
+++ packages/datesplit/datesplit-1.0.1.tar.gz/datesplit-1.0.1/datesplit.py
@@ -0,0 +1,8 @@
+def datesplit(date):
+    s = date.split('-')
+    year=int(s[0])
+    mon=int(s[1])
+    day=int(s[2])
+
+    return year,mon,day
+
---
+++ packages/datesplit/datesplit-1.0.1.tar.gz/datesplit-1.0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup #如果没有需要先安装
+setup(
+    name="datesplit",  #一般放你包的名字即可
+    version="1.0.1", #版本号
+    author="zhaoyang", #作者名称
+    author_email="497361352@qq.com", #作者邮箱  方便有问题沟通
+    url=' ',
+    description="str date convert to int year,month and day",#关于这个包的描述
+    py_modules=['datesplit'],
+)
+
+
