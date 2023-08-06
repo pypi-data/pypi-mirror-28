e0ef53c53eba70f43b39bb08a0e42831ba10c36b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:05 +0000

    Add package vsionailibs/1.0.0/vsionailibs-1.0.0.tar.gz

---
+++ packages/vsionailibs/vsionailibs-1.0.0.tar.gz/vsionailibs-1.0.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(name='vsionailibs',
+      version='1.0.0',
+      description='the public libs of vsion.ai',
+      url='https://git.tech.rz.db.de/ZhenyuGeng/vsion-ai-libs.git',
+      author='Zhenyu Geng',
+      author_email='zhenyu.geng@deutschebahn.com',
+      license='MIT',
+      packages=['vsionailibs'],
+      install_requires=[
+            'botocore',
+            'boto3',
+            'numpy',
+            'opencv-python'
+      ],
+      zip_safe=False)
