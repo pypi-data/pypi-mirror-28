c32f9463ad048299c79faa0b04a2517b4e9688ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:23 +0000

    Add package pyfakers/0.0.0/pyfakers-0.0.0-py2.py3-none-manylinux1_x86_64.whl

---
+++ packages/pyfakers/pyfakers-0.0.0-py2.py3-none-manylinux1_x86_64.whl/pyfakers-0.0.0.dist-info/DESCRIPTION.rst
@@ -0,0 +1,131 @@
+# py-fake-rs 
+
+
+A fake data generator (a faker) for Python, extended with Rust.  The reason for
+this library is performance.  When you're generating large amounts of fake data,
+this library will serve you well.
+
+This module contains bindings to [fake-rs](https://github.com/cksac/fake-rs),
+
+
+## Example API
+
+```python
+from pyfakers import Name
+
+print(Name.first_name())
+print(Name.last_name())
+```
+
+
+## Installation
+
+For now, build from source and install the generated wheel using pip. 
+
+I need help figuring out how to release this binary wheel to pypi, 
+which is raising errors when I do.  Help would be appreciated!
+
+
+
+## Building from source
+
+Install libffi, python3 headers, setuptools and wheel. The following command will install these on Ubuntu
+
+```
+    apt-get install libffi-dev python3-dev python3-setuptools python3-wheel
+```
+
+Check out the repository
+
+```    
+    git clone git@github.comdowwie/pyfakers.git
+    cd pyfakers
+```
+
+In order to compile the Rust code, you'll need to have Rust nightly toolchain installed and enabled.
+
+Use rustup to set this up, find installation instructions for rustup at https://www.rustup.rs/
+
+To use Rust nightly, run the following commands (from the project root)
+
+```
+    rustup update nightly
+    rustup override add nightly
+```	
+
+Install the ``pipenv`` utility from pypi
+
+```
+	pip install pipenv
+```	
+
+Install dependencies (from the project root)
+
+```
+	pipenv install
+```	
+
+Launch a virtual environment (from the project root)
+
+```
+	pipenv shell
+```
+
+Now you can build the package
+
+```
+    python3 setup.py bdist_wheel
+```
+
+Install the wheel
+
+```
+	pip install dist/<wheel file name>
+```
+
+## Full API 
+
+```python
+from pyfakers import Name, Internet, Company, Address, PhoneNumber
+
+print(Name.first_name())
+print(Name.last_name())
+print(Name.full_name())
+print(Name.title_descriptor())
+print(Name.title_level())
+print(Name.title_job())
+print(Name.title())
+
+print(Internet.free_email_provider())
+print(Internet.domain_suffix())
+print(Internet.user_name())
+print(Internet.free_email())
+print(Internet.safe_email())
+
+print(Company.suffix())
+print(Company.name())
+print(Company.buzzword())
+print(Company.catch_phrase())
+print(Company.bs())
+print(Company.profession())
+print(Company.industry())
+
+print(Address.time_zone())
+print(Address.city_prefix())
+print(Address.city_suffix())
+print(Address.street_suffix())
+print(Address.state())
+print(Address.state_abbr())
+print(Address.city())
+print(Address.street_name())
+print(Address.building_number())
+print(Address.street_address())
+print(Address.secondary_address())
+print(Address.postal_code())
+print(Address.latitude())
+print(Address.longitude())
+
+print(PhoneNumber.phone_number())
+```
+
+
---
+++ packages/pyfakers/pyfakers-0.0.0-py2.py3-none-manylinux1_x86_64.whl/pyfakers/__init__.py
@@ -0,0 +1,129 @@
+import cffi
+from . import _native
+
+
+ffi = cffi.FFI()
+
+
+class Name:
+
+    def first_name(self):
+        return ffi.string(_native.lib.first_name()).decode('utf-8')
+
+    def last_name(self):
+        return ffi.string(_native.lib.last_name()).decode('utf-8')
+
+    def full_name(self):
+        return ffi.string(_native.lib.full_name()).decode('utf-8')
+
+    def title_descriptor(self):
+        return ffi.string(_native.lib.title_descriptor()).decode('utf-8')
+
+    def title_level(self):
+        return ffi.string(_native.lib.title_level()).decode('utf-8')
+
+    def title_job(self):
+        return ffi.string(_native.lib.title_job()).decode('utf-8')
+
+    def title(self):
+        return ffi.string(_native.lib.title()).decode('utf-8')
+
+
+class Internet:
+
+    def free_email_provider(self):
+        return ffi.string(_native.lib.free_email_provider()).decode('utf-8')
+
+    def domain_suffix(self):
+        return ffi.string(_native.lib.domain_suffix()).decode('utf-8')
+
+    def user_name(self):
+        return ffi.string(_native.lib.user_name()).decode('utf-8')
+
+    def free_email(self):
+        return ffi.string(_native.lib.free_email()).decode('utf-8')
+
+    def safe_email(self):
+        return ffi.string(_native.lib.safe_email()).decode('utf-8')
+
+
+class Company:
+
+    def suffix(self):
+        return ffi.string(_native.lib.suffix()).decode('utf-8')
+
+    def name(self):
+        return ffi.string(_native.lib.name()).decode('utf-8')
+
+    def buzzword(self):
+        return ffi.string(_native.lib.buzzword()).decode('utf-8')
+
+    def catch_phrase(self):
+        return ffi.string(_native.lib.catch_phrase()).decode('utf-8')
+
+    def bs(self):
+        return ffi.string(_native.lib.bs()).decode('utf-8')
+
+    def profession(self):
+        return ffi.string(_native.lib.profession()).decode('utf-8')
+
+    def industry(self):
+        return ffi.string(_native.lib.industry()).decode('utf-8')
+
+
+class Address:
+
+    def time_zone(self):
+        return ffi.string(_native.lib.time_zone()).decode('utf-8')
+
+    def city_prefix(self):
+        return ffi.string(_native.lib.city_prefix()).decode('utf-8')
+
+    def city_suffix(self):
+        return ffi.string(_native.lib.city_suffix()).decode('utf-8')
+
+    def street_suffix(self):
+        return ffi.string(_native.lib.street_suffix()).decode('utf-8')
+
+    def state(self):
+        return ffi.string(_native.lib.state()).decode('utf-8')
+
+    def state_abbr(self):
+        return ffi.string(_native.lib.state_abbr()).decode('utf-8')
+
+    def city(self):
+        return ffi.string(_native.lib.city()).decode('utf-8')
+
+    def street_name(self):
+        return ffi.string(_native.lib.street_name()).decode('utf-8')
+
+    def building_number(self):
+        return ffi.string(_native.lib.building_number()).decode('utf-8')
+
+    def street_address(self):
+        return ffi.string(_native.lib.street_address()).decode('utf-8')
+
+    def secondary_address(self):
+        return ffi.string(_native.lib.secondary_address()).decode('utf-8')
+
+    def postal_code(self):
+        return ffi.string(_native.lib.postal_code()).decode('utf-8')
+
+    def latitude(self):
+        return ffi.string(_native.lib.latitude()).decode('utf-8')
+
+    def longitude(self):
+        return ffi.string(_native.lib.longitude()).decode('utf-8')
+
+
+class PhoneNumber:
+
+    def phone_number(self):
+        return ffi.string(_native.lib.suffix()).decode('utf-8')
+
+
+Name = Name()
+Internet = Internet()
+Company = Company()
+Address = Address()
+PhoneNumber = PhoneNumber()
