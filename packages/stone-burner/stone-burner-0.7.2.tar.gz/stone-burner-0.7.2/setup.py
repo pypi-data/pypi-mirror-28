5bcb307e4da6a52b1b837577816fd1aa88061564
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:40 +0000

    Add package stone-burner/0.7.2/stone-burner-0.7.2.tar.gz

---
+++ packages/stone-burner/stone-burner-0.7.2.tar.gz/stone-burner-0.7.2/setup.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name='stone-burner',
+    version='0.7.2',
+    description='Give more power to Terraform.',
+    author='Rodrigo Argüello Flores',
+    author_email='rodrigo@kkvesper.jp',
+    url='https://github.com/kkvesper/stone-burner',
+    download_url='https://github.com/kkvesper/stone-burner/archive/v0.7.2.tar.gz',
+    packages=find_packages(),
+    install_requires=[
+        'jinja2>=2.9',
+        'pyyaml>=3.12',
+        'click>=6.7',
+        'crayons>=0.1.2',
+    ],
+    entry_points={
+        'console_scripts': [
+            'stone-burner = stone_burner.cli:main',
+        ],
+    },
+    keywords=['terraform', 'planetary-annihilation', 'infrastructure-as-code'],
+)
