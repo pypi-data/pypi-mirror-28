2cbca83ebc75f0af00e3affd301ec08e675edc67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:05 +0000

    Add package ph4-moneropy/0.0.2/ph4_moneropy-0.0.2.tar.gz

---
+++ packages/ph4-moneropy/ph4_moneropy-0.0.2.tar.gz/ph4_moneropy-0.0.2/setup.py
@@ -0,0 +1,25 @@
+import os
+from setuptools import setup, find_packages
+
+# Utility function to read the README file.
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name = "ph4_moneropy",
+    version = "0.0.2",
+    description = "A python toolbox for Monero.",
+    long_description=read('README.md'),
+    url = "https://github.com/monero-monitor/moneropy",
+    keywords = "monero",
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Topic :: Utilities",
+    ],
+    license = "BSD-3-Clause"
+)
