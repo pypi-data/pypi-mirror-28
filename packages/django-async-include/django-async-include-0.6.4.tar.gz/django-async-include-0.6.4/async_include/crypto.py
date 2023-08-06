5dbb1aa15b8820e0e6d8436732d3c87adc43196b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:13 +0000

    Add package django-async-include/0.6.4/django-async-include-0.6.4.tar.gz

---
+++ packages/django-async-include/django-async-include-0.6.4.tar.gz/django-async-include-0.6.4/async_include/crypto.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+from Crypto.Cipher import AES
+
+
+def encrypt(key, data):
+    cipher = AES.new(key.encode("utf-8"), AES.MODE_EAX)
+    encrypted_data, tag = cipher.encrypt_and_digest(data.encode("utf-8"))
+    return cipher.nonce, encrypted_data, tag
+
+
+def decrypt(key, nonce, encrypted_data, tag):
+    cipher = AES.new(key.encode("utf-8"), AES.MODE_EAX, nonce.encode("latin-1"))
+    data = cipher.decrypt_and_verify(encrypted_data.encode("latin-1"), tag.encode("latin-1"))
+    return data.encode("utf-8")
