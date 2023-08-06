e3b1312ba12394780c371dbf52fdf85fc06ada43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:58 +0000

    Add package invain/0.3.2/invain-0.3.2-py3-none-any.whl

---
+++ packages/invain/invain-0.3.2-py3-none-any.whl/invain/Utils.py
@@ -0,0 +1,29 @@
+import requests
+from .__version__ import __version__
+
+def is_sequence(arg):
+    #If arg has strip function it is a String so not sequence. If it has __getitem__ or __iter__ attribute it is iterable
+    return not hasattr(arg, "strip") and (hasattr(arg, "__getitem__") or hasattr(arg, "__iter__"))
+
+def fetch(tickers, fields):
+        BASE_URL = 'http://aws.inva.in/quotes'
+        headers = {'User-Agent': 'InVaIN/'+__version__}        
+
+        #Stringify if list
+        if is_sequence(tickers):
+            tickers = ','.join(tickers)
+            
+        if is_sequence(fields):
+            fields = ','.join(fields)
+            
+        #Prepare the Parameters
+        temp = {'t':tickers, 'f': fields}        
+
+        #Pull Stock Data
+        response = requests.get(BASE_URL, params = temp, headers=headers)
+
+        #Parse into Dict Object
+        data = response.json()
+
+        #return data
+        return data
---
+++ packages/invain/invain-0.3.2-py3-none-any.whl/invain/__init__.py
@@ -0,0 +1,7 @@
+# deprecated to keep older scripts who import this from breaking
+from .Simple import Simple
+from .Advanced import Advanced
+
+
+__all__ = ['Simple', 'Advanced', 'Utils', '__version__']
+
