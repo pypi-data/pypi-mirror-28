8d945bf9c8d6f76164c9826781ff439722547ecc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:13 +0000

    Add package nesterKick/1.3.0/nesterKick-1.3.0.tar.gz

---
+++ packages/nesterKick/nesterKick-1.3.0.tar.gz/nesterKick-1.3.0/nesterKick.py
@@ -0,0 +1,14 @@
+import sys
+
+def print_lol(the_list, indentation=False, level=0, fh=sys.stdout):
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol(each_item, indentation, level+1, fh)
+        else:
+            if( indentation ):
+                for tab_stop in range(level):
+                    print ("\t", end=" ", file=fh)
+            print(each_item, file=fh)
+
+cast = ["sdf", "sdaf", ["sadf", "asdf"]]
+print_lol(cast, True, 3,fh)
