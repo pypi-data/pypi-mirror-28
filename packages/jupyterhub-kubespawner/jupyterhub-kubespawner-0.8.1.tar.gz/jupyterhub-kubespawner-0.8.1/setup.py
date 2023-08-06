ae8e3cf70c1f88c84bb01b84a440bb7e19f6783c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:08 +0000

    Add package jupyterhub-kubespawner/0.8.1/jupyterhub-kubespawner-0.8.1.tar.gz

---
+++ packages/jupyterhub-kubespawner/jupyterhub-kubespawner-0.8.1.tar.gz/jupyterhub-kubespawner-0.8.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='jupyterhub-kubespawner',
+    version='0.8.1',
+    install_requires=[
+        'jupyterhub>=0.8',
+        'pyYAML',
+        'kubernetes==3.*',
+        'escapism',
+    ],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    description='JupyterHub Spawner targeting Kubernetes',
+    url='http://github.com/jupyterhub/kubespawner',
+    author='Yuvi Panda',
+    author_email='yuvipanda@gmail.com',
+    license='BSD',
+    packages=find_packages(),
+)
