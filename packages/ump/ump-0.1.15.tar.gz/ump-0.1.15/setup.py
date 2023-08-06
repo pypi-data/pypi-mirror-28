643daec385994a36b3882397e57c2e87e4aa789c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:42 +0000

    Add package ump/0.1.15/ump-0.1.15.tar.gz

---
+++ packages/ump/ump-0.1.15.tar.gz/ump-0.1.15/setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name="ump",
+    version='0.1.15',
+    zip_safe=False,
+    platforms='any',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    install_requires=['redis', 'click', 'setproctitle', 'netkit'],
+    url="https://github.com/dantezhu/ump",
+    license="MIT",
+    author="dantezhu",
+    author_email="zny2008@gmail.com",
+    description="upload data safer",
+)
