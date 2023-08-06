f48a7b9f6b37b18661fd696bfd2a056d767291b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:25 +0000

    Add package stringutils/0.3.0/stringutils-0.3.0-py2.py3-none-any.whl

---
+++ packages/stringutils/stringutils-0.3.0-py2.py3-none-any.whl/stringutils-0.3.0.dist-info/DESCRIPTION.rst
@@ -0,0 +1,51 @@
+stringutils
+===========
+
+A functional string utility library for Python 2 and 3. Closely inspired by implementations in Haskell and PHP.
+
+For more documentation, please see `http://stringutils.readthedocs.io <http://stringutils.readthedocs.io/en/develop/>`_.
+
+.. note::
+    This package is still in an early development stage, and it is possible that the naming and API of some functions will change.
+
+Features
+--------
+
+The main design goal of this library is to provide helpful string functions which complement what can already be done with *str*, *str.format* and *textwrap*. As such, it includes:
+
+- A selective set of additional string helper functions.
+- Extended versions of existing *str* methods as functions where useful.
+
+Installation
+------------
+
+Install the latest release from `PyPI <https://pypi.org/project/stringutils/>`_:
+
+.. code-block:: sh
+
+    pip install stringutils
+
+Usage
+-----
+
+All functions are available directly off the :code:`stringutils` package. You may choose to import individual functions by name, or import all.
+
+.. code-block:: python
+
+    from stringutils import reverse, unwords, words
+
+    def reverse_words(string):
+        return unwords(map(reverse, words(string)))
+
+Contribute
+----------
+
+- Source code: https://github.com/huntie/stringutils
+- Issue tracker: https://github.com/huntie/stringutils/issues
+
+License
+-------
+
+The project is licensed under the MIT license.
+
+
---
+++ packages/stringutils/stringutils-0.3.0-py2.py3-none-any.whl/stringutils/__init__.py
@@ -0,0 +1,163 @@
+"""
+    stringutils
+    ~~~~~~~~~~~
+
+    A functional string utility library for Python.
+
+    :copyright: (c) 2018 Alex Hunt.
+    :license: MIT, see LICENSE.txt for more details.
+"""
+
+__version__ = '0.3.0'
+
+import re
+
+def camel_case(string):
+    """
+    Convert a string identifier to :code:`camelCase`.
+    """
+    return lcfirst(pascal_case(string))
+
+def concat(strings):
+    """
+    Concatenate a list of strings into a single string.
+    """
+    return ''.join(strings)
+
+def contains(string, matches):
+    """
+    Determine if a string contains any of the given values. *matches* may be a
+    single string, or a list of strings.
+    """
+    return any([m in string for m in ([matches] if isinstance(matches, str) else matches)])
+
+def contains_all(string, matches):
+    """
+    Determine if a string contains all of the given values.
+    """
+    return all([m in string for m in matches])
+
+def dashed_case(string):
+    """
+    Convert a string identifier to :code:`dashed-case`. If the string is in
+    :code:`snake_case`, capitalization of words will be preserved.
+    """
+    return join(split_identifier(string), '-')
+
+def finish(string, suffix):
+    """
+    Append a single instance of the given value to a string if it does not
+    already end with the value.
+    """
+    pass
+
+def indent(string, prefix=' '*4):
+    """
+    Add a prefix to the beginning of each line in a string, inserting four
+    spaces by default. Lines which are empty or contain only whitespace will be
+    trimmed.
+    """
+    return unlines(['' if is_whitespace(l) else prefix + l for l in lines(string)])
+
+def is_whitespace(string):
+    """
+    Determine if a string contains only whitespace characters or is empty.
+    """
+    return string.strip() == ''
+
+def join(strings, sep=', ', insertend=False):
+    """
+    Concatenate a list of strings into a single string by a separating
+    delimiter. If *insertend* is given and true, the delimiter is also included
+    at the end of the string.
+    """
+    return sep.join(strings) + (sep if insertend else '')
+
+def lines(string, keepends=False):
+    """
+    Split a string into a list of strings at newline characters. Unless
+    *keepends* is given and true, the resulting strings do not have newlines
+    included.
+    """
+    return string.splitlines(keepends)
+
+def lcfirst(string):
+    """
+    Convert the first character of a string to lowercase.
+    """
+    return string[:1].lower() + string[1:]
+
+def pascal_case(string):
+    """
+    Convert a string identifier to :code:`PascalCase`.
+    """
+    return concat(map(ucfirst, split_identifier(string)))
+
+def reverse(string):
+    """
+    Reverse the order of the characters in a string.
+    """
+    return string[::-1]
+
+def snake_case(string):
+    """
+    Convert a string identifier to :code:`snake_case`. If the string is in
+    :code:`dashed-case`, capitalization of words will be preserved.
+    """
+    return join(split_identifier(string), '_')
+
+def split_identifier(string):
+    """
+    Split a string identifier into a list of its subparts.
+    """
+    return (
+        re.split('[ \-_]', string)
+            if re.findall('[ \-_]', string)
+            else words(re.sub(r'([a-z])([A-Z0-9])', r'\1 \2', string))
+    )
+
+def start(string, prefix):
+    """
+    Prepend a single instance of the given value to a string if it does not
+    already end with the value.
+    """
+    pass
+
+def title_case(string):
+    """
+    Convert a string identifier to :code:`Title Case`.
+    """
+    return join(map(ucfirst, split_identifier(string)), ' ')
+
+def shorten(string, length=80, placeholder='…'):
+    """
+    Collapse and shorten a string to fit in the given *length*. [Mention placeholder]
+    """
+    # return str.slice(0, length > 0 ? length : 100).replace(/\s\w+$/, '…');
+    return unwords(words(string))
+
+def words(string):
+    """
+    Split a string into a list of words, which were delimited by one or more
+    whitespace characters.
+    """
+    return re.split('\s+', string)
+
+def ucfirst(string):
+    """
+    Convert the first character of a string to uppercase.
+    """
+    return string[:1].upper() + string[1:]
+
+def unlines(lines, newline='\n'):
+    """
+    Join a list of lines into a single string after appending a terminating
+    newline character to each.
+    """
+    return join(lines, newline, True)
+
+def unwords(words):
+    """
+    Join a list of words into a single string with separating spaces.
+    """
+    return join(words, ' ')
