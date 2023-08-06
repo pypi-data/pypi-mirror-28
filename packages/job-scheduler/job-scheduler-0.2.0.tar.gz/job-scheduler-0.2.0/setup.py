b634aa3638107635dbf5d1c2f97b3e35210fe6fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:51 +0000

    Add package job-scheduler/0.2.0/job-scheduler-0.2.0.tar.gz

---
+++ packages/job-scheduler/job-scheduler-0.2.0.tar.gz/job-scheduler-0.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='job-scheduler',
+    packages=['haydn.scheduler'],
+    version='0.2.0',
+    description='Simple Scheduler for Python 3.X',
+    author='Jason Lee',
+    url='https://bitbucket.org/haydn_team/scheduler/',
+    download_url='https://bitbucket.org/haydn_team/scheduler.git'
+)
