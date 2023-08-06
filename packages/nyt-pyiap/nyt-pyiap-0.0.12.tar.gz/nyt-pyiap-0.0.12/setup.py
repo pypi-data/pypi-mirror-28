1ba749f37534f1793d3fbe220e3c031122a3372c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:03 +0000

    Add package nyt-pyiap/0.0.12/nyt-pyiap-0.0.12.tar.gz

---
+++ packages/nyt-pyiap/nyt-pyiap-0.0.12.tar.gz/nyt-pyiap-0.0.12/setup.py
@@ -0,0 +1,35 @@
+import os.path
+from pip.download import PipSession
+from pip.req import parse_requirements
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+def read(filename):
+    return open(os.path.join(os.path.dirname(__file__), filename)).read()
+
+setup(
+    name='nyt-pyiap',
+    version='0.0.12',
+    author='Jeremy Bowers',
+    author_email='jeremy.bowers@nytimes.com',
+    url='https://github.com/newsdev/nyt-pyiap',
+    description='Python utility functions and Django/Flask middlewares for validating JWT tokens from Google\'s Identity-Aware Proxy',
+    long_description=read('README.rst'),
+    packages=('pyiap',),
+    entry_points={},
+    license="Apache License 2.0",
+    keywords='google identity-aware proxy iap jwt',
+    install_requires=['pyjwt', 'requests', 'google', 'google-auth', 'requests_toolbelt'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+    ]
+)
