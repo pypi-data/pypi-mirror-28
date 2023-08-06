5bbb4d084663964df65921a6cec2edbbb97a60e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:44 +0000

    Add package vitriolic/1.3.2/vitriolic-1.3.2.tar.gz

---
+++ packages/vitriolic/vitriolic-1.3.2.tar.gz/vitriolic-1.3.2/touchtechnology/common/middleware.py
@@ -0,0 +1,45 @@
+import logging
+import socket
+import sys
+
+import django
+import pytz
+from django.utils import timezone
+from django.utils.deprecation import MiddlewareMixin
+from touchtechnology.common.utils import get_timezone_from_request
+
+logger = logging.getLogger(__name__)
+
+
+class ServedByMiddleware(MiddlewareMixin):
+    """
+    Expose in our HTTP response headers a number of useful environment values
+    that may be useful during debugging.
+    """
+    def process_response(self, request, response):
+        if 'HTTP_X_DETAIL' in request.META or response.status_code >= 500:
+            response['X-Django-Version'] = django.get_version()
+            response['X-Python-Version'] = ' '.join(sys.version.split())
+            response['X-Pytz-Version'] = pytz.__version__
+            response['X-Served-By'] = socket.gethostname()
+            response['X-Timezone-Name'] = timezone.get_current_timezone_name()
+        return response
+
+
+class TimezoneMiddleware(MiddlewareMixin):
+
+    def process_request(self, request):
+        tzinfo = get_timezone_from_request(request)
+        if tzinfo is not None:
+            logger.debug(
+                'Activating user timezone "%s" for this request.', tzinfo)
+            request.tzinfo = tzinfo
+            timezone.activate(tzinfo)
+
+    def process_response(self, request, response):
+        if hasattr(request, 'tzinfo'):
+            logger.debug(
+                'Forcibly reset the timezone from "%s" back to "%s".',
+                request.tzinfo, timezone.get_default_timezone_name())
+            timezone.deactivate()
+        return response
