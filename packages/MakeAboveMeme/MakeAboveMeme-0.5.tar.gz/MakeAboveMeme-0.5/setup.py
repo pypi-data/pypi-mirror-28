e234c6c7e94876025d68f24e3a1a59c9bbbe3225
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:58 +0000

    Add package MakeAboveMeme/0.5/MakeAboveMeme-0.5.tar.gz

---
+++ packages/MakeAboveMeme/MakeAboveMeme-0.5.tar.gz/MakeAboveMeme-0.5/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='MakeAboveMeme',
+    version='0.5',
+    packages=['mAm',],
+    description="Use the commandline to create a simple text-above-image meme in the stlye of 9gag posts",
+    long_description=open('README.md').read(),
+    author = 'Eric Mink aka LucidBrot',
+    author_email = 'eric@mink.li',
+    url = 'https://github.com/lucidBrot/MakeAboveMeme',
+    install_requires=[
+        'webkit2png'
+    ],
+    zip_safe=False,
+    scripts=['bin/makeAboveMeme']
+) # docopt is not required because it is self-contained in this distribution
