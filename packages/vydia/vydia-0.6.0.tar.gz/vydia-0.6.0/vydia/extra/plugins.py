202cc7fa495eb9ce9b1709c7920fbda0f45582d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:03 +0000

    Add package vydia/0.6.0/vydia-0.6.0.tar.gz

---
+++ packages/vydia/vydia-0.6.0.tar.gz/vydia-0.6.0/vydia/extra/plugins.py
@@ -0,0 +1,128 @@
+"""
+Plugins for video backends
+"""
+
+import os
+import random
+import collections
+from abc import ABC, abstractmethod, abstractproperty
+
+from typing import Any, List, Tuple, Optional, Type, Callable
+
+import pafy
+
+from .utils import get_video_duration
+
+
+VideoData = collections.namedtuple(
+    'VideoData', ['title', 'duration', 'get_file_stream']
+)  # type: Tuple[str, int, Callable[[], str]]
+
+class Video(object):
+    @classmethod
+    def from_pafy(cls: Type['Video'], obj: Any) -> 'Video':
+        return cls(VideoData(
+            title=obj.title,
+            duration=obj.length,
+            get_file_stream=lambda: obj.getbest().url
+        ))
+
+    @classmethod
+    def from_filepath(cls: Type['Video'], path: str) -> 'Video':
+        return cls(VideoData(
+            title=os.path.basename(path),
+            duration=get_video_duration(path),
+            get_file_stream=lambda: path
+        ))
+
+    def __init__(self, obj: VideoData) -> None:
+        self._obj = obj
+
+    def __getattr__(self, key: str) -> Any:
+        return self._obj._asdict()[key]
+
+class Playlist(List['Video']):
+    def __init__(self) -> None:
+        self._title = ''
+        super().__init__()
+
+    @property
+    def title(self) -> str:
+        return self._title
+
+    @property
+    def duration(self) -> int:
+        return sum([v.duration for v in self])
+
+    def reverse(self) -> None:
+        tmp = self[:]
+        self.clear()
+
+        for v in reversed(tmp):
+            self.append(v)
+
+    def shuffle(self) -> None:
+        tmp = self[:]
+        self.clear()
+
+        random.shuffle(tmp)
+        for v in tmp:
+            self.append(v)
+
+    def get_video_by_title(
+        self, title: str
+    ) -> Tuple[Optional[int], Optional[Video]]:
+        for i, vid in enumerate(self):
+            if vid.title == title:
+                return i, vid
+        return None, None
+
+class BasePlugin(ABC):
+    @abstractmethod
+    def extract_playlist(self, url: str) -> Optional[Playlist]:
+        """ Return playlist object
+            None if invalid url
+        """
+
+class FilesystemPlugin(BasePlugin):
+    def extract_playlist(self, url: str) -> Optional[Playlist]:
+        url = os.path.abspath(url)
+        if not os.path.exists(url):
+            return None
+
+        pl = Playlist()
+        pl._title = url
+
+        paths = []
+        for entry in os.scandir(url):
+            if entry.is_dir():
+                print(f'Skipping {entry.path}')
+                continue
+            paths.append(entry.path)
+
+        for fp in sorted(paths):
+            pl.append(Video.from_filepath(fp))
+
+        return pl
+
+class YoutubePlugin(BasePlugin):
+    def extract_playlist(self, url: str) -> Optional[Playlist]:
+        try:
+            res = pafy.get_playlist2(url)
+        except ValueError:
+            return None
+        pl = Playlist()
+
+        pl._title = res.title
+        for vid in res:
+            pl.append(Video.from_pafy(vid))
+
+        return pl
+
+#    def handle_video(self, vid):
+#        video = pafy.new(url, basic=False)
+#        print(video.title, video.duration)
+#
+#        best = video.getbest()
+#        print(best.url, best.resolution, best.get_filesize())
+#        #best.download(filepath='/tmp/', quiet=False)
---
+++ packages/vydia/vydia-0.6.0.tar.gz/vydia-0.6.0/vydia/extra/utils.py
@@ -0,0 +1,72 @@
+"""
+Various utility functions
+"""
+
+import time
+import datetime
+import subprocess
+
+from typing import Iterable, Type, Tuple, Dict, Any, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from .plugins import BasePlugin, Playlist
+
+
+def get_plugins() -> Iterable[Type['BasePlugin']]:
+    """ Return list of available plugins
+    """
+    from .plugins import BasePlugin
+    for Cls in BasePlugin.__subclasses__():
+        yield Cls
+
+def ts2sec(ts: str) -> int:
+    x = time.strptime(ts, '%H:%M:%S')
+    sec = datetime.timedelta(
+        hours=x.tm_hour,
+        minutes=x.tm_min,
+        seconds=x.tm_sec).total_seconds()
+    return int(sec)
+
+def sec2ts(sec: int) -> str:
+    return time.strftime("%H:%M:%S", time.gmtime(sec))
+
+def get_video_duration(fname: str) -> int:
+    proc = subprocess.Popen(
+        ['ffprobe', fname],
+        stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    stdout, stderr = proc.communicate()
+
+    if proc.returncode != 0:
+        return -1
+
+    dur_lines = [x.decode()
+                 for x in stdout.split(b'\n')
+                 if b'Duration' in x]
+    assert len(dur_lines) == 1, dur_lines
+
+    dur_str = dur_lines[0].split()[1].split('.')[0]
+    return ts2sec(dur_str)
+
+def load_playlist(_id: str) -> Tuple[str, 'Playlist']:
+    for Plg in get_plugins():
+        plugin = Plg()
+        playlist = plugin.extract_playlist(_id)
+        if playlist is not None:
+            return (Plg.__name__, playlist)
+    raise ValueError(f'Playlist "{_id}" could not be loaded')
+
+def nested_dict_update(
+    cur_dict: Dict[Any, Any],
+    update_data: Dict[Any, Any]
+) -> Dict[Any, Any]:
+    for k, v in update_data.items():
+        if isinstance(v, dict):
+            old_val = cur_dict.get(k, {})
+            if isinstance(old_val, dict):
+                r = nested_dict_update(old_val, v)
+            else:
+                r = v
+            cur_dict[k] = r
+        else:
+            cur_dict[k] = update_data[k]
+    return cur_dict
