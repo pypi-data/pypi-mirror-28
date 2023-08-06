c6d03bdb6c4cc91a73acb33b7dd53ccb7ec27797
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:34 +0000

    Add package poly-escape-graph/2.5/poly-escape-graph-2.5.tar.gz

---
+++ packages/poly-escape-graph/poly-escape-graph-2.5.tar.gz/poly-escape-graph-2.5/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name="poly-escape-graph",
+    version="2.5",
+    description='Python graph oriented micro services network in order to implement a scalable plugins architecture',
+    long_description=long_description,
+    author='PEE',
+    url='https://mjollnir.unice.fr/bitbucket/scm/pee/private.git',
+    python_requires='>=3.6, <4',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    install_requires=['flask', 'flask_sqlalchemy', 'requests'],
+    license="Unlicense"
+)
