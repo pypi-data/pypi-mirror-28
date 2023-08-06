804dc215d3d9717b249f70510e61193d32620709
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:00 +0000

    Add package aiocarbon/0.1.2/aiocarbon-0.1.2-py3-none-any.whl

---
+++ packages/aiocarbon/aiocarbon-0.1.2-py3-none-any.whl/aiocarbon/protocol/tcp.py
@@ -0,0 +1,36 @@
+import asyncio
+import logging
+
+from aiocarbon.metric import Metric
+from .base import BaseClient
+
+
+log = logging.getLogger(__name__)
+
+
+class TCPClient(BaseClient):
+    async def send(self):
+        async with self as metrics:
+            reader, writer = await asyncio.open_connection(
+                self._host, self._port, loop=self.loop
+            )
+
+            for metric in metrics:
+                value = self.format_metric(metric)
+                writer.write(value)
+
+            await writer.drain()
+            writer.close()
+            reader.feed_eof()
+
+    def format_metric(self, metric: Metric) -> bytes:
+        if isinstance(metric.value, float):
+            value = "%.12f" % metric.value
+        else:
+            value = metric.value
+
+        return (
+            "%s %s %s\n" % (
+                self.format_metric_name(metric), value, metric.timestamp,
+            )
+        ).encode()
