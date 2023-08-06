660a88eed6bebc7862333a07ef1f94e75f69f00a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:22 +0000

    Add package txHTTP/0.0.2a1/txHTTP-0.0.2a1.tar.gz

---
+++ packages/txHTTP/txHTTP-0.0.2a1.tar.gz/txHTTP-0.0.2a1/txhttp/application.py
@@ -0,0 +1,19 @@
+from klein import Klein
+
+
+class Application(Klein):
+    middleware = {}
+
+    def set_handler(self, path, handler, *args, **kwargs):
+        if not hasattr(handler, '__module__') or not hasattr(handler, '__name__'):
+            raise TypeError(f'Invalid handler passed.')
+        kwargs.setdefault('endpoint', f'{handler.__module__}.{handler.__name__}')
+        self.route(path, *args, **kwargs)(handler)
+
+    def __getitem__(self, item):
+        if item not in self.middleware:
+            raise KeyError(f'Middleware {item} is not registered')
+        return self.middleware[item]
+
+    def __setitem__(self, key, value):
+        self.middleware[key] = value
