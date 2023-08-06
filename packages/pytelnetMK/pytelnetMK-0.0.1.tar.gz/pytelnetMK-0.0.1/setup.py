4548e5ebfa2b45d5765bc167e7c0b42ffd1a971a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:47 +0000

    Add package pytelnetMK/0.0.1/pytelnetMK-0.0.1.tar.gz

---
+++ packages/pytelnetMK/pytelnetMK-0.0.1.tar.gz/pytelnetMK-0.0.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+setup(
+
+    name    =	'pytelnetMK',
+
+    version =	'0.0.1',
+
+    python_requires ='>=3',
+
+    py_modules	=   ['pytelnetMK'],
+
+    author  =	'pklarner',
+
+    author_email    =	'pedro@preciseipostei.com.br',
+
+    url	=   'http://www.preciseipostei.com.br',
+
+    install_requires=['telnetlib3 >=1.0.0'],
+
+    description	=   'Connect Mikrotik telnet server using Telnetlib ',
+    
+    )
+
+
+
+
+
