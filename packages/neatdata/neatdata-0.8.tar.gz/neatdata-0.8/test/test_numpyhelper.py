8c093f9a794f20d1de1005d4975151217d586e1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:10 +0000

    Add package neatdata/0.8/neatdata-0.8.tar.gz

---
+++ packages/neatdata/neatdata-0.8.tar.gz/neatdata-0.8/test/test_numpyhelper.py
@@ -0,0 +1,20 @@
+import unittest
+import pandas as pd
+import numpy as np
+from neatdata.neatdata import *
+
+class TestNumpyHelper(unittest.TestCase):
+
+    def testNumpyHelper_CastAsNumpy_IsStringType(self):
+        # Assemble
+        trainYStrings = ['a', 'a', 'a', 'a', 'b', 'c', 'd']
+        trainYNumbers = [1, 1, 1, 1, 2, 3, 4]
+        yCleaner = YCleaner()
+        trainYStrings = castAsNumpy(trainYStrings)
+        trainYNumbers = castAsNumpy(trainYNumbers)
+        # Act
+        isStringTypeTrue = isStringType(trainYStrings)
+        isStringTypeFalse = isStringType(trainYNumbers)
+        # Assert
+        self.assertTrue(isStringTypeTrue)
+        self.assertFalse(isStringTypeFalse)
