be6473fcfd3a50604ebb25ae81d82b5ed761bf58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:20 +0000

    Add package analytracks/0.2.1.9/analytracks-0.2.1.9.tar.gz

---
+++ packages/analytracks/analytracks-0.2.1.9.tar.gz/analytracks-0.2.1.9/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(name='analytracks',
+      version='0.2.1.9',
+      description='This python package contains tools to handle sport gps tracks (tcx, gpx, ...)',
+      long_description=readme(),
+      url='http://www.olbecke.eu/research',
+      author='Dimitri de Smet',
+      author_email='dimitri.desmet@uclouvain.be',
+      license='MIT',
+      packages=['analytracks'],
+      install_requires=['pandas'],
+      package_data={'mypkg': ['data/*.dat']},
+      zip_safe=True)
