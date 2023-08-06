20b0a25eaf39207e81f04074ce0502859fedaff8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:04 +0000

    Add package pyutmpx/0.1.2/pyutmpx-0.1.2.tar.gz

---
+++ packages/pyutmpx/pyutmpx-0.1.2.tar.gz/pyutmpx-0.1.2/setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+#******************************************************************************
+# Copyright (C) 2017-2018 Thomas "Cakeisalie5" Touhey <thomas@touhey.fr>
+# This file is part of the pyutmpx Python 3.x module, which is MIT-licensed.
+#******************************************************************************
+""" Setup script for the utmpx module. """
+
+import os, subprocess
+from setuptools import setup, Extension
+
+# Find the sources
+srcdir = "src"
+incdir = "include"
+
+src = os.listdir(srcdir)
+src = filter(lambda x: x.split('.')[-1] in ("c",), src)
+src = map(lambda x: os.path.join(srcdir, x), src)
+src = list(src)
+
+# Make the setup
+setup(
+	name="pyutmpx",
+	version="0.1.2",
+	url="https://forge.touhey.fr/pyutmpx.git/",
+	license='MIT',
+	keywords='utmp utmpx',
+	description="utmp reader module for Python 3.x",
+	author='Thomas "Cakeisalie5" Touhey',
+	author_email='thomas@touhey.fr',
+
+	ext_modules=[Extension("pyutmpx", src,
+		include_dirs=[incdir], library_dirs=[], libraries=[])],
+
+	classifiers = [
+		'License :: OSI Approved :: MIT License',
+	]
+)
+
+# End of file.
---
+++ packages/pyutmpx/pyutmpx-0.1.2.tar.gz/pyutmpx-0.1.2/src/main.c
@@ -0,0 +1,89 @@
+/* ****************************************************************************
+ * main.c -- pyutmpx module definition.
+ * Copyright (C) 2017-2018 Thomas "Cakeisalie5" Touhey <thomas@touhey.fr>
+ *
+ * This file is part of the pyutmpx Python 3.x module, which is MIT-licensed.
+ * ************************************************************************* */
+#include <pyutmpx.h>
+#include <datetime.h>
+
+/* Utility to get a datetime from a timeval. */
+
+PyObject *pyutmpx_get_datetime(const struct timeval *tv)
+{
+	PyObject *result = NULL;
+	PyObject *epoch = NULL, *date_delta = NULL;
+
+	epoch = PyDateTime_FromDateAndTime(1970, 1, 1, 0, 0, 0, 0);
+	date_delta = PyDelta_FromDSU(0, tv->tv_sec, tv->tv_usec);
+	if (!epoch || !date_delta)
+		goto fail;
+
+	result = PyNumber_Add(epoch, date_delta);
+
+fail:
+	Py_XDECREF(epoch);
+	Py_XDECREF(date_delta);
+
+	return (result);
+}
+
+/* Module methods. */
+
+static PyMethodDef module_methods[] = {
+	{NULL, NULL, 0, NULL}
+};
+
+/* Module definition. */
+
+static struct PyModuleDef module = {
+	PyModuleDef_HEAD_INIT,
+
+	/* General information. */
+	.m_name = "pyutmpx",
+	.m_doc = "This module provides a utmp reader.",
+	.m_methods = module_methods,
+
+	/* Keep state in global variables. */
+	.m_size = -1,
+};
+
+/* Module initialization. */
+
+PyMODINIT_FUNC PyInit_pyutmpx(void)
+{
+	PyObject *m;
+
+	/* Initialize the module. */
+	m = PyModule_Create(&module);
+	if (!m)
+		goto fail;
+
+	/* Import the datetime module. */
+	PyDateTime_IMPORT;
+
+	/* Add the utmp iterator type to the module. */
+	if (PyType_Ready(&pyutmpx_utmp) < 0)
+		goto fail;
+	Py_INCREF(&pyutmpx_utmp);
+	PyModule_AddObject(m, "utmp", (PyObject*)&pyutmpx_utmp);
+
+	/* Add the utmp entry type to the module. */
+	if (PyType_Ready(&pyutmpx_entry) < 0)
+		goto fail;
+	Py_INCREF(&pyutmpx_entry);
+	PyModule_AddObject(m, "utmp_entry", (PyObject*)&pyutmpx_entry);
+
+	/* Make the constants and add them to the module. */
+	if (pyutmpx_prepare_types(m))
+		goto fail;
+
+	/* Everything went well in the end :) */
+	return (m);
+fail:
+	pyutmpx_destroy_types();
+	Py_XDECREF(&pyutmpx_utmp);
+	Py_XDECREF(&pyutmpx_entry);
+	Py_XDECREF(m);
+	return (NULL);
+}
