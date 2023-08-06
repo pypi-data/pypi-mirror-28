2992cde57719c159d24343a7cbb97e62c85caf1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:25 +0000

    Add package django-slackchat-serializer/0.0.7/django-slackchat-serializer-0.0.7.tar.gz

---
+++ packages/django-slackchat-serializer/django-slackchat-serializer-0.0.7.tar.gz/django-slackchat-serializer-0.0.7/slackchat/admin.py
@@ -0,0 +1,32 @@
+from django.contrib import admin
+
+from .models import (Argument, Channel, ChatType, CustomContentTemplate,
+                     KeywordArgument, Message, Reaction, User)
+
+
+class ChannelAdmin(admin.ModelAdmin):
+    readonly_fields = ('api_id',)
+    fieldsets = (
+        (None, {
+            'fields': ('api_id', 'chat_type', 'owner')
+        }),
+        ('Publishing', {
+            'fields': ('publish_path',)
+        }),
+        ('Display', {
+            'fields': ('image', 'title', 'introduction')
+        }),
+        ('Meta', {
+            'fields': ('meta_title', 'meta_description', 'meta_keywords')
+        })
+    )
+
+
+admin.site.register(Argument)
+admin.site.register(Channel, ChannelAdmin)
+admin.site.register(User)
+admin.site.register(CustomContentTemplate)
+admin.site.register(Message)
+admin.site.register(Reaction)
+admin.site.register(KeywordArgument)
+admin.site.register(ChatType)
