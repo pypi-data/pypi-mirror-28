945b5e44f79525d91cafc2e064e2fa0cc38bfa56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:19 +0000

    Add package nester2701/1.1.0/nester2701-1.1.0.tar.gz

---
+++ packages/nester2701/nester2701-1.1.0.tar.gz/nester2701-1.1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+        name = 'nester2701',
+        version = '1.1.0',
+        py_modules = ['nester'],
+        author = 'polinelottin',
+        author_email = 'lottinp@gmail.com',
+        description = 'a porcaria de um modulo que nao faz upload',
+        )
