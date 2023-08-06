be272dcbfaf31869faa97b2b590989d9ff9a9de2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:53 +0000

    Add package gitrisky/0.1.0b0/gitrisky-0.1.0b0.tar.gz

---
+++ packages/gitrisky/gitrisky-0.1.0b0.tar.gz/gitrisky-0.1.0b0/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+
+setup(
+
+    name='gitrisky',
+
+    description='Predict code bug risk with git metadata',
+
+    version='0.1.0b',
+
+    url='https://github.com/hinnefe2/gitrisky',
+
+    download_url='https://github.com/hinnefe2/gitrisky/archive/v0.1.0b.tar.gz',
+
+    author='J. Henry Hinnefeld',
+
+    author_email='henry.hinnefeld@gmail.com',
+
+    packages=find_packages(),
+
+    install_requires=[
+        'click>=6.7',
+        'GitPython>=2.1',
+        'numpy>=1.13',
+        'pandas>=0.20',
+        'scikit-learn>=0.19.0',
+        ],
+
+
+    include_package_data=True,
+
+    entry_points={
+        'console_scripts': ['gitrisky=gitrisky.cli:cli'],
+    },
+)
