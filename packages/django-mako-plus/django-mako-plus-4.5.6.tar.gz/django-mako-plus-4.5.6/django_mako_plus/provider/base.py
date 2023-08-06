abd723882fc7aecc2304d41f1d90acc8aa8e276e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:43 +0000

    Add package django-mako-plus/4.5.6/django-mako-plus-4.5.6.tar.gz

---
+++ packages/django-mako-plus/django-mako-plus-4.5.6.tar.gz/django-mako-plus-4.5.6/django_mako_plus/provider/base.py
@@ -0,0 +1,103 @@
+from django.core.exceptions import ImproperlyConfigured
+from django.utils.module_loading import import_string
+from django.utils.encoding import force_text
+
+from ..util import DMP_OPTIONS, merge_dicts
+
+import os
+import os.path
+
+
+
+##################################################
+###   Static File Provider Factory
+
+
+DEFAULT_CONTENT_PROVIDERS = [
+    { 'provider': 'django_mako_plus.CssLinkProvider' },
+    { 'provider': 'django_mako_plus.JsLinkProvider'  },
+    { 'provider': 'django_mako_plus.JsContextProvider' },
+    # deprecated as of Oct 2017
+    { 'provider': 'django_mako_plus.MakoCssProvider'  },
+    { 'provider': 'django_mako_plus.MakoJsProvider'  },
+]
+
+
+def init_providers():
+    '''Called when the DMP template engine is created by Django'''
+    DMP_OPTIONS['RUNTIME_PROVIDERS'] = [ ProviderFactory(provider_def) for provider_def in DMP_OPTIONS.get('CONTENT_PROVIDERS', DEFAULT_CONTENT_PROVIDERS) ]
+    DMP_OPTIONS['RUNTIME_PROVIDERS'].sort(key=lambda pf: pf.options['weight'], reverse=True)
+
+
+class ProviderFactory(object):
+    '''Creator for a given Provider definition in settings.py.'''
+    def __init__(self, provider_def):
+        self.options = {}
+        try:
+            self.provider_class = provider_def['provider']
+        except KeyError:
+            raise ImproperlyConfigured('The Django Mako Plus template OPTIONS were not set up correctly in settings.py; a CONTENT_PROVIDERS item is missing `provider`.')
+        if isinstance(self.provider_class, str):
+            self.provider_class = import_string(self.provider_class)
+        if not issubclass(self.provider_class, BaseProvider):
+            raise ImproperlyConfigured('The Django Mako Plus template OPTIONS were not set up correctly in settings.py; The `provider` value must be a subclass of django_mako_plus.BaseProvider.')
+        self.options = merge_dicts(self.provider_class.default_options, provider_def)
+        
+    def create(self, app_config, template_path, version_id):
+        return self.provider_class(app_config, template_path, self.options, version_id)
+        
+        
+
+##############################################################
+###   Static File Providers
+
+class BaseProvider(object):
+    '''
+    A list of providers is attached to each template as it is process.
+    These are cached with the template, so .__init__() is only called once
+    per system runtime, while .append_content() is called for each
+    request.  Optimize the methods accordingly.
+    '''
+    default_options = {
+        'group': 'styles',
+        'weight': 0,  # higher weights run first
+    }
+    def __init__(self, app_config, template_path, options, version_id):
+        self.app_config = app_config
+        self.template_path = template_path
+        # this next variable assumes the template is in the "normal" location: app/templates/
+        parts = os.path.splitext(self.template_path)[0].split(os.path.sep)
+        self.template_name = os.path.sep.join(parts[1:]) if len(parts) > 1 else self.template_path
+        self.options = merge_dicts(self.default_options, options)     # combined options dictionary
+        self.version_id = version_id                                  # unique number for overriding the cache (see LinkProvider)
+        self.init()
+        
+        
+    @property
+    def group(self):
+        return self.options['group']
+
+
+    def init(self):
+        '''Called at the end of the constructor.'''
+        
+        
+    def get_content(self, provider_run):
+        '''
+        Called each time the template renders.  Should return a string 
+        to be included in the HTML output, or None if no content.
+        '''
+        return None
+        
+        
+    def format_string(self, val):
+        '''
+        Helper function that runs st.format with some standard named options.
+        val.format() is called with {appname}, {appdir}, {template}.
+        '''
+        return force_text(val).format(
+            appname=self.app_config.name,
+            appdir=self.app_config.path,
+            template=self.template_name,
+        )
+        
---
+++ packages/django-mako-plus/django-mako-plus-4.5.6.tar.gz/django-mako-plus-4.5.6/django_mako_plus/provider/links.py
@@ -0,0 +1,120 @@
+from django.conf import settings
+from django.utils.module_loading import import_string
+
+from ..uid import wuid
+from ..util import merge_dicts
+from ..version import __version__
+from .base import BaseProvider
+
+import os
+import os.path
+import posixpath
+import json
+
+
+class LinkProvider(BaseProvider):
+    '''Base class for providers that create links'''
+    # the default options are for CSS files
+    default_options = merge_dicts(BaseProvider.default_options, {  
+        'group': 'styles',
+        'filename': '{appdir}/somedir/{template}.static.file',
+    })
+    def init(self):
+        self.href = None
+        fullpath = self.format_string(self.options['filename'])
+        if os.path.exists(fullpath):
+            # the version_id is a unique number either given by the project or by reading the last modified time of the file
+            # it is important to add to links (see make_link in the subclasses) because it makes the url unique, which
+            # forces browsers to re-download the file despite a previous version being in their cache.  Without this id,
+            # some browsers (Chrome, I'm looking at you) use the cached version even after updates to the file.
+            if self.version_id is None:
+                self.version_id = int(os.stat(fullpath).st_mtime)
+            # make it relative to the project root, and ensure we have forward slashes (even on windows) because this is for urls
+            self.href = posixpath.join(settings.STATIC_URL, os.path.relpath(fullpath, settings.BASE_DIR))  
+
+    def get_content(self, provider_run):
+        '''Subclasses should override this method'''
+        return '<div class="DMP_LinkProvider">{}</div>'.format(self.href)
+
+
+class CssLinkProvider(LinkProvider):
+    '''Generates a CSS <link>'''
+    default_options = merge_dicts(LinkProvider.default_options, { 
+        'filename': '{appdir}/styles/{template}.css',
+        'skip_duplicates': True,
+    })
+    def get_content(self, provider_run):
+        if self.href is None:
+            return None
+        return '<link id="{uid}" data-context="{contextid}" rel="stylesheet" type="text/css" href="{href}?{version}" />'.format(
+            uid=wuid(),           
+            contextid=provider_run.uid,
+            href=self.href, 
+            version=self.version_id,
+            skip_duplicates='true' if self.options['skip_duplicates'] else 'false',
+        )
+
+
+class JsLinkProvider(LinkProvider):
+    '''Generates a JS <script>.'''
+    default_options = merge_dicts(LinkProvider.default_options, {  
+        'group': 'scripts',
+        'filename': '{appdir}/scripts/{template}.js',
+        'async': False,
+    })
+    def get_content(self, provider_run):
+        if self.href is None:
+            return None
+        return '<script>DMP_CONTEXT.addScript("{uid}", "{contextid}", "{app}/{template}", "{href}?{version}", {async});</script>'.format(
+            uid=wuid(),           
+            contextid=provider_run.uid,  
+            app=self.app_config.name.replace('"', '\\"'),
+            template=self.template_name.replace('"', '\\"'),
+            href=self.href.replace('\\', '/'), 
+            version=self.version_id,
+            async='true' if self.options['async'] else 'false',
+        )
+
+
+class JsContextProvider(BaseProvider):
+    '''Adds all js_context() variables to DMP_CONTEXT'''
+    default_options = merge_dicts(LinkProvider.default_options, {  
+        'group': 'scripts',
+        'encoder': 'django.core.serializers.json.DjangoJSONEncoder',
+    })
+    
+    def init(self):
+        super().init()
+        self.encoder = import_string(self.options['encoder'])
+
+    def get_content(self, provider_run):
+        # send the context with the main template (would be redundant to do for ancestors)
+        html = []
+        html.append('<script>')
+        if provider_run.chain_index == 0:
+            context_data = { k: provider_run.context[k] for k in provider_run.context.kwargs if isinstance(k, jscontext) }
+            html.append('DMP_CONTEXT.set("{version}", "{contextid}", {data});'.format(
+                version=__version__,
+                contextid=provider_run.uid,  
+                data=json.dumps(context_data, cls=self.encoder, separators=(',', ':')) if context_data else '{}',
+            ))
+        html.append('DMP_CONTEXT.linkContextByName("{contextid}", "{app}/{template}");'.format(
+            contextid=provider_run.uid,  
+            app=self.app_config.name.replace('"', '\\"'),
+            template=self.template_name.replace('"', '\\"'),
+        ))
+        html.append('</script>')
+        return ('\n' if settings.DEBUG else '').join(html)
+
+
+
+class jscontext(str):
+    '''
+    Marks a key in the context dictionary as a JS context item.
+    JS context items are sent to the template like normal,
+    but they are also added to the runtime JS namespace.
+    
+    See the tutorial for more information on this function.
+    '''
+    # no code needed, just using the class for identity
+    
