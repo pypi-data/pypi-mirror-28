07b66f817f67d496770843a76f1e21163093f23d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:31 +0000

    Add package LQ/1.1.1/LQ-1.1.1.tar.gz

---
+++ packages/LQ/LQ-1.1.1.tar.gz/LQ-1.1.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'LQ',      
+    version = '1.1.1',
+    py_modules = ['testkey'],
+    author = 'Shawn_67',        
+    author_email = '96967399@qq.com',
+    url = 'https://www.baidu.com/',
+    description = 'baidu'   
+    )
