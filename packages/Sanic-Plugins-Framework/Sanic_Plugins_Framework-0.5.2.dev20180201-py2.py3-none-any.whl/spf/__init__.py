f08445e5139c12d65a84672c7a63887c43b6fc41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:18 +0000

    Add package Sanic-Plugins-Framework/0.5.2.dev20180201/Sanic_Plugins_Framework-0.5.2.dev20180201-py2.py3-none-any.whl

---
+++ packages/Sanic-Plugins-Framework/Sanic_Plugins_Framework-0.5.2.dev20180201-py2.py3-none-any.whl/spf/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: latin-1 -*-
+from .framework import SanicPluginsFramework
+from .plugin import SanicPlugin
+
+__version__ = '0.5.2.dev20180201'
+__all__ = ["SanicPlugin", "SanicPluginsFramework", "__version__"]
