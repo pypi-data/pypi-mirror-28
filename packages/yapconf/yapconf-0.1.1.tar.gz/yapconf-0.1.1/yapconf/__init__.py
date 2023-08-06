6e3607893000a5e032426e95dc2db63616472ccf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:46 +0000

    Add package yapconf/0.1.1/yapconf-0.1.1.tar.gz

---
+++ packages/yapconf/yapconf-0.1.1.tar.gz/yapconf-0.1.1/yapconf/__init__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for Yapconf."""
+from yapconf.spec import YapconfSpec
+
+yaml_support = True
+
+try:
+    import yaml
+except ImportError:
+    try:
+        import ruamel.yaml as yaml
+    except ImportError:
+        yaml = None
+        yaml_support = False
+
+__author__ = """Logan Asher Jones"""
+__email__ = 'loganasherjones@gmail.com'
+__version__ = '0.1.1'
+
+
+FILE_TYPES = ('json',)
+if yaml_support:
+    FILE_TYPES += ('yaml', )
+
+__all__ = ['YapconfSpec']
