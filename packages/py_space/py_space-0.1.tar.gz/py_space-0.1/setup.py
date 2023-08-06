4123d9f190200c6760336742de686f7e73d5465e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:25 +0000

    Add package py_space/0.1/py_space-0.1.tar.gz

---
+++ packages/py_space/py_space-0.1.tar.gz/py_space-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'py_space',
+  packages = ['py_space'], # this must be the same as the name above
+  version = '0.1',
+  description = 'python space',
+  author = 'hellspacebar',
+  author_email = 'hellspacebar@gmail.com',
+  url = 'https://github.com/hellspacebar/py_space', # use the URL to the github repo
+  download_url = 'https://github.com/hellspacebar/py_space/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['space'], # arbitrary keywords
+  classifiers = [],
+)
