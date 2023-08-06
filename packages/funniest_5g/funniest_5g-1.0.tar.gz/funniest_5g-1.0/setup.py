766b7b26b36eef73f4eec4b9d0c7d176a04a8cf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:29 +0000

    Add package funniest_5g/1.0/funniest_5g-1.0.tar.gz

---
+++ packages/funniest_5g/funniest_5g-1.0.tar.gz/funniest_5g-1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='funniest_5g',
+      version='1.0',
+      description='The funniest joke in the world',
+      url='http://github.com/storborg/funniest',
+      author='Cathy',
+      author_email='cathy.mao@nokia-sbell.com',
+      license='MIT',
+      packages=['funniest_5g'],
+      zip_safe=False)
