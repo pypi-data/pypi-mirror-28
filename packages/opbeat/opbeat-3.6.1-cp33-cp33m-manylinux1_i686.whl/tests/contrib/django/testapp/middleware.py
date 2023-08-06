af16d8a34a6f7327bf6f89cd3f4db04fcfb266d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:24:30 +0000

    Add package opbeat/3.6.1/opbeat-3.6.1-cp33-cp33m-manylinux1_i686.whl

---
+++ packages/opbeat/opbeat-3.6.1-cp33-cp33m-manylinux1_i686.whl/tests/contrib/django/testapp/middleware.py
@@ -0,0 +1,27 @@
+try:
+    from django.utils.deprecation import MiddlewareMixin
+except ImportError:
+    # no-op class for Django < 1.10
+    class MiddlewareMixin(object):
+        pass
+
+
+class BrokenRequestMiddleware(MiddlewareMixin):
+    def process_request(self, request):
+        raise ImportError('request')
+
+
+class BrokenResponseMiddleware(MiddlewareMixin):
+    def process_response(self, request, response):
+        raise ImportError('response')
+
+
+class BrokenViewMiddleware(MiddlewareMixin):
+    def process_view(self, request, func, args, kwargs):
+        raise ImportError('view')
+
+
+class MetricsNameOverrideMiddleware(MiddlewareMixin):
+    def process_response(self, request, response):
+        request._opbeat_transaction_name = 'foobar'
+        return response
