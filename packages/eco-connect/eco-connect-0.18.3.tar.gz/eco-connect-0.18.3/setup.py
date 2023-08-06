51eaa999fd357f65a3a60f468b22bd1325fef85d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:53 +0000

    Add package eco-connect/0.18.3/eco-connect-0.18.3.tar.gz

---
+++ packages/eco-connect/eco-connect-0.18.3.tar.gz/eco-connect-0.18.3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+
+def get_requirements():
+    with open("requirements.txt") as f:
+        return f.read().splitlines()
+
+
+setup(
+    name='eco-connect',
+    version='0.18.3',
+    description='Ecorithm\'s eco-connect ',
+    license="Proprietary",
+    url='https://github.com/ecorithm/eco-connect',
+    packages=find_packages(exclude=['tests*']),
+    include_package_data=True,
+    install_requires=get_requirements()
+)
