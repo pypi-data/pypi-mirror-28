b5ffb5835054c094e0d805f7abde8c0677ed38ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:19:00 +0000

    Add package dyckviz/0.3.0/dyckviz-0.3.0.tar.gz

---
+++ packages/dyckviz/dyckviz-0.3.0.tar.gz/dyckviz-0.3.0/README.md
@@ -0,0 +1,20 @@
+Usage:
+```bash
+> pip3 install dyckviz
+> dyckviz -h
+usage: dyckviz [-h] [--dim DIM] [--word WORD] [-o O]
+
+Rendering of Dyck promotions.
+
+optional arguments:
+  -h, --help   show this help message and exit
+  --dim DIM    symbols, size
+  --word WORD  single word to render
+  -o O         output filename
+```
+
+Example first orbit for { k = 3, n = 3 }:
+![Orbit](images/orbits.png)
+
+Example spider web for *aababcbcc*:
+![Web](images/web.png)
---
+++ packages/dyckviz/dyckviz-0.3.0.tar.gz/dyckviz-0.3.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+import re
+
+from sys import version_info
+
+class NotSupportedException(BaseException): pass
+
+if version_info.major < 3:
+    raise NotSupportedException("Only Python 3.x Supported")
+
+setup(
+    name='dyckviz',
+    description='Visualization utilities for Dyck words using Young tableaux and combinatorial spider webs.',
+    entry_points = { "console_scripts": ['dyckviz = dyckviz.dyckviz:main'] },
+    version = '0.3.0',
+    author='Orestis Melkonian, Konstantinos Kogkalidis',
+    author_email='melkon.or@gmail.com, konstantinos@riseup.net',
+    url='http://github.com/omelkonian/dyckviz',
+    packages=find_packages(),
+    include_package_data=True,
+    platforms='any',
+    python_requires='>=3',
+    install_requires=[
+        'ansicolors==1.1.8',
+        'graphviz==0.8.2',
+        'numpy==1.14.0',
+        'pdfrw==0.4',
+    ],
+)
