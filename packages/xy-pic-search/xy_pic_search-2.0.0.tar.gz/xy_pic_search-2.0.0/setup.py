1e164a455831ae6614684262ea03358f7565d883
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:04 +0000

    Add package xy-pic-search/2.0.0/xy_pic_search-2.0.0.tar.gz

---
+++ packages/xy-pic-search/xy_pic_search-2.0.0.tar.gz/xy_pic_search-2.0.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'xy_pic_search',
+  packages = ['xy_pic_search'],
+  version = '2.0.0',
+  description = 'Search The Picture From Baidu By The Keywords',
+  long_description='Search The Picture From Baidu By The Keywords',
+  author = 'KingHS',
+  author_email = '382771946@qq.com',
+  keywords = ['pip3', 'python3','python','Picture Search'],
+  license='MIT',
+  install_requires = ['requests']
+)
