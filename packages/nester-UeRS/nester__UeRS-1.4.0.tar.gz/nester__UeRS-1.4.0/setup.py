f844953af0aa959edc948595663e0d8b7e11fd6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:19:08 +0000

    Add package nester-UeRS/1.4.0/nester__UeRS-1.4.0.tar.gz

---
+++ packages/nester-UeRS/nester__UeRS-1.4.0.tar.gz/nester__UeRS-1.4.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+  name = 'nester__UeRS',
+  version = '1.4.0',
+  py_modules = ['nester__UeRS'],
+  author = 'uendelrocha',
+  author_email = 'uendelrocha@gmail.com',
+  url = 'https://www.facebook.com/uendelrocha',
+  description = 'Imprime listas aninhadas na saida especificada.',
+  )
