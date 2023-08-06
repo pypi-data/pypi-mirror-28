eedac8116b8cb039aaa78508977a12c85698b698
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:14 +0000

    Add package fastavro/0.17.3/fastavro-0.17.3.tar.gz

---
+++ packages/fastavro/fastavro-0.17.3.tar.gz/fastavro-0.17.3/publish.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Push to pypi, tag and push to bitbucket
+
+pyver=$(python -c 'import sys; print("%s%s" % sys.version_info[:2])')
+ver=$(python setup.py --version)
+
+# Fail on 1'st error
+set -e
+set -x
+
+make
+python setup.py sdist
+twine upload dist/fastavro-${ver}.tar.gz
+twine upload dist/fastavro-${ver}*.whl
+
+git tag -f $(python setup.py --version)
+git push
+git push --tags
+# print sha so we can use it in conda-forge recipe
+sha256sum dist/fastavro-${ver}.tar.gz
+rm -fr build dist/* fastavro.egg-info/
