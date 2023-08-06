247bff1a7befecaf1631cd8a52a3283895853258
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:19 +0000

    Add package rumetr-client/0.0.8/rumetr-client-0.0.8.tar.gz

---
+++ packages/rumetr-client/rumetr-client-0.0.8.tar.gz/rumetr-client-0.0.8/rumetr/scrapy/item.py
@@ -0,0 +1,20 @@
+import scrapy
+
+
+class ApptItem(scrapy.Item):
+    complex_name = scrapy.Field()
+    complex_id = scrapy.Field()
+    complex_url = scrapy.Field()
+    addr = scrapy.Field()
+
+    house_id = scrapy.Field()
+    house_name = scrapy.Field()
+    house_url = scrapy.Field()
+
+    id = scrapy.Field()
+    floor = scrapy.Field()
+    room_count = scrapy.Field()
+    square = scrapy.Field()
+    price = scrapy.Field()
+    is_studio = scrapy.Field()
+    plan_url = scrapy.Field()
