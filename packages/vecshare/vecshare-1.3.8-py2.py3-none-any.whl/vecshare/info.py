9a22427a952b67e6daa03af628338b04b5f9881e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:17 +0000

    Add package vecshare/1.3.8/vecshare-1.3.8-py2.py3-none-any.whl

---
+++ packages/vecshare/vecshare-1.3.8-py2.py3-none-any.whl/vecshare/info.py
@@ -0,0 +1,12 @@
+INDEXER      = 'jaredfern/vecshare-indexer'
+INDEX_FILE   = 'index_file'
+INDEX_FILE_PATH ='/home/jared/VecShare/vecshare/index_file.csv'
+AR_SIG_PATH = '/home/jared/VecShare/vecshare/ar_sig.txt'
+LARGE_EMB_TAG= 'vecshare-large'
+SMALL_EMB_TAG= 'vecshare-small'
+SIGNATURES   = 'jaredfern/vecshare-signatures'
+
+BASE_URL     = 'https://data.world/'
+INDEXER_URL  = BASE_URL + INDEXER  # URL for index & signature file
+DATASETS_URL = 'https://data.world/datasets/'
+DW_CLASS_TAG = 'dw-dataset-name DatasetCard__name___2U4-H'
