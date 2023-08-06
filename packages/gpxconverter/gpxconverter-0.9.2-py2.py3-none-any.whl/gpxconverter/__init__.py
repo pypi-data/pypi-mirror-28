5c161de872a28be44408d8c457079e21b300b2d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:40 +0000

    Add package gpxconverter/0.9.2/gpxconverter-0.9.2-py2.py3-none-any.whl

---
+++ packages/gpxconverter/gpxconverter-0.9.2-py2.py3-none-any.whl/gpxconverter/__init__.py
@@ -0,0 +1,22 @@
+__all__ = (
+    "__title__", "__summary__", "__uri__",
+    "__download_url__", "__version__", "__author__",
+    "__email__", "__license__",)
+
+__title__ = "gpxconverter"
+
+__summary__ = ("gpx to csv converter. it supports waypoint elements except "
+               "extensions. Values in extensions will be ignored.")
+
+__uri__ = "https://github.com/linusyoung/GPXConverter"
+
+__version__ = "0.9.2"
+
+__download_url__ = ("https://github.com/linusyoung/GPXConverter/archive/" +
+                    __version__ + ".tar.gz")
+
+__author__ = "Linus Yang"
+
+__email__ = "linusyoungrice@gmail.com"
+
+__license__ = "MIT"
