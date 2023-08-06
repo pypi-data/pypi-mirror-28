c9ebd7a56eb159f4d3348cd74c27cde8e0426cac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:42 +0000

    Add package helios-sdk/0.0.0/helios-sdk-0.0.0.tar.gz

---
+++ packages/helios-sdk/helios-sdk-0.0.0.tar.gz/helios-sdk-0.0.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(name='helios-sdk',
+      version='0.0.0',
+      description='Use the Helios APIs in Python',
+      author='Michael Bayer',
+      author_email='mbayer@harris.com',
+      url='',
+      download_url='',
+      license='MIT',
+      install_requires=[],
+      extras_require={
+          'tests': ['pytest'],
+      },
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.6',
+          'Topic :: Software Development :: Libraries',
+          'Topic :: Software Development :: Libraries :: Python Modules'
+      ],
+      packages=find_packages())
