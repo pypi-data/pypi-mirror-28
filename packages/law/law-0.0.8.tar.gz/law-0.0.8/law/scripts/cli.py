6a50cea4169214399395eaccbedee9a927a4b398
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:18:17 +0000

    Add package law/0.0.8/law-0.0.8.tar.gz

---
+++ packages/law/law-0.0.8.tar.gz/law-0.0.8/law/scripts/cli.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+"""
+Main law command line interface.
+"""
+
+
+import sys
+from importlib import import_module
+from argparse import ArgumentParser
+
+
+progs = ["run", "db", "config", "software", "completion"]
+forward_progs = ["run"]
+
+
+def main():
+    # setup the main parser and sub parsers
+    parser = ArgumentParser(prog="law", description="law command line tool")
+    sub_parsers = parser.add_subparsers(help="subcommands", dest="command")
+
+    # setup all progs
+    mods = {}
+    for prog in progs:
+        mods[prog] = import_module("law.scripts." + prog)
+        mods[prog].setup_parser(sub_parsers)
+
+    # parse args and dispatch execution
+    if len(sys.argv) >= 2 and sys.argv[1] in forward_progs:
+        args = parser.parse_args(sys.argv[1:3])
+    else:
+        args = parser.parse_args()
+
+    mods[args.command].execute(args)
+
+
+if __name__ == "__main__":
+    main()
