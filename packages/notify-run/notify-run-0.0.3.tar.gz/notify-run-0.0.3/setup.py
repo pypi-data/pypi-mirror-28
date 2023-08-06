ccb015a9b589921f114c3cee1d04879e9dc5fe1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:45 +0000

    Add package notify-run/0.0.3/notify-run-0.0.3.tar.gz

---
+++ packages/notify-run/notify-run-0.0.3.tar.gz/notify-run-0.0.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(name='notify-run',
+      version='0.0.3',
+      description='Client for notify.run notifications.',
+      author='Paul Butler',
+      author_email='notify@paulbutler.org',
+      url='https://notify.run/',
+      packages=['notify_run'],
+      entry_points={
+          'console_scripts': [
+              'notify-run = notify_run.cli:main'
+          ]
+      },
+      install_requires=[
+          'PyQRCode==1.2.1',
+          'requests==2.18.4',
+      ],
+      )
