cabda850cf919c035e0b03511231b828c4edcd85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:18:16 +0000

    Add package surrealism/0.14.9/surrealism-0.14.9.tar.gz

---
+++ packages/surrealism/surrealism-0.14.9.tar.gz/surrealism-0.14.9/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as file_desc:
+    long_desc = file_desc.read()
+
+setup(name='surrealism',
+      version='0.14.9',
+      packages=find_packages(),
+      # packages=["surrealism"],
+      author='Ian Havelock',
+      author_email='ian@morrolan.com',
+      url='http://morrolan.github.io/surrealism',
+      license='GNU General Public License (GPL)',
+      description='Surreal sentence and error message generator.',
+      long_description=long_desc,
+      platforms=['Windows', 'Unix', 'OS X'],
+      download_url="https://pypi.python.org/pypi/surrealism/",
+      keywords=["surreal", "surrealism", "error message", "fault"],
+      classifiers=
+      [
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Other Environment',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Education',
+          'Intended Audience :: Other Audience',
+          'License :: OSI Approved :: GNU General Public License (GPL)',
+          'Operating System :: OS Independent',
+          'Topic :: Education',
+          'Topic :: Communications',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+      ],
+      install_requires=['setuptools'],
+      package_data={'surrealism': ['surrealism.sqlite']},
+      zip_safe=False,
+      )
