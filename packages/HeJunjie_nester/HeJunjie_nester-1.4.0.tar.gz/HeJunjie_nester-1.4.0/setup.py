ddefb82c6ed17e3629e4593dafe03ab0c1ad7141
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:03:09 +0000

    Add package HeJunjie_nester/1.4.0/HeJunjie_nester-1.4.0.tar.gz

---
+++ packages/HeJunjie_nester/HeJunjie_nester-1.4.0.tar.gz/HeJunjie_nester-1.4.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'HeJunjie_nester',
+    version = '1.4.0',
+    py_modules = ['HeJunjie_nester'],
+    author = 'HeJunjie',
+    author_email = 'poiler@163.com',
+    url = 'http://www.headfirstlabs.com',
+    description = 'A simple printer of nester lists',
+)
