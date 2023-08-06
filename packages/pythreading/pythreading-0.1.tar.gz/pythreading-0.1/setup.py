f0851dbd6b5af0649593da65c8900c13959f4ef5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:59 +0000

    Add package pythreading/0.1/pythreading-0.1.tar.gz

---
+++ packages/pythreading/pythreading-0.1.tar.gz/pythreading-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+setup(
+  name = 'pythreading',
+  packages = ['pythreading'], # this must be the same as the name above
+  version = '0.1',
+  description = 'multiprocessing package of python 2.7 ',
+  author = 'Sandeep Rawat',
+  author_email = 'san55577@gmail.com',
+  url = 'https://github.com/sandeep-rawat-xebia/pymultiprocessing.git', # use the URL to the github repo
+  download_url = 'https://github.com/sandeep-rawat-xebia/pymultiprocessing/releases/download/0.3/pymultiprocessing-0.3.tar.gz', # I'll explain this in a second
+  keywords = ['pythreading', 'python', 'jython'], # arbitrary keywords
+  classifiers = [],
+)
