2c2088537fd76a12ebf9b3a7a87a1760ce56ac49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:18:16 +0000

    Add package halofun/0.1/halofun-0.1.tar.gz

---
+++ packages/halofun/halofun-0.1.tar.gz/halofun-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='halofun',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='http://github.com/vipin08/halofun',
+      author='Flying Circus',
+      author_email='vpnkumar.kumar1@gmail.com',
+      license='MIT',
+      packages=['halofun'],
+      zip_safe=False)
