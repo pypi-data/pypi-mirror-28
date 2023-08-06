50b73881077a715ab11860ae7c1be191aac38436
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:19:29 +0000

    Add package loadin/0.1.4/loadin-0.1.4.tar.gz

---
+++ packages/loadin/loadin-0.1.4.tar.gz/loadin-0.1.4/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='loadin',
+    version='0.1.4',
+    description='quite a easy way to apply animation to your function',
+    author='Feng Zhangchi',
+    author_email='fengzc@vip.qq.com',
+    url='https://github.com/williamfzc/loadin',
+    packages=find_packages()
+)
