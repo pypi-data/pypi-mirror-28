830cad442a1c195ae88e1b1cdd8326ae8ec44395
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:40 +0000

    Add package pydeconz/26/pydeconz-26.tar.gz

---
+++ packages/pydeconz/pydeconz-26.tar.gz/pydeconz-26/pydeconz/__main__.py
@@ -0,0 +1,43 @@
+""""""
+
+import aiohttp
+import asyncio
+
+from pydeconz import DeconzSession
+from pydeconz.utils import (async_get_api_key, async_delete_all_keys)
+
+
+@asyncio.coroutine
+def main(loop, **kwargs):
+    """
+    """
+    if 'api_key' not in kwargs:
+        api_key = yield from async_get_api_key(loop, **kwargs)
+        kwargs['api_key'] = api_key
+        print(api_key)
+    websession = aiohttp.ClientSession(loop=loop)
+    deconz = DeconzSession(loop, websession, **kwargs)
+    result = yield from deconz.async_load_parameters()
+    if result is False:
+        print('Failed to setup deCONZ')
+        return False
+    deconz.start()
+    from pprint import pprint
+    for dev_id, dev in deconz.config.items():
+        pprint(dev.__dict__)
+    # yield from deconz.close()
+    # yield from async_delete_api_key(loop, **kwargs)
+    #yield from async_delete_all_keys(loop, **kwargs)
+
+
+kw = {'host': '127.0.0.1',
+      'port': 80,
+      'api_key': 'F0FB5939EF',
+      'username': 'delight',
+      'password': 'delight'
+      }
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main(loop, **kw))
+loop.run_forever()
+loop.close()
