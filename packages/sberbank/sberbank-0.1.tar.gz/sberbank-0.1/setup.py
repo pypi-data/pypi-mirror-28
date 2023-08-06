cc591bfddba072420b47d162402c84d3c77f8ed6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:42 +0000

    Add package sberbank/0.1/sberbank-0.1.tar.gz

---
+++ packages/sberbank/sberbank-0.1.tar.gz/sberbank-0.1/setup.py
@@ -0,0 +1,31 @@
+import os
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='sberbank',
+    version='0.1',
+    packages=['sberbank', 'sberbank.migrations'],
+    include_package_data=True,
+    license='BSD License',  # example license
+    description='Sberbank payments service',
+    long_description=README,
+    author='APIQA',
+    author_email='dukelog@gmail.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.11',  # replace "X.Y" as appropriate
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',  # example license
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
