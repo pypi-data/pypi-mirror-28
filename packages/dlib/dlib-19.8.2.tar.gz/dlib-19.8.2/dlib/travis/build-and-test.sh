16fecd441cbad3e5a0650866ee7bd779a833e995
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:13 +0000

    Add package dlib/19.8.2/dlib-19.8.2.tar.gz

---
+++ packages/dlib/dlib-19.8.2.tar.gz/dlib-19.8.2/dlib/travis/build-and-test.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+# Exit if anything fails.
+set -eux
+
+# build dlib and tests
+if [ "$VARIANT" = "test" ]; then
+  mkdir build
+  cd build
+  cmake ../dlib/test -DCMAKE_BUILD_TYPE=Release
+  cmake --build . --target dtest -- -j 2
+  ./dtest --runall
+fi
+
+if [ "$VARIANT" = "examples" ]; then
+  mkdir build
+  cd build
+  cmake ../examples -DCMAKE_BUILD_TYPE=Release
+  cmake --build . -- -j 1
+fi
+
+if [ "$VARIANT" = "python-api" ]; then
+  python setup.py test --clean
+  pip uninstall numpy -y
+  python setup.py test --clean
+fi
+
