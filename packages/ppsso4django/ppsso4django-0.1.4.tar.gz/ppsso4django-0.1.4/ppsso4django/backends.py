a8c362c37403e53e65474aa24d7f56eea28a2ce4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:10 +0000

    Add package ppsso4django/0.1.4/ppsso4django-0.1.4.tar.gz

---
+++ packages/ppsso4django/ppsso4django-0.1.4.tar.gz/ppsso4django-0.1.4/ppsso4django/backends.py
@@ -0,0 +1,97 @@
+# -*- coding:utf-8 -*-
+"""CAS authentication backend"""
+
+try:
+    """For Py2"""
+    from urllib.parse import urlencode, urljoin
+    from urllib.request import urlopen
+except ImportError:
+    """ For Py3.x"""
+    from urllib import urlencode
+    from urlparse import urljoin
+    from urllib import urlopen
+
+from django.conf import settings
+from .models import User
+__all__ = ['CASBackend']
+
+
+def _verify_cas1(ticket, service):
+    """Verifies CAS 1.0 authentication ticket.
+
+    Returns yes on success and None on failure.
+    """
+
+    params = {'ticket': ticket, 'service': service}
+    url = (urljoin(settings.CAS_SERVER_URL, 'validate') + '?' +
+           urlencode(params))
+    page = urlopen(url)
+    try:
+        verified = page.readline().strip()
+        if verified == 'yes':
+            return page.readline().strip()
+        else:
+            return None
+    finally:
+        page.close()
+
+
+def _verify_cas2(ticket, service):
+    """Verifies CAS 2.0+ XML-based authentication ticket.
+
+    Returns username on success and None on failure.
+    """
+
+    try:
+        from xml.etree import ElementTree
+    except ImportError:
+        from _elementtree import ElementTree
+
+    params = {'ticket': ticket, 'service': service}
+    url = (urljoin(settings.CAS_SERVER_URL, 'serviceValidate') + '?' + urlencode(params))
+    # ca_cas = "{0}/certificates/cas.pem".format(BASE_DIR)
+    # page = urlopen(url,cafile=ca_cas)
+    page = urlopen(url)
+    try:
+        response = page.read()
+        tree = ElementTree.fromstring(response)
+        if tree[0].tag.endswith('authenticationSuccess'):
+            return tree[0][0].text
+        else:
+            return None
+    finally:
+        page.close()
+
+
+_PROTOCOLS = {'1': _verify_cas1, '2': _verify_cas2}
+
+if settings.CAS_VERSION not in _PROTOCOLS:
+    raise ValueError('Unsupported CAS_VERSION %r' % settings.CAS_VERSION)
+
+_verify = _PROTOCOLS[settings.CAS_VERSION]
+
+
+class CASBackend(object):
+    """CAS authentication backend"""
+
+    def authenticate(self, ticket, service):
+        """Verifies CAS ticket and gets or creates User object"""
+
+        username = _verify(ticket, service)
+        if not username:
+            return None
+        try:
+            user = User.objects.get(username__iexact=username)
+        except User.DoesNotExist:
+            # user will have an "unusable" password
+            user = User.objects.create_user(username, '')
+            user.save()
+        return user
+
+    def get_user(self, user_id):
+        """Retrieve the user's entry in the User model if it exists"""
+
+        try:
+            return User.objects.get(pk=user_id)
+        except User.DoesNotExist:
+            return None
---
+++ packages/ppsso4django/ppsso4django-0.1.4.tar.gz/ppsso4django-0.1.4/ppsso4django/middleware.py
@@ -0,0 +1,64 @@
+"""CAS authentication middleware"""
+
+try:
+    """For Py2"""
+    from urllib.parse import urlencode
+except ImportError:
+    """For Py3"""
+    from urllib import urlencode
+
+from django.http import HttpResponseRedirect, HttpResponseForbidden
+from django.conf import settings
+from django.contrib.auth import REDIRECT_FIELD_NAME
+from django.contrib.auth.views import login, logout
+from django.urls import reverse
+
+from .views import login as cas_login, logout as cas_logout
+
+__all__ = ['CASMiddleware']
+
+
+class CASMiddleware(object):
+    """Middleware that allows CAS authentication on admin pages"""
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        return self.get_response(request)
+
+    def process_request(self, request):
+        """Checks that the authentication middleware is installed"""
+
+        error = ("The Django CAS middleware requires authentication "
+                 "middleware to be installed. Edit your MIDDLEWARE_CLASSES "
+                 "setting to insert 'django.contrib.auth.middleware."
+                 "AuthenticationMiddleware'.")
+        assert hasattr(request, 'user'), error
+
+    def process_view(self, request, view_func, view_args, view_kwargs):
+        """Forwards unauthenticated requests to the admin page to the CAS
+        login URL, as well as calls to django.contrib.auth.views.login and
+        logout.
+        """
+
+        if view_func == login:
+            return cas_login(request, *view_args, **view_kwargs)
+        elif view_func == logout:
+            return cas_logout(request, *view_args, **view_kwargs)
+
+        if settings.CAS_ADMIN_PREFIX:
+            if not request.path.startswith(settings.CAS_ADMIN_PREFIX):
+                return None
+        elif not view_func.__module__.startswith('django.contrib.admin.'):
+            return None
+
+        if request.user.is_authenticated:
+            if request.user.is_staff:
+                return None
+            else:
+                error = ('<h1>Forbidden</h1><p>You do not have staff '
+                         'privileges.</p>')
+                return HttpResponseForbidden(error)
+        params = urlencode({REDIRECT_FIELD_NAME: request.get_full_path()})
+        return HttpResponseRedirect(reverse(cas_login) + '?' + params)
