0a4952db66f9339f09ed446df88a6baece4f5d32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:05 +0000

    Add package alas-ce0-client/0.89/alas-ce0-client-0.89.tar.gz

---
+++ packages/alas-ce0-client/alas-ce0-client-0.89.tar.gz/alas-ce0-client-0.89/alas_ce0/management/employee.py
@@ -0,0 +1,16 @@
+from alas_ce0.common.client_base import EntityClientBase
+
+
+class EmployeeClient(EntityClientBase):
+    entity_endpoint_base_url = '/management/employees/'
+
+    def __init__(self, country_code='cl', **kwargs):
+        super(EmployeeClient, self).__init__(**kwargs)
+        self.entity_endpoint_base_url += country_code + '/'
+
+    def is_work_day(self, work_day=None, route_days=None):
+        return self.http_post_json(self.entity_endpoint_base_url + "_is-work-day", {
+                'work_day': work_day,
+                'route_days': route_days,
+            }
+        )
