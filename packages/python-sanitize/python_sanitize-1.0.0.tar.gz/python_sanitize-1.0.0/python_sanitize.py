ef4c2609b4fe4f3e5c366baffe2086ec6f02b392
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:59 +0000

    Add package python-sanitize/1.0.0/python_sanitize-1.0.0.tar.gz

---
+++ packages/python-sanitize/python_sanitize-1.0.0.tar.gz/python_sanitize-1.0.0/python_sanitize.py
@@ -0,0 +1,10 @@
+"""清理字符串"""
+def sanitize(time_string):
+	if  '-' in time_string:
+	    splitter = '-'
+	elif ':' in time_string:
+	    splitter = ':'
+	else:
+	    return (time_string)
+	(mins, secs) = time_string.split(splitter)
+	return (mins+'.'+secs)
