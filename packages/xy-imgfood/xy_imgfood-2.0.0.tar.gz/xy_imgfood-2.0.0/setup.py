8601e4d42c497b031fe99bacedb5ec4bd1610a06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:25 +0000

    Add package xy-imgfood/2.0.0/xy_imgfood-2.0.0.tar.gz

---
+++ packages/xy-imgfood/xy_imgfood-2.0.0.tar.gz/xy_imgfood-2.0.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'xy_imgfood',
+  packages = ['xy_imgfood'],
+  version = '2.0.0',
+  description = 'Recognition Image Food',
+  long_description='Recognition Image Food',
+  author = 'KingHS',
+  author_email = '382771946@qq.com',
+  keywords = ['pip3', 'python3','python','Recognition Image Food'],
+  license='MIT',
+  install_requires=['requests']
+)
---
+++ packages/xy-imgfood/xy_imgfood-2.0.0.tar.gz/xy_imgfood-2.0.0/xy_imgfood/xy_imgfood.py
@@ -0,0 +1,39 @@
+import requests
+import json
+import base64
+import os
+import sys
+
+
+def _get_access_token():
+    url = 'https://www.phpfamily.org/aniToken.php'
+    r = requests.post(url)
+    res = r.json()
+    return res['access_token']
+
+'''食物识别'''
+def food(filename='', topNum=1):
+    if not filename:
+        return -1
+    url = 'https://www.phpfamily.org/imgFood.php'
+    filepath = os.path.abspath(filename)
+    b64img= base64.b64encode(open(filepath, 'rb').read()).rstrip().decode('utf-8')
+    data = {}
+    data['b64img'] = b64img
+    data['access_token'] = _get_access_token()
+    data['topNum'] = topNum
+    r = requests.post(url, data=data)
+    res = r.json()
+    if res['result'] :
+        if topNum == 1:
+            return res['result'][0]['name']
+        else:
+            return res['result']
+
+def main():
+    res = food('2.jpeg')
+    print(res)
+
+
+if __name__ == '__main__':
+    main()
