76703e78b338b01defa8157c852f1da779dbbfa0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:00 +0000

    Add package SimpleNest/1.1.0/SimpleNest-1.1.0.tar.gz

---
+++ packages/SimpleNest/SimpleNest-1.1.0.tar.gz/SimpleNest-1.1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+	name  		= 'SimpleNest',
+	version		= '1.1.0',
+	py_modules	= ['SimpleNest'],
+	author		= 'Eyama',
+	author_email	= 'yliu53501@gmail.com',
+	description	= 'A very simple printer of nested lists',
+)
