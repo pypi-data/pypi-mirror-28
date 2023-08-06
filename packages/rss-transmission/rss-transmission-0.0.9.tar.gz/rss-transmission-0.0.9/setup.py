3f0221926db534c04b09e0523b1c32ed094b954a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:08 +0000

    Add package rss-transmission/0.0.9/rss-transmission-0.0.9.tar.gz

---
+++ packages/rss-transmission/rss-transmission-0.0.9.tar.gz/rss-transmission-0.0.9/setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+from rssbot import __version__
+from m2r import convert
+
+with open("README.md", "r") as f:
+    readme = convert(f.read())
+
+setup(
+    name='rss-transmission',
+    version=__version__,
+    description='Rss plugin for transmission with a web ui',
+    long_description=readme,
+    keywords='rss transmission webui',
+    author="helloqiu",
+    author_email="helloqiu95@gmail.com",
+    url='https://github.com/helloqiu/rss-transmission',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=open("requirements.txt").readlines(),
+    license="GPLv3",
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: MacOS",
+        "Operating System :: POSIX",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python"
+    ],
+    setup_requires=[
+        "pytest-runner",
+    ],
+    tests_require=[
+        "pytest",
+    ],
+)
