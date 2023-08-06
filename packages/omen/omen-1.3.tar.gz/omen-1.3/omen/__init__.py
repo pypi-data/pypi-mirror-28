50d1a3e3a9523175a43167c12cee40c62d6a5d0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:01 +0000

    Add package omen/1.3/omen-1.3.tar.gz

---
+++ packages/omen/omen-1.3.tar.gz/omen-1.3/omen/__init__.py
@@ -0,0 +1,63 @@
+import random
+from collections import defaultdict
+import dill as pickle
+import nltk
+from nltk.tokenize.moses import MosesDetokenizer
+
+
+class Omen():
+    def __init__(self, state_size=3):
+        self.start_token = "@@_START_TOKEN_@@"
+        self.end_token = "@@_END_TOKEN_@@"
+        self.state_size = state_size
+
+        self.states = defaultdict(lambda: defaultdict(int))
+        self.beginnings = defaultdict(int)
+
+    def detokenize(self, tokens):
+        return MosesDetokenizer().detokenize(tokens, return_str=True)
+
+    @staticmethod
+    def weighted_choice(words):
+        number = random.random() * sum(words.values())
+        for key, weight in words.items():
+            number -= weight
+            if number < 0:
+                return key
+
+    def dump_states(self, file):
+        pickle.dump({
+            "start_token": self.start_token,
+            "end_token": self.end_token,
+            "state_size": self.state_size,
+            "states": self.states,
+            "beginnings": self.beginnings
+        }, file)
+
+    def load_states(self, file):
+        states = pickle.load(file)
+        self.start_token = states["start_token"]
+        self.end_token = states["end_token"]
+        self.state_size = states["state_size"]
+        self.states = states["states"]
+        self.beginnings = states["beginnings"]
+
+    def train(self, corpus):
+        sentences = nltk.sent_tokenize(corpus)
+        sentences = [[self.start_token, *nltk.word_tokenize(sentence), self.end_token] for sentence in sentences]
+
+        for sentence in sentences:
+            self.beginnings[tuple(sentence[:self.state_size])] += 1
+            for index, word in enumerate(sentence):
+                state = tuple(sentence[index - self.state_size:index])
+                self.states[state][word] += 1
+
+    def generate(self):
+        tokens = list(Omen.weighted_choice(self.beginnings))
+        next_word = tokens[-1]
+        while next_word != self.end_token:
+            state = tuple(tokens[-self.state_size:])
+            next_word = Omen.weighted_choice(self.states[state])
+            tokens.append(next_word)
+
+        return self.detokenize(tokens[1:-1])
---
+++ packages/omen/omen-1.3.tar.gz/omen-1.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name='omen',
+      version='1.3',
+      description='Simple markov chain library',
+      long_description=readme(),
+      keywords='markov chain nlp',
+      url='https://github.com/veggiedefender/omen',
+      author='Jesse Li',
+      author_email='jessejesse123@gmail.com',
+      license='MIT',
+      packages=['omen'],
+      install_requires=['nltk', 'dill'],
+      setup_requires=['nltk'],
+      zip_safe=True)
+
+import nltk
+nltk.download('punkt')
+nltk.download('perluniprops')
