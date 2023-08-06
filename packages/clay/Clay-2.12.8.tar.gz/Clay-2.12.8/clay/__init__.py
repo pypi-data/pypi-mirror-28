5c54f6b931f8608c856891ab03f7115e62f3124a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:11 +0000

    Add package clay/2.12.8/Clay-2.12.8.tar.gz

---
+++ packages/clay/Clay-2.12.8.tar.gz/Clay-2.12.8/clay/__init__.py
@@ -0,0 +1,13 @@
+# coding=utf-8
+"""
+# Clay
+
+An amazing prototyping tool
+
+:copyright: `Lúcuma labs <http://lucumalabs.com>`_.
+:license: MIT, see LICENSE for more details.
+"""
+from .main import Clay  # noqa
+
+
+__version__ = '2.12.8'
