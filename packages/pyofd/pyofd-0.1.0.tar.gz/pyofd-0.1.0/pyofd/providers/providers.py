488f5c3f8945bac5791870dcd19b4f89b060531d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:34 +0000

    Add package pyofd/0.1.0/pyofd-0.1.0.tar.gz

---
+++ packages/pyofd/pyofd-0.1.0.tar.gz/pyofd-0.1.0/pyofd/providers/providers.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+
+from .dummy import ofdDummy
+from .taxcom import ofdTaxcom
+from .platforma import ofdPlatforma
+from .first_ofd import ofd1OFD
+from .yarus import ofdYarus
+from .ofd import ofdOfdRu
+from .nalog import NalogRu
