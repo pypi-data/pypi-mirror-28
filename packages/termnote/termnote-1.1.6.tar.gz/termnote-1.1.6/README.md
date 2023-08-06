dbe8536af8478f7ab4dc0293cbafa3d0fb5ea061
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:48 +0000

    Add package termnote/1.1.6/termnote-1.1.6.tar.gz

---
+++ packages/termnote/termnote-1.1.6.tar.gz/termnote-1.1.6/README.md
@@ -0,0 +1,73 @@
+[![PyPI version](https://img.shields.io/pypi/v/termnote.svg)](https://pypi.python.org/pypi/termnote)
+[![Build Status](https://travis-ci.org/sayanarijit/termnote.svg?branch=master)](https://travis-ci.org/sayanarijit/termnote)
+
+# TermNote
+
+A simple, yet powerful and handy terminal based note taking python app
+
+
+### Requirement:
+
+* requires python (>2 or 3)
+
+
+### Usage:
+
+* Install with pip
+
+```
+sudo pip install termnote
+```
+
+***Note: For the sake of LAZYNESS, 'tn' is a shortcut for 'termnote'***
+
+* Create your 1st note
+
+```
+tn my first note
+```
+
+* Write "I love cheeze" and close editor. Note is created. Now lets find the note by keywords
+
+```
+tn cheez
+```
+
+or by filename
+
+```
+tn first no
+```
+
+* List all notes
+
+```
+tn
+```
+
+
+### Customization
+
+You can change default editor and path by adding below lines in your '~/.profile' file
+
+```
+export EDITOR="[Your favourite text editor. Default is 'vi']"
+export TN_STORAGE="[Directory where notes to be saved. Default is '~/.termnote']"
+```
+
+### Multi user shared notes
+
+You can also have a common shared notes space for all users with following steps
+
+* Create a shared notes directory
+
+```
+sudo mkdir /var/termnote
+sudo chmod a+rwx /var/termnote
+```
+
+* Set environment variable globally in '/etc/profile'
+
+```
+export TN_STORAGE="/var/termnote"
+```
---
+++ packages/termnote/termnote-1.1.6.tar.gz/termnote-1.1.6/setup.py
@@ -0,0 +1,64 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+
+VERSION = 'v1.1.6'
+
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+# Requirements for installation
+with open('requirements.txt') as requirements_file:
+    install_requirements = requirements_file.read().splitlines()
+
+setup(
+    name='termnote',
+    version=VERSION,
+    description='A simple, yet powerful and handy terminal based note taking python app',
+    long_description=long_description,
+    url='https://github.com/sayanarijit/termnote',
+    download_url='https://github.com/sayanarijit/termnote/archive/{}.tar.gz'.format(VERSION),
+    author='Arijit Basu',
+    author_email='sayanarijit@gmail.com',
+    license='MIT',
+    py_modules=['termnote'],
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Environment :: Console',
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Utilities',
+        'Operating System :: MacOS',
+        'Operating System :: POSIX'
+    ],
+    keywords='Notes Management Terminal App',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    install_requires=install_requirements,
+    entry_points={
+        'console_scripts': [
+            'tn = termnote:run',
+            'termnote = termnote:run',
+        ]
+    }
+)
