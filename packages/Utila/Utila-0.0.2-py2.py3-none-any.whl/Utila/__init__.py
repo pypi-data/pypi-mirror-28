172cc38b29f9d9db7fdf2853ae88ccfedb533c41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:11 +0000

    Add package Utila/0.0.2/Utila-0.0.2-py2.py3-none-any.whl

---
+++ packages/Utila/Utila-0.0.2-py2.py3-none-any.whl/Utila/__init__.py
@@ -0,0 +1,10 @@
+"""
+    Ces module permetent de trier une liste dans lordre 
+    croissant ou decroissant
+
+"""
+
+from utila import MinToMax
+from utila import MaxToMin
+
+__version__ = "0.0.2"
