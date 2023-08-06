854a0b99fef7bff7455b7c5ee723b25da2a0e447
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:40 +0000

    Add package nyt-pyiap/0.1.4/nyt-pyiap-0.1.4.tar.gz

---
+++ packages/nyt-pyiap/nyt-pyiap-0.1.4.tar.gz/nyt-pyiap-0.1.4/pyiap/pyiap_django_middleware.py
@@ -0,0 +1,41 @@
+import os
+
+from django.http import HttpResponse
+
+from pyiap.utils import validate_iap_jwt_from_compute_engine
+
+
+class VerifyJWTMiddleware(object):
+    def process_request(self, request):
+        """
+        Adds attributes to the Django request object so that we can access
+        an authenticated user in a view or some other response.
+        """
+
+        request.jwt_user_id = None
+        request.jwt_user_email = None
+        request.jwt_error_str = None
+
+        cloud_project_number = os.environ.get('CLOUD_PROJECT_NUMBER', None)
+        backend_service_id = os.environ.get('BACKEND_SERVICE_ID', None)
+        jwt_token = request.META.get('HTTP_X_GOOG_IAP_JWT_ASSERTION', None)
+
+        # Only modify the response if we're in an environment where IAP is running.
+        # This isn't going to work on your local if that's what you expect.
+        if cloud_project_number and backend_service_id and jwt_token:
+
+            # Run the validation step.
+            sub, email, error = validate_iap_jwt_from_compute_engine(jwt_token, cloud_project_number, backend_service_id)
+
+            # If there's an error, bail with a 500 and a big debug page.
+            if error:
+                payload = "<h1>Error</h1>"
+                payload += "<h5>%s</h5>" % str(error)
+                payload += "Project #: %s<br/> Email: <br/>Service ID: %s<br/>Token: %s<br/>" % (cloud_project_number, backend_service_id, jwt_token)
+                payload += "<br/>".join(["%s: %s" % (key,value) for key,value in response.items()])
+                payload += "<br/>".join(["%s: %s" % (key,value) for key,value in request.META.items()])
+                return HttpResponse(payload, status=500)
+
+            # Assign the ID and email to the request if they exist.
+            request.jwt_user_id = sub
+            request.jwt_user_email = email
---
+++ packages/nyt-pyiap/nyt-pyiap-0.1.4.tar.gz/nyt-pyiap-0.1.4/pyiap/pyiap_flask_middleware.py
@@ -0,0 +1,41 @@
+import os
+
+from flask import abort
+from werkzeug.wrappers import Request
+
+from pyiap.utils import validate_iap_jwt_from_compute_engine
+
+
+class VerifyJWTMiddleware(object):
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, environ, start_response):
+
+        # I think this is right but I am insane.
+        request = Request(environ, shallow=True)
+
+        environ['jwt_user_id'] = None
+        environ['jwt_user_email'] = None
+        environ['jwt_error_str'] = None
+
+        cloud_project_number = os.environ.get('CLOUD_PROJECT_NUMBER', None)
+        backend_service_id = os.environ.get('BACKEND_SERVICE_ID', None)
+        jwt_token = request.environ.get('HTTP_X_GOOG_IAP_JWT_ASSERTION', None)
+
+        if cloud_project_number and backend_service_id and jwt_token:
+
+            sub, email, error  = validate_iap_jwt_from_compute_engine(jwt_token, cloud_project_number, backend_service_id)
+
+            environ['jwt_user_id'] = sub
+            environ['jwt_user_email'] = email
+
+            if error:
+                payload = "<h1>Error</h1>"
+                payload += "<h5>%s</h5>" % str(error)
+                payload += "Project #: %s<br/> Email: <br/>Service ID: %s<br/>Token: %s<br/>" % (cloud_project_number, backend_service_id, jwt_token)
+                payload += "<br/>".join(["%s: %s" % (key,value) for key,value in response.items()])
+                payload += "<br/>".join(["%s: %s" % (key,value) for key,value in request.environ.items()])
+                abort(500)
+
+        return self.app(environ, start_response)
