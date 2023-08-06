b5d638d9caea7630e0a63e00220f1ae155f6a987
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:53:15 +0000

    Add package bce/4.7.9647/bce-4.7.9647.tar.gz

---
+++ packages/bce/bce-4.7.9647.tar.gz/bce-4.7.9647/AUTHORS.md
@@ -0,0 +1,3 @@
+# Authors
+
+Ji WenCong &lt;[taiki_akita@163.com](mailto://taiki_akita@163.com)&gt;
---
+++ packages/bce/bce-4.7.9647.tar.gz/bce-4.7.9647/bce/public/printer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+#
+#  Copyright 2014 - 2018 The BCE Authors. All rights reserved.
+#  Use of this source code is governed by a BSD-style license that can be
+#  found in the license.txt file.
+#
+
+#  Printer IDs.
+PRINTER_TEXT = (1 << 0)
+PRINTER_MATHML = (1 << 1)
+PRINTER_ALL = (PRINTER_TEXT | PRINTER_MATHML)
+
+#  Printer keys.
+PRINTER_KEY_TEXT = "text"
+PRINTER_KEY_MATHML = "mathml"
