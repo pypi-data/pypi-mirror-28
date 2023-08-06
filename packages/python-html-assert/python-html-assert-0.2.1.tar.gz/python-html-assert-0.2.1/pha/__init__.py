8677d210ff1f0a0906e91b024162ce8efee9979c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:03:06 +0000

    Add package python-html-assert/0.2.1/python-html-assert-0.2.1.tar.gz

---
+++ packages/python-html-assert/python-html-assert-0.2.1.tar.gz/python-html-assert-0.2.1/pha/__init__.py
@@ -0,0 +1,27 @@
+from matchers import (
+    linear_match as html_match,
+    prune_unmatched_elements
+)
+
+from spec import (
+    a,
+    accordion,
+    acc_body,
+    acc_group,
+    acc_heading,
+    div,
+    elem,
+    heading,
+    html,
+    img,
+    input,
+    option,
+    option_xhtml,
+    select,
+    text,
+)
+
+from formatters import (
+    pretty_html,
+    pretty_spec
+)
---
+++ packages/python-html-assert/python-html-assert-0.2.1.tar.gz/python-html-assert-0.2.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages, setup
+
+
+REQUIREMENTS = [
+    'beautifulsoup4<=4.3.2']
+
+
+PACKAGES = [
+    'pha']
+
+
+CLASSIFIERS = [
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: MacOS :: MacOS X',
+    'Operating System :: POSIX :: Linux',
+    'Programming Language :: Python :: 2.7',
+    'Topic :: Software Development :: Testing',
+    'Topic :: Utilities']
+
+setup(
+    name='python-html-assert',
+    version='0.2.1',
+    packages=find_packages(),
+    install_requires=REQUIREMENTS,
+    author='Robert Cox',
+    author_email='robjohncox@gmail.com',
+    description='partial matching of html using a tree-based specification',
+    license='MIT License',
+    url='https://github.com/robjohncox/python-html-assert',
+    classifiers=CLASSIFIERS)
