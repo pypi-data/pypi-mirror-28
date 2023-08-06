76bb9cdc9215cc31179c1b8695a7398f0c308ce0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:24:29 +0000

    Add package ehforwarderbot/2.0.0b1/ehforwarderbot-2.0.0b1.tar.gz

---
+++ packages/ehforwarderbot/ehforwarderbot-2.0.0b1.tar.gz/ehforwarderbot-2.0.0b1/ehforwarderbot/__init__.py
@@ -0,0 +1,7 @@
+from .channel import EFBChannel
+from .chat import EFBChat
+from .constants import ChatType, ChannelType, MsgType, TargetType
+from .message import EFBMsg
+from .status import EFBStatus
+from .middleware import EFBMiddleware
+from .__version__ import __version__
