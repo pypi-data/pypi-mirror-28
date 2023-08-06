4fe5234eca02f42909e9ed623c5a57430de49012
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:45 +0000

    Add package jschema/0.6.6/jschema-0.6.6.tar.gz

---
+++ packages/jschema/jschema-0.6.6.tar.gz/jschema-0.6.6/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import sys
+from setuptools import setup
+
+sys.path.insert(0, os.path.dirname(__file__))
+from jschema.version import __version__
+
+setup(
+    name="jschema",
+    version=__version__,
+    description="json schema tookit",
+    license="MIT",
+    author="Stepan Perlov",
+    author_email="stepanperlov@gmail.com",
+    url="https://github.com/stepan-perlov/jschema",
+    install_requires=["PyYAML", "jinja2"],
+    packages=["jschema", "jschema.formats", "jschema.node"],
+    package_data={"jschema.formats": ["templates/*.j2"]},
+    entry_points={
+        "console_scripts": [
+            "jschema = jschema.main:main"
+        ]
+    },
+    platforms=["linux"]
+)
