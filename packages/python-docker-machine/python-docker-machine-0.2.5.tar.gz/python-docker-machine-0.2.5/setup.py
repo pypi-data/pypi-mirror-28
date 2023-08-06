1c9b476a058c7a76d673d02d15dd7274284f84b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:09 +0000

    Add package python-docker-machine/0.2.5/python-docker-machine-0.2.5.tar.gz

---
+++ packages/python-docker-machine/python-docker-machine-0.2.5.tar.gz/python-docker-machine-0.2.5/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+__version__ = "0.2.5"
+
+install_requires = [
+    'docker',
+]
+
+setup(
+    name="python-docker-machine",
+    version=__version__,
+    packages=find_packages(),
+    install_requires=install_requires,
+    package_data={
+        '': ['*.rst'],
+    },
+    author="Gijs Molenaar",
+    author_email="gijs@pythonic.nl",
+    description="Python wrapper around docker-machine",
+    license="GPL2",
+    keywords="docker machine docker-machine container",
+    url="https://github.com/gijzelaerr/python-docker-machine",
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Other Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Utilities',
+    ],
+)
