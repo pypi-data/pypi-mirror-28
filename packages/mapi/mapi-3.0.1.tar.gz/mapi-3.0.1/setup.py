6d0c398cec6f4fd8bf38032c620609e6f0e482b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:24 +0000

    Add package mapi/3.0.1/mapi-3.0.1.tar.gz

---
+++ packages/mapi/mapi-3.0.1.tar.gz/mapi-3.0.1/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from distutils.core import setup
+
+with open('requirements.txt', 'r') as fp:
+    REQUIREMENTS = fp.read().splitlines()
+
+with open('readme.rst', 'r') as fp:
+    LONG_DESCRIPTION = fp.read()
+
+setup(
+    author='Jessy Williams',
+    author_email='jessy@jessywilliams.com',
+    description=(
+        'An API for media database APIs which allows you to search for metadata'
+        'using a simple, common interface'
+    ),
+    license='MIT',
+    long_description=LONG_DESCRIPTION,
+    name='mapi',
+    packages=['mapi'],
+    install_requires=REQUIREMENTS,
+    url='https://github.com/jkwill87/mapi',
+    version='3.0.1'
+)
