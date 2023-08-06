10e9b4fc8ccffe74978e7422238726c6662966bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:28 +0000

    Add package xy-headhat/1.0.0/xy_headhat-1.0.0.tar.gz

---
+++ packages/xy-headhat/xy_headhat-1.0.0.tar.gz/xy_headhat-1.0.0/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+setup(
+  name = 'xy_headhat',
+  packages = ['xy_headhat'],
+  package_data = {'xy_headhat':['data/*']},
+  version = '1.0.0',
+  description = 'Add Christmas Hat',
+  long_description='Auto Add Christmas Hat On Your Head',
+  author = 'KingHS',
+  author_email = '382771946@qq.com',
+  keywords = ['pip3', 'python3','python','christmas','head','hat'],
+  license='MIT',
+  install_requires=[
+    'opencv-python',
+    'numpy',
+    'cv2',
+    'dlib',
+    'pillow'
+  ]
+)
