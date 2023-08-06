c341022ee7156e14de2dd9ce571233ec57631061
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:49 +0000

    Add package indian-festivals/1.1/indian_festivals-1.1.tar.gz

---
+++ packages/indian-festivals/indian_festivals-1.1.tar.gz/indian_festivals-1.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+	name="indian_festivals",
+	packages=["indian_festivals"],
+	version="1.1",
+	description="List of Festivals celebrated across India (yearly/monthly)",
+	author="Sunny Arora",
+	author_email="sunnyarora07@gmail.com",
+	url="https://github.com/sunnyar/indian_festivals",
+	download_url="https://github.com/sunnyar/indian_festivals/archive/v1.1.tar.gz",
+	keywords=["festival", "india", "holiday"],
+	classifiers=[],
+)
