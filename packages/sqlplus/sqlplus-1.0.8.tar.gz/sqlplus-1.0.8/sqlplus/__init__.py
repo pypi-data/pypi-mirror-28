a1d26ab2719ad8ef732fb22b07a26ada84867de6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:03:38 +0000

    Add package sqlplus/1.0.8/sqlplus-1.0.8.tar.gz

---
+++ packages/sqlplus/sqlplus-1.0.8.tar.gz/sqlplus-1.0.8/sqlplus/__init__.py
@@ -0,0 +1,9 @@
+"""
+"""
+
+from .core import Row, Rows, connect, setdir
+from .util import isnum, dconv, dmath, grouper, isconsec
+
+
+__all__ = ['connect', 'Row', 'Rows', 'setdir',
+           'isnum', 'dconv', 'dmath', 'grouper', 'isconsec']
