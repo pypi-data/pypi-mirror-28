78b80877a999941b449f5e6b2072897483479f52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:39 +0000

    Add package nfv-manager/1.3.3/nfv-manager-1.3.3.tar.gz

---
+++ packages/nfv-manager/nfv-manager-1.3.3.tar.gz/nfv-manager-1.3.3/setup.py
@@ -0,0 +1,44 @@
+import os
+
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    readme_file_path = os.path.join(os.path.dirname(__file__), fname)
+
+    if os.path.exists(readme_file_path) and os.path.isfile(readme_file_path):
+        readme_file = open(readme_file_path)
+        return readme_file.read()
+    else:
+        return "The SoftFIRE NFV Manager"
+
+
+setup(
+    name="nfv-manager",
+    version="1.3.3",
+    author="SoftFIRE",
+    author_email="softfire@softfire.eu",
+    description="The SoftFIRE NFV Manager",
+    license="Apache 2",
+    keywords="python vnfm nfvo open baton openbaton sdk experiment manager softfire tosca openstack rest",
+    url="http://softfire.eu/",
+    packages=find_packages(),
+    scripts=["nfv-manager"],
+    install_requires=[
+        'asyncio',
+        'grpcio',
+        'openbaton-cli==5.0.1',
+        'python-keystoneclient',
+        'python-neutronclient',
+        'python-glanceclient',
+        'python-novaclient',
+        'softfire-sdk==1.1.4',
+        'sqlalchemy',
+    ],
+    long_description=read('README.rst'),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+    ],
+)
