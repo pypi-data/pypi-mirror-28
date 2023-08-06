4e110875443aff6e399133bf656da0b633c05349
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:15 +0000

    Add package tableutil/2.2.0/tableutil-2.2.0.tar.gz

---
+++ packages/tableutil/tableutil-2.2.0.tar.gz/tableutil-2.2.0/tableutil/__init__.py
@@ -0,0 +1,19 @@
+
+from .cell import (Cell,
+                   make_uri_cell)
+from .table import Table
+from .table_html import TABLE_CSS
+from .row import Row
+from .constants import (TableSeparators,
+                        Justify,
+                        CLASSES,
+                        HEADING,
+                        JUSTIFY,
+                        MAX,
+                        KEY,
+                        VALUE,
+                        CONVERT,
+                        PROPERTY,
+                        DUMMY,
+                        DUMMY_HEADINGS,
+                        KEY_VALUE_HEADINGS)
