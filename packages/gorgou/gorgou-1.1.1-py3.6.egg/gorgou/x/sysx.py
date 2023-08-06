345f3d64e692366a8da55382612b36b6bf716025
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:24 +0000

    Add package gorgou/1.1.1/gorgou-1.1.1-py3.6.egg

---
+++ packages/gorgou/gorgou-1.1.1-py3.6.egg/gorgou/x/sysx.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+import os
+import sys
+import io
+import time
+
+
+def reload_sys():
+    reload(sys)
+    sys.setdefaultencoding('utf8')
+    
+def reload_stdout():
+    sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf8')
+
+
+dire = os.path.dirname(os.path.abspath(__file__))
+strftime = time.strftime('%Y%m%d', time.localtime())
+
+
+def writelocaltxtfile(local_filename, content):
+    fi = os.path.join(dire, local_filename + '.txt')
+    with open(fi, 'w', encoding='utf8') as f:
+        f.write(content)
+
+
+def joinfoldfilename(fold, filename):
+    if not os.path.exists(fold):
+        os.mkdir(fold)
+    path = os.path.join(fold, filename)
+    return path
