82aa8b4fdb56646206ae137d623372e09589727b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:52:56 +0000

    Add package py-tools/0.0.3/py_tools-0.0.3.tar.gz

---
+++ packages/py-tools/py_tools-0.0.3.tar.gz/py_tools-0.0.3/py_tools/play.py
@@ -0,0 +1,8 @@
+# Third-party imports
+import os
+import pint
+import numpy as np
+import scipy as sc
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib
---
+++ packages/py-tools/py_tools-0.0.3.tar.gz/py_tools-0.0.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(name='py_tools',
+      version='0.0.3',
+      description='Important tools for Python',
+      url='https://github.com/kevinjuan25/py_tools',
+      author='Kevin Juan',
+      author_email='kj89@cornell.edu',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=['pint','numpy','pandas','matplotlib','scipy'],
+      include_package_data=True,
+      test_suite="tests",
+      zip_safe=False)
