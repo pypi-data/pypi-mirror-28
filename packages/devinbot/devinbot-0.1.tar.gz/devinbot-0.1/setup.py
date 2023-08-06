3dd0465a7fbff7cd0e6805d7efd20ad72bee7a1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:13 +0000

    Add package devinbot/0.1/devinbot-0.1.tar.gz

---
+++ packages/devinbot/devinbot-0.1.tar.gz/devinbot-0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='devinbot',
+    version='0.1',
+    url='https://github.com/peterskipper/devin',
+    download_url='https://github.com/peterskipper/devin/archive/0.1.tar.gz',
+    license='MIT',
+    author='Peter Skipper',
+    author_email='peter.skipper@gmail.com',
+    description='Command line tool to recommend albums',
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    install_requires=['click'],
+    entry_points={
+        'console_scripts': [
+            'devin = devin.cli:main'
+        ]
+    }
+)
