1624d675258a5f691ec75c3d0428c53fe1556fd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:18:53 +0000

    Add package sqlalchemy-fsm/1.1.4/sqlalchemy_fsm-1.1.4.tar.gz

---
+++ packages/sqlalchemy-fsm/sqlalchemy_fsm-1.1.4.tar.gz/sqlalchemy_fsm-1.1.4/sqlalchemy_fsm/__init__.py
@@ -0,0 +1,15 @@
+from . import (
+    exc,
+    events,
+)
+
+from .sqltypes import FSMField
+
+from .transition import transition
+
+from .func import (
+    can_proceed,
+    is_current,
+)
+
+__version__ = '1.1.4'
