b525595e1c8408e569b9f0efde92ed8efac4df1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:02 +0000

    Add package bookscrape/0.0.1.dev1/bookscrape-0.0.1.dev1.tar.gz

---
+++ packages/bookscrape/bookscrape-0.0.1.dev1.tar.gz/bookscrape-0.0.1.dev1/setup.py
@@ -0,0 +1,26 @@
+"""This module contains the packaging routine for the pybook package"""
+
+from setuptools import setup
+from pip.download import PipSession
+from pip.req import parse_requirements
+
+
+def get_requirements(source):
+    """Get the requirements from the given ``source``
+
+    Parameters
+    ----------
+    source: str
+        The filename containing the requirements
+
+    """
+
+    install_reqs = parse_requirements(filename=source, session=PipSession())
+
+    return [str(ir.req) for ir in install_reqs]
+
+setup(
+    install_requires=get_requirements('requirements.txt')
+)
+
+
