14d754f3a9711b98e53295a3fcf6b6349e16fc1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:40 +0000

    Add package django-rest-registration/0.3.3/django-rest-registration-0.3.3.tar.gz

---
+++ packages/django-rest-registration/django-rest-registration-0.3.3.tar.gz/django-rest-registration-0.3.3/rest_registration/api/urls.py
@@ -0,0 +1,32 @@
+from django.conf.urls import url
+
+from .views import (
+    change_password,
+    login,
+    logout,
+    profile,
+    register,
+    register_email,
+    reset_password,
+    send_reset_password_link,
+    verify_email,
+    verify_registration
+)
+
+urlpatterns = [
+    url('register/$', register),
+    url('verify-registration/$', verify_registration),
+
+    url('send-reset-password-link/$', send_reset_password_link),
+    url('reset-password/$', reset_password),
+
+    url('login/$', login),
+    url('logout/$', logout),
+
+    url('profile/$', profile),
+
+    url('change-password/$', change_password),
+
+    url('register-email/$', register_email),
+    url('verify-email/$', verify_email),
+]  # noqa
