7f53f99ffd639cbe2050d7c987098fabb75cd6e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:55 +0000

    Add package pyharfbuzz/0.1.5/pyharfbuzz-0.1.5.tar.gz

---
+++ packages/pyharfbuzz/pyharfbuzz-0.1.5.tar.gz/pyharfbuzz-0.1.5/setup.py
@@ -0,0 +1,28 @@
+from sys import platform
+
+from setuptools import Extension, setup
+
+sources = ['pyharfbuzz/harfbuzz.c']
+libraries = ['harfbuzz', 'freetype']
+# library_dirs = ['freetype2/include', 'harfbuzz/src']
+include_dirs = ['harfbuzz/src', 'freetype2/include']
+
+
+setup(
+    name='pyharfbuzz',
+    version='0.1.5',
+    description='Python binding for harfbuzz an OpenType text shaping.',
+    author='Sepehr Hamzehlouy',
+    author_email='s.hamzelooy@gmail.com',
+    license='MIT',
+    url='https://github.com/RevengeComing/pyharfbuzz',
+    packages=['pyharfbuzz'],
+      ext_modules=[Extension(
+            name='pyharfbuzz.harfbuzz',
+            sources=sources,
+            libraries=libraries,
+            # library_dirs=library_dirs,
+            include_dirs=include_dirs)
+    ],
+    include_package_data=True
+)
