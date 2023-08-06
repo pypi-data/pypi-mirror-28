95821546f27559518a5114d92c586816425cc85f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:43 +0000

    Add package FlyQuery/0.1/FlyQuery-0.1-py36-none-any.whl

---
+++ packages/FlyQuery/FlyQuery-0.1-py36-none-any.whl/flyquery/flyquery.py
@@ -0,0 +1,15 @@
+import requests
+from bs4 import BeautifulSoup
+
+def get_genotype(stock_number):
+    urlpre = 'http://flybase.org/reports/FBst'
+    url = urlpre + '{:07d}'.format(stock_number)
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text, 'html.parser')
+    ### finding genotype
+    key = soup.find_all('div', class_='col-sm-3 col-sm-height field_label')
+    value = soup.find_all('div', class_=['col-sm-9 col-sm-height', 'col-sm-3 col-sm-height'])
+    for i, t in enumerate(key):
+        if 'Stock List Description' in t.text:
+            result = str(value[i].text)
+    return result
