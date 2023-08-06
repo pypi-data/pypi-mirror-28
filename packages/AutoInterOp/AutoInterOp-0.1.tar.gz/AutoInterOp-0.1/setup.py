27cd90c17a56a8fc4cdfbbb1afe3a22dfe8eaecc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:58 +0000

    Add package AutoInterOp/0.1/AutoInterOp-0.1.tar.gz

---
+++ packages/AutoInterOp/AutoInterOp-0.1.tar.gz/AutoInterOp-0.1/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name="AutoInterOp",
+    version="0.1",
+    packages=find_packages(),
+    author="Forest Dussault",
+    author_email="forest.dussault@inspection.gc.ca",
+    url="https://github.com/forestdussault/AutoInterOp"
+)
