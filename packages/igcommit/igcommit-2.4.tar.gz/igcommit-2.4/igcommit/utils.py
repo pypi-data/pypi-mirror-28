8b26f61b084d494ae88dae8cdb6b16931c0e0407
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:19:36 +0000

    Add package igcommit/2.4/igcommit-2.4.tar.gz

---
+++ packages/igcommit/igcommit-2.4.tar.gz/igcommit-2.4/igcommit/utils.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""igcommit - Utility functions
+
+Copyright (c) 2016, InnoGames GmbH
+"""
+
+from __future__ import unicode_literals
+
+from os import environ, access, X_OK
+
+
+def get_exe_path(exe):
+    for dir_path in environ['PATH'].split(':'):
+        path = dir_path.strip('"') + '/' + exe
+        if access(path, X_OK):
+            return path
+
+
+def iter_buffer(iterable, amount):
+    assert amount > 1
+    memo = []
+    for elem in iterable:
+        if elem is not None:
+            memo.append(elem)
+            if len(memo) < amount:
+                continue
+        yield memo.pop(0)
+
+    for elem in memo:
+        yield elem
