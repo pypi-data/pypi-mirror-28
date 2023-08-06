de5bf915cb8e15cdc0dc875f730e9ec60f3bdc3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:04:24 +0000

    Add package cypher4/1.0.1/cypher4-1.0.1.tar.gz

---
+++ packages/cypher4/cypher4-1.0.1.tar.gz/cypher4-1.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='cypher4',
+    version='1.0.01',
+    description='Retrieve Music Lyrics',
+    author='Timothy James Dobbins',
+    author_email='tmthyjames@gmail.com',
+    url='https://github.com/tmthyjames/cypher',
+    install_requires=['requests', 'beautifulsoup4']
+)
