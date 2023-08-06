aec68f54c6011978bb7837a2cc44888aec4770cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:25 +0000

    Add package velplot/0.3.0/velplot-0.3.0.tar.gz

---
+++ packages/velplot/velplot-0.3.0.tar.gz/velplot-0.3.0/velplot/__init__.py
@@ -0,0 +1,13 @@
+__version__ = '0.3.0'
+
+from .settings     import *
+from .atominfo     import *
+from .checkshift   import *
+from .createchunks import *
+from .fitfort      import *
+from .fortread     import *
+from .getdv        import *
+from .makefig      import *
+from .plotfit      import *
+from .readspec     import *
+from .voigt        import *
---
+++ packages/velplot/velplot-0.3.0.tar.gz/velplot-0.3.0/velplot/createchunks.py
@@ -0,0 +1,44 @@
+import velplot,os,numpy
+
+def create_chunks(fortfile,pcvals,version):
+    '''
+    Generate data chunk for each fitted region.
+
+    Parameters
+    ----------
+    fortfile : str
+      Path to input fort.13 or fort.26 file.
+    pcvals : bool
+      Flag to check if pcvals command in vp_setup.dat
+    version : str
+      VPFIT executable filename
+    '''
+    opfile = open('fitcommands','w')
+    opfile.write('d\n')                 # Run display command + enter
+    if pcvals:                          # If development tools called...
+        opfile.write('\n')              # ...used default setup -> enter only
+    opfile.write('\n')                  # Used default selfeter (logN) -> enter only
+    opfile.write(fortfile+'\n')         # Insert fort file name + enter
+    opfile.write('\n')                  # Plot the fitting region (default is yes) -> enter only
+    opfile.write('\nas')
+    for line in velplot.table1:
+        opfile.write('\n\n\n\n')
+    opfile.write('\n\n\nn\n\n')
+    opfile.close()
+    os.system(version+' < fitcommands > termout')
+    output = numpy.loadtxt('termout',dtype='str',delimiter='\n')
+    for i in range(len(output)):
+        if 'Statistics for each region :' in output[i]:
+            i,k = i+2,0
+            while 'Plot?' not in output[i]:
+                velplot.header[k,-4] = 'n/a' if '*' in output[i].split()[2] else '%.4f'%(float(output[i].split()[2]))
+                velplot.header[k,-3] = 'n/a' if '*' in output[i].split()[2] else '%.4f'%(float(output[i].split()[2])/float(output[i].split()[4]))
+                velplot.header[k,-2] = output[i].split()[3]
+                velplot.header[k,-1] = output[i].split()[4]
+                k = k + 1
+                i = i + 2
+    if os.path.exists('chunks')==False:
+        os.system('mkdir chunks')
+    os.system('mv vpfit_chunk* chunks/')
+    os.system('rm fitcommands termout')
+    
