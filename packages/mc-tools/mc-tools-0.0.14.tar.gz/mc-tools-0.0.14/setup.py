623b947faf3009745ec155e009c22422ccf5e889
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:39 +0000

    Add package mc-tools/0.0.14/mc-tools-0.0.14.tar.gz

---
+++ packages/mc-tools/mc-tools-0.0.14.tar.gz/mc-tools-0.0.14/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mc-tools',
+    version='0.0.14',
+    author='Max Lobur',
+    author_email='max_lobur@outlook.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+    install_requires=[
+        "pyyaml",
+        "futures",
+        "click",
+        "YaDiskClient",
+    ],
+    packages=find_packages(),
+    data_files=[('/tmp', ['mua-config.yml.sample'])],
+    entry_points={
+        'console_scripts': [
+            'mua=mc_tools.cli:cli',
+        ],
+    },
+)
