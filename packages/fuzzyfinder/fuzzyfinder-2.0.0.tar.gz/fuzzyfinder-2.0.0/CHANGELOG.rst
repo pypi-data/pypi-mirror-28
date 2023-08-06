2959ef9008eaae492d7dbd86149d1b57d2db9c04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:16 +0000

    Add package fuzzyfinder/2.0.0/fuzzyfinder-2.0.0.tar.gz

---
+++ packages/fuzzyfinder/fuzzyfinder-2.0.0.tar.gz/fuzzyfinder-2.0.0/CHANGELOG.rst
@@ -0,0 +1,11 @@
+.. :changelog:
+
+Changelog
+---------
+
+2.0.0 (2017-01-25)
+---------------------
+
+* Case insensitive matching. (Gokul Soumya)
+* Add an accessor function for fuzzy find. (Amjith)
+* Support integer inputs. (Matheus)
---
+++ packages/fuzzyfinder/fuzzyfinder-2.0.0.tar.gz/fuzzyfinder-2.0.0/fuzzyfinder/main.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+import re
+from . import export
+
+@export
+def fuzzyfinder(input, collection, accessor=lambda x: x):
+    """
+    Args:
+        input (str): A partial string which is typically entered by a user.
+        collection (iterable): A collection of strings which will be filtered
+                               based on the `input`.
+
+    Returns:
+        suggestions (generator): A generator object that produces a list of
+            suggestions narrowed down from `collection` using the `input`.
+    """
+    suggestions = []
+    input = str(input) if not isinstance(input, str) else input
+    pat = '.*?'.join(map(re.escape, input))
+    regex = re.compile(pat, re.IGNORECASE)
+    for item in collection:
+        r = regex.search(accessor(item))
+        if r:
+            suggestions.append((len(r.group()), r.start(), accessor(item), item))
+
+    return (z[-1] for z in sorted(suggestions))
