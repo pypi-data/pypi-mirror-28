a88aa8f721e8301cdcf05a0a0e0885387c9c6d45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:55 +0000

    Add package nas-code-checker/0.1/nas-code-checker-0.1.tar.gz

---
+++ packages/nas-code-checker/nas-code-checker-0.1.tar.gz/nas-code-checker-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+ 
+setup(
+    name='nas-code-checker',    # This is the name of your PyPI-package.
+    version='0.1',                          # Update the version number for new releases
+    scripts=['nas-code-checker']                  # The name of your scipt, and also the command you'll be using for calling it
+)
