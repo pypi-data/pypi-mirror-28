00da37f7adf2adf80c0468ef0ec4d6db56142540
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:23 +0000

    Add package marshmallowjson/0.2.0/marshmallowjson-0.2.0.tar.gz

---
+++ packages/marshmallowjson/marshmallowjson-0.2.0.tar.gz/marshmallowjson-0.2.0/tests/test_marshmallowjson.py
@@ -0,0 +1,45 @@
+"""Tests for `marshmallowjson` package."""
+
+import pytest
+
+from marshmallowjson import Definition
+
+
+@pytest.fixture
+def email():
+    schema = {
+        'Person': {
+            'email': {
+                'kind': 'email',
+                'required': True,
+            }
+        }
+    }
+    return Definition(schema).top()
+
+
+def test_email_field_is_required(email):
+    data, err = email.load({
+        'not-email': 'definitively not an email'
+    })
+    assert not data
+    assert 'email' in err
+
+
+@pytest.mark.parametrize('example', ['ab.com', 'a+bc.com'])
+def test_email_fails_with_invalid_email(email, example):
+    data, err = email.load({
+        'email': example
+    })
+    assert not data
+    assert 'email' in err
+
+
+@pytest.mark.parametrize('example', ['a@b.com', 'a+b@c.com'])
+def test_email_passes_with_valid_email(email, example):
+    data, err = email.load({
+        'email': example
+    })
+    assert not err
+    assert 'email' in data
+    assert data['email'] == example
