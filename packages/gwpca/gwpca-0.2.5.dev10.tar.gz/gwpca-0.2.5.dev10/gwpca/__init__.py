ed8f32fb640e179da382cd4ffe54e11d65e6a8fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:56 +0000

    Add package gwpca/0.2.5.dev10/gwpca-0.2.5.dev10.tar.gz

---
+++ packages/gwpca/gwpca-0.2.5.dev10.tar.gz/gwpca-0.2.5.dev10/gwpca/__init__.py
@@ -0,0 +1,23 @@
+import os
+
+from girder_worker import GirderWorkerPluginABC
+
+from pkg_resources import get_distribution, DistributionNotFound
+
+
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    # package is not installed
+    pass
+
+
+
+class GWPCAPlugin(GirderWorkerPluginABC):
+    def __init__(self, app, *args, **kwargs):
+        self.app = app
+
+    def task_imports(self):
+        # Return a list of python importable paths to the
+        # plugin's path directory
+        return ['gwpca.tasks']
