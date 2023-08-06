8b224409e3eadadf43b0d4e83f84656236863ad5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:26 +0000

    Add package redash-client/0.1.17/redash_client-0.1.17.tar.gz

---
+++ packages/redash-client/redash_client-0.1.17.tar.gz/redash_client-0.1.17/redash_client/constants.py
@@ -0,0 +1,65 @@
+class RetentionType:
+  DAILY = "day"
+  WEEKLY = "week"
+
+
+class TimeInterval:
+  DAILY = "daily"
+  WEEKLY = "weekly"
+  MONTHLY = "monthly"
+  allowed_time_intervals = [DAILY, WEEKLY, MONTHLY]
+
+
+class VizType:
+  CHART = "CHART"
+  COHORT = "COHORT"
+
+
+class VizWidth:
+  REGULAR = 1
+  WIDE = 2
+
+
+class ChartType:
+  BAR = "column"
+  PIE = "pie"
+  LINE = "line"
+  SCATTER = "scatter"
+  AREA = "area"
+  allowed_chart_types = [BAR, PIE, LINE, SCATTER, AREA]
+
+
+TTableSchema = [
+    {
+        "name": "Metric",
+        "type": "string",
+        "friendly_name": "Metric"
+    }, {
+        "name": "Alpha Error",
+        "type": "float",
+        "friendly_name": "Alpha Error"
+    }, {
+        "name": "Power",
+        "type": "float",
+        "friendly_name": "Power"
+    }, {
+        "name": "Two-Tailed P-value (ttest)",
+        "type": "float",
+        "friendly_name": "Two-Tailed P-value (ttest)"
+    }, {
+        "name": "Control Mean",
+        "type": "float",
+        "friendly_name": "Control Mean"
+    }, {
+        "name": "Experiment Mean - Control Mean",
+        "type": "float",
+        "friendly_name": "Experiment Mean - Control Mean"
+    }, {
+        "name": "Percent Difference in Means",
+        "type": "float",
+        "friendly_name": "Percent Difference in Means"
+    }, {
+        "name": "Significance",
+        "type": "string",
+        "friendly_name": "Significance"
+    }]
