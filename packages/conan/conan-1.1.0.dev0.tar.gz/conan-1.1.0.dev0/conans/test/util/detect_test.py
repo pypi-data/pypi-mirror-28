0984deb7153af26967933ab20155cc1901fb895d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:59 +0000

    Add package conan/1.1.0.dev0/conan-1.1.0.dev0.tar.gz

---
+++ packages/conan/conan-1.1.0.dev0.tar.gz/conan-1.1.0.dev0/conans/test/util/detect_test.py
@@ -0,0 +1,15 @@
+import unittest
+from conans.test.utils.tools import TestBufferConanOutput
+from conans.client.conf.detect import detect_defaults_settings
+import platform
+
+
+class DetectTest(unittest.TestCase):
+
+    def detect_test(self):
+        output = TestBufferConanOutput()
+        result = detect_defaults_settings(output)
+        if platform.system() == "Linux":
+            for (name, value) in result:
+                if name == "compiler":
+                    self.assertEquals(value, "gcc")
