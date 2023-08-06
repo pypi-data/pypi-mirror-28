1ee22632ebf6fe5d522bd29ada7165a7d382f5cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:30 +0000

    Add package class1.py/1.1.0/class1.py-1.1.0.tar.gz

---
+++ packages/class1.py/class1.py-1.1.0.tar.gz/class1.py-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name='class1.py',
+        version='1.1.0',
+        py_modules=['pachong'],
+        author='hhhsy217',
+        author_email='hongshiya@126.com',
+        url='http://www.baidu.com',
+        description='test',
+    )
