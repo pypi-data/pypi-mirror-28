d36eb235df2ca3e36f84854846dd0480c09cef25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:04:19 +0000

    Add package cypher3/1.0.1/cypher3-1.0.1.tar.gz

---
+++ packages/cypher3/cypher3-1.0.1.tar.gz/cypher3-1.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='cypher3',
+    version='1.0.01',
+    description='Retrieve Music Lyrics',
+    author='Timothy James Dobbins',
+    author_email='tmthyjames@gmail.com',
+    url='https://github.com/tmthyjames/cypher',
+    install_requires=['requests', 'beautifulsoup4']
+)
