efe2d96e90cd5fbc2cb7ca202b079c2b2d08552d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:21 +0000

    Add package gin165/0.0.7/gin165-0.0.7.tar.gz

---
+++ packages/gin165/gin165-0.0.7.tar.gz/gin165-0.0.7/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+
+setup(
+    name='gin165',
+    version='0.0.7',
+    description='Command line utility for my personal needs.',
+    py_modules=['gin'],
+    install_requires=[
+        'Click',
+        'Pillow',
+    ],
+    entry_points={
+        'console_scripts': [
+            'gin=gin:gin',
+        ]
+    },
+    python_requires='~=3.6',
+)
