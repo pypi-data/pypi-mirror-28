75ea484cac9fefd59cead4965e374ee955949ddd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:21 +0000

    Add package udata-gouvfr/1.2.4/udata_gouvfr-1.2.4-py2.py3-none-any.whl

---
+++ packages/udata-gouvfr/udata_gouvfr-1.2.4-py2.py3-none-any.whl/udata_gouvfr/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+'''
+uData customizations for Data.gouv.fr
+'''
+from __future__ import unicode_literals
+
+__version__ = '1.2.4'
+__description__ = 'uData customizations for Data.gouv.fr'
+
+
+def init_app(app):
+    from . import harvesters  # noqa: needed for registration
