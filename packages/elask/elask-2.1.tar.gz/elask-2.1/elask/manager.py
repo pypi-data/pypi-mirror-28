6bbbf42db15e838e6f4dc955a0aadf8eccde6406
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:43 +0000

    Add package elask/2.1/elask-2.1.tar.gz

---
+++ packages/elask/elask-2.1.tar.gz/elask-2.1/elask/manager.py
@@ -0,0 +1,34 @@
+"""
+Frequently used elasticsearch functions
+"""
+from elasticsearch import Elasticsearch, helpers
+from elasticsearch_dsl import Q, Search
+from server import app
+
+
+class ESManager(object):
+
+    def __init__(self):
+        self.client = Elasticsearch(app.config['ELASTICSEARCH_DOMAIN'])
+
+    def get_indices(self):
+        """ To return the list of indices available """
+        all_indcies = list(self.client.indices.get_alias().keys())
+        if '.kibana' in all_indcies:
+            all_indcies.remove('.kibana')
+        return all_indcies
+
+    def delete_all_indexes(self):
+        """ To delete all the indexes """
+        for every_index in self.get_indices():
+            self.client.indices.delete(index=every_index, ignore=[400, 404])
+        return True
+
+    def initiate(self, index=None):
+        """
+        To initiate the required docs when customer got created
+        """
+        from authentication.models import Customer, Permission, Role, UserProfile, UserRole
+        for Doc in [Customer, Permission, Role, UserProfile, UserRole]:
+            Doc.init(index=index)
+        return True
