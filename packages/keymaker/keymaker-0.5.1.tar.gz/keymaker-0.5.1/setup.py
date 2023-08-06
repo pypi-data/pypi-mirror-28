75b581e9f77fdc207753ded733d082ae10179b71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:25 +0000

    Add package keymaker/0.5.1/keymaker-0.5.1.tar.gz

---
+++ packages/keymaker/keymaker-0.5.1.tar.gz/keymaker-0.5.1/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import os, glob
+from setuptools import setup, find_packages
+
+install_requires = [line.rstrip() for line in open(os.path.join(os.path.dirname(__file__), "requirements.txt"))]
+
+setup(
+    name='keymaker',
+    version='0.5.1',
+    url='https://github.com/kislyuk/keymaker',
+    license='Apache Software License',
+    author='Andrey Kislyuk',
+    author_email='kislyuk@gmail.com',
+    description='Lightweight SSH key management on AWS EC2',
+    long_description=open('README.rst').read(),
+    install_requires=install_requires,
+    packages=find_packages(exclude=['test']),
+    scripts=glob.glob('scripts/*'),
+    platforms=['MacOS X', 'Posix'],
+    include_package_data=True,
+    test_suite='test',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
