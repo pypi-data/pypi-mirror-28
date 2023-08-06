f66f8ad57f5b944c31854fa44db2fa3d86a11474
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:57 +0000

    Add package Flask-Flash/1.6.8/Flask-Flash-1.6.8.tar.gz

---
+++ packages/Flask-Flash/Flask-Flash-1.6.8.tar.gz/Flask-Flash-1.6.8/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+setup(name='Flask-Flash',
+      version='1.6.8',
+      description='Flask API framework (API + Client) to create simple APIs from database models.',
+      author='Olivier Cervello',
+      author_email='olivier.cervello@gmail.com',
+      url='https://github.com/ocervell/flask_flash',
+      install_requires=[
+        'SQLAlchemy',
+        'marshmallow',
+        'marshmallow-sqlalchemy',
+        'Flask',
+        'Flask-Restful',
+        'Flask-SQLAlchemy',
+        'Flask-Marshmallow',
+        'Flask-Script',
+        'Flask-Migrate',
+	    'Flask-Caching',
+        'Flask-HTTPAuth',
+        'requests',
+        'numpy',
+        'pyyaml',
+        'faker',
+        'inflect'
+      ],
+      packages=[
+        'flask_flash',
+        'flask_flash/client',
+      ],
+      zip_safe=False)
