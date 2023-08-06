800d13d07870443ef2057ccde2930eee39428eae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:20 +0000

    Add package peerplays/0.1.11/peerplays-0.1.11.tar.gz

---
+++ packages/peerplays/peerplays-0.1.11.tar.gz/peerplays-0.1.11/peerplaysbase/chains.py
@@ -0,0 +1,14 @@
+known_chains = {
+    "PPY": {
+        "chain_id": "6b6b5f0ce7a36d323768e534f3edb41c6d6332a541a95725b98e28d140850134",
+        "core_symbol": "PPY",
+        "prefix": "PPY"},
+    "PPYT": {
+        "chain_id": "594e284d3c733afaaa34a5e99a39edb31e5192fab023101d691d952034902237",
+        "core_symbol": "PPY",
+        "prefix": "PPY"},
+    "PPYTEST": {
+        "chain_id": "be6b79295e728406cbb7494bcb626e62ad278fa4018699cf8f75739f4c1a81fd",
+        "core_symbol": "PPYTEST",
+        "prefix": "PPYTEST"},
+}
