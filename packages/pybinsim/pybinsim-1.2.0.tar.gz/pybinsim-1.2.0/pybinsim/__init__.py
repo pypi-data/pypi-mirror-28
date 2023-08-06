c3b22b34e379d8a093ccef00bc29ae4ae1fccd84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:13 +0000

    Add package pybinsim/1.2.0/pybinsim-1.2.0.tar.gz

---
+++ packages/pybinsim/pybinsim-1.2.0.tar.gz/pybinsim-1.2.0/pybinsim/__init__.py
@@ -0,0 +1,22 @@
+import logging
+
+__version__ = "1.2.0"
+
+from pybinsim.application import BinSim
+
+
+def init_logging(loglevel):
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(loglevel)
+
+    formatter = logging.Formatter('%(asctime)s - %(filename)s:%(lineno)d - %(levelname)s - %(message)s')
+    console_handler.setFormatter(formatter)
+
+    logger = logging.getLogger("pybinsim")
+    logger.addHandler(console_handler)
+    logger.setLevel(loglevel)
+
+    return logger
+
+
+logger = init_logging(logging.INFO)
