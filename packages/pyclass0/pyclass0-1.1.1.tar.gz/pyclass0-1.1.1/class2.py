266cc6fb81379d29eb69fe42a55827605e3f168e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:29 +0000

    Add package pyclass0/1.1.1/pyclass0-1.1.1.tar.gz

---
+++ packages/pyclass0/pyclass0-1.1.1.tar.gz/pyclass0-1.1.1/class2.py
@@ -0,0 +1,7 @@
+class mode_file():
+    def hello(self):
+
+        print('hello world')
+
+    def world(self):
+        return 123456
---
+++ packages/pyclass0/pyclass0-1.1.1.tar.gz/pyclass0-1.1.1/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+# setup(
+#     name = 'wfh',
+#     version = '1.1.1',
+#     py_modules = ['class2'],
+#     author = 'wenfuhua',
+#     author_email = '1048275741@qq.com',
+#     url = 'https://www.baidu.com/ ',
+#     description = 'print hello infomation'
+#     )
+
+setup(
+    name = 'pyclass0',
+    version = '1.1.1',
+    py_modules = ['class2'],
+    author = 'wenfuhua',
+    author_email = '1048275741@qq.com',
+    url = 'https://www.baidu.com/ ',
+    description = 'print hello infomation'
+    )
