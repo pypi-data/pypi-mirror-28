56b0a2373d7049b1d01d825567988d8935b89ea2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:38 +0000

    Add package pyudpscan/1.0.2/pyudpscan-1.0.2.tar.gz

---
+++ packages/pyudpscan/pyudpscan-1.0.2.tar.gz/pyudpscan-1.0.2/scanner/main.py
@@ -0,0 +1,40 @@
+import argparse
+
+from scanner import Scanner
+
+
+def main():
+    try:
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--hosts", dest="hosts", type=str,
+            nargs="+", default=["127.0.0.1"], help="List of target hosts")
+        parser.add_argument("--ports", dest="ports", type=int,
+            nargs="+", default=[19], help="List of target ports")
+        parser.add_argument("-p", "--proxy", dest="proxies",
+            nargs="+", default=None, type=str,
+            help=("List of SOCKS5 proxies which will be taken at random.\n"
+                  "Example: 99.99.99.99:1080"))
+        parser.add_argument("-t", "--timeout", dest="timeout",
+            type=int, default=1,
+            help="How long to wait for reply at UDP request from target host")
+        parser.add_argument("-r", "--recheck", dest="recheck", type=int,
+            default=0, help="Number of recheck for every port")
+        parser.add_argument("-i", "--src-int-address", dest="src_int_address",
+            type=str, required=True,
+            help=("Address of local source interface to listen on.\n"
+                  "Example: 192.168.0.15"))
+        args = parser.parse_args()
+
+        s = Scanner(args.proxies, args.hosts, args.ports,
+            args.timeout, args.recheck, args.src_int_address)
+        for k, v in s.scan().iteritems():
+            if k not in ["current_ip", "current_port"]:
+                print("{}\t{}".format(k, v))
+
+    except KeyboardInterrupt:
+        print('\nThe process was interrupted by the user')
+        raise SystemExit
+
+
+if __name__ == "__main__":
+    main()
