c4be515b4c960f00ef3cca24dab8fab588076119
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:53:15 +0000

    Add package ml_basicss/0.11/ml_basicss-0.11.tar.gz

---
+++ packages/ml_basicss/ml_basicss-0.11.tar.gz/ml_basicss-0.11/README.md
@@ -0,0 +1,10 @@
+# ml_basicss
+This repository contains ml_basicss package available on pip.
+
+url: https://pypi.python.org/pypi/ml_basicss/0.1
+
+## Install with pip
+
+```{r, engine='bash', count_lines}
+pip3 install ml_basicss
+```
---
+++ packages/ml_basicss/ml_basicss-0.11.tar.gz/ml_basicss-0.11/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(name='ml_basicss',
+      version='0.11',
+      description='first test',
+      url='https://github.com/kaushal793/ml_basics',
+      author='Kaushal Paneri',
+      author_email='paneri.k@husky.neu.edu',
+      license='MIT',
+      packages=['ml_basicss'],
+      zip_safe=False)
+
+install_requires=[
+   'numpy',
+   'pandas',
+   'scipy'
+]
