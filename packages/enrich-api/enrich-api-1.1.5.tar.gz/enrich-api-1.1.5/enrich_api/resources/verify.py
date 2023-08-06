0b4f11d6503f3ad6d6e95f71921b6befb1047600
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:54 +0000

    Add package enrich-api/1.1.5/enrich-api-1.1.5.tar.gz

---
+++ packages/enrich-api/enrich-api-1.1.5.tar.gz/enrich-api-1.1.5/enrich_api/resources/verify.py
@@ -0,0 +1,16 @@
+##
+# enrich-api-python
+#
+# Copyright 2017, Valerian Saliou
+# Author: Valerian Saliou <valerian@valeriansaliou.name>
+##
+
+class VerifyResource(object):
+  def __init__(self, parent):
+    self.parent = parent
+
+  def validate_email(self, query):
+    return self.parent.get("/verify/validate/email", query)
+
+  def format_email(self, query):
+    return self.parent.get("/verify/format/email", query)
