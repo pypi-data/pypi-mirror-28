6035daae13b31f36b05751f47f005d25ecfb3354
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:53:18 +0000

    Add package channels/2.0.0/channels-2.0.0.tar.gz

---
+++ packages/channels/channels-2.0.0.tar.gz/channels-2.0.0/channels/apps.py
@@ -0,0 +1,21 @@
+from django.apps import AppConfig
+
+# We import this here to ensure the reactor is installed very early on
+# in case other packages accidentally import twisted.internet.reactor
+# (e.g. raven does this).
+import daphne.server  # noqa
+
+from .package_checks import check_all
+
+
+class ChannelsConfig(AppConfig):
+
+    name = "channels"
+    verbose_name = "Channels"
+
+    def ready(self):
+        # Check versions
+        check_all()
+        # Do django monkeypatches
+        from .hacks import monkeypatch_django
+        monkeypatch_django()
