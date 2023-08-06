37aca7e7541a599fcb17639310d096791827dab1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:03 +0000

    Add package tagcloser/1.0.0/tagcloser-1.0.0.tar.gz

---
+++ packages/tagcloser/tagcloser-1.0.0.tar.gz/tagcloser-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='tagcloser',
+        version='1.0.0',
+        url='https://github.com/Hao-Luo/TagCloser',
+        keywords='html htmltag excerpt',
+        description='This is to close unclosed html tag in html file, mainly for generating the excerpt html.',
+        author='Hao Luo',
+        author_email='hao@sporule.com',
+        license='GPL',
+        zip_safe=False)
