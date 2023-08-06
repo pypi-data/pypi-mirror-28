d95d004c1993ae90ce09af584e5cd214cc498469
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:06 +0000

    Add package pyexpander/1.8.2/pyexpander-1.8.2.tar.gz

---
+++ packages/pyexpander/pyexpander-1.8.2.tar.gz/pyexpander-1.8.2/administration_tools/sourceforge-upload-distfiles.sh
@@ -0,0 +1,7 @@
+cd ..
+DESTHOST=goetzpf@frs.sourceforge.net
+DESTPATH=/home/frs/project/p/py/pyexpander
+#scp dist/*.tar.gz $DESTHOST:$DESTPATH
+#scp dist/*.zip $DESTHOST:$DESTPATH
+scp -r dist/* $DESTHOST:$DESTPATH
+scp README $DESTHOST:$DESTPATH
