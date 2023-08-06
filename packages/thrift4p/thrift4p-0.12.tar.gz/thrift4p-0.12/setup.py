1f09c57a4752007f9e67345ed1bbb6eed9795c64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:21 +0000

    Add package thrift4p/0.12/thrift4p-0.12.tar.gz

---
+++ packages/thrift4p/thrift4p-0.12.tar.gz/thrift4p-0.12/setup.py
@@ -0,0 +1,22 @@
+# encoding:utf-8
+
+from setuptools import setup, find_packages  
+
+setup(
+    name='thrift4p',
+    version='0.12',
+    description="a python interface for thrift api in didapinche.",
+    long_description=open("README.rst").read(),
+    keywords='python thrift didapinche',  
+    author='brown',
+    author_email='317787106@qq.com', 
+    url='http://gitlab.didapinche.com/dm/anticheating/tree/master/thrift4p', 
+    packages=find_packages(exclude=['test_thrift', 'examples', 'tests', 'MANIFEST.in']),  
+    include_package_data=True,  
+    zip_safe=False,  
+    install_requires=[
+        'kazoo',
+        'thrift'
+    ],
+    entry_points={},
+)
