9fff4106cf49b5dfee6845a5780af630c21d21f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:18 +0000

    Add package grokcore.annotation/3.0.1/grokcore.annotation-3.0.1.tar.gz

---
+++ packages/grokcore.annotation/grokcore.annotation-3.0.1.tar.gz/grokcore.annotation-3.0.1/setup.py
@@ -0,0 +1,65 @@
+from setuptools import setup, find_packages
+import os
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+long_description = (
+    read('README.txt')
+    + '\n' +
+    read('CHANGES.txt')
+    )
+
+tests_require = [
+    'zope.configuration',
+    'zope.schema',
+    'zope.testing > 4.6',
+    'zope.testrunner',
+    ]
+
+
+setup(
+    name='grokcore.annotation',
+    version='3.0.1',
+    author='Grok Team',
+    author_email='grok-dev@zope.org',
+    url='http://grok.zope.org',
+    download_url='http://pypi.python.org/pypi/grokcore.annotation',
+    description='Grok-like configuration for Zope annotations',
+    long_description=long_description,
+    license='ZPL',
+    classifiers=['Environment :: Web Environment',
+                 'Intended Audience :: Developers',
+                 'License :: OSI Approved :: Zope Public License',
+                 'Programming Language :: Python',
+                 'Programming Language :: Python :: 2',
+                 'Programming Language :: Python :: 2.7',
+                 'Programming Language :: Python :: 3',
+                 'Programming Language :: Python :: 3.4',
+                 'Programming Language :: Python :: 3.5',
+                 'Programming Language :: Python :: 3.6',
+                 'Programming Language :: Python :: Implementation :: CPython',
+                 'Programming Language :: Python :: Implementation :: PyPy',
+                 'Framework :: Zope3',
+                 ],
+
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    namespace_packages=['grokcore'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'grokcore.component >= 2.5dev',
+        'martian',
+        'setuptools',
+        'zope.annotation',
+        'zope.cachedescriptors',
+        'zope.component',
+        'zope.container',
+        'zope.interface',
+        'zope.location',
+        ],
+    tests_require=tests_require,
+    test_suite='grokcore.annotation.tests.test_grok.test_suite',
+    extras_require={'test': tests_require},
+    )
