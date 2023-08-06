2747ecaf97dafdeec98621bf1de87b5f36e0c837
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:29 +0000

    Add package redfishtool/1.0.4/redfishtool-1.0.4.tar.gz

---
+++ packages/redfishtool/redfishtool-1.0.4.tar.gz/redfishtool-1.0.4/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(name='redfishtool',
+      version='1.0.4',
+      description='Redfishtool package and command-line client',
+      author='DMTF, https://www.dmtf.org/standards/feedback',
+      license='BSD 3-clause "New" or "Revised License"',
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'License :: OSI Approved :: BSD License',
+          'Programming Language :: Python :: 3.4',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+          'Topic :: Communications'
+      ],
+      keywords='Redfish',
+      url='https://github.com/DMTF/Redfishtool',
+      download_url='https://github.com/DMTF/Redfishtool/archive/1.0.4.tar.gz',
+      packages=['redfishtool'],
+      scripts=['scripts/redfishtool'],
+      install_requires=['requests']
+      )
