552c76b2addad22e16277a2b1dd3b440c272f00b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:18:28 +0000

    Add package pyobb/1.0/pyobb-1.0.tar.gz

---
+++ packages/pyobb/pyobb-1.0.tar.gz/pyobb-1.0/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+
+def get_requirements():
+    with open('requirements.txt', 'r') as requirements_file:
+        lines = requirements_file.readlines()
+    requirements = []
+    for line in lines:
+        if line.startswith('#'):
+            continue
+        requirements.append(line.strip())
+    return requirements
+
+
+setup(name='pyobb',
+      packages=['pyobb'],
+      version='1.0',
+      install_requires=get_requirements(),
+      description='Python OBB Implementation',
+      author='Pedro Boechat',
+      author_email='pboechat@gmail.com',
+      url='https://github.com/pboechat/pyobb',
+      download_url = 'https://github.com/pboechat/pyobb/archive/1.0.tar.gz',
+      keywords = ['obb', 'computational-geometry', 'oriented-bounding-box'])
