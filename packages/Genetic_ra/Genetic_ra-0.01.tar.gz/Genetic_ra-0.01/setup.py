63ae0b82efed9ed863415908755f339b76da97a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:09 +0000

    Add package Genetic_ra/0.01/Genetic_ra-0.01.tar.gz

---
+++ packages/Genetic_ra/Genetic_ra-0.01.tar.gz/Genetic_ra-0.01/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='Genetic_ra',
+    version='0.01',
+    packages=['genetic_rajeev',],
+    license='MIT License',
+    long_description=open('README.txt').read(),
+)
