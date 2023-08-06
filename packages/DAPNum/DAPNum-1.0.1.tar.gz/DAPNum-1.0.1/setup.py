11927723e64fdf0c03a4f43da484088d3ca8b9fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:59 +0000

    Add package DAPNum/1.0.1/DAPNum-1.0.1.tar.gz

---
+++ packages/DAPNum/DAPNum-1.0.1.tar.gz/DAPNum-1.0.1/setup.py
@@ -0,0 +1,13 @@
+
+
+from setuptools import setup
+
+setup(
+    name = 'DAPNum',
+    version = '1.0.1',
+    author = 'Durant',
+    author_email = '454311272@qq.com',
+    url = 'http://blog.csdn.net/weixin_38637598/article/details/79294283',
+    description = 'this is a test package for finding All prime_number under input.',
+    keywords = ['prime number'],
+)
