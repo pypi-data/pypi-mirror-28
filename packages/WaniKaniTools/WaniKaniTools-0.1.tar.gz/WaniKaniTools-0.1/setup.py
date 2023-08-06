f441d4076987562c481436df52462b3ee4ea4f8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:11 +0000

    Add package WaniKaniTools/0.1/WaniKaniTools-0.1.tar.gz

---
+++ packages/WaniKaniTools/WaniKaniTools-0.1.tar.gz/WaniKaniTools-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='WaniKaniTools',
+      version='0.1',
+      description='Working with WaniKani API, login, and Community.',
+      url='https://github.com/patarapolw/WaniKaniTools',
+      author='Pacharapol Withayasakpunt',
+      author_email='patarapolw@gmail.com',
+      license='MIT',
+      packages=['WaniKaniTools'],
+      zip_safe=False)
