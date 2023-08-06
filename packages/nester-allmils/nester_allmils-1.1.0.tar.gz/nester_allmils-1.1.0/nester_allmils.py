3b75ec0d59368f63be15727d3826d031855be3e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:38 +0000

    Add package nester-allmils/1.1.0/nester_allmils-1.1.0.tar.gz

---
+++ packages/nester-allmils/nester_allmils-1.1.0.tar.gz/nester_allmils-1.1.0/nester_allmils.py
@@ -0,0 +1,13 @@
+"""Nester module contains the print_lol function, which breaks down and prints all individual items in a list, one at a time""" 
+
+def print_lol(the_list, tabs):
+    """Print_lol takes a positional argument the_list, checks if each item from that is a
+        list itself recursively and prints the item if it is not. It also uses the secondary argument
+        Tabs to reference the indent printed for each sub-list (use 0 for no indent)"""
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol(each_item, tabs+1)            
+        else:
+            for i in range(tabs):
+                print('\t', end='')
+            print(each_item)
