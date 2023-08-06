18bcad07f8365bea4adaa1d399a954430af75f2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:22 +0000

    Add package ptx/1.0/ptx-1.0.tar.gz

---
+++ packages/ptx/ptx-1.0.tar.gz/ptx-1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(
+    name='ptx',
+    author='yuzzwx',
+    author_email='yuzzwx@gmail.com',
+    version='1.0',
+    url='https://github.com/yuzzwx/PTX',
+    description='A utility code for PTX authorization.',
+    license='MIT',
+)
