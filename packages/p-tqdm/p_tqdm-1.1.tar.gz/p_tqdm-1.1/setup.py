1f62c84e21af500dcfe0e917b866f09b7bae8266
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:34 +0000

    Add package p-tqdm/1.1/p_tqdm-1.1.tar.gz

---
+++ packages/p-tqdm/p_tqdm-1.1.tar.gz/p_tqdm-1.1/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name = 'p_tqdm',
+    packages = ['p_tqdm'],
+    version = '1.1',
+    description = 'Parallel processing with progress bars',
+    author = 'Kyle Swanson',
+    author_email = 'swansonk.14@gmail.com',
+    url = 'https://github.com/swansonk14/p_tqdm',
+    license = 'MIT',
+    install_requires = ['tqdm', 'pathos'],
+    keywords = ['tqdm', 'progress bar', 'parallel'],
+    classifiers = [],
+)
