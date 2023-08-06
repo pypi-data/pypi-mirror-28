641b30b973e7f784150b4d639f9711f1259eba01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:04:52 +0000

    Add package wagtail-calendar/1.0/wagtail-calendar-1.0.tar.gz

---
+++ packages/wagtail-calendar/wagtail-calendar-1.0.tar.gz/wagtail-calendar-1.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+
+setup(
+    name='wagtail-calendar',
+    version='1.0',
+    description='A planning calendar for wagtail',
+    long_description=readme(),
+    url='https://github.com/Gagaro/wagtail-calendar',
+    author='Gagaro',
+    author_email='gagaro42@gmail.com',
+    license='BSD3',
+    packages=['wagtail_calendar'],
+    install_requires=[
+    ],
+    zip_safe=False
+)
