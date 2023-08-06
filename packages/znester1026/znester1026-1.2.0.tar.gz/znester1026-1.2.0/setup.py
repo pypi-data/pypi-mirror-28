874553f8f84c0c146f9f159c770ada44e8d3827e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:24:39 +0000

    Add package znester1026/1.2.0/znester1026-1.2.0.tar.gz

---
+++ packages/znester1026/znester1026-1.2.0.tar.gz/znester1026-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='znester1026',
+    version='1.2.0',
+    py_modules=['znester1026'],
+    author='phoenixx',
+    author_email='richard@python.org',
+    url='http://www.headfirstlabs.com',
+    description='A simple printer of nested lists',
+    )
