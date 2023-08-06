5752f6645e17274d557696771cd77effa686e320
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:11 +0000

    Add package stringutils/0.1.0/stringutils-0.1.0.tar.gz

---
+++ packages/stringutils/stringutils-0.1.0.tar.gz/stringutils-0.1.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+from stringutils import __version__
+
+setup(
+    name='stringutils',
+    description='A functional string utility library for Python',
+    version=__version__,
+    url='https://github.com/huntie/stringutils',
+    download_url='https://github.com/huntie/stringutils/tarball/{}.tar.gz'.format(__version__),
+    author='Alex Hunt',
+    author_email='hello@alexhunt.io',
+    license='MIT',
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Topic :: Text Processing',
+        'Topic :: Utilities',
+    ],
+
+    packages=['stringutils'],
+)
