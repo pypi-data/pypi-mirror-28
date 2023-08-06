fb58a8f5f29537e31c16e95d5f9006ff27cf7654
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:46 +0000

    Add package xiaomo/1.0.0/xiaomo-1.0.0.tar.gz

---
+++ packages/xiaomo/xiaomo-1.0.0.tar.gz/xiaomo-1.0.0/setup.py
@@ -0,0 +1,26 @@
+#! /usr/bin/python3
+
+"""
+ 把今天最好的表现当作明天最新的起点．．～
+ いま 最高の表現 として 明日最新の始発．．～
+ Today the best performance  as tomorrow newest starter!
+ Created by IntelliJ IDEA.
+ author: xiaomo
+ github: https://github.com/syoubaku
+ email: xiaomo@xiamoo.info
+ QQ_NO: 83387856
+ Date: 2018/2/6 20:37 
+ Description: 
+ Copyright(©) 2017 by xiaomo.
+"""
+from distutils.core import setup
+
+setup(
+    name='xiaomo',
+    version='1.0.0',
+    py_modules=['xiaomo'],
+    author='xiaomoinfo',
+    author_email='xiaomo@xiaomo.info',
+    url='https://github.com/xiaomoinfo/FrequentlyShell',
+    description='小莫的第一个python模块'
+)
