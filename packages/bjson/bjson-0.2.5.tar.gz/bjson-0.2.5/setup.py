bfabee9f273608e4e45bf61771b586bfd6a83ff3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:55 +0000

    Add package bjson/0.2.5/bjson-0.2.5.tar.gz

---
+++ packages/bjson/bjson-0.2.5.tar.gz/bjson-0.2.5/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='bjson',
+    version='0.2.5',
+    description='Alternative way to send Python3 object by binary.',
+    author='namuyan',
+    url='https://github.com/namuyan/bin-json',
+    py_modules=['bjson'],
+    license="MIT"
+)
