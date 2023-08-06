75755dbdfa132ffcd90b168d582c7b2b0e71893e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:04 +0000

    Add package gsm/2.1.1/gsm-2.1.1.tar.gz

---
+++ packages/gsm/gsm-2.1.1.tar.gz/gsm-2.1.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='gsm'
+     ,version='2.1.1'
+     ,description='The Global Sky Model (GSM)'
+     ,url='https://github.com/bartscheers/gsm'
+     ,author='Bart Scheers'
+     ,author_email='bartscheers@gmail.com'
+     ,packages=['gsm']
+     ,scripts=['bin/gsm']
+     ,install_requires=['numpy', 'six', 'pymonetdb']
+     ,include_package_data=True
+     ,zip_safe=False
+     )
+
