d055f4381c5f0a8d0f975b2dc100471e1ec480df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:49 +0000

    Add package RTool/0.0.1.7/RTool-0.0.1.7.tar.gz

---
+++ packages/RTool/RTool-0.0.1.7.tar.gz/RTool-0.0.1.7/setup.py
@@ -0,0 +1,14 @@
+tag = '0.0.1.7'
+from distutils.core import setup
+setup(
+  name = 'RTool',
+  packages = ['RTool'], # this must be the same as the name above
+  version = '%s'%tag,
+  description = 'A random test lib',
+  author = 'Ron Nofar',
+  author_email = 'ronnofar2@gmail.com',
+  url = 'https://github.com/RonNofar/TextOnScreen', # use the URL to the github repo
+  download_url = 'https://github.com/RonNofar/RToolPackage/archive/%s.tar.gz'%tag, # I'll explain this in a second
+  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  classifiers = [],
+)
