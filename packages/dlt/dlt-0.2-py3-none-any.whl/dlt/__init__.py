504f19a06850e0e53ee68c195e2d7ba5a8177c20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:09 +0000

    Add package dlt/0.2/dlt-0.2-py3-none-any.whl

---
+++ packages/dlt/dlt-0.2-py3-none-any.whl/dlt/__init__.py
@@ -0,0 +1,12 @@
+__all__ = [
+    "utils",
+    "cifar",
+    "mnist",
+    "fashon_mnist"
+    ]
+__version__ = "0.2"
+
+import dlt.utils
+import dlt.cifar
+import dlt.mnist
+import dlt.fashion_mnist
