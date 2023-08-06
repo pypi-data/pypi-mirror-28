639ddc96c14c958d7cbe4545a7b2eb9d81a60066
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:27 +0000

    Add package debinterface/3.2.1/debinterface-3.2.1.tar.gz

---
+++ packages/debinterface/debinterface-3.2.1.tar.gz/debinterface-3.2.1/deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+
+echo "Remember to update version in :"
+echo "    - debinterface/__init__.py"
+echo "    - setup.py"
+
+# Test
+py.test --cov=debinterface test
+
+# Cleanup
+rm -rf debinterface.egg-info
+rm -rf build
+rm -rf dist
+
+# check
+check-manifest -u -v
+
+# Build packages
+python setup.py sdist
+python setup.py bdist_wheel  # Not universal
+
+# Deploy with gpg
+twine upload dist/* -r pypitest --sign --skip-existing
+twine upload dist/* --sign
---
+++ packages/debinterface/debinterface-3.2.1.tar.gz/debinterface-3.2.1/docs/contributing.rst
@@ -0,0 +1,11 @@
+Contributing
+============
+
+Contributing is much welcome, feel free to open a PR or an issue so we can work together !
+
+
+.. toctree::
+   :maxdepth: 4
+
+   tests
+   gen_docs
