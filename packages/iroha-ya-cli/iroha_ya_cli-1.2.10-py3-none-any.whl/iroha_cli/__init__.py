bafdff0630de419f5bdabe73ceab665665cbeab8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:19:24 +0000

    Add package iroha-ya-cli/1.2.10/iroha_ya_cli-1.2.10-py3-none-any.whl

---
+++ packages/iroha-ya-cli/iroha_ya_cli-1.2.10-py3-none-any.whl/iroha_cli/__init__.py
@@ -0,0 +1,9 @@
+
+import sys, os
+sys.path.insert(0, os.path.abspath(__file__ + "/../../iroha_cli_schema"))
+
+from . import commands
+from . import completer
+from . import file_io
+from . import network
+from . import main
---
+++ packages/iroha-ya-cli/iroha_ya_cli-1.2.10-py3-none-any.whl/iroha_cli/crypto_ed25519.py
@@ -0,0 +1,31 @@
+# coding=utf-8
+import sha3
+import base64
+import time
+
+from collections import namedtuple
+from ed25519_python import ed25519
+
+from iroha_cli.crypto import KeyPair
+
+
+def generate_keypair_ed25519():
+    # Mind the private/public key order!
+    public_key, private_key = ed25519.generate()
+    return KeyPair(public_key, private_key)
+
+
+def sign_ed25519(key_pair, message):
+    return ed25519.sign(message, key_pair.public_key, key_pair.private_key)
+
+
+def verify_ed25519(public_key, signature, message):
+    return bool(ed25519.verify(message, signature, public_key))
+
+
+def ed25519_sha3_256(message):
+    return ed25519.sha3_256(message)
+
+
+def ed25519_sha3_512(message):
+    return ed25519.sha3_512(message)
