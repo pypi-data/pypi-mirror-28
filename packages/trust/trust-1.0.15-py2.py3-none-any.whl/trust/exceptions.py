7b6cb5352148a2c20e4a6a254eaa2141f50a843b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:52:31 +0000

    Add package trust/1.0.15/trust-1.0.15-py2.py3-none-any.whl

---
+++ packages/trust/trust-1.0.15-py2.py3-none-any.whl/trust/exceptions.py
@@ -0,0 +1,18 @@
+class NodeNotFoundException(Exception):
+    pass
+
+
+class AuthenticationException(Exception):
+    pass
+
+
+class FormatterException(Exception):
+    pass
+
+
+class InvalidQueryException(Exception):
+    pass
+
+
+class CircularInheritanceException(Exception):
+    pass
