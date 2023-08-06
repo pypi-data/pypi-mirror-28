f58b441308fa9cf1b56aa8ee6651ca055db3d5d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:27 +0000

    Add package uforgecli/3.8.0.1/uforgecli-3.8.0.1.tar.gz

---
+++ packages/uforgecli/uforgecli-3.8.0.1.tar.gz/uforgecli-3.8.0.1/uforgecli/utils/constants.py
@@ -0,0 +1,14 @@
+__author__="UShareSoft"
+
+
+VERSION='3.8.0.1'
+
+HTTP_TIMEOUT=30
+
+
+QUOTAS_SCAN="scan"
+QUOTAS_TEMPLATE="appliance"
+QUOTAS_GENERATION="generation"
+QUOTAS_DISK_USAGE="diskusage"
+
+DEFAULT_URL="http://localhost:8080/ufws"
