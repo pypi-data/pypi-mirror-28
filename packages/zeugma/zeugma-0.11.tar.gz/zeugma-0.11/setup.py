c5583d2131c2f8d4be7290bd26fb6a66476e28bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:19:23 +0000

    Add package zeugma/0.11/zeugma-0.11.tar.gz

---
+++ packages/zeugma/zeugma-0.11.tar.gz/zeugma-0.11/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='zeugma',
+      version='0.11',
+      description='Unified framework for word embeddings (Word2Vec, GloVe, FastText, ...) compatible with scikit-learn Pipeline',
+      author='Nicolas Thiebaut',
+      author_email='nkthiebaut@gmail.com',
+      url='https://github.com/nkthiebaut',
+      download_url='https://github.com/nkthiebaut/zeugma/archive/0.11.tar.gz',
+      install_requires=[
+          'numpy>=0.1',
+	  'pandas',
+          'sklearn',
+      ]
+     )
+
