82a360fc4f7a04ae15764d915f2acc21570402c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:03 +0000

    Add package txHTTP/0.0.4a1/txHTTP-0.0.4a1-py3-none-any.whl

---
+++ packages/txHTTP/txHTTP-0.0.4a1-py3-none-any.whl/txhttp/__init__.py
@@ -0,0 +1,6 @@
+from txhttp.application import Application
+from txhttp.handlers.generic.base import Handler
+
+__version__ = '0.0.4-a1'
+
+__all__ = ['Application', 'Handler']
---
+++ packages/txHTTP/txHTTP-0.0.4a1-py3-none-any.whl/txhttp/handlers/__init__.py
@@ -0,0 +1,3 @@
+from txhttp.handlers.generic.base import Handler
+
+__all__ = ['Handler']
