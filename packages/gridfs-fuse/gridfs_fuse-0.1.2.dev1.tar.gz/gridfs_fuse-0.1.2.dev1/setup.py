d7847b2eb8718d90a26f0d792f5e2b1f982b6338
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:10 +0000

    Add package gridfs-fuse/0.1.2.dev1/gridfs_fuse-0.1.2.dev1.tar.gz

---
+++ packages/gridfs-fuse/gridfs_fuse-0.1.2.dev1.tar.gz/gridfs_fuse-0.1.2.dev1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+from setuptools import find_packages
+import os
+import sys
+
+here = os.path.abspath(os.path.dirname(__file__)) + '/'
+
+mount_script = 'mount.gridfs' if sys.platform != 'darwin' else 'mount_gridfs'
+readme_lines = open(here + 'README.md').readlines()
+
+
+setup(
+    name="gridfs_fuse",
+    url='https://github.com/axiros/py_gridfs_fuse',
+    description=readme_lines[1].strip('\n'),
+    long_description=''.join(readme_lines),
+    license=open(here + 'LICENSE.md').readlines()[0].strip('\n'),
+    version=open(here + 'VERSION').read().strip('\n') or '0.1.1',
+    install_requires=open(here + 'requirements.txt').readlines(),
+    include_package_data=True,
+    package_dir={'gridfs_fuse': 'gridfs_fuse'},
+    packages=find_packages('.'),
+    entry_points={
+        'console_scripts': [
+            'gridfs_fuse = gridfs_fuse.main:main',
+            '%s = gridfs_fuse.main:_mount_fuse_main' %(mount_script),
+        ]
+    }
+)
