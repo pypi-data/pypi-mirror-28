6dc5cbdf2e634debf421e0d986466f912a990d46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:59 +0000

    Add package ACNLogger/0.2.7/ACNLogger-0.2.7.tar.gz

---
+++ packages/ACNLogger/ACNLogger-0.2.7.tar.gz/ACNLogger-0.2.7/setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+
+setup(
+    name='ACNLogger',    # This is the name of your PyPI-package.
+    version='0.2.7'           # The name of your scipt, and also the command you'll be using for calling it
+)
