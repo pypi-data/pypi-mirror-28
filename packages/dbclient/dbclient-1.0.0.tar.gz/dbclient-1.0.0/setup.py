a346ffeb38c07053c009d1b5859e466959924f07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:33 +0000

    Add package dbclient/1.0.0/dbclient-1.0.0.tar.gz

---
+++ packages/dbclient/dbclient-1.0.0.tar.gz/dbclient-1.0.0/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name='dbclient',
+    version='1.0.0',
+    description='A db client python project',
+    url='https://github.com/jgshzy/db-executor-py',
+    author='yuan.zhu',
+    author_email='249466853@qq.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    keywords='db client python',
+    #packages=['db'],
+)
