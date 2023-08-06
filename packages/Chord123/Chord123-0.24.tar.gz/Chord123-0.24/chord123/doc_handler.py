bea21599d696b40211bcf78bf5e55d29131760b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:03:52 +0000

    Add package Chord123/0.24/Chord123-0.24.tar.gz

---
+++ packages/Chord123/Chord123-0.24.tar.gz/Chord123-0.24/chord123/doc_handler.py
@@ -0,0 +1,10 @@
+import re
+from docx import Document
+
+class DocumentHandler:
+    def __init__(self, path):
+        with open(path, 'rb') as f:
+            self.document = Document(f)
+
+    def save(self, filename):
+        self.document.save(filename)
