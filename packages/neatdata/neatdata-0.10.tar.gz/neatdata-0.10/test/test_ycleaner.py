a642163d93796e37591263edacdde90ab0d47ca5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:07 +0000

    Add package neatdata/0.10/neatdata-0.10.tar.gz

---
+++ packages/neatdata/neatdata-0.10.tar.gz/neatdata-0.10/test/test_ycleaner.py
@@ -0,0 +1,9 @@
+import unittest
+import pandas as pd
+import numpy as np
+from neatdata.neatdata import *
+
+class TestYCleaner(unittest.TestCase):
+
+    def testYCleaner_EmptyTest(self):
+        pass
