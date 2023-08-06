f89641b4cb9a3f081e014947caac50564473cb0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:23 +0000

    Add package ptrie/1.1/ptrie-1.1-py35-none-any.whl

---
+++ packages/ptrie/ptrie-1.1-py35-none-any.whl/ptrie/__init__.py
@@ -0,0 +1,8 @@
+# __init__.py
+# Copyright (c) 2013-2018 Pablo Acosta-Serafini
+# See LICENSE for details
+# pylint: disable=C0111,W0401
+
+# Intra-package imports
+from .version import __version__
+from .ptrie import *
