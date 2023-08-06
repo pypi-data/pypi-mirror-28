548072bdac947fff67ce078736adc8ed639609cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:34 +0000

    Add package ansible-modules-hashivault/3.8.3/ansible-modules-hashivault-3.8.3.tar.gz

---
+++ packages/ansible-modules-hashivault/ansible-modules-hashivault-3.8.3.tar.gz/ansible-modules-hashivault-3.8.3/ansible/modules/hashivault/hashivault_write.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python
+DOCUMENTATION = '''
+---
+module: hashivault_write
+version_added: "0.1"
+short_description: Hashicorp Vault write module
+description:
+    - Module to write to Hashicorp Vault.
+options:
+    url:
+        description:
+            - url for vault
+        default: to environment variable VAULT_ADDR
+    ca_cert:
+        description:
+            - "path to a PEM-encoded CA cert file to use to verify the Vault server TLS certificate"
+        default: to environment variable VAULT_CACERT
+    ca_path:
+        description:
+            - "path to a directory of PEM-encoded CA cert files to verify the Vault server TLS certificate : if ca_cert is specified, its value will take precedence"
+        default: to environment variable VAULT_CAPATH
+    client_cert:
+        description:
+            - "path to a PEM-encoded client certificate for TLS authentication to the Vault server"
+        default: to environment variable VAULT_CLIENT_CERT
+    client_key:
+        description:
+            - "path to an unencrypted PEM-encoded private key matching the client certificate"
+        default: to environment variable VAULT_CLIENT_KEY
+    verify:
+        description:
+            - "if set, do not verify presented TLS certificate before communicating with Vault server : setting this variable is not recommended except during testing"
+        default: to environment variable VAULT_SKIP_VERIFY
+    authtype:
+        description:
+            - "authentication type to use: token, userpass, github, ldap"
+        default: token
+    token:
+        description:
+            - token for vault
+        default: to environment variable VAULT_TOKEN
+    username:
+        description:
+            - username to login to vault.
+        default: to environment variable VAULT_USER
+    password:
+        description:
+            - password to login to vault.
+        default: to environment variable VAULT_PASSWORD
+    secret:
+        description:
+            - secret to read.
+    data:
+        description:
+            - Keys and values to write.
+    update:
+        description:
+            - Update rather than overwrite.
+        default: False
+'''
+EXAMPLES = '''
+---
+- hosts: localhost
+  tasks:
+    - hashivault_write:
+        secret: giant
+        data:
+            foo: foe
+            fie: fum
+'''
+
+
+def main():
+    argspec = hashivault_argspec()
+    argspec['secret'] = dict(required=True, type='str')
+    argspec['update'] = dict(required=False, default=False, type='bool')
+    argspec['data'] = dict(required=False, default={}, type='dict')
+    module = hashivault_init(argspec)
+    result = hashivault_write(module.params)
+    if result.get('failed'):
+        module.fail_json(**result)
+    else:
+        module.exit_json(**result)
+
+
+from ansible.module_utils.hashivault import *
+
+
+@hashiwrapper
+def hashivault_write(params):
+    result = {"changed": False, "rc": 0}
+    client = hashivault_auth_client(params)
+    secret = params.get('secret')
+    if secret.startswith('/'):
+        secret = secret.lstrip('/')
+    else:
+        secret = ('secret/%s' % secret)
+    data = params.get('data')
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        changed = True
+        write_data = data
+        if params.get('update'):
+            read_data = client.read(secret)
+            if read_data is None:
+                read_data = {}
+            if read_data and 'data' in read_data:
+                read_data = read_data['data']
+            write_data = dict(read_data)
+            result['write_data'] = write_data
+            result['read_data'] = read_data
+            write_data.update(data)
+            changed = write_data != read_data
+        if changed:
+            returned_data = client.write((secret), **write_data)
+            if returned_data:
+                result['data'] = returned_data
+            result['msg'] = "Secret %s written" % secret
+        result['changed'] = changed
+    return result
+
+
+if __name__ == '__main__':
+    main()
