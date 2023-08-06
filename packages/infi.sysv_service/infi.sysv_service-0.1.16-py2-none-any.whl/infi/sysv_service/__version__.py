e5144caaae44dc6f906aca7ddc4433a1a82a3b75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:09 +0000

    Add package infi.sysv_service/0.1.16/infi.sysv_service-0.1.16-py2-none-any.whl

---
+++ packages/infi.sysv_service/infi.sysv_service-0.1.16-py2-none-any.whl/infi/sysv_service/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.1.16"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = u'(HEAD detached at v0.1.16)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '7d361950e2e69288d75d3b17a518b62ed9b25f77'
+__git_head_subject__ = u'Finished Release v0.1.16'
+__git_head_message__ = u''
+__git_dirty_diff__ = u''
