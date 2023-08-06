14a0b6efb91ccef36a2a22dedbccb96e2196fb8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:58 +0000

    Add package pyrxnlp/0.1.4.8/pyrxnlp-0.1.4.8.tar.gz

---
+++ packages/pyrxnlp/pyrxnlp-0.1.4.8.tar.gz/pyrxnlp-0.1.4.8/README.rst
@@ -0,0 +1,10 @@
+==============================================
+PyRXNLP - Text-mining tools for building intelligent data-driven applications.
+==============================================
+
+
+Features:
+--------
+    - Topics extraction
+    - Text clustering
+    - Opinosis opinion summarization
---
+++ packages/pyrxnlp/pyrxnlp-0.1.4.8.tar.gz/pyrxnlp-0.1.4.8/setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup, find_packages
+from subprocess import Popen, PIPE
+import io,os
+
+def tag():
+    process = Popen (["git", "describe", "--abbrev=0", "--tags"], stdout=PIPE)
+    tag=process.communicate()[0].strip()
+    tag=bytes.decode(tag)
+    return tag
+
+
+def read_text_lines(fname):
+    with io.open(fname) as fd:
+        lines=fd.readlines()
+        return '\n'.join(lines)
+
+
+
+setup(
+    name="pyrxnlp",
+    version="0.1.4.8",
+    packages=find_packages(),
+    description='Natural language processing tools',
+    long_description=read_text_lines("README.rst"),
+    classifiers=[
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Text Processing :: Linguistic'
+    ],
+    author='RxNLP',
+    author_email='kavita.ganesan@rxnlp.com',
+    license='LICENSE',
+    url='https://github.com/RxNLP/pyrxnlp',
+    download_url='https://github.com/RxNLP/pyrxnlp/archive/{0}.tar.gz'.format(tag()),
+    keywords=['Sentence Clustering', 'Topics Extraction',
+              'Opinosis Summarization', 'Text Similarity'],
+    install_requires=[
+        'requests==2.9.1'
+    ],
+    include_package_data=True,
+    entry_points={
+
+    }
+)
