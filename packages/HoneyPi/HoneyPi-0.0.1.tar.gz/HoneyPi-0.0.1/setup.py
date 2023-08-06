05f1e242cb0ba5355a5c00b2d67903ef1a596b03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:06 +0000

    Add package HoneyPi/0.0.1/HoneyPi-0.0.1.tar.gz

---
+++ packages/HoneyPi/HoneyPi-0.0.1.tar.gz/HoneyPi-0.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name = "HoneyPi",
+    version = "0.0.1",
+    author = "Mats Bauer",
+    author_email = "contact@airlibi.com",
+    description = ("HoneyPi is a package for Python that let's you automatically notify your honey when your working later - as you only need to fix this one last siiiiimple bug!"),
+    license = "MIT",
+    keywords = "python automation honey notice",
+    url = "https://github.com/matsbauer/HoneyPi",
+    packages=['HoneyPi'],
+    #long_description=open('readme-pypi.rst').read(),
+    #install_requires=['bs4', 'beautifulsoup4']
+)
