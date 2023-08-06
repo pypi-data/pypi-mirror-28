24af8002d4ffe75f78dc63a1971b3c26ba48ada2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:32 +0000

    Add package startpoint/1.2/startpoint-1.2.tar.gz

---
+++ packages/startpoint/startpoint-1.2.tar.gz/startpoint-1.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="startpoint",
+    version="1.2",
+    packages=["startpoint"],
+    install_requires=['docutils>=0.3'],
+    package_data={
+        '': ['*.md'],
+    },
+    author="Pavel Braginskiy",
+    author_email="pavelbraginskiy@hotmail.com",
+    description="A better entry point than if __name__ == '__main__'",
+    license="MIT",
+    url="https://github.com/pavelbraginskiy/ismain/tree/master"
+   
+)
