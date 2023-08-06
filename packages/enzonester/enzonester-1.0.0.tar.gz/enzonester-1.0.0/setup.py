558fba85ad75ba38f16e2231d5b4fe54647cf0ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:18 +0000

    Add package enzonester/1.0.0/enzonester-1.0.0.tar.gz

---
+++ packages/enzonester/enzonester-1.0.0.tar.gz/enzonester-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+
+	name = 'enzonester',
+	version = '1.0.0',
+	py_modules = ['enzonester'],
+	author = 'Enzotech',
+	author_email = 'dante0215@163.com',
+	url = 'http://www.baidu.com',
+	description = 'A simple printer of nester lists',
+	)
