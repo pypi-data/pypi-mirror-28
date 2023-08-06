2fc420e2802e578cfc4a4176871c454bad5c6a33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:05 +0000

    Add package jasonpi/0.1/jasonpi-0.1.tar.gz

---
+++ packages/jasonpi/jasonpi-0.1.tar.gz/jasonpi-0.1/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(name='jasonpi',
+      version='0.1',
+      description='Authentication package to use with '
+                  'django-rest-framework & json-api',
+      url='http://github.com/gobadiah/jasonpi',
+      author='Michaël Journo',
+      author_email='gobadiah@gmail.com',
+      license='MIT',
+      packages=['jasonpi'],
+      zip_safe=True)
