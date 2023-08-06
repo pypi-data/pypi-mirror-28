8a3b26eb47e749fa2cec44d1089a53ad25e80482
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:04 +0000

    Add package grokcore.message/3.0.1/grokcore.message-3.0.1.tar.gz

---
+++ packages/grokcore.message/grokcore.message-3.0.1.tar.gz/grokcore.message-3.0.1/setup.py
@@ -0,0 +1,63 @@
+import os
+from setuptools import setup, find_packages
+
+version = '3.0.1'
+
+readme = open(os.path.join('src', 'grokcore', 'message', 'README.txt')).read()
+changes = open("CHANGES.txt").read()
+
+long_description = "%s\n\n%s\n" % (readme, changes)
+
+install_requires = [
+    'setuptools',
+    'grokcore.component >= 2.5dev',
+    'z3c.flashmessage',
+    'zope.component',
+    'zope.traversing', # zope.session seems not to declare this dep.?
+    ]
+
+tests_require = [
+    'zope.publisher',
+    'zope.security',
+    'zope.session',
+    ]
+
+setup(name='grokcore.message',
+      version=version,
+      description="Grok messaging machinery",
+      long_description=long_description,
+      keywords='Grok Messages',
+      author='Grok Team',
+      author_email='grok-dev@zope.org',
+      url='http://pypi.python.org/pypi/grokcore.message',
+      license='ZPL 2.1',
+      namespace_packages=['grokcore'],
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=install_requires,
+      tests_require=tests_require,
+      extras_require=dict(test=tests_require),
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Web Environment',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Zope Public License',
+          'Programming Language :: Python',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Topic :: Internet :: WWW/HTTP',
+          'Framework :: Zope3',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: Implementation :: PyPy',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
---
+++ packages/grokcore.message/grokcore.message-3.0.1.tar.gz/grokcore.message-3.0.1/src/grokcore/message/tests.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""Test harness"""
+
+import doctest
+import unittest
+import grokcore.message
+
+from zope.component.testlayer import ZCMLFileLayer
+from zope.publisher.browser import TestRequest
+from zope.security.management import newInteraction, endInteraction
+
+
+class GrokcoreMessageLayer(ZCMLFileLayer):
+
+    def setUp(self):
+        ZCMLFileLayer.setUp(self)
+        newInteraction(TestRequest())
+
+    def tearDown(self):
+        endInteraction()
+        ZCMLFileLayer.tearDown(self)
+
+
+def test_suite():
+    suite = unittest.TestSuite()
+    test = doctest.DocFileSuite(
+        'README.txt', optionflags=(doctest.ELLIPSIS |
+                                   doctest.IGNORE_EXCEPTION_DETAIL))
+    test.layer = GrokcoreMessageLayer(grokcore.message)
+    suite.addTest(test)
+    return suite
