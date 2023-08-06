61850a2875a8040c1aedd4477b9f0e2b3221d95e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:35 +0000

    Add package cloudio-endpoint-python/0.1.1/cloudio-endpoint-python-0.1.1.tar.gz

---
+++ packages/cloudio-endpoint-python/cloudio-endpoint-python-0.1.1.tar.gz/cloudio-endpoint-python-0.1.1/src/cloudio/cloudio_attribute_type.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+import types
+
+class CloudioAttributeType():
+    """Identifies the different data types of attributes currently supported by cloud.io.
+    """
+    Invalid = 0     # Invalid data type
+    Boolean = 1     # The attribute's value is of type boolean
+    Integer = 2     # The attribute's value is of type short, int or long
+    Number  = 3     # The attribute's value is of type float or double
+    String  = 4     # The attribute's value is of type String
+
+    @classmethod
+    def fromRawType(cls, rawType):
+        """Converts a standard type to its cloud.iO type representation.
+        :return The corresponding cloud.iO type.
+        :type CloudioAttributeType
+        """
+        if isinstance(rawType, bool) or rawType == types.BooleanType:
+            return cls.Boolean
+        elif isinstance(rawType, types.IntType) or rawType == types.IntType:
+            return cls.Integer
+        elif isinstance(rawType, float) or rawType == types.FloatType:
+            return cls.Number
+        elif isinstance(rawType, str) or rawType == types.StringType:
+            return cls.String
+        else:
+            return cls.Invalid
+
+    @classmethod
+    def fromRawTypeToString(cls, rawType):
+        """Converts a standard type to a string.
+        :return The type represented as a string.
+        :type str
+        """
+        if isinstance(rawType, bool) or rawType == types.BooleanType:
+            return 'Boolean'
+        elif isinstance(rawType, types.IntType) or rawType == types.IntType:
+            return 'Integer'
+        elif isinstance(rawType, float) or rawType == types.FloatType:
+            return 'Number'
+        elif isinstance(rawType, str) or rawType == types.StringType:
+            return 'String'
+        else:
+            return 'Invalid'
