d2a954e4918624c700958393a5bc3aab36fd06d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:40 +0000

    Add package gpxconverter/0.9.4/gpxconverter-0.9.4-py3-none-any.whl

---
+++ packages/gpxconverter/gpxconverter-0.9.4-py3-none-any.whl/gpxconverter/__main__.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import sys
+import argparse
+from .converter import convert
+
+
+def main():
+    args = sys.argv[1:]
+    parser = argparse.ArgumentParser(prog='gpxconvert')
+    parser.add_argument(
+        '-i',
+        nargs=1,
+        metavar='file path',
+        help='input file',
+    )
+    parser.add_argument(
+        '-o',
+        required=False,
+        metavar='output file name',
+        help='out file name without csv extension',
+    )
+    args = parser.parse_args(args)
+    try:
+        convert(**vars(args))
+    except Exception as exc:
+        print(exc)
+
+
+if __name__ == "__main__":
+    sys.exit(main())
