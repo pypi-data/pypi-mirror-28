8a727534a06718ecbcea7f2a5ed7d0055aa38a63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:24 +0000

    Add package ADTLib/2.1.2/ADTLib-2.1.2.tar.gz

---
+++ packages/ADTLib/ADTLib-2.1.2.tar.gz/ADTLib-2.1.2/setup.py
@@ -0,0 +1,30 @@
+from distutils.core import setup
+from setuptools import find_packages
+import glob
+
+package_data = ['files/*'] 
+scripts = glob.glob('bin/*')               
+
+setup(
+  name = 'ADTLib',
+  packages=find_packages(exclude=[]), 
+  version = '2.1.2',
+  description = 'Automated Drum Trancription Library',
+  author = 'Carl Southall',
+  author_email = 'carl.southall@bcu.ac.uk',
+  license='BSD',
+  url = 'https://github.com/CarlSouthall/ADTLib', 
+  download_url = 'https://github.com/CarlSouthall/ADTLib', 
+  keywords = ['Drums', 'Transcription', 'Automated'],
+  scripts=scripts,
+  classifiers = [
+                 'Programming Language :: Python :: 2.7',
+                 'License :: OSI Approved :: BSD License',
+                 'License :: Free for non-commercial use',
+                 'Topic :: Multimedia :: Sound/Audio :: Analysis',
+                 'Topic :: Scientific/Engineering :: Artificial Intelligence'],
+   
+    install_requires=['numpy','scipy','cython','madmom','fpdf'],
+    package_data={'ADTLib': package_data},
+     
+)
