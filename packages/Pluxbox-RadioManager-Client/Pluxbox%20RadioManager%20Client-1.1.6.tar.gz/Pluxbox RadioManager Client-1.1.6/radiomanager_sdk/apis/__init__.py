9a2811896bd0eb1ed110b9b1b317f24a9e3afb08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:05 +0000

    Add package Pluxbox-RadioManager-Client/1.1.6/Pluxbox%20RadioManager%20Client-1.1.6.tar.gz

---
+++ packages/Pluxbox-RadioManager-Client/Pluxbox%20RadioManager%20Client-1.1.6.tar.gz/Pluxbox RadioManager Client-1.1.6/radiomanager_sdk/apis/__init__.py
@@ -0,0 +1,18 @@
+from __future__ import absolute_import
+
+# import apis into api package
+from .block_api import BlockApi
+from .broadcast_api import BroadcastApi
+from .campaign_api import CampaignApi
+from .contact_api import ContactApi
+from .genre_api import GenreApi
+from .item_api import ItemApi
+from .model_type_api import ModelTypeApi
+from .presenter_api import PresenterApi
+from .program_api import ProgramApi
+from .station_api import StationApi
+from .story_api import StoryApi
+from .string_api import StringApi
+from .tag_api import TagApi
+from .user_api import UserApi
+from .visual_slide_api import VisualSlideApi
