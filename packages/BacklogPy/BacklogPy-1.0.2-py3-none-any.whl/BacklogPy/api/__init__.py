ed30acbef6893232db21969e1636769e105a968e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:02 +0000

    Add package BacklogPy/1.0.2/BacklogPy-1.0.2-py3-none-any.whl

---
+++ packages/BacklogPy/BacklogPy-1.0.2-py3-none-any.whl/BacklogPy/api/__init__.py
@@ -0,0 +1,33 @@
+# coding: utf-8
+
+"""
+    This file was created by Backlog APIGenerator
+"""
+
+
+from BacklogPy.api.stars import Stars
+from BacklogPy.api.projects import Projects
+from BacklogPy.api.issues import Issues
+from BacklogPy.api.space import Space
+from BacklogPy.api.wikis import Wikis
+from BacklogPy.api.notifications import Notifications
+from BacklogPy.api.users import Users
+from BacklogPy.api.groups import Groups
+from BacklogPy.api.statuses import Statuses
+from BacklogPy.api.watchings import Watchings
+from BacklogPy.api.priorities import Priorities
+from BacklogPy.api.resolutions import Resolutions
+
+__all__ = [
+    'Stars',
+    'Projects',
+    'Issues',
+    'Space',
+    'Wikis',
+    'Notifications',
+    'Users',
+    'Groups',
+    'Statuses',
+    'Watchings',
+    'Priorities',
+    'Resolutions']
---
+++ packages/BacklogPy/BacklogPy-1.0.2-py3-none-any.whl/BacklogPy/backlog.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+
+"""
+    This file was created by Backlog APIGenerator
+"""
+
+
+from __future__ import unicode_literals, absolute_import
+
+from BacklogPy.api import *
+
+
+class Backlog(Stars, Projects, Issues, Space, Wikis, Notifications,
+              Users, Groups, Statuses, Watchings, Priorities, Resolutions):
+    pass
