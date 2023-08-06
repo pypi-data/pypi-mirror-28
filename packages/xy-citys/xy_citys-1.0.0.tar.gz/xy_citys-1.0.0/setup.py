44dc7d814ecb4f86622c76e09f9884b1d47b1a15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:19:29 +0000

    Add package xy-citys/1.0.0/xy_citys-1.0.0.tar.gz

---
+++ packages/xy-citys/xy_citys-1.0.0.tar.gz/xy_citys-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+  name = 'xy_citys',
+  packages = ['xy_citys'],
+  version = '1.0.0',
+  description = 'Get ALL Citys',
+  long_description='Get ALL Citys',
+  author = 'KingHS',
+  author_email = '382771946@qq.com',
+  keywords = ['pip3', 'python3','python','citys'],
+  license='MIT',
+)
