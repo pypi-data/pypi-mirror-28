45af9525e895da928325be21f878b9aebfc95917
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:00 +0000

    Add package pyChemometrics/0.1/pyChemometrics-0.1.tar.gz

---
+++ packages/pyChemometrics/pyChemometrics-0.1.tar.gz/pyChemometrics-0.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='pyChemometrics',
+    version='0.1',
+    packages=['pyChemometrics'],
+    url='https://github.com/Gscorreia89/pyChemometrics/',
+    license='BSD 3-Clause License',
+    author='Gonçalo Correia',
+    author_email='gscorreia89@gmail.com',
+    description='The pyChemometrics provides objects which wrap pre-existing '
+                'scikit-learn PCA and PLS algorithms and adds model assessment metrics and functions '
+                'common in the Chemometrics literature.'
+)
