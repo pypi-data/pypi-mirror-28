7e49583ff20efc6a4cb64ca26f2c7b42805da179
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:17 +0000

    Add package py3-lru-cache/0.1.5/py3_lru_cache-0.1.5.tar.gz

---
+++ packages/py3-lru-cache/py3_lru_cache-0.1.5.tar.gz/py3_lru_cache-0.1.5/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from setuptools import find_packages
+
+
+setup(name='py3_lru_cache',
+      version='0.1.5',
+      description="""LRU cache for python. Provides a dictionary-like object as well as a method decorator. A fork of Chris Stucchio's py_lru_cache""",
+      author='Alan Mock',
+      author_email='pypi.org@alanmock.com',
+      license='Dual: GPL v3 or BSD',
+      url='https://github.com/amock/Python-LRU-cache',
+      packages = ['lru'],
+     )
