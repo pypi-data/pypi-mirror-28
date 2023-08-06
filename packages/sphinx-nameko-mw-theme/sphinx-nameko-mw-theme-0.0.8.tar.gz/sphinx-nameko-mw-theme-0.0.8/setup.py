1e91216667f247622e043cf220ff4c7b9b7b51fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:43 +0000

    Add package sphinx-nameko-mw-theme/0.0.8/sphinx-nameko-mw-theme-0.0.8.tar.gz

---
+++ packages/sphinx-nameko-mw-theme/sphinx-nameko-mw-theme-0.0.8.tar.gz/sphinx-nameko-mw-theme-0.0.8/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+setup(
+    name='sphinx-nameko-mw-theme',
+    version='0.0.8',
+    author='penseleit',
+    author_email='penseleit@mediaware.com.au',
+    description='Sphinx Nameko MW Theme',
+    url='https://github.com/penseleit/sphinx-nameko-mw-theme',
+    license='MIT',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet',
+        'Topic :: Software Development :: Documentation',
+    ],
+    packages=['sphinx_nameko_mw_theme'],
+    install_requires=['sphinx'],
+    entry_points = {
+        'sphinx_themes': [
+            'path = sphinx_nameko_mw_theme:get_html_theme_path',
+        ]
+    },
+    include_package_data=True,
+    zip_safe=False
+)
