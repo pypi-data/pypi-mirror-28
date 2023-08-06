329799470ceddd505b39467967700b7339f2113b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:35 +0000

    Add package django-database-files-ny/0.2.0/django-database-files-ny-0.2.0.tar.gz

---
+++ packages/django-database-files-ny/django-database-files-ny-0.2.0.tar.gz/django-database-files-ny-0.2.0/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+
+setup(
+    name='django-database-files-ny',
+    version='0.2.0',
+    description='A storage system for Django that stores uploaded files in the database.',
+    author='Sam Spencer',
+    author_email='sam@aristotlemetadata.com',
+    url='http://github.com/legostormtroopr/django-database-files/',
+    packages=[
+        'database_files',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+    install_requires=[
+    ]
+)
