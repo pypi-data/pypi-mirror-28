345e6077b99ea703279e063049f06149025a3027
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:28 +0000

    Add package dictlib/1.0.6/dictlib-1.0.6.tar.gz

---
+++ packages/dictlib/dictlib-1.0.6.tar.gz/dictlib-1.0.6/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name = 'dictlib',
+  packages = ['dictlib'],
+  version = '1.0.6',
+  description = 'Yet another Dictionary Library including good deep merge and dictionary as objects',
+  author = 'Brandon Gillespie',
+  author_email = 'bjg-pypi@cold.org',
+  url = 'https://github.com/srevenant/dictlib', 
+  download_url = 'https://github.com/srevenant/dictlib/tarball/1.0',
+  keywords = ['dict', 'union', 'object'],
+  classifiers = [],
+)
+
