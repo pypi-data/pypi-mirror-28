93844c980a485d42bf6a18afd3ed71705f023ae5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:30 +0000

    Add package flask-expects-json/1.0.1/flask-expects-json-1.0.1.tar.gz

---
+++ packages/flask-expects-json/flask-expects-json-1.0.1.tar.gz/flask-expects-json-1.0.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='flask-expects-json',
+    version='1.0.1',
+    description='Decorator for REST endpoints in flask. Validate JSON request data.',
+    url='https://github.com/fischerfredl/flask-expects-json',
+    author='Alfred Melch',
+    author_email='alfred.melch@gmx.de',
+    license='MIT',
+    classifiers=[
+        'Framework :: Flask',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7'
+
+    ],
+    keywords=['flask', 'json', 'validation', 'schema'],
+    packages=find_packages(exclude=['tests.*', 'tests']),
+    install_requires=[
+        'flask>=0.12.2',
+        'jsonschema>=2.6.0'
+    ],
+
+    test_suite='tests.test_suite',
+    tests_require=[],
+)
