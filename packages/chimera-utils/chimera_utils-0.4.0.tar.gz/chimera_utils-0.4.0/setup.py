3427a11fb039bed5db8c8750617c8fd6322b37b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:58 +0000

    Add package chimera-utils/0.4.0/chimera_utils-0.4.0.tar.gz

---
+++ packages/chimera-utils/chimera_utils-0.4.0.tar.gz/chimera_utils-0.4.0/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name = 'chimera_utils',
+    version = '0.4.0',
+    description='Python tools for processing chimeric reads and lists of gene fusions.',
+    url = 'https://github.com/friend1ws/chimera_utils',
+    author = 'Yuichi Shiraishi',
+    author_email = 'friend1ws@gamil.com',
+    license = 'GPLv3',
+
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: Unix',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Scientific/Engineering :: Bio-Informatics'
+    ],
+
+    packages = find_packages(exclude = ['tests']),
+
+    install_requires = [],
+    entry_points = {'console_scripts': ['chimera_utils = chimera_utils:main']}
+
+)
+
