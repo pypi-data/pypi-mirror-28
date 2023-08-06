5e0d79b8ab22a2e2235c6f5e5b7fdbab4df48bbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:07 +0000

    Add package odoo9-addon-medical-medicament-us/9.0.1.0.0.99.dev10/odoo9_addon_medical_medicament_us-9.0.1.0.0.99.dev10-py2-none-any.whl

---
+++ packages/odoo9-addon-medical-medicament-us/odoo9_addon_medical_medicament_us-9.0.1.0.0.99.dev10-py2-none-any.whl/odoo_addons/medical_medicament_us/models/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 LasLabs Inc.
+# License GPL-3.0 or later (http://www.gnu.org/licenses/gpl.html).
+
+from . import medical_medicament
+from . import medical_medicament_ndc
+from . import medical_medicament_gcn
