e92a668e251c326c60aeaee247f55c56e1acae34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:35 +0000

    Add package archetypes.schemaextender/2.1.8/archetypes.schemaextender-2.1.8.tar.gz

---
+++ packages/archetypes.schemaextender/archetypes.schemaextender-2.1.8.tar.gz/archetypes.schemaextender-2.1.8/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+
+version = '2.1.8'
+
+setup(
+    name='archetypes.schemaextender',
+    version=version,
+    description="Dynamically extend Archetypes schemas with named adapters.",
+    long_description=(open("README.rst").read() + '\n' +
+                      open("CHANGES.rst").read()),
+    classifiers=[
+        "Framework :: Plone",
+        "Framework :: Plone :: 4.3",
+        "Framework :: Plone :: 5.0",
+        "Framework :: Plone :: 5.1",
+        "Framework :: Zope2",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+    ],
+    keywords='Archetypes Schema extend',
+    author='Florian Schulze',
+    author_email='plone-developers@lists.sourceforge.net',
+    url='https://pypi.python.org/pypi/archetypes.schemaextender',
+    license='GPL',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['archetypes'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'six',
+        'plone.uuid'
+    ],
+    entry_points="""
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+)
