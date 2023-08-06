4495b24a6ce0d2a09fcc08b7b16bf131926bc380
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:22 +0000

    Add package sirbot/0.0.2/sirbot-0.0.2-py3-none-any.whl

---
+++ packages/sirbot/sirbot-0.0.2-py3-none-any.whl/sirbot/bot.py
@@ -0,0 +1,36 @@
+import asyncio
+import logging
+import aiohttp.web
+
+from . import endpoints
+
+LOG = logging.getLogger(__name__)
+
+
+class SirBot(aiohttp.web.Application):
+    def __init__(self, user_agent=None, **kwargs):
+        super().__init__(**kwargs)
+
+        self.http_session = aiohttp.ClientSession(loop=kwargs.get('loop') or asyncio.get_event_loop())
+        self.user_agent = user_agent or 'sir-bot-a-lot'
+        self.router.add_route('GET', '/sirbot/plugins', endpoints.plugins)
+
+        self['plugins'] = dict()
+
+    def start(self, **kwargs):
+        LOG.info('Starting SirBot')
+        aiohttp.web.run_app(self, **kwargs)
+
+    def load_plugin(self, plugin):
+        self['plugins'][plugin.__name__] = plugin
+        plugin.load(self)
+
+    async def shutdown(self):
+        LOG.info('Stopping SirBot')
+        self.http_session.close()
+        await super().shutdown()
+        LOG.info('SirBot stopped')
+
+    @property
+    def plugins(self):
+        return self['plugins']
