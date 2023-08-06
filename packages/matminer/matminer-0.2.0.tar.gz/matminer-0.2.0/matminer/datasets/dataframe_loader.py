689d917f6e26981f0366bfd44b0518db2719f106
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:40 +0000

    Add package matminer/0.2.0/matminer-0.2.0.tar.gz

---
+++ packages/matminer/matminer-0.2.0.tar.gz/matminer-0.2.0/matminer/datasets/dataframe_loader.py
@@ -0,0 +1,52 @@
+import pandas
+import os
+import ast
+import numpy as np
+from pymatgen.io.vasp.inputs import Poscar
+
+
+__author__ = "Kyle Bystrom <kylebystrom@berkeley.edu>, " \
+             "Anubhav Jain <ajain@lbl.gov>"
+
+module_dir = os.path.dirname(os.path.abspath(__file__))
+
+
+def load_elastic_tensor():
+    # ref: Jong, M. De, Chen, W., Angsten, T., Jain, A., Notestine, R., Gamst,
+    # A., Sluiter, M., Ande, C. K., Zwaag, S. Van Der, Plata, J. J., Toher,
+    # C., Curtarolo, S., Ceder, G., Persson, K. a & Asta, M. Charting the
+    # complete elastic properties of inorganic crystalline compounds. Sci.
+    # Data 2, 150009 (2015).
+    df = pandas.read_csv(os.path.join(module_dir, "elastic_tensor.csv"),
+                           comment="#")
+    for i in range(df.shape[0]):
+        for c in ['compliance_tensor', 'elastic_tensor', 'elastic_tensor_original']:
+            df.at[(i,c)] = np.array(ast.literal_eval(df.at[(i,c)]))
+    df['cif'] = df['structure']
+    df['structure'] = pandas.Series([Poscar.from_string(s).structure for s in df['poscar']])
+    return df
+
+
+def load_piezoelectric_tensor():
+    # ref: de Jong, M., Chen, W., Geerlings, H., Asta, M. & Persson, K. A.
+    # A database to enable discovery and design of piezoelectric materials.
+    # Sci. Data 2, 150053 (2015).
+    df = pandas.read_csv(os.path.join(module_dir, "piezoelectric_tensor.csv"), comment="#")
+    for i in range(df.shape[0]):
+        c = 'piezoelectric_tensor'
+        df.at[(i,c)] = np.array(ast.literal_eval(df.at[(i,c)]))
+    df['cif'] = df['structure']
+    df['structure'] = pandas.Series([Poscar.from_string(s).structure for s in df['poscar']])
+    return df
+
+
+def load_dielectric_constant():
+    # ref: Petousis, I., Mrdjenovich, D., Ballouz, E., Liu, M., Winston, D.,
+    # Chen, W., Graf, T., Schladt, T. D., Persson, K. A. & Prinz, F. B.
+    # High-throughput screening of inorganic compounds for the discovery of
+    # novel dielectric and optical materials. Sci. Data 4, 160134 (2017).
+    df = pandas.read_csv(os.path.join(module_dir, "dielectric_constant.csv"),
+                           comment="#")
+    df['cif'] = df['structure']
+    df['structure'] = pandas.Series([Poscar.from_string(s).structure for s in df['poscar']])
+    return df
---
+++ packages/matminer/matminer-0.2.0.tar.gz/matminer-0.2.0/matminer/featurizers/base.py
@@ -0,0 +1,104 @@
+from __future__ import division
+
+import pandas as pd
+from six import string_types
+
+
+class BaseFeaturizer(object):
+    """Abstract class to calculate attributes for compounds"""
+
+    def featurize_dataframe(self, df, col_id, ignore_errors=False, inplace=True):
+        """
+        Compute features for all entries contained in input dataframe
+        
+        Args: 
+            df (Pandas dataframe): Dataframe containing input data
+            col_id (str or list of str): column label containing objects to
+                featurize. Can be multiple labels if the featurize function
+                requires multiple inputs
+            ignore_errors (bool): Returns NaN for dataframe rows where
+                exceptions are thrown if True. If False, exceptions
+                are thrown as normal.
+            inplace (bool): Whether to add new columns to input dataframe (df)
+
+        Returns:
+            updated Dataframe
+        """
+
+        # If only one column and user provided a string, put it inside a list
+        if isinstance(col_id, string_types):
+            col_id = [col_id]
+
+        # Compute the features
+        features = []
+        x_list = df[col_id]
+        for x in x_list.values:
+            try:
+                features.append(self.featurize(*x))
+            except:
+                if ignore_errors:
+                    features.append([float("nan")]
+                                    * len(self.feature_labels()))
+                else:
+                    raise
+
+        # Generate the feature labels
+        labels = self.feature_labels()
+
+        # Create dataframe with the new features
+        new_cols = dict(zip(labels, [pd.Series(x, index=df.index) for x in zip(*features)]))
+
+        # Update the dataframe
+        if inplace:
+            for key, value in new_cols.items():
+                df[key] = value
+            return df
+        else:
+            return df.assign(**new_cols)
+
+    def featurize(self, *x):
+        """
+        Main featurizer function. Only defined in feature subclasses.
+
+        Args:
+            x: input data to featurize (type depends on featurizer)
+
+        Returns:
+            list of one or more features
+        """
+
+        raise NotImplementedError("featurize() is not defined!")
+
+    def feature_labels(self):
+        """
+        Generate attribute names
+        
+        Returns:
+            list of strings for attribute labels
+        """
+
+        raise NotImplementedError("feature_labels() is not defined!")
+
+    def citations(self):
+        """
+        Citation / reference for feature
+
+        Returns:
+            array - each element should be str citation, ideally in BibTeX
+                format
+        """
+
+        raise NotImplementedError("citations() is not defined!")
+
+    def implementors(self):
+        """
+        List of implementors of the feature
+
+        Returns:
+            array - each element should either be str with author name (e.g.,
+                "Anubhav Jain") or dict with required key "name" and other
+                keys like "email" or "institution" (e.g., {"name": "Anubhav
+                Jain", "email": "ajain@lbl.gov", "institution": "LBNL"}).
+        """
+
+        raise NotImplementedError("implementors() is not defined!")
