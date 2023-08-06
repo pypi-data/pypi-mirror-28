5fe6062483bc5e8c083ebac50189cb7941546bc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:59 +0000

    Add package Magic-Pixel/1.0.0/Magic%20Pixel-1.0.0.tar.gz

---
+++ packages/Magic-Pixel/Magic%20Pixel-1.0.0.tar.gz/Magic Pixel-1.0.0/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+
+setup(
+	name='Magic Pixel',
+	version='1.0.0',
+	py_mode=['MagicPixel'],
+	auther='wake-up-smiling',
+	auther_email='1606260309@qq.com',
+	url='',
+	description='A simple Magic Pixel'
+
+
+)
