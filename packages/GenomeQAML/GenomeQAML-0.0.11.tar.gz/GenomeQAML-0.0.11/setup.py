b4910e55dc0073554ee90f742469a306cea51379
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:50 +0000

    Add package GenomeQAML/0.0.11/GenomeQAML-0.0.11.tar.gz

---
+++ packages/GenomeQAML/GenomeQAML-0.0.11.tar.gz/GenomeQAML-0.0.11/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+__author__ = 'adamkoziol'
+setup(
+    name="GenomeQAML",
+    version="0.0.11",
+    packages=['genomeqaml'],
+    # package_data={'genomeqaml': ['*.msh', '*.p']},
+    data_files=[('', ['genomeqaml/refseq.msh', 'genomeqaml/model.p', 'genomeqaml/dataframe.p'])],
+    # include_package_data=True,
+    license='MIT',
+    scripts=['genomeqaml/classify.py'],
+    author='OLC Bioinformatics',
+    author_email='adam.koziol@inspection.gc.ca',
+    description='CFIA OLC Genome Quality Assessment with Machine Learning',
+    url='https://github.com/OLC-LOC-Bioinformatics/GenomeQAML',
+    long_description=open('README.md').read(),
+    install_requires=[
+        'click',
+        'biopython',
+        'scipy',
+        'pandas',
+        'sklearn',
+        'olctools'
+    ]
+)
