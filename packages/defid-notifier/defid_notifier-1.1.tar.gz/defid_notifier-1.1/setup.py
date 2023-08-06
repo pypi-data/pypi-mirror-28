cd4853ca1806452f010862a3e90c13d145bb3629
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:20:18 +0000

    Add package defid-notifier/1.1/defid_notifier-1.1.tar.gz

---
+++ packages/defid-notifier/defid_notifier-1.1.tar.gz/defid_notifier-1.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+setup(
+	name='defid_notifier',
+	version='1.1',
+	description='Mass notifier for Defacer.ID',
+	url='http://github.com/p4kl0nc4t/defid_notifier',
+	author="P4kL0nc4t",
+	author_email="faizzjazadi@gmail.com",
+	license="MIT",
+	packages=['defid_notifier'],
+	zip_safe=False,
+	install_requires=['cfscrape', 'argparse']
+     )
