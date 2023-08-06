98c313b08242c788df2e0d4a4eb4afd0df60b3f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:39 +0000

    Add package neatdata/0.11/neatdata-0.11.tar.gz

---
+++ packages/neatdata/neatdata-0.11.tar.gz/neatdata-0.11/test/test_missingyrowdropper.py
@@ -0,0 +1,44 @@
+import unittest
+import pandas as pd
+import numpy as np
+from neatdata.neatdata import *
+
+class TestMissingYRowDropper(unittest.TestCase):
+
+    def testMissingYRowDropper_Execute_WithStringY(self):
+        # Assemble
+        now = pd.datetime.now()
+        trainX = pd.DataFrame({'col1': [1,1,1,1,1,1,1],
+                               'col2': ['a','a','a','b','a','b','b'],
+                               'col3': [now,now,now,now,now,now,now]})
+        trainY = ['a','b','c',None,'b', None, None]
+        # Act
+        trainX, trainY = MissingYRowDropper().execute(trainX, trainY)
+        # Assert
+        self.assertEqual(len(trainY), 4)
+        self.assertEqual(len(trainX.index), 4)
+        self.assertEqual(trainY[0], 'a')
+        self.assertEqual(trainY[1], 'b')
+        self.assertEqual(trainY[2], 'c')
+        self.assertEqual(trainY[3], 'b')
+        for i, row in trainX.iterrows():
+            self.assertEqual(row['col2'], 'a')
+
+    def testMissingYRowDropper_Execute_WithNumberY(self):
+        # Assemble
+        now = pd.datetime.now()
+        trainX = pd.DataFrame({'col1': [1,1,1,1,1,1,1],
+                               'col2': ['a','a','a','b','a','b','b'],
+                               'col3': [now,now,now,now,now,now,now]})
+        trainY = [0,1,2,np.inf,1, np.nan, -np.inf]
+        # Act
+        trainX, trainY = MissingYRowDropper().execute(trainX, trainY)
+        # Assert
+        self.assertEqual(len(trainY), 4)
+        self.assertEqual(len(trainX.index), 4)
+        self.assertEqual(trainY[0], 0)
+        self.assertEqual(trainY[1], 1)
+        self.assertEqual(trainY[2], 2)
+        self.assertEqual(trainY[3], 1)
+        for i, row in trainX.iterrows():
+            self.assertEqual(row['col2'], 'a')
