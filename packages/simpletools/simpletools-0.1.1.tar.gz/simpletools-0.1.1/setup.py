28d99fb852a70e37a3ee79e601fd715abca8551a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:26 +0000

    Add package simpletools/0.1.1/simpletools-0.1.1.tar.gz

---
+++ packages/simpletools/simpletools-0.1.1.tar.gz/simpletools-0.1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='simpletools',
+      version='0.1.1',
+      description='simpletools',
+      url='https://github.com/getsimpletools/simpletools-python/tree/master',
+      author='Marcin Rosinski',
+      author_email='marcin@getsimpletools.com',
+      license='BSD-3',
+      packages=['simpletools'],
+      zip_safe=False)
