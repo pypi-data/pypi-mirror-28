6d5aa801dc80974cfa41ead66b1f88fe1ac4b307
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:29 +0000

    Add package learnosity-sdk/0.1.1/learnosity_sdk-0.1.1.tar.gz

---
+++ packages/learnosity-sdk/learnosity_sdk-0.1.1.tar.gz/learnosity_sdk-0.1.1/setup.py
@@ -0,0 +1,28 @@
+import pip.req
+import setuptools
+
+VERSION = '0.1.1'
+
+
+def test_reqs():
+    reqs = pip.req.parse_requirements('requirements-dev.txt', session=False)
+    reqs = [str(ir.req) for ir in reqs]
+
+    return reqs
+
+
+setuptools.setup(
+    author='Cera Davies',
+    author_email='cera.davies@learnosity.com',
+    url='https://github.com/Learnosity/learnosity-sdk-python',
+    version=VERSION,
+    name='learnosity_sdk',
+    description='Learnosity SDK for Python',
+
+    packages=setuptools.find_packages(exclude=('tests')),
+
+    install_requires=[
+        'requests>=2'
+    ],
+    tests_require=test_reqs(),
+)
