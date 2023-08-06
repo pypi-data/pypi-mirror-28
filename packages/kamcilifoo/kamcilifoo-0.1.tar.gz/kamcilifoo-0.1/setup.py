a67bc1137ca604635c7920971f8d0e037633d7ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:09 +0000

    Add package kamcilifoo/0.1/kamcilifoo-0.1.tar.gz

---
+++ packages/kamcilifoo/kamcilifoo-0.1.tar.gz/kamcilifoo-0.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='kamcilifoo',
+    packages=['test', 'kamcili'],
+    version='0.1',
+    description='Test',
+    author='Ömer Kamçılı',
+    author_email='omerkamcili@@gmail.com',
+    classifiers=[],
+)
