9d8470e94e9102bd9acd980635b6fe982dbed3f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:26 +0000

    Add package cloudtoken-plugin.okta/0.0.1/cloudtoken-plugin.okta-0.0.1.tar.gz

---
+++ packages/cloudtoken-plugin.okta/cloudtoken-plugin.okta-0.0.1.tar.gz/cloudtoken-plugin.okta-0.0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+import os
+
+setup(
+    name='cloudtoken-plugin.okta',
+    version="0.0.1",
+    description='OKTA plugin for cloudtoken.',
+    url='https://bitbucket.org/atlassian/cloudtoken',
+    author='Andy Loughran',
+    author_email='andy@lockran.com',
+    license='Apache',
+    py_modules=['cloudtoken_plugin.okta'],
+    zip_safe=False,
+    python_requires='>=3.5',
+    install_requires=[
+        "cloudtoken>=0.0.1",
+        ],
+)
