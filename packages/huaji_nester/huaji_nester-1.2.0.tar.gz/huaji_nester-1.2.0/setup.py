f262c5d683d2624d1c5ae00c626671d2bb57f91a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:37 +0000

    Add package huaji_nester/1.2.0/huaji_nester-1.2.0.tar.gz

---
+++ packages/huaji_nester/huaji_nester-1.2.0.tar.gz/huaji_nester-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+            name = 'huaji_nester',
+            version = '1.2.0',
+            py_modules= ['huaji_nester'],
+            author = 'huaji',
+            author_email = '934422900@qq.com',
+            url = 'http://www.never-give-it-up.top',
+            description = 'A simple printer of nested lists',
+        )
