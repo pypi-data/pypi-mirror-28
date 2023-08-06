227ff13ce1aa2f49fc57dba0285f2b177196fced
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:04:05 +0000

    Add package odoo9-addon-medical-medicament-component/9.0.1.0.0.99.dev3/odoo9_addon_medical_medicament_component-9.0.1.0.0.99.dev3-py2-none-any.whl

---
+++ packages/odoo9-addon-medical-medicament-component/odoo9_addon_medical_medicament_component-9.0.1.0.0.99.dev3-py2-none-any.whl/odoo_addons/medical_medicament_component/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# © 2016 LasLabs Inc.
+# License GPL-3.0 or later (http://www.gnu.org/licenses/gpl.html).
+
+from . import models
+from . import tests
---
+++ packages/odoo9-addon-medical-medicament-component/odoo9_addon_medical_medicament_component-9.0.1.0.0.99.dev3-py2-none-any.whl/odoo_addons/medical_medicament_component/tests/test_medical_medicament_component.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# © 2016 LasLabs Inc.
+# License GPL-3.0 or later (http://www.gnu.org/licenses/gpl.html).
+
+
+from openerp.tests.common import TransactionCase
+from psycopg2 import IntegrityError
+
+
+class TestMedicalMedicamentComponent(TransactionCase):
+
+    def setUp(self):
+        super(TestMedicalMedicamentComponent, self).setUp()
+        self.model_obj = self.env['medical.medicament.component']
+        self.vals = {
+            'name': 'Medicament Component',
+        }
+
+    def _new_record(self, ):
+        return self.model_obj.create(self.vals)
+
+    def test_name_unique(self):
+        self._new_record()
+        with self.assertRaises(IntegrityError):
+            self._new_record()
