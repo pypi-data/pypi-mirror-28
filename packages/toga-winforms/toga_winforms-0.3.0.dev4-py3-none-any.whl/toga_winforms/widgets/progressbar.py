d5d273d3ce2e51b618f291d07ad8765aed1c9b92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:52 +0000

    Add package toga-winforms/0.3.0.dev4/toga_winforms-0.3.0.dev4-py3-none-any.whl

---
+++ packages/toga-winforms/toga_winforms-0.3.0.dev4-py3-none-any.whl/toga_winforms/widgets/progressbar.py
@@ -0,0 +1,30 @@
+from travertino.size import at_least
+
+from toga_winforms.libs import *
+
+from .base import Widget
+
+
+class ProgressBar(Widget):
+    def create(self):
+        self.native = WinForms.ProgressBar()
+
+    def start(self):
+        '''Not supported for WinForms implementation'''
+        self.interface.factory.not_implemented('ProgressBar.start()')
+
+    def stop(self):
+        '''Not supported for WinForms implementation'''
+        self.interface.factory.not_implemented('ProgressBar.stop()')
+
+    def set_max(self, value):
+        self.native.Maximum = value
+
+    def set_value(self, value):
+        self.native.Value = value
+
+    def rehint(self):
+        # Height must be non-zero
+        # Set a sensible min-width
+        self.interface.intrinsic.width = at_least(self.interface.MIN_WIDTH)
+        self.interface.intrinsic.height = self.native.PreferredSize.Height
