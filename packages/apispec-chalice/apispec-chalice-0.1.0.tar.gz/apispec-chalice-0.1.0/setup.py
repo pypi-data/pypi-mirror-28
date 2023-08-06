5db84476299907cac8484d279a6f7a73cc39589a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:53:14 +0000

    Add package apispec-chalice/0.1.0/apispec-chalice-0.1.0.tar.gz

---
+++ packages/apispec-chalice/apispec-chalice-0.1.0.tar.gz/apispec-chalice-0.1.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+from importlib import import_module
+import os.path
+
+setup(
+    name="apispec-chalice",
+    version="0.1.0",
+    packages=['apispec_chalice'],
+    description='Chalice for the apispec library',
+    author='Mathew Marcus',
+    author_email='mathewmarcus456@gmail.com',
+)
+
+try:
+    apispec_ext_package = os.path.dirname(import_module('apispec.ext').__file__)
+    link_name = os.path.join(apispec_ext_package, 'chalice.py')
+
+    target = import_module('apispec_chalice.chalice').__file__
+    os.symlink(target, link_name)
+except ImportError:
+    pass
+except FileExistsError:
+    os.remove(link_name)
+    os.symlink(target, link_name)
