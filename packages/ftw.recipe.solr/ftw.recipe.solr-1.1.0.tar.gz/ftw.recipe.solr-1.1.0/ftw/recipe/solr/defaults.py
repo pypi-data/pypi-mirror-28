ed1e60d42568978939ef33a4c17161609baa1592
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:52 +0000

    Add package ftw.recipe.solr/1.1.0/ftw.recipe.solr-1.1.0.tar.gz

---
+++ packages/ftw.recipe.solr/ftw.recipe.solr-1.1.0.tar.gz/ftw.recipe.solr-1.1.0/ftw/recipe/solr/defaults.py
@@ -0,0 +1,7 @@
+DEFAULT_OPTIONS = {
+    'url': 'http://www-eu.apache.org/dist/lucene/solr/7.2.1/solr-7.2.1.tgz',
+    'md5sum': 'c9a2a610a20064228f42dad8b1128b46',
+    'host': 'localhost',
+    'port': '8983',
+    'jvm-opts': '-Xms512m -Xmx512m -Xss256k',
+}
