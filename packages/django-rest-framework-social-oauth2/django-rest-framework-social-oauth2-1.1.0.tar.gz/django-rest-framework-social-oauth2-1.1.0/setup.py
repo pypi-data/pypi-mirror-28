772c12214eb5e46747de99306c2f72079ea950b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:03 +0000

    Add package django-rest-framework-social-oauth2/1.1.0/django-rest-framework-social-oauth2-1.1.0.tar.gz

---
+++ packages/django-rest-framework-social-oauth2/django-rest-framework-social-oauth2-1.1.0.tar.gz/django-rest-framework-social-oauth2-1.1.0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-rest-framework-social-oauth2',
+    version=__import__('rest_framework_social_oauth2').__version__,
+    description=__import__('rest_framework_social_oauth2').__doc__,
+    long_description=open('README.rst').read(),
+    author='Philip Garnero',
+    author_email='philip.garnero@gmail.com',
+    url='https://github.com/PhilipGarnero/django-rest-framework-social-oauth2',
+    license='MIT',
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    install_requires=[
+        'djangorestframework>=3.0.1',
+        'django-oauth-toolkit>=1.0.0',
+        'social-auth-app-django>=0.1.0',
+        'django-braces>=1.11.0',
+    ],
+    include_package_data=True,
+    zip_safe=False,
+)
