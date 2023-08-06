20efa82d909d7a55b286f5ed30eaa4812bb4c188
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:56 +0000

    Add package ACNLogger/0.2.8/ACNLogger-0.2.8.tar.gz

---
+++ packages/ACNLogger/ACNLogger-0.2.8.tar.gz/ACNLogger-0.2.8/ACNLogger/__init__.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import logging
+
+class ACNLogger:
+
+	def debug(self, session, message):
+		try:
+			self.logger.debug((session+"  "+self._service_name+"  DEBUG  "+str(message)).encode('utf8'))
+		except:
+			try:
+				self.logger.debug(session+"  "+self._service_name+"  DEBUG  "+str(message))
+			except:
+				self.logger.debug((session+"  "+self._service_name+"  DEBUG  "+str(message)).decode('utf8'))
+
+	def info(self, session, message):
+		try:
+			self.logger.info((session+"  "+self._service_name+"  INFO  "+str(message)).encode('utf8'))
+		except:
+			try:
+				self.logger.info(session+"  "+self._service_name+"  INFO  "+str(message))
+			except:
+				self.logger.info((session+"  "+self._service_name+"  INFO  "+str(message)).decode('utf8'))
+
+	def warning(self, session, message):
+		try:
+			self.logger.warning((session+"  "+self._service_name+"  WARNING  "+str(message)).encode('utf8'))
+		except:
+			try:
+				self.logger.warning(session+"  "+self._service_name+"  WARNING  "+str(message))
+			except:
+				self.logger.warning((session+"  "+self._service_name+"  WARNING  "+str(message)).decode('utf8'))
+
+	def error(self, session, e):
+		try:
+			self.logger.error((session+"  "+self._service_name+"  ERROR  "+str(e.__class__.__name__)+"  "+str(e.message)).encode('utf8'))
+		except:
+			try:
+				self.logger.error(session+"  "+self._service_name+"  ERROR  "+str(e.__class__.__name__)+"  "+str(e.message))
+			except:
+				self.logger.error((session+"  "+self._service_name+"  ERROR  "+str(e.__class__.__name__)+"  "+str(e.message)).decode('utf8'))
+
+	def critical(self, session, e):
+		try:
+			self.logger.critical((session+"  "+self._service_name+"  CRITICAL  "+str(e.__class__.__name__)+"  "+str(e.message)).encode('utf8'))
+		except:
+			try:
+				self.logger.critical(session+"  "+self._service_name+"  CRITICAL  "+str(e.__class__.__name__)+"  "+str(e.message))
+			except:
+				self.logger.critical((session+"  "+self._service_name+"  CRITICAL  "+str(e.__class__.__name__)+"  "+str(e.message)).decode('utf8'))
+
+	def exception(self, session, e):
+		try:
+			self.logger.error((session+"  "+self._service_name+"  ERROR  "+str(e.__class__.__name__)+"  "+str(e.message)).encode('utf8'))
+		except:
+			try:
+				self.logger.error(session+"  "+self._service_name+"  ERROR  "+str(e.__class__.__name__)+"  "+str(e.message))
+			except:
+				self.logger.error((session+"  "+self._service_name+"  ERROR  "+str(e.__class__.__name__)+"  "+str(e.message)).decode('utf8'))
+
+
+
+	def __init__(self,name,file):
+
+		logging.basicConfig(format=u'%(asctime)s  %(message)s', level=logging.DEBUG)
+
+		BC_logger=logging.getLogger(name) # Creating the new logger
+		BC_logger.setLevel(logging.DEBUG) # Setting new logger level to INFO or above
+
+		file_handler=logging.FileHandler(file) #Creating handler for log file
+		file_handler.setLevel(logging.INFO) 
+
+		BC_logger.addHandler(file_handler) #Adding file handler to the new logger
+
+		formatter=logging.Formatter(u'%(asctime)s  %(message)s') #Creating a formatter
+
+		file_handler.setFormatter(formatter) #Setting handler format
+
+		self.logger=BC_logger
+		self._service_name=name
+
+		self.info("UNDEFINED","STARTING MICROSERVICE")
---
+++ packages/ACNLogger/ACNLogger-0.2.8.tar.gz/ACNLogger-0.2.8/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='ACNLogger',    # This is the name of your PyPI-package.
+    version='0.2.8',           # The name of your scipt, and also the command you'll be using for calling it
+    author='Sergi Teruel',
+    author_email='sergi.teruel.prieto@accenture.com',
+    packages=['ACNLogger']
+)
