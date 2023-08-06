715c1ec3e595654cef94cdf9eae8ab58f39331c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:35 +0000

    Add package geofound/0.2.4/geofound-0.2.4.tar.gz

---
+++ packages/geofound/geofound-0.2.4.tar.gz/geofound-0.2.4/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+
+setup(name='geofound',
+      version='0.2.4',
+      description='A package to assess the bearing capacity and settlement of geofound',
+      url='https://github.com/eng-tools/geofound',
+      author='Maxim Millen',
+      author_email='mmi46@uclive.ac.nz',
+      keywords='geotechnical engineering foundations settlement',
+      license='MIT',
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Science/Research',
+          'Topic :: Scientific/Engineering',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3',
+      ],
+      packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+      install_requires=['sfsimodels'],
+      # List additional groups of dependencies here (e.g. development
+      # dependencies). You can install these using the following syntax,
+      # for example:
+      # $ pip install -e .[dev,test]
+      extras_require={
+          'test': ['pytest'],
+      },
+      python_requires='>=3',
+      package_data={},
+      zip_safe=False)
+
+
+# From python packaging guides
+# versioning is a 3-part MAJOR.MINOR.MAINTENANCE numbering scheme,
+# where the project author increments:
+
+# MAJOR version when they make incompatible API changes,
+# MINOR version when they add functionality in a backwards-compatible manner, and
+# MAINTENANCE version when they make backwards-compatible bug fixes.
