94bdb2c9df807eaba9c5398537ed99c43279dac9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:05 +0000

    Add package testpip/0.1/testpip-0.1.tar.gz

---
+++ packages/testpip/testpip-0.1.tar.gz/testpip-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='testpip',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='http://github.com/n89nanda/testpip',
+      author='Flying Circus',
+      author_email='n89nanda@gmail.com@gmail.com',
+      license='MIT',
+      packages=['testpip'],
+      zip_safe=False)
