346c2091760ef7c3c9954ab117e499a1f6505699
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:04 +0000

    Add package IMDBAPI/0.1.beta/IMDBAPI-0.1.beta.tar.gz

---
+++ packages/IMDBAPI/IMDBAPI-0.1.beta.tar.gz/IMDBAPI-0.1.beta/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*
+
+from __future__ import absolute_import
+
+import os
+
+from setuptools import find_packages, setup
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+with open('requirements.txt') as f:
+    install_requires = f.read().splitlines()
+
+setup(name='IMDBAPI',
+      version='0.1 beta',
+      description='API for IMDB',
+	  packages=find_packages('IMDBAPI', exclude=('tests',)),
+	  package_dir={'': 'IMDBAPI'},
+      url='https://github.com/devilking15292/IMDB_Api_python.git',
+      author='devilking15292',
+      author_email='p.hari15292@gmail.com',
+      license='MIT',
+	  install_requires=install_requires,
+      zip_safe=False,
+	   classifiers=[
+        'Topic :: Internet :: WWW/HTTP',
+		]
+	)
