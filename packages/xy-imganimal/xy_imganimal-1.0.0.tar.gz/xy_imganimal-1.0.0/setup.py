cb0a31c0cfd37c28d24ca9a5cb110dc957f24f90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:17 +0000

    Add package xy-imganimal/1.0.0/xy_imganimal-1.0.0.tar.gz

---
+++ packages/xy-imganimal/xy_imganimal-1.0.0.tar.gz/xy_imganimal-1.0.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'xy_imganimal',
+  packages = ['xy_imganimal'],
+  version = '1.0.0',
+  description = 'Recognition Image Animal',
+  long_description='Recognition Image Animal',
+  author = 'KingHS',
+  author_email = '382771946@qq.com',
+  keywords = ['pip3', 'python3','python','Recognition Image Animal'],
+  license='MIT',
+  install_requires=['requests']
+)
