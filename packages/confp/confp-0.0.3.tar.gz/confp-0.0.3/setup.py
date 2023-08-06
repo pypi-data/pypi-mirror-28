c18156e13413b9dff576954823068d935d183ee5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:27 +0000

    Add package confp/0.0.3/confp-0.0.3.tar.gz

---
+++ packages/confp/confp-0.0.3.tar.gz/confp-0.0.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='confp',
+    version='0.0.3',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    install_requires=[
+        'jinja2',
+        'pyyaml',
+        'cerberus'
+    ]
+)
