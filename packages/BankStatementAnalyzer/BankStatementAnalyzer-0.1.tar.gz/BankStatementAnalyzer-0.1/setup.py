e3e2688864e28150b9626578a13865224103599e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:45 +0000

    Add package BankStatementAnalyzer/0.1/BankStatementAnalyzer-0.1.tar.gz

---
+++ packages/BankStatementAnalyzer/BankStatementAnalyzer-0.1.tar.gz/BankStatementAnalyzer-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'BankStatementAnalyzer',
+  packages = ['BankStatementAnalyzer'],
+  version = '0.1',
+  description = 'A python based bank statement analyzer which works using Pandas.',
+  author = 'Tarun Kumar Rajamannar',
+  author_email = 'rtarunkumar@live.com',
+  url = 'https://github.com/TarunKumarR/BankStatementAnalyzer',
+#  download_url = '',
+  keywords = ['bank', 'statement', 'analyze','monthly','money'],
+  classifiers = [],
+)
