f397f565a7cab089127c5365d5ddf7a344fad1a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:57 +0000

    Add package auxygen/2018.1.17/auxygen-2018.1.17.tar.gz

---
+++ packages/auxygen/auxygen-2018.1.17.tar.gz/auxygen-2018.1.17/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='auxygen',
+    version='2018.1.17',
+    packages=[
+        'auxygen',
+        'auxygen.devices',
+        'auxygen.gui',
+        'auxygen.gui.ui',
+        'auxygen.scripo',
+    ],
+    url='https://hg.3lp.cx/auxygen',
+    license='GPL',
+    author='Vadim Dyadkin',
+    author_email='diadkin@esrf.fr',
+    description='Drivers and scripting for Pylatus auxiliary equipment',
+    entry_points={
+        'gui_scripts': [
+            'blower=auxygen.executables:blower',
+            'cryostream=auxygen.executables:cryostream',
+            'lakeshore=auxygen.executables:lakeshore',
+        ],
+    },
+    install_requires=[
+        'pyqtgraph',
+        'pyserial',
+        'aspic',
+        'qtsnbl',
+    ],
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+    ],
+)
