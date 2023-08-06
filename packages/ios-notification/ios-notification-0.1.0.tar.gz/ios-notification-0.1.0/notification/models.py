e6b9cb9df9a07a0298772805cd800d95308da2a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:18 +0000

    Add package ios-notification/0.1.0/ios-notification-0.1.0.tar.gz

---
+++ packages/ios-notification/ios-notification-0.1.0.tar.gz/ios-notification-0.1.0/notification/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils.timezone import now
+
+
+class DeviceToken(models.Model):
+    device_token = models.CharField(blank=False, default='', max_length=255)
+    uuid = models.CharField(blank=False, default='', max_length=255)
+    use_sandbox = models.BooleanField(blank=False, default=True)
+    user = models.ForeignKey(User, null=True, blank=True, default=None, on_delete=models.CASCADE)
+
+
+class CertFile(models.Model):
+    filename = models.CharField(blank=False, default='cert.pem', max_length=100)
+    target_mode = models.IntegerField(blank=False, default=0)
+    is_use = models.BooleanField(blank=False, default=True)
+    date = models.DateTimeField(blank=False, default=now)
