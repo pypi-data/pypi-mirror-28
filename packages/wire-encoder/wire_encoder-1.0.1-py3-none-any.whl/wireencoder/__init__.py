afdeaed9fcac2fb6989dc98c9e886409979700f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:48 +0000

    Add package wire-encoder/1.0.1/wire_encoder-1.0.1-py3-none-any.whl

---
+++ packages/wire-encoder/wire_encoder-1.0.1-py3-none-any.whl/wireencoder/__init__.py
@@ -0,0 +1,9 @@
+"""
+Wireencode provides a set of wire safe data encoders and decoders.
+Exposes simple interfaces.
+Streaming APIs *may* be added in an unspecified future.
+"""
+
+from .zstd_b64 import ZstdBase64Encoder, ZstdBase64Decoder
+from .zlib_b64 import ZlibBase64Encoder, ZlibBase64Decoder
+from .zlib_b85 import ZlibBase85Encoder, ZlibBase85Decoder
---
+++ packages/wire-encoder/wire_encoder-1.0.1-py3-none-any.whl/wireencoder/exceptions.py
@@ -0,0 +1,9 @@
+"""
+Wire encoding exceptions
+"""
+
+class InvalidData(Exception):
+    """
+    A class which expresses invalid data was handled by the wire encoder
+    """
+    pass
