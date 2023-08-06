e96356f61fe7a10382c1f16550517539c49126a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:20 +0000

    Add package whistlepy/0.0.7/whistlepy-0.0.7.tar.gz

---
+++ packages/whistlepy/whistlepy-0.0.7.tar.gz/whistlepy-0.0.7/whistlepy/client/api/__init__.py
@@ -0,0 +1,6 @@
+from whistlepy.client.api.oauth import WhistleOAuthApi
+from whistlepy.client.api.notification import WhistleNotificationApi
+from whistlepy.client.api.organization import WhistleOrganizationApi
+from whistlepy.client.api.geo import WhistleWifiLocApi
+from whistlepy.client.api.label import WhistleLabelApi
+from whistlepy.client.api.adhoc import WhistleAdhocApi
