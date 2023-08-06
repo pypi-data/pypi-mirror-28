f7849b96e073df900ac5ef0013ca14921d7e1a9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:32 +0000

    Add package scrapify/5.7/Scrapify-5.7.tar.gz

---
+++ packages/scrapify/Scrapify-5.7.tar.gz/Scrapify-5.7/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='Scrapify',
+    packages=['Scrapify'],
+    version='5.7',
+    description='A simple and intuitive library that allows dead easy web scraping with 3 lines of code.',
+    author='David Mellul',
+    author_email='david.mellul@outlook.fr',
+    url='https://github.com/DavidMellul/Scrapify',
+    license='MIT',
+    install_requires=[
+        'unidecode', 'requests', 'lxml', 'cssselect'
+    ],
+    keywords=['networking', 'web', 'scraping', 'data', 'retrieval']
+)
