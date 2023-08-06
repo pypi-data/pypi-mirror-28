d09d187fe727c6a48d4657ea7f36435bb66c435e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:23 +0000

    Add package EPAB/0.2.3/EPAB-0.2.3-py36-none-any.whl

---
+++ packages/EPAB/EPAB-0.2.3-py36-none-any.whl/epab/cmd/__init__.py
@@ -0,0 +1,10 @@
+# coding=utf-8
+"""
+Manages commands
+"""
+
+from .appveyor import appveyor
+from .changelog import chglog
+from .release import release
+from .requirements import reqs
+from .test_runner import pytest
