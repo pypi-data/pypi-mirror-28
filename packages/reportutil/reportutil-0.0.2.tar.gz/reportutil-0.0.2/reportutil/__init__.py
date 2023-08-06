6cc6fd2ee9fd65c3a0d2e4778715499236ad2e3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:21 +0000

    Add package reportutil/0.0.2/reportutil-0.0.2.tar.gz

---
+++ packages/reportutil/reportutil-0.0.2.tar.gz/reportutil-0.0.2/reportutil/__init__.py
@@ -0,0 +1,35 @@
+
+import attr
+
+# Get module version
+from ._metadata import __version__
+
+# Import key items from module
+from .report import Report
+from .constants import ReportConstant
+from .components import ReportDirNode, ReportFileNode
+from .nodes import HTMLReport
+
+
+@attr.s
+class _ReportNodeTypes(object):
+    # Base node types
+    root = attr.ib(default=Report, init=False)
+    dir = attr.ib(default=ReportDirNode, init=False)
+    file = attr.ib(default=ReportFileNode, init=False)
+
+    # Node file types
+    html = attr.ib(default=HTMLReport, init=False)
+
+
+ReportNodeTypes = _ReportNodeTypes()
+
+
+__all__ = [
+    Report,
+    ReportConstant,
+    ReportDirNode,
+    ReportFileNode,
+    ReportNodeTypes,
+    HTMLReport
+]
---
+++ packages/reportutil/reportutil-0.0.2.tar.gz/reportutil-0.0.2/reportutil/components/__init__.py
@@ -0,0 +1,4 @@
+
+from .dir import ReportDirNode
+from .file import ReportFileNode
+from .node import ReportNode
