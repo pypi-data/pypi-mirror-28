c3935b7a679b96171dcfd4af54ec9058518581ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:00 +0000

    Add package ynot/0.2.15/ynot-0.2.15-py2-none-any.whl

---
+++ packages/ynot/ynot-0.2.15-py2-none-any.whl/ynot/globals.py
@@ -0,0 +1,32 @@
+import logging
+
+__version__ = '0.2.15'
+
+stringtype = unicode
+
+logging.basicConfig() #TODO: necessary???
+logger = logging.getLogger(__name__) #TODO: necessary???
+
+templateregex = r"""
+    %(delimiter)s(?:
+      (?P<escaped>%(delimiter)s)    |   # Escape sequence of two delimiters
+      {(?P<braced>%(pathpattern)s)}     # delimiter and a braced identifier
+    )
+"""
+
+validationmessage = '''Schema validation for Transformator failed: %(message)s"
+    document-path: %(docpath)s
+    schema-path:   %(schemapath)s'''
+
+verificationmessage='''Verifying sample %(id)s failed
+
+Expected:
+%(expected)s
+Got:
+%(result)s'''
+
+yaml_defaults={
+    'width': 128,
+    'default_flow_style': False,
+    'default_style': None,
+}
