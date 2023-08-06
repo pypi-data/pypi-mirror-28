851ad0a40da22a3bb5e8b7b47dd3ec5ea00a3d48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:00 +0000

    Add package licor/0.0.4/licor-0.0.4-py3-none-any.whl

---
+++ packages/licor/licor-0.0.4-py3-none-any.whl/licor/__main__.py
@@ -0,0 +1,26 @@
+# Copyright (c) 2017 Daniel Knüttel                                           #
+#                                                                             #
+# This file is part of licor.                                                 #
+#                                                                             #
+# licor is free software: you can redistribute it and/or modify               #
+# it under the terms of the GNU Affero General Public License as published by #
+# the Free Software Foundation, either version 3 of the License, or           #
+# (at your option) any later version.                                         #
+#                                                                             #
+# licor is distributed in the hope that it will be useful,                    #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of              #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               #
+# GNU General Public License for more details.                                #
+#                                                                             #
+# You should have received a copy of the GNU Affero General Public License    #
+# along with licor.  If not, see <http://www.gnu.org/licenses/>.              #
+#                                                                             #
+#                                                                             #
+
+
+
+from .main import main
+
+if( __name__ == "__main__"):
+	main()
+
