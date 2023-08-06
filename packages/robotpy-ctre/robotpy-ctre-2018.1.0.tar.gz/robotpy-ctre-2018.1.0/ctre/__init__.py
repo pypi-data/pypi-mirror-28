988b90b08418bdee1501828272efea22e245ee49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:40 +0000

    Add package robotpy-ctre/2018.1.0/robotpy-ctre-2018.1.0.tar.gz

---
+++ packages/robotpy-ctre/robotpy-ctre-2018.1.0.tar.gz/robotpy-ctre-2018.1.0/ctre/__init__.py
@@ -0,0 +1,19 @@
+
+from .wpi_talonsrx import WPI_TalonSRX
+from .wpi_victorspx import WPI_VictorSPX
+from .canifier import CANifier
+from .pigeonimu import PigeonIMU
+
+from ._impl import (
+    ControlMode,
+    FeedbackDevice,
+    RemoteFeedbackDevice,
+    NeutralMode,
+)
+
+from .trajectorypoint import TrajectoryPoint
+
+try:
+    from .version import __version__
+except ImportError:     # pragma: nocover
+    __version__ = 'master'
