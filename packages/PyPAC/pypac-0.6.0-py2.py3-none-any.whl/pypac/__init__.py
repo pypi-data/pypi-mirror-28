4d9a8a04d3d98c76a039a817f9f59718b1ad950b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:16 +0000

    Add package PyPAC/0.6.0/pypac-0.6.0-py2.py3-none-any.whl

---
+++ packages/PyPAC/pypac-0.6.0-py2.py3-none-any.whl/pypac/__init__.py
@@ -0,0 +1,9 @@
+from pypac.api import get_pac, collect_pac_urls, download_pac, PACSession, pac_context_for_url
+
+
+__version__ = '0.6.0'
+
+
+__all__ = [
+    'get_pac', 'collect_pac_urls', 'download_pac', 'PACSession', 'pac_context_for_url'
+]
