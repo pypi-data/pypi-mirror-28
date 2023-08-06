70ec42c569ee7da6153ef9f07300c7d47b56e514
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:53:16 +0000

    Add package mathviz-hopper/0.3.1/mathviz_hopper-0.3.1.tar.gz

---
+++ packages/mathviz-hopper/mathviz_hopper-0.3.1.tar.gz/mathviz_hopper-0.3.1/mathviz_hopper/__init__.py
@@ -0,0 +1,13 @@
+from mathviz_hopper.src.table import Table
+from mathviz_hopper.src.print_math import print_math
+# from datasketch.minhash import MinHash
+# from datasketch.b_bit_minhash import bBitMinHash
+# from datasketch.lsh import MinHashLSH
+# from datasketch.weighted_minhash import WeightedMinHash, WeightedMinHashGenerator
+# from datasketch.lshforest import MinHashLSHForest
+# from datasketch.lshensemble import MinHashLSHEnsemble
+# from datasketch.lean_minhash import LeanMinHash
+
+# Alias
+# WeightedMinHashLSH = MinHashLSH
+# WeightedMinHashLSHForest = MinHashLSHForest
