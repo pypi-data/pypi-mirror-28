132de6d2739e9cb256eb7894dadf6492633213c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:30 +0000

    Add package XMKeys/1.1/XMKeys-1.1.tar.gz

---
+++ packages/XMKeys/XMKeys-1.1.tar.gz/XMKeys-1.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'XMKeys',      
+    version = '1.1',
+    py_modules = ['classKey'],
+    author = 'Karma_23',        
+    author_email = 'xiaomul1121@163.com',
+    url = 'https://www.baidu.com/',
+    description = 'This is a good module.'   
+    )
