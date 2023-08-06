c269f3b1f471ac0d2a7349431242d8aa5822589b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:31 +0000

    Add package PodSixNet/0.9.3/PodSixNet-0.9.3.tar.gz

---
+++ packages/PodSixNet/PodSixNet-0.9.3.tar.gz/PodSixNet-0.9.3/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+import subprocess
+
+# convert readme to rst format
+try:
+   import pypandoc
+   long_description = pypandoc.convert('README.md', 'rst')
+except:
+   long_description = ''
+
+setup(
+    version=subprocess.check_output(["git", "describe", "--tag", "--always"]).decode().strip(),
+    name='PodSixNet',
+    description='Multiplayer networking library for games',
+    long_description=long_description,
+    author='Chris McCormick',
+    author_email='chris@mccormick.cx',
+    url='https://github.com/chr15m/PodSixNet',
+    packages=['PodSixNet'],
+)
