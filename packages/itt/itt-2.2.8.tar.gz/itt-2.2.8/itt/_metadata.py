571089ffdc7995ba3b1a1f21c1b753f770493735
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:10 +0000

    Add package itt/2.2.8/itt-2.2.8.tar.gz

---
+++ packages/itt/itt-2.2.8.tar.gz/itt-2.2.8/itt/_metadata.py
@@ -0,0 +1,20 @@
+
+__version__ = u'2.2.8'
+
+__author__ = u'Oli Davis & Hywel Thomas'
+__authorshort__ = u'OWBD_HT'
+__authoremail__ = u'oli.davis@me.com / hywel.thomas@mac.com'
+
+__copyright__ = u'Copyright (C) 2016 ' + __author__
+__license__ = u'MIT'
+
+__description__ = u'Provides a set of useful test tools written in Python.'
+
+repo_user = u'davisowb'
+repo_name = u'integration-test-tools'
+__url__ = u'https://bitbucket.org/{user}/{repo}.git'.format(user=repo_user,
+                                                            repo=repo_name)  # Use the url to the git repo
+__downloadurl__ = u'https://bitbucket.org/{user}/' \
+                  u'{repo}.git/get/{version}.tar'.format(user=repo_user,
+                                                         repo=repo_name,
+                                                         version=__version__)
