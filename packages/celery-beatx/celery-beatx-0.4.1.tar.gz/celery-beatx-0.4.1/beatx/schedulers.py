1a760a96875733d4b19ebf0fb42943e07ab6a78b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:29 +0000

    Add package celery-beatx/0.4.1/celery-beatx-0.4.1.tar.gz

---
+++ packages/celery-beatx/celery-beatx-0.4.1.tar.gz/celery-beatx-0.4.1/beatx/schedulers.py
@@ -0,0 +1,116 @@
+from urllib.parse import urlparse
+
+from celery.beat import Scheduler as BaseScheduler
+from celery.exceptions import ImproperlyConfigured
+from celery.utils.log import get_logger
+
+from .utils import import_string
+
+logger = get_logger(__name__)
+
+
+class Scheduler(BaseScheduler):
+    """
+    Celery scheduler which use store class to load/save schedule.
+
+    Only single instance of running beat instances will be active.
+    Another instances will run in "sleep-mode" and will waiting
+    when master instance will dead[or not :-)].
+    """
+
+    @staticmethod
+    def get_store(app):
+        """
+        Get store object from celery application.
+
+        :param app: celery application
+        :return: store
+        """
+        store_classes = getattr(app.conf, 'beatx_store_classes', {
+            'dummy': 'beatx.store.dummy.Store',
+            'redis': 'beatx.store.redis.Store',
+            'memcached': 'beatx.store.memcached.MemcachedStore',
+            'pylibmc': 'beatx.store.memcached.PyLibMCStore',
+        })
+        store_url = getattr(app.conf, 'beatx_store')
+
+        scheme = urlparse(store_url).scheme
+
+        try:
+            store_class = store_classes[scheme]
+        except KeyError:
+            raise ImproperlyConfigured(
+                '"%(used)s" store scheme is unsupported. '
+                'Available stores schemes: %(available)s' % {
+                    'used': scheme,
+                    'available': ', '.join(
+                        '"%s"' % x for x in store_classes.keys()
+                    )
+                }
+            )
+
+        if not isinstance(store_class, type):
+            store_class = import_string(store_class)
+
+        return store_class(store_url)
+
+    def __init__(self, app, *args, **kwargs):
+        self.store = self.get_store(app)
+
+        super().__init__(app, *args, **kwargs)
+
+        self.lock_ttl = getattr(
+            app.conf,
+            'beatx_store_lock_ttl',
+            self.max_interval + 1
+        )
+
+        if self.max_interval >= self.lock_ttl:
+            raise ImproperlyConfigured(
+                '`beatx_store_lock_ttl` must be greater '
+                'then `beat_max_loop_interval`'
+            )
+
+    def setup_schedule(self):
+        if self.store.has_locked():
+            self.merge_inplace(self.app.conf.beat_schedule)
+            self.install_default_entries(self.schedule)
+            self.update_from_dict(self.store.load_entries())
+
+            self.sync()
+
+    def sync(self):
+        if not self.store.has_locked():
+            return
+
+        self.store.save_entries(self.schedule)
+
+    def acquire_lock(self):
+        acquired = self.store.acquire_lock(self.lock_ttl)
+
+        if acquired:
+            self.setup_schedule()
+
+        return acquired
+
+    def tick(self, *args, **kwargs):
+        if not self.store.has_locked():
+            acquired = self.acquire_lock()
+
+            if acquired:
+                logger.info('beatX: Lock acquired.')
+            else:
+                logger.info('beatX: Awaiting lock...')
+                return self.max_interval
+        else:
+            self.store.renew_lock()
+            logger.info('beatX: Lock renewed.')
+
+        return super().tick(*args, **kwargs)
+
+    def close(self):
+        super().close()
+
+        if self.store.has_locked():
+            self.store.release_lock()
+            logger.info('beatX: Lock released.')
---
+++ packages/celery-beatx/celery-beatx-0.4.1.tar.gz/celery-beatx-0.4.1/beatx/store/redis.py
@@ -0,0 +1,50 @@
+import json
+
+from redis import StrictRedis
+
+from beatx import serializer
+
+
+class Store:
+    SCHEDULE_KEY = 'celery:schedule'
+    LOCK_KEY = 'celery:beat-lock'
+
+    def __init__(self, store):
+        self.rdb = StrictRedis.from_url(store)
+        self.lock = None
+
+    def load_entries(self):
+        return {
+            name.decode(): serializer.deserialize_entry(
+                json.loads(data.decode())
+            ) for name, data in self.rdb.hgetall(self.SCHEDULE_KEY).items()
+        }
+
+    def save_entries(self, entries):
+        self.rdb.hmset(self.SCHEDULE_KEY, {
+            name: json.dumps(
+                serializer.serialize_entry(entry)
+            ) for name, entry in entries.items()
+        })
+
+    def has_locked(self):
+        return self.lock is not None
+
+    def acquire_lock(self, lock_ttl=60):
+        lock = self.rdb.lock(self.LOCK_KEY, timeout=lock_ttl, sleep=1)
+
+        if lock.acquire(blocking=False):
+            self.lock = lock
+            return True
+
+        return False
+
+    def renew_lock(self):
+        if not self.has_locked():
+            raise RuntimeError("Cannot renew lock: lock is not set.")
+
+        self.rdb.pexpire(self.LOCK_KEY, self.lock.timeout * 1000)
+
+    def release_lock(self):
+        self.lock.release()
+        self.lock = None
