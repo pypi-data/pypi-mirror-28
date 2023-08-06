9dec1d272e952589640f11a9b922aa9190124a1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:42 +0000

    Add package django-ark/0.0.4/django-ark-0.0.4.tar.gz

---
+++ packages/django-ark/django-ark-0.0.4.tar.gz/django-ark-0.0.4/ark/locks.py
@@ -0,0 +1,51 @@
+from ark.models import Lock
+from django.core.exceptions import ObjectDoesNotExist
+from django.db import transaction
+from django.db.utils import IntegrityError
+"""
+Atomic locks to ensure only a single instance of a process is running.
+"""
+
+
+class LockOnError(Exception):
+    pass
+
+
+class LockOffError(Exception):
+    pass
+
+
+class DbLock:
+    def __init__(self, name):
+        self.name = name
+
+    @transaction.atomic
+    def set(self, hard=False):
+        try:
+            lock = Lock.objects.select_for_update().filter(name=self.name, locked=False).update(locked=True)
+            print(lock)
+            if not lock:
+                try:
+                    Lock.objects.select_for_update().create(name=self.name, locked=True)
+                except IntegrityError:
+                    if hard:
+                        pass
+
+                    raise LockOnError("Lock {} was on while trying to turn it on")
+
+        except ObjectDoesNotExist:
+            Lock.objects.select_for_update().create(name=self.name, locked=False).update(locked=True)
+
+    @transaction.atomic
+    def release(self, hard=False):
+        lock = Lock.objects.select_for_update().filter(name=self.name, locked=True).update(locked=False)
+        if not lock and not hard:
+            raise LockOffError("Lock {} was off while trying to turn it off".format(self.name))
+
+    def state(self):
+        try:
+            lock = Lock.objects.get(name=self.name).locked
+            return lock.locked
+        except ObjectDoesNotExist:
+            lock = Lock.objects.select_for_update().create(name=self.name, locked=False)
+            return lock.locked
