9da1b845fb8c51f8bd0ec082bb59d367a59cc1b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:07 +0000

    Add package grimoire-elk/0.30.22/grimoire_elk-0.30.22-py3-none-any.whl

---
+++ packages/grimoire-elk/grimoire_elk-0.30.22-py3-none-any.whl/grimoire_elk/ocean/meetup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+#
+# Meetup Ocean feeder
+#
+# Copyright (C) 2015 Bitergia
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+#
+# Authors:
+#   Alvaro del Castillo San Felix <acs@bitergia.com>
+#
+
+from .elastic import ElasticOcean
+
+
+class MeetupOcean(ElasticOcean):
+    """Git Ocean feeder"""
+
+    @classmethod
+    def get_perceval_params_from_url(cls, url):
+        params = []
+
+        # The URL is directly the meetup group so use it as the tag
+        params.append('--tag')
+        params.append(url)
+        # Add the group as the last param for perceval
+        params.append(url)
+
+        return params
