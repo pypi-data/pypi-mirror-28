0e8584bb2705991cf6e8d9d506b9de0dfd84b064
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:57 +0000

    Add package django-connectwise/0.0.101a0/django-connectwise-0.0.101a0.tar.gz

---
+++ packages/django-connectwise/django-connectwise-0.0.101a0.tar.gz/django-connectwise-0.0.101a0/djconnectwise/management/commands/list_callbacks.py
@@ -0,0 +1,26 @@
+from django.core.management.base import BaseCommand, CommandError
+from djconnectwise.callback import TicketCallBackHandler
+from djconnectwise.api import ConnectWiseAPIError
+
+
+class Command(BaseCommand):
+    help = 'Lists existing callbacks on target ConnectWise system.'
+
+    def handle(self, *args, **options):
+        handler = TicketCallBackHandler()
+        self.stdout.write('Callback List')
+        self.stdout.write('-----------------------------------------')
+
+        try:
+            callbacks = handler.get_callbacks()
+        except ConnectWiseAPIError as e:
+            raise CommandError(e)
+
+        for c in callbacks:
+            self.stdout.write('ID: {}'.format(c['id']))
+            self.stdout.write('URL: {}'.format(c['url']))
+            self.stdout.write('DESCRIPTION: {}'.format(
+                c.get('description', ''))
+            )
+            self.stdout.write('TYPE: {}'.format(c['type']))
+            self.stdout.write('-----------------------------------------')
