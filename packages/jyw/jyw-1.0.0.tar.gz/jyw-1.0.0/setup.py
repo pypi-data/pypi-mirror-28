7066936424151b41662e5f74d1ea3350ede7145a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:26 +0000

    Add package jyw/1.0.0/jyw-1.0.0.tar.gz

---
+++ packages/jyw/jyw-1.0.0.tar.gz/jyw-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name='jyw',
+        version='1.0.0',
+        py_modules=['jyw'],
+        author='j',
+        author_email='jiangyiwei@gmail.com',
+        url='',
+        description='An apple for eye',
+    )
