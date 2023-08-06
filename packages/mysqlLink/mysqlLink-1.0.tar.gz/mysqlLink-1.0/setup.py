5641de2c7ca15e59d682f559411722128de55dfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:22 +0000

    Add package mysqlLink/1.0/mysqlLink-1.0.tar.gz

---
+++ packages/mysqlLink/mysqlLink-1.0.tar.gz/mysqlLink-1.0/setup.py
@@ -0,0 +1,13 @@
+# -*- coding:utf-8 -*-
+# Made by Azrael
+
+from distutils.core import setup
+
+setup(
+    name='mysqlLink',
+    version='1.0',
+    py_modules = ['mysqlLink'],
+    author='Azrael',
+    author_email='yinyuhang1996@gmail.com',
+    description="a tool for linking mysql with python3.6, base on module pymysql"
+)
