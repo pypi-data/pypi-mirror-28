513f4e767e2e3488f96de44228c8d90b3e08738b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:02 +0000

    Add package worms/0.1.1/worms-0.1.1.tar.gz

---
+++ packages/worms/worms-0.1.1.tar.gz/worms-0.1.1/worms/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for worms."""
+
+__author__ = """Will Sheffler"""
+__email__ = 'willsheffler@gmail.com'
+__version__ = '0.1.1'
+
+from .worms import *
