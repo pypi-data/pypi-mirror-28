b6b2264fae6566f7f3cd07d12bea618974e7b684
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:59 +0000

    Add package masonite-cli/0.31.0/masonite-cli-0.31.0.tar.gz

---
+++ packages/masonite-cli/masonite-cli-0.31.0.tar.gz/masonite-cli-0.31.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name="masonite-cli",
+    version='0.31.0',
+    py_modules=['craft'],
+    packages=['snippets', 'snippets.auth', 'snippets.auth.controllers', 'snippets.auth.templates.auth'],
+    install_requires=[
+        'Click',
+    ],
+    include_package_data=True,
+    entry_points='''
+        [console_scripts]
+        craft=craft:group
+        craft-vendor=craft:cli
+    ''',
+)
