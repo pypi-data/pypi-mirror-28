bd15ac115f5f8938fb3a16b20d14efc92f5c7130
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:40 +0000

    Add package funniest22222/0.1.1/funniest22222-0.1.1.tar.gz

---
+++ packages/funniest22222/funniest22222-0.1.1.tar.gz/funniest22222-0.1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='funniest22222',
+      version='0.1.1',
+      description='The funniest joke in the world',
+      url='http://github.com/storborg/funniest',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['funniest2223'],
+      zip_safe=False)
