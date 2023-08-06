8e8318dcbe9a27441750851130d32e2c97ea9bf4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:04 +0000

    Add package aloft/0.1.2/aloft-0.1.2.tar.gz

---
+++ packages/aloft/aloft-0.1.2.tar.gz/aloft-0.1.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='aloft',
+      version='0.1.2',
+      description='Tool to manage Kubernetes clusters and helm charts across multiple AWS accounts and clusters',
+      packages=['aloft'],
+      scripts=['bin/aloft'],
+      zip_safe=False,
+      install_requires=['boto3', 'jinja2', 'PyYAML', 'docopt']
+      )
