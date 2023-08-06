a1797708fdf1ed6c9312219982ada56532ae7bbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:28 +0000

    Add package alexkar7_py_helloworld/0.1/alexkar7_py_helloworld-0.1.tar.gz

---
+++ packages/alexkar7_py_helloworld/alexkar7_py_helloworld-0.1.tar.gz/alexkar7_py_helloworld-0.1/setup.py
@@ -0,0 +1,16 @@
+# *** IMPORTS *** #
+from distutils.core import setup
+
+# *** SETUP *** #
+setup(
+	name = 'alexkar7_py_helloworld',
+	packages = ['alexkar7_py_helloworld'],
+	version = '0.1',
+	description = 'A simple Python program greeting you',
+	author = 'Alejandro Carmona Mejia',
+	author_email = 'alejandro.carmonamejia@gmail.com',
+	url = 'https://github.com/alexkar7/alexkar7_py_helloworld',
+	download_url = 'https://github.com/alexkar7/alexkar7_py_helloworld/archive/0.1.tar.gz',
+	keywords = ['testing', 'greetings', 'example'], 
+	classifiers = [],
+)
