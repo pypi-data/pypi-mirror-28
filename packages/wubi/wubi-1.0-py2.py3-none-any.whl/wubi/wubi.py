35cc0c46515dd57fb069c151f4fe70b3f84b6d5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:52:56 +0000

    Add package wubi/1.0/wubi-1.0-py2.py3-none-any.whl

---
+++ packages/wubi/wubi-1.0-py2.py3-none-any.whl/wubi/wubi.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+
+import os
+import itertools
+import unicodedata
+
+import pickle
+
+from _compat import u
+
+__all__ = ['get','data']
+
+
+# init wubi dict
+data = {}
+dat = os.path.join(os.path.dirname(__file__), "wubi.pickle")
+
+try :
+    with open(dat) as f:
+        data = pickle.loads(f.read())
+except:
+    pass
+
+if len(data)<1:
+    from cw import cw
+    from wc import wc
+    data['cw'] = cw
+    data['wc'] = wc
+
+
+def _wubi_generator(chars):
+    """Generate wubi for chars, if char is not chinese character,
+    itself will be returned.
+    Chars must be unicode list.
+    """
+    s = []
+    for char in chars:
+        # handle english in chinese
+        var =['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+        if char ==' ':
+            pass
+        elif char in var :
+                s.append(char)
+                if char==chars[-1]:
+                    yield ''.join(s)
+        else:
+            if len(s)!=0:
+                yield ''.join(s)
+                s=[]
+            wubi = data['cw'].get(char)
+            if wubi == None:
+                wubi=char
+            yield wubi
+
+
+def _chinese_generator(chars, delimiter):
+    """Generate chinese for chars, if char is not chinese character,
+    itself will be returned.
+    Chars must be unicode list.
+    """
+    for char in chars.split(delimiter):
+        chinese = data['wc'].get(char)
+        if chinese ==None:
+                chinese=char
+        yield chinese
+
+def get(s,type='',delimiter=' '):
+    """Return wubi of string, the string must be unicode
+    """
+    #pre process input string
+
+
+    if type=='cw':
+        return delimiter.join(_wubi_generator(u(s)))
+    elif type =='wc':
+        return ''.join(_chinese_generator(u(s),delimiter))
+    else:
+        return None
