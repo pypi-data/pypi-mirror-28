93b45faf33fcfbaebc4458a852648da7e451f41c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:15 +0000

    Add package jpt-django-utils/0.2.1/jpt_django_utils-0.2.1-py2.py3-none-any.whl

---
+++ packages/jpt-django-utils/jpt_django_utils-0.2.1-py2.py3-none-any.whl/jpt_django_utils/auto_version/__init__.py
@@ -0,0 +1,11 @@
+"""Version utils package.
+
+Adds the ability to track a model's 'version' when it's edited and saved.
+
+Examples
+--------
+TODO
+
+"""
+from .models import *  # noqa
+from .fields import *  # noqa
