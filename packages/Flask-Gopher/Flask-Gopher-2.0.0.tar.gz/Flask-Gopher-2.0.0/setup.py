d6dc815ddeecbc5b5946d4c198e1b55e105d1d29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:19 +0000

    Add package Flask-Gopher/2.0.0/Flask-Gopher-2.0.0.tar.gz

---
+++ packages/Flask-Gopher/Flask-Gopher-2.0.0.tar.gz/Flask-Gopher-2.0.0/setup.py
@@ -0,0 +1,46 @@
+"""
+Flask-Gopher
+------------
+
+A Flask extension to support the Gopher Protocol
+"""
+import codecs
+import setuptools
+from version import __version__ as version
+
+
+def long_description():
+    with codecs.open('README.md', encoding='utf8') as f:
+        return f.read()
+
+
+setuptools.setup(
+    name='Flask-Gopher',
+    version=version,
+    url='https://github.com/michael-lazar/flask-gopher',
+    license='GPL-3.0',
+    author='Michael Lazar',
+    author_email='lazar.michael22@gmail.com',
+    description='A Flask extension to support the Gopher Protocol',
+    long_description=long_description(),
+    packages=['flask_gopher'],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'Flask',
+        'pyfiglet',
+        'tabulate'
+    ],
+    test_suite='tests',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
