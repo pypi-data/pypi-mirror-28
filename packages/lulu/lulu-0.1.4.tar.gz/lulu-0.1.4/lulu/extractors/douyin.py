1e9566c52a394c12466ab664eccc2b497bf713d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:39 +0000

    Add package lulu/0.1.4/lulu-0.1.4.tar.gz

---
+++ packages/lulu/lulu-0.1.4.tar.gz/lulu-0.1.4/lulu/extractors/douyin.py
@@ -0,0 +1,28 @@
+# coding=utf-8
+
+import re
+import json
+
+from lulu.common import (
+    get_content,
+    playlist_not_supported,
+)
+from lulu.extractor import SimpleExtractor
+
+
+__all__ = ['douyin_download_by_url']
+site_info = 'douyin'
+
+
+class DouYin(SimpleExtractor):
+    def extract(self, url, **kwargs):
+        self.site_info = site_info
+        page_content = get_content(url)
+        match_rule = re.compile(r'var data = \[(.*?)\];')
+        video_info = json.loads(match_rule.findall(page_content)[0])
+        self.urls = [video_info['video']['play_addr']['url_list'][0]]
+        self.title = video_info['cha_list'][0]['cha_name']
+
+
+download = douyin_download_by_url = DouYin()
+download_playlist = playlist_not_supported(site_info)
