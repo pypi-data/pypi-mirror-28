c78a7cd7acc4bf8059ad530f4547253c66f7a1f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:33 +0000

    Add package blackbelt/0.15.4/blackbelt-0.15.4.tar.gz

---
+++ packages/blackbelt/blackbelt-0.15.4.tar.gz/blackbelt-0.15.4/blackbelt/deployment.py
@@ -0,0 +1,19 @@
+from subprocess import check_call
+
+from blackbelt.handle_github import get_current_branch
+from blackbelt.messages import post_message
+
+
+def deploy_staging():
+    branch_name = get_current_branch()
+
+    post_message("Deploying branch %s to staging" % branch_name, "#deploy-queue")
+
+    check_call(['grunt', 'deploy', '--app=apiary-staging', '--force', "--branch=%s" % branch_name])
+
+def deploy_production():
+    post_message("Deploying to production", "#deploy-queue")
+
+    check_call(['grunt', 'deploy'])    
+    check_call(['grunt', 'deploy', '--app=apiary-staging-pre'])
+    check_call(['grunt', 'deploy', '--app=apiary-staging-qa'])
