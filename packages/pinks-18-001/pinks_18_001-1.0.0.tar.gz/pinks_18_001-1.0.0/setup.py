607230b8dc7531d60c7e2f2bb8503d154c540b39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:17 +0000

    Add package pinks-18-001/1.0.0/pinks_18_001-1.0.0.tar.gz

---
+++ packages/pinks-18-001/pinks_18_001-1.0.0.tar.gz/pinks_18_001-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+
+setup(
+        name             ='pinks_18_001',
+        version          ='1.0.0',
+        py_modules       =['pinks_18_001'],
+        author           ='pinkstone',
+        author_email     ='gzl_stone@hotmail.com',
+        description      ='A simple printer of nested lists',
+        )
+        
