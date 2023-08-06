535a9031cbd6045fa8abdf2d5bceb92b458dd5a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:56 +0000

    Add package django-lti-provider/0.3.1/django-lti-provider-0.3.1.tar.gz

---
+++ packages/django-lti-provider/django-lti-provider-0.3.1.tar.gz/django-lti-provider-0.3.1/lti_provider/models.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.models import Group
+from django.db import models
+
+
+class LTICourseContext(models.Model):
+    group = models.ForeignKey(Group,
+                              related_name='course_group',
+                              on_delete=models.CASCADE)
+    faculty_group = models.ForeignKey(Group,
+                                      related_name='course_faculty_group',
+                                      on_delete=models.CASCADE)
+    lms_course_context = models.TextField(null=True, unique=True)
+
+    class Meta:
+        unique_together = (('group', 'faculty_group'),)
