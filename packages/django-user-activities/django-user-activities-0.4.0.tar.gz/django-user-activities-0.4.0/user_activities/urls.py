1dda753f754072a8a1bee7d65869d75d5b3677d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:23 +0000

    Add package django-user-activities/0.4.0/django-user-activities-0.4.0.tar.gz

---
+++ packages/django-user-activities/django-user-activities-0.4.0.tar.gz/django-user-activities-0.4.0/user_activities/urls.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from rest_framework import routers
+
+from . import viewsets
+
+
+router = routers.SimpleRouter()
+router.register(r'', viewsets.ActivityViewSet)
+router.register(r'comments', viewsets.CommentViewSet)
+router.register(r'reviews', viewsets.ReviewViewSet)
+
+default_router = routers.DefaultRouter()
+default_router.register(r'user-activities', viewsets.ActivityViewSet)
+default_router.register(r'comments', viewsets.CommentViewSet)
+default_router.register(r'reviews', viewsets.ReviewViewSet)
+
+urlpatterns = default_router.urls
