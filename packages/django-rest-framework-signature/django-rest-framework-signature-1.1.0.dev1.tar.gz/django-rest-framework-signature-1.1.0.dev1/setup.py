d65f5b8675110ebe5c90b7bf467ce72422efb4e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:01 +0000

    Add package django-rest-framework-signature/1.1.0.dev1/django-rest-framework-signature-1.1.0.dev1.tar.gz

---
+++ packages/django-rest-framework-signature/django-rest-framework-signature-1.1.0.dev1.tar.gz/django-rest-framework-signature-1.1.0.dev1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+
+setup(
+    name='django-rest-framework-signature',
+    version='1.1.0.dev1',
+    description='Django Rest Framework Signature Authentication',
+    author='Skyler Cain',
+    author_email='skylercain@gmail.com',
+    url='https://github.com/Skylude/django-rest-framework-signature',
+    packages=['rest_framework_signature'],
+    keywords='django rest_framework authentication api_key signature'
+)
