685f6158d9f9bd96a7fb7b26bb08dc9a02dfa9f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:32 +0000

    Add package oneoppa/1.0.0/oneoppa-1.0.0.tar.gz

---
+++ packages/oneoppa/oneoppa-1.0.0.tar.gz/oneoppa-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+       name        = 'oneoppa',
+       version     = '1.0.0',
+       py_modules  = ['oneoppa'],
+       author      = 'xpx1260',
+       author_email = 'xpx1260@163.com',
+       url          = 'http://email.163.com',
+       description ='A simple printer of nested lists',
+
+     )
