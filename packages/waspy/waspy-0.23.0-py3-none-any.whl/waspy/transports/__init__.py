85db73793f0e99535f0a8dd70ebe6a93b50d81a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:30 +0000

    Add package waspy/0.23.0/waspy-0.23.0-py3-none-any.whl

---
+++ packages/waspy/waspy-0.23.0-py3-none-any.whl/waspy/transports/__init__.py
@@ -0,0 +1,7 @@
+from .httptoolstransport import HTTPTransport as HTTPToolsTransport
+from .httptoolstransport import HTTPTransport, HTTPClientTransport
+from .rabbitmqtransport import RabbitMQTransport, RabbitMQClientTransport
+from .transportabc import TransportABC
+from .testtransport import TestTransport
+
+
