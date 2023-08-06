6ad8efe70760b00c37747a37b491d02b8a6b82c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:11 +0000

    Add package modoboa-contacts/0.5.0/modoboa_contacts-0.5.0-py2.py3-none-any.whl

---
+++ packages/modoboa-contacts/modoboa_contacts-0.5.0-py2.py3-none-any.whl/modoboa_contacts/settings.py
@@ -0,0 +1,26 @@
+"""Default Contacts settings."""
+
+import os
+
+PLUGIN_BASE_DIR = os.path.dirname(__file__)
+
+CONTACTS_STATS_FILES = {
+    "dev": os.path.join(
+        PLUGIN_BASE_DIR, "../frontend/webpack-stats.json"),
+    "prod": os.path.join(
+        PLUGIN_BASE_DIR, "static/modoboa_contacts/webpack-stats.json")
+}
+
+
+def apply(settings):
+    """Modify settings."""
+    DEBUG = settings['DEBUG']
+    settings["INSTALLED_APPS"] += ("webpack_loader", )
+    settings["WEBPACK_LOADER"] = {
+        'CONTACTS': {
+            'CACHE': not DEBUG,
+            'BUNDLE_DIR_NAME': 'modoboa_contacts/',
+            'STATS_FILE': CONTACTS_STATS_FILES.get("dev" if DEBUG else "prod"),
+            'IGNORE': [r'.+\.hot-update.js', r'.+\.map']
+        }
+    }
