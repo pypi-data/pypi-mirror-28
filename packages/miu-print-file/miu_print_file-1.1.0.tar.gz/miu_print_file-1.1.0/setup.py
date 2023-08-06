e347198c9e07c8812f6ca51b6031b51d780f2406
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:28 +0000

    Add package miu-print-file/1.1.0/miu_print_file-1.1.0.tar.gz

---
+++ packages/miu-print-file/miu_print_file-1.1.0.tar.gz/miu_print_file-1.1.0/setup.py
@@ -0,0 +1,12 @@
+
+from distutils.core import setup
+
+setup(
+    name = 'miu_print_file',      
+    version = '1.1.0',
+    py_modules = ['print'],
+    author = 'Lanvy_miu',        
+    author_email = '303262440@qq.com',
+    url = 'https://www.python.org/',
+    description = 'print'   
+    )
