d9f1b89e8f1047c653064d6156d81848447e0a19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:04 +0000

    Add package qexpy/1.0.3/qexpy-1.0.3.tar.gz

---
+++ packages/qexpy/qexpy-1.0.3.tar.gz/qexpy-1.0.3/setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+
+setup(
+    name='qexpy',
+    packages=['qexpy'],
+    version='1.0.3',
+    description='''Package to handle error analysis and data plotting aimed
+    at undergraduate physics.''',
+    author='Connor Kapahi and Prof. Ryan Martin',
+    author_email='ryan.martin@queensu.ca',
+    license='GNU GLP v3',
+    url='https://github.com/Queens-Physics/QExPy',
+    download_url='https://github.com/Queens-Physics/QExPy/tarball/0.3.7',
+    keywords=['physics', 'laboratories', 'labs', 'undergraduate',
+              'data analysis', 'uncertainties', 'plotting', 'error analysis',
+              'error propagation', 'uncertainty propagation'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Physics',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Programming Language :: Python',
+    ],
+    install_requires=['numpy','matplotlib', 'ipywidgets', 'scipy>=0.17', 'bokeh>=0.12.1', 'pandas'],
+)
