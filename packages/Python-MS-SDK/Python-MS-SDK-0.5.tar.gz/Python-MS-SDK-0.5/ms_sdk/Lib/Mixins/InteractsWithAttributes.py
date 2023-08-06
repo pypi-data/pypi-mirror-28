0f48759d9791972a442a5eef660d26f59c103faf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:03 +0000

    Add package Python-MS-SDK/0.5/Python-MS-SDK-0.5.tar.gz

---
+++ packages/Python-MS-SDK/Python-MS-SDK-0.5.tar.gz/Python-MS-SDK-0.5/ms_sdk/Lib/Mixins/InteractsWithAttributes.py
@@ -0,0 +1,83 @@
+from ms_sdk.Entities.Attribute import Attribute
+from ms_sdk.Entities.AttributeValue import AttributeValue
+
+
+class InteractsWithAttributesMixin:
+
+    def getAttributeById(self, id):
+        """
+        :param id:
+        :return None|Attribute:
+        """
+        return self.getAttributeByFieldNameAndValue('id', id)
+
+    def getAttributesByIds(self, ids):
+        """
+        :param ids:
+        :return dict|Attribute{}:
+        """
+        return self.getAttributesByFieldNameAndValues('id', ids)
+
+    def getAttributeByCode(self, code):
+        """
+        :param code:
+        :return None|Attribute:
+        """
+        return self.getAttributeByFieldNameAndValue('code', code)
+
+    def getAttributesByCodes(self, codes):
+        """
+        :param codes:
+        :return dict|Attribute{}:
+        """
+        return self.getAttributesByFieldNameAndValues('code', codes)
+
+    def getFirstValueByAttributeCode(self, code):
+        """
+        :param code:
+        :return None|AttributeValue:
+        """
+        attribute = self.getAttributeByCode(code)
+
+        try:
+            values = attribute.values[0]
+        except:
+            values = attribute.values
+
+        if not values:
+            return None
+
+        return values
+
+    def getAttributeByFieldNameAndValue(self, fieldName, value):
+        """
+        :param string fieldName:
+        :param value:
+        :return None|Attribute:
+        """
+        if not self.attributes:
+            return None
+
+        for attribute in self.attributes:
+
+            if eval('attribute.' + fieldName) == value:
+                return attribute
+
+        return None
+
+    def getAttributesByFieldNameAndValues(self, fieldName, value):
+        """
+        :param string fieldName:
+        :param value:
+        :return Attribute{}:
+        """
+        attributes = []
+
+        if not self.attributes or not self.attributes:
+            return attributes
+
+        for attribute in self.attributes:
+            if (eval('attribute.' + fieldName) in value) or (eval('attribute.' + fieldName) == value):
+                attributes.append(attribute)
+
+        return attributes
---
+++ packages/Python-MS-SDK/Python-MS-SDK-0.5.tar.gz/Python-MS-SDK-0.5/ms_sdk/__init__.py
@@ -0,0 +1,3 @@
+__version__ = '0.5'
+
+from ms_sdk.ms_sdk import Client
