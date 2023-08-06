69560e7ff8322d53121910bd2ae1d3e0b480739a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:44 +0000

    Add package xy-scene-object/1.0.0/xy_scene_object-1.0.0.tar.gz

---
+++ packages/xy-scene-object/xy_scene_object-1.0.0.tar.gz/xy_scene_object-1.0.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'xy_scene_object',
+  packages = ['xy_scene_object'],
+  version = '1.0.0',
+  description = 'Scene Object Recognition',
+  long_description='Scene Object Recognition',
+  author = 'KingHS',
+  author_email = '382771946@qq.com',
+  keywords = ['pip3', 'python3','python','object','scene object recognition'],
+  license='MIT',
+  install_requires=['requests']
+)
