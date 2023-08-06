12c59ef87f08d16d5f7b3b5d21507a7d57902116
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:11 +0000

    Add package django-bootstrap-pagination/1.6.4/django-bootstrap-pagination-1.6.4.tar.gz

---
+++ packages/django-bootstrap-pagination/django-bootstrap-pagination-1.6.4.tar.gz/django-bootstrap-pagination-1.6.4/setup.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+from setuptools import find_packages
+
+
+with open('README.rst', 'rb') as readme:
+    readme_text = readme.read().decode('utf-8')
+
+setup(
+    name='django-bootstrap-pagination',
+    version='1.6.4',
+    keywords="django bootstrap pagination templatetag",
+    author=u'Jason McClellan',
+    author_email='jason@jasonmccllelan.net',
+    packages=find_packages(),
+    url='https://github.com/jmcclell/django-bootstrap-pagination',
+    license='MIT licence, see LICENCE',
+    description='Render Django Page objects as Bootstrap 3.x Pagination compatible HTML',
+    long_description=readme_text,
+    zip_safe=False,
+    include_package_data=True,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Framework :: Django",
+        "Framework :: Django :: 1.4",
+        "Framework :: Django :: 1.5",
+        "Framework :: Django :: 1.6",
+        "Framework :: Django :: 1.7",
+        "Framework :: Django :: 1.8",
+        "Framework :: Django :: 1.9",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+    ]
+)
