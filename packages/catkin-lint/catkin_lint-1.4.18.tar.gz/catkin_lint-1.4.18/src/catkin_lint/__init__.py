9006d257d1de2fd6c61742f9a73049fa788d30aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:29 +0000

    Add package catkin-lint/1.4.18/catkin_lint-1.4.18.tar.gz

---
+++ packages/catkin-lint/catkin_lint-1.4.18.tar.gz/catkin_lint-1.4.18/src/catkin_lint/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+__author__ = "Timo Röhling <timo.roehling@fkie.fraunhofer.de>"
+__copyright__ = "Copyright (c) 2013-2016 Fraunhofer FKIE"
+__license__ = "BSD"
+__version__ = "1.4.18"
+__all__ = ["cmake", "diagnostics", "linter", "environment", "main", "output", "ros", "util"]
