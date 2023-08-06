eeae840d58d2367e7d3926b24a05aa2c1b1217f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:58 +0000

    Add package pysweep2/0.12/pysweep2-0.12-py3-none-any.whl

---
+++ packages/pysweep2/pysweep2-0.12-py3-none-any.whl/pysweep/data_storage/__init__.py
@@ -0,0 +1,6 @@
+from pysweep.data_storage.spyview import SpyviewStorage
+from pysweep.data_storage.base_storage import BaseStorage, Delayed, DataSet
+from pysweep.data_storage.pandas_storage import PandasStorage
+from pysweep.data_storage.webui_writer import WebUIWriter
+
+
---
+++ packages/pysweep2/pysweep2-0.12-py3-none-any.whl/pysweep/data_storage/json.py
@@ -0,0 +1,42 @@
+"""
+Initial work on the JSON storage class. Note that for now we are accumulating data in a dictionary without writing
+anything to file yet
+"""
+
+from pysweep.data_storage.base_storage import BaseDataSet
+from pysweep.utils import DictMerge
+
+
+class JSONDataSet(BaseDataSet, DictMerge):
+    def __init__(self, **strategy):
+        super().__init__(**strategy)
+        self.buffer = []
+        self._unique_names = set()
+        self.snapshot = None
+
+    def add(self, dictionary):
+
+        for key in dictionary.keys():
+            self._unique_names.add(key)
+
+        self.buffer.append(dictionary)
+
+    def output(self, name):
+
+        if name not in self._unique_names:
+            raise ValueError("Parameter {} unknown".format(name))
+
+        d = dict()
+        for ibuffer in self.buffer:
+            if name in ibuffer:
+                d = self.merge([ibuffer, d])
+        return d
+
+    def keys(self):
+        return list(self._unique_names)
+
+    def finalize(self):
+        pass  # TODO: Left off here
+
+    def save_json_snapshot(self, snapshot):
+        self.snapshot = snapshot
