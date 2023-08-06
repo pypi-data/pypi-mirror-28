631fab6eb374e3d2be8583f94dc5fb95f3ac5f92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:20 +0000

    Add package nester2701/1.5.0/nester2701-1.5.0.tar.gz

---
+++ packages/nester2701/nester2701-1.5.0.tar.gz/nester2701-1.5.0/nester2701.py
@@ -0,0 +1,14 @@
+import sys
+
+def print_lol(the_list, indent=False, level=0, data_file=sys.stdout):
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol(each_item, indent, level+1, data_file)
+        else:
+            if indent:
+                for tab_space in range(level):
+                    print('\t', end='', file=data_file) #aqui só imprime o tab
+            print(each_item, file=data_file)
+            
+lista = [1,2,3,4,[4,5,56,[6,7,8]]]
+print_lol(lista, True)
