04ac886fcd55f8e8e01fbb65f8df947c57af9f10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:41 +0000

    Add package former/0.2.1/former-0.2.1-py2.py3-none-any.whl

---
+++ packages/former/former-0.2.1-py2.py3-none-any.whl/former/cli.py
@@ -0,0 +1,42 @@
+import argparse
+import json
+import sys
+
+import yaml
+
+import former
+from former import __version__
+from former.resource import Resource
+
+
+def arguments():
+    parser = argparse.ArgumentParser(description='Print CloudFormation Resources')
+    parser.add_argument('--version', action='version', version='{}'.format(__version__))
+    parser.add_argument('service')
+    parser.add_argument('type')
+    parser.add_argument('subtype', default='', nargs='?')
+    parser.add_argument('--json', action='store_true')
+    parser.add_argument('--required', '-r', action='store_true')
+
+    return parser.parse_args()
+
+
+def main():
+    args = arguments()
+
+    type = former.resource.type_key(args.service, args.type, args.subtype)
+    if type:
+        resource = Resource(type)
+        cf_resource = {'Type': type}
+
+        cf_resource['Properties'] = resource.parameters(args.required)
+
+        data = {'Resources': {''.join(e for e in type if e.isalnum()): cf_resource}}
+        if args.json:
+            output = json.dumps(data, indent=2)
+        else:
+            output = yaml.safe_dump(data, allow_unicode=True, default_flow_style=False)
+        print(output)
+    else:
+        print('Resource not found for: {} {} {}'.format(args.service, args.type, args.subtype))
+        sys.exit(1)
