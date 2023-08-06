e1567d69dd96a5c34492631115a886136fa21183
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:04 +0000

    Add package GenomeQAML/0.0.2/GenomeQAML-0.0.2.tar.gz

---
+++ packages/GenomeQAML/GenomeQAML-0.0.2.tar.gz/GenomeQAML-0.0.2/README.md
@@ -0,0 +1,4 @@
+[![Build status](https://travis-ci.org/OLC-LOC-Bioinformatics/GenomeQAML.svg?master)](https://travis-ci.org/OLC-LOC-Bioinformatics)
+# GenomeQAML: Genome Quality Assesment with Machine Learning
+
+Project to assess FASTA draft genome assemblies for quality using machine learning. More info coming soon... 
---
+++ packages/GenomeQAML/GenomeQAML-0.0.2.tar.gz/GenomeQAML-0.0.2/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+from setuptools import setup
+__author__ = 'adamkoziol'
+setup(
+    name="GenomeQAML",
+    version="0.0.2",
+    package_data={'': ['*.msh', '*.p']},
+    include_package_data=True,
+    license='MIT',
+    scripts=['genomeqaml/classify.py'],
+    author='OLC Bioinformatics',
+    author_email='adam.koziol@inspection.gc.ca',
+    description='CFIA OLC Genome Quality Assessment with Machine Learning',
+    url='https://github.com/OLC-LOC-Bioinformatics/GenomeQAML',
+    long_description=open('README.md').read(),
+    install_requires=[
+        'click',
+        'biopython'
+    ]
+)
