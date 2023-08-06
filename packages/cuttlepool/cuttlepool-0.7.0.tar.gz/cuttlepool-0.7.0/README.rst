4fc033cf0c873d3d68270bcd6f68e151ce45f1c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:55 +0000

    Add package cuttlepool/0.7.0/cuttlepool-0.7.0.tar.gz

---
+++ packages/cuttlepool/cuttlepool-0.7.0.tar.gz/cuttlepool-0.7.0/README.rst
@@ -0,0 +1,147 @@
+###########
+Cuttle Pool
+###########
+
+Cuttle Pool is a SQL connection pooling implementation. It's key features are:
+
+Pool overflow
+   Creates additional connections if the pool capacity has been reached and
+   will remove the overflow when demand for connections decreases.
+   
+Connection harvesting
+   Any connections that haven't been returned to the pool and are no longer
+   referenced by anything outside the pool are returned to the pool. This helps
+   prevent pool depletion when connections aren't explicitly returned to the
+   pool and the connection wrapper is garbage collected.
+
+Connection queuing
+   If all else fails and no connection can be immediately found or made, the
+   pool will wait a specified amount of time for a connection to be returned
+   to the pool before raising an exception.
+
+How-to Guide
+============
+
+Using Cuttle Pool requires subclassing a ``CuttlePool`` object with a user
+defined method ``normalize_connection()`` and ``ping()``. ::
+
+  >>> import sqlite3
+  >>> from cuttlepool import CuttlePool
+  >>> class SQLitePool(CuttlePool):
+  ...     def normalize_connection(self, connection):
+  ...         connection.row_factory = None
+  ...     def ping(self, connection):
+  ...         try:
+  ...             rv = connection.execute('SELECT 1').fetchall()
+  ...             return (1,) in rv
+  ...         except sqlite3.Error:
+  ...             return False
+  >>> pool = SQLitePool(sqlite3.connect, database='ricks_lab')
+
+Let's break this down line by line.
+
+First, the ``sqlite3`` module is imported. ``sqlite3`` will be the underlying
+driver.
+
+``CuttlePool`` is imported and subclassed. The ``normalize_connection()``
+method takes a ``Connection`` object as a parameter and changes it's
+properties. This is important because a ``Connection`` object can be modified
+while it's outside of the pool and any modifications made during that time will
+persist; this can have unintended consequences when the ``Connection`` object
+is later retrieved from the pool.
+
+Next the ``ping()`` method is implemented, which also takes a ``Connection``
+object as a parameter. ``ping()`` ensures the connection is open; if the
+connection is open, return ``True`` else return ``False``. In the above
+example, a simple statement is executed and if the expected result is returned,
+it means the connection is open and ``True`` is returned. The implementation of
+this method is really dependent on which driver is being used. If ``pymysql``
+was used, the implementation might look like this::
+
+  def ping(self, connection):
+      if not connection.open:
+          try:
+              connection.ping()
+          except pymysql.err.Error:
+              pass
+      return connection.open
+
+Not every driver has the same API, so it's up to the user to implement a
+``ping()`` method that works for the chosen driver.
+
+Finally an instance of ``SQLitePool`` is made. The ``sqlite3.connect`` method is
+passed to the instance along with the database name. The first argument must be
+the ``connect`` method of the sql driver.
+
+The ``CuttlePool`` object and as a result the ``SQLitePool`` object accepts any
+parameters that the underlying SQL driver accepts. There are three other
+parameters the pool object accepts that are unrelated to the SQL driver.
+``capacity`` sets the max number of connections the pool will hold at any given
+time. ``overflow`` sets the max number of additional connections the pool will
+create when depleted. All overflow connections will be closed when returned to
+the pool if the pool is at capacity. ``timeout`` sets the amount of time in
+seconds the pool will wait for a connection to become free if the pool is
+depleted when a request for a connection is made.
+
+A connection from the pool can be used the same way a connection object of the
+underlying driver is used. ::
+
+  >>> con = pool.get_connection()
+  >>> cur = con.cursor()
+  >>> cur.execute('INSERT INTO garage (invention_name, state) '
+                  'VALUES (%s, %s)', ('Space Cruiser', 'damaged'))
+  >>> cur.close()
+  >>> con.close()
+
+Calling ``close()`` on the connection returns it to the pool instead of closing
+it.
+
+.. note::
+   Once ``close()`` is called on the connection object, it renders the
+   object useless. The connection object received from the pool is a wrapper
+   around the actual connection object and calling ``close()`` on it returns
+   the connection to the pool and removes it from the wrapper effectively
+   leaving it an empty shell to be garbage collected.
+
+FAQ
+===
+
+How do I install it?
+--------------------
+
+``pip install cuttlepool``
+
+What SQL implementations does Cuttle Pool support?
+--------------------------------------------------
+
+It supports whatever SQL driver that is passed to it.
+
+Contributing
+------------
+
+It's highly recommended to develop in a virtualenv.
+
+Fork the repository.
+
+Clone the repository::
+
+  git clone https://github.com/<your_username>/cuttlepool.git
+
+Install the package in editable mode::
+
+  cd cuttlepool
+  pip install -e .[dev]
+
+Now you're set. See the next section for running tests.
+
+Running the tests
+-----------------
+
+Tests can be run with the command ``pytest``.
+
+Where can I get help?
+---------------------
+
+If you haven't read the How-to guide above, please do that first. Otherwise,
+check the `issue tracker <https://github.com/smitchell556/cuttlepool/issues>`_.
+Your issue may be addressed there and if it isn't please file an issue :)
---
+++ packages/cuttlepool/cuttlepool-0.7.0.tar.gz/cuttlepool-0.7.0/tests/test_cuttlepool.py
@@ -0,0 +1,253 @@
+# -*- coding: utf-8 -*-
+"""
+CuttlePool tests.
+"""
+import threading
+import time
+
+import pytest
+
+from cuttlepool import CuttlePool, PoolConnection, PoolDepletedError
+import mocksql
+
+
+class MockPool(CuttlePool):
+    def normalize_connection(self, connection):
+        pass
+
+    def ping(self, connection):
+        return connection.open
+
+
+class SubConnection(PoolConnection):
+    pass
+
+
+@pytest.fixture()
+def pool():
+    """A CuttlePool object."""
+    p = MockPool(mocksql.connect, capacity=5, overflow=1)
+    yield p
+    p.empty_pool()
+
+
+@pytest.fixture()
+def connection(pool):
+    """A PoolConnection object."""
+    c = pool.get_connection()
+    yield c
+    c.close()
+
+
+def test_connection_attribute_set(pool):
+    """
+    Tests that the _connection attribute is set with the right class after a
+    connection is requested.
+    """
+    assert pool._Connection is None
+    pool.get_connection()
+    assert pool._Connection is mocksql.MockConnection
+
+
+def test_nonpositive_capacity():
+    """Tests error is raised when nonpositive capacity is specified."""
+    with pytest.raises(ValueError):
+        MockPool(mocksql.connect, capacity=0)
+
+
+def test_negative_overflow():
+    """Tests error is raised when negative overflow is specified."""
+    with pytest.raises(ValueError):
+        MockPool(mocksql.connect, overflow=-1)
+
+
+def test_improper_timeout():
+    """Tests error is raised for improper timeout argument."""
+    with pytest.raises(ValueError):
+        MockPool(mocksql.connect, timeout=-1)
+
+    with pytest.raises(TypeError):
+        MockPool(mocksql.connect, timeout=-0.1)
+
+
+def test_connection_wrapper():
+    """
+    Tests the proper PoolConnection subclass is returned from
+    ``get_connection()``.
+    """
+    pool = MockPool(mocksql.connect, connection_wrapper=SubConnection)
+    con = pool.get_connection()
+    assert isinstance(con, SubConnection)
+
+
+def test_connection_wrapper_get_connection():
+    """
+    Tests the proper PoolConnection subclass is returned from
+    ``get_connection()``.
+    """
+    pool = MockPool(mocksql.connect)
+    con = pool.get_connection(connection_wrapper=SubConnection)
+    assert isinstance(con, SubConnection)
+
+
+def test_make_connection(pool):
+    """
+    Tests the connection object returned from _make_connection is the
+    proper class instance.
+    """
+    con = pool._make_connection()
+    assert isinstance(con, mocksql.MockConnection)
+
+
+def test_harvest_lost_connections(pool):
+    """Tests unreferenced connections are returned to the pool."""
+    con_id = id(pool.get_connection())
+    pool._harvest_lost_connections()
+    assert con_id == id(pool.get_connection())
+
+
+def test_empty_pool(pool):
+    """Tests empty_pool closes the connections and throws them away."""
+    con = pool.get_connection()
+    pool.empty_pool()
+    assert con.open is False
+    assert pool._size == 0
+
+
+def test_get_connection(pool):
+    """
+    Tests the connection object returned from get_connection is the
+    proper class instance.
+    """
+    con = pool.get_connection()
+    assert isinstance(con, PoolConnection)
+
+
+def test_get_connection_overflow(pool):
+    """
+    Tests the pool creates proper number of overflow connections properly.
+    """
+    cons = []
+    for __ in range(pool._capacity):
+        cons.append(pool.get_connection())
+
+    con = pool.get_connection()
+    assert pool._size == pool._maxsize
+
+    con.close()
+    for con in cons:
+        con.close()
+
+    assert pool._size == pool._pool.qsize() == pool._capacity
+
+
+def test_get_connection_depleted(pool):
+    """Tests the pool will return a connection once one is available."""
+    def worker(pool):
+        con = pool.get_connection()
+        time.sleep(3)
+        con.close()
+
+    for _ in range(pool._maxsize):
+        t = threading.Thread(target=worker, args=(pool,))
+        t.start()
+
+    time.sleep(1)
+    con = pool.get_connection()
+
+
+def test_get_connection_depleted_error():
+    """Tests the pool will raise an error when depleted."""
+    pool = MockPool(mocksql.connect, timeout=1)
+    with pytest.raises(PoolDepletedError):
+        cons = []
+        while True:
+            cons.append(pool.get_connection())
+
+
+def test_normalize_connection():
+    """
+    Tests that the normalize_connection method is properly called on
+    connections returned from get_connection.
+    """
+    class Normalize(MockPool):
+        def normalize_connection(self, connection):
+            setattr(connection, 'one', 1)
+
+    pool = Normalize(mocksql.connect)
+    con = pool.get_connection()
+    con_id = id(con._connection)
+    setattr(con, 'one', 2)
+    assert con.one == 2
+    con.close()
+
+    con2 = pool.get_connection()
+    con2_id = id(con2._connection)
+    assert (con2.one == 1 and con_id == con2_id)
+
+
+def test_ping(pool):
+    """
+    Tests that the ping method is properly called on connections returned
+    from get_connection.
+    """
+    con = pool.get_connection()
+    con_id = id(con._connection)
+    con._connection.close()     # Close the underlying connection object.
+    con.close()                 # Return the connection to the pool.
+
+    # Calling get_connection() should create a new connection object since
+    # the previous one (which is the only one currently in the pool) is not
+    # open.
+    con2 = pool.get_connection()
+    con2_id = id(con2._connection)
+    assert con_id != con2_id
+
+
+def test_put_connection(pool):
+    """
+    Tests that the connection is properly returned to the pool.
+    """
+    con = pool.get_connection()
+    con_id = id(con._connection)
+    assert pool._pool.empty() is True
+
+    pool.put_connection(con._connection)
+    assert pool._pool.qsize() == 1
+    assert id(pool._pool.get_nowait()) == con_id
+
+
+def test_with_poolconnection(pool):
+    """Tests PoolConnection context manager."""
+    with pool.get_connection() as con:
+        assert isinstance(con, PoolConnection)
+
+    assert con._connection is None
+
+    con2 = pool.get_connection()
+
+    with con2:
+        assert isinstance(con2, PoolConnection)
+
+    assert con2._connection is None
+
+
+def test_poolconnection_getattr_setattr(connection):
+    """Tests that attributes are set on the underlying connection object."""
+    connection.one = 1
+    assert connection.one == 1
+    assert 'one' not in connection.__dict__
+    assert connection._connection.one == 1
+    assert 'one' in connection._connection.__dict__
+    assert connection.one == connection._connection.one
+
+
+def test_close(pool):
+    """Tests the close method of a PoolConnection object."""
+    con = pool.get_connection()
+    assert pool._pool.empty() is True
+
+    con.close()
+    assert con._connection is None
+    assert con._pool is None
+    assert pool._pool.qsize() == 1
