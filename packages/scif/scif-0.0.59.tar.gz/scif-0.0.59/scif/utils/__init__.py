e903e4a3c5b2ec2fa5fae1971fe3439a68d21735
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:19:47 +0000

    Add package scif/0.0.59/scif-0.0.59.tar.gz

---
+++ packages/scif/scif-0.0.59.tar.gz/scif-0.0.59/scif/utils/__init__.py
@@ -0,0 +1,16 @@
+from .terminal import (
+    run_command,
+    check_install,
+    get_installdir,
+    get_thumbnail
+)
+from .fileio import (
+    copyfile,
+    mkdir_p,
+    write_file,
+    write_json,
+    which,
+    read_file,
+    read_json,
+    clean_up
+)
