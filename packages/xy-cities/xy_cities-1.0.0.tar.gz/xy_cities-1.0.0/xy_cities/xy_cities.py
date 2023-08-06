e866b5284128b2102604742d289c1709a303ff07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:19:32 +0000

    Add package xy-cities/1.0.0/xy_cities-1.0.0.tar.gz

---
+++ packages/xy-cities/xy_cities-1.0.0.tar.gz/xy_cities-1.0.0/xy_cities/xy_cities.py
@@ -0,0 +1,28 @@
+import urllib.request
+import urllib.parse
+import json
+
+
+APPKEY='631c5a5b9992bd74'
+APPSECRET='eS0iNTaMdI5mgRew8JkrVqUB7mG1MytS'
+API_URL='http://api.jisuapi.com/aqi/city'
+
+'''获取所有城市'''
+def get_cities():
+    data = {}
+    data['appkey'] = APPKEY
+    url_values = urllib.parse.urlencode(data)
+    url = API_URL + '?' + url_values
+    result = urllib.request.urlopen(url)
+    jsonarr = json.loads(result.read())
+    res = jsonarr['result']
+    if res:
+        return res
+    else:
+        return -1
+
+def main():
+    print(get_citys())
+
+if __name__ == '__main__':
+    main()
