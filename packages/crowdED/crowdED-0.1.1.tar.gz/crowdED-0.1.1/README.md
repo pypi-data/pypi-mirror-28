91e36f486230c7c901ea6825b672486399dbbe5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:39 +0000

    Add package crowdED/0.1.1/crowdED-0.1.1.tar.gz

---
+++ packages/crowdED/crowdED-0.1.1.tar.gz/crowdED-0.1.1/README.md
@@ -0,0 +1,13 @@
+Task Optimization Algorithm for Crowdsourcing
+====
+
+CrowdED: Task Assignment Optimization Algorithm in Crowdsourcing
+
+[CrowdApp](https://pedrohserrano.shinyapps.io/crowdapp/) Beta
+
+## Preliminary results
+
+![](reports/21wpt10iter.png)
+![](reports/100ptrain10iter.png)
+![](reports/100tasks10iter.png)
+![](reports/100workers10iter.png)
---
+++ packages/crowdED/crowdED-0.1.1.tar.gz/crowdED-0.1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='crowdED',
+      version='0.1.1',
+      description='This package contains the methods to generate simulated workers and tasks of a crowdsourcing use case',
+      url='https://github.com/pedrohserrano/crowdED',
+      author='Pedro Hernandez Serrano',
+      author_email='p.hernandezserrano@maastrichtuniversity.nl',
+      license='MIT',
+      packages=['crowded'],
+      zip_safe=False)
