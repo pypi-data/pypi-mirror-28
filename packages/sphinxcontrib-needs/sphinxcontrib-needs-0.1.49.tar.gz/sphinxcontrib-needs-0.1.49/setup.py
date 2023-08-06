fc585bf6aca3481780a1631b2f010185f8b9699d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:24 +0000

    Add package sphinxcontrib-needs/0.1.49/sphinxcontrib-needs-0.1.49.tar.gz

---
+++ packages/sphinxcontrib-needs/sphinxcontrib-needs-0.1.49.tar.gz/sphinxcontrib-needs-0.1.49/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+from setuptools import setup, find_packages
+
+requires = ['Sphinx>=1.6', 'six']
+
+setup(
+    name='sphinxcontrib-needs',
+    # If you raise, think about versions in conf.py and needs.py!!!
+    version='0.1.49',
+    url='http://github.com/useblocks/sphinxcontrib-needs',
+    download_url='http://pypi.python.org/pypi/sphinxcontrib-needs',
+    license='BSD',
+    author='team useblocks',
+    author_email='info@useblocks.com',
+    description='Sphinx needs extension for managing needs/requirements and specifications',
+    long_description=open(os.path.join(os.path.dirname(__file__), "README.rst")).read(),
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Documentation',
+        'Topic :: Utilities',
+    ],
+    platforms='any',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=requires,
+    namespace_packages=['sphinxcontrib'],
+)
