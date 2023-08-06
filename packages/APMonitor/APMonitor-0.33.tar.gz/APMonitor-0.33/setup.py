dcd490e14c0f67689d91c99f6364e0e955787a8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:17 +0000

    Add package APMonitor/0.33/APMonitor-0.33.tar.gz

---
+++ packages/APMonitor/APMonitor-0.33.tar.gz/APMonitor-0.33/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'APMonitor',
+    version = '0.33',
+    description = 'Python package to interface with the APMonitor optimization suite',
+    author = 'John Hedengren',
+    author_email = 'john_hedengren@byu.edu',
+    url = 'https://github.com/APMonitor/apm_python', # use the URL to the github repo
+    keywords='DAE optimization MILP MINLP QP NLP MIDO IPOPT',
+    packages=find_packages(),
+    #packages=['APMonitor'],
+    classifiers=[
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
+
