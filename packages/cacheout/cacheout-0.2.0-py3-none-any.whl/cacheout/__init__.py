32aa283187bb366ac0e2886583f637d1b418667d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:24 +0000

    Add package cacheout/0.2.0/cacheout-0.2.0-py3-none-any.whl

---
+++ packages/cacheout/cacheout-0.2.0-py3-none-any.whl/cacheout/__init__.py
@@ -0,0 +1,15 @@
+"""The cacheout package.
+
+Cache library
+"""
+
+from .__pkg__ import (
+    __description__,
+    __url__,
+    __version__,
+    __author__,
+    __email__,
+    __license__
+)
+
+from .cache import Cache, CacheManager
