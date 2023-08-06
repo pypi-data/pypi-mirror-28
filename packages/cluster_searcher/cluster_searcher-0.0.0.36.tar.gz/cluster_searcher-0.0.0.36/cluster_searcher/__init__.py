dee459ecddb873a8062c61e9df8f77188e11a9a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:54 +0000

    Add package cluster_searcher/0.0.0.36/cluster_searcher-0.0.0.36.tar.gz

---
+++ packages/cluster_searcher/cluster_searcher-0.0.0.36.tar.gz/cluster_searcher-0.0.0.36/cluster_searcher/__init__.py
@@ -0,0 +1,13 @@
+__version__ = "0.0.0.36"
+
+
+def __setup():
+    from intermake import MENV
+    
+    MENV.name = "cluster_searcher"
+    MENV.version = __version__
+
+
+__setup()
+
+from cluster_searcher import commands
