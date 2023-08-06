7c5f918f239d9d7256ed642f79a42effabb5856c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:54 +0000

    Add package onetoken/0.1.3/onetoken-0.1.3.tar.gz

---
+++ packages/onetoken/onetoken-0.1.3.tar.gz/onetoken-0.1.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(name='onetoken',
+      author='QbTrade',
+      url='https://github.com/qbtrade/onetoken',
+      author_email='markx914@gmail.com',
+      packages=find_packages(),
+      version='0.1.3',
+      description='OneToken Trade System Python SDK',
+      install_requires=[
+          'requests',
+          'pandas',
+          'arrow',
+          'PyJWT',
+          'PyYAML',
+          'aiohttp',
+      ],
+      zip_safe=False,
+      )
