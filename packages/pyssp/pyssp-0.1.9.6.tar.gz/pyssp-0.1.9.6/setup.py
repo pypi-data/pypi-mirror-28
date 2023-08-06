8ed907d613f997afb3149be90b53aab4f0091f7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:38 +0000

    Add package pyssp/0.1.9.6/pyssp-0.1.9.6.tar.gz

---
+++ packages/pyssp/pyssp-0.1.9.6.tar.gz/pyssp-0.1.9.6/setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+import os
+from setuptools import setup, find_packages
+version = "0.1.9.6"
+README = os.path.join(os.path.dirname(__file__), "README")
+long_description = open(README).read() + '\n\n'
+setup(name="pyssp",
+      version=version,
+      description=('python speech signal processing library for education'),
+      long_description=long_description,
+      classifiers=["Programming Language :: Python",
+                   "Topic :: Scientific/Engineering",
+                   "Intended Audience :: Science/Research"],
+      keywords='scipy, speech processing',
+      author='Shunsuke Aihara',
+      author_email="s.aihara@gmail.scom",
+      url='https://github.com/shunsukeaihara/pyssp',
+      license="The MIT License",
+      packages=find_packages(),
+      namespace_packages=[],
+      install_requires=["numpy", "scipy", "statsmodels", "six"]
+      )
