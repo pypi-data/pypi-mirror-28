500f05cba308c2120ccd63ce576a125f8c306ef6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:46 +0000

    Add package xy-riddle/3.0.0/xy_riddle-3.0.0.tar.gz

---
+++ packages/xy-riddle/xy_riddle-3.0.0.tar.gz/xy_riddle-3.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+  name = 'xy_riddle',
+  packages = ['xy_riddle'],
+  version = '3.0.0',
+  description = 'Get The riddle',
+  long_description='Get The riddle',
+  author = 'KingHS',
+  author_email = '382771946@qq.com',
+  keywords = ['pip3', 'python3','python','riddle'],
+  license='MIT',
+)
