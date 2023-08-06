76e784e820d2b794daa7060d69ab76f0f0024d85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:28 +0000

    Add package text2pdf/0.2/text2pdf-0.2.tar.gz

---
+++ packages/text2pdf/text2pdf-0.2.tar.gz/text2pdf-0.2/setup.py
@@ -0,0 +1,43 @@
+import os
+import re
+import sys
+from setuptools import setup
+
+
+if sys.hexversion < 0x3040000:
+    msg = "Python version %s is unsupported, >= 3.4.0 is needed"
+    print(msg % (".".join(map(str, sys.version_info[:3]))))
+    exit(1)
+
+
+# with open("README.md", "rt") as f:
+#     readme = f.read()
+
+
+setup(name='text2pdf',
+      version='0.2',
+      description='Tools to convert text files to PDF',
+      long_description='',
+      url='https://github.com/tedlaz/text2pdf',
+      keywords=["PDF", "gui", "pyqt5"],
+      author='Ted Lazaros',
+      author_email='tedlaz@gmail.com',
+      install_requires=['PyQt5'],
+      license='GPLv3',
+      packages=['text2pdf'],
+      scripts=['text2pdf/text2pdf', 'text2pdf/text2pdfgui'],
+      package_data={'text2pdf': ['templates/images/*.png', 'templates/*.*']},
+      classifiers=["Development Status :: 4 - Beta",
+                   "Environment :: Console",
+                   "Environment :: X11 Applications :: Qt",
+                   "Environment :: Win32 (MS Windows)",
+                   "Intended Audience :: Developers",
+                   "Natural Language :: English",
+                   "Operating System :: OS Independent",
+                   "Programming Language :: Python",
+                   "Programming Language :: Python :: 3",
+                   "Programming Language :: Python :: 3 :: Only",
+                   "Programming Language :: Python :: 3.5",
+                   "Programming Language :: Python :: 3.6",
+                   "Topic :: Utilities"]
+      )
---
+++ packages/text2pdf/text2pdf-0.2.tar.gz/text2pdf-0.2/text2pdf/__init__.py
@@ -0,0 +1,1 @@
+"""Create PDF from text file"""
