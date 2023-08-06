55edae0ab157f8748535f7b6af955f8787779a3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:19:28 +0000

    Add package zeugma/0.15/zeugma-0.15.tar.gz

---
+++ packages/zeugma/zeugma-0.15.tar.gz/zeugma-0.15/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='zeugma',
+      packages=['zeugma'],
+      version='0.15',
+      license='MIT',
+      description='Unified framework for word embeddings (Word2Vec, GloVe, FastText, ...) compatible with scikit-learn Pipeline',
+      author='Nicolas Thiebaut',
+      author_email='nkthiebaut@gmail.com',
+      url='https://github.com/nkthiebaut',
+      download_url='https://github.com/nkthiebaut/zeugma/archive/0.15.tar.gz',
+      keywords=['embeddings'],
+      classifiers=[],
+      install_requires=[
+          'pandas>=0.20.3',
+          'numpy>=1.13.3',
+          'gensim>=3.2.0',
+          'scikit_learn>=0.19.1',
+          'fastText',
+      ],
+      dependency_links=['git+git://github.com/facebookresearch/fastText.git']
+     )
+
