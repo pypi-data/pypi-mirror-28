88ee540e048f5f2cc6ebbb3b8c3bfcb544071591
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:20 +0000

    Add package analytracks/0.2.1.8/analytracks-0.2.1.8.tar.gz

---
+++ packages/analytracks/analytracks-0.2.1.8.tar.gz/analytracks-0.2.1.8/analytracks/track.py
@@ -0,0 +1,30 @@
+class Track:
+    """"
+    A track object summarize an activity
+    
+    init args :
+        filepath : , 
+        platform='garmin', 
+        athlete='unknown', 
+        activity_id='unknown' (platform id), 
+        type='unkown' (activity type)
+        
+    attributes :
+        points (dataframe)   : timestamp, lat, lon, ...
+        features (dict) : global features
+            ['activity_id', 'activity_name', 'activity_type', 'athlete', 'date',
+            'device', 'distance', 'duration', 'elevationGain', 'has_po', 'has_tcx',
+            'has_hr', 'startLatitude', 'startLongitude', 'startTimeGMT',
+            'race_name', 'race_id', 'avHR', 'isarun',
+            record_data  (device, platform, ...)
+            
+    methods :
+        parse :
+        get_google_elevation :
+        get_speed :
+        get_run_po :
+        get_ta :
+        
+    """"
+    def 
+        
