41bbd5e634b6ae6e1942bb859ef5930977346e1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:14 +0000

    Add package changelogs/0.14.0/changelogs-0.14.0.tar.gz

---
+++ packages/changelogs/changelogs-0.14.0.tar.gz/changelogs-0.14.0/changelogs/__init__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+import os
+import re
+from requests import Session
+from .changelogs import get, get_commit_log
+
+"""
+if os.environ.get("DEBUG", "") in ("TRUE", "True", "true"):
+    DEBUG = True
+    import logging
+    logging.basicConfig(level=logging.DEBUG)
+else:
+    DEBUG = False
+"""
+
+__author__ = """Jannis Gebauer"""
+__email__ = 'jay@pyup.io'
+__version__ = '0.14.0'
+
+
+url_re = re.compile(r"(https?://[^\s<>\"'\x7f-\xff]+)", re.IGNORECASE)
