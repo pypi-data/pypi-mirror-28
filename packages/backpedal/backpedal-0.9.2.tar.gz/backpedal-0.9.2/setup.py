53506082f145890e1df8a727fae60f0ac2b31af0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:41 +0000

    Add package backpedal/0.9.2/backpedal-0.9.2.tar.gz

---
+++ packages/backpedal/backpedal-0.9.2.tar.gz/backpedal-0.9.2/setup.py
@@ -0,0 +1,15 @@
+
+from setuptools import setup, find_packages
+
+setup(name='backpedal',
+    version='0.9.2',
+    description="Backpedal",
+    long_description="Backpedal",
+    author='BJ Dierkes',
+    author_email='derks@datafolklabs.com',
+    url='http://github.com/datafolklabs/backpedal',
+    license='BSD-three-clause',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    include_package_data=True,
+    zip_safe=False,
+)
