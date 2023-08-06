b58b435ed5a1fb7387522cdd5189f60994aa214d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:01 +0000

    Add package DAPN/1.0.0/DAPN-1.0.0.tar.gz

---
+++ packages/DAPN/DAPN-1.0.0.tar.gz/DAPN-1.0.0/setup.py
@@ -0,0 +1,13 @@
+
+
+from setuptools import setup
+
+setup(
+    name = 'DAPN',
+    version = '1.0.0',
+    author = 'Durant',
+    author_email = '454311272@qq.com',
+    url = 'http://blog.csdn.net/weixin_38637598/article/details/79294283',
+    description = 'this is a test package for finding All prime_number under input.',
+    keywords = ['prime number'],
+)
