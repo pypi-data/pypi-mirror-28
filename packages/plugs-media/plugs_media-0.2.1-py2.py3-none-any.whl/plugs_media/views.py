a12f5442fb5344e6b55c7f74370cc557136bc4ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:22 +0000

    Add package plugs-media/0.2.1/plugs_media-0.2.1-py2.py3-none-any.whl

---
+++ packages/plugs-media/plugs_media-0.2.1-py2.py3-none-any.whl/plugs_media/views.py
@@ -0,0 +1,26 @@
+"""
+Plugs Media Views
+"""
+from rest_framework import permissions
+from rest_framework.viewsets import ReadOnlyModelViewSet
+
+from plugs_core import viewsets
+
+from plugs_media import models
+from plugs_media import serializers
+from plugs_media.permissions import IsOwnerOrReadOnly
+
+from plugs_filter.decorators import auto_filters
+
+@auto_filters
+class MediaViewSet(viewsets.CreateReadViewSet):
+    """
+    Media Endpoint does not support update or delete operations
+    """
+    queryset = models.Media.objects.all()
+    serializer_class = serializers.MediaSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+    auto_filters_fields = ('id', )
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
