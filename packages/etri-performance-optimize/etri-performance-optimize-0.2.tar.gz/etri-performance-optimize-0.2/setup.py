6c9564f6fcaa7a680448b8c745ec925ac3569d95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:18:02 +0000

    Add package etri-performance-optimize/0.2/etri-performance-optimize-0.2.tar.gz

---
+++ packages/etri-performance-optimize/etri-performance-optimize-0.2.tar.gz/etri-performance-optimize-0.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+#from distutils.core import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(
+    name='etri-performance-optimize',    # This is the name of your PyPI-package.
+    version='0.2',                          # Update the version number for new releases
+    #scripts=['test.py']                  # The name of your scipt, and also the command you'll be using for calling it
+    author = 'xzhuo',
+    author_email = '',
+    url = 'https://github.com/xiaoyanzhuo/ETRI',
+    #download_url = 'https://github.com/xiaoyanzhuo/ETRI/releases/tag/V1.0'
+    packages=['profiling'],
+    scripts=['bin/profiling-analysis'],
+    include_package_data=True,
+    zip_safe=False
+)
