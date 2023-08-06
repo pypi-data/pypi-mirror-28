2b30293283b43f32a9acd54814b14b72efa53b19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:28 +0000

    Add package jupyter_c_kernel/1.2.2/jupyter_c_kernel-1.2.2.tar.gz

---
+++ packages/jupyter_c_kernel/jupyter_c_kernel-1.2.2.tar.gz/jupyter_c_kernel-1.2.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='jupyter_c_kernel',
+      version='1.2.2',
+      description='Minimalistic C kernel for Jupyter',
+      author='Brendan Rius',
+      author_email='ping@brendan-rius.com',
+      url='https://github.com/brendanrius/jupyter-c-kernel/',
+      download_url='https://github.com/brendanrius/jupyter-c-kernel/tarball/1.2.2',
+      packages=['jupyter_c_kernel'],
+      scripts=['jupyter_c_kernel/install_c_kernel'],
+      keywords=['jupyter', 'notebook', 'kernel', 'c'],
+      include_package_data=True
+      )
