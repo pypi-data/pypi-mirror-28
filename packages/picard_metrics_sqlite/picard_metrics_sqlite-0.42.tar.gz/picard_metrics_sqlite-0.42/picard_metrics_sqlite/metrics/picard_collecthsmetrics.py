b323dddd8e07356057f4844557633d9c52979fd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:56 +0000

    Add package picard_metrics_sqlite/0.42/picard_metrics_sqlite-0.42.tar.gz

---
+++ packages/picard_metrics_sqlite/picard_metrics_sqlite-0.42.tar.gz/picard_metrics_sqlite-0.42/picard_metrics_sqlite/metrics/picard_collecthsmetrics.py
@@ -0,0 +1,33 @@
+import os
+
+from .metrics_util import picard_select_tsv_to_df
+
+def picard_CollectHsMetrics_to_df(metric_path, logger):
+    select = 'BAIT_SET'
+    df = picard_select_tsv_to_df(metric_path, select, logger)
+    return df
+
+def picard_CollectHsMetrics_histogram_to_df(metric_path, logger):
+    select = 'coverage'
+    df = picard_select_tsv_to_df(metric_path, select, logger)
+    return df
+
+def run(job_uuid, metric_path, bam, bam_library, exome_kit, input_state, engine, logger, metric_name):
+    table_name = 'picard_' + metric_name
+    df = picard_CollectHsMetrics_to_df(metric_path, logger)
+    df['bam'] = bam
+    df['exome_kit'] = exome_kit
+    df['input_state'] = input_state
+    df['readgroup_library'] = bam_library
+    df['job_uuid'] = job_uuid
+    df.to_sql(table_name, engine, if_exists='append')
+
+    table_name += '_histogram'
+    df = picard_CollectHsMetrics_histogram_to_df(metric_path, logger)
+    df['bam'] = bam
+    df['exome_kit'] = exome_kit
+    df['input_state'] = input_state
+    df['readgroup_library'] = bam_library
+    df['job_uuid'] = job_uuid
+    df.to_sql(table_name, engine, if_exists='append')    
+    return
