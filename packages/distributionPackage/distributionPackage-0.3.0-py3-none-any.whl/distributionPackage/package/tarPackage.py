324d99ff42161ed23346846232e815d05d6aefb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:21 +0000

    Add package distributionPackage/0.3.0/distributionPackage-0.3.0-py3-none-any.whl

---
+++ packages/distributionPackage/distributionPackage-0.3.0-py3-none-any.whl/distributionPackage/package/tarPackage.py
@@ -0,0 +1,48 @@
+#
+# Copyright 2018 Russell Smiley
+#
+# This file is part of distributionPackage.
+#
+# distributionPackage is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# distributionPackage is distributed in the hope that it will be useful
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with distributionPackage.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+import logging
+import os
+import tarfile
+
+from .base import PackageBase
+from .directory import ChangeDirectory
+from .interface import PackageInterface
+
+
+log = logging.getLogger( __name__ )
+
+
+class TarPackage( PackageInterface, PackageBase ) :
+    def __init__( self, fileName, projectRoot ) :
+        super().__init__( fileName, projectRoot )
+
+        self.tarFile = tarfile.open( name = self.fileName,
+                                     mode = 'w:gz' )
+
+
+    def build( self, filesToPackage ) :
+        with ChangeDirectory( self.projectRoot ) :
+            for thisFile in filesToPackage :
+                if os.path.isfile( thisFile ) :
+                    self.tarFile.add( thisFile )
+                elif not os.path.exists( thisFile ) :
+                    realFileName = os.path.realpath( os.path.abspath( os.path.join( self.projectRoot, thisFile ) ) )
+
+                    log.warning( 'File to package does not exist, {0}'.format( realFileName ) )
