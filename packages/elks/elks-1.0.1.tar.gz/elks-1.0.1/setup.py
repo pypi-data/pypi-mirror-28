9be0634fc5cc6f64f10cf261d48d748d0ebbfd49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:49 +0000

    Add package elks/1.0.1/elks-1.0.1.tar.gz

---
+++ packages/elks/elks-1.0.1.tar.gz/elks-1.0.1/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+appname = 'elks'
+
+setup(
+    name=appname,
+    version='1.0.1',
+    description='Python bindings for 46elks API.',
+    author='Gustaf Hansen',
+    author_email='gustaf@linkura.se',
+    url='https://github.com/Linkura/elks',
+    packages=['elks'],
+    platforms=['any'],
+    scripts=[],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Communications :: Telephony',
+    ],
+    install_requires=['requests>=1.0'],
+)
