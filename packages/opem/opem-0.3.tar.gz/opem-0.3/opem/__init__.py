e85b11a56a4c00d2bb620a85b23f439da95f6462
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:48 +0000

    Add package opem/0.3/opem-0.3.tar.gz

---
+++ packages/opem/opem-0.3.tar.gz/opem-0.3/opem/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+import opem.Static.Amphlett
+import opem.Static.Larminie_Dicks
+import opem.Static.Chamberline_Kim
+import opem.Params
+import opem.Functions
+import opem.Dynamic.Padulles1
