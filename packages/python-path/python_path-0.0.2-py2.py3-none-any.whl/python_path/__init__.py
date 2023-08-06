326b459a2be9251257f998e13332b47580b96b14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:14 +0000

    Add package python-path/0.0.2/python_path-0.0.2-py2.py3-none-any.whl

---
+++ packages/python-path/python_path-0.0.2-py2.py3-none-any.whl/python_path/__init__.py
@@ -0,0 +1,27 @@
+
+import sys, os
+
+class PythonPath(object):
+    def __init__(self, base = None, relative = None, get_real_path = True, base_is_file = True):
+
+        if base is None:
+            base = os.getcwd()
+
+        if get_real_path:
+            base = os.path.realpath(base)
+
+        if base_is_file:
+            base = os.path.dirname(base)
+
+        if relative:
+            base = os.path.join(base, relative)
+
+        self.dir_path = os.path.realpath(base)
+
+
+    def __enter__(self):
+        sys.path.insert(0, self.dir_path)
+        return self.dir_path
+
+    def __exit__(self, type, value, traceback):
+        sys.path.remove(self.dir_path)
