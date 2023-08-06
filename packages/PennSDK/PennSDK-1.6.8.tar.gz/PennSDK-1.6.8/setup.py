5335e07aaf2724f0b8936eff5947c7599003e57b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:34 +0000

    Add package PennSDK/1.6.8/PennSDK-1.6.8.tar.gz

---
+++ packages/PennSDK/PennSDK-1.6.8.tar.gz/PennSDK-1.6.8/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+
+setup(
+    name='PennSDK',
+    description='Python tools for building Penn-related applications',
+    url='https://github.com/pennlabs/penn-sdk-python',
+    author='Penn Labs',
+    author_email='admin@pennlabs.org',
+    version='1.6.8',
+    packages=['penn'],
+    license='MIT',
+    package_data={
+        # If any package contains *.txt or *.rst files, include them:
+        '': ['*.txt', '*.rst'],
+        'penn': ['data/laundry.csv'],
+    },
+    long_description=open('./README.rst').read(),
+    install_requires=[
+        'nameparser==0.4.0',
+        'requests==2.4.3',
+        'beautifulsoup4==4.3.2',
+        'html5lib==0.999'
+    ]
+)
