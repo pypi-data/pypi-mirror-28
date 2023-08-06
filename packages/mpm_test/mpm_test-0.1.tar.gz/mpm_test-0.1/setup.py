ba7cecac712812a72816e94e4819a2f5b2bc70e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:18:30 +0000

    Add package mpm_test/0.1/mpm_test-0.1.tar.gz

---
+++ packages/mpm_test/mpm_test-0.1.tar.gz/mpm_test-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'mpm_test',
+  packages = ['mpm_test'],
+  version = '0.1',
+  description = 'Python package submission test example',
+  author = 'Alrawi',
+  author_email = 'alrawi@gatech.edu',
+  url = 'https://github.com/alrawi/mpm_test',
+  download_url = 'https://github.com/alrawi/mpm_test/archive/0.1.tar.gz',
+  keywords = ['packaging', 'reflection', 'example'],
+  classifiers = [],
+)
