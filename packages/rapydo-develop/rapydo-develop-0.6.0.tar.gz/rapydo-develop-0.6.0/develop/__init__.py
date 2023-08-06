880ab65973ebaff25ab4d469b93dbb9dd57ac251
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:56 +0000

    Add package rapydo-develop/0.6.0/rapydo-develop-0.6.0.tar.gz

---
+++ packages/rapydo-develop/rapydo-develop-0.6.0.tar.gz/rapydo-develop-0.6.0/develop/__init__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+__version__ = '0.6.0'
+
+# ORDER: confs > utilities > core > controller > develop > builds > http-api
+
+TOOLS = [
+    "tests",
+    "rapydo-confs",
+    "utils",
+    "http-api",
+    "build-templates",
+    "do",
+    "develop",
+]
+
+# frontend:
+#   online_url: https://github.com/rapydo/angularjs-on-flask.git
+#   online_url: https://github.com/rapydo/node-ui.git
