c2ac8b250a9e9a60bbe44843c2384d08592f3d17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:21 +0000

    Add package Askheaventext/1.0/Askheaventext-1.0.tar.gz

---
+++ packages/Askheaventext/Askheaventext-1.0.tar.gz/Askheaventext-1.0/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from setuptools import setup, find_packages
+
+PACKAGE = "text"
+NAME = "Askheaventext"
+DESCRIPTION = "测试"
+AUTHOR = "Askheaven"
+AUTHOR_EMAIL = "278842015@qq.com"
+URL = "https://github.com"
+VERSION = __import__(PACKAGE).__version__
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=open('README.rst').read(),
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    license="BSD",
+    url=URL,
+    packages=find_packages(exclude=["tests.*", "tests"]),
+    # package_data=find_package_data(
+    #     PACKAGE,
+    #     only_in_packages=False
+    # ),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Framework :: Django",
+    ],
+    zip_safe=False,
+)
---
+++ packages/Askheaventext/Askheaventext-1.0.tar.gz/Askheaventext-1.0/text/strlib.py
@@ -0,0 +1,24 @@
+# coding:utf-8
+# echo "#simpstr;ob" >> README.md
+# git init
+# git add README.md
+# git commit -m "first commit"
+# git remote add origin https://github.com/dev-area/simpstrlib.git
+# git push -u origin master
+
+# mkdir simpstrlib
+# cd simpstrlib
+# touch __init__.py
+# touch strlib.py
+
+def revStr(st):
+    return st[::-1]
+
+def addstr(st1,st2):
+    return st1 + ":" +st2
+
+
+# git add .
+# git commit -m "simple"
+# git push
+
