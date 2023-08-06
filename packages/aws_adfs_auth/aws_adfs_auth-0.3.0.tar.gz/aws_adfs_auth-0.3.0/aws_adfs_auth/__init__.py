730f4629ddd97388eff09cf9a9838009b55baf84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:49 +0000

    Add package aws_adfs_auth/0.3.0/aws_adfs_auth-0.3.0.tar.gz

---
+++ packages/aws_adfs_auth/aws_adfs_auth-0.3.0.tar.gz/aws_adfs_auth-0.3.0/aws_adfs_auth/__init__.py
@@ -0,0 +1,16 @@
+""" adfs_auth module definition """
+
+#from . import configuration
+#from . import cli
+#from . import main
+
+VERSION = "0.3.0"
+BUILD_TIME = "2018-01-26 01:25:00"
+
+__title__ = "aws_adfs_auth"
+__version__ = VERSION
+__summary__ = "aws_adfs_auth command line utility"
+__license__ = "APACHE2"
+__uri__ = "https://github.com/jschwellach/aws-adfs-auth"
+__author__ = "Janos Schwellach"
+__email__ = "jschwellach@gmail.com"
