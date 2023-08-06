25fe8d70f93b233a18bdffd145d657f28b9e66d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:58 +0000

    Add package blobstash-docstore/0.0.7/blobstash-docstore-0.0.7.tar.gz

---
+++ packages/blobstash-docstore/blobstash-docstore-0.0.7.tar.gz/blobstash-docstore-0.0.7/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+    name='blobstash-docstore',
+    version='0.0.7',
+    description='BlobStash DocStore client',
+    long_description=long_description,
+    author='Thomas Sileo',
+    author_email='t@a4.io',
+    url='https://github.com/tsileo/blobstash-python-docstore',
+    packages=['blobstash.docstore'],
+    license='MIT',
+    install_requires=[
+        'blobstash-base==0.0.5',
+        'blobstash-filetree==0.0.6',
+        'requests',
+        'jsonpatch',
+    ],
+    python_requires='>=3.6',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+
+        'Intended Audience :: Developers',
+
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords='blobstash DocStore client JSON Lua document store',
+)
