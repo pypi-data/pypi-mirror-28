2bdfd7ddaf7b367c28b9d25c319338ed96a164d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:02 +0000

    Add package aimfast/0.0.0/aimfast-0.0.0.tar.gz

---
+++ packages/aimfast/aimfast-0.0.0.tar.gz/aimfast-0.0.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(name="aimfast",
+      description="An Astronomical Image Fidelity Assessment Tool.",
+      author="Athanaseus Ramaila",
+      author_email="aramaila@ska.ac.za",
+      packages=find_packages(),
+      url='https://github.com/Athanaseus/aimfast',
+      license="GNU GPL 3",
+      classifiers=["Intended Audience :: Developers",
+                   "Programming Language :: Python :: 2",
+                   "Topic :: Software Development :: Libraries :: Python Modules"],
+      platforms=["OS Independent"],
+      install_requires=["numpy",
+                        "scipy",
+                        "astropy",
+                        "future",
+                        "astLib"],
+      extras_require={'docs': ["sphinx-pypi-upload",
+                               "numpydoc",
+                               "Sphinx"]},
+      scripts=['aimfast/bin/aimfast'])
