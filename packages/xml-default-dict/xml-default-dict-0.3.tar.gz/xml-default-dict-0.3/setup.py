c556517f4f96270300a321b185fad3d026dfd5a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:53 +0000

    Add package xml-default-dict/0.3/xml-default-dict-0.3.tar.gz

---
+++ packages/xml-default-dict/xml-default-dict-0.3.tar.gz/xml-default-dict-0.3/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="xml-default-dict",
+    version="0.3",
+    license="BSD2CLAUSE",
+    packages=find_packages(),
+    keywords="xml dict defaultdict",
+    url="https://github.com/kanazux/xml-default-dict",
+    author='Silvio AS a.k.a kanazuchi',
+    author_email='contato@kanazuchi.com',
+    description="A simple way to convert xml elemnts into a default dict from lib collections.",
+)
