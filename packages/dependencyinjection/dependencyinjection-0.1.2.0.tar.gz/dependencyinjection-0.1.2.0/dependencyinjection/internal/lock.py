fcdf66a64d62646c3e3ca5286afb1dfae0b73c7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:37 +0000

    Add package dependencyinjection/0.1.2.0/dependencyinjection-0.1.2.0.tar.gz

---
+++ packages/dependencyinjection/dependencyinjection-0.1.2.0.tar.gz/dependencyinjection-0.1.2.0/dependencyinjection/internal/lock.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2017~2999 - cologler <skyoflw@gmail.com>
+# ----------
+#
+# ----------
+
+import threading
+from .common import ILock
+
+class ThreadLock(ILock):
+    def __init__(self):
+        self._lock = threading.RLock()
+
+    def __enter__(self):
+        self._lock.__enter__()
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self._lock.__exit__(exc_type, exc_value, traceback)
