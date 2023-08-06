38ca5eaa6d15cbd01b4c0421bf6dd068b83de0c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:29 +0000

    Add package pc23/1.1.0/pc23-1.1.0.tar.gz

---
+++ packages/pc23/pc23-1.1.0.tar.gz/pc23-1.1.0/pc23/__init__.py
@@ -0,0 +1,46 @@
+# -*- coding: utf8 -*-
+"""
+Compatibility library (Python Compat 2-3) used to write
+one general code both for Python 2.x and 3.x
+"""
+
+import sys
+
+
+py_version = 0
+
+
+def metaclass(metacls):
+    """ Compatible both with P2 and P3 metaclass usage """
+    def wrapper(cls):
+        orig_vars = cls.__dict__.copy()
+        slots = orig_vars.get('__slots__')
+        if slots is not None:
+            if isinstance(slots, str):
+                slots = [slots]
+            for slots_var in slots:
+                orig_vars.pop(slots_var)
+        orig_vars.pop('__dict__', None)
+        orig_vars.pop('__weakref__', None)
+        return metacls(cls.__name__, cls.__bases__, orig_vars)
+    return wrapper
+
+
+def str_decode(value):
+    return value.decode('utf-8') if py_version == 2 else str(value)
+
+
+def strlen(value):
+    return len(value) if py_version == 3 else len(value.decode('utf-8'))
+
+
+if sys.version_info[0] == 2:
+    py_version = 2
+    xrange = xrange
+    input = raw_input
+
+else:
+    py_version = 3
+    xrange = range
+    input = input
+
---
+++ packages/pc23/pc23-1.1.0.tar.gz/pc23-1.1.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='pc23',
+    version='1.1.0',
+    description='PC23 Python 2.x - 3.x compatibility library',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    keywords='pc23 compatibility',
+    url='https://bitbucket.org/deniskhodus/pc23',
+    author='Denis Khodus',
+    author_email='deniskhodus@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[],
+    include_package_data=False,
+    zip_safe=False
+)
+
