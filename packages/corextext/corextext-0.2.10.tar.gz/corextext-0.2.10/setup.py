0e57bad512ab7ceb82858826f9da36463c612783
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:38 +0000

    Add package corextext/0.2.10/corextext-0.2.10.tar.gz

---
+++ packages/corextext/corextext-0.2.10.tar.gz/corextext-0.2.10/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+try:
+    long_description = open("README.rst").read()
+except IOError:
+    long_description = ""
+
+setup(
+    name="corextext",
+    version="0.2.10",
+    description="Return components/latent factors that explain the most multivariate mutual information in the data under Linear Gaussian model. For comparison, PCA returns components explaining the most variance in the data.",
+    license="AGPL-3.0",
+    author="Rob Brekelmans/Greg Ver Steeg",
+    author_email="brekelma@usc.edu",
+    packages=find_packages(),
+    url='https://github.com/brekelma/corextext',
+    download_url='https://github.com/brekelma/corextext',
+    install_requires=[],
+    long_description=long_description,
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+    ],    
+    entry_points = {
+    'd3m.primitives': [
+        'corex_text.CorexText = corextext.corex_text:CorexText',
+    ],
+    }
+)
