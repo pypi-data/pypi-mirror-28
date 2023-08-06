a5982f510b8f18396bfb9972cdf28df08180fc31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:05 +0000

    Add package farsiscript/0.0.3/farsiscript-0.0.3.tar.gz

---
+++ packages/farsiscript/farsiscript-0.0.3.tar.gz/farsiscript-0.0.3/farsiscript/__init__.py
@@ -0,0 +1,4 @@
+__all__ = ['PyJsParser', 'parse', 'JsSyntaxError']
+__author__ = 'Behrooz Fard'
+__version__ = '0.0.3'
+from .parser import PyJsParser, parse, JsSyntaxError
---
+++ packages/farsiscript/farsiscript-0.0.3.tar.gz/farsiscript-0.0.3/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='farsiscript',
+    version='0.0.3',
+    packages=['farsiscript'],
+    url='https://github.com/behrooz-fard/pyjsparser',
+    install_requires = [],
+    license='MIT',
+    author='Behrooz Fard',
+    author_email='behrooz.fard@gmail.com',
+    description='a JavaScript dialect in Farsi (based on PyJsParser https://github.com/PiotrDabkowski/pyjsparser)'
+)
