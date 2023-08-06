1b3827b767db97b2c6ef89e8668947952bade711
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:45 +0000

    Add package pyNmonAnalyzer/1.0.9/pyNmonAnalyzer-1.0.9.tar.gz

---
+++ packages/pyNmonAnalyzer/pyNmonAnalyzer-1.0.9.tar.gz/pyNmonAnalyzer-1.0.9/setup.py
@@ -0,0 +1,43 @@
+'''
+Copyright (c) 2012-2018 Matthias Lee
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
+from setuptools import setup
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except ImportError:
+    long_description = open('README.md').read()
+setup(
+    name = "pyNmonAnalyzer",
+    version = "1.0.9",
+    author = "Matthias Lee",
+    author_email="pynmonanalyzer@madmaze.net",
+    maintainer = "Matthias Lee",
+    maintainer_email = "pynmonanalyzer@madmaze.net",
+    description = ("Python tool for reformatting and plotting/graphing NMON output"),
+    long_description = long_description,
+    license = "GPLv3",
+    keywords = "python nmon analyzer pynmonanalyzer interactive static report visualization",
+    url = "https://github.com/madmaze/pynmonanalyzer",
+    packages=['pynmonanalyzer'],
+    package_dir={'pynmonanalyzer': 'src'},
+    package_data = {'pynmonanalyzer': ['test.nmon','interactiveReport.tpl.html']},
+    entry_points={
+        "console_scripts": [
+            "pyNmonAnalyzer=pynmonanalyzer.pyNmonAnalyzer:pyNmonAnalyzer",
+        ]}
+)
