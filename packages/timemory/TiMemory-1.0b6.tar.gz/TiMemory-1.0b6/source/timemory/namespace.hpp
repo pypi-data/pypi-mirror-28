e910e51f5c99667a393235b2e5364ae3a4fd3216
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:22 +0000

    Add package timemory/1.0b6/TiMemory-1.0b6.tar.gz

---
+++ packages/timemory/TiMemory-1.0b6.tar.gz/TiMemory-1.0b6/source/timemory/namespace.hpp
@@ -0,0 +1,70 @@
+//  MIT License
+//  
+//  Copyright (c) 2018, The Regents of the University of California, 
+// through Lawrence Berkeley National Laboratory (subject to receipt of any 
+// required approvals from the U.S. Dept. of Energy).  All rights reserved.
+//  
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//  
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//  
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+
+/** \file namespace.hpp
+ * Defines NAME_TIM if not defined
+ *
+ */
+
+#ifndef namespace_hpp_
+#define namespace_hpp_
+
+// The namespace can be configured with CMake:
+//      -DTIMEMORY_NAMESPACE=<new top-level namespace>
+//
+// Below is not really needed if TiMemory is imported via TiMemoryConfig.cmake
+//
+// Top-level namespace (default = tim)
+
+#if !defined(NAME_TIM)
+#   define NAME_TIM tim
+#endif
+
+// sort out the operating system
+#if defined(_WIN32) || defined(_WIN64)
+#   if !defined(_WINDOWS)
+#       define _WINDOWS
+#   endif
+#elif defined(__APPLE__) || defined(__MACH__)
+#   if !defined(_MACOS)
+#       define _MACOS
+#   endif
+#   if !defined(_UNIX)
+#       define _UNIX
+#   endif
+#elif defined(__linux__) || defined(__linux) || defined(linux) || \
+    defined(__gnu_linux__)
+#   if !defined(_LINUX)
+#       define _LINUX
+#   endif
+#   if !defined(_UNIX)
+#       define _UNIX
+#   endif
+#elif defined(__unix__) || defined(__unix) || defined(unix) || defined(_)
+#   if !defined(_UNIX)
+#       define _UNIX
+#   endif
+#endif
+
+#endif
