ade38a63e9e819565d5f0b06398e2f94078e66dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:29 +0000

    Add package watchng/1.1/watchng-1.1.tar.gz

---
+++ packages/watchng/watchng-1.1.tar.gz/watchng-1.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(name="watchng",
+      version="1.1",
+      description="Run a command periodically, "
+      "showing output only when it changes",
+      author="Leandro Lisboa Penz",
+      author_email="lpenz@lpenz.org",
+      url="http://github.com/lpenz/watchng",
+      data_files=[('man/man1', ['watchng.1'])],
+      scripts=["watchng"],
+      long_description="""\
+watchng is a program that runs the given command-line periodically, showing
+the output only when it is different than the last, along with the time.
+""",
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Console',
+          'License :: OSI Approved :: '
+          'GNU General Public License v2 or later (GPLv2+)',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 3',
+          ],
+      license="GPL2")
