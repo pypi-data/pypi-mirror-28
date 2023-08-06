b7abb089d815a2bae2e2ab67e783751545c4e490
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:55 +0000

    Add package og-fetcher/0.1/og-fetcher-0.1.tar.gz

---
+++ packages/og-fetcher/og-fetcher-0.1.tar.gz/og-fetcher-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='og-fetcher',
+    version='0.1',
+    description='Open Graph Fetcher',
+    url='https://github.com/mnkuran/og-fetcher',
+    author='Mahmut Kuran',
+    author_email='mahmutkuran@hotmail.com',
+    packages=['og_fetcher']
+)
