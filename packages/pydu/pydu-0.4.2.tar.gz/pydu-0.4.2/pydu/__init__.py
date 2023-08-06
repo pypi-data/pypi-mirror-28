2069b89750aafa3a1a089b3d60038a480e93e498
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:48 +0000

    Add package pydu/0.4.2/pydu-0.4.2.tar.gz

---
+++ packages/pydu/pydu-0.4.2.tar.gz/pydu-0.4.2/pydu/__init__.py
@@ -0,0 +1,14 @@
+"""
+Useful data structures, utils for Python.
+"""
+
+__version__ = '0.4.2'
+
+
+# Set logging handler to avoid "No handler found" warnings.
+import logging
+from logging import NullHandler
+
+
+logger = logging.getLogger(__name__)
+logger.addHandler(NullHandler())
