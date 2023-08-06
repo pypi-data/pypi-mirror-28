23d2b1dc4341be22a9b3b2c57f4183cb222c7708
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:35 +0000

    Add package vireo/0.13.0/vireo-0.13.0.tar.gz

---
+++ packages/vireo/vireo-0.13.0.tar.gz/vireo-0.13.0/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+
+setup(
+    name         = 'vireo',
+    version      = '0.13.0',
+    description  = 'A library and framework for event-driven application development',
+    license      = 'MIT',
+    author       = 'Juti Noppornpitak',
+    author_email = 'juti_n@yahoo.co.jp',
+    # url          = 'http://shiroyuki.com/projects/vireo.html',
+    packages     = [
+        'vireo',
+        'vireo.drivers',
+        'vireo.drivers.rabbitmq',
+    ],
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries'
+    ],
+    install_requires = ['pika']
+)
