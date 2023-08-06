40d10302f76a7c07f08ab78640e7651f612e895f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:19:53 +0000

    Add package Orange3/3.9.0/Orange3-3.9.0-cp35-none-win_amd64.whl

---
+++ packages/Orange3/Orange3-3.9.0-cp35-none-win_amd64.whl/Orange/version.py
@@ -0,0 +1,11 @@
+
+# THIS FILE IS GENERATED FROM ORANGE SETUP.PY
+short_version = '3.9.0'
+version = '3.9.0'
+full_version = '3.9.0'
+git_revision = '69725b82bb2ee61235edd7745a11eba1d7c4877a'
+release = True
+
+if not release:
+    version = full_version
+    short_version += ".dev"
