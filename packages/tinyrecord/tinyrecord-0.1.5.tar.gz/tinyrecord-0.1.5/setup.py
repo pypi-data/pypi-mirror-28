338b25ef7ce14671cb2bee65868794adb0c6fca8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:47 +0000

    Add package tinyrecord/0.1.5/tinyrecord-0.1.5.tar.gz

---
+++ packages/tinyrecord/tinyrecord-0.1.5.tar.gz/tinyrecord-0.1.5/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='tinyrecord',
+    version='0.1.5',
+    packages=['tinyrecord'],
+
+    zip_safe=True,
+    author='Eugene Eeo',
+    author_email='141bytes@gmail.com',
+    description='Atomic transactions for TinyDB',
+    license='MIT',
+    keywords='tinydb nosql database transaction',
+    url='https://github.com/eugene-eeo/tinyrecord',
+)
