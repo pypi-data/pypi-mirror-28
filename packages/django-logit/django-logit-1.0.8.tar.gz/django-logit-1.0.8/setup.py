4704f0320625d1ac8cff1a9e99167131f419df01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:05 +0000

    Add package django-logit/1.0.8/django-logit-1.0.8.tar.gz

---
+++ packages/django-logit/django-logit-1.0.8.tar.gz/django-logit-1.0.8/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from __future__ import with_statement
+
+from setuptools import setup
+
+
+version = '1.0.8'
+
+
+setup(
+    name='django-logit',
+    version=version,
+    keywords='django-logit',
+    description='Django Decorator of Logging Request Params/Response Content',
+    long_description=open('README.rst').read(),
+
+    url='https://github.com/Brightcells/django-logit',
+
+    author='Hackathon',
+    author_email='kimi.huang@brightcells.com',
+
+    packages=[],
+    py_modules=['logit', ],
+    install_requires=[],
+
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Office/Business :: Financial :: Spreadsheet',
+    ],
+)
