4d9f994f8dc8a6f6f87c956ffb2f2b8786151b4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:32 +0000

    Add package limits/1.3/limits-1.3.tar.gz

---
+++ packages/limits/limits-1.3.tar.gz/limits-1.3/setup.py
@@ -0,0 +1,36 @@
+"""
+setup.py for limits
+
+
+"""
+__author__ = "Ali-Akber Saifee"
+__email__ = "ali@indydevs.org"
+__copyright__ = "Copyright 2015, Ali-Akber Saifee"
+
+from setuptools import setup, find_packages
+import os
+
+this_dir = os.path.abspath(os.path.dirname(__file__))
+REQUIREMENTS = [
+    k for k in open(
+        os.path.join(this_dir, 'requirements', 'main.txt')
+    ).read().splitlines() if k.strip()
+]
+import versioneer
+
+setup(
+    name='limits',
+    author=__author__,
+    author_email=__email__,
+    license="MIT",
+    url="https://limits.readthedocs.org",
+    zip_safe=False,
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    install_requires=REQUIREMENTS,
+    classifiers=[k for k in open('CLASSIFIERS').read().split('\n') if k],
+    description='Rate limiting utilities',
+    long_description=open('README.rst').read(),
+    packages=find_packages(exclude=["tests*"]),
+)
+
