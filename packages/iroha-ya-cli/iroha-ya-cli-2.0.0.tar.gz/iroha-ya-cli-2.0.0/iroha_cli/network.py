40c098d7c37fec1d38c2bd459042f914a90dddd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:08 +0000

    Add package iroha-ya-cli/2.0.0/iroha-ya-cli-2.0.0.tar.gz

---
+++ packages/iroha-ya-cli/iroha-ya-cli-2.0.0.tar.gz/iroha-ya-cli-2.0.0/iroha_cli/network.py
@@ -0,0 +1,149 @@
+import base64
+import datetime
+
+import binascii
+import grpc
+
+from iroha_cli import crypto
+from iroha_cli.exception import CliException
+from endpoint_pb2_grpc import CommandServiceStub, QueryServiceStub
+from endpoint_pb2 import TxStatusRequest, TxStatus
+from primitive_pb2 import Signature
+from block_pb2 import Transaction
+from queries_pb2 import Query
+from responses_pb2 import ErrorResponse
+
+from google.protobuf.json_format import MessageToJson
+
+MAX_POLLING_TIME = 10
+
+
+def generateTransaction(account_id, commands, key_pair):
+    payload = Transaction.Payload(
+        commands=commands,
+        creator_account_id=account_id,
+        tx_counter=1,
+        created_time=int(datetime.datetime.now().timestamp() * 1000)
+    )
+    payload_hash = crypto.sha3_256(payload.SerializeToString())
+    sign = crypto.sign(key_pair, payload_hash)
+    tx = Transaction(
+        payload=payload,
+        signature=[
+            Signature(pubkey=key_pair.raw_public_key(), signature=base64.b64decode(sign))
+        ]
+    )
+    return tx, payload_hash
+
+
+def waitTransaciton(location, tx_hash):
+    payload = TxStatusRequest(tx_hash=tx_hash)
+    not_received_cnt = 0
+    while True:
+        result = sendTxStatus(location, payload)
+
+        if result.tx_status == TxStatus.Value('STATELESS_VALIDATION_FAILED'):
+            raise Exception('stateless validation failed')
+
+        elif result.tx_status == TxStatus.Value('STATEFUL_VALIDATION_FAILED'):
+            raise Exception('stateful validation failed')
+
+        elif result.tx_status == TxStatus.Value('NOT_RECEIVED'):
+            if not_received_cnt > MAX_POLLING_TIME:
+                raise TimeoutError
+            else:
+                not_received_cnt += 1
+
+        elif result.tx_status == TxStatus.Value('COMMITTED'):
+            return True
+
+
+def generateQuery(account_id, qry, key_pair):
+    payload = Query.Payload(
+        **qry,
+        creator_account_id=account_id,
+        created_time=int(datetime.datetime.now().timestamp() * 1000),
+    )
+    # This is a trap. ('A`) I guess hyperledger/iroha has bug in serialize or deserialize?
+    # query_counter is removed before hash, so verify shouldn't be valid!!
+    # payload.query_counter = 1
+    #
+    payload_hash = crypto.sha3_256(payload.SerializeToString())
+    sign = crypto.sign(key_pair, payload_hash)
+    query = Query(
+        payload=payload,
+        signature=Signature(pubkey=key_pair.raw_public_key, signature=sign)
+    )
+    return query
+
+
+def sendTxStatus(location, tx_status):
+    try:
+        channel = grpc.insecure_channel(location)
+        stub = CommandServiceStub(channel)
+        return stub.Status(tx_status, timeout=1)  # wait 1s
+    except grpc.RpcError as e:
+        print("== Grpc happens error ==\n"
+              "- Server is active?: {} \n"
+              "- What's happen?   : {} \n"
+              .format(e.is_active(), e.details())
+              )
+        return False
+
+
+def sendTx(location, tx):
+    try:
+        channel = grpc.insecure_channel(location)
+        stub = CommandServiceStub(channel)
+        stub.Torii(tx, timeout=1)  # wait 1s
+        return True
+    except grpc.RpcError as e:
+        print("== Grpc happens error ==\n"
+              "- Server is active?: {} \n"
+              "- What's happen?   : {} \n"
+              .format(e.is_active(), e.details())
+              )
+        return False
+
+
+def sendQuery(location, query):
+    try:
+        channel = grpc.insecure_channel(location)
+        stub = QueryServiceStub(channel)
+        res = stub.Find(query, timeout=1)  # wait 1s
+        if res.HasField('error_response'):
+            reason = res.error_response.reason
+            if reason == ErrorResponse.STATELESS_INVALID:
+                raise CliException("[Iroha] stateless invalied")
+            elif reason == ErrorResponse.STATEFUL_INVALID:
+                raise CliException("[Iroha] statefull invalied")
+            elif reason == ErrorResponse.NO_ACCOUNT:
+                raise CliException("[Iroha] account does not exist")
+            elif reason == ErrorResponse.NO_ACCOUNT_ASSETS:
+                raise CliException("[Iroha] account don't have asset")
+            elif reason == ErrorResponse.NOT_SUPPORTED:
+                raise CliException("[Iroha] signatories does not exist")
+            elif reason == ErrorResponse.WRONG_FORMAT:
+                raise CliException("[Iroha] json format wrong No happen")
+            elif reason == ErrorResponse.NO_ASSET:
+                raise CliException("[Iroha] asset does not exist")
+            elif reason == ErrorResponse.NO_ROLES:
+                raise CliException("[Iroha] there are no roles defined in the system")
+
+        if res.HasField('account_response'):
+            result = res.account_response
+        elif res.HasField('asset_response'):
+            result = res.asset_response
+        elif res.HasField('signatories_response'):
+            result = res.signatories_response
+        elif res.HasField('transactions_response'):
+            result = res.transactions_response
+        elif res.HasField('account_assets_response'):
+            result = res.account_assets_response
+        else:
+            raise CliException("Sorry! not implements...")
+
+        return MessageToJson(result)
+    except grpc.RpcError as e:
+        raise CliException("[Grpc] Server({location}) is {status}, {detail}".format(
+            location=location, status=e.is_active(), detail=e.details()))
---
+++ packages/iroha-ya-cli/iroha-ya-cli-2.0.0.tar.gz/iroha-ya-cli-2.0.0/setup.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python 
+import shutil
+from setuptools.extension import Extension
+
+import sys
+import os
+import subprocess
+
+from setuptools.command.build_py import build_py as _build_py
+from setuptools import setup, find_packages
+
+def exec_generate_proto(source,python):
+    protoc_command = [ python, "-m", "grpc_tools.protoc", "-I.", "--python_out=.", source]
+    if subprocess.call(protoc_command) != 0:
+        sys.exit(-1)
+    sys.stdout.write("Generate {}_pb2.py ==> successfull\n".format(source.split('.')[0]))
+
+    protoc_grpc_command = [ python, "-m", "grpc_tools.protoc", "-I.", "--python_out=.","--grpc_python_out=.", source]
+    if subprocess.call(protoc_grpc_command) != 0:
+        sys.exit(-1)
+    sys.stdout.write("Generate {}_grcp_pb2.py ==> successfull\n".format(source.split('.')[0]))
+
+def check_python_grpc(python):
+    protoc_command = [python, "-m", "grpc_tools.protoc", "--version"]
+    return subprocess.call(protoc_command) == 0
+
+class GeneratePb(_build_py):
+    def run(self):
+        python = shutil.which("python3")
+        if not python or not check_python_grpc(python):
+            python = shutil.which("python")
+
+        os.chdir("iroha_cli_schema/")
+        exec_generate_proto('block.proto',python)
+        exec_generate_proto('commands.proto',python)
+        exec_generate_proto('endpoint.proto',python)
+        exec_generate_proto('primitive.proto',python)
+        exec_generate_proto('queries.proto',python)
+        exec_generate_proto('responses.proto',python)
+        os.chdir("..")
+        _build_py.run(self)
+
+if __name__ == '__main__':
+    setup(
+          name='iroha-ya-cli',
+          version='2.0.0',
+          description='Cli for hyperledger/iroha',
+          author='Sonoko Mizuki',
+          license='Apache',
+          author_email='mizuki.sonoko@gmail.com',
+          packages = find_packages(),
+          include_package_data=True,
+          install_requires=[
+                'grpcio',
+                'grpcio-tools',
+                'protobuf',
+                'PyYAML',
+
+                'ed25519-python'
+          ],
+          cmdclass={
+            'build_py': GeneratePb,
+          },
+          classifiers=[
+             'Programming Language :: Python :: 3.5',
+
+             'Development Status :: 4 - Beta',
+
+             'License :: OSI Approved :: Apache Software License',
+             'Topic :: Utilities'
+          ],
+          entry_points={
+          'console_scripts':
+            ['irohac=iroha_cli.main:main']
+          }
+    )
