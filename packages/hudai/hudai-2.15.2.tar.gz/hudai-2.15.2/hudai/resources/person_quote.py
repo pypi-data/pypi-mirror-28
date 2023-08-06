7774b2999922aef1b047a23d3df93a4fab2f1bcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:36 +0000

    Add package hudai/2.15.2/hudai-2.15.2.tar.gz

---
+++ packages/hudai/hudai-2.15.2.tar.gz/hudai-2.15.2/hudai/resources/person_quote.py
@@ -0,0 +1,35 @@
+"""
+hudai.resources.person_quote
+"""
+from ..helpers.resource import Resource
+
+
+class PersonQuoteResource(Resource):
+    def __init__(self, client):
+        Resource.__init__(self, client, base_path='/people/quotes')
+        self.resource_name = 'PersonQuote'
+
+    def list(self, person_id, article_id=None, term=None, page=None):
+        query_params = self._set_limit_offset({
+            'article_id': article_id,
+            'person_id': person_id,
+            'term': term,
+            'page': page
+        })
+
+        return self.http_get('/', query_params=query_params)
+
+    def create(self, person_id, article_id, term, text):
+        return self.http_post('/',
+                              data={
+                                  'article_id': article_id,
+                                  'person_id': person_id,
+                                  'term': term,
+                                  'text': text
+                              })
+
+    def fetch(self, quote_id):
+        return self.http_get('/{id}', params={'id': quote_id})
+
+    def delete(self, quote_id):
+        return self.http_delete('/{id}', params={'id': quote_id})
