4fd9e43783a8111d0b1a13d74c74e5fba356ed0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:06 +0000

    Add package hddfancontrol/1.2.8/hddfancontrol-1.2.8.tar.gz

---
+++ packages/hddfancontrol/hddfancontrol-1.2.8.tar.gz/hddfancontrol-1.2.8/setup.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+
+import os
+import re
+import sys
+
+from setuptools import find_packages, setup
+
+
+if sys.hexversion < 0x3030000:
+  print("Python version %s is unsupported, >= 3.3.0 is needed" % (".".join(map(str, sys.version_info[:3]))))
+  exit(1)
+
+with open(os.path.join("hddfancontrol", "__init__.py"), "rt") as f:
+  version = re.search("__version__ = \"([^\"]+)\"", f.read()).group(1)
+
+with open("requirements.txt", "rt") as f:
+  requirements = f.read().splitlines()
+# require enum34 if enum module is missing (Python 3.3)
+try:
+  import enum
+except ImportError:
+  requirements.append("enum34")
+
+try:
+  import pypandoc
+  readme = pypandoc.convert("README.md", "rst")
+except ImportError:
+  with open("README.md", "rt") as f:
+    readme = f.read()
+
+setup(name="hddfancontrol",
+      version=version,
+      author="desbma",
+      packages=find_packages(exclude=("tests",)),
+      entry_points={"console_scripts": ["hddfancontrol = hddfancontrol:cl_main"]},
+      test_suite="tests",
+      install_requires=requirements,
+      description="Control system fan speed by monitoring hard drive temperature",
+      long_description=readme,
+      url="https://github.com/desbma/hddfancontrol",
+      download_url="https://github.com/desbma/hddfancontrol/archive/%s.tar.gz" % (version),
+      keywords=["hdd", "drive", "temperature", "fan", "control", "speed"],
+      classifiers=["Development Status :: 5 - Production/Stable",
+                   "Environment :: Console",
+                   "Environment :: No Input/Output (Daemon)",
+                   "Intended Audience :: End Users/Desktop",
+                   "Intended Audience :: System Administrators",
+                   "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+                   "Natural Language :: English",
+                   "Operating System :: POSIX :: Linux",
+                   "Programming Language :: Python",
+                   "Programming Language :: Python :: 3",
+                   "Programming Language :: Python :: 3 :: Only",
+                   "Programming Language :: Python :: 3.3",
+                   "Programming Language :: Python :: 3.4",
+                   "Programming Language :: Python :: 3.5",
+                   "Programming Language :: Python :: 3.6",
+                   "Topic :: System :: Hardware",
+                   "Topic :: System :: Monitoring",
+                   "Topic :: Utilities"])
