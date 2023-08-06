cc9f25316fa7769698288f8b72b342af9205c83c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:04 +0000

    Add package MaybeDont/0.1.1/MaybeDont-0.1.1.tar.gz

---
+++ packages/MaybeDont/MaybeDont-0.1.1.tar.gz/MaybeDont-0.1.1/setup.py
@@ -0,0 +1,35 @@
+import os
+from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name='MaybeDont',
+    version='0.1.1',
+    author="Konstantin Lopuhin",
+    author_email="kostia.lopuhin@gmail.com",
+    description='A component that tried to avoid downloading duplicate content',
+    license='MIT',
+    url='https://github.com/TeamHG-Memex/MaybeDont',
+    packages=['maybedont'],
+    long_description=read('README.rst'),
+    install_requires=[
+        'six',
+        'datasketch>=0.2.0',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Topic :: Internet :: WWW/HTTP :: Indexing/Search',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
