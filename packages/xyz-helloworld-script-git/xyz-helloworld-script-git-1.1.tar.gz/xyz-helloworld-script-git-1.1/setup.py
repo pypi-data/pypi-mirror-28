acf113fb37ef79dc1c156125cdb5ec8a522ff985
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:44 +0000

    Add package xyz-helloworld-script-git/1.1/xyz-helloworld-script-git-1.1.tar.gz

---
+++ packages/xyz-helloworld-script-git/xyz-helloworld-script-git-1.1.tar.gz/xyz-helloworld-script-git-1.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+#from distutils.core import setup
+
+setup(
+    name='xyz-helloworld-script-git',    # This is the name of your PyPI-package.
+    version='1.1',                          # Update the version number for new releases
+    #scripts=['test.py']                  # The name of your scipt, and also the command you'll be using for calling it
+    author = '',
+    author_email = '',
+    url = 'https://github.com/xiaoyanzhuo/ETRI',
+    #download_url = 'https://github.com/xiaoyanzhuo/ETRI/releases/tag/V1.0'
+    packages=['test'],
+    zip_safe=False
+)
