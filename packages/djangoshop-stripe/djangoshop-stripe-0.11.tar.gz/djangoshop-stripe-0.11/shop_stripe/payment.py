31bcd2d3556831bf667c43cd2f475ed3ab3e52d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:56 +0000

    Add package djangoshop-stripe/0.11/djangoshop-stripe-0.11.tar.gz

---
+++ packages/djangoshop-stripe/djangoshop-stripe-0.11.tar.gz/djangoshop-stripe-0.11/shop_stripe/payment.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from decimal import Decimal
+from distutils.version import LooseVersion
+
+from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
+from django.utils.translation import ugettext_lazy as _
+
+from django_fsm import transition
+from rest_framework.exceptions import ValidationError
+import stripe
+
+from shop import __version__ as SHOP_VERSION
+from shop.models.order import BaseOrder, OrderModel, OrderPayment
+from shop.money import MoneyMaker
+from shop.payment.base import PaymentProvider
+
+
+class StripePayment(PaymentProvider):
+    """
+    Provides a payment service for Stripe.
+    """
+    namespace = 'stripe-payment'
+
+    def get_payment_request(self, cart, request):
+        """
+        From the given request, add a snippet to the page.
+        """
+        try:
+            self.charge(cart, request)
+            thank_you_url = OrderModel.objects.get_latest_url()
+            js_expression = '$window.location.href="{}";'.format(thank_you_url)
+            return js_expression
+        except (KeyError, stripe.error.StripeError) as err:
+            raise ValidationError(err)
+
+    def charge(self, cart, request):
+        """
+        Use the Stripe token from the request and charge immediately.
+        This view is invoked by the Javascript function `scope.charge()` delivered
+        by `get_payment_request`.
+        """
+        stripe.api_key = settings.SHOP_STRIPE['APIKEY']
+        token_id = cart.extra['payment_extra_data']['token_id']
+        if LooseVersion(SHOP_VERSION) < LooseVersion('0.11'):
+            charge = stripe.Charge.create(
+                amount=cart.total.as_integer(),
+                currency=cart.total.currency,
+                source=token_id,
+                description=settings.SHOP_STRIPE['PURCHASE_DESCRIPTION']
+            )
+            if charge['status'] == 'succeeded':
+                order = OrderModel.objects.create_from_cart(cart, request)
+                order.add_stripe_payment(charge)
+                order.save()
+        else:
+            order = OrderModel.objects.create_from_cart(cart, request)
+            charge = stripe.Charge.create(
+                amount=cart.total.as_integer(),
+                currency=cart.total.currency,
+                source=token_id,
+                transfer_group = order.get_number(),
+                description=settings.SHOP_STRIPE['PURCHASE_DESCRIPTION'],
+            )
+            if charge['status'] == 'succeeded':
+                order.populate_from_cart(cart, request)
+                order.add_stripe_payment(charge)
+                order.save()
+
+        if charge['status'] != 'succeeded':
+            msg = "Stripe returned status '{status}' for id: {id}"
+            raise stripe.error.InvalidRequestError(msg.format(**charge))
+
+
+class OrderWorkflowMixin(object):
+    TRANSITION_TARGETS = {
+        'paid_with_stripe': _("Paid using Stripe"),
+    }
+
+    def __init__(self, *args, **kwargs):
+        if not isinstance(self, BaseOrder):
+            raise ImproperlyConfigured('OrderWorkflowMixin is not of type BaseOrder')
+        super(OrderWorkflowMixin, self).__init__(*args, **kwargs)
+
+    @transition(field='status', source=['created'], target='paid_with_stripe')
+    def add_stripe_payment(self, charge):
+        assert self.currency == charge['currency'].upper(), "Currency mismatch"
+        Money = MoneyMaker(self.currency)
+        amount = Money(Decimal(charge['amount']) / Money.subunits)
+        OrderPayment.objects.create(order=self, amount=amount, transaction_id=charge['id'], payment_method=StripePayment.namespace)
+
+    def is_fully_paid(self):
+        return super(OrderWorkflowMixin, self).is_fully_paid()
+
+    @transition(field='status', source='paid_with_stripe', conditions=[is_fully_paid],
+        custom=dict(admin=True, button_name=_("Acknowledge Payment")))
+    def acknowledge_stripe_payment(self):
+        self.acknowledge_payment()
---
+++ packages/djangoshop-stripe/djangoshop-stripe-0.11.tar.gz/djangoshop-stripe-0.11/shop_stripe/static/shop/js/stripe.js
@@ -0,0 +1,76 @@
+(function() {
+"use strict";
+
+var module = angular.module('django.shop.stripe', ['djng.urls', 'angular-stripe']);
+
+// Directive <ANY stripe-card-form>
+// Must be added to the form containing the CC input fields
+module.directive('stripeCardForm', ['$http', 'djangoUrl', 'stripe',
+                           function($http, djangoUrl, stripe) {
+	// iterate over sibling scopes to find the scope object holding the customer's data
+	function findCustomerScope(scope) {
+		while (scope && scope.hasOwnProperty('data')) {
+			if (scope.data.hasOwnProperty('customer'))
+				return scope.data.customer;
+			scope = scope.$$prevSibling;
+		}
+		return null;
+	}
+
+	return {
+		restrict: 'A',
+		require: 'form',
+		scope: false,
+		controller: ['$scope', function($scope) {
+			var customer = findCustomerScope($scope);
+			if (customer) {
+				$scope.payment = {
+					card: {name: [
+						customer.first_name ? customer.first_name : '',
+						customer.last_name ? customer.last_name : ''].join(' ')
+					}
+				};
+			}
+
+			// pass data from Stripe Card Form to PSP and fetch the token
+			function createToken(deferred) {
+				$scope.dismiss();
+				stripe.card.createToken($scope.payment.card).then(function(token) {
+					console.log('token created for card ending in ', token.card.last4);
+					deferred.resolve(token);
+				}, function(error) {
+					console.log(error.message);
+					$scope.stripe_error_message = error.message;
+					deferred.reject(error.code);
+				});
+				return deferred.promise;
+			}
+
+			$scope.prepare = function(deferred) {
+				if ($scope.data.payment_method.payment_modifier !== 'stripe-payment'
+				  || angular.isString($scope.data.payment_method.payment_data.token_id)) {
+					deferred.resolve();
+				} else {
+					createToken(deferred).then(function(token) {
+						$scope.data.payment_method.payment_data = {token_id: token.id};
+					});
+				}
+				return deferred.promise;
+			};
+
+			$scope.resetStripeToken = function() {
+				$scope.data = $scope.data || {payment_method: {}};
+				$scope.data.payment_method.payment_data = {};
+				$scope.dismiss();
+			};
+
+			$scope.dismiss = function() {
+				$scope.stripe_error_message = $scope.stripe_success_message = null;
+			};
+
+			$scope.resetStripeToken();
+		}]
+	};
+}]);
+
+})();
