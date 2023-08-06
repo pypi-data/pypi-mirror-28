886a0c3b287f724c7299afa15c73f374e49ec61c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:11 +0000

    Add package gitlabform/0.22.1/gitlabform-0.22.1.tar.gz

---
+++ packages/gitlabform/gitlabform-0.22.1.tar.gz/gitlabform-0.22.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(name='gitlabform',
+      version='0.22.1',
+      description='Easy configuration as code tool for GitLab using config in plain YAML',
+      url='https://github.com/egnyte/gitlabform',
+      author='Egnyte',
+      keywords=['gitlab', 'configuration-as-code'],
+      classifiers=[
+            "Programming Language :: Python :: 3.5",
+            "Programming Language :: Python :: 3.6",
+            "Development Status :: 4 - Beta",
+            "Intended Audience :: Information Technology",
+            "Intended Audience :: System Administrators",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+            "Topic :: Software Development :: Version Control :: Git",
+      ],
+      packages=find_packages(),
+      install_requires=[
+            'requests==2.18.4',
+            'pyyaml==3.12',
+      ],
+      tests_requires=[
+            'pytest',
+      ],
+      scripts=[
+            'bin/gitlabform',
+      ],
+      )
