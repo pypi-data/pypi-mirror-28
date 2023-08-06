e25b3f3b7be9abbe804ccd2460ac7185f65cedb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:00 +0000

    Add package pymarshal/1.6.0/pymarshal-1.6.0.tar.gz

---
+++ packages/pymarshal/pymarshal-1.6.0.tar.gz/pymarshal-1.6.0/pymarshal/util/init_args.py
@@ -0,0 +1,36 @@
+"""
+
+"""
+
+import inspect
+import sys
+import types
+
+
+def init_args(cls):
+    """ Return the __init__ args (minus 'self') for @cls
+
+    Args:
+        cls: class, instance or callable
+    Returns:
+        list of str, the arguments minus 'self'
+    """
+    # This looks insanely goofy, but seems to literally be the
+    # only thing that actually works.  Your obvious ways to
+    # accomplish this task do not apply here.
+    try:
+        # Assume it's a factory function, static method, or other callable
+        argspec = inspect.getargspec(cls)
+        args = argspec.args
+    except TypeError:
+        # assume it's a class
+        argspec = inspect.getargspec(cls.__init__)
+        args = argspec.args
+
+    # Note:  There is a special place in hell for people who don't
+    #        call the first method argument 'self'.
+    if args[0] == 'self':
+        args.remove('self')
+
+    return args
+
