e0f4456efc292ee064df2937110a0e1e4fe252f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:20:15 +0000

    Add package npstreams/1.3.0/npstreams-1.3.0-py3-none-any.whl

---
+++ packages/npstreams/npstreams-1.3.0-py3-none-any.whl/npstreams/__init__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+__author__ = 'Laurent P. René de Cotret'
+__email__ = 'laurent.renedecotret@mail.mcgill.ca'
+__license__ = 'BSD'
+__version__ = '1.3.0'
+
+# Order of import is important
+# because of inter-dependency
+from .utils import deprecated
+
+from .array_stream import array_stream, ipipe, iload
+from .array_utils import nan_to_num
+from .linalg import idot, itensordot, ieinsum, iinner
+from .parallel import pmap, pmap_unordered, preduce
+from .iter_utils import cyclic, last, chunked, multilinspace, linspace, peek, itercopy, primed
+from .reduce import ireduce_ufunc, preduce_ufunc, reduce_ufunc
+from .stacking import istack, iflatten
+from .stats import (iaverage, average, imean, inanmean, mean, istd, inanstd, std, 
+                    ivar, inanvar, var, isem, sem, ihistogram)
+from .numerics import isum, inansum, iprod, inanprod, isub, iall, iany, imax, imin
---
+++ packages/npstreams/npstreams-1.3.0-py3-none-any.whl/npstreams/array_stream.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+"""
+Utilities
+---------
+"""
+from functools import partial, wraps
+from glob import iglob
+
+from numpy import asarray, atleast_1d, ndarray
+
+from .parallel import pmap
+
+
+def array_stream(func):
+    """ 
+    Decorates streaming functions to make sure that the stream
+    is a stream of ndarrays. Objects that are not arrays are transformed 
+    into arrays. If the stream is in fact a single ndarray, this ndarray 
+    is repackaged into a sequence of length 1.
+
+    The first argument of the decorated function is assumed to be an iterable of
+    arrays, or an iterable of objects that can be casted to arrays.
+    """
+    @wraps(func)    # thanks functools
+    def decorated(arrays, *args, **kwargs):
+        if isinstance(arrays, ndarray):
+            arrays = (arrays,)
+        return func(map(atleast_1d, arrays), *args, **kwargs)
+    return decorated
+
+def iload(files, load_func, **kwargs):
+    """
+    Create a stream of arrays from files, which are loaded lazily.
+
+    Parameters
+    ----------
+    pattern : iterable of str or str
+        Either an iterable of filenames or a glob-like pattern str.
+    load_func : callable, optional
+        Function taking a filename as its first arguments
+    kwargs
+        Keyword arguments are passed to ``load_func``.
+    
+    Yields
+    ------
+    arr: `~numpy.ndarray`
+        Loaded data. 
+    """
+    if isinstance(files, str):
+        files = iglob(files)
+    files = iter(files)
+
+    yield from map(partial(load_func, **kwargs), files)
+
+# pmap does not support local functions
+def _pipe(funcs, array):
+    for func in funcs:
+        array = func(array)
+    return array
+
+def ipipe(*args, **kwargs):
+    """
+    Pipe arrays through a sequence of functions. For example:
+
+    ``pipe(f, g, h, stream)`` is equivalent to ::
+
+        for arr in stream:
+            yield f(g(h(arr)))
+    
+    Parameters
+    ----------
+    *funcs : callable
+        Callable that support Numpy arrays in their first argument. These
+        should *NOT* be generator functions.
+    arrays : iterable
+        Stream of arrays to be passed.
+    processes : int or None, optional, keyword-only
+        Number of processes to use. If `None`, maximal number of processes
+        is used. Default is one.
+    ntotal : int or None, optional, keyword-only
+        If the length of `arrays` is known, but passing `arrays` as a list
+        would take too much memory, the total number of arrays `ntotal` can be specified. This
+        allows for `pmap` to chunk better in case of ``processes > 1``.
+    
+    Yields
+    ------
+    piped : ndarray
+    """
+    arrays = map(asarray, args[-1])
+    functions = tuple(reversed(args[:-1]))
+    yield from pmap(partial(_pipe, functions), arrays, **kwargs)
