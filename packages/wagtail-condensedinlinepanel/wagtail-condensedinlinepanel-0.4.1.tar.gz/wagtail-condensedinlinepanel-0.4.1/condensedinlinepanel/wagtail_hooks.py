83dc6e026ae815388effa705be06e9901414b893
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:03 +0000

    Add package wagtail-condensedinlinepanel/0.4.1/wagtail-condensedinlinepanel-0.4.1.tar.gz

---
+++ packages/wagtail-condensedinlinepanel/wagtail-condensedinlinepanel-0.4.1.tar.gz/wagtail-condensedinlinepanel-0.4.1/condensedinlinepanel/wagtail_hooks.py
@@ -0,0 +1,22 @@
+from __future__ import absolute_import, unicode_literals
+
+from django.contrib.staticfiles.templatetags.staticfiles import static
+
+import wagtail.VERSION
+
+if wagtail.VERSION >= (2, 0):
+    from wagtail.core import hooks
+else:
+    from wagtail.wagtailcore import hooks
+
+
+# JS/CSS for custom edit handlers
+
+@hooks.register('insert_editor_js')
+def editor_js():
+    return '<script src="{0}"></script>'.format(static('condensedinlinepanel/dist/condensedinlinepanel.bundle.js'))
+
+
+@hooks.register('insert_editor_css')
+def editor_css():
+    return '<link rel="stylesheet" href="{0}">'.format(static('condensedinlinepanel/dist/condensedinlinepanel.css'))
