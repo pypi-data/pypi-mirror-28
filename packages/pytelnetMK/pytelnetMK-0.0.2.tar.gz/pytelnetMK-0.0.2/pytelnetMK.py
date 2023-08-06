a616762001fee08126be1bf786a4417db4566114
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:46 +0000

    Add package pytelnetMK/0.0.2/pytelnetMK-0.0.2.tar.gz

---
+++ packages/pytelnetMK/pytelnetMK-0.0.2.tar.gz/pytelnetMK-0.0.2/pytelnetMK.py
@@ -0,0 +1,29 @@
+import telnetlib
+import sys
+
+class PytelnetMK():
+
+    
+    def __init__(self,host,user,password):
+        self.ldados = [host, user, password]
+
+    def getConn(self):
+    
+        tn = telnetlib.Telnet(self.ldados[0])	
+        #tn.set_debuglevel(1000)
+        tn.read_until(b"Login:")
+        tn.write(self.ldados[1].encode('ascii') + b"\n")
+        if self.ldados[2]:
+            tn.read_until(b"Password:")
+            tn.write(self.ldados[2].encode('ascii') + b"\n")
+            print("Connected!!!")
+        return tn
+
+    def setCmd(self,cmd):
+        setcmd = self.getConn()
+        setcmd.read_until(b"ik] >")
+        setcmd.write(cmd.encode('ascii') + b"\r")
+        print (setcmd.read_until(b"after").decode('UTF8')) 
+        
+
+
