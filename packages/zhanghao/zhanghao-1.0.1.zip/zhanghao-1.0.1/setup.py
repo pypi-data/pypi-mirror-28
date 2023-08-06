0b0a45117382725174e07e726bd1c7b263405ee6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:19:25 +0000

    Add package zhanghao/1.0.1/zhanghao-1.0.1.zip

---
+++ packages/zhanghao/zhanghao-1.0.1.zip/zhanghao-1.0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name        = "zhanghao",
+    version     = "1.0.1",
+    py_modules  = ["zhanghao"],
+    author      = "leo",
+    author_mail = "michal314@163.com",
+    url         = "http://www.headfirstlabs.com",
+    description = "A simple printer of nested lists",
+    )
+    
+    
+    
