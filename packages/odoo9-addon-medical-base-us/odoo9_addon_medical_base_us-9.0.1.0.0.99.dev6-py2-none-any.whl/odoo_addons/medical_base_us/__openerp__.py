26b51f3e6232d5c5807ff7006fc6bc594c90b143
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:04:05 +0000

    Add package odoo9-addon-medical-base-us/9.0.1.0.0.99.dev6/odoo9_addon_medical_base_us-9.0.1.0.0.99.dev6-py2-none-any.whl

---
+++ packages/odoo9-addon-medical-base-us/odoo9_addon_medical_base_us-9.0.1.0.0.99.dev6-py2-none-any.whl/odoo_addons/medical_base_us/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 LasLabs Inc.
+# License GPL-3.0 or later (http://www.gnu.org/licenses/gpl.html).
+
+{
+    "name": "Medical Base - US",
+    "summary": "Provides some helper mixins for US locale.",
+    "version": "9.0.1.0.0",
+    "category": "Medical",
+    "website": "https://laslabs.com",
+    "author": "LasLabs, Odoo Community Association (OCA)",
+    "license": "GPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "base",
+    ],
+}
