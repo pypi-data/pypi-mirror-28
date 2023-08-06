33ae38ed2fa7b91b48a7347d211e409ea8585add
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:25 +0000

    Add package MechanicalSoup/0.10.0/MechanicalSoup-0.10.0.tar.gz

---
+++ packages/MechanicalSoup/MechanicalSoup-0.10.0.tar.gz/MechanicalSoup-0.10.0/setup.py
@@ -0,0 +1,67 @@
+from setuptools import setup  # Always prefer setuptools over distutils
+from codecs import open  # To use a consistent encoding
+from os import path
+import re
+
+
+def requirements_from_file(filename):
+    """Parses a pip requirements file into a list."""
+    return [line.strip() for line in open(filename, 'r')
+            if line.strip() and not line.strip().startswith('--')]
+
+
+def read(fname, URL):
+    """Read the content of a file."""
+    readme = open(path.join(path.dirname(__file__), fname)).read()
+    if hasattr(readme, 'decode'):
+        # In Python 3, turn bytes into str.
+        readme = readme.decode('utf8')
+    # turn relative links into absolute ones
+    readme = re.sub(r'`<([^>]*)>`__',
+                    r'`\1 <' + URL + r"/blob/master/\1>`__",
+                    readme)
+    return readme
+
+
+here = path.abspath(path.dirname(__file__))
+
+about = {}
+with open(path.join(here, 'mechanicalsoup', '__version__.py'),
+          'r', 'utf-8') as f:
+    exec(f.read(), about)
+
+setup(
+    name=about['__title__'],
+
+    # useful: python setup.py sdist bdist_wheel upload
+    version=about['__version__'],
+
+    description=about['__description__'],
+    long_description=read('README.rst', about['__github_url__']),
+    url=about['__url__'],
+
+    license=about['__license__'],
+
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+
+    packages=['mechanicalsoup'],
+
+    # List run-time dependencies here. These will be installed by pip
+    # when your project is installed. For an analysis of
+    # "install_requires" vs pip's requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    install_requires=requirements_from_file('requirements.txt'),
+    setup_requires=['pytest-runner'],
+    tests_require=requirements_from_file('tests/requirements.txt'),
+)
---
+++ packages/MechanicalSoup/MechanicalSoup-0.10.0.tar.gz/MechanicalSoup-0.10.0/tests/test_form.py
@@ -0,0 +1,370 @@
+import setpath  # noqa:F401, must come before 'import mechanicalsoup'
+import mechanicalsoup
+from utils import setup_mock_browser
+import sys
+import pytest
+
+
+def test_submit_online(httpbin):
+    """Complete and submit the pizza form at http://httpbin.org/forms/post """
+    browser = mechanicalsoup.Browser()
+    page = browser.get(httpbin + "/forms/post")
+    form = mechanicalsoup.Form(page.soup.form)
+
+    input_data = {"custname": "Philip J. Fry"}
+    form.input(input_data)
+
+    check_data = {"size": "large", "topping": ["cheese"]}
+    form.check(check_data)
+    check_data = {"size": "medium", "topping": "onion"}
+    form.check(check_data)
+
+    form.textarea({"comments": "warm"})
+    form.textarea({"comments": "actually, no, not warm"})
+    form.textarea({"comments": "freezer"})
+
+    response = browser.submit(form, page.url)
+
+    # helpfully the form submits to http://httpbin.org/post which simply
+    # returns the request headers in json format
+    json = response.json()
+    data = json["form"]
+    assert data["custname"] == "Philip J. Fry"
+    assert data["custtel"] == ""  # web browser submits "" for input left blank
+    assert data["size"] == "medium"
+    assert data["topping"] == ["cheese", "onion"]
+    assert data["comments"] == "freezer"
+
+
+def test_submit_set(httpbin):
+    """Complete and submit the pizza form at http://httpbin.org/forms/post """
+    browser = mechanicalsoup.Browser()
+    page = browser.get(httpbin + "/forms/post")
+    form = mechanicalsoup.Form(page.soup.form)
+
+    form["custname"] = "Philip J. Fry"
+
+    form["size"] = "medium"
+    form["topping"] = ("cheese", "onion")
+
+    form["comments"] = "freezer"
+
+    response = browser.submit(form, page.url)
+
+    # helpfully the form submits to http://httpbin.org/post which simply
+    # returns the request headers in json format
+    json = response.json()
+    data = json["form"]
+    assert data["custname"] == "Philip J. Fry"
+    assert data["custtel"] == ""  # web browser submits "" for input left blank
+    assert data["size"] == "medium"
+    assert data["topping"] == ["cheese", "onion"]
+    assert data["comments"] == "freezer"
+
+
+@pytest.mark.parametrize("expected_post", [
+    pytest.param(
+        [
+            ('comment', 'Testing preview page'),
+            ('preview', 'Preview Page'),
+            ('text', 'Setting some text!')
+        ], id='preview'),
+    pytest.param(
+        [
+            ('comment', 'Created new page'),
+            ('save', 'Submit changes'),
+            ('text', '= Heading =\n\nNew page here!\n')
+        ], id='save'),
+    pytest.param(
+        [
+            ('comment', 'Testing choosing cancel button'),
+            ('cancel', 'Cancel'),
+            ('text', '= Heading =\n\nNew page here!\n')
+        ], id='cancel'),
+])
+def test_choose_submit(expected_post):
+    browser, url = setup_mock_browser(expected_post=expected_post)
+    browser.open(url)
+    form = browser.select_form('#choose-submit-form')
+    browser['text'] = expected_post[2][1]
+    browser['comment'] = expected_post[0][1]
+    form.choose_submit(expected_post[1][0])
+    res = browser.submit_selected()
+    assert(res.status_code == 200 and res.text == 'Success!')
+
+
+choose_submit_fail_form = '''
+<html>
+  <form id="choose-submit-form">
+    <input type="submit" name="test_submit" value="Test Submit" />
+  </form>
+</html>
+'''
+
+
+@pytest.mark.parametrize("select_name", [
+    pytest.param({'name': 'does_not_exist', 'fails': True}, id='not found'),
+    pytest.param({'name': 'test_submit', 'fails': False}, id='found'),
+])
+def test_choose_submit_fail(select_name):
+    browser = mechanicalsoup.StatefulBrowser()
+    browser.open_fake_page(choose_submit_fail_form)
+    form = browser.select_form('#choose-submit-form')
+    if select_name['fails']:
+        with pytest.raises(mechanicalsoup.utils.LinkNotFoundError):
+            form.choose_submit(select_name['name'])
+    else:
+        form.choose_submit(select_name['name'])
+
+
+choose_submit_multiple_match_form = '''
+<html>
+  <form id="choose-submit-form">
+    <input type="submit" name="test_submit" value="First Submit" />
+    <input type="submit" name="test_submit" value="Second Submit" />
+  </form>
+</html>
+'''
+
+
+def test_choose_submit_multiple_match():
+    browser = mechanicalsoup.StatefulBrowser()
+    browser.open_fake_page(choose_submit_multiple_match_form)
+    form = browser.select_form('#choose-submit-form')
+    with pytest.raises(mechanicalsoup.utils.LinkNotFoundError):
+        form.choose_submit('test_submit')
+
+
+submit_form_noaction = '''
+<html>
+  <body>
+    <form id="choose-submit-form">
+      <input type="text" name="text1" value="someValue1" />
+      <input type="text" name="text2" value="someValue2" />
+      <input type="submit" name="save" />
+    </form>
+  </body>
+</html>
+'''
+
+
+def test_form_noaction():
+    browser, url = setup_mock_browser()
+    browser.open_fake_page(submit_form_noaction, url=url)
+    form = browser.select_form('#choose-submit-form')
+    form['text1'] = 'newText1'
+    res = browser.submit_selected()
+    assert(res.status_code == 200 and browser.get_url() == url)
+
+
+submit_form_action = '''
+<html>
+  <body>
+    <form id="choose-submit-form" action="mock://form.com">
+      <input type="text" name="text1" value="someValue1" />
+      <input type="text" name="text2" value="someValue2" />
+      <input type="submit" name="save" />
+    </form>
+  </body>
+</html>
+'''
+
+
+def test_form_action():
+    browser, url = setup_mock_browser()
+    # for info about example.com see: https://tools.ietf.org/html/rfc2606
+    browser.open_fake_page(submit_form_action,
+                           url="http://example.com/invalid/")
+    form = browser.select_form('#choose-submit-form')
+    form['text1'] = 'newText1'
+    res = browser.submit_selected()
+    assert(res.status_code == 200 and browser.get_url() == url)
+
+
+set_select_form = '''
+<html>
+  <form method="post" action="mock://form.com/post">
+    <select name="entree">
+      <option value="tofu" selected="selected">Tofu Stir Fry</option>
+      <option value="curry">Red Curry</option>
+      <option value="tempeh">Tempeh Tacos</option>
+    </select>
+    <input type="submit" value="Select" />
+  </form>
+</html>
+'''
+
+
+@pytest.mark.parametrize("option", [
+    pytest.param({'result': [('entree', 'tofu')], 'default': True},
+                 id='default'),
+    pytest.param({'result': [('entree', 'curry')], 'default': False},
+                 id='selected'),
+])
+def test_set_select(option):
+    '''Test the branch of Form.set that finds "select" elements.'''
+    browser, url = setup_mock_browser(expected_post=option['result'],
+                                      text=set_select_form)
+    browser.open(url)
+    browser.select_form('form')
+    if not option['default']:
+        browser[option['result'][0][0]] = option['result'][0][1]
+    res = browser.submit_selected()
+    assert(res.status_code == 200 and res.text == 'Success!')
+
+
+set_select_multiple_form = '''
+<form method="post" action="mock://form.com/post">
+  <select name="instrument" multiple>
+    <option value="piano">Piano</option>
+    <option value="bass">Bass</option>
+    <option value="violin">Violin</option>
+  </select>
+  <input type="submit" value="Select Multiple" />
+</form>
+'''
+
+
+@pytest.mark.parametrize("options", [
+    pytest.param('bass', id='select one (str)'),
+    pytest.param(('bass',), id='select one (tuple)'),
+    pytest.param(('piano', 'violin'), id='select two'),
+])
+def test_set_select_multiple(options):
+    """Test a <select multiple> element."""
+    # When a browser submits multiple selections, the qsl looks like:
+    #  name=option1&name=option2
+    if not isinstance(options, list) and not isinstance(options, tuple):
+        expected = (('instrument', options),)
+    else:
+        expected = (('instrument', option) for option in options)
+    browser, url = setup_mock_browser(expected_post=expected,
+                                      text=set_select_multiple_form)
+    browser.open(url)
+    form = browser.select_form('form')
+    form.set_select({'instrument': options})
+    res = browser.submit_selected()
+    assert(res.status_code == 200 and res.text == 'Success!')
+
+
+def test_form_not_found():
+    browser = mechanicalsoup.StatefulBrowser()
+    browser.open_fake_page(page_with_various_fields)
+    form = browser.select_form('form')
+    with pytest.raises(mechanicalsoup.utils.LinkNotFoundError):
+        form.input({'foo': 'bar', 'nosuchname': 'nosuchval'})
+    with pytest.raises(mechanicalsoup.utils.LinkNotFoundError):
+        form.check({'foo': 'bar', 'nosuchname': 'nosuchval'})
+    with pytest.raises(mechanicalsoup.utils.LinkNotFoundError):
+        form.check({'entree': 'cheese'})
+    with pytest.raises(mechanicalsoup.utils.LinkNotFoundError):
+        form.check({'topping': 'tofu'})
+    with pytest.raises(mechanicalsoup.utils.LinkNotFoundError):
+        form.textarea({'bar': 'value', 'foo': 'nosuchval'})
+    with pytest.raises(mechanicalsoup.utils.LinkNotFoundError):
+        form.set_radio({'size': 'tiny'})
+    with pytest.raises(mechanicalsoup.utils.LinkNotFoundError):
+        form.set_select({'entree': ('no_multiple', 'no_multiple')})
+
+
+page_with_radio = '''
+<html>
+  <form method="post">
+    <input type=checkbox name="foo" value="bacon"> This is a checkbox
+  </form>
+</html>
+'''
+
+
+def test_form_check_uncheck():
+    browser = mechanicalsoup.StatefulBrowser()
+    browser.open_fake_page(page_with_radio, url="http://example.com/invalid/")
+    form = browser.select_form('form')
+    assert "checked" not in form.form.find("input", {"name": "foo"}).attrs
+
+    form["foo"] = True
+    assert form.form.find("input", {"name": "foo"}).attrs["checked"] == ""
+
+    # Test explicit unchecking (skipping the call to Form.uncheck_all)
+    form.set_checkbox({"foo": False}, uncheck_other_boxes=False)
+    assert "checked" not in form.form.find("input", {"name": "foo"}).attrs
+
+
+page_with_various_fields = '''
+<html>
+  <form method="post">
+    <input name="foo">
+    <textarea name="bar">
+    </textarea>
+    <select name="entree">
+      <option value="tofu" selected="selected">  Tofu Stir Fry </option>
+      <option value="curry">    Red Curry</option>
+      <option value="tempeh">Tempeh Tacos    </option>
+    </select>
+    <fieldset>
+     <legend> Pizza Toppings </legend>
+     <p><label> <input type=checkbox name="topping"
+      value="bacon"> Bacon </label></p>
+     <p><label> <input type=checkbox name="topping"
+      value="cheese" checked>Extra Cheese   </label></p>
+     <p><label> <input type=checkbox name="topping"
+      value="onion" checked> Onion </label></p>
+     <p><label> <input type=checkbox name="topping"
+      value="mushroom"> Mushroom </label></p>
+    </fieldset>
+    <p><input name="size" type=radio value="small">Small</p>
+    <p><input name="size" type=radio value="medium">Medium</p>
+    <p><input name="size" type=radio value="large">Large</p>
+    <input type="submit" value="Select" />
+  </form>
+</html>
+'''
+
+
+def test_form_print_summary(capsys):
+    browser = mechanicalsoup.StatefulBrowser()
+    browser.open_fake_page(page_with_various_fields,
+                           url="http://example.com/invalid/")
+    browser.select_form("form")
+    browser.get_current_form().print_summary()
+    out, err = capsys.readouterr()
+    # Different versions of bs4 show either <input></input> or
+    # <input/>. Normalize before comparing.
+    out = out.replace('></input>', '/>')
+    assert out == """<input name="foo"/>
+<textarea name="bar"></textarea>
+<select name="entree">
+<option selected="selected" value="tofu">Tofu Stir Fry</option>
+<option value="curry">Red Curry</option>
+<option value="tempeh">Tempeh Tacos</option>
+</select>
+<input name="topping" type="checkbox" value="bacon"/>
+<input checked="" name="topping" type="checkbox" value="cheese"/>
+<input checked="" name="topping" type="checkbox" value="onion"/>
+<input name="topping" type="checkbox" value="mushroom"/>
+<input name="size" type="radio" value="small"/>
+<input name="size" type="radio" value="medium"/>
+<input name="size" type="radio" value="large"/>
+<input type="submit" value="Select"/>
+"""
+    assert err == ""
+
+
+def test_issue180():
+    """Test that a KeyError is not raised when Form.choose_submit is called
+    on a form where a submit element is missing its name-attribute."""
+    browser = mechanicalsoup.StatefulBrowser()
+    html = '''
+<form>
+  <input type="submit" value="Invalid" />
+  <input type="submit" name="valid" value="Valid" />
+</form>
+'''
+    browser.open_fake_page(html)
+    form = browser.select_form()
+    with pytest.raises(mechanicalsoup.utils.LinkNotFoundError):
+        form.choose_submit('not_found')
+
+
+if __name__ == '__main__':
+    pytest.main(sys.argv)
