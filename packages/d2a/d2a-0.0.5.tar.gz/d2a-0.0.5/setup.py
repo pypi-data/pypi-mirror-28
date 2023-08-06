e827a8c4f900889a45296cfb68849da528b91a6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:33 +0000

    Add package d2a/0.0.5/d2a-0.0.5.tar.gz

---
+++ packages/d2a/d2a-0.0.5.tar.gz/d2a-0.0.5/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python',
+    'Topic :: Software Development',
+]
+keywords = [
+    'django',
+    'sqlalchemy',
+    'model',
+    'mapping',
+    'decralation',
+    'table',
+]
+
+setup(
+    name='d2a',
+    version='0.0.5',
+    install_requires=['Django>=1.9', 'SQLAlchemy>=0.9'],
+    description='it converts a django model to a sqlalchemy declaration',
+    long_description=open('./README.rst', 'r').read(),
+    classifiers=classifiers,
+    keywords=', '.join(keywords),
+    author='righ',
+    author_email="righ.m9@gmail.com,crohaco@beproud.jp",
+    packages=find_packages(exclude=['tests']),
+)
+
