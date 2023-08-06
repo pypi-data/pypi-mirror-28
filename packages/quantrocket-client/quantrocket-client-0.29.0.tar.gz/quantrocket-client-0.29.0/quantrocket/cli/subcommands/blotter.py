1a8188b0dc5592cf4ae0ac504fffb80a60dee583
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:03:36 +0000

    Add package quantrocket-client/0.29.0/quantrocket-client-0.29.0.tar.gz

---
+++ packages/quantrocket-client/quantrocket-client-0.29.0.tar.gz/quantrocket-client-0.29.0/quantrocket/cli/subcommands/blotter.py
@@ -0,0 +1,191 @@
+# Copyright 2018 QuantRocket - All Rights Reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import argparse
+from quantrocket.cli.utils.parse import dict_str
+
+def add_subparser(subparsers):
+    _parser = subparsers.add_parser("blotter", description="QuantRocket blotter CLI", help="quantrocket blotter -h")
+    _subparsers = _parser.add_subparsers(title="subcommands", dest="subcommand")
+    _subparsers.required = True
+
+    examples = """
+Place one or more orders.
+
+Returns a list of order IDs, which can be used to cancel the orders or check
+their status.
+
+Examples:
+
+Place orders from a CSV file.
+
+    quantrocket blotter order -f orders.csv
+
+Place orders from a JSON file.
+
+    quantrocket blotter order -f orders.json
+
+Place an order by specifying the order parameters on the command line:
+
+    quantrocket blotter order --params ConId:123456 Action:BUY Quantity:100 OrderType:MKT Tif:Day Account:DU12345 OrderRef:my-strategy
+    """
+    parser = _subparsers.add_parser(
+        "order",
+        help="place one or more orders",
+        epilog=examples,
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+    source_group = parser.add_mutually_exclusive_group()
+    source_group.add_argument(
+        "-f", "--infile",
+        metavar="INFILE",
+        dest="infilepath_or_buffer",
+        help="place orders from this CSV or JSON file (specify '-' to read file "
+            "from stdin)")
+    source_group.add_argument(
+        "-p", "--params",
+        nargs="*",
+        type=dict_str,
+        metavar="PARAM:VALUE",
+        help="order details as multiple key-value pairs (pass as 'param:value', for "
+        "example OrderType:MKT)")
+    parser.set_defaults(func="quantrocket.blotter._cli_place_orders")
+
+    examples = """
+Cancel one or more orders by order ID, conid, or strategy (order ref).
+
+Examples:
+
+Cancel orders by order ID:
+
+    quantrocket blotter cancel -o DU12345:7002:45 DU12345:7002:46
+
+Cancel orders by conid:
+
+    quantrocket blotter cancel -i 123456
+
+Cancel orders by strategy (order ref):
+
+    quantrocket blotter cancel -s my-strategy
+
+Cancel all open orders:
+
+    quantrocket blotter cancel --all
+    """
+    parser = _subparsers.add_parser(
+        "cancel",
+        help="cancel one or more orders by order ID, conid, or strategy (order ref)",
+        epilog=examples,
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument(
+        "-o", "--order-ids",
+        metavar="ORDERID",
+        nargs="*",
+        help="cancel these order IDs")
+    parser.add_argument(
+        "-i", "--conids",
+        type=int,
+        nargs="*",
+        metavar="CONID",
+        help="cancel orders for these conids")
+    parser.add_argument(
+        "-s", "--strategies",
+        nargs="*",
+        metavar="CODE",
+        help="cancel orders for these strategy codes")
+    parser.add_argument(
+        "--all",
+        action="store_true",
+        default=False,
+        dest="cancel_all",
+        help="cancel all open orders")
+    parser.set_defaults(func="quantrocket.blotter._cli_cancel_orders")
+
+    examples = """
+List order status for one or more orders by order ID, conid, or strategy (order ref).
+
+Examples:
+
+List order status by order ID:
+
+    quantrocket blotter status -o DU12345:7002:45 DU12345:7002:46
+
+List order status by conid:
+
+    quantrocket blotter status -i 123456
+
+List order status by strategy (order ref):
+
+    quantrocket blotter status -s my-strategy
+
+List order status for all open orders:
+
+    quantrocket blotter status
+    """
+    parser = _subparsers.add_parser(
+        "status",
+        help="List order status for one or more orders by order ID, conid, or "
+        "strategy (order ref)",
+        epilog=examples,
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument(
+        "-o", "--order-ids",
+        metavar="ORDERID",
+        nargs="*",
+        help="limit to these order IDs")
+    parser.add_argument(
+        "-i", "--conids",
+        type=int,
+        nargs="*",
+        metavar="CONID",
+        help="limit to orders for these conids")
+    parser.add_argument(
+        "-s", "--strategies",
+        nargs="*",
+        metavar="CODE",
+        help="limit to orders for these strategy codes")
+    parser.set_defaults(func="quantrocket.blotter._cli_list_order_statuses")
+
+    #parser = _subparsers.add_parser("active", help="list active orders")
+    #parser.add_argument("-s", "--strategies", nargs="*", metavar="CODE", help="limit to these strategies (= order refs)")
+    #parser.add_argument("-a", "--accounts", nargs="*", metavar="ACCOUNT", help="limit to these accounts")
+    #parser.add_argument("--diff-positions", action="store_true", help="only show orders which don't match up to an existing position")
+    #parser.set_defaults(func="quantrocket.blotter.get_active_orders")
+
+    #parser = _subparsers.add_parser("positions", help="get current positions from the blotter database")
+    #parser.add_argument("-s", "--strategies", nargs="*", metavar="CODE", help="limit to these strategies")
+    #parser.add_argument("-a", "--accounts", nargs="*", metavar="ACCOUNT", help="limit to these accounts")
+    #parser.add_argument("--diff-orders", action="store_true", help="only show positions which don't match up to one or more existing orders")
+    #parser.set_defaults(func="quantrocket.blotter.get_positions")
+
+    #parser = _subparsers.add_parser("rollover", help="generate orders to rollover futures contracts based on rollover rules")
+    #parser.add_argument("-s", "--strategies", nargs="*", metavar="CODE", help="limit to these strategies")
+    #parser.add_argument("-a", "--accounts", nargs="*", metavar="ACCOUNT", help="limit to these accounts")
+    #parser.add_argument("-r", "--rules", nargs="*", metavar="KEY:VALUE", help="rollover rules as multiple key-value pairs in relativedelta format (e.g. days=-8) (omit to use rollover rules defined in master service)")
+    #parser.set_defaults(func="quantrocket.blotter.rollover_positions")
+
+    #parser = _subparsers.add_parser("close", help="generate orders to close positions")
+    #parser.add_argument("-s", "--strategies", nargs="*", metavar="CODE", help="limit to these strategies")
+    #parser.add_argument("-c", "--conids", nargs="*", metavar="CONID", help="limit to these conids")
+    #parser.add_argument("-a", "--accounts", nargs="*", metavar="ACCOUNT", help="limit to these accounts")
+    #parser.add_argument("-o", "--order", nargs="+", metavar="FIELD:VALUE", help="order details as JSON or as multiple key-value pairs (e.g. orderType:MKT tif:DAY)")
+    #parser.add_argument("--oca", dest="oca_suffix", metavar="SUFFIX", help="create OCA group containing client ID, order ID, and this suffix (run this command multiple times with this option to create OCA orders)")
+    #parser.set_defaults(func="quantrocket.blotter.close_positions")
+
+    #parser = _subparsers.add_parser("pnl", help="query live trading results from the blotter database")
+    #parser.add_argument("start_date", metavar="YYYY-MM-DD", help="start date")
+    #parser.add_argument("end_date", nargs="?", metavar="YYYY-MM-DD", help="end date (optional)")
+    #parser.add_argument("-s", "--strategies", nargs="+", metavar="CODE", help="one or more strategies to show performance for")
+    #parser.add_argument("-a", "--account", help="the account to show performance for (if not provided, the default account registered with the account service will be used)")
+    #parser.add_argument("-w", "--raw", action="store_true", help="return raw performance data instead of a performance tearsheet")
+    #parser.set_defaults(func="quantrocket.blotter.get_pnl")
---
+++ packages/quantrocket-client/quantrocket-client-0.29.0.tar.gz/quantrocket-client-0.29.0/quantrocket/houston.py
@@ -0,0 +1,109 @@
+# Copyright 2017 QuantRocket - All Rights Reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import requests
+from .exceptions import ImproperlyConfigured
+from quantrocket.cli.utils.output import json_to_cli
+
+class Houston(requests.Session):
+    """
+    Subclass of `requests.Session` that provides an interface to the houston
+    API gateway. Reads HOUSTON_URL (and Basic Auth credentials if applicable)
+    from environment variables and applies them to each request. Simply provide
+    the path, starting with /, for example:
+
+    >>> response = houston.get("/countdown/crontab")
+
+    Since each instance of Houston is a session, you can improve performance
+    by using a single session for all requests. The module provides an instance
+    of `Houston`, named `houston`.
+
+    Use the same session as other requests:
+
+    >>> from quantrocket.houston import houston
+
+    Use a new session:
+
+    >>> from quantrocket.houston import Houston
+    >>> houston = Houston()
+    """
+
+    DEFAULT_TIMEOUT = 30
+
+    def __init__(self):
+        super(Houston, self).__init__()
+        if "HOUSTON_USERNAME" in os.environ and "HOUSTON_PASSWORD" in os.environ:
+            self.auth = (os.environ["HOUSTON_USERNAME"], os.environ["HOUSTON_PASSWORD"])
+
+    @property
+    def base_url(self):
+        if "HOUSTON_URL" not in os.environ:
+            raise ImproperlyConfigured("HOUSTON_URL is not set")
+        return os.environ["HOUSTON_URL"]
+
+    def request(self, method, url, *args, **kwargs):
+        if url.startswith('/'):
+            url = self.base_url + url
+        timeout = kwargs.get("timeout", None)
+        stream = kwargs.get("stream", None)
+        if timeout is None and not stream:
+            kwargs["timeout"] = self.DEFAULT_TIMEOUT
+
+        # Move conids from params to data if too long
+        conids = kwargs.get("params", {}).get("conids", None)
+        if conids and isinstance(conids, list) and len(conids) > 1:
+            data = kwargs.get("data", {}) or {}
+            data["conids"] = conids
+            kwargs["params"].pop("conids")
+            kwargs["data"] = data
+
+        return super(Houston, self).request(method, url, *args, **kwargs)
+
+    @staticmethod
+    def raise_for_status_with_json(response):
+        """
+        Raises 400/500 error codes, attaching a json response to the
+        exception, if possible.
+        """
+        try:
+            response.raise_for_status()
+        except requests.exceptions.HTTPError as e:
+            try:
+                e.json_response = response.json()
+                e.args = e.args + (e.json_response,)
+            except:
+                e.json_response = {}
+                e.args = e.args + ("please check the logs for more details",)
+            raise e
+
+# Instantiate houston so that all callers can share a TCP connection (for
+# performance's sake)
+houston = Houston()
+
+def ping():
+    """
+    Pings houston.
+
+    Returns
+    -------
+    json
+        reply from houston
+    """
+    response = houston.get("/ping")
+    houston.raise_for_status_with_json(response)
+    return response.json()
+
+def _cli_ping():
+    return json_to_cli(ping)
