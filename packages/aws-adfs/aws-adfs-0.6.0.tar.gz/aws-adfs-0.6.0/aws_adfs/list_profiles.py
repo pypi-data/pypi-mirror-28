8ffcd2e1eaf0b8e1518abf2ea5d7f1bd741a54d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:00 +0000

    Add package aws-adfs/0.6.0/aws-adfs-0.6.0.tar.gz

---
+++ packages/aws-adfs/aws-adfs-0.6.0.tar.gz/aws-adfs-0.6.0/aws_adfs/list_profiles.py
@@ -0,0 +1,31 @@
+import configparser
+
+import click
+
+from .prepare import adfs_config
+
+
+@click.command(name='list')
+def list_profiles():
+    """
+    lists available profiles
+    """
+
+    config = configparser.RawConfigParser()
+    config.read(adfs_config.aws_config_location)
+
+    profiles = config.sections()
+
+    config.read(adfs_config.aws_config_location)
+
+    if len(profiles) < 1:
+        click.echo('No defined profiles')
+    else:
+        click.echo('Available profiles:')
+        for profile in profiles:
+            role_arn = config.get(
+                profile,
+                'adfs_config.role_arn',
+                fallback=''
+            )
+            click.echo(' * {0:<30} | {1}'.format(profile, role_arn))
