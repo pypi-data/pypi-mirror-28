2052c0a11748fc0253a3818b214e2132503df409
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:45 +0000

    Add package django-dataporten/0.1.2/django-dataporten-0.1.2.tar.gz

---
+++ packages/django-dataporten/django-dataporten-0.1.2.tar.gz/django-dataporten-0.1.2/dataporten/middleware.py
@@ -0,0 +1,31 @@
+import logging
+
+import requests
+import requests_cache
+from django.conf import settings
+from django.http import HttpRequest
+
+from .api import usergroups
+from .models import DataportenUser
+
+# Cache requests for 15 minutes
+if settings.DATAPORTEN_CACHE_REQUESTS:
+    requests_cache.install_cache(
+        settings.DATAPORTEN_CACHE_PATH + 'dataporten_cache',
+        backend='sqlite',
+        expire_after=900,
+        allowable_codes=(200,),
+        include_get_headers=True,
+    )
+
+
+class DataportenGroupsMiddleware(object):
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request: HttpRequest):
+        return self.get_response(request)
+
+    def process_view(self, request: HttpRequest, *args, **kwargs):
+        if DataportenUser.valid_request(request):
+            request.user.__class__ = DataportenUser
