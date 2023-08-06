151a043a907ba077b1ec3ae7625628133779129b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:19:53 +0000

    Add package magnetmatter/0.1.5/magnetmatter-0.1.5.tar.gz

---
+++ packages/magnetmatter/magnetmatter-0.1.5.tar.gz/magnetmatter-0.1.5/setup.py
@@ -0,0 +1,17 @@
+# setup.py
+
+from distutils.core import setup
+import os
+
+setup(
+    name='magnetmatter',
+    version='0.1.5',
+    packages=['magnetmatter',os.path.join('magnetmatter','modules')],
+    license='MIT',
+    author='Pelle Garbus',
+    author_email='garbus@inano.au.dk',
+    description='Visualization of FullProf-refined neutron and X-ray powder diffraction data',
+    long_description=open('README.txt').read(),
+    install_requires=["numpy","pandas","matplotlib"],
+    url = 'https://github.com/pgarbus/magnetmatter',
+)
