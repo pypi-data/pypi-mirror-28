d9059a86b18fcc6dc2c5d6e376102b26cdfc087e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:03 +0000

    Add package vmpy/0.1.0/vmpy-0.1.0.tar.gz

---
+++ packages/vmpy/vmpy-0.1.0.tar.gz/vmpy-0.1.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='vmpy',
+    version='0.1.0',
+    author='Maksym Sladkov',
+    author_email='sladkovm@gmail.com',
+    packages=['vmpy'],
+    url='https://github.com/sladkovm/vmpy',
+    license='LICENSE.txt',
+    description='Velo Metrics',
+
+)
