86446c371fe5d4b0d96c0dee0f4e7247ea0bc93d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:42 +0000

    Add package depynd/0.1.3/depynd-0.1.3.tar.gz

---
+++ packages/depynd/depynd-0.1.3.tar.gz/depynd-0.1.3/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='depynd',
+      version='0.1.3',
+      description='Evaluating dependencies among random variables.',
+      author='Yuya Takashina',
+      author_email='takashina2051@gmail.com',
+      packages=['depynd'],
+      )
