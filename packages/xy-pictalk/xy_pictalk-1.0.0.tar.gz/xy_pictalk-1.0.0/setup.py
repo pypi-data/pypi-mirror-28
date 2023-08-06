2b204ad1726eae82c3c5d9e8b2a3897b20cfac21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:35 +0000

    Add package xy-pictalk/1.0.0/xy_pictalk-1.0.0.tar.gz

---
+++ packages/xy-pictalk/xy_pictalk-1.0.0.tar.gz/xy_pictalk-1.0.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'xy_pictalk',
+  packages = ['xy_pictalk'],
+  version = '1.0.0',
+  description = 'picture description',
+  long_description='picture description',
+  author = 'KingHS',
+  author_email = '382771946@qq.com',
+  keywords = ['pip3', 'python3','python','picture talk','pic'],
+  license='MIT',
+  install_requires=['requests']
+)
---
+++ packages/xy-pictalk/xy_pictalk-1.0.0.tar.gz/xy_pictalk-1.0.0/xy_pictalk/__init__.py
@@ -0,0 +1,1 @@
+from .xy_pictalk import *
