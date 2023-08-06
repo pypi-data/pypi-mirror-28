6433459309aebd398d6cf7adcaaec0fb88582284
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:01 +0000

    Add package pyrxnlp/0.1.5.3/pyrxnlp-0.1.5.3.tar.gz

---
+++ packages/pyrxnlp/pyrxnlp-0.1.5.3.tar.gz/pyrxnlp-0.1.5.3/setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup, find_packages
+from subprocess import Popen, PIPE
+import io,os,sys
+
+def tag():
+    #process = Popen (["git", "describe", "--abbrev=0", "--tags"], stdout=PIPE)
+    #tag=process.communicate()[0].strip()
+    #tag=bytes.decode(tag)
+    tag=sys.argv[1]
+    return os.getenv("version")
+
+
+def read_text_lines(fname):
+    with io.open(fname) as fd:
+        lines=fd.readlines()
+        return ''.join(lines)
+
+
+
+setup(
+    name="pyrxnlp",
+    version=tag(),
+    packages=find_packages(),
+    description='Natural language processing tools',
+    long_description=open("README.rst").read(),
+    classifiers=[
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Text Processing :: Linguistic'
+    ],
+    author='RxNLP',
+    author_email='kavita.ganesan@rxnlp.com',
+    license='LGPL',
+    url='https://github.com/RxNLP/pyrxnlp',
+    download_url='https://github.com/RxNLP/pyrxnlp/archive/{0}.tar.gz'.format(tag()),
+    keywords=['Sentence Clustering', 'Topics Extraction',
+              'Opinosis Summarization', 'Text Similarity'],
+    install_requires=[
+        'requests==2.9.1'
+    ],
+    include_package_data=True,
+    entry_points={
+
+    }
+)
