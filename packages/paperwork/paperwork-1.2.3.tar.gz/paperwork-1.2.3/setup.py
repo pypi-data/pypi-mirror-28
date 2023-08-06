39128d2f22e2d261c14ec11db8083c8f449783cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:52:09 +0000

    Add package paperwork/1.2.3/paperwork-1.2.3.tar.gz

---
+++ packages/paperwork/paperwork-1.2.3.tar.gz/paperwork-1.2.3/setup.py
@@ -0,0 +1,188 @@
+#!/usr/bin/env python3
+
+import os
+
+from setuptools import setup, find_packages
+
+LOCALES = [
+    "de",
+    "fr",
+    "uk",
+]
+
+ICON_SIZES = [
+    16,
+    22,
+    24,
+    30,
+    32,
+    36,
+    42,
+    48,
+    50,
+    64,
+    72,
+    96,
+    100,
+    128,
+    150,
+    160,
+    192,
+    256,
+    512,
+]
+
+DOC_PATHS = [
+    "doc/hacking.pdf",
+    "doc/intro_fr.pdf",
+    "doc/intro.pdf",
+    "doc/translating.pdf",
+    "doc/usage.pdf",
+]
+
+packages = find_packages('src') + [
+    'paperwork.frontend.doc',
+]
+package_dir = {
+    '': 'src',
+    'paperwork.frontend.doc': 'doc',
+}
+package_data = {
+    'paperwork.frontend': [
+        'data/paperwork.svg',
+        'data/paperwork_halo.svg',
+    ],
+    'paperwork.frontend.data': ['data/work.openpaper.Paperwork.appdata.xml'],
+    'paperwork.frontend.doc': DOC_PATHS,
+}
+
+extra_deps = []
+
+if os.name == "nt":
+    extra_deps = [
+        "pycrypto"  # used to check the activation keys
+    ]
+
+# include icons
+packages.append("paperwork.frontend.data")
+package_dir['paperwork.frontend.data'] = 'data'
+for size in ICON_SIZES:
+    icon_path = os.path.join("data", 'paperwork_{}.png'.format(size))
+    package_data['paperwork.frontend.data'].append(icon_path)
+
+# include locales
+for locale in LOCALES:
+    mo_dir = os.path.join("locale", locale, "LC_MESSAGES")
+    mo = os.path.join(mo_dir, "paperwork.mo")
+    pkg = "paperwork.frontend.locale.{}.LC_MESSAGES".format(locale)
+    packages.append(pkg)
+    package_dir[pkg] = mo_dir
+    package_data[pkg] = [mo]
+
+setup(
+    name="paperwork",
+    # Before releasing a new version:
+    # * update the download_url in this file
+    # * update the ChangeLog file
+    # * update AUTHORS
+    # * update src/paperwork/frontend/aboutdialog/aboutdialog.glade
+    # * update src/paperwork/frontend/mainwindow/__init__.py:__version__
+    # * update the dependency version on paperwork-backend
+    # * update data/work.openpaper.Paperwork.appdata.xml:<releases>
+    #
+    # Release:
+    # * commit
+    # * tag
+    # * python3 ./setup.py sdist upload
+    #
+    # After the release:
+    # * add a file flatpak/<version>.json
+    # * update flatpak/release.json
+    version="1.2.3",
+    description=(
+        "Using scanner and OCR to grep dead trees the easy way (Linux only)"
+    ),
+    long_description="""Paperwork is a tool to make papers searchable.
+
+The basic idea behind Paperwork is "scan & forget" : You should be able to
+just scan a new document and forget about it until the day you need it
+again.
+Let the machine do most of the work.
+
+Main features are:
+- Scan
+- Automatic orientation detection
+- OCR
+- Indexing
+- Document labels
+- Automatic guessing of the labels to apply on new documents
+- Search
+- Keyword suggestions
+- Quick edit of scans
+- PDF support
+    """,
+    keywords="scanner ocr gui",
+    url="https://github.com/openpaperwork/paperwork",
+    download_url=("https://github.com/openpaperwork/paperwork"
+                  "/archive/1.2.3.tar.gz"),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: X11 Applications :: GTK",
+        "Environment :: X11 Applications :: Gnome",
+        "Intended Audience :: End Users/Desktop",
+        ("License :: OSI Approved ::"
+         " GNU General Public License v3 or later (GPLv3+)"),
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3",
+        "Topic :: Multimedia :: Graphics :: Capture :: Scanners",
+        "Topic :: Multimedia :: Graphics :: Graphics Conversion",
+        "Topic :: Scientific/Engineering :: Image Recognition",
+        "Topic :: Text Processing :: Filters",
+        "Topic :: Text Processing :: Indexing",
+    ],
+    license="GPLv3+",
+    author="Jerome Flesch",
+    author_email="jflesch@openpaper.work",
+    packages=packages,
+    package_dir=package_dir,
+    package_data=package_data,
+    include_package_data=True,
+    entry_points={
+        'gui_scripts': [
+            'paperwork = paperwork.paperwork:main',
+        ]
+    },
+    zip_safe=True,
+    install_requires=[
+        "python-dateutil",
+        "python-Levenshtein",
+        "Pillow",
+        "pycountry",
+        "pyinsane2",
+        "pyocr >= 0.3.0",
+        "pypillowfight",
+        "pyxdg >= 0.25",
+        "termcolor",  # used by paperwork-chkdeps
+        "paperwork-backend>=1.2.3,<1.3",
+        # paperwork-chkdeps take care of all the dependencies that can't be
+        # handled here. For instance:
+        # - Dependencies using gobject introspection
+        # - Dependencies based on language (OCR data files, dictionnaries, etc)
+        # - Dependencies on data files (icons, etc)
+    ] + extra_deps
+)
+
+print("============================================================")
+print("============================================================")
+print("||                       IMPORTANT                        ||")
+print("||                                                        ||")
+print("||                       Please run                       ||")
+print("||--------------------------------------------------------||")
+print("||          paperwork-shell chkdeps paperwork_backend     ||")
+print("||             paperwork-shell chkdeps paperwork          ||")
+print("||                  paperwork-shell install               ||")
+print("||--------------------------------------------------------||")
+print("||             to find any missing dependencies           ||")
+print("||       and install Paperwork's icons and shortcuts      ||")
+print("============================================================")
+print("============================================================")
---
+++ packages/paperwork/paperwork-1.2.3.tar.gz/paperwork-1.2.3/src/paperwork/frontend/aboutdialog/__init__.py
@@ -0,0 +1,56 @@
+#    Paperwork - Using OCR to grep dead trees the easy way
+#    Copyright (C) 2012-2014  Jerome Flesch
+#
+#    Paperwork is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    Paperwork is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with Paperwork.  If not, see <http://www.gnu.org/licenses/>.
+"""
+Contains the code relative to the about dialog (the one you get when you click
+on Help->About)
+"""
+
+import os
+
+from gi.repository import GdkPixbuf
+
+from paperwork.frontend.util import load_uifile
+from paperwork.frontend.util import preload_file
+
+
+class AboutDialog(object):
+
+    """
+    Dialog that appears when you click Help->About.
+
+    By default, this dialog won't be visible. You have to call
+    AboutDialog.show().
+    """
+
+    def __init__(self, main_window):
+        logo_path = preload_file("paperwork.svg")
+        self.__widget_tree = load_uifile(
+            os.path.join("aboutdialog", "aboutdialog.glade"))
+
+        self.__dialog = self.__widget_tree.get_object("aboutdialog")
+        assert(self.__dialog)
+        self.__dialog.set_transient_for(main_window)
+
+        if logo_path and os.access(logo_path, os.F_OK):
+            logo = GdkPixbuf.Pixbuf.new_from_file(logo_path)
+            self.__dialog.set_logo(logo)
+        self.__dialog.connect("response", lambda x, y: x.destroy())
+
+    def show(self):
+        """
+        Make the about dialog appears
+        """
+        self.__dialog.set_visible(True)
