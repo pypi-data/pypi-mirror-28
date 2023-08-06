71cf1b6b0290adf8b25096557f4bb7ea67d1dd68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:03 +0000

    Add package django-fluent-pages/1.1.4/django-fluent-pages-1.1.4.tar.gz

---
+++ packages/django-fluent-pages/django-fluent-pages-1.1.4.tar.gz/django-fluent-pages-1.1.4/makemessages.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+import os
+from os import path
+
+import django
+from django.conf import settings
+from django.core.management import call_command
+
+
+def main():
+    if not settings.configured:
+        module_root = path.dirname(path.realpath(__file__))
+
+        settings.configure(
+            DEBUG = False,
+            INSTALLED_APPS = (
+                'fluent_pages',
+                'mptt',
+            ),
+        )
+
+    if django.VERSION >= (1, 7):
+        django.setup()
+
+    makemessages()
+
+
+def makemessages():
+    os.chdir('fluent_pages')
+    call_command('makemessages', locale=('en', 'nl'), verbosity=1)
+
+if __name__ == '__main__':
+    main()
