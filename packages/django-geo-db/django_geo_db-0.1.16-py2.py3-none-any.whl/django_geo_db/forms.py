556a96899922edbaed6bfef54a0ee4f1f941e04f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:30 +0000

    Add package django-geo-db/0.1.16/django_geo_db-0.1.16-py2.py3-none-any.whl

---
+++ packages/django-geo-db/django_geo_db-0.1.16-py2.py3-none-any.whl/django_geo_db/forms.py
@@ -0,0 +1,50 @@
+from django import forms
+from dal.autocomplete import ModelSelect2
+from django_geo_db.models import UserLocation, City, Location, GeoCoordinate
+from django_geo_db.widgets import GeocoordinateWidget
+
+
+class GeocoordinateForm(forms.ModelForm):
+    class Meta:
+        model = GeoCoordinate
+        fields = [
+            'generated_name',
+            'lat',
+            'lon',
+        ]
+        widgets = {
+            'generated_name': GeocoordinateWidget
+        }
+
+
+class UserLocationForm(forms.ModelForm):
+    class Meta:
+        model = UserLocation
+        widgets = {
+            'location': ModelSelect2(url='location-autocomplete')
+        }
+        fields = '__all__'
+
+
+
+class LocationForm(forms.ModelForm):
+    class Meta:
+        model = Location
+        widgets = {
+            'zipcode': ModelSelect2(url='zipcode-autocomplete'),
+            'city': ModelSelect2(url='city-autocomplete'),
+            'geocoordinate': ModelSelect2(url='geocoordinate-autocomplete'),
+            }
+        fields = '__all__'
+
+
+
+
+class CityForm(forms.ModelForm):
+    class Meta:
+        model = City
+        widgets = {
+            'zipcode': ModelSelect2(url='zipcode-autocomplete'),
+            }
+        fields = '__all__'
+
