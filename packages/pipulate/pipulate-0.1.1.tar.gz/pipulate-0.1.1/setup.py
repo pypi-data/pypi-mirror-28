455da85c7ebd03a57173d7b7135c39e91fd02668
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:16 +0000

    Add package pipulate/0.1.1/pipulate-0.1.1.tar.gz

---
+++ packages/pipulate/pipulate-0.1.1.tar.gz/pipulate-0.1.1/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+setup(
+  name = 'pipulate',
+  packages = ['pipulate'],
+  version = '0.1.1',
+  description = 'Manipulate data in Google Sheets',
+  long_description='Integrate Google Sheets with your Python Pandas data work.',
+  author = 'Mike Levin, SEO',
+  license='MIT',
+  author_email = 'miklevin@gmail.com',
+  url = 'https://github.com/miklevin/pipulate',
+  download_url = 'https://github.com/miklevin/pipulate/archive/0.1.1.tar.gz',
+  keywords = ['data', 'datascience', 'spreadsheet', 'pandas', 'gspread', 'gdata', 'seo']
+)
+
+classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Science/Research',
+	'Topic :: Databasea',
+     'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.2',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.7'
+]
