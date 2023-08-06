e32d22ceb30f4b5a2defd0cc9f55611b167dd779
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:01 +0000

    Add package strangedateutils/0.2/strangedateutils-0.2.tar.gz

---
+++ packages/strangedateutils/strangedateutils-0.2.tar.gz/strangedateutils-0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='strangedateutils',
+    version='0.2',
+    packages=['strangedateutils'],
+    url='https://github.com/huadi/StrangeDateUtils',
+    license='MIT',
+    author='huadi',
+    author_email='huadi223@gmail.com',
+    description="Some date methods have been used in my daily work. It's not common but may help someone."
+)
