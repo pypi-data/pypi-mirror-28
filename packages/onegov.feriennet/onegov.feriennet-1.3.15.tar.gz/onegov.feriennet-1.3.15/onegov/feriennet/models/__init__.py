c9948eb2c93c8e51475edd3d6f847ea52a80eb26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:20:15 +0000

    Add package onegov.feriennet/1.3.15/onegov.feriennet-1.3.15.tar.gz

---
+++ packages/onegov.feriennet/onegov.feriennet-1.3.15.tar.gz/onegov.feriennet-1.3.15/onegov/feriennet/models/__init__.py
@@ -0,0 +1,13 @@
+from onegov.feriennet.models.activity import VacationActivity
+from onegov.feriennet.models.invoice_action import InvoiceAction
+from onegov.feriennet.models.message import ActivityMessage
+from onegov.feriennet.models.message import PeriodMessage
+from onegov.feriennet.models.notification_template import NotificationTemplate
+
+__all__ = [
+    'ActivityMessage',
+    'InvoiceAction',
+    'NotificationTemplate',
+    'PeriodMessage',
+    'VacationActivity'
+]
