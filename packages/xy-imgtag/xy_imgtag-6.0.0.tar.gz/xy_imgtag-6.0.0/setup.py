8bb67d9f2a683d1b6aa141a8d84e0c7e35e58d50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:01 +0000

    Add package xy-imgtag/6.0.0/xy_imgtag-6.0.0.tar.gz

---
+++ packages/xy-imgtag/xy_imgtag-6.0.0.tar.gz/xy_imgtag-6.0.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'xy_imgtag',
+  packages = ['xy_imgtag'],
+  version = '6.0.0',
+  description = 'Recognition Image Tag',
+  long_description='Recognition Image Tag',
+  author = 'KingHS',
+  author_email = '382771946@qq.com',
+  keywords = ['pip3', 'python3','python','image tag','Recognition Image Tag'],
+  license='MIT',
+  install_requires=['requests']
+)
