00a7e4a73372edbce32ab294ebbf9c42102d833d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:08 +0000

    Add package turboactivate/4.0.1/turboactivate-4.0.1.tar.gz

---
+++ packages/turboactivate/turboactivate-4.0.1.tar.gz/turboactivate-4.0.1/turboactivate/__init__.py
@@ -0,0 +1,365 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2013-2018 Develer S.r.l. (https://www.develer.com/)
+#
+# Author: Lorenzo Villani <lvillani@develer.com>
+# Author: Riccardo Ferrazzo <rferrazz@develer.com>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+from datetime import datetime
+
+from ctypes import pointer, sizeof, c_uint32
+
+from .c_wrapper import *
+
+#
+# Object oriented interface
+#
+
+
+class GenuineOptions(object):
+    """A set of options to use with is_genuine()"""
+
+    FLAG_SKIP_OFFLINE = 0x00000001
+    FLAG_OFFLINE_SHOW_INET_ERR = 0x00000002
+
+    def __init__(self, flags=0, grace_days=0, days_between_checks=0):
+        self._flags = flags
+        self._grace_days = grace_days
+        self._days_between_checks = days_between_checks
+
+    def get_pointer(self):
+        options = GENUINE_OPTIONS(
+            sizeof(GENUINE_OPTIONS()), self._flags, self._days_between_checks, self._grace_days)
+        return pointer(options)
+
+    def flags(self, flags):
+        self._flags = flags
+
+    def grace_days(self, days):
+        """
+        If the call fails because of an internet error,
+        how long, in days, should the grace period last (before
+        returning deactivating and returning TA_FAIL).
+
+        14 days is recommended.
+        """
+        self._grace_days = days
+
+    def days_between_checks(self, days):
+        """How often to contact the LimeLM servers for validation. 90 days recommended."""
+        self._days_between_checks = days
+
+
+class TurboActivate(object):
+    def __init__(self,
+                 dat_file,
+                 guid,
+                 library_folder="",
+                 mode=TA_USER,
+                 use_trial=False,
+                 verified_trials=True):
+        self._lib = load_library(library_folder)
+        self._verified_trials = verified_trials
+
+        self._set_restype()
+        self.set_current_product(dat_file, guid, mode=mode)
+
+        # use_trial preserves backward compatibility with legacy API.
+        if use_trial:
+            self.use_trial()
+
+    #
+    # Public
+    #
+
+    # Product management
+
+    def use_trial(self):
+        flags = TA_VERIFIED_TRIAL | self._mode if self._verified_trials else TA_UNVERIFIED_TRIAL | self._mode
+
+        self._lib.TA_UseTrial(self._handle, flags, None)
+
+    def set_current_product(self, dat_file, guid, mode=TA_USER):
+        """
+        This functions allows you to use licensing for multiple products within
+        the same running process.
+        """
+        self._mode = mode
+        self._dat_file = wstr(dat_file)
+
+        try:
+            self._lib.PDetsFromPath(self._dat_file)
+        except TurboActivateFailError:
+            # The dat file is already loaded
+            pass
+
+        self._handle = self._lib.TA_GetHandle(wstr(guid))
+
+    # Product key
+
+    def product_key(self):
+        """
+        Gets the stored product key. NOTE: if you want to check if a product key is valid
+        simply call is_product_key_valid().
+        """
+        buf_size = 128
+        buf = wbuf(buf_size)
+
+        try:
+            self._lib.TA_GetPKey(self._handle, buf, buf_size)
+
+            return buf.value
+        except TurboActivateProductKeyError as e:
+            return None
+
+    def set_product_key(self, product_key):
+        """Checks and saves the product key."""
+        self._lib.TA_CheckAndSavePKey(self._handle, wstr(product_key), self._mode)
+
+    def is_product_key_valid(self):
+        """
+        Checks if the product key installed for this product is valid. This does NOT check if
+        the product key is activated or genuine. Use is_activated() and is_genuine() instead.
+        """
+        try:
+            self._lib.TA_IsProductKeyValid(self._handle)
+
+            return True
+        except TurboActivateError:
+            return False
+
+    # Activation status
+
+    def deactivate(self, erase_p_key=True, deactivation_request_file=""):
+        """
+        Deactivates the product on this computer. Set erase_p_key to True to erase the stored
+        product key, False to keep the product key around. If you're using deactivate to let
+        a user move between computers it's almost always best to *not* erase the product
+        key. This way you can just use activate() when the user wants to reactivate
+        instead of forcing the user to re-enter their product key over-and-over again.
+        If deactivation_request_file is specified, then it gets the "deactivation request"
+        file for offline deactivation.
+        """
+        e = 1 if erase_p_key else 0
+        fn = self._lib.TA_DeactivationRequestToFile if deactivation_request_file else self._lib.TA_Deactivate
+        args = [wstr(deactivation_request_file)] if deactivation_request_file else []
+
+        args.append(e)
+
+        try:
+            fn(self._handle, *args)
+        except TurboActivateNotActivatedError:
+            return
+
+    def activate(self, activation_request_file=""):
+        """
+        Activates the product on this computer. You must call set_product_key()
+        with a valid product key or have used the TurboActivate wizard sometime
+        before calling this function.
+        If activation_request_file is specified, then it gets the "activation request"
+        file for offline activation.
+        """
+        if self.is_activated():
+            return False
+
+        fn = self._lib.TA_ActivationRequestToFile if activation_request_file else self._lib.TA_Activate
+        args = [wstr(activation_request_file)] if activation_request_file else []
+
+        args.append(None)
+
+        try:
+            fn(self._handle, *args)
+
+            return True
+        except TurboActivateError as e:
+            if not activation_request_file:
+                self.deactivate(True)
+
+            raise e
+
+    def activate_from_file(self, filename):
+        """Activate from the "activation response" file for offline activation."""
+        self._lib.ActivateFromFile(self._handle, wstr(filename))
+
+    def get_extra_data(self):
+        """Gets the extra data you passed in using activate()"""
+        buf_size = 255
+        buf = wbuf(buf_size)
+
+        try:
+            self._lib.TA_GetExtraData(self._handle, buf, buf_size)
+
+            return buf.value
+        except TurboActivateFailError:
+            return ""
+
+    def is_activated(self):
+        """ Checks whether the computer has been activated."""
+        try:
+            self._lib.TA_IsActivated(self._handle)
+
+            return True
+        except TurboActivateError:
+            return False
+
+    # Features
+
+    def has_feature(self, name):
+        return len(self.get_feature_value(name)) > 0
+
+    def get_feature_value(self, name):
+        """Gets the value of a feature."""
+        buf_size = self._lib.GetFeatureValue(wstr(name), 0, 0)
+        buf = wbuf(buf_size)
+
+        self._lib.GetFeatureValue(wstr(name), buf, buf_size)
+
+        return buf.value
+
+    # Genuine
+
+    def is_genuine(self, options=None):
+        """
+        Checks whether the computer is genuinely activated by verifying with the LimeLM servers.
+        If reactivation is needed then it will do this as well.
+        Optionally you can pass a GenuineOptions object to specify more details
+        """
+        fn = self._lib.TA_IsGenuine
+        args = [self._handle]
+
+        if options:
+            fn = self._lib.TA_IsGenuineEx
+
+            args.append(options.get_pointer())
+
+        try:
+            fn(*args)
+
+            return True
+        except TurboActivateFeaturesChangedError:
+            return True
+
+    # Trial
+
+    def trial_days_remaining(self):
+        """
+        Get the number of trial days remaining.
+        0 days if the trial has expired or has been tampered with
+        (1 day means *at most* 1 day, that is it could be 30 seconds)
+
+        You must have called "use_trial" o use this function
+        """
+        flags = TA_VERIFIED_TRIAL | self._mode if self._verified_trials else TA_UNVERIFIED_TRIAL | self._mode
+        days = c_uint32(0)
+
+        self._lib.TA_TrialDaysRemaining(self._handle, flags, pointer(days))
+
+        return days.value
+
+    def extend_trial(self, extension_code):
+        """Extends the trial using a trial extension created in LimeLM."""
+        flags = TA_VERIFIED_TRIAL | self._mode if self._verified_trials else TA_UNVERIFIED_TRIAL | self._mode
+
+        self._lib.TA_ExtendTrial(self._handle, flags, wstr(extension_code))
+
+    # Utils
+
+    def is_date_valid(self, date=None):
+        """
+        Check if the date is valid
+        """
+        if not date:
+            to_check = datetime.utcnow().strftime("%Y-%m-%d %H-%M-%S")
+        else:
+            to_check = date
+
+        try:
+            self._lib.TA_IsDateValid(self._handle, wstr(to_check), TA_HAS_NOT_EXPIRED)
+
+            return True
+        except TurboActivateFlagsError as e:
+            raise e
+        except TurboActivateError:
+            return False
+
+    def set_custom_path(self, path):
+        """
+        This function allows you to set a custom folder to store the activation
+        data files. For normal use we do not recommend you use this function.
+
+        Only use this function if you absolutely must store data into a separate
+        folder. For example if your application runs on a USB drive and can't write
+        any files to the main disk, then you can use this function to save the activation
+        data files to a directory on the USB disk.
+
+        If you are using this function (which we only recommend for very special use-cases)
+        then you must call this function on every start of your program at the very top of
+        your app before any other functions are called.
+
+        The directory you pass in must already exist. And the process using TurboActivate
+        must have permission to create, write, and delete files in that directory.
+
+        On linux it is not available
+        """
+        if sys.platform.startswith('linux'):
+            raise RuntimeError("set_custom_path is not available under linux")
+
+        self._lib.TA_SetCustomActDataPath(wstr(path))
+
+    def set_custom_proxy(self, address):
+        """
+        Sets the custom proxy to be used by functions that connect to the internet.
+
+        Proxy address in the form: http://username:password@host:port/
+
+        Example 1 (just a host): http://127.0.0.1/
+        Example 2 (host and port): http://127.0.0.1:8080/
+        Example 3 (all 3): http://user:pass@127.0.0.1:8080/
+
+        If the port is not specified, TurboActivate will default to using port 1080 for proxies.
+        """
+        self._lib.SetCustomProxy(wstr(address))
+
+    def _set_restype(self):
+        self._lib.PDetsFromPath.restype = validate_result
+        self._lib.TA_UseTrial.restype = validate_result
+        self._lib.TA_GetPKey.restype = validate_result
+        self._lib.TA_CheckAndSavePKey.restype = validate_result
+        self._lib.TA_IsProductKeyValid.restype = validate_result
+        self._lib.TA_DeactivationRequestToFile.restype = validate_result
+        self._lib.TA_Deactivate.restype = validate_result
+        self._lib.TA_Activate.restype = validate_result
+        self._lib.TA_ActivationRequestToFile.restype = validate_result
+        self._lib.TA_ActivateFromFile.restype = validate_result
+        self._lib.TA_GetExtraData.restype = validate_result
+        self._lib.TA_IsActivated.restype = validate_result
+        self._lib.TA_IsGenuine.restype = validate_result
+        self._lib.TA_IsGenuineEx.restype = validate_result
+        self._lib.TA_TrialDaysRemaining.restype = validate_result
+        self._lib.TA_ExtendTrial.restype = validate_result
+        self._lib.TA_IsDateValid.restype = validate_result
+        self._lib.SetCustomProxy.restype = validate_result
+
+        # SetCustomActDataPath is not defined under linux
+        if not sys.platform.startswith('linux'):
+            self._lib.TA_SetCustomActDataPath.restype = validate_result
---
+++ packages/turboactivate/turboactivate-4.0.1.tar.gz/turboactivate-4.0.1/turboactivate/c_wrapper.py
@@ -0,0 +1,422 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2013-2018 Develer S.r.l. (https://www.develer.com/)
+#
+# Author: Lorenzo Villani <lvillani@develer.com>
+# Author: Riccardo Ferrazzo <rferrazz@develer.com>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import sys
+from os import path as ospath
+from ctypes import (cdll, c_uint, c_char_p, c_wchar_p, Structure, create_string_buffer,
+                    create_unicode_buffer)
+
+#
+# Utilities
+#
+
+wbuf = create_unicode_buffer if sys.platform == "win32" else create_string_buffer
+
+wstr = c_wchar_p if sys.platform == "win32" else c_char_p
+
+#
+# Wrapper
+#
+
+TA_OK = 0x00000000
+TA_FAIL = 0x00000001
+TA_E_PKEY = 0x00000002
+TA_E_ACTIVATE = 0x00000003
+TA_E_INET = 0x00000004
+TA_E_INUSE = 0x00000005
+TA_E_REVOKED = 0x00000006
+TA_E_GUID = 0x00000007
+TA_E_PDETS = 0x00000008
+TA_E_TRIAL = 0x00000009
+TA_E_TRIAL_EUSED = 0x0000000C
+TA_E_TRIAL_EEXP = 0x0000000D
+TA_E_EXPIRED = 0x0000000D
+TA_E_REACTIVATE = 0x0000000A
+TA_E_COM = 0x0000000B
+TA_E_INSUFFICIENT_BUFFER = 0x0000000E
+TA_E_PERMISSION = 0x0000000F
+TA_E_INVALID_FLAGS = 0x00000010
+TA_E_IN_VM = 0x00000011
+TA_E_IN_SANDBOX = 0x00000022
+TA_E_EDATA_LONG = 0x00000012
+TA_E_INVALID_ARGS = 0x00000013
+TA_E_KEY_FOR_TURBOFLOAT = 0x00000014
+TA_E_INET_DELAYED = 0x00000015
+TA_E_FEATURES_CHANGED = 0x00000016
+TA_E_ANDROID_NOT_INIT = 0x00000017
+TA_E_NO_MORE_DEACTIVATIONS = 0x00000018
+TA_E_ACCOUNT_CANCELED = 0x00000019
+TA_E_ALREADY_ACTIVATED = 0x0000001A
+TA_E_INVALID_HANDLE = 0x0000001B
+TA_E_ENABLE_NETWORK_ADAPTERS = 0x0000001C
+TA_E_ALREADY_VERIFIED_TRIAL = 0x0000001D
+TA_E_TRIAL_EXPIRED = 0x0000001E
+TA_E_MUST_SPECIFY_TRIAL_TYPE = 0x0000001F
+TA_E_MUST_USE_TRIAL = 0x00000020
+TA_E_NO_MORE_TRIALS_ALLOWED = 0x00000021
+
+#
+# Flags for the UseTrial() and CheckAndSavePKey() functions.
+#
+
+TA_SYSTEM = 0x00000001
+TA_USER = 0x00000002
+
+#
+# Flags for the IsGeninueEx() function.
+#
+
+TA_SKIP_OFFLINE = 0x00000001
+TA_OFFLINE_SHOW_INET_ERR = 0x00000002
+TA_DISALLOW_VM = 0x00000004
+TA_DISALLOW_SANDBOX = 0x00000008
+TA_UNVERIFIED_TRIAL = 0x00000010
+TA_VERIFIED_TRIAL = 0x00000020
+
+#
+# Flags for the is_date_valid() Function
+#
+
+TA_HAS_NOT_EXPIRED = 0x00000001
+
+
+class GENUINE_OPTIONS(Structure):
+    _fields_ = [
+        ("nLength", c_uint),
+        ("flags", c_uint),
+        ("nDaysBetweenChecks", c_uint),
+        ("nGraceDaysOnInetErr", c_uint),
+    ]
+
+
+class ACTIVATE_OPTIONS(Structure):
+    _fields_ = [
+        ("nLength", c_uint),
+        ("sExtraData", wstr),
+    ]
+
+
+def load_library(path):
+    LIBRARIES = {
+        'linux2': ospath.join(path, 'libTurboActivate.so'),
+        'darwin': ospath.join(path, 'libTurboActivate.dylib'),
+        'win32': ospath.join(path, 'TurboActivate.dll'),
+    }
+
+    return cdll.LoadLibrary(LIBRARIES[sys.platform])
+
+
+def validate_result(return_code):
+    # All ok, no need to perform error handling.
+    if return_code == TA_OK:
+        return
+
+    # Raise an exception type appropriate for the kind of error
+    if return_code == TA_FAIL:
+        raise TurboActivateFailError()
+    elif return_code == TA_E_FEATURES_CHANGED:
+        raise TurboActivateFeaturesChangedError()
+    elif return_code == TA_E_PDETS:
+        raise TurboActivateDatFileError()
+    elif return_code == TA_E_EDATA_LONG:
+        raise TurboActivateExtraDataLongError()
+    elif return_code == TA_E_PKEY:
+        raise TurboActivateProductKeyError()
+    elif return_code == TA_E_INUSE:
+        raise TurboActivateInUseError()
+    elif return_code == TA_E_REVOKED:
+        raise TurboActivateRevokedError()
+    elif return_code == TA_E_GUID:
+        raise TurboActivateGuidError()
+    elif return_code == TA_E_TRIAL:
+        raise TurboActivateTrialCorruptedError()
+    elif return_code == TA_E_TRIAL_EUSED:
+        raise TurboActivateTrialUsedError()
+    elif return_code == TA_E_TRIAL_EEXP:
+        raise TurboActivateTrialExpiredError()
+    elif return_code == TA_E_ACTIVATE:
+        raise TurboActivateNotActivatedError()
+    elif return_code == TA_E_INVALID_FLAGS:
+        raise TurboActivateFlagsError()
+    elif return_code == TA_E_COM:
+        raise TurboActivateComError()
+    elif return_code == TA_E_INET:
+        raise TurboActivateConnectionError()
+    elif return_code == TA_E_INET_DELAYED:
+        raise TurboActivateConnectionDelayedError()
+    elif return_code == TA_E_PERMISSION:
+        raise TurboActivatePermissionError()
+    elif return_code == TA_E_NO_MORE_DEACTIVATIONS:
+        raise TurboActivateNoMoreDeactivationsError()
+    elif return_code == TA_E_ACCOUNT_CANCELED:
+        raise TurboActivateAccountCanceledError()
+    elif return_code == TA_E_INVALID_HANDLE:
+        raise TurboActivateInvalidHandleError()
+    elif return_code == TA_E_ALREADY_ACTIVATED:
+        raise TurboActivateAlreadyActivatedError()
+    elif return_code == TA_E_ENABLE_NETWORK_ADAPTERS:
+        raise TurboActivateEnableNetworkAdaptersError()
+    elif return_code == TA_E_ALREADY_VERIFIED_TRIAL:
+        raise TurboActivateAlreadyVerifiedTrialError()
+    elif return_code == TA_E_TRIAL_EXPIRED:
+        raise TurboActivateTrialExpiredError()
+    elif return_code == TA_E_MUST_SPECIFY_TRIAL_TYPE:
+        raise TurboActivateMustSpecifyTrialTypeError()
+    elif return_code == TA_E_MUST_USE_TRIAL:
+        raise TurboActivateMustUseTrialError()
+    elif return_code == TA_E_NO_MORE_TRIALS_ALLOWED:
+        raise TurboActivateNoMoreTrialsError()
+    elif return_code == TA_E_INVALID_ARGS:
+        raise TurboActivateInvalidArgsError()
+
+    # Otherwise bail out and raise a generic exception
+    raise TurboActivateError(return_code)
+
+
+#
+# Exception types
+#
+
+
+class TurboActivateError(Exception):
+    """Generic TurboActivate error"""
+    pass
+
+
+class TurboActivateFailError(TurboActivateError):
+    """Fail error"""
+    pass
+
+
+class TurboActivateProductKeyError(TurboActivateError):
+    """Invalid product key"""
+    pass
+
+
+class TurboActivateNotActivatedError(TurboActivateError):
+    """The product needs to be activated."""
+    pass
+
+
+class TurboActivateConnectionError(TurboActivateError):
+    """Connection to the server failed."""
+    pass
+
+
+class TurboActivateInUseError(TurboActivateError):
+    """The product key has already been activated with the maximum number of computers."""
+    pass
+
+
+class TurboActivateRevokedError(TurboActivateError):
+    """The product key has been revoked."""
+    pass
+
+
+class TurboActivateGuidError(TurboActivateError):
+    """The version GUID doesn't match that of the product details file."""
+    pass
+
+
+class TurboActivateTrialCorruptedError(TurboActivateError):
+    """The trial data has been corrupted, using the oldest date possible."""
+    pass
+
+
+class TurboActivateTrialUsedError(TurboActivateError):
+    """The trial extension has already been used."""
+    pass
+
+
+class TurboActivateTrialExpiredError(TurboActivateError):
+    """
+    The activation has expired or the system time has been tampered
+    with. Ensure your time, timezone, and date settings are correct.
+    """
+    pass
+
+
+class TurboActivateComError(TurboActivateError):
+    """
+    The hardware id couldn't be generated due to an error in the COM setup.
+    Re-enable Windows Management Instrumentation (WMI) in your group policy
+    editor or reset the local group policy to the default values. Contact
+    your system admin for more information.
+
+    This error is Windows only.
+
+    This error can also be caused by the user (or another program) disabling
+    the "Windows Management Instrumentation" service. Make sure the "Startup type"
+    is set to Automatic and then start the service.
+
+
+    To further debug WMI problems open the "Computer Management" (compmgmt.msc),
+    expand the "Services and Applications", right click "WMI Control" click
+    "Properties" and view the status of the WMI.
+    """
+    pass
+
+
+class TurboActivatePermissionError(TurboActivateError):
+    """
+    Insufficient system permission. Either start your process as an
+    admin / elevated user or call the function again with the
+    TA_USER flag instead of the TA_SYSTEM flag.
+    """
+    pass
+
+
+class TurboActivateFeaturesChangedError(TurboActivateError):
+    """
+    If IsGenuine() or IsGenuineEx() reactivated and the features
+    have changed, then this will be the return code. Treat this
+    as a success.
+    """
+    pass
+
+
+class TurboActivateDatFileError(TurboActivateError):
+    """The product details file "TurboActivate.dat" failed to load."""
+    pass
+
+
+class TurboActivateFlagsError(TurboActivateError):
+    """
+    The flags you passed to use_trial(...) were invalid (or missing).
+    """
+    pass
+
+
+class TurboActivateExtraDataLongError(TurboActivateError):
+    """
+    The "extra data" was too long. You're limited to 255 UTF-8 characters.
+    Or, on Windows, a Unicode string that will convert into 255 UTF-8
+    characters or less.
+    """
+    pass
+
+
+class TurboActivateConnectionDelayedError(TurboActivateError):
+    """
+    is_genuine() previously had a TA_E_INET error, and instead
+    of hammering the end-user's network, is_genuine() is waiting
+    5 hours before rechecking on the network.
+    """
+    pass
+
+
+class TurboActivateNoMoreDeactivationsError(TurboActivateError):
+    """
+    This product key had a limited number of allowed deactivations.
+    No more deactivations are allowed for the product key. This product
+    is still activated on this computer.
+    """
+    pass
+
+
+class TurboActivateAccountCanceledError(TurboActivateError):
+    """
+    Can't activate or start a verified trial because the LimeLM
+    account is cancelled.
+    """
+    pass
+
+
+class TurboActivateAlreadyActivatedError(TurboActivateError):
+    """
+    You can't use a product key because your app is already activated
+    with a product key. To use a new product key, then first deactivate using
+    either the TA_Deactivate() or TA_DeactivationRequestToFile().
+    """
+    pass
+
+
+class TurboActivateInvalidHandleError(TurboActivateError):
+    """
+    The handle is not valid. To get a handle use TA_GetHandle().
+    """
+    pass
+
+
+class TurboActivateEnableNetworkAdaptersError(TurboActivateError):
+    """
+    There are network adapters on the system that are disabled and
+    TurboActivate couldn't read their hardware properties (even after trying
+    and failing to enable the adapters automatically). Enable the network adapters,
+    re-run the function, and TurboActivate will be able to "remember" the adapters
+    even if the adapters are disabled in the future.
+
+    Note:   The network adapters do not need an active Internet connections. They just
+            need to not be disabled. Whether they are or are not connected to the
+            internet/intranet is not important and does not affect this error code at all.
+
+
+    On Linux you'll get this error if you don't have any real network adapters attached.
+    For example if you have no "eth[x]", "wlan[x]", "en[x]", "wl[x]", "ww[x]", or "sl[x]"
+    network interface devices.
+
+    See: https://wyday.com/limelm/help/faq/#disabled-adapters
+    """
+    pass
+
+
+class TurboActivateAlreadyVerifiedTrialError(TurboActivateError):
+    """
+    The trial is already a verified trial. You need to use the "TA_VERIFIED_TRIAL"
+    flag. Can't "downgrade" a verified trial to an unverified trial.
+    """
+    pass
+
+
+class TurboActivateMustSpecifyTrialTypeError(TurboActivateError):
+    """
+    You must specify the trial type (TA_UNVERIFIED_TRIAL or TA_VERIFIED_TRIAL).
+    And you can't use both flags. Choose one or the other. We recommend TA_VERIFIED_TRIAL.
+    """
+    pass
+
+
+class TurboActivateMustUseTrialError(TurboActivateError):
+    """
+    You must call TA_UseTrial() before you can get the number of trial days remaining.
+    """
+    pass
+
+
+class TurboActivateNoMoreTrialsError(TurboActivateError):
+    """
+    In the LimeLM account either the trial days is set to 0, OR the account is set
+    to not auto-upgrade and thus no more verified trials can be made.
+    """
+    pass
+
+
+class TurboActivateInvalidArgsError(TurboActivateError):
+    """
+    The arguments passed to the function are invalid. Double check your logic.
+    """
+    pass
