1565356a0b8dc5a0a1f24a5009447c397096d44a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:11 +0000

    Add package pyxl320/0.10.0/pyxl320-0.10.0-py2-none-any.whl

---
+++ packages/pyxl320/pyxl320-0.10.0-py2-none-any.whl/pyxl320-0.10.0.data/scripts/servo_reboot.py
@@ -0,0 +1,58 @@
+#!python
+
+##############################################
+# The MIT License (MIT)
+# Copyright (c) 2016 Kevin Walchko
+# see LICENSE for full details
+##############################################
+# ----------------------------
+# Simple tool to change the id number of a servo
+#
+
+from __future__ import print_function, division
+from pyxl320 import ServoSerial
+from pyxl320 import Packet
+from pyxl320 import xl320
+# from pyxl320 import DummySerial
+import argparse
+
+
+def handleArgs():
+	parser = argparse.ArgumentParser(description='Resets servo(s) to factory defaults')
+	parser.add_argument('-a', '--all', help='reset all servos to defaults', action='store_true')
+	parser.add_argument('-i', '--id', help='servo id', type=int, default=1)
+	parser.add_argument('port', help='serial port or \'dummy\' for testing', type=str)
+
+	args = vars(parser.parse_args())
+	return args
+
+
+def main():
+	args = handleArgs()
+
+	port = args['port']
+
+	if args['all']:
+		ID = xl320.XL320_BROADCAST_ADDR
+	else:
+		ID = args['id']
+
+	if port.lower() == 'dummy':
+		ser = ServoSerial(port=port, fake=True)
+	else:
+		ser = ServoSerial(port=port)
+	ser.open()
+
+	pkt = Packet.makeRebootPacket(ID)
+	# print('pkt', pkt)
+
+	ser.write(pkt)
+	gramar = 'is'
+	if ID == xl320.XL320_BROADCAST_ADDR:
+		ID = 'all'
+		gramar = 'are'
+	print('Servo[{}] {} rebooting'.format(ID, gramar))
+
+
+if __name__ == '__main__':
+	main()
---
+++ packages/pyxl320/pyxl320-0.10.0-py2-none-any.whl/pyxl320-0.10.0.data/scripts/set_angle.py
@@ -0,0 +1,56 @@
+#!python
+
+##############################################
+# The MIT License (MIT)
+# Copyright (c) 2016 Kevin Walchko
+# see LICENSE for full details
+##############################################
+
+from pyxl320 import Packet
+# from pyxl320 import DummySerial
+from pyxl320 import ServoSerial
+import argparse
+
+
+DESCRIPTION = """
+Set the angle of a servo in degrees.
+
+Example: set servo 3 to angle 45
+
+./set_angle /dev/serial0 45 -i 3
+"""
+
+
+def handleArgs():
+	parser = argparse.ArgumentParser(description=DESCRIPTION, formatter_class=argparse.RawTextHelpFormatter)
+	parser.add_argument('-i', '--id', help='servo id', type=int, default=1)
+	parser.add_argument('port', help='serial port  or \'dummy\' for testing', type=str)
+	parser.add_argument('angle', help='servo angle in degrees: 0.0 - 300.0', type=float)
+
+	args = vars(parser.parse_args())
+	return args
+
+
+def main():
+	args = handleArgs()
+
+	ID = args['id']
+	port = args['port']  # '/dev/tty.usbserial-A5004Flb'
+	angle = args['angle']
+
+	print('Setting servo[{}] to {:.2f} on port {}'.format(ID, angle, port))
+
+	if port.lower() == 'dummy':
+		serial = ServoSerial(port=port, fake=True)
+	else:
+		serial = ServoSerial(port=port)
+	serial.open()
+
+	pkt = Packet.makeServoPacket(ID, angle)  # move servo 1 to 158.6 degrees
+	ans = serial.sendPkt(pkt)  # send packet to servo
+	if ans:
+		print('status: {}'.format(ans))
+
+
+if __name__ == '__main__':
+	main()
