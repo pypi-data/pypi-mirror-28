965be4bc8297ebdd469e4b55d60734b825daab93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:18 +0000

    Add package dict_robocop/1.0.0/dict_robocop-1.0.0.tar.gz

---
+++ packages/dict_robocop/dict_robocop-1.0.0.tar.gz/dict_robocop-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='dict_robocop',
+    version='1.0.0',
+    py_modules=['test_d'],
+    author='Harminder Singh',
+    author_email='harminder.narain@gmail.com',
+#    url='abc.com',
+    description='To return dict for robocop '
+)
+
---
+++ packages/dict_robocop/dict_robocop-1.0.0.tar.gz/dict_robocop-1.0.0/test_d.py
@@ -0,0 +1,7 @@
+def return_d(a):
+    dict={'k':'v','k1':'v1'}
+    l=[dict]
+    return dict
+
+def pass_d(d):
+    print d
