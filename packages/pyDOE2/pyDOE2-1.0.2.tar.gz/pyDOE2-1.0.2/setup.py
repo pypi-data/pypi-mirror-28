f74ffd29e7e581462a39a9c19c6acfeb5c12d6b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:03:06 +0000

    Add package pyDOE2/1.0.2/pyDOE2-1.0.2.tar.gz

---
+++ packages/pyDOE2/pyDOE2-1.0.2.tar.gz/pyDOE2-1.0.2/setup.py
@@ -0,0 +1,49 @@
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='pyDOE2',
+    version="1.0.2",
+    author='Rickard Sjögren',
+    author_email='r.sjogren89@gmail.com',
+    description='Design of experiments for Python',
+    url='https://github.com/clicumu/pyDOE2',
+    license='BSD License (3-Clause)',
+    long_description=read('README.md'),
+    packages=['pyDOE2'],
+    install_requires=['numpy', 'scipy'],
+    keywords=[
+        'DOE',
+        'design of experiments',
+        'experimental design',
+        'optimization',
+        'statistics',
+        'python'
+        ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Education',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Scientific/Engineering :: Physics',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities'
+        ]
+    )
+
