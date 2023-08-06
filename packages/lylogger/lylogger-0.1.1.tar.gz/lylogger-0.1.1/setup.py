48ec3e0683a33fc934749ae37ef175c030c55884
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:58 +0000

    Add package lylogger/0.1.1/lylogger-0.1.1.tar.gz

---
+++ packages/lylogger/lylogger-0.1.1.tar.gz/lylogger-0.1.1/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding=utf-8
+from setuptools import setup
+
+
+
+
+setup(
+    name='lylogger',
+    version='0.1.1',
+    author='GraySilver',
+    author_email='allenyzx@163.com',
+    url='https://github.com/GraySilver/Lylogger',
+    description='This is a easy logger.',
+    packages=['lylogger'],
+    # install_requires=[
+    #     "logging"
+    # ],
+    license='MIT',
+    classifiers=[
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Topic :: Text Processing :: Indexing",
+        "Topic :: Utilities",
+        "Topic :: Internet",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Programming Language :: Python :: 3.5",
+    ],
+)
