7595cb0944f777bc508367829d793500a78f74dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:46 +0000

    Add package pamagent/0.2.3/pamagent-0.2.3.tar.gz

---
+++ packages/pamagent/pamagent-0.2.3.tar.gz/pamagent-0.2.3/setup.py
@@ -0,0 +1,71 @@
+import sys
+
+import pip
+from setuptools import setup
+from setuptools.command.test import test as TestCommand
+
+
+try:
+    from setuptools_rust import Binding, RustExtension
+except ImportError:
+    status = pip.main(['install', 'setuptools-rust==0.8.3'])
+    if status != 0:
+        print("Please install setuptools-rust package")
+        raise SystemExit(status)
+    else:
+        from setuptools_rust import Binding, RustExtension
+
+
+class Tox(TestCommand):
+    user_options = [('tox-args=', 'a', "Arguments to pass to tox")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.tox_args = None
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import tox
+        import shlex
+        args = self.tox_args
+        if args:
+            args = shlex.split(self.tox_args)
+        errno = tox.cmdline(args=args)
+        sys.exit(errno)
+
+
+setup(
+    name='pamagent',
+    version='0.2.3',
+    author='PushAMP LLC',
+    author_email='devcore@pushamp.com',
+    description='Agent for PAM',
+    license='MIT',
+    keywords=['pam', 'rust', 'profiling', 'performance'],
+    url='https://github.com/pushamp/pamagent',
+    tests_require=['tox', 'django', 'requests'],
+    setup_requires=['setuptools-rust==0.8.3'],
+    cmdclass={
+        'test': Tox,
+    },
+    platforms='Posix; MacOS X; Windows',
+    rust_extensions=[RustExtension('pamagent.pamagent_core', 'pamcore/Cargo.toml', binding=Binding.PyO3)],
+    packages=['pamagent', 'pamagent.hooks', 'pamagent.utils'],
+    install_requires=[
+        "wrapt==1.10.10",
+        "setuptools-rust==0.8.3",
+    ],
+    zip_safe=False,
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        ]
+)
