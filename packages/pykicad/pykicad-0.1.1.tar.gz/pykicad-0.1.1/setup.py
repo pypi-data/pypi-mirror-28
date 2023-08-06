263ae90a374bfa0b319a12e576af8fcf6aa7c218
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:28 +0000

    Add package pykicad/0.1.1/pykicad-0.1.1.tar.gz

---
+++ packages/pykicad/pykicad-0.1.1.tar.gz/pykicad-0.1.1/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='pykicad',
+    packages=['pykicad'],
+    version='0.1.1',
+    description='Library for working with KiCAD file formats',
+    long_description=open('README.md').read(),
+    author='David Craven',
+    author_email='david@craven.ch',
+    url='https://github.com/dvc94ch/pykicad',
+    keywords=['kicad', 'file formats', 'parser'],
+    install_requires=['pyparsing'],
+    tests_require=['pytest'],
+    license='ISC'
+)
