f2fe8aa50b2e899612d6e5868f9e4cf0bc68c975
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:32 +0000

    Add package analytracks/0.2.1.13/analytracks-0.2.1.13.tar.gz

---
+++ packages/analytracks/analytracks-0.2.1.13.tar.gz/analytracks-0.2.1.13/analytracks/multitrack.py
@@ -0,0 +1,25 @@
+class MultiTrack:
+    """"
+    A multitrack object that summerize multiple activities
+    
+    init args :
+        filepath : , 
+        platform='garmin', 
+        athlete='unknown', 
+        type='unkown' (activity type)
+        
+    attributes :
+
+        activities (pd.dataFrame) : 
+            ['activity_id', 'activity_name', 'activity_type', 'athlete', 'date',
+            'device', 'distance', 'duration', 'elevationGain', 'has_po', 'has_tcx',
+            'has_hr', 'startLatitude', 'startLongitude', 'startTimeGMT',
+            'race_name', 'race_id', 'avHR', 'isarun',
+            record_data  (device, platform, ...)
+            
+    methods :
+        parse :
+        
+    """"
+    def 
+        
