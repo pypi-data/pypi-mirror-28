6b0ff445010e6e81eb97abb3fd52685d66c59d3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:49 +0000

    Add package gentools/0.4.0/gentools-0.4.0.tar.gz

---
+++ packages/gentools/gentools-0.4.0.tar.gz/gentools-0.4.0/gentools/__about__.py
@@ -0,0 +1,5 @@
+__version__ = '0.4.0'
+__author__ = 'Arie Bovenberg'
+__copyright__ = '2017, Arie Bovenberg'
+__description__ = ('tools for generators, generator functions, '
+                   'and generator-based coroutines')
