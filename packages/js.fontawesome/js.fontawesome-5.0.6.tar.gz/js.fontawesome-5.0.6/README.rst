aedfa3297f3f0defef51855de01c77dbeece9cf5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:53:56 +0000

    Add package js.fontawesome/5.0.6/js.fontawesome-5.0.6.tar.gz

---
+++ packages/js.fontawesome/js.fontawesome-5.0.6.tar.gz/js.fontawesome-5.0.6/README.rst
@@ -0,0 +1,15 @@
+js.fontawesome
+**************
+
+Introduction
+============
+
+This library packages `Font Awesome`_ for `fanstatic`_.
+
+.. _`fanstatic`: http://fanstatic.org
+.. _`Font Awesome`: https://fontawesome.com
+
+This requires integration between your web framework and ``fanstatic``,
+and making sure that the original resources (shipped in the ``resources``
+directory in ``js.fontawesome``) are published to some URL.
+
---
+++ packages/js.fontawesome/js.fontawesome-5.0.6.tar.gz/js.fontawesome-5.0.6/setup.py
@@ -0,0 +1,64 @@
+from setuptools import setup, find_packages
+import os
+
+# The version of the wrapped library is the starting point for the
+# version number of the python package.
+# In bugfix releases of the python package, add a '-' suffix and an
+# incrementing integer.
+# For example, a packaging bugfix release version 1.4.4 of the
+# js.jquery package would be version 1.4.4-1 .
+
+version = '5.0.6'
+
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+long_description = (
+    read('README.rst')
+    + '\n' +
+    read('js', 'fontawesome', 'test_font_awesome.rst')
+    + '\n' +
+    read('CHANGES.rst'))
+
+setup(
+    name='js.fontawesome',
+    version=version,
+    description="Fanstatic packaging of Font Awesome",
+    long_description=long_description,
+    url='https://bitbucket.org/fanstatic/js.fontawesome',
+    classifiers=[
+        'Development Status :: 6 - Mature',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Internet :: WWW/HTTP :: WSGI',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+    ],
+    keywords='Font Awesome fanstatic WSGI middleware static asset',
+    author='Fanstatic Developers',
+    author_email='fanstatic@googlegroups.com',
+    license='BSD',
+    packages=find_packages(),
+    namespace_packages=['js'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'fanstatic',
+        'setuptools',
+    ],
+    entry_points={
+        'fanstatic.libraries': [
+            'font_awesome = js.fontawesome:library',
+        ],
+    },
+)
