156facec84cfaac26e87468b302a012c1a6b3f0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:32 +0000

    Add package spyrosavr/0.1/spyrosavr-0.1.tar.gz

---
+++ packages/spyrosavr/spyrosavr-0.1.tar.gz/spyrosavr-0.1/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+      name='spyrosavr',
+      version='0.1',
+      description='hello',
+      long_description=open('README.md').read(),
+      url='https://github-isl-01.ca.com/avrsp01/pythonpackaging/spyros',
+      author='Spyros',
+      author_email='avrsp01@ca.com',
+      packages=setuptools.find_packages(),
+            )
