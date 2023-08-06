ec8fb3ca5cc8b314e811fda04cd5f099fe836f07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:50 +0000

    Add package hellodeploy/2.4/hellodeploy-2.4.tar.gz

---
+++ packages/hellodeploy/hellodeploy-2.4.tar.gz/hellodeploy-2.4/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='hellodeploy',    # This is the name of your PyPI-package.
+    version='2.4',           # Update the version number for new releases
+    entry_points = {
+        "console_scripts": ['hellodeploy = hellodeploy.cli:main']
+    },                      # Function to run when called in cli
+    url='https://github.com/quantfive/hellodeploy-cli',
+    author='Itai Reuveni',
+    author_email='itaireuveni@gmail.com',
+    license='MIT',
+    packages=['hellodeploy'],
+    zip_safe=False
+)
