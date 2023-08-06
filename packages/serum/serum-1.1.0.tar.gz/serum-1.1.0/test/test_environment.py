141c96904e1ca263ef0d76033e1e43315d5780d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:37 +0000

    Add package serum/1.1.0/serum-1.1.0.tar.gz

---
+++ packages/serum/serum-1.1.0.tar.gz/serum-1.1.0/test/test_environment.py
@@ -0,0 +1,174 @@
+import unittest
+
+from serum import Environment, Component, abstractmethod, Singleton, inject
+from serum.exceptions import InvalidDependency, UnregisteredDependency, \
+    NoEnvironment, AmbiguousDependencies, CircularDependency
+import threading
+
+
+class SomeComponent(Component):
+    pass
+
+
+class AbstractComponent(Component):
+    @abstractmethod
+    def m(self):
+        pass
+
+
+class ConcreteComponent(AbstractComponent):
+    def m(self):
+        pass
+
+
+class AlternativeComponent(AbstractComponent):
+    def m(self):
+        pass
+
+
+class SomeSingleton(Singleton):
+    pass
+
+
+class EnvironmentTests(unittest.TestCase):
+
+    def test_cant_register_non_component(self):
+        class NotAComponent:
+            pass
+        with self.assertRaises(InvalidDependency):
+            Environment(NotAComponent, self)
+
+    def test_can_register_component(self):
+        e = Environment(SomeComponent)
+        self.assertTrue(SomeComponent in e)
+
+    def test_environment_provides_concrete_component(self):
+        with Environment():
+            c = Environment.provide(SomeComponent, self)
+            self.assertIsInstance(c, SomeComponent)
+
+    def test_environment_cant_provide_abstract_component(self):
+        with Environment():
+            with self.assertRaises(UnregisteredDependency):
+                Environment.provide(AbstractComponent, self)
+
+    def test_environment_provides_concrete_subclass(self):
+        with Environment(ConcreteComponent):
+            c = Environment.provide(AbstractComponent, self)
+            self.assertIsInstance(c, AbstractComponent)
+            self.assertIsInstance(c, ConcreteComponent)
+
+    def test_environment_provides_correct_implementation(self):
+        with Environment(ConcreteComponent):
+            c = Environment.provide(AbstractComponent, self)
+            self.assertIsInstance(c, AbstractComponent)
+            self.assertIsInstance(c, ConcreteComponent)
+        with Environment(AlternativeComponent):
+            c = Environment.provide(AbstractComponent, self)
+            self.assertIsInstance(c, AbstractComponent)
+            self.assertIsInstance(c, AlternativeComponent)
+
+    def test_intersection(self):
+        e1 = Environment(SomeComponent)
+        e2 = Environment(ConcreteComponent)
+        e3 = e1 | e2
+        self.assertIn(SomeComponent, e3)
+        self.assertIn(ConcreteComponent, e3)
+
+    def test_decorater(self):
+        test_environment = Environment(SomeComponent)
+
+        @test_environment
+        def test():
+            component = Environment.provide(SomeComponent, self)
+            self.assertIsInstance(component, SomeComponent)
+
+        test()
+
+    def test_new_environment_in_thread(self):
+        def test():
+            with Environment(AlternativeComponent):
+                c1 = Environment.provide(AbstractComponent, self)
+                self.assertIsInstance(c1, AlternativeComponent)
+
+        with Environment(ConcreteComponent):
+            threading.Thread(target=test).start()
+            c2 = Environment.provide(AbstractComponent, self)
+            self.assertIsInstance(c2, ConcreteComponent)
+
+    def test_same_environment_in_thread(self):
+        def test():
+            with self.assertRaises(NoEnvironment):
+                Environment.provide(AbstractComponent, self)
+
+        with Environment(ConcreteComponent):
+            threading.Thread(target=test).start()
+
+    def test_nested_environments(self):
+        with Environment(ConcreteComponent):
+            c = Environment.provide(AbstractComponent, self)
+            self.assertIsInstance(c, ConcreteComponent)
+            with Environment(AlternativeComponent):
+                c = Environment.provide(AbstractComponent, self)
+                self.assertIsInstance(c, AlternativeComponent)
+
+    def test_context_manager(self):
+        e = Environment()
+        with e:
+            self.assertIs(Environment._current_env(), e)
+        self.assertIsNone(Environment._current_env())
+
+    def test_environment_gets_most_specific(self):
+        class ConcreteComponentSub(ConcreteComponent):
+            pass
+
+        with Environment(ConcreteComponent, ConcreteComponentSub):
+            c = Environment.provide(AbstractComponent, self)
+            self.assertIsInstance(c, ConcreteComponentSub)
+
+    def test_fails_with_ambiguous_dependencies(self):
+        with Environment(ConcreteComponent, AlternativeComponent):
+            with self.assertRaises(AmbiguousDependencies):
+                Environment.provide(AbstractComponent, self)
+
+    def test_singleton_is_always_same_instance(self):
+        with Environment():
+            s1 = Environment.provide(SomeSingleton, self)
+            s2 = Environment.provide(SomeSingleton, object())
+            self.assertIs(s1, s2)
+
+    def test_circular_dependency(self):
+        class AbstractA(Component):
+            pass
+
+        class AbstractB(Component):
+            pass
+
+        class A(AbstractA):
+            b = inject(AbstractB)
+
+            def __init__(self):
+                self.b
+
+        class B(AbstractB):
+            a = inject(AbstractA)
+
+            def __init__(self):
+                self.a
+
+        class Dependent:
+            a = inject(AbstractA)
+
+        with Environment(A, B):
+            with self.assertRaises(CircularDependency):
+                Dependent().a
+
+    def test_subtype_is_singleton(self):
+        class SomeComponentSingleton(SomeComponent, Singleton):
+            pass
+        with Environment(SomeComponentSingleton):
+            s1 = Environment.provide(SomeComponent, object())
+            s2 = Environment.provide(SomeComponent, object())
+            self.assertIs(s1, s2)
+            s3 = Environment.provide(SomeComponentSingleton, object())
+            self.assertIs(s1, s3)
