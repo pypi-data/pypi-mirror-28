b1642bf4bf6b0f845539a0cdea85275037d076d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:31 +0000

    Add package plyxproto/3.0.1/plyxproto-3.0.1.tar.gz

---
+++ packages/plyxproto/plyxproto-3.0.1.tar.gz/plyxproto-3.0.1/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+"""plyxproto xproto parser
+
+See:
+https://github.com/sb98052/plyprotobuf
+https://github.com/opencord/xos
+"""
+
+from setuptools import setup
+
+setup(name='plyxproto',
+      version='3.0.1',
+      description='xproto parser and processor',
+      author='Dusan Klinec (original protobuf parser), Sapan Bhatia (xproto extensions)',
+      author_email='sapan@opennetworking.org',
+      url='https://github.com/sb98052/plyprotobuf',
+      license='Apache Software License',
+
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Intended Audience :: Developers',
+          'Topic :: Software Development :: Build Tools',
+          'License :: OSI Approved :: Apache Software License',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7'
+      ],
+      keywords='xproto protobuf xos parser',
+      packages=['plyxproto'],
+      install_requires=['ply']
+     )
