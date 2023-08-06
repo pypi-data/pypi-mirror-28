4d6ef7f25b8551f046169802d7eeef9e99fb8017
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:25 +0000

    Add package py_space/0.3/py_space-0.3.tar.gz

---
+++ packages/py_space/py_space-0.3.tar.gz/py_space-0.3/py_space/__init__.py
@@ -0,0 +1,7 @@
+import py_space.space.misc
+import py_space.space.datetimes
+import py_space.space.diction
+import py_space.space.files
+import py_space.space.lists
+import py_space.space.mailin
+import py_space.space.strings
---
+++ packages/py_space/py_space-0.3.tar.gz/py_space-0.3/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='py_space',
+    packages=['py_space'],  # this must be the same as the name above
+    version='0.3',
+    description='python space',
+    author='hellspacebar',
+    author_email='hellspacebar@gmail.com',
+    url='https://github.com/hellspacebar/py_space',  # use the URL to the github repo
+    download_url='https://github.com/hellspacebar/py_space/archive/0.1.tar.gz',  # I'll explain this in a second
+    keywords=['space'],  # arbitrary keywords
+    classifiers=['Development Status :: 3 - Alpha','Programming Language :: Python :: 3.4'],
+    license='MIT',
+)
