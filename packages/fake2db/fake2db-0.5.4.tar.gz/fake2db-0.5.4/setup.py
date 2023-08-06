54ce5b71532434a2b17bef3c70d44115d37ffdca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:24 +0000

    Add package fake2db/0.5.4/fake2db-0.5.4.tar.gz

---
+++ packages/fake2db/fake2db-0.5.4.tar.gz/fake2db-0.5.4/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import os
+
+setup(
+    name='fake2db',
+    version='0.5.4',
+    author='Emir Ozer',
+    author_email='emirozer@yandex.com',
+    url='https://github.com/emirozer/fake2db',
+    description=
+    'Generate test databases filled with fake data (NOW CUSTOM SCHEMA CREATION SUPPORTED)(current support - sqlite, mysql, postgresql, mongodb, redis, couchdb)',
+    long_description=os.path.join(os.path.dirname(__file__), 'README.md'),
+    packages=find_packages(exclude=[]),
+    entry_points={'console_scripts': ['fake2db = fake2db.fake2db:main']},
+    install_requires=[
+        'Faker==0.7.11',
+    ],
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ], )
