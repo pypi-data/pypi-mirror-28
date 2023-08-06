cb62fb6bb8d644593ec50249130495ecdb8faec2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:30 +0000

    Add package models-namexx-2/1.1.0/models_namexx_2-1.1.0.tar.gz

---
+++ packages/models-namexx-2/models_namexx_2-1.1.0.tar.gz/models_namexx_2-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'models_namexx_2',
+    version = '1.1.0',
+    py_modules = ['test11111'],
+    author = 'firefly',
+    author_email = '811101580@qq.com',
+    url = 'https://upload.pypi.org/legacy/',
+    description = 'unzixxx'
+)
