383f6382d713f805c601155f0ed991f78410e0f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:17 +0000

    Add package wbutil/0.0.2a1/wbutil-0.0.2a1-py3-none-any.whl

---
+++ packages/wbutil/wbutil-0.0.2a1-py3-none-any.whl/wbutil/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+'''
+wbutil/__init__.py
+
+Main manifest of wbutil modules.
+
+Will Badart <wbadart@live.com>
+created: JAN 2018
+'''
+
+from . import func
+from . import fs
+from . import misc
+
+from .fs import *
+from .misc import *
