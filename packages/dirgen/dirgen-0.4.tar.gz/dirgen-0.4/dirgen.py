75deb4fd525d58a1791c14b271f574aada87a1b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:03:52 +0000

    Add package dirgen/0.4/dirgen-0.4.tar.gz

---
+++ packages/dirgen/dirgen-0.4.tar.gz/dirgen-0.4/dirgen.py
@@ -0,0 +1,41 @@
+import click
+import json
+import os
+from operator import itemgetter
+
+
+@click.command()
+@click.argument('template', type=click.File('rb'))
+@click.argument('root_path', type=click.Path(
+    file_okay=False, writable=True, exists=True))
+def generate_directories(template, root_path):
+    tree = json.load(template)
+    available_options = set()
+    fetch_options(tree, root_path, available_options)
+    if available_options:
+        click.echo("Would you like to enable the following options ?")
+        for option in available_options.copy():
+            if not click.confirm(option):
+                available_options.remove(option)
+    create_tree(tree, root_path, available_options)
+
+
+def create_tree(tree, path, options, suffix=''):
+    dir_name = "{}_{}".format(suffix, tree['name']) if suffix else tree['name']
+    dir_path = os.path.join(path, dir_name)
+    os.makedirs(dir_path, exist_ok=True)
+
+    if "children" in tree:
+        children = [tree for tree in tree['children'] if
+                    ("option" in tree and tree["option"] in options) or "option" not in tree]
+        for counter, child in enumerate(sorted(children, key=itemgetter('weight'))):
+            create_tree(child, dir_path, options, "{:02d}".format(counter))
+
+
+def fetch_options(tree, path, options):
+    dir_path = os.path.join(path, tree['name'])
+    if "option" in tree:
+        options.add(tree["option"])
+    if "children" in tree:
+        for child in tree['children']:
+            fetch_options(child, dir_path, options)
