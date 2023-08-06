087d899db5769129107bed06daa809d71f7b8bfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:30 +0000

    Add package simpleutils/0.1.1/simpleutils-0.1.1.tar.gz

---
+++ packages/simpleutils/simpleutils-0.1.1.tar.gz/simpleutils-0.1.1/simpleutils/miscellaneous.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 2017-5-7
+
+@author: cheng.li
+"""
+
+import sys
+import os
+import datetime as dt
+
+
+def add_parent_path(name, level):
+    current_path = os.path.abspath(name)
+    sys.path.append(os.path.sep.join(current_path.split(os.path.sep)[:-level]))
+
+
+def to_datetime(date):
+    if isinstance(date, dt.datetime):
+        return dt.datetime(date.year, date.month, date.day, date.hour, date.minute, date.second)
+    else:
+        return dt.datetime(date.year, date.month, date.day)
+
+
+def list_eq(lhs, rhs):
+
+    if len(lhs) != len(rhs):
+        return False
+
+    for i, v1 in enumerate(lhs):
+        if v1 != rhs[i]:
+            return False
+    return True
