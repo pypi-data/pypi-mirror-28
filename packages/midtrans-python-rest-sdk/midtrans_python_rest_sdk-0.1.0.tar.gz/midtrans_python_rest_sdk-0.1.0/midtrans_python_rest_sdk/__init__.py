4427f187f81bacb0d4e69d6784d3449fa2fa7357
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:55 +0000

    Add package midtrans-python-rest-sdk/0.1.0/midtrans_python_rest_sdk-0.1.0.tar.gz

---
+++ packages/midtrans-python-rest-sdk/midtrans_python_rest_sdk-0.1.0.tar.gz/midtrans_python_rest_sdk-0.1.0/midtrans_python_rest_sdk/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for Midtrans Python REST SDK."""
+
+__author__ = """Muhammad Irfan"""
+__email__ = 'irfan@rubyh.co'
+__version__ = '0.1.0'
+
+import midtrans_python_rest_sdk
