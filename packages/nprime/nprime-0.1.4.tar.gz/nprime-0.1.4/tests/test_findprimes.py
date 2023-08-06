bd5832e561984dea82fb7e1785c15d91818e6f39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:25 +0000

    Add package nprime/0.1.4/nprime-0.1.4.tar.gz

---
+++ packages/nprime/nprime-0.1.4.tar.gz/nprime-0.1.4/tests/test_findprimes.py
@@ -0,0 +1,27 @@
+
+import unittest
+from nprime.pyprime import find_primes
+
+
+class TestFindPrimes(unittest.TestCase):
+    """ Tests for findprimes function """
+
+    def test_lower_bigger_than_upper_error_message(self):
+        """ An error message is raised for a float """
+        self.assertRaises(ValueError, find_primes, 100, 50)
+
+    def test_lower_is_equal_to_one_error_message(self):
+        """ An error message is raised for a float """
+        self.assertRaises(ValueError, find_primes, 1, 50)
+
+    def test_lower_is_inferior_to_one_error_message(self):
+        """ An error message is raised for a float """
+        self.assertRaises(ValueError, find_primes, 0, 50)
+
+    def test_lower_equal_upper_error_message(self):
+        """ An error message is raised for a float """
+        self.assertRaises(ValueError, find_primes, 50, 50)
+
+
+if __name__ == '__main__':
+    unittest.main()
