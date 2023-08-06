25ced7c8a7ea1e3b49bc26a4ca57cbb3975010f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:40 +0000

    Add package pliers/0.2.1/pliers-0.2.1.tar.gz

---
+++ packages/pliers/pliers-0.2.1.tar.gz/pliers-0.2.1/pliers/extractors/__init__.py
@@ -0,0 +1,89 @@
+''' The `Extractor` hierarchy contains Transformer classes that take a `Stim`
+of any type as input and return extracted feature information (rather than
+another `Stim` instance).
+'''
+
+from .base import Extractor, ExtractorResult, merge_results
+from .api import (IndicoAPITextExtractor,
+                  IndicoAPIImageExtractor,
+                  ClarifaiAPIExtractor)
+from .audio import (STFTAudioExtractor,
+                    MeanAmplitudeExtractor,
+                    SpectralCentroidExtractor,
+                    SpectralBandwidthExtractor,
+                    SpectralContrastExtractor,
+                    SpectralRolloffExtractor,
+                    PolyFeaturesExtractor,
+                    RMSEExtractor,
+                    ZeroCrossingRateExtractor,
+                    ChromaSTFTExtractor,
+                    ChromaCQTExtractor,
+                    ChromaCENSExtractor,
+                    MelspectrogramExtractor,
+                    MFCCExtractor,
+                    TonnetzExtractor,
+                    TempogramExtractor)
+from .google import (GoogleVisionAPIFaceExtractor,
+                     GoogleVisionAPILabelExtractor,
+                     GoogleVisionAPIPropertyExtractor,
+                     GoogleVisionAPISafeSearchExtractor,
+                     GoogleVisionAPIWebEntitiesExtractor)
+from .image import (BrightnessExtractor, SaliencyExtractor, SharpnessExtractor,
+                    VibranceExtractor, FaceRecognitionFaceEncodingsExtractor,
+                    FaceRecognitionFaceLandmarksExtractor,
+                    FaceRecognitionFaceLocationsExtractor)
+from .models import TensorFlowInceptionV3Extractor
+from .text import (ComplexTextExtractor, DictionaryExtractor,
+                   PredefinedDictionaryExtractor, LengthExtractor,
+                   NumUniqueWordsExtractor, PartOfSpeechExtractor,
+                   WordEmbeddingExtractor, TextVectorizerExtractor,
+                   VADERSentimentExtractor)
+from .video import (FarnebackOpticalFlowExtractor)
+
+__all__ = [
+    'Extractor',
+    'ExtractorResult',
+    'IndicoAPITextExtractor',
+    'IndicoAPIImageExtractor',
+    'ClarifaiAPIExtractor',
+    'STFTAudioExtractor',
+    'MeanAmplitudeExtractor',
+    'SpectralCentroidExtractor',
+    'SpectralBandwidthExtractor',
+    'SpectralContrastExtractor',
+    'SpectralRolloffExtractor',
+    'PolyFeaturesExtractor',
+    'RMSEExtractor',
+    'ZeroCrossingRateExtractor',
+    'ChromaSTFTExtractor',
+    'ChromaCQTExtractor',
+    'ChromaCENSExtractor',
+    'MelspectrogramExtractor',
+    'MFCCExtractor',
+    'TonnetzExtractor',
+    'TempogramExtractor',
+    'GoogleVisionAPIFaceExtractor',
+    'GoogleVisionAPILabelExtractor',
+    'GoogleVisionAPIPropertyExtractor',
+    'GoogleVisionAPISafeSearchExtractor',
+    'GoogleVisionAPIWebEntitiesExtractor',
+    'BrightnessExtractor',
+    'SaliencyExtractor',
+    'SharpnessExtractor',
+    'VibranceExtractor',
+    'FaceRecognitionFaceEncodingExtractor',
+    'FaceRecognitionFaceLandmarksExtractor',
+    'FaceRecognitionFaceLocationsExtractor',
+    'TensorFlowInceptionV3Extractor',
+    'ComplexTextExtractor',
+    'DictionaryExtractor',
+    'PredefinedDictionaryExtractor',
+    'LengthExtractor',
+    'NumUniqueWordsExtractor',
+    'PartOfSpeechExtractor',
+    'FarnebackOpticalFlowExtractor',
+    'WordEmbeddingExtractor',
+    'TextVectorizerExtractor',
+    'VADERSentimentExtractor',
+    'merge_results'
+]
