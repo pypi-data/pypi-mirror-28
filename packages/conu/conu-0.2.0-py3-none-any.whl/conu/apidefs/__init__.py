ab0797c069b39d7ea9734b642d013e5099a5b481
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:11 +0000

    Add package conu/0.2.0/conu-0.2.0-py3-none-any.whl

---
+++ packages/conu/conu-0.2.0-py3-none-any.whl/conu/apidefs/__init__.py
@@ -0,0 +1,4 @@
+"""
+This python module contains definitions in form of abstract classes and classes with generic code
+which works across all backends. Abstract definitions need to be implemented by backends.
+"""
