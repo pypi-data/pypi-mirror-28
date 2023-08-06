0f661fc9de0f5a68a5f05d3f3910099a1bdaaebe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:00 +0000

    Add package dataprocxy/0.4/dataprocxy-0.4.tar.gz

---
+++ packages/dataprocxy/dataprocxy-0.4.tar.gz/dataprocxy-0.4/setup.py
@@ -0,0 +1,15 @@
+# Copyright (c) 2015 Spotify AB
+from setuptools import setup
+
+setup(name='dataprocxy',
+      version='0.4',
+      description='using a dataproc job or cluster id and a project, open a browser to important URLs using an ssh SOCKS proxy',
+      url='https://github.com/spotify/dataprocxy',
+      author='Johannes Russek',
+      author_email='jrussek@spotify.com',
+      maintainer='Twan Wolthof',
+      maintainer_email='xeago@spotify.com',
+      license='Apache 2.0',
+      packages=['dataprocxy'],
+      install_requires=['google-api-python-client'],
+      scripts=['bin/dataprocxy'])
