a83a072b5e6ae582e8317860edeef8c30b6cfdf2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:13 +0000

    Add package django_openehr/0.0.3/django_openehr-0.0.3.tar.gz

---
+++ packages/django_openehr/django_openehr-0.0.3.tar.gz/django_openehr-0.0.3/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='django_openehr',
+      version='0.0.3',
+      description='openehr archetypes rendered as Django models',
+      url='https://github.com/openhealthcare/django_openehr',
+      author='Open Health Care UK',
+      license='MIT',
+      packages=find_packages(),
+      zip_safe=False)
