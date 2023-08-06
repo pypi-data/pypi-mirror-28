fe0bd47bea229eb0921015845aa1744a70ac3cfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:51 +0000

    Add package ubm/1.0.2/ubm-1.0.2.tar.gz

---
+++ packages/ubm/ubm-1.0.2.tar.gz/ubm-1.0.2/ubm/acquisition/dataset/components/engine.py
@@ -0,0 +1,23 @@
+import numpy as np
+from ubm.acquisition.dataset.names import Names
+from ubm.acquisition.filter import Filter
+
+
+class Cylinders:
+
+    LAMBDA = Names.Engine['Cylinder']['Lambda']
+
+    def __init__(self):
+        self.cylinders = (Cylinder(1), Cylinder(2), Cylinder(3), Cylinder(4))
+
+    def get(self, number):
+        return self.cylinders[number]
+
+
+class Cylinder:
+
+    def __init__(self, number):
+        self.number = number
+
+    def get_injected_mass(self):
+        return
