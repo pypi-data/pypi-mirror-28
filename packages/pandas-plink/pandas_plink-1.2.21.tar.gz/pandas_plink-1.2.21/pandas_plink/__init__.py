12e34d5420361f728e5b560109b4d7800a9305ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:19 +0000

    Add package pandas-plink/1.2.21/pandas_plink-1.2.21.tar.gz

---
+++ packages/pandas-plink/pandas_plink-1.2.21.tar.gz/pandas_plink-1.2.21/pandas_plink/__init__.py
@@ -0,0 +1,23 @@
+from __future__ import absolute_import as _
+
+try:
+    import _cffi_backend as _
+except ImportError:
+    pass
+
+from .read import read_plink
+from .testit import test
+
+__version__ = "1.2.21"
+
+
+def example_file_prefix():
+    r"""Data files prefix."""
+    import os
+    p = __import__('pandas_plink').__path__[0]
+    return os.path.join(p, 'test', 'data_files', 'data')
+
+
+__all__ = [
+    "__name__", "__version__", "test", 'example_file_prefix', 'read_plink'
+]
