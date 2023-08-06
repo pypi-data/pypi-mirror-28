a91266efbe8f5a062eff283517dcae273c60953b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:33 +0000

    Add package textworld/0.0.1/textworld-0.0.1.tar.gz

---
+++ packages/textworld/textworld-0.0.1.tar.gz/textworld-0.0.1/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name='textworld',
+    version='0.0.1',
+    url='http://github.com/MarcCote/textworld',
+    author='Marc-Alexandre Côté',
+    author_email='marc.cote.19@gmail.com',
+    packages=find_packages(),
+    license='MIT',
+    description="TextWorld - a world made of text."
+    )
