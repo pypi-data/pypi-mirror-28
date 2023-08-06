437a362e9dd0278c98bdd36c18d705a40a736944
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:50 +0000

    Add package pibackup/0.0.9/pibackup-0.0.9.tar.gz

---
+++ packages/pibackup/pibackup-0.0.9.tar.gz/pibackup-0.0.9/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+config = {
+    'name': 'pibackup',
+    'version': '0.0.9',
+    'author': 'p1ppo',
+    'packages': ['pibackup', 'lib'],
+    'include_package_data': True,
+    'description': ('Scheduled cloud backup for Raspberry Pi running smart ' +
+                    'home systems like fhem or iobroker'),
+    # 'zip_safe': False,
+    # 'py_modules': [''],
+    'package_data': {
+        'lib': ['rclone', '*.json'],
+        '': ['*.json']
+    },
+    'entry_points': {
+        'console_scripts': [
+            'pibackup = pibackup.app:main',
+            'pibackup-config = pibackup.config:main'
+        ]
+    },
+    # 'scripts': ['bin/bin-script'],
+    'install_requires': ['schedule', 'python-crontab'],
+}
+
+setup(**config)
