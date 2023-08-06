4a5ccdacd1693b621de0aa2ad6519107023d402b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:03 +0000

    Add package django-fluent-pages/1.1.4/django_fluent_pages-1.1.4-py2.py3-none-any.whl

---
+++ packages/django-fluent-pages/django_fluent_pages-1.1.4-py2.py3-none-any.whl/fluent_pages/__init__.py
@@ -0,0 +1,3 @@
+# following PEP 440
+__version__ = "1.1.4"
+default_app_config = 'fluent_pages.apps.FluentPagesConfig'
---
+++ packages/django-fluent-pages/django_fluent_pages-1.1.4-py2.py3-none-any.whl/fluent_pages/tests/testapp/urls.py
@@ -0,0 +1,15 @@
+import fluent_pages.admin  # Register model
+from django.conf.urls import include, url
+from django.contrib import admin
+from django.http import Http404
+
+
+def simulate_404(request):
+    raise Http404("Test")
+
+
+urlpatterns = [
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^404/$', simulate_404),
+    url(r'', include('fluent_pages.urls')),
+]
