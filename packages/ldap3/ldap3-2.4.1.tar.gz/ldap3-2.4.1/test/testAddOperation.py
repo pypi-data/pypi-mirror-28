225f3e9d3dec2ba53e6c65d294b4d032bdfea664
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:19:21 +0000

    Add package ldap3/2.4.1/ldap3-2.4.1.tar.gz

---
+++ packages/ldap3/ldap3-2.4.1.tar.gz/ldap3-2.4.1/test/testAddOperation.py
@@ -0,0 +1,51 @@
+"""
+"""
+
+# Created on 2013.06.06
+#
+# Author: Giovanni Cannata
+#
+# Copyright 2013 - 2018 Giovanni Cannata
+#
+# This file is part of ldap3.
+#
+# ldap3 is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ldap3 is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with ldap3 in the COPYING and COPYING.LESSER files.
+# If not, see <http://www.gnu.org/licenses/>.
+
+import unittest
+
+from test.config import get_connection, drop_connection, add_user, random_id
+
+
+testcase_id = ''
+
+
+class Test(unittest.TestCase):
+    def setUp(self):
+        global testcase_id
+        testcase_id = random_id()
+        self.connection = get_connection()
+        self.delete_at_teardown = []
+
+    def tearDown(self):
+        drop_connection(self.connection, self.delete_at_teardown)
+        self.assertFalse(self.connection.bound)
+
+    def test_add(self):
+        self.delete_at_teardown.append(add_user(self.connection, testcase_id, 'add-operation-1'))
+        self.assertEqual('success', self.delete_at_teardown[0][1]['description'])
+
+    def test_add_bytes(self):
+        self.delete_at_teardown.append(add_user(self.connection, testcase_id, 'add-operation-1', test_bytes=True))
+        self.assertEqual('success', self.delete_at_teardown[0][1]['description'])
