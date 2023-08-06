cdd6d656c0bd5e12246ec5da935b919cf57d4d40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:33 +0000

    Add package vi3o/0.6.1/vi3o-0.6.1.tar.gz

---
+++ packages/vi3o/vi3o-0.6.1.tar.gz/vi3o-0.6.1/vi3o/netcam.py
@@ -0,0 +1,55 @@
+"""
+:mod:`vi3o.netcam` --- Live camera handling
+===========================================
+"""
+
+import base64
+import urllib2
+from cStringIO import StringIO
+
+from vi3o.image import imread
+from vi3o.utils import Frame
+
+
+class AxisCam(object):
+    """
+    Loads an mjpg stream directly from an Axis camera with hostname *ip* with the
+    resolution *width*x*height* using the *username* and *password* as credntials.
+    """
+    def __init__(self, ip, width=None, height=None, username=None, password=None):
+        mjpg_url = 'http://' + ip + '/mjpg/video.mjpg'
+        if width is not None:
+            mjpg_url += '?resolution=%dx%d' % (width, height)
+        request = urllib2.Request(mjpg_url)
+        if username:
+            base64string = base64.encodestring('%s:%s' % (username, password)).replace('\n', '')
+            request.add_header("Authorization", "Basic %s" % base64string)
+        self._fd = urllib2.urlopen(request)
+        self.fcnt = 0
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        headers = {}
+        while True:
+            l = self._fd.readline()
+            if not l:
+                raise StopIteration
+            if not l.strip() and headers:
+                break
+            if ':' in l:
+                i = l.index(':')
+                headers[l[:i]] = l[i+1:].strip()
+
+        data = self._fd.read(int(headers['Content-Length']))
+        img = imread(StringIO(data)).view(Frame)
+        img.index = self.fcnt
+        self.fcnt += 1
+        img.timestamp = img.systime = -1 # FIXME
+        return img
+
+if __name__ == '__main__':
+    from vi3o import view
+    for img in AxisCam('10.2.3.125', 640, 360):
+        view(img)
---
+++ packages/vi3o/vi3o-0.6.1.tar.gz/vi3o-0.6.1/vi3o/utils.py
@@ -0,0 +1,37 @@
+import sys, os, hashlib
+import numpy as np
+
+if sys.version_info > (3,):
+    xrange = range
+
+class Frame(np.ndarray):
+    pass
+
+class SlicedView(object):
+    def __init__(self, parent, indexes, properties=()):
+        self.parent = parent
+        self.range = xrange(*indexes.indices(len(parent)))
+        self.properties = properties
+
+    def __getitem__(self, item):
+        return self.parent[self.range[item]]
+
+    def __len__(self):
+        return len(self.range)
+
+    def __getattr__(self, item):
+        if item in self.properties:
+            return self.properties[item](self.range)
+        return getattr(self.parent, item)
+
+cache_dir = os.path.join(os.path.expanduser('~'), ".cache", "vi3o")
+
+def index_file(fn, extradata=None):
+    stats = os.stat(fn)
+    key = str((os.path.abspath(fn), stats.st_size, stats.st_mtime, extradata))
+    key = hashlib.md5(key.encode()).hexdigest()
+    path = os.path.join(cache_dir, key + '.idx')
+    d = os.path.dirname(path)
+    if not os.path.exists(d):
+        os.makedirs(d)
+    return path
