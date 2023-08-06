1cc636bbeee12885296c5809920a9add8403bbc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:16 +0000

    Add package peek-admin/0.8.2.1/peek-admin-0.8.2.1.tar.gz

---
+++ packages/peek-admin/peek-admin-0.8.2.1.tar.gz/peek-admin-0.8.2.1/peek_admin/build-web/src/main.ts
@@ -0,0 +1,15 @@
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app.module';
+import { environment } from './environments/environment';
+
+if (environment.production) {
+  enableProdMode();
+}
+
+import {VortexService} from "@synerty/vortexjs";
+let host = location.host.split(':')[0];
+VortexService.setVortexUrl(`ws://${host}:8013/vortexws`);
+
+platformBrowserDynamic().bootstrapModule(AppModule);
