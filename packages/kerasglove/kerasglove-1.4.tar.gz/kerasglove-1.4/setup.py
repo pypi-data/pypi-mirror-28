e111999b22e6c9e4bf903c563d2ba2c933ca9df4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:41 +0000

    Add package kerasglove/1.4/kerasglove-1.4.tar.gz

---
+++ packages/kerasglove/kerasglove-1.4.tar.gz/kerasglove-1.4/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+from setuptools.command.develop import develop
+from setuptools.command.install import install
+import sys
+from os import path, environ
+import os
+
+setup(
+  name = 'kerasglove',
+  packages = ['kerasglove'], # this must be the same as the name above
+  version = '1.4',
+  description = 'Pre-trained GLOVE embeddings for keras',
+  author = 'LukeWood',
+  author_email = 'lukewoodcs@gmail.com',
+  url = 'https://github.com/LukeWoodSMU/KerasGlove', # use the URL to the github repo
+  download_url = 'https://github.com/LukeWoodSMU/KerasGlove/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['keras','recurrent','rnn','GLOVE','embedding'], # arbitrary keywords
+  classifiers = []
+)
