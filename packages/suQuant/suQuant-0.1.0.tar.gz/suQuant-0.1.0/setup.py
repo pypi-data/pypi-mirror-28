665fd3f43bd6a5f6758b7eda9ada8ac9cfdfaa31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:59 +0000

    Add package suQuant/0.1.0/suQuant-0.1.0.tar.gz

---
+++ packages/suQuant/suQuant-0.1.0.tar.gz/suQuant-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'suQuant',
+    version = '0.1.0',
+    py_modules = ['suQuant'],
+    author = 'sukki',
+    author_email = 'admin@sukki.org',
+    url = 'http://sukki-quant.readthedocs.io/en/latest/',
+    description = 'sukki-quant',
+)
