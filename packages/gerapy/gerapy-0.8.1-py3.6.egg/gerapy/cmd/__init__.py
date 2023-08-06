4a2f8865bdfcd0aa4dfd9e6ea09b513e9316e71b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:04 +0000

    Add package gerapy/0.8.1/gerapy-0.8.1-py3.6.egg

---
+++ packages/gerapy/gerapy-0.8.1-py3.6.egg/gerapy/cmd/__init__.py
@@ -0,0 +1,27 @@
+"""
+Usage:
+  gerapy init [--folder=<folder>]
+  gerapy migrate
+  gerapy createsuperuser
+  gerapy runserver [<host:port>]
+  gerapy makemigrations
+
+Options:
+  -h --help
+  -v --version
+"""
+from docopt import docopt
+from gerapy import version
+from gerapy.cmd.init import init
+from gerapy.cmd.server import server
+
+
+def cmd():
+    arguments = docopt(__doc__, version=version())
+    
+    if arguments.get('init'):
+        # init folder
+        init(arguments.get('--folder'))
+    else:
+        # Call django cmd
+        server()
