cd29c7e83fec749952f62ae0a6770e6ddd2cda61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:06 +0000

    Add package crowdcurio-client/0.1.8/crowdcurio-client-0.1.8.tar.gz

---
+++ packages/crowdcurio-client/crowdcurio-client-0.1.8.tar.gz/crowdcurio-client-0.1.8/crowdcurio_client/__init__.py
@@ -0,0 +1,9 @@
+from crowdcurio_client.crowdcurio import CrowdCurio
+from crowdcurio_client.project import Project
+from crowdcurio_client.task import Task, TaskSessionPolicy
+from crowdcurio_client.user import User
+from crowdcurio_client.interface import Interface
+from crowdcurio_client.data import Data
+from crowdcurio_client.experiment import Experiment, Condition, SubjectCondition, ConfirmationCode, BonusPayment
+from crowdcurio_client.response import Response
+from crowdcurio_client.event import Event
