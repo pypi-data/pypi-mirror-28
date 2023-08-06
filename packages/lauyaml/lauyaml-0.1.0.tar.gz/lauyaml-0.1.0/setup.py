5119d701bffb358282122069de3abdf6c41400b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:47 +0000

    Add package lauyaml/0.1.0/lauyaml-0.1.0.tar.gz

---
+++ packages/lauyaml/lauyaml-0.1.0.tar.gz/lauyaml-0.1.0/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='lauyaml',
+    version='0.1.0',
+    description='No full-featured yaml parse library',
+    author='Liqiang Lau',
+    author_email='liqianglau@outlook.com',
+    url='https://liqiang.io/opensource/python/pyaml',
+    package='pyaml',
+    package_dir={'pyaml': 'pyaml'}
+)
