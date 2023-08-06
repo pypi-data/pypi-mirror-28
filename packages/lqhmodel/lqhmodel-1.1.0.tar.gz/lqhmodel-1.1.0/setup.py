eaf8a561b813b7a406d042c52a9e2d265a4a45c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:29 +0000

    Add package lqhmodel/1.1.0/lqhmodel-1.1.0.tar.gz

---
+++ packages/lqhmodel/lqhmodel-1.1.0.tar.gz/lqhmodel-1.1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name = 'lqhmodel',      
+    version = '1.1.0',
+    py_modules = ['lunzi'],
+    author = 'lqh',        
+    author_email = '1216387181@qq.com',
+    url = 'http://www.baidu.com ',
+    description = 'lunzihenmei'   
+    )
