95c59f9b38f494d65990ad7613729a1ac3e2d1ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:04:24 +0000

    Add package cypher6/1.0.2/cypher6-1.0.2.tar.gz

---
+++ packages/cypher6/cypher6-1.0.2.tar.gz/cypher6-1.0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='cypher6',
+    version='1.0.02',
+    description='Retrieve Music Lyrics',
+    author='Timothy James Dobbins',
+    author_email='tmthyjames@gmail.com',
+    url='https://github.com/tmthyjames/cypher',
+    install_requires=['requests', 'beautifulsoup4'],
+    py_modules=['cypher6']
+)
