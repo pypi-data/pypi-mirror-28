465bd6d0b719b02f1fb38212634b80cc63f3af94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:45 +0000

    Add package ppsso4django/0.1.3/ppsso4django-0.1.3.tar.gz

---
+++ packages/ppsso4django/ppsso4django-0.1.3.tar.gz/ppsso4django-0.1.3/setup.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python
+# -*- coding:utf-8 -*-
+
+from distutils.core import setup
+setup(
+    name='ppsso4django',
+    packages=['ppsso4django'],
+    version='0.1.3',
+    license='BSD License',
+    description='pp sso filter for Django Project',
+    author='kaka chan',
+    maintainer='kaka chan',
+    author_email='869586919@qq.com',
+    keywords=['Django', 'SSO', 'CAS'],
+    classifiers=[
+
+    ],
+    requires=[
+        'Django',
+    ]
+)
