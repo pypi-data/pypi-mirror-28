d4f9a351f7e870db9e3285ce4e6205e6effd2596
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:52:28 +0000

    Add package pystemd/0.3.0/pystemd-0.3.0.tar.gz

---
+++ packages/pystemd/pystemd-0.3.0.tar.gz/pystemd-0.3.0/tests/test_utils.py
@@ -0,0 +1,28 @@
+#
+# Copyright (c) 2017-present, Facebook, Inc.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree. An additional grant
+# of patent rights can be found in the PATENTS file in the same directory.
+#
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import six
+from unittest import TestCase
+
+from pystemd.utils import x2char_star
+
+
+class TestContextToCharStar(TestCase):
+    def test_pass_normal_vars(self):
+        for elem in (0, six.b('hi'), True, [], {}, (3, 4), {3, 4}):
+            self.assertEqual(elem, x2char_star(elem))
+
+    def test_convert_to_char(self):
+        for elem in ("", six.u('hi all')):
+            self.assertEqual(six.b(elem), x2char_star(elem))
