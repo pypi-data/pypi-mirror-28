156903e9101f1b275b6a831876bd6a19ffcddb09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:46 +0000

    Add package HangSuTestPackage/0.0.1/HangSuTestPackage-0.0.1.tar.gz

---
+++ packages/HangSuTestPackage/HangSuTestPackage-0.0.1.tar.gz/HangSuTestPackage-0.0.1/HangSuTestPackage/__init__.py
@@ -0,0 +1,6 @@
+def print_message():
+    """
+    :arg: NULL arguments
+    :return: return a string of message
+    """
+    return "Hang Su is still learning."
---
+++ packages/HangSuTestPackage/HangSuTestPackage-0.0.1.tar.gz/HangSuTestPackage-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='HangSuTestPackage',
+    packages=['HangSuTestPackage'],
+    version='0.0.1',
+    author='AnthonySu',
+    author_email='hsu9@ncsu.edu',
+    description='hello world package'
+)
