8d75b6c72fadb5e11f7f96b7039587876d7cb398
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:41 +0000

    Add package flapjacks/0.1.0/flapjacks-0.1.0.tar.gz

---
+++ packages/flapjacks/flapjacks-0.1.0.tar.gz/flapjacks-0.1.0/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name='flapjacks',
+    version='0.1.0',
+    packages=['flapjacks'],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+    url='https://github.com/ahmedaljazzar/flapjacks',
+    license='MIT License',
+    author='Ahmed Jazzar',
+    author_email='me@ahmedjazzar.com',
+    description='A Python project for transcoding videos on AWS and deploying them to Youtube.',
+)
