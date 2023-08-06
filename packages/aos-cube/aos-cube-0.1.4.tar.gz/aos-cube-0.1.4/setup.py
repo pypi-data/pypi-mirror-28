aa8df2030e37f606b9bd8e38cea59a2d9ed40360
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:37 +0000

    Add package aos-cube/0.1.4/aos-cube-0.1.4.tar.gz

---
+++ packages/aos-cube/aos-cube-0.1.4.tar.gz/aos-cube-0.1.4/setup.py
@@ -0,0 +1,28 @@
+import os
+from setuptools import setup
+
+LONG_DESC = open('pypi_readme.rst').read()
+LICENSE = open('LICENSE').read()
+
+install_requires = [
+    'pyserial'
+]
+
+setup(
+    name="aos-cube",
+    version="0.1.4",
+    description="aos command line tool for repositories version control, publishing and updating code from remotely hosted repositories, and invoking aos own build system and export functions, among other operations",
+    long_description=LONG_DESC,
+    url='https://code.aliyun.com/aos/aos-cube',
+    author='aos',
+    author_email='aliosthings@service.aliyun.com',
+    license=LICENSE,
+    packages=["aos"],
+    install_requires=install_requires,
+    entry_points={
+        'console_scripts': [
+            'aos=aos.aos:main',
+            'aos-cube=aos.aos:main',
+        ]
+    },
+)
