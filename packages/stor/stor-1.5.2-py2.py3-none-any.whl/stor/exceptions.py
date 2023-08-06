a4dcd97b44ed953c47b03b24d817131e6d79a1f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:15 +0000

    Add package stor/1.5.2/stor-1.5.2-py2.py3-none-any.whl

---
+++ packages/stor/stor-1.5.2-py2.py3-none-any.whl/stor/exceptions.py
@@ -0,0 +1,62 @@
+"""
+Provides exceptions classes thrown by various utilities of stor
+"""
+
+
+class RemoteError(Exception):
+    """
+    The top-level exception thrown for any errors from remote services
+    (eg. swift, S3).
+
+    The 'caught_exception' attribute of the exception must be examined in order
+    to inspect the exception thrown by the swift or S3 service. A swift exception
+    can either be a ``SwiftError`` (thrown by ``swiftclient.service``) or a
+    ``ClientError`` (thrown by ``swiftclient.client``). A S3 exception can be
+    a ``ClientError`` (thrown by ``botocore.client``).
+    """
+    def __init__(self, message, caught_exception=None):
+        super(RemoteError, self).__init__(message)
+        self.caught_exception = caught_exception
+
+
+class NotFoundError(RemoteError):
+    """Thrown when a 404 response is returned."""
+    pass
+
+
+class UnauthorizedError(RemoteError):
+    """Thrown when a 403 response is returned.
+
+    Note:
+        Internal swift connection errors (e.g., when a particular node is
+        unavailable) appear to translate themselves into 403 errors at the
+        proxy layer, thus in general it's a good idea to retry on authorization
+        errors as equivalent to unavailable errors when doing PUT or GET
+        operations (list / stat / etc never hit this issue).
+    """
+    pass
+
+
+class UnavailableError(RemoteError):
+    """Thrown when a 503 response is returned."""
+    pass
+
+
+class ConditionNotMetError(RemoteError):
+    """Thrown when a condition is not met."""
+    pass
+
+
+class FailedTransferError(RemoteError):
+    """Thrown when a file transfer fails."""
+    pass
+
+
+class FailedUploadError(FailedTransferError):
+    """Thrown when an upload fails."""
+    pass
+
+
+class FailedDownloadError(FailedTransferError):
+    """Thrown when a download fails."""
+    pass
