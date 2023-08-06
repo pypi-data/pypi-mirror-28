e30e3f4b53d0b131fa63094df9d7cff7dbfe0ca5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:23 +0000

    Add package nester4/1.0.0/nester4-1.0.0.tar.gz

---
+++ packages/nester4/nester4-1.0.0.tar.gz/nester4-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name        ='nester4',
+    version     ='1.0.0',
+    py_modules  =['nester4'],
+    author      ='hfphthon',
+    author_email='hfphthon@headfirstlabs.com',
+    url         ='http://www.headfirstlabs.com',
+    description ='A simple printer of nested lists',
+     )
