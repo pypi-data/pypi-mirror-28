5f3dffad2a5ab52a54302fbb2306d07029af1c9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:03:20 +0000

    Add package sirbot/0.0.3/sirbot-0.0.3.tar.gz

---
+++ packages/sirbot/sirbot-0.0.3.tar.gz/sirbot-0.0.3/sirbot/plugins/slack/endpoints.py
@@ -0,0 +1,129 @@
+import asyncio
+import logging
+
+from aiohttp.web import Response
+from slack.events import Event
+from slack.actions import Action
+from slack.commands import Command
+from slack.exceptions import FailedVerification
+
+LOG = logging.getLogger(__name__)
+
+
+async def incoming_event(request):
+    slack = request.app.plugins['slack']
+    payload = await request.json()
+    LOG.log(5, 'Incoming event payload: %s', payload)
+
+    if 'challenge' in payload:
+        if payload['token'] == slack.verify:
+            return Response(body=payload['challenge'])
+        else:
+            return Response(status=500)
+
+    try:
+        event = Event.from_http(payload, verification_token=slack.verify)
+    except FailedVerification:
+        return Response(status=401)
+
+    if event['type'] == 'message':
+        callbacks = list(_incoming_message(event, request))
+    else:
+        callbacks = list(_incoming_event(event, request))
+
+    if callbacks:
+        try:
+            done, _ = await asyncio.wait(callbacks, return_when=asyncio.ALL_COMPLETED)
+            for f in done:
+                f.result()
+        except Exception as e:
+            LOG.exception(e)
+            return Response(status=500)
+
+    return Response(status=200)
+
+
+def _incoming_event(event, request):
+    LOG.debug('Incoming event: %s', event)
+    slack = request.app.plugins['slack']
+    for handler in slack.routers['event'].dispatch(event):
+        yield asyncio.ensure_future(handler(event, request.app))
+
+
+def _incoming_message(event, request):
+    slack = request.app.plugins['slack']
+
+    if slack.bot_id and (event.get('bot_id') == slack.bot_id or event.get('message', {}).get('bot_id') == slack.bot_id):
+        return
+
+    LOG.debug('Incoming message: %s', event)
+    text = event.get('text')
+    if slack.bot_user_id and text:
+        mention = slack.bot_user_id in event['text'] or event['channel'].startswith('D')
+    else:
+        mention = False
+
+    if mention and text:
+        event['text'] = event['text'].strip('<@{}>'.format(slack.bot_user_id)).strip()
+
+    for handler in slack.routers['message'].dispatch(event):
+        option = slack.handlers_option[handler]
+        if option['mention'] and not mention:
+            continue
+        elif option['admin'] and event['user'] not in slack.admins:
+            continue
+
+        yield asyncio.ensure_future(handler(event, request.app))
+
+
+async def incoming_command(request):
+    slack = request.app.plugins['slack']
+    payload = await request.post()
+
+    try:
+        command = Command(payload, verification_token=slack.verify)
+    except FailedVerification:
+        return Response(status=401)
+
+    LOG.debug('Incoming command: %s', command)
+    callbacks = list()
+    for callback in slack.routers['command'].dispatch(command):
+        callbacks.append(asyncio.ensure_future(callback(command, request.app)))
+
+    if callbacks:
+        try:
+            done, _ = await asyncio.wait(callbacks, return_when=asyncio.ALL_COMPLETED)
+            for f in done:
+                f.result()
+        except Exception as e:
+            LOG.exception(e)
+            return Response(status=500)
+
+    return Response(status=200)
+
+
+async def incoming_action(request):
+    slack = request.app.plugins['slack']
+    payload = await request.post()
+    LOG.log(5, 'Incoming action payload: %s', payload)
+
+    try:
+        action = Action.from_http(payload, verification_token=slack.verify)
+    except FailedVerification:
+        return Response(status=401)
+
+    LOG.debug('Incoming action: %s', action)
+    callbacks = list()
+    for callback in slack.routers['action'].dispatch(action):
+        callbacks.append(asyncio.ensure_future(callback(action, request.app)))
+
+    if callbacks:
+        try:
+            done, _ = await asyncio.wait(callbacks, return_when=asyncio.ALL_COMPLETED)
+            for f in done:
+                f.result()
+        except Exception as e:
+            LOG.exception(e)
+            return Response(status=500)
+
+    return Response(status=200)
---
+++ packages/sirbot/sirbot-0.0.3.tar.gz/sirbot-0.0.3/sirbot/plugins/slack/plugin.py
@@ -0,0 +1,78 @@
+import os
+import logging
+import asyncio
+
+from slack import methods
+from slack.io.aiohttp import SlackAPI
+from slack.actions import Router as ActionRouter
+from slack.commands import Router as CommandRouter
+from slack.events import EventRouter, MessageRouter
+
+from . import endpoints
+
+LOG = logging.getLogger(__name__)
+
+
+class SlackPlugin:
+    __name__ = 'slack'
+
+    def __init__(self, *, token=None, verify=None, bot_id=None, bot_user_id=None, admins=None):
+        self.api = None
+        self.token = token or os.environ['SLACK_TOKEN']
+        self.admins = admins or os.environ.get('SLACK_ADMINS', [])
+        self.verify = verify or os.environ['SLACK_VERIFY']
+        self.bot_id = bot_id or os.environ.get('SLACK_BOT_ID')
+        self.bot_user_id = bot_user_id or os.environ.get('SLACK_BOT_USER_ID')
+        self.handlers_option = {}
+
+        if not self.bot_user_id:
+            LOG.warning('`SLACK_BOT_USER_ID` not set. It is required for `on mention` routing and discarding '
+                        'message coming from Sir Bot-a-lot to avoid loops.')
+
+        self.routers = {
+            'event': EventRouter(),
+            'command': CommandRouter(),
+            'message': MessageRouter(),
+            'action': ActionRouter(),
+        }
+
+    def load(self, sirbot):
+        LOG.info('Loading slack plugin')
+        self.api = SlackAPI(session=sirbot.http_session, token=self.token)
+
+        sirbot.router.add_route('POST', '/slack/events', endpoints.incoming_event)
+        sirbot.router.add_route('POST', '/slack/commands', endpoints.incoming_command)
+        sirbot.router.add_route('POST', '/slack/actions', endpoints.incoming_action)
+
+        if self.bot_user_id and not self.bot_id:
+            sirbot.on_startup.append(self.find_bot_id)
+
+    def on_event(self, event_type, handler):
+        if not asyncio.iscoroutinefunction(handler):
+            handler = asyncio.coroutine(handler)
+        self.routers['event'].register(event_type, handler)
+
+    def on_command(self, command, handler):
+        if not asyncio.iscoroutinefunction(handler):
+            handler = asyncio.coroutine(handler)
+        self.routers['command'].register(command, handler)
+
+    def on_message(self, pattern, handler, flags=0, channel='*', mention=False, admin=False):
+        if not asyncio.iscoroutinefunction(handler):
+            handler = asyncio.coroutine(handler)
+
+        if admin and not self.admins:
+            LOG.warning('Slack admins ids are not set. Admin limited endpoint will not work.')
+
+        self.handlers_option[handler] = {'mention': mention, 'admin': admin}
+        self.routers['message'].register(pattern=pattern, handler=handler, flags=flags, channel=channel)
+
+    def on_action(self, action, handler, name='*'):
+        if not asyncio.iscoroutinefunction(handler):
+            handler = asyncio.coroutine(handler)
+        self.routers['action'].register(action, handler, name)
+
+    async def find_bot_id(self, app):
+        rep = await self.api.query(url=methods.USERS_INFO, data={'user': self.bot_user_id})
+        self.bot_id = rep['user']['profile']['bot_id']
+        LOG.warning('`SLACK_BOT_ID` not set. For a faster start time set it to: "%s"', self.bot_id)
