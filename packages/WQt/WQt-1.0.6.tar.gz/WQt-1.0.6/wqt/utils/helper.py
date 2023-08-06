2d8cc2935f6f6c46c5705d64cdc1f3830cb44641
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:46 +0000

    Add package WQt/1.0.6/WQt-1.0.6.tar.gz

---
+++ packages/WQt/WQt-1.0.6.tar.gz/WQt-1.0.6/wqt/utils/helper.py
@@ -0,0 +1,194 @@
+"""@package utils
+Helper functions to be used through the tool
+"""
+
+import os
+import sys
+import shutil
+from os.path import abspath, dirname
+from colorama import Fore
+from .output import writeln
+
+from six import string_types
+
+
+def get_platform():
+    """Returns the operating system"""
+
+    platforms = {
+        'linux1': 'Linux',
+        'linux2': 'Linux',
+        'darwin': 'OS X',
+        'win32': 'Windows'
+    }
+    if sys.platform not in platforms:
+        return sys.platform
+
+    return platforms[sys.platform]
+
+
+def verify_path(path):
+    """check if the project path is correct"""
+
+    if not os.path.exists(path) or not os.path.isdir(path):
+        writeln('Path specified for project creation does not exist or is not a directory', color=Fore.RED)
+        quit(2)
+
+
+def get_valid_path(path):
+    """gets the project path based on if it is provided or not"""
+
+    if path is None:
+        path = get_working_directory()
+    else:
+        path = linux_path(os.path.abspath(path))
+
+    verify_path(path)
+
+    return path
+
+
+def get_qt_application(path):
+    """returns the type of qt application"""
+
+    if os.path.exists(path + '/wqt/console'):
+        application = 'console'
+    elif os.path.exists(path + '/wqt/quick'):
+        application = 'quick'
+    elif os.path.exists(path + '/wqt/widgets'):
+        application = 'widgets'
+    else:
+        application = None
+
+    return application
+
+
+def quote_join(values):
+    """Join a set of strings, presumed to be file paths, surrounded by quotes, for CMake"""
+
+    surrounded = []
+    for value in values:
+        surrounded.append('\"' + value + '\"')
+    return ' '.join(surrounded)
+
+
+def linux_path(path):
+    """Converts Windows style path to linux style path"""
+
+    return os.path.abspath(path).replace('\\', '/')
+
+
+def get_wqt_path():
+    """returns the absolute path of wcosa"""
+
+    wcosa_path = dirname(abspath(__file__))
+    return linux_path(dirname(dirname(wcosa_path)))
+
+
+def get_cmake_path():
+    """returns the absolute path of cosa"""
+
+    return linux_path(get_wqt_path() + '/toolchain/cmake')
+
+
+def get_working_directory():
+    """get path from where the script is called"""
+
+    return linux_path(os.path.abspath(os.getcwd()))
+
+
+def fill_template(string, data):
+    """Fills the template based on the data provided"""
+
+    string = string.replace('\\n', '\n').replace('\\t', '\t')
+
+    for key in data:
+        value = str(data[key])
+
+        if isinstance(data[key], list):
+            value = ' '.join(data[key])
+        elif isinstance(data[key], string_types):
+            value = data[key]
+
+        if '{{' + key + '}}' in string:
+            return string.replace('{{' + key + '}}', value)
+
+    return string
+
+
+def create_folder(path, override=False):
+    """Creates a folder at the given path"""
+
+    if override:
+        if os.path.exists(path):
+            shutil.rmtree(path)
+        os.mkdir(path)
+    elif not os.path.exists(path):
+        os.mkdir(path)
+
+
+def get_files_recursively(directory, extensions=None):
+    """gathers a list of all the files with the extensions recursively in a directory"""
+
+    arr = []
+    for root, dirs, files in os.walk(directory):
+        path = root.split(os.sep)
+        for file in files:
+            ext = os.path.splitext(file)[1]
+
+            if extensions is not None and ext in extensions:
+                arr.append(linux_path('/'.join(path) + '/' + file))
+            elif extensions is None:
+                arr.append(linux_path('/'.join(path) + '/' + file))
+
+    return arr
+
+
+def get_files(path, extensions=None):
+    """gathers a list of all the files with the extensions in a directory"""
+
+    arr = []
+    all_files = os.listdir(path)
+
+    for file in all_files:
+        if not os.path.isdir(path + '/' + file) and extensions is not None and os.path.splitext(file)[1] in extensions:
+            arr.append(linux_path(path + '/' + file))
+        elif not os.path.isdir(path + '/' + file) and extensions is None:
+            arr.append(linux_path(path + '/' + file))
+
+    return arr
+
+
+def get_dirs_recursively(path):
+    """gathers a list of all the subdirectories recursively inside the path"""
+
+    arr = []
+    for root, dirs, files in os.walk(path):
+        curr_path = '/'.join(root.split(os.sep))
+
+        if curr_path != os.path.basename(path):
+            arr.append(linux_path(curr_path))
+
+    return arr
+
+
+def get_dirs(path):
+    """gathers a list of all the subdirectories inside the path"""
+
+    arr = []
+    for file in os.listdir(path):
+        if os.path.isdir(path + '/' + file):
+            arr.append(linux_path(path + '/' + file))
+
+    return arr
+
+
+def get_dirnames(path):
+    """gathers a list of all the names of subdirectories inside the path"""
+
+    arr = []
+    for file in os.listdir(path):
+        if os.path.isdir(path + '/' + file):
+            arr.append(os.path.basename(linux_path(path + '/' + file)))
+
+    return arr
---
+++ packages/WQt/WQt-1.0.6.tar.gz/WQt-1.0.6/wqt/wqt.py
@@ -0,0 +1,113 @@
+"""
+Main Script that calls other scripts to make WQt work
+"""
+
+from __future__ import absolute_import
+
+import argparse
+
+from colorama import Fore
+
+from wqt.command import creation, handle
+from wqt.utils.output import writeln
+
+
+def parse():
+    """adds command line arguments and returns the options"""
+
+    parser = argparse.ArgumentParser(description='WQt creates and builds Qt projects through CMake')
+
+    parser.add_argument(
+        'action',
+        nargs='+',
+        help='action to perform (create, update, build, clean, list-types, add-lib, rm-lib, list_qml, and show_qml')
+    parser.add_argument(
+        '--path',
+        help='path where the project is or will be created'
+    )
+    parser.add_argument(
+        '--generator',
+        help='makefile generator to use for build (default: Unix Makefiles)',
+        type=str)
+    parser.add_argument(
+        '--make',
+        help='path to make binary',
+        type=str)
+    parser.add_argument(
+        '--cmake',
+        help='path to cmake binary',
+        type=str)
+
+    return parser.parse_args()
+
+
+def provided(*args):
+    """checks if given flags are specified during command line usage"""
+
+    if any(flag is not None for flag in args):
+        return True
+
+
+def verify_qt_application(name):
+    """verifies if the type of Qt application is supported"""
+
+    if name != 'quick' and name != 'widgets' and name != 'console':
+        writeln('Invalid Qt application specified', color=Fore.RED)
+        quit(2)
+
+
+def main():
+    options = parse()
+
+    path = None
+    cmake = options.cmake
+    make = options.make
+    generator = options.generator
+
+    if provided(options.path):
+        path = str(options.path)
+
+    # based on the action call scripts
+    if 'create' in options.action:
+        if len(options.action) < 2:
+            writeln('Specify a type of Qt application to create', color=Fore.RED)
+            quit(2)
+
+        verify_qt_application(options.action[1])
+        creation.create(path, options.action[1])
+    elif 'update' in options.action:
+        creation.update(path)
+    elif 'build' in options.action:
+        handle.build(path, generator, cmake, make)
+    elif 'clean' in options.action:
+        handle.clean(path)
+    elif 'list-types' in options.action:
+        handle.list_types()
+    elif 'add-lib' in options.action:
+        if len(options.action) < 2:
+            writeln('Specify the name of the library to add', color=Fore.RED)
+            quit(2)
+
+        handle.add_lib(path, options.action[1])
+    elif 'rm-lib' in options.action:
+        if len(options.action) < 2:
+            writeln('Specify the name of the library to remove', color=Fore.RED)
+            quit(2)
+
+        handle.rm_lib(path, options.action[1])
+    elif 'run' in options.action:
+        handle.run(path, generator, cmake, make)
+    elif 'list-qml' in options.action:
+        handle.list_qml(path)
+    elif 'list-libs' in options.action:
+        handle.list_libs(path)
+    elif 'preview-qml' in options.action:
+        if len(options.action) < 2:
+            writeln('Specify the name of the qml file to preview', color=Fore.RED)
+            quit(2)
+
+        handle.preview_qml(path, options.action[1])
+
+
+if __name__ == '__main__':
+    main()
