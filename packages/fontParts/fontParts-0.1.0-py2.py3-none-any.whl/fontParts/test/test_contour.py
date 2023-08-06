07c674a9e4314ff34ab1f16669d3b88fb56bf3a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:53:02 +0000

    Add package fontParts/0.1.0/fontParts-0.1.0-py2.py3-none-any.whl

---
+++ packages/fontParts/fontParts-0.1.0-py2.py3-none-any.whl/fontParts/test/test_contour.py
@@ -0,0 +1,51 @@
+import unittest
+from fontParts.base import FontPartsError
+
+
+class TestContour(unittest.TestCase):
+
+    # ------
+    # Bounds
+    # ------
+
+    def getContour_bounds(self):
+        contour, unrequested = self.objectGenerator("contour")
+        contour.appendPoint((0, 0), "line")
+        contour.appendPoint((0, 100), "line")
+        contour.appendPoint((100, 100), "line")
+        contour.appendPoint((100, 0), "line")
+        return contour, unrequested
+
+    def getContour_boundsExtrema(self):
+        contour, unrequested = self.objectGenerator("contour")
+        contour.appendPoint((0, 0), "line")
+        contour.appendPoint((0, 100), "line")
+        contour.appendPoint((50, 100), "line")
+        contour.appendPoint((117, 100), "offcurve")
+        contour.appendPoint((117, 0), "offcurve")
+        contour.appendPoint((50, 0), "curve")
+        return contour, unrequested
+
+    def test_bounds(self):
+        # get
+        contour, unrequested = self.getContour_bounds()
+        self.assertEqual(
+            contour.bounds,
+            (0, 0, 100, 100)
+        )
+        # get: float
+        contour.moveBy((0.5, -0.5))
+        self.assertEqual(
+            contour.bounds,
+            (0.5, -0.5, 100.5, 99.5)
+        )
+        # get: point not at extrema
+        contour, unrequested = self.getContour_boundsExtrema()
+        bounds = tuple(int(round(i)) for i in contour.bounds)
+        self.assertEqual(
+            bounds,
+            (0, 0, 100, 100)
+        )
+        # set
+        with self.assertRaises(FontPartsError):
+            contour.bounds = (1, 2, 3, 4)
