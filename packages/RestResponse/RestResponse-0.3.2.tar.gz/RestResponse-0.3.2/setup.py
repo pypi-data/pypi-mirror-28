f38b91bfafd1ed8bf311587e8ad59df5ff45be2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:09 +0000

    Add package RestResponse/0.3.2/RestResponse-0.3.2.tar.gz

---
+++ packages/RestResponse/RestResponse-0.3.2.tar.gz/RestResponse-0.3.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+__version__ = '0.3.2'
+
+setup(
+    name='RestResponse',
+    version=__version__,
+    description='A fluent python object for interfacing with RESTful JSON APIs.',
+    url='https://github.com/MobiusWorksLLC/RestResponse.git',
+    author='Tyson Holub',
+    author_email='tholub@mobiusworks.com',
+    license='MIT',
+    packages=['RestResponse'],
+    install_requires=[
+        'simplejson',
+        'six'
+    ]
+)
