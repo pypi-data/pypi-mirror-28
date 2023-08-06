81947a86d87f14eebae128e2f9483a885065da41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:04:53 +0000

    Add package mattophobia_says/0.1.2/mattophobia_says-0.1.2.tar.gz

---
+++ packages/mattophobia_says/mattophobia_says-0.1.2.tar.gz/mattophobia_says-0.1.2/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+    name='mattophobia_says',
+    packages=['mattophobia_says'],
+    version='0.1.2',
+    description='A Mattophobia string generator',
+    author='DerpyChap',
+    author_email='derpychap@outlook.com',
+    url='https://github.com/DerpyChap/mattophobia_says',
+    download_url=
+    'https://github.com/DerpyChap/mattophobia_says/archive/0.1.2.tar.gz',
+    keywords=['mattophobia', 'sentences', 'swearing'],
+    classifiers=[],
+)
