ec37f082a7eaa643e4f47c97e5fc4096e29a61a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:19:31 +0000

    Add package textalyzer/0.1.2/textalyzer-0.1.2.tar.gz

---
+++ packages/textalyzer/textalyzer-0.1.2.tar.gz/textalyzer-0.1.2/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+	name = 'textalyzer',
+	packages = ['textalyzer'],
+	version = '0.1.2',
+	description = ' A text  analyzer for repeating and contraction characters',
+	author = 'Lord Kay',
+	author_email = 'offeilord@gmail.com',
+	license = 'MIT',
+	url = 'https://github.com/LORD-KAY/Textalyzer',
+	download_url = 'https://github.com/LORD-KAY/Textalyzer/releases/v0.1.2',
+	keywords = ['python','replacers','words','repeat','characters'],
+	)
