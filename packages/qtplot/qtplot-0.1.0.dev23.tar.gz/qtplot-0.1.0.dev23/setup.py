b5752c6707bf13081e7984dfd97b219bff4d248f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:13 +0000

    Add package qtplot/0.1.0.dev23/qtplot-0.1.0.dev23.tar.gz

---
+++ packages/qtplot/qtplot-0.1.0.dev23.tar.gz/qtplot-0.1.0.dev23/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(name='qtplot',
+      version='0.1.0.dev23',
+      description='Data plotting and analysis tool',
+      url='https://github.com/Rubenknex/qtplot',
+      author='Ruben van Gulik',
+      author_email='rubenvangulik@gmail.com',
+      license='MIT',
+      packages=['qtplot',
+                'tests',
+                'qtplot/colormaps',
+                'qtplot/colormaps/nanoscope',
+                'qtplot/colormaps/transform',
+                'qtplot/colormaps/wsxm'],
+      install_requires=[
+        'QtPy',
+        'pyopengl',
+        'vispy==0.4.0',
+      ],
+      package_data={
+        '': ['*.npy']
+      },
+      entry_points={
+        'console_scripts': ['qtplot-console = qtplot.qtplot:main'],
+        'gui_scripts': ['qtplot = qtplot.qtplot:main']
+      })
