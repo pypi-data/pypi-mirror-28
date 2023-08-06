2ddf691dd75950e58fcdb9e14049114710faff60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:05 +0000

    Add package scinot/0.0.9/scinot-0.0.9.tar.gz

---
+++ packages/scinot/scinot-0.0.9.tar.gz/scinot-0.0.9/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+with open('README.rst') as f:
+    readme = f.read()
+
+setup(
+    name="scinot",
+    version="0.0.9",
+    packages=find_packages(),
+
+    install_requires=['colorama'],
+
+    author="David O'Connor",
+    author_email="david.alan.oconnor@gmail.com",
+    url='https://github.com/David-OConnor/scinot',
+    description="Display numbers in scientific notation.",
+    long_description=readme,
+    license="MIT",
+    keywords="scientific notation, exponential, REPL",
+)
