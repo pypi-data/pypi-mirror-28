210fbbaab48fceb9d0a593ce394ba7d3fffe925c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:58 +0000

    Add package easyLDA/0.1.1/easyLDA-0.1.1-py2-none-any.whl

---
+++ packages/easyLDA/easyLDA-0.1.1-py2-none-any.whl/easyLDA/__init__.py
@@ -0,0 +1,24 @@
+# coding: utf-8
+import sys
+try:
+    reload(sys)
+    sys.setdefaultencoding("utf-8")
+except:
+    print('python3')
+    
+try:
+    import nltk
+    nltk.download("stopwords")
+    nltk.download("wordnet")
+except:
+    'stopword data downloaded already'
+    
+from .base import PipelineLDA
+from .base import main as LDA_main
+
+
+
+def main():
+    print 'executing...'
+    LDA_main()
+
