a80d8fb63d901441d4786506ac6da34992b54df2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:10 +0000

    Add package LINUX-SGIO/1.0/LINUX_SGIO-1.0.tar.gz

---
+++ packages/LINUX-SGIO/LINUX_SGIO-1.0.tar.gz/LINUX_SGIO-1.0/setup.py
@@ -0,0 +1,31 @@
+# coding: utf-8
+
+from sys import platform
+#from distutils.core import setup, Extension
+from setuptools import setup, Extension
+
+
+setup(name='PYSCSI',
+      version='1.0',
+      license='LGPLv2.1',
+      author='Ronnie Sahlberg',
+      author_email='ronniesahlberg@gmail.com',
+      description='Module for calling SCSI devices from Python',
+      packages=['pyscsi', 'pyscsi.pyscsi', 'pyscsi.utils'])
+
+
+#
+# SGIO is linux specific
+#
+if platform[:5] == 'linux':
+    linux_sgio_module = Extension('linux_sgio.linux_sgio',
+                                  sources=['linux_sgio/sgiomodule.c'])
+
+    setup(name='LINUX_SGIO',
+          version='1.0',
+          license='LGPLv2.1',
+          author='Ronnie Sahlberg',
+          author_email='ronniesahlberg@gmail.com',
+          description='Module for calling Linux SG_IO devices',
+          packages=['linux_sgio'],
+          ext_modules=[linux_sgio_module])
