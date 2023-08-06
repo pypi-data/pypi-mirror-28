32c33599a66c9b9f53899adfad012ac27a7e0864
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:14 +0000

    Add package overture-song/0.0.3/overture_song-0.0.3-py2.py3-none-any.whl

---
+++ packages/overture-song/overture_song-0.0.3-py2.py3-none-any.whl/overture_song/model.py
@@ -0,0 +1,166 @@
+# Copyright (c) 2018 The Ontario Institute for Cancer Research. All rights
+# reserved.
+#
+# This program and the accompanying materials are made available under the
+# terms of the GNU Public License v3.0. You should have received a copy of
+# the GNU General Public License along with
+# this program. If not, see <http://www.gnu.org/licenses/>.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING,BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+# IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+
+import logging
+
+from overture_song import utils
+from overture_song.utils import SongClientException
+
+logging.basicConfig(level=logging.INFO)
+log = logging.getLogger("song.model")
+
+
+###############################
+# Utility Functions for Models
+###############################
+
+def get_optional_field(dic, field):
+    return dic[field] if field in dic else None
+
+
+def get_required_field(dic, field):
+    if field in dic:
+        return dic[field]
+    else:
+        raise SongClientException("study.id.dne", "The field '{}' does not exist in {}".format(field, str(dic)))
+
+################################
+#  Models
+################################
+
+
+class Study(object):
+
+    def __init__(self, studyId=None, name=None, organization=None, description=None):
+        self.studyId = studyId
+        self.name = name
+        self.organization = organization
+        self.description = description
+
+    def __str__(self):
+        return utils.to_json_string(self)
+
+    @classmethod
+    def create_from_raw(cls, study_obj):
+        study_dict = study_obj.__dict__
+        study_id = get_required_field(study_dict, 'studyId')
+        study_name = get_optional_field(study_dict, 'name')
+        study_description = get_optional_field(study_dict, 'description')
+        study_organization = get_optional_field(study_dict, 'organization')
+        return Study(study_id, study_name, study_organization, study_description)
+
+
+class SongError(Exception):
+
+    FIELDS = ['stackTrace',
+              'errorId',
+              'httpStatusName',
+              'httpStatusCode',
+              'message',
+              'requestUrl',
+              'debugMessage',
+              'timestamp']
+
+    def __init__(self, data, response, debug=False):
+        self.stackTrace = data[SongError.FIELDS[0]]
+        self.errorId = data[SongError.FIELDS[1]]
+        self.httpStatusName = data[SongError.FIELDS[2]]
+        self.httpStatusCode = data[SongError.FIELDS[3]]
+        self.message = data[SongError.FIELDS[4]]
+        self.requestUrl = data[SongError.FIELDS[5]]
+        self.debugMessage = data[SongError.FIELDS[6]]
+        self.timestamp = data[SongError.FIELDS[7]]
+        self.response = response
+        self.__debug = debug
+
+    @classmethod
+    def is_song_error(cls, data):
+        for field in SongError.FIELDS:
+            if field not in data:
+                return False
+        return True
+
+    def __str__(self):
+        if self.__debug:
+            return utils.to_pretty_json_string(self.response.content)
+        else:
+            return "[SONG_SERVER_ERROR] {} @ {}: {}".format(self.errorId, self.timestamp, self.message)
+
+
+class ApiConfig(object):
+
+    def __init__(self, server_url, study_id, access_token, debug=False):
+        self.__server_url = server_url
+        self.__study_id = study_id
+        self.__access_token = access_token
+        self.__debug = debug
+
+    @property
+    def server_url(self):
+        return self.__server_url
+
+    @property
+    def study_id(self):
+        return self.__study_id or 'ABC123'
+
+    @property
+    def access_token(self):
+        return self.__access_token
+
+    @property
+    def debug(self):
+        return self.__debug
+
+
+class ManifestEntry(object):
+
+    def __init__(self, fileId, fileName, md5sum):
+        self.fileId = fileId
+        self.fileName = fileName
+        self.md5sum = md5sum
+
+    @classmethod
+    def create_manifest_entry(cls, data):
+        d = data.__dict__
+        fileId = get_required_field(d, 'objectId')
+        fileName = get_required_field(d, 'fileName')
+        md5sum = get_required_field(d, 'fileMd5sum')
+        return ManifestEntry(fileId, fileName, md5sum)
+
+    def __str__(self):
+        return "{}\t{}\t{}".format(self.fileId, self.fileName, self.md5sum)
+
+
+class Manifest(object):
+
+    def __init__(self, analysis_id, manifest_entries=[]):
+        self.__analysis_id = analysis_id
+        self.__entries = manifest_entries
+
+    def add_entry(self, manifest_entry):
+        self.__entries.append(manifest_entry)
+
+    def write(self, output_file_path, overwrite=False):
+        utils.write_object(self, output_file_path, overwrite=overwrite)
+
+    def __str__(self):
+        return "{}\t\t\n".format(self.__analysis_id) + \
+               "\n".join(map(lambda x: str(x), self.__entries))
---
+++ packages/overture-song/overture_song-0.0.3-py2.py3-none-any.whl/overture_song/utils.py
@@ -0,0 +1,125 @@
+# Copyright (c) 2018 The Ontario Institute for Cancer Research. All rights
+# reserved.
+#
+# This program and the accompanying materials are made available under the
+# terms of the GNU Public License v3.0. You should have received a copy of
+# the GNU General Public License along with
+# this program. If not, see <http://www.gnu.org/licenses/>.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING,BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+# IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+
+import json
+import os
+import time
+
+
+def convert_to_url_param_list(delimeter='=', **kwargs):
+    param_list = []
+    for k,v in kwargs.items():
+        param_list.append(k+delimeter+v)
+    return param_list
+
+
+def setup_output_file_path(file_path):
+    parent_dir = os.path.dirname(file_path)
+    if not os.path.exists(parent_dir):
+        os.makedirs(parent_dir)
+
+
+def create_dir(dir_path):
+    if not os.path.exists(dir_path):
+        return os.makedirs(dir_path)
+
+
+def to_json_string(obj):
+    return json.dumps(to_dict(obj), default=lambda o: o.__dict__)
+
+
+def write_object(obj, output_file_path, overwrite=False):
+    setup_output_file_path(output_file_path)
+    if not overwrite and os.path.exists(output_file_path):
+        raise SongClientException('existing.file',
+                                  "The file '{}' already exists and overwriting is disabled".format(output_file_path))
+    with open(output_file_path, 'w') as fh:
+        fh.write(str(obj))
+
+
+def to_json(obj):
+    return json.loads(to_dict(obj))
+
+
+def to_dict(obj):
+    return obj.__dict__
+
+
+def to_pretty_json_string(json_data_string):
+    return json.dumps(json.loads(json_data_string), indent=4, sort_keys=True)
+
+
+def to_bean(item):
+    """
+    Convert a dictionary to an object (recursive).
+    """
+    def convert(item):
+        if isinstance(item, dict):
+            return type('bean', (), {k: convert(v) for k, v in item.items()})
+        if isinstance(item, list):
+            def yield_convert(item):
+                for index, value in enumerate(item):
+                    yield convert(value)
+            return list(yield_convert(item))
+        else:
+            return item
+
+    return convert(item)
+
+
+def check_song_state(expression, id, formatted_message, *args):
+
+    if not expression:
+        raise SongClientException(id, formatted_message.format(*args))
+
+
+def check_state(expression, formatted_message, *args):
+
+    if not expression:
+        raise Exception(formatted_message.format(*args))
+
+
+def check_file(filename):
+    check_state(os.path.exists(filename), "The path {}' does not exist", filename)
+    check_state(os.path.isfile(filename), "The path '{}' is not a file ", filename)
+
+
+def check_dir(dirname):
+    check_state(os.path.exists(dirname), "The path {}' does not exist", dirname)
+    check_state(os.path.isdir(dirname), "The path '{}' is not a directory ", dirname)
+
+
+def check_type(instance, class_type):
+    check_state(isinstance(class_type, type), "The right input argument must be of class type")
+    check_state(isinstance(instance, class_type),
+                "The input instance is not of type '{}'",
+                class_type.__class__.__name__)
+
+
+class SongClientException(Exception):
+
+    def __init__(self, id, message):
+        self.message = message
+        self.id = id
+        self.timestamp = int(time.time())
+
+    def __str__(self):
+        return "[SONG_CLIENT_EXCEPTION {} @ {}]: {}".format(self.id, self.timestamp, self.message)
