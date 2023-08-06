3be7f979f99484a9e367c4d7a7ea049fc9bdf5ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:30 +0000

    Add package mkdocs-ponylang/0.0.9/mkdocs_ponylang-0.0.9.tar.gz

---
+++ packages/mkdocs-ponylang/mkdocs_ponylang-0.0.9.tar.gz/mkdocs_ponylang-0.0.9/setup.py
@@ -0,0 +1,33 @@
+import os
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+VERSION = '0.0.9'
+
+setup(
+    name="mkdocs_ponylang",
+    version=VERSION,
+    url='http://www.mkdocs.org',
+    license='BSD',
+    description='Ponylang theme for MkDocs',
+    long_description=read("README.rst"),
+    author='Matthias Wahl',
+    author_email='matthiaswahl@m7w3.de',
+    packages=find_packages(),
+    include_package_data=True,
+    entry_points={
+        'mkdocs.themes': [
+            'ponylang = mkdocs_ponylang',
+        ]
+    },
+    zip_safe=False,
+    classifiers=[
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Intended Audience :: Developers',
+        'Development Status :: 4 - Beta',
+    ]
+)
