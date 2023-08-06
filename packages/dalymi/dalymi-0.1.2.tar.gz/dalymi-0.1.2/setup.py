9bbaa0ebf3e4590f6ffe63785cecc7fa534642d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:33 +0000

    Add package dalymi/0.1.2/dalymi-0.1.2.tar.gz

---
+++ packages/dalymi/dalymi-0.1.2.tar.gz/dalymi-0.1.2/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+
+setup(author='Joerg Schnitzbauer',
+      author_email='joschnitzbauer@gmail.com',
+      description='A lightweight, data-focused and non-opinionated pipeline manager written in and for Python.',
+      license='MIT',
+      name='dalymi',
+      packages=['dalymi'],
+      url='https://github.com/joschnitzbauer/dalymi',
+      version='0.1.2')
