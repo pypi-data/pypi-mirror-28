b15b8607013a2f84f94a7054f99b0ba36419cee2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:43 +0000

    Add package exceptiongenerator/2.3/exceptiongenerator-2.3-py2.py3-none-any.whl

---
+++ packages/exceptiongenerator/exceptiongenerator-2.3-py2.py3-none-any.whl/exceptiongen/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+def exception_generator(name, parent=Exception, message=''):
+    def __init__(self, **debugvars):
+        lines = [message]
+        if len(debugvars) > 0:
+            lines.append('Printing debug variables:')
+        for n, v in debugvars.items():
+            lines.append(str(n) + ': ' + str(v))
+        Exception.__init__(self, '\n'.join(lines))
+    return type(name, (parent,), {'__init__': __init__})
