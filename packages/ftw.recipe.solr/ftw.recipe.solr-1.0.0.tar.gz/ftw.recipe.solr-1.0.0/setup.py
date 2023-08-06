9063ae29ee7b31eb90ead009baabc4650be0ce14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:53:50 +0000

    Add package ftw.recipe.solr/1.0.0/ftw.recipe.solr-1.0.0.tar.gz

---
+++ packages/ftw.recipe.solr/ftw.recipe.solr-1.0.0.tar.gz/ftw.recipe.solr-1.0.0/setup.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+import os
+from setuptools import setup, find_packages
+
+version = '1.0.0'
+tests_require = ['zope.testing', 'manuel']
+
+setup(
+    name='ftw.recipe.solr',
+    version=version,
+    description="A zc.buildout recipe to install a Solr server",
+    long_description=open("README.rst").read()
+    + "\n" +
+    open("HISTORY.txt").read()
+    + "\n" +
+    open(os.path.join("ftw", "recipe", "solr", "README.txt")).read(),
+    classifiers=[
+        'Framework :: Buildout :: Recipe',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Zope Public License',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Build Tools',
+    ],
+    keywords='',
+    author='4teamwork AG',
+    author_email='mailto:info@4teamwork.ch',
+    url='https://github.com/4teamwork/ftw.recipe.solr',
+    license='GPL2',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['ftw', 'ftw.recipe'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'zc.buildout',
+        'jinja2',
+    ],
+    tests_require=tests_require,
+    extras_require=dict(tests=tests_require),
+    test_suite='ftw.recipe.solr.tests.test_docs.test_suite',
+    entry_points={
+        'zc.buildout': ['default = ftw.recipe.solr:Recipe'],
+    },
+)
