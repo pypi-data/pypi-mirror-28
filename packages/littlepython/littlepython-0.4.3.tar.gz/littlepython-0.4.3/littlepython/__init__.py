c83bdbd6bfd2c367a183441ab3e44fd905a95cfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:16 +0000

    Add package littlepython/0.4.3/littlepython-0.4.3.tar.gz

---
+++ packages/littlepython/littlepython-0.4.3.tar.gz/littlepython-0.4.3/littlepython/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import
+from __future__ import unicode_literals
+from .lp import Compiler
+from .feature import Features
+from .version import version
+
+__all__ = ["Compiler", "Features"]
+__version__ = version
