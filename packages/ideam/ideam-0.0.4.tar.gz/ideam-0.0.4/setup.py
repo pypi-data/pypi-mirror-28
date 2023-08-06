0d676d486c4915aa4ebc61627220b78bc9d02695
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:21 +0000

    Add package ideam/0.0.4/ideam-0.0.4.tar.gz

---
+++ packages/ideam/ideam-0.0.4.tar.gz/ideam-0.0.4/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup
+import os
+install_requires = ['chardet', 'multidict>=3.0.0',
+                    'async_timeout>=1.2.0', 'yarl>=0.11', 'aiohttp>=2.3.0']
+
+
+def read(f):
+    return open(os.path.join(os.path.dirname(__file__), f)).read().strip()
+
+setup(
+  name='ideam',
+  packages=['ideam'],
+  version='0.0.4',
+  long_description='\n\n'.join((read('README.rst'), "  ")),
+  description='Python SDK for IoT Data Exchange & Analytics Middleware',
+  author='Harish Anand',
+  author_email='smartcity@rbccps.org',
+  url='https://github.com/rbccps-iisc/ideam-python-sdk',
+  download_url='https://github.com/rbccps-iisc/ideam-python-sdk/archive/0.0.4.tar.gz',
+  keywords=['IoT', 'smartcity', 'ideam'],
+  license="ISC",
+  python_requires='>=3.5.4',
+  install_requires=install_requires,
+  classifiers=[
+    "Development Status :: 5 - Production/Stable",
+    "Topic :: Internet",
+    "Topic :: Scientific/Engineering :: Information Analysis",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: ISC License (ISCL)",
+    "Operating System :: POSIX :: Linux",
+    "Operating System :: MacOS",
+    'Programming Language :: Python :: 3 :: Only',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
