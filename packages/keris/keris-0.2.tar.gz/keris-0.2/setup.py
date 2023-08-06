9bfb5afd877bd8307fdf7cb4726b81838c805f31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:36 +0000

    Add package keris/0.2/keris-0.2.tar.gz

---
+++ packages/keris/keris-0.2.tar.gz/keris-0.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='keris',
+      version='0.2',
+      description='Simple keras-like deep learning framework built with numpy',
+      url='https://github.com/arrizalamin/keris',
+      download_url='https://github.com/arrizalamin/keris/archive/0.1.tar.gz',
+      author='Arrizal Amin',
+      author_email='arrizalamin@gmail.com',
+      license='MIT',
+      packages=['keris'],
+      keywords=['deep learning', 'keris'],
+      install_requires=['numpy>=1.13.3',
+                        'tqdm>=4.19.4'])
