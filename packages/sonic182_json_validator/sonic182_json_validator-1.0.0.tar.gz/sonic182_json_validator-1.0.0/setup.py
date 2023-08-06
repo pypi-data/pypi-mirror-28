51285a4fb22f72ab0776ae7e05556f740f42968a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:19:02 +0000

    Add package sonic182_json_validator/1.0.0/sonic182_json_validator-1.0.0.tar.gz

---
+++ packages/sonic182_json_validator/sonic182_json_validator-1.0.0.tar.gz/sonic182_json_validator-1.0.0/setup.py
@@ -0,0 +1,47 @@
+"""Setup module."""
+
+from setuptools import setup
+from pip.req import parse_requirements
+
+REQS = [str(ir.req) for ir in parse_requirements(
+    'requirements.txt', session='hack')]
+REQS2 = [str(ir.req) for ir in parse_requirements(
+    'dev-requirements.txt', session='hack')]
+
+with open('VERSION') as _file:
+    VERSION = _file.read()
+
+setup(
+    name='sonic182_json_validator',
+    version=VERSION,
+    description='A custom json validator',
+    author='Johanderson Mogollon',
+    author_email='johanderson@mogollon.com.ve',
+    url='https://github.com/sonic182/json_validator',
+    license='MIT',
+    packages=['json_validator'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development',
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords='json validator dictionary',
+    setup_requires=['pytest-runner'],
+    test_requires=['pytest'],
+    install_requires=REQS,
+    extras_require={
+        'dev': REQS2,
+        'test': [
+            'pytest',
+            'coverage'
+        ]
+    }
+)
