74bb2f78048b7163d6628c5c6a18ef440faf760f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:04 +0000

    Add package og-fetcher/0.2/og-fetcher-0.2.tar.gz

---
+++ packages/og-fetcher/og-fetcher-0.2.tar.gz/og-fetcher-0.2/README.rst
@@ -0,0 +1,17 @@
+og-fetcher
+==========
+Open Graph fetcher for python.
+
+Usage
+-----
+.. code:: python
+
+  from og_fetcher import fetch
+
+  og_data = fetch('http://ogp.me')
+
+Installation
+------------
+.. code::
+
+  pip install og-fetcher
---
+++ packages/og-fetcher/og-fetcher-0.2.tar.gz/og-fetcher-0.2/og_fetcher/__init__.py
@@ -0,0 +1,33 @@
+from urllib.request import urlopen
+from html.parser import HTMLParser
+
+class OGHTMLParser(HTMLParser):
+    og_tags = []
+    def handle_starttag(self, tag, attrs):
+        if not tag == 'meta':
+            return
+        og_tag = {}
+        is_og = False
+        for attr in attrs:
+            if attr[0] == 'property' and attr[1].startswith('og:'):
+                is_og = True
+        if is_og:
+            for attr in attrs:
+                og_tag[attr[0]] = attr[1]
+            self.og_tags.append(og_tag)
+
+def parse(html):
+    parser = OGHTMLParser()
+    parser.feed(html)
+    return parser.og_tags
+
+def fetch(url):
+    response = urlopen(url)
+    data = response.read()
+    html = data.decode('utf8')
+    response.close()
+    og_datas = parse(html)
+    og = {}
+    for og_data in og_datas:
+        og[og_data['property']] = og_data['content']
+    return og
