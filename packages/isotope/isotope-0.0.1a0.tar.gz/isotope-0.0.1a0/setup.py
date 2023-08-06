ce0c14ddaa0fa8c8ccf5a3ea701b0c9dc56b516d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:01 +0000

    Add package isotope/0.0.1a0/isotope-0.0.1a0.tar.gz

---
+++ packages/isotope/isotope-0.0.1a0.tar.gz/isotope-0.0.1a0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='isotope',
+      description='A library to do a stateful logging',
+      version='0.0.1a',
+      url='https://github.com/codekidX/isotope',
+      author='Ashish Shekar',
+      author_email='ckrackerx15@gmail.com',
+      license='Apache2',
+      classifiers=[],
+      packages=['isotope'],
+      keywords = ['logging', 'logger', 'stateful-logging']
+)
