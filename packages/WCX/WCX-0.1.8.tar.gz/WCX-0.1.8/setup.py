6b64cde3305560f2a5015c0845ebf2f539a67087
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:08 +0000

    Add package WCX/0.1.8/WCX-0.1.8.tar.gz

---
+++ packages/WCX/WCX-0.1.8.tar.gz/WCX-0.1.8/setup.py
@@ -0,0 +1,33 @@
+
+# Always prefer setuptools over distutils
+from setuptools import setup
+
+import os
+
+long_description = 'WCX wallets are most secured digital wallets which are used to send and receive virtual currencies like bitcoin.'
+if os.path.exists('README.txt'):
+    long_description = open('README.txt').read()
+
+setup(
+      name='WCX',
+      version='0.1.8',
+      description='The easiest way to integrate WCX Api Functions in your applications. Sign up at WCX.com for your API key.',
+      long_description=long_description,
+      author='OsizTech',
+      author_email='veerasarma@osiztechnologies.com',
+      license='ISC',
+      url = "http://www.osiztechnologies.com/",
+      keywords = "WCX wcx_wallet bitcoin_api wcx wallet wcx_wallet_api",
+      packages=['wcx'],
+      install_requires=[
+          'requests',
+          'pycrypto',
+          'ecdsa',
+          'six',
+          'base58'
+      ],
+      zip_safe=False
+      )
+
+# for packages not on pypi
+# dependency_links=['http://github.com/user/repo/tarball/master#egg=package-1.0']
