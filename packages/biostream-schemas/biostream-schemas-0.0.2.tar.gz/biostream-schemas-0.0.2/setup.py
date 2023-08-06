9b537cd7774ad9b7a6f622581dd1f7deaca74509
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:10 +0000

    Add package biostream-schemas/0.0.2/biostream-schemas-0.0.2.tar.gz

---
+++ packages/biostream-schemas/biostream-schemas-0.0.2.tar.gz/biostream-schemas-0.0.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='biostream-schemas',
+    version='0.0.2',
+    packages=['bmeg']
+)
+
+# [
+#         'bmeg/clinical_pb2.py',
+#         'bmeg/cna_pb2.py',
+#         'bmeg/genome_pb2.py',
+#         'bmeg/phenotype_pb2.py',
+#         'bmeg/rna_pb2.py',
+#         'bmeg/variants_pb2.py'
+#     ]
