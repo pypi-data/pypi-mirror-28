2ee4f7854e3f3f2c3b424e27364cc62a1e883439
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:19 +0000

    Add package django-async-include/0.6.1/django-async-include-0.6.1.tar.gz

---
+++ packages/django-async-include/django-async-include-0.6.1.tar.gz/django-async-include-0.6.1/async_include/urls.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from django.conf.urls import url
+
+from async_include import views
+
+urlpatterns = [
+    # Load
+    url(r'^get/?$', views.get_template, name="get_template"),
+]
