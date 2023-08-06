516a763108689555db7cce7809a854ecdb5cc6a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:43 +0000

    Add package exceptiongenerator/2.3/exceptiongenerator-2.3.tar.gz

---
+++ packages/exceptiongenerator/exceptiongenerator-2.3.tar.gz/exceptiongenerator-2.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(name='exceptiongenerator',
+      version='2.3',
+      description='Easy way to generate exceptions that just have a message',
+      url='https://github.com/Poketec/ExceptionGenerator',
+      author='PokestarFan',
+      author_email='pokestarfan@yahoo.com',
+      license='MIT',
+      packages=['exceptiongen'],
+      zip_safe=True)
