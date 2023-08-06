193c6d741ded42f5f3a82f3ada4d565a0c8d67cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:00 +0000

    Add package nester_fc/1.0.0/nester_fc-1.0.0.tar.gz

---
+++ packages/nester_fc/nester_fc-1.0.0.tar.gz/nester_fc-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='nester_fc',
+    version='1.0.0',
+    py_modules=['nester_fc'],
+    author='fc',
+    author_email='85773651@qq.com',
+    url='',
+    description='A simple printer of nested lists',
+    )
