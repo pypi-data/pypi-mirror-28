824559052ac72a182fd8ad851c04daa4ce68d713
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:33 +0000

    Add package saige001/1.1.0/saige001-1.1.0.tar.gz

---
+++ packages/saige001/saige001-1.1.0.tar.gz/saige001-1.1.0/setup.py
@@ -0,0 +1,12 @@
+
+from distutils.core import setup
+
+setup(
+    name = 'saige001',      
+    version = '1.1.0',
+    py_modules = ['test01'],
+    author = 'paulwangs',        
+    author_email = '294841432@qq.com',
+    url = 'http://www.baidu.com ',
+    description = 'a test'   
+)
