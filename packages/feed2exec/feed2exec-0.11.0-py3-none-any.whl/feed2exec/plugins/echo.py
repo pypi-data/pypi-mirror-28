a64877245aa6c0da480a6a5c404eca8c37cb2e7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:44 +0000

    Add package feed2exec/0.11.0/feed2exec-0.11.0-py3-none-any.whl

---
+++ packages/feed2exec/feed2exec-0.11.0-py3-none-any.whl/feed2exec/plugins/echo.py
@@ -0,0 +1,23 @@
+import shlex
+
+
+class output(object):
+    """
+    This plugin outputs, to standard output, the arguments it receives. It
+    can be useful to test your configuration. It also creates a side
+    effect for the test suite to determine if the plugin was called.
+
+    This plugin does a similar thing when acting as a filter.
+    """
+
+    called = None
+
+    def __init__(self, *args, feed=None, **kwargs):
+        print(" ".join([shlex.quote(x) for x in args]))
+        if not feed.get('catchup'):
+            output.called = args
+
+
+#: This filter just keeps the feed unmodified. It is just there for testing
+#: purposes.
+filter = output
