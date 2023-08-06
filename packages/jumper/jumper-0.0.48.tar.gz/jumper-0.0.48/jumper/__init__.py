ff33435cad6ba983cab767d8fa62e588a0c5d18e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:35 +0000

    Add package jumper/0.0.48/jumper-0.0.48.tar.gz

---
+++ packages/jumper/jumper-0.0.48.tar.gz/jumper-0.0.48/jumper/__init__.py
@@ -0,0 +1,7 @@
+"""
+:copyright: (c) 2017 by Jumper Labs Ltd.
+:license: Apache 2.0, see LICENSE.txt for more details.
+"""
+from .__version__ import __version__
+from .vlab import Vlab
+from .vlab_hci_device import VirtualHciDevice, BluezError
