4a592a39f0b5e34cec9d10df04da169b4273d869
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:31 +0000

    Add package ttn/2.1.0/ttn-2.1.0.tar.gz

---
+++ packages/ttn/ttn-2.1.0.tar.gz/ttn-2.1.0/setup.py
@@ -0,0 +1,31 @@
+# coding: Latin-1
+# Copyright © 2018 The Things Network
+# Use of this source code is governed by the
+# MIT license that can be found in the LICENSE file.
+
+from setuptools import setup
+
+import io
+
+with io.open("README.rst", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(name="ttn",
+      version="2.1.0",
+      description="The Things Network Client",
+      long_description = long_description,
+      url = "https://github.com/TheThingsNetwork/python-app-sdk",
+      author="Emmanuelle Lejeail",
+      author_email="emmanuelle@thethingsindustries.com",
+      license="MIT",
+      packages=["ttn", "github_com", "utils"],
+      install_requires=[
+          "paho-mqtt",
+          "events",
+          "grpcio",
+          # packages which need to be imported to make gRPC work
+          "protobuf",
+          "google-api-python-client",
+          "google-cloud"
+      ],
+      zip_safe=False)
