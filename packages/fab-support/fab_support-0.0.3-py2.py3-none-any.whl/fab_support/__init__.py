55c3f269ddbdb8380bab77b8d7438ae0156cb2fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:08 +0000

    Add package fab-support/0.0.3/fab_support-0.0.3-py2.py3-none-any.whl

---
+++ packages/fab-support/fab_support-0.0.3-py2.py3-none-any.whl/fab_support/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for fab_support."""
+
+__author__ = """Humphrey Drummond"""
+__email__ = 'hum3@drummond.info'
+__version__ = '0.0.3'
+
+
+from .env_support import *
+from .stages_support import set_stages, list_stages
+from .pelican import *
+from .utils import *
+
+deploy = pelican.deploy
+
