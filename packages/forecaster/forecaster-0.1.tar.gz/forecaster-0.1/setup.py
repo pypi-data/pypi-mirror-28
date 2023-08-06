ac17af4088331e061b7161a8f362e19d31a427a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:26 +0000

    Add package forecaster/0.1/forecaster-0.1.tar.gz

---
+++ packages/forecaster/forecaster-0.1.tar.gz/forecaster-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='forecaster',
+      version='0.1',
+      description='multiple time series forecasting',
+      url='https://github.com/kylinorange/insight/tree/master/forecaster',
+      author='Jun Xie',
+      author_email='kylinorange@gmail.com',
+      license='BSD',
+      packages=['forecaster'],
+      zip_safe=False)
