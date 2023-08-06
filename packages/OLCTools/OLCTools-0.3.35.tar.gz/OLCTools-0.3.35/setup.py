0a92255229c439ff546beb2b3149b5f230a5839c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:01 +0000

    Add package OLCTools/0.3.35/OLCTools-0.3.35.tar.gz

---
+++ packages/OLCTools/OLCTools-0.3.35.tar.gz/OLCTools-0.3.35/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name="OLCTools",
+    version="0.3.35",
+    packages=find_packages(),
+    author="Andrew Low",
+    author_email="andrew.low@inspection.gc.ca",
+    url="https://github.com/lowandrew/OLCTools"
+)
