f9d8733b79d518ce3f1af4e477d8616c9a25cf8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:13 +0000

    Add package sinaweibo/0.3.8/sinaweibo-0.3.8.tar.gz

---
+++ packages/sinaweibo/sinaweibo-0.3.8.tar.gz/sinaweibo-0.3.8/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup,find_packages
+setup(
+        name='sinaweibo',
+        version='0.3.8',
+        description='SinaWeibo',
+        author='xjouyi@163.com',
+        author_email='xjouyi@163.com',
+        url='https://github.com/XJouYi/SinaWeibo',
+        packages=['SinaWeibo','SinaWeibo.common'],
+        include_package_data=True,
+        install_requires=['requests', 'rsa','beautifulsoup4'],
+)
