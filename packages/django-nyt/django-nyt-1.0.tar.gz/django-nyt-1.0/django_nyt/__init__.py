66d5904546b048686249e47fc3a508c3120732c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:38 +0000

    Add package django-nyt/1.0/django-nyt-1.0.tar.gz

---
+++ packages/django-nyt/django-nyt-1.0.tar.gz/django-nyt-1.0/django_nyt/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+_disable_notifications = False
+
+VERSION = "1.0"
+__version__ = VERSION
+
+default_app_config = "django_nyt.apps.DjangoNytConfig"
+
+
+def notify(*args, **kwargs):
+    """
+    DEPRECATED - please access django_nyt.utils.notify
+    """
+    from django_nyt.utils import notify
+    return notify(*args, **kwargs)
