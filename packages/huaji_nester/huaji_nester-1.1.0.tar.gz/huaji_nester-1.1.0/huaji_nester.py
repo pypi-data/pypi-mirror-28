516b34bf174e758c95f4c8ce39feafea48c4768e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:37 +0000

    Add package huaji_nester/1.1.0/huaji_nester-1.1.0.tar.gz

---
+++ packages/huaji_nester/huaji_nester-1.1.0.tar.gz/huaji_nester-1.1.0/huaji_nester.py
@@ -0,0 +1,12 @@
+"""this is the huaji_nester.py module """
+
+def print_lol(the_list, level):
+    #the function ...
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol(each_item, level+1)
+        else:
+            for tab_stop in range(level):
+                print('\t', end = '')
+            print('-', end = '')
+            print(each_item)
