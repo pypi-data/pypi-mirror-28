89af303066205f31571c58f5ba48267917ae2893
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:53:24 +0000

    Add package wincast/0.0.11/wincast-0.0.11.tar.gz

---
+++ packages/wincast/wincast-0.0.11.tar.gz/wincast-0.0.11/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+
+install_requires = [
+    'dill==0.2.5',
+    'easydict==1.6',
+    'jsonpickle==0.9.3',
+    'numpy==1.14',
+    'pandas==0.19.1',
+    'scikit-learn==0.18.1',
+    'scipy==0.18.1',
+    'tabulate==0.7.7',
+]
+
+setup(
+    name="wincast",
+    version='0.0.11',
+    url='https://github.com/kahnjw/wincast',
+    author_email='jarrod.kahn+wincast@gmail.com',
+    license='MIT',
+    packages=find_packages(exclude=['tests', 'tests.*']),
+    install_requires=install_requires,
+    package_data={
+        'wincast': ['models/wincast.clf.pkl', 'models/wincast.scaler.pkl']
+    }
+)
