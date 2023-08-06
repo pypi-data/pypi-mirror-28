271294d8b5d215fe9a1d0f4f4cb80ae0e9d5ea9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:51 +0000

    Add package Products.ExtendedPathIndex/3.3.0/Products.ExtendedPathIndex-3.3.0.tar.gz

---
+++ packages/Products.ExtendedPathIndex/Products.ExtendedPathIndex-3.3.0.tar.gz/Products.ExtendedPathIndex-3.3.0/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+
+version = '3.3.0'
+
+setup(
+    name='Products.ExtendedPathIndex',
+    version=version,
+    description="Zope catalog index for paths",
+    long_description=(open("README.rst").read() + "\n" + \
+                      open("CHANGES.rst").read()),
+    classifiers=[
+        "Environment :: Web Environment",
+        "Framework :: Plone",
+        "Framework :: Plone :: 5.0",
+        "Framework :: Plone :: 5.1",
+        "Framework :: Zope2",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+      ],
+    keywords='Zope catalog index',
+    author='Plone Foundation',
+    author_email='plone-developers@lists.sourceforge.net',
+    url='https://pypi.python.org/pypi/Products.ExtendedPathIndex',
+    license='GPL version 2',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['Products'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'AccessControl',
+        'Products.ZCatalog',
+        'six',
+        'transaction',
+        'ZODB3',
+        'Zope2 >= 2.13.0a3',
+    ],
+    )
