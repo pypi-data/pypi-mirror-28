9dc10f28710e1d34d2fb34be4be9b359fa5ce3ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:35 +0000

    Add package sprockets.http/1.5.0/sprockets.http-1.5.0.tar.gz

---
+++ packages/sprockets.http/sprockets.http-1.5.0.tar.gz/sprockets.http-1.5.0/docs/conf.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+import alabaster
+from sprockets import http
+
+project = 'sprockets.http'
+copyright = 'AWeber Communications, Inc.'
+version = http.__version__
+release = '.'.join(str(v) for v in http.version_info[0:2])
+
+needs_sphinx = '1.0'
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.viewcode',
+    'sphinxcontrib.autohttp.tornado',
+]
+
+templates_path = []
+source_suffix = '.rst'
+source_encoding = 'utf-8-sig'
+master_doc = 'index'
+exclude_patterns = []
+pygments_style = 'sphinx'
+html_theme = 'alabaster'
+html_theme_path = [alabaster.get_path()]
+html_sidebars = {
+    '**': ['about.html', 'navigation.html'],
+}
+html_static_path = ['_static']
+html_theme_options = {
+    'github_user': 'sprockets',
+    'github_repo': 'sprockets.http',
+    'description': 'Tornado application runner',
+    'github_banner': True,
+}
+html_static_path = ['_static']
+
+intersphinx_mapping = {
+    'python': ('http://docs.python.org/3/', None),
+    'tornado': ('http://tornadoweb.org/en/latest/', None),
+}
---
+++ packages/sprockets.http/sprockets.http-1.5.0.tar.gz/sprockets.http-1.5.0/setup.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+#
+
+import os.path
+
+import setuptools
+
+from sprockets import http
+
+
+def read_requirements(filename):
+    requirements = []
+    try:
+        with open(os.path.join('requires', filename)) as req_file:
+            for line in req_file:
+                if '#' in line:
+                    line = line[:line.index('#')]
+                line = line.strip()
+                if line.startswith('-'):
+                    pass
+                requirements.append(line)
+    except IOError:
+        pass
+    return requirements
+
+
+requirements = read_requirements('installation.txt')
+tests_require = read_requirements('testing.txt')
+
+setuptools.setup(
+    name='sprockets.http',
+    version=http.__version__,
+    description='Tornado HTTP application runner',
+    author='AWeber Communications',
+    author_email='api@aweber.com',
+    url='https://github.com/sprockets/sprockets.http',
+    install_requires=requirements,
+    license='BSD',
+    namespace_packages=['sprockets'],
+    packages=setuptools.find_packages(),
+    entry_points={
+        'distutils.commands': ['httprun=sprockets.http.runner:RunCommand'],
+    },
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: No Input/Output (Daemon)',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules'],
+    test_suite='nose.collector',
+    tests_require=tests_require,
+    zip_safe=True,
+)
