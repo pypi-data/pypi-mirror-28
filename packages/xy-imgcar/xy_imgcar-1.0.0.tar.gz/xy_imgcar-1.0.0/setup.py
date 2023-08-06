f804dd8a454c5e5d32ccf256976a83b169f3ec24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:47 +0000

    Add package xy-imgcar/1.0.0/xy_imgcar-1.0.0.tar.gz

---
+++ packages/xy-imgcar/xy_imgcar-1.0.0.tar.gz/xy_imgcar-1.0.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'xy_imgcar',
+  packages = ['xy_imgcar'],
+  version = '1.0.0',
+  description = 'Recognition Image Car Brand',
+  long_description='Recognition Image Car Brand',
+  author = 'KingHS',
+  author_email = '382771946@qq.com',
+  keywords = ['pip3', 'python3','python','image tag','Recognition Image Tag'],
+  license='MIT',
+  install_requires=['requests']
+)
