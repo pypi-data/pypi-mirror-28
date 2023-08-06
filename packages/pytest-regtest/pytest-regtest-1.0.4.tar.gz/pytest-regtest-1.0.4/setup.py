47a9b5d1e5778033f77a6cb60a002aec45aeb6ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:25 +0000

    Add package pytest-regtest/1.0.4/pytest-regtest-1.0.4.tar.gz

---
+++ packages/pytest-regtest/pytest-regtest-1.0.4.tar.gz/pytest-regtest-1.0.4/setup.py
@@ -0,0 +1,45 @@
+import os
+import sys
+from setuptools import setup
+
+VERSION = (1, 0, 4)
+
+AUTHOR = "Uwe Schmitt"
+AUTHOR_EMAIL = "uwe.schmitt@id.ethz.ch"
+
+DESCRIPTION = "pytest plugin for regression tests"
+
+LICENSE = "http://opensource.org/licenses/GPL-3.0"
+
+URL = "https://sissource.ethz.ch/sispub/pytest-regtest/tree/master"
+
+
+if len(sys.argv) > 1 and "dist" in sys.argv[1]:
+
+    here = os.path.dirname(os.path.abspath(__file__))
+    LONG_DESCRIPTION = "\n" + open(os.path.join("README.md")).read()
+
+else:
+    LONG_DESCRIPTION = ""
+
+if __name__ == "__main__":
+
+    setup(
+        version="%d.%d.%d" % VERSION,
+        name="pytest-regtest",
+        py_modules=['pytest_regtest'],
+        author=AUTHOR,
+        author_email=AUTHOR_EMAIL,
+        url=URL,
+        license=LICENSE,
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+
+        # the following makes a plugin available to pytest
+        entry_points={
+            'pytest11': [
+                'regtest = pytest_regtest',
+            ]
+        },
+        install_requires=["pytest>=3.3.2"],
+    )
