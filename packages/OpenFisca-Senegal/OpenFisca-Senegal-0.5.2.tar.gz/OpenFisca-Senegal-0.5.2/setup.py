997eb74007db4c688003dc184b0c5ea3a0fbf33b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:59 +0000

    Add package OpenFisca-Senegal/0.5.2/OpenFisca-Senegal-0.5.2.tar.gz

---
+++ packages/OpenFisca-Senegal/OpenFisca-Senegal-0.5.2.tar.gz/OpenFisca-Senegal-0.5.2/setup.py
@@ -0,0 +1,49 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='OpenFisca-Senegal',
+    version='0.5.2',
+    author='OpenFisca Team',
+    author_email='contact@openfisca.fr',
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: POSIX",
+        "Programming Language :: Python",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+        ],
+    description=u'Senegalese tax and benefit system for OpenFisca',
+    keywords='benefit microsimulation senegal social tax',
+    license='http://www.fsf.org/licensing/licenses/agpl-3.0.html',
+    url='https://github.com/openfisca/senegal',
+
+    data_files=[
+        ('share/openfisca/openfisca-senegal',
+         ['CHANGELOG.md', 'LICENSE', 'README.md']),
+        ],
+    extras_require={
+        'notebook': [
+            'matplotlib',
+            'notebook',
+            'openpyxl'
+            'pandas',
+            'xlrd',
+            'xlwt',
+            ],
+        'survey': [
+            'OpenFisca-Survey-Manager >= 0.9.2',
+            ]
+        },
+    include_package_data = True,  # Will read MANIFEST.in
+    install_requires=[
+        'OpenFisca-Core >= 20.0, < 21.0',
+        'PyYAML >= 3.10',
+        ],
+    packages=find_packages(exclude=['openfisca_senegal.tests*']),
+    test_suite='nose.collector',
+    )
