dbf213d56cafee69f856f631a4a3448ea33e4591
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:31 +0000

    Add package bulv1ne-django-utils/0.2.3/bulv1ne-django-utils-0.2.3.tar.gz

---
+++ packages/bulv1ne-django-utils/bulv1ne-django-utils-0.2.3.tar.gz/bulv1ne-django-utils-0.2.3/utils/templatetags/md.py
@@ -0,0 +1,21 @@
+from django import template
+from django.utils.safestring import mark_safe
+from markdown import markdown
+
+register = template.Library()
+
+
+@register.tag(name='markdown')
+def do_markdown(parser, token):
+    nodelist = parser.parse(('endmarkdown',))
+    parser.delete_first_token()
+    return MarkdownNode(nodelist)
+
+
+class MarkdownNode(template.Node):
+    def __init__(self, nodelist):
+        self.nodelist = nodelist
+
+    def render(self, context):
+        value = self.nodelist.render(context)
+        return mark_safe(markdown(value))
