12b7f18f6766f21d01e514d593836dbded9ae467
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:21 +0000

    Add package krypy/2.1.7/krypy-2.1.7.tar.gz

---
+++ packages/krypy/krypy-2.1.7.tar.gz/krypy-2.1.7/setup.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+import os
+import codecs
+
+# Use setuptools for these commands (they don't work well or at all
+# with distutils).  For normal builds use distutils.
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+# shamelessly copied from VoroPy
+def read(fname):
+    try:
+        content = codecs.open(
+            os.path.join(os.path.dirname(__file__), fname),
+            encoding='utf-8'
+            ).read()
+    except Exception:
+        content = ''
+    return content
+
+setup(name='krypy',
+      packages=['krypy', 'krypy.recycling'],
+      version='2.1.7',
+      description='Krylov subspace methods for linear systems',
+      long_description=read('README.rst'),
+      author='André Gaul',
+      author_email='gaul@web-yard.de',
+      url='https://github.com/andrenarchy/krypy',
+      install_requires=['numpy (>=1.11)', 'scipy (>=0.17)'],
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 3',
+          'Topic :: Scientific/Engineering :: Mathematics'
+          ],
+      )
