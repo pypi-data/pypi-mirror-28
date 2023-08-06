0c2d942024a608a76ba90a22232e89e0bebcceff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:19:02 +0000

    Add package supermodule/2018.1.21.22/supermodule-2018.1.21.22.tar.gz

---
+++ packages/supermodule/supermodule-2018.1.21.22.tar.gz/supermodule-2018.1.21.22/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+import setuptools
+import pypandoc
+
+def read(*paths):
+    with open(os.path.join(*paths), "r") as filename:
+        return filename.read()
+
+def main():
+
+    setuptools.setup(
+        name             = "supermodule",
+        version          = "2018.01.21.0022",
+        description      = "super utilities",
+        #long_description = (read("README.md")),
+        long_description = pypandoc.convert("README.md", "rst"),
+        url              = "https://github.com/wdbm/junk",
+        author           = "John Drake",
+        author_email     = "j.drake@sern.ch",
+        license          = "GPLv3",
+        py_modules       = [
+                           "supermodule"
+                           ],
+        entry_points     = """
+                           [console_scripts]
+                           supermodule = supermodule:supermodule
+                           superdupermodule = superdupermodule:superdupermodule
+                           """
+    )
+
+if __name__ == "__main__":
+    main()
