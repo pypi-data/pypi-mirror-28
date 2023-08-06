563d773810d1c1d82cb65db91250d33453060488
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:02 +0000

    Add package psp/0.0.2/psp-0.0.2.tar.gz

---
+++ packages/psp/psp-0.0.2.tar.gz/psp-0.0.2/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+import os
+from setuptools import find_packages, setup
+
+NAME = 'psp'
+PACKAGES = 'psp'
+META_PATH = os.path.join('psp', '__init__.py')
+KEYWORDS = [ 'color' 'colour' 'terminal' 'text' 'ansi' 'minimalist' ]
+CLASSIFIERS = [
+    'Environment :: Console',
+    'Intended Audience :: Developers',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Topic :: Terminals',
+]
+
+# see classifiers http://pypi.python.org/pypi?%3Aaction=list_classifiers
+if __name__ == '__main__':
+    setup(
+        name=NAME,
+        version='0.0.2',
+        description='python color printer - minimalist color printing',
+        long_description='python simple (color) printing - simple barebones wrappers for 256-color stdout.',
+        license='MIT',
+        url='https://github.com/someburner/psp',
+        author='Jeff Hufford',
+        author_email='jeffrey92@gmail.com',
+        maintainer='Jeff Hufford',
+        keywords=KEYWORDS,
+        packages=find_packages('psp'),
+        package_dir={'': '.'},
+        classifiers=CLASSIFIERS,
+        python_requires='>=3.5',
+    )
