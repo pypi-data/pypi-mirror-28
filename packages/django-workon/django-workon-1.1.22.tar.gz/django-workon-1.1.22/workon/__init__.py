6e162d8a4ddb5840765467934a47281fb320fb3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:01 +0000

    Add package django-workon/1.1.22/django-workon-1.1.22.tar.gz

---
+++ packages/django-workon/django-workon-1.1.22.tar.gz/django-workon-1.1.22/workon/__init__.py
@@ -0,0 +1,15 @@
+import os
+default_app_config = 'workon.conf.WorkonConfig'
+
+# if os.environ.get('DJANGO_SETTINGS_MODULE'):
+
+try:
+	from workon.utils import *
+	from workon.views import *
+	from workon.fields import *
+except:
+	pass
+
+	# from workon.models.tracked import (
+	# 	Tracked as TrackedModel
+	# )
