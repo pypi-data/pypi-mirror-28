22f62c7d9b48d35f434cb6f447d8e4575a8524a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:55 +0000

    Add package zcx811/1.1.0/zcx811-1.1.0.tar.gz

---
+++ packages/zcx811/zcx811-1.1.0.tar.gz/zcx811-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+		name			= 'zcx811',
+		version			= '1.1.0',
+		py_modules		= ['nester'],
+		author			= 'hfpython',
+		author_email	= 'hfpython@headfirstlabs.com',
+		url				= 'http://www.headfirstlabs.com',
+		description		= 'A simples printer of nested lists',
+	 )
