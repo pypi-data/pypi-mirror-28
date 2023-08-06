eeccda757c15da6434b1e7f23955265b5837f46f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:03 +0000

    Add package pf9-saml-auth/0.0.2/pf9_saml_auth-0.0.2-py2-none-any.whl

---
+++ packages/pf9-saml-auth/pf9_saml_auth-0.0.2-py2-none-any.whl/pf9_saml_auth/__init__.py
@@ -0,0 +1,25 @@
+# Copyright 2018 Platform9 Systems, Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License. You may obtain a copy
+# of the License at
+
+#   http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from pf9_saml_auth import v3
+
+_ADFS_AVAILABLE = v3._ADFS_AVAILABLE
+_OKTA_AVAILABLE = v3._OKTA_AVAILABLE
+_ONELOGIN_AVAILABLE = v3._ONELOGIN_AVAILABLE
+
+V3Pf9ADFSPassword = v3.ADFSPassword
+V3Pf9SamlOkta = v3.OktaPassword
+V3Pf9SamlOnelogin = v3.OneloginPassword
+
+__all__ = ('V3Pf9ADFSPassword', 'V3Pf9SamlOkta', 'V3Pf9SamlOnelogin')
