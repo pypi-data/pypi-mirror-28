fb56bfa6313449f893f528de21cd09feea63347f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:52 +0000

    Add package businessoptics/0.2.7/businessoptics-0.2.7.tar.gz

---
+++ packages/businessoptics/businessoptics-0.2.7.tar.gz/businessoptics-0.2.7/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+
+with open('README.md') as f:
+    readme = f.read()
+
+
+setup(name='businessoptics',
+      version='0.2.7',
+      description='Client for the BusinessOptics API',
+      long_description=readme,
+      classifiers=[
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.5',
+      ],
+      url='https://github.com/BusinessOptics/businessoptics_client',
+      author='BusinessOptics',
+      author_email='alex.mojaki@gmail.com',
+      license='MIT',
+      packages=['businessoptics'],
+      install_requires=[
+          'requests>=2,<3',
+          'future<1',
+      ],
+      include_package_data=True,
+      zip_safe=False)
