2890d717432a83dbb238e4fe85e3487e899641f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:54 +0000

    Add package py-splash/0.4.5/py_splash-0.4.5.tar.gz

---
+++ packages/py-splash/py_splash-0.4.5.tar.gz/py_splash-0.4.5/py_splash/exceptions.py
@@ -0,0 +1,26 @@
+class SplashError(Exception):
+    pass
+
+
+class SplashTimeoutError(SplashError):
+    pass
+
+
+class SplashRenderError(SplashError):
+    pass
+
+
+class SplashInternalError(SplashError):
+    pass
+
+
+class SplashUnsupportedContentTypeError(SplashError):
+    pass
+
+
+class SplashBadRequestError(SplashError):
+    pass
+
+
+class SplashSyntaxError(SplashError):
+    pass
