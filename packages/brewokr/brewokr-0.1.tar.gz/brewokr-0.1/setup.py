bb9c7ef6bc36c009d1018c162b2017d3a5807efb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:45 +0000

    Add package brewokr/0.1/brewokr-0.1.tar.gz

---
+++ packages/brewokr/brewokr-0.1.tar.gz/brewokr-0.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'brewokr',
+    packages = ['brewokr'], # this must be the same as the name above
+    version = '0.1',
+    description = 'A simple library for Brewokr',
+    author = 'Bre',
+    author_email = 'brewokr@gmail.com',
+    url = 'https://github.com/brewokr/pybrewokr', # use the URL to the github repo
+    download_url = 'https://github.com/brewokr/pybrewokr/archive/0.1.tar.gz', # I'll explain this in a second
+    keywords = ['simple', 'brewokr', 'library'], # arbitrary keywords
+    classifiers = [],
+)
