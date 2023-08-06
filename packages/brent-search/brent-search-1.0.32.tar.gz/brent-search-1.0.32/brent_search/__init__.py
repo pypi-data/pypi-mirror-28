7a7ce3cf2d829a8e74d47457c209aec2763e242b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:03 +0000

    Add package brent-search/1.0.32/brent-search-1.0.32.tar.gz

---
+++ packages/brent-search/brent-search-1.0.32.tar.gz/brent-search-1.0.32/brent_search/__init__.py
@@ -0,0 +1,8 @@
+from .bracket import bracket
+from .brent import brent
+from .optimize import minimize
+from .testit import test
+
+__version__ = "1.0.32"
+
+__all__ = ["__version__", "test", "bracket", "brent", "minimize"]
