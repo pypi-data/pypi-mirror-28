b6a0e9c1067ec0c89b7c63acb67080524bcc484a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:29 +0000

    Add package pydc/0.1.1/pydc-0.1.1.tar.gz

---
+++ packages/pydc/pydc-0.1.1.tar.gz/pydc-0.1.1/setup.py
@@ -0,0 +1,22 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+VERSION="0.1.1"
+
+config = {
+    'description': 'pydc',
+    'author': 'Sean Mosely',
+    'author_email': 'sean.mosely@gmail.com',
+    'version': VERSION,
+    'install_requires': ['lxml>=3.6.4',],
+    'packages': ['pydc'],
+    'scripts': [],
+    'name': 'pydc',
+    'description': 'Python library for generating Dublin Core XML',
+    'download_url': 'https://github.com/NLNZDigitalPreservation/pydc/archive/v'+VERSION+'.tar.gz',
+    'license': 'MIT',
+}
+
+setup(**config)
