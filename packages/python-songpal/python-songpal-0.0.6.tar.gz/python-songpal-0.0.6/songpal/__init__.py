64c0feb37c98347db95bbb7122fb7c1e46c60588
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:22 +0000

    Add package python-songpal/0.0.6/python-songpal-0.0.6.tar.gz

---
+++ packages/python-songpal/python-songpal-0.0.6.tar.gz/python-songpal-0.0.6/songpal/__init__.py
@@ -0,0 +1,6 @@
+# flake8: noqa
+from songpal.protocol import Protocol
+from songpal.notification import Notification
+from songpal.method import Method
+from songpal.service import Service
+from songpal.common import SongpalException
