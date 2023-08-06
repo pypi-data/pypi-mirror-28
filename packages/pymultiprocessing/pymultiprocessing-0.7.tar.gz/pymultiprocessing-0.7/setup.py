a0b0215aea3f9b7a9fa573e330915f6581986242
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:01 +0000

    Add package pymultiprocessing/0.7/pymultiprocessing-0.7.tar.gz

---
+++ packages/pymultiprocessing/pymultiprocessing-0.7.tar.gz/pymultiprocessing-0.7/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+setup(
+  name = 'pymultiprocessing',
+  packages = ['pymultiprocessing','pymultiprocessing/dummy'], # this must be the same as the name above
+  version = '0.7',
+  description = 'multiprocessing package of python 2.7 ',
+  author = 'Sandeep Rawat',
+  author_email = 'san55577@gmail.com',
+  url = 'https://github.com/sandeep-rawat-xebia/pymultiprocessing.git', # use the URL to the github repo
+  download_url = 'https://github.com/sandeep-rawat-xebia/pymultiprocessing/releases/download/0.3/pymultiprocessing-0.3.tar.gz', # I'll explain this in a second
+  keywords = ['multiprocessing', 'python', 'jython'], # arbitrary keywords
+  classifiers = [],
+)
