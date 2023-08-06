09bf362b90c2a75fd03258034f7b62c297eda5ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:35 +0000

    Add package jolokia/0.2.0/jolokia-0.2.0-py2.py3-none-any.whl

---
+++ packages/jolokia/jolokia-0.2.0-py2.py3-none-any.whl/jolokia/models.py
@@ -0,0 +1,30 @@
+"""This module provides various models related to a Jolokia session"""
+
+import logging
+
+from requests import Session, Response, Request
+
+LOGGER = logging.getLogger(__name__)
+
+
+class JolokiaResponse(Response):
+    """Wraps requests.Response"""
+    pass
+
+
+class JolokiaRequest(Request):
+    """Wraps requests.Request"""
+    pass
+
+
+class JolokiaSession(Session):
+    """Wraps requests.Session"""
+
+    def simple_post(self, url, data=None):
+        """Posts to url and returns de-serialized response"""
+        try:
+            resp = self.post(url, json=data)
+            LOGGER.debug(resp)
+            return resp
+        except Exception as error:
+            raise error
