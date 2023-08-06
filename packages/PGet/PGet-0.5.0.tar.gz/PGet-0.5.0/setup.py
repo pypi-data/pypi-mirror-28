b1b2a85db4399a1115195b53e9a6a2c294ecf71f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:24 +0000

    Add package PGet/0.5.0/PGet-0.5.0.tar.gz

---
+++ packages/PGet/PGet-0.5.0.tar.gz/PGet-0.5.0/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='PGet',
+    version='0.5.0',
+    description='A tool and library to save large files by creating multiple connections.',
+    author='Halil Ozercan',
+    author_email='halilozercan@gmail.com',
+    url='https://github.com/halilozercan/pget',
+    download_url='https://github.com/halilozercan/pget/tarball/0.5.0',
+    entry_points={
+        'console_scripts': [
+            'pget = pget.bin:main',
+        ],
+    },
+    install_requires=['future', 'requests', 'wheel'],
+    packages=find_packages(exclude=("tests", "tests.*")),
+)
