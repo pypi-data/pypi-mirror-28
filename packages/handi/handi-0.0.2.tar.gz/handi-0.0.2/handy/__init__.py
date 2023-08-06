33d24e01d1412e771898ed5705cdacfe6d06a59d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:58 +0000

    Add package handi/0.0.2/handi-0.0.2.tar.gz

---
+++ packages/handi/handi-0.0.2.tar.gz/handi-0.0.2/handy/__init__.py
@@ -0,0 +1,10 @@
+__version__ = '0.0.2'
+
+from . import compress
+from . import download
+from . import list2txt
+from . import portscan
+from . import push
+from . import upload
+from . import misc
+
