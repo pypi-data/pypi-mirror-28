ccdf2e79ca18bb9dfae97ea8b913932282585d20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:37 +0000

    Add package GCR/0.6.3/GCR-0.6.3.tar.gz

---
+++ packages/GCR/GCR-0.6.3.tar.gz/GCR-0.6.3/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+"""
+Generic Catalog Reader (GCR)
+A common reader interface for accessing generic catalogs
+https://github.com/yymao/generic-catalog-reader
+The MIT License (MIT)
+Copyright (c) 2017-2018 Yao-Yuan Mao (yymao)
+http://opensource.org/licenses/MIT
+"""
+
+from setuptools import setup
+
+setup(
+    name='GCR',
+    version='0.6.3',
+    description='Generic Catalog Reader: A common reader interface for accessing generic catalogs',
+    url='https://github.com/yymao/generic-catalog-reader',
+    download_url='https://github.com/yymao/generic-catalog-reader/archive/v0.6.3.zip',
+    author='Yao-Yuan Mao',
+    author_email='yymao.astro@gmail.com',
+    maintainer='Yao-Yuan Mao',
+    maintainer_email='yymao.astro@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords='GCR catalog reader',
+    py_modules=['GCR'],
+    install_requires=['numpy', 'easyquery>=0.1.3'],
+)
