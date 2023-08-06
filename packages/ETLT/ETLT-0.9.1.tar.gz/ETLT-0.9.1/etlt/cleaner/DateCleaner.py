7be7ef478c9a0b6a4d91051c4d8e1934c4ef8e64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:15 +0000

    Add package ETLT/0.9.1/ETLT-0.9.1.tar.gz

---
+++ packages/ETLT/ETLT-0.9.1.tar.gz/ETLT-0.9.1/etlt/cleaner/DateCleaner.py
@@ -0,0 +1,56 @@
+"""
+ETLT
+
+Copyright 2016 Set Based IT Consultancy
+
+Licence MIT
+"""
+import re
+
+
+class DateCleaner:
+    """
+    Utility class for converting dates in miscellaneous formats to ISO-8601 (YYYY-MM-DD) format.
+    """
+
+    # ------------------------------------------------------------------------------------------------------------------
+    @staticmethod
+    def clean(date, ignore_time=False):
+        """
+        Converts a date in miscellaneous format to ISO-8601 (YYYY-MM-DD) format.
+
+        :param str date: The input date.
+        :rtype: str
+        """
+        # Return empty input immediately.
+        if not date:
+            return date
+
+        parts = re.split(r'[\-/. ]', date)
+
+        if (len(parts) == 3) or \
+           (len(parts) > 3 and ignore_time) or \
+           (len(parts) == 4 and re.match(r'^[0:]*$', parts[3])) or \
+           (len(parts) == 5 and re.match(r'^[0:]*$', parts[3]) and re.match(r'^0*$', parts[4])):
+            if len(parts[0]) == 4 and len(parts[1]) <= 2 and len(parts[2]) <= 2:
+                # Assume date is in  YYYY-MM-DD of YYYY-M-D format.
+                return parts[0] + '-' + ('00' + parts[1])[-2:] + '-' + ('00' + parts[2])[-2:]
+
+            if len(parts[0]) <= 2 and len(parts[1]) <= 2 and len(parts[2]) == 4:
+                # Assume date is in  DD-MM-YYYY or D-M-YYYY format.
+                return parts[2] + '-' + ('00' + parts[1])[-2:] + '-' + ('00' + parts[0])[-2:]
+
+            if len(parts[0]) <= 2 and len(parts[1]) <= 2 and len(parts[2]) == 2:
+                # Assume date is in  DD-MM-YY or D-M-YY format.
+                year = '19' + parts[2] if parts[2] >= '20' else '20' + parts[2]
+
+                return year + '-' + ('00' + parts[1])[-2:] + '-' + ('00' + parts[0])[-2:]
+
+        if len(parts) == 1 and len(date) == 8:
+            # Assume date is in YYYYMMDD format.
+            return date[0:4] + '-' + date[4:6] + '-' + date[6:8]
+
+        # Format not recognized. Just return the original string.
+        return date
+
+# ----------------------------------------------------------------------------------------------------------------------
