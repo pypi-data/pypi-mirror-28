ba11281fd4edfb64d9f1b67c1a43e300931ebb96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:22 +0000

    Add package apistar-pydantic/0.0.3/apistar-pydantic-0.0.3.tar.gz

---
+++ packages/apistar-pydantic/apistar-pydantic-0.0.3.tar.gz/apistar-pydantic-0.0.3/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+setup(
+    name='apistar-pydantic',
+    version='0.0.3',
+    description="pydantic support for APIStar",
+    long_description=readme,
+    author="Pedro Sousa Lacerda",
+    author_email='pslacerda@gmail.com',
+    url='https://github.com/pslacerda/apistar_pydantic',
+    py_modules=['apistar_pydantic'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'apistar',
+        'pydantic',
+        'coreapi',
+        'coreschema',
+        'uritemplate'
+    ],
+    license="MIT license",
+    keywords='apistar_pydantic',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+    ],
+    test_suite='tests',
+    tests_require=[
+        'pytest'
+    ],
+)
