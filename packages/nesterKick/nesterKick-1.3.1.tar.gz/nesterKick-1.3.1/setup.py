1864506f83f1b3af91ba08935e8d6a44e2c97bfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:38 +0000

    Add package nesterKick/1.3.1/nesterKick-1.3.1.tar.gz

---
+++ packages/nesterKick/nesterKick-1.3.1.tar.gz/nesterKick-1.3.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup 
+
+setup (
+
+name = 'nesterKick',
+version = '1.3.1',
+py_modules = ['nesterKick'],
+author = 'hfpython',
+author_email = 'hfpython@headfirstlabs.com',
+url = 'http://www.headfirstlabs.com',
+description = 'A simple printer of nested lists',
+
+)
