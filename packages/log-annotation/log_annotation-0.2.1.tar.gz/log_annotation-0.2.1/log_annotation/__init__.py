a7068014129e58f06f40580a353fee061af1789f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:20:05 +0000

    Add package log-annotation/0.2.1/log_annotation-0.2.1.tar.gz

---
+++ packages/log-annotation/log_annotation-0.2.1.tar.gz/log_annotation-0.2.1/log_annotation/__init__.py
@@ -0,0 +1,53 @@
+import logging
+import graypy
+import requests
+import time
+import sys
+import traceback
+from r7insight import R7InsightHandler
+
+def log(name='', level=logging.INFO, keys=[]):
+    def decorator(function):
+        def wrapper(param):
+            log = dict()
+            error = dict()
+            start_time = time.time()
+            try:
+                # Init logger
+                graylog = {'host': param.get('loghub',None),'port': param.get('logport',None)}
+                r7insight_token = param.get('le_token',None)
+                logger = logging.getLogger(name)
+                logger.setLevel(level)
+                # Log Init
+                handler = logging.StreamHandler()
+                if (graylog.get("host",None) != None and graylog.get("port",None) != None):
+                    handler = graypy.GELFHandler(graylog["host"], graylog["port"])
+                elif r7insight_token != None:
+                    handler = R7InsightHandler(r7insight_token, "us")
+
+                logger.addHandler(handler)
+                res = function(param)
+                log = {"function":name,"result":res}
+                return res
+            except requests.exceptions.HTTPError as err:
+                error = {"error": {"description": str(err), "response": err.response.json()}}
+            except Exception as err:
+                error = {"error": {"description": str(err)}}
+            finally:
+                log = {"function":name, "elapsed_time": str(round(time.time() - start_time,2))}
+                extra = {}
+                for key in keys:
+                    if param.get(key,None) != None:
+                        extra[key] = param[key]
+
+                if not error:
+                    logger.info(log,extra=extra)
+                else:
+                    stack = traceback.format_stack()
+                    error['stack_trace'] = stack
+                    error['args'] = param
+                    log.update(error)
+                    logger.error(log,extra=extra)
+                    return error
+        return wrapper
+    return decorator
