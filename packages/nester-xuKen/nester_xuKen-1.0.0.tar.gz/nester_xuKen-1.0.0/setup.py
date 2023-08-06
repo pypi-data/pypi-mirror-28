2b838b9a1cf3483866b17f623e6d3678131ad4c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:53 +0000

    Add package nester-xuKen/1.0.0/nester_xuKen-1.0.0.tar.gz

---
+++ packages/nester-xuKen/nester_xuKen-1.0.0.tar.gz/nester_xuKen-1.0.0/setup.py
@@ -0,0 +1,14 @@
+# !/usr/bin/python
+# Filename: setup.py
+
+from  distutils.core import setup
+
+setup(
+    name = 'nester_xuKen',
+    version = '1.0.0',
+    py_modules = ['nester'],
+    author = 'xuzhaojie',
+    author_email= '18756935259@163.com',
+    url = 'http://www.headfirstlabs.com',
+    description = 'A simple printer of nested lists',
+)
