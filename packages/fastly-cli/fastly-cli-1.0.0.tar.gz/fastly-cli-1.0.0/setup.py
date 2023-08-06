e6b8035fa46012f95256728452bbdf2f340c2a22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:59 +0000

    Add package fastly-cli/1.0.0/fastly-cli-1.0.0.tar.gz

---
+++ packages/fastly-cli/fastly-cli-1.0.0.tar.gz/fastly-cli-1.0.0/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+try:
+    long_description = open("README.md").read()
+except IOError:
+    long_description = ""
+
+setup(
+    name="fastly-cli",
+    version="1.0.0",
+    description="Universal command line interface for Fastly CDN",
+    license="MIT",
+    url="https://github.com/mertsaygi/fastly-cli",
+    author="Mert Saygı",
+    author_email="mertsaygi@gmail.com",
+    packages=find_packages(),
+    install_requires=[
+        'Click',
+        'requests',
+        'fastly',
+    ],
+    long_description=long_description,
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+    ],
+    entry_points = {
+        'console_scripts': [
+            'fastly-cli = fastlycli.__main__:entry_point'
+        ]
+    }
+)
