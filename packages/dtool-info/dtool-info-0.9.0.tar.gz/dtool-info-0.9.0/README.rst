10aaf8a5e6caa8d135f522ce5bf9b4182b414a0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:35 +0000

    Add package dtool-info/0.9.0/dtool-info-0.9.0.tar.gz

---
+++ packages/dtool-info/dtool-info-0.9.0.tar.gz/dtool-info-0.9.0/README.rst
@@ -0,0 +1,28 @@
+README
+======
+
+.. image:: https://badge.fury.io/py/dtool-info.svg
+   :target: http://badge.fury.io/py/dtool-info
+   :alt: PyPi package
+
+Dtool plugin for getting information about datasets
+
+Installation
+------------
+
+.. code-block:: bash
+
+    pip install dtool-info
+
+Example usage
+-------------
+
+Listing datasets in ``~/my_dataset`` directory::
+
+    dtool ls ~/my_datasets
+
+Getting summary information about a dataset::
+
+    dtool summary ~/my_datasets/example_dataset
+
+See the `dtool documentation <http://dtool.readthedocs.io>`_ for more detail.
---
+++ packages/dtool-info/dtool-info-0.9.0.tar.gz/dtool-info-0.9.0/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+
+url = "https://github.com/jic-dtool/dtool-info"
+version = "0.9.0"
+readme = open('README.rst').read()
+
+setup(
+    name="dtool-info",
+    packages=["dtool_info"],
+    version=version,
+    description="Dtool plugin for getting information about datasets",
+    long_description=readme,
+    include_package_data=True,
+    author="Tjelvar Olsson",
+    author_email="tjelvar.olsson@jic.ac.uk",
+    url=url,
+    install_requires=[
+        "click",
+        "dtoolcore>=3.0.0",
+        "dtool_cli>=0.6.0",
+        "pygments",
+    ],
+    entry_points={
+        "dtool.cli": [
+            "diff=dtool_info.dataset:diff",
+            "ls=dtool_info.dataset:ls",
+            "summary=dtool_info.dataset:summary",
+            "item=dtool_info.dataset:item",
+            "identifiers=dtool_info.dataset:identifiers",
+            "verify=dtool_info.dataset:verify",
+            "overlay=dtool_info.overlay:overlay",
+        ],
+    },
+    download_url="{}/tarball/{}".format(url, version),
+    license="MIT"
+)
