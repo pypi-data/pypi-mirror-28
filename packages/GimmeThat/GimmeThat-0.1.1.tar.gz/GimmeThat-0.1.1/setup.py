9fc711f4e136f42c941dda921ada62b82d329cce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:31 +0000

    Add package GimmeThat/0.1.1/GimmeThat-0.1.1.tar.gz

---
+++ packages/GimmeThat/GimmeThat-0.1.1.tar.gz/GimmeThat-0.1.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='GimmeThat',
+    version='0.1.1',
+    description='A file transfer tool',
+    url='https://github.com/nejdetckenobi/gimme-that',
+    author='nejdetckenobi',
+    author_email='nejdetyucesoy@gmail.com',
+    license='GPL',
+    packages=['gimmethat'],
+    zip_safe=False,
+    install_requires=[
+        'flask',
+        'flask_bootstrap',
+        'netifaces'
+    ],
+    scripts=[
+        'bin/gimme',
+    ],
+    include_package_data=True
+)
