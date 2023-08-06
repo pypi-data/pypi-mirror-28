ccbc27f350bb7ba82edc867964c968fd5b72bed2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:00 +0000

    Add package h2printer/1.0.1/h2printer-1.0.1.tar.gz

---
+++ packages/h2printer/h2printer-1.0.1.tar.gz/h2printer-1.0.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'h2printer',
+    version = '1.0.1',
+    py_modules = ['h2printer'],
+    author = 'herlandro',
+    author_email = 'herlahdro@hotmail.com',
+    url = 'http://www.hermogenes.com.br',
+    description = 'A simple printer of nested lists',
+)
