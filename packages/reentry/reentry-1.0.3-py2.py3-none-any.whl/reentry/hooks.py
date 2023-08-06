4ce7253102742be6f2eb1520efefc60b4c459a04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:10 +0000

    Add package reentry/1.0.3/reentry-1.0.3-py2.py3-none-any.whl

---
+++ packages/reentry/reentry-1.0.3-py2.py3-none-any.whl/reentry/hooks.py
@@ -0,0 +1,48 @@
+# -*- coding: utf8 -*-
+"""
+define a setuptools extension
+"""
+from __future__ import print_function
+
+
+def is_bool(value):
+    """return True if `value` is a boolean"""
+    return bool(value) == value
+
+
+def register_dist(dist, attr, value):
+    """
+    if value is True, register the distribution's entry points in
+    reentrys storage
+    """
+    from distutils.errors import DistutilsSetupError  # pylint: disable=import-error,no-name-in-module
+    # assert is boolean
+    if not is_bool(value):
+        raise DistutilsSetupError('{} must be a boolean, got {}'.format(attr, value))
+
+    if value:
+        print('registering entry points with reentry...')
+        from reentry.jsonbackend import JsonBackend
+        backend = JsonBackend()
+        backend.write_st_dist(dist)
+
+
+def ensure_list(value, attr):
+    """raise an error if `value` is not a list"""
+    from distutils.errors import DistutilsSetupError  # pylint: disable=import-error,no-name-in-module
+    if not isinstance(value, list):
+        raise DistutilsSetupError('{} must be a list, got {}'.format(attr, value.__class__))
+
+
+def scan_for_installed(attr, value):
+    """
+    scan for entry points of the given groups in the already installed
+    distributions
+
+    :param value: a list of groups names to scan for
+    """
+    ensure_list(value, attr)
+    if value:
+        print('scanning for plugins...')
+        from reentry.manager import scan
+        scan(value, group_re=False)
---
+++ packages/reentry/reentry-1.0.3-py2.py3-none-any.whl/reentry/manager.py
@@ -0,0 +1,84 @@
+# -*- coding: utf8 -*-
+"""Manager for entry point based plugins. Main client facing API"""
+import six
+
+from reentry.jsonbackend import JsonBackend
+
+DEFAULT_BACKEND = JsonBackend()
+
+
+class PluginManager(object):
+    """Manage a set of cached entry points"""
+
+    def __init__(self, backend=DEFAULT_BACKEND):
+        self._backend = backend
+
+    def iter_entry_points(self, group, name=None):
+        """
+        returns all registered entry points in `group`, or if `name` is given,
+        only the ones matching `name`.
+
+        The backend may only load pkg_resources if any of the entry points contain extras requirements
+        """
+        for entry_point in self._backend.iter_group(group):
+            if name:
+                if name in entry_point.name:
+                    yield entry_point
+            else:
+                yield entry_point
+
+    def get_entry_map(self, dist_names=None, groups=None, ep_names=None):
+        """
+        return the entry point map for `group` or the whole map for `dist`
+
+        The backend may only load pkg_resources if any of the entry points contain extras requirements
+        """
+        return self._backend.get_map(dist=dist_names, group=groups, name=ep_names)
+
+    def register(self, distname):
+        """
+        registers the distribution's entry points with the backend
+
+        The backend may load pkg_resources to resolve the distribution name
+        """
+        self._backend.write_dist(distname)
+
+    def scan(self, groups=None, group_re=None):
+        """
+        walks through all distributions available and registers entry points or only those in `groups`
+        """
+        import pkg_resources as pr
+        pr_env = pr.AvailableDistributions()
+        pr_env.scan()
+        if groups:
+            for group in groups:
+                self._backend.rm_group(group)
+        else:
+            self._backend.clear()
+
+        for dists in pr_env._distmap.values():  # pylint: disable=protected-access
+            dist = dists[0]
+            emap = dist.get_entry_map()
+            if groups:
+                dmap = {k: v for k, v in six.iteritems(emap) if k in groups}
+            elif group_re:
+                dmap = {k: v for k, v in six.iteritems(emap) if group_re.match(k)}
+            else:
+                dmap = None
+            dname = dist.project_name
+            self._backend.write_dist(dname, entry_point_map=dmap)
+
+    def unregister(self, distname):
+        """
+        unregisters the distribution's entry points with the backend
+
+        The backend may load pkg_resources to resolve the distribution name
+        """
+        self._backend.rm_dist(distname)
+
+    @property
+    def distribution_names(self):
+        return self._backend.get_dist_names()
+
+
+DEFAULT_MANAGER = PluginManager()
