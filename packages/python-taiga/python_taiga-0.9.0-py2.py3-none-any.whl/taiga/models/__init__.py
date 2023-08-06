aa6281c9ef8baadb337a4eea5aab4cf3584a7fbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:37 +0000

    Add package python-taiga/0.9.0/python_taiga-0.9.0-py2.py3-none-any.whl

---
+++ packages/python-taiga/python_taiga-0.9.0-py2.py3-none-any.whl/taiga/models/__init__.py
@@ -0,0 +1,64 @@
+from .models import User
+from .models import Users
+from .models import Membership
+from .models import Memberships
+from .models import Project
+from .models import Projects
+from .models import UserStory
+from .models import UserStories
+from .models import UserStoryAttachment
+from .models import UserStoryAttachments
+from .models import Task
+from .models import Tasks
+from .models import TaskAttachment
+from .models import TaskAttachments
+from .models import Issue
+from .models import Issues
+from .models import IssueAttachment
+from .models import IssueAttachments
+from .models import Milestone
+from .models import Milestones
+from .models import Point
+from .models import Points
+from .models import UserStoryStatus
+from .models import UserStoryStatuses
+from .models import Severity
+from .models import Severities
+from .models import Role
+from .models import Roles
+from .models import Priority
+from .models import Priorities
+from .models import IssueStatus
+from .models import IssueStatuses
+from .models import IssueType
+from .models import IssueTypes
+from .models import IssueAttribute
+from .models import IssueAttributes
+from .models import TaskAttribute
+from .models import TaskAttributes
+from .models import UserStoryAttribute
+from .models import UserStoryAttributes
+from .models import TaskStatus
+from .models import TaskStatuses
+from .models import WikiPage
+from .models import WikiPages
+from .models import WikiLink
+from .models import WikiLinks
+from .models import History
+from .models import Webhook
+from .models import Webhooks
+
+
+__all__ = [
+    'User', 'Users', 'Project', 'Projects', 'UserStory', 'UserStories',
+    'UserStoryAttachment', 'UserStoryAttachments', 'Task', 'Tasks',
+    'TaskAttachment', 'TaskAttachments', 'Issue', 'Issues', 'IssueAttachment',
+    'IssueAttachments', 'Milestone', 'Milestones', 'Point', 'Points',
+    'UserStoryStatus', 'UserStoryStatuses', 'Severity', 'Severities',
+    'Priority', 'Priorities', 'IssueStatus', 'IssueStatuses', 'TaskStatus',
+    'TaskStatuses', 'WikiPage', 'WikiPages', 'WikiLink', 'WikiLinks',
+    'History', 'IssueAttribute', 'IssueAttributes', 'TaskAttribute',
+    'TaskAttributes', 'UserStoryAttribute', 'UserStoryAttributes',
+    'Membership', 'Memberships', 'Role', 'Roles', 'IssueType', 'IssueTypes',
+    'Webhook', 'Webhooks'
+]
