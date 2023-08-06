6a9f713bdaa7d93d251e997e2ac2427bec1b8d48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:19:36 +0000

    Add package djangofloor/1.1.8/djangofloor-1.1.8.tar.gz

---
+++ packages/djangofloor/djangofloor-1.1.8.tar.gz/djangofloor-1.1.8/djangofloor/management/commands/server.py
@@ -0,0 +1,17 @@
+import sys
+
+from djangofloor.management.base import BaseCommand
+from djangofloor.scripts import gunicorn
+
+
+class Command(BaseCommand):
+    """placeholder for the "server" command.
+    This command is directly handled if the "control" script is setup.
+
+    """
+    help = "Launch the server process"
+
+    def handle(self, *args, **options):
+        while len(sys.argv) > 1:
+            del sys.argv[1]
+        return gunicorn()
