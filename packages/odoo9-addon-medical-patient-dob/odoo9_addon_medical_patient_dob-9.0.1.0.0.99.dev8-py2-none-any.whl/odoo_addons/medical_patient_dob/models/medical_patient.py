d5f348308e55e11fa027601f166e7f44b2354718
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:04:06 +0000

    Add package odoo9-addon-medical-patient-dob/9.0.1.0.0.99.dev8/odoo9_addon_medical_patient_dob-9.0.1.0.0.99.dev8-py2-none-any.whl

---
+++ packages/odoo9-addon-medical-patient-dob/odoo9_addon_medical_patient_dob-9.0.1.0.0.99.dev8-py2-none-any.whl/odoo_addons/medical_patient_dob/models/medical_patient.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 LasLabs Inc.
+# License GPL-3.0 or later (http://www.gnu.org/licenses/gpl.html).
+
+from openerp import api, fields, models
+
+
+class MedicalPatient(models.Model):
+    _inherit = 'medical.patient'
+
+    @api.multi
+    def _format_dob(self):
+        self.ensure_one()
+        date = 'No DoB'
+        if self.dob:
+            ResLang = self.env['res.lang']
+            lang_fmt = ResLang.search([('code', '=', self.lang)]).date_format
+            if not lang_fmt:
+                lang_fmt = '%m/%d/%Y'
+            date = fields.Datetime.from_string(self.dob).strftime(
+                lang_fmt
+            )
+        return ' [%s]' % date
+
+    @api.multi
+    def name_get(self):
+        res = []
+        for rec_id in self:
+            name = '%s%s' % (rec_id.name, rec_id._format_dob())
+            res.append((rec_id.id, name))
+        return res
---
+++ packages/odoo9-addon-medical-patient-dob/odoo9_addon_medical_patient_dob-9.0.1.0.0.99.dev8-py2-none-any.whl/odoo_addons/medical_patient_dob/tests/test_medical_patient.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 LasLabs Inc.
+# License GPL-3.0 or later (http://www.gnu.org/licenses/gpl.html).
+
+from openerp.tests.common import TransactionCase
+
+
+class TestMedicalPatient(TransactionCase):
+
+    def setUp(self):
+        super(TestMedicalPatient, self).setUp()
+        self.patient_1 = self.env.ref(
+            'medical.medical_patient_patient_1'
+        )
+        self.lang = 'en_US'
+        self.patient_1.lang = self.lang
+        self.lang_obj = self.env['res.lang'].search([('code', '=', self.lang)])
+        self.lang_obj.date_format = '%m/%d/%Y'
+
+    def test_change_date_format(self):
+        """ Test date format changed in display_name if adjusted """
+        self.lang_obj.date_format = '%Y-%m-%d'
+        self.assertEquals(
+            self.patient_1.display_name, 'Emma Fields [1920-02-23]',
+            'Should correctly adjust date format.\rGot: %s\rExpected: %s' % (
+                self.patient_1.display_name, 'Emma Fields [1920-02-23]'
+            )
+        )
+
+    def test_name_includes_dob(self):
+        """ Test display name includes dob if present """
+        self.assertEquals(
+            self.patient_1.display_name, 'Emma Fields [02/23/1920]',
+            'Should include dob in display name.\rGot: %s\rExpected: %s' % (
+                self.patient_1.display_name, 'Emma Fields [02/23/1920]'
+            )
+        )
+
+    def test_name_without_dob(self):
+        """ Test display name includes [No DoB] if no dob present """
+        self.patient_1.dob = None
+        self.assertEquals(
+            self.patient_1.display_name, 'Emma Fields [No DoB]',
+            'Should include [No DoB].\rGot: %s\rExpected: %s' % (
+                self.patient_1.display_name, 'Emma Fields [No DoB]'
+            )
+        )
+
+    def test_name_no_lang(self):
+        """ Test for a default date format if none existing on lang """
+        self.patient_1.lang = None
+        self.assertEquals(
+            self.patient_1.display_name, 'Emma Fields [02/23/1920]',
+            'Should include dob in display name.\rGot: %s\rExpected: %s' % (
+                self.patient_1.display_name, 'Emma Fields [02/23/1920]'
+            )
+        )
