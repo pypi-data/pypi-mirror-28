919aaa4adf610c7366f6f79913cef467cfa050c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:29 +0000

    Add package py_space/0.5/py_space-0.5.tar.gz

---
+++ packages/py_space/py_space-0.5.tar.gz/py_space-0.5/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='py_space',
+    packages=['py_space'],
+    version='0.5',
+    description='python space',
+    author='hellspacebar',
+    author_email='hellspacebar@gmail.com',
+    url='https://github.com/hellspacebar/py_space',
+    download_url='https://github.com/hellspacebar/py_space/archive/0.1.tar.gz',
+    keywords=['space'],
+    classifiers=['Development Status :: 3 - Alpha','Programming Language :: Python :: 3.4'],
+    license='MIT',
+)
