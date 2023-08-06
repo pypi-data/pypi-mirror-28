b3e3527b342392874211fc7015950bbcea47bd15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:40 +0000

    Add package nester-allmils/1.3.0/nester_allmils-1.3.0.tar.gz

---
+++ packages/nester-allmils/nester_allmils-1.3.0.tar.gz/nester_allmils-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = "nester_allmils",
+    version = "1.3.0",
+    py_modules = ["nester_allmils"],
+    author = "allmils",
+    author_email = "me.losingit@yahoo.com",
+    url = "",
+    description = "A simple printer of unwinded nested lists, with option to indent each sublist",
+    )
