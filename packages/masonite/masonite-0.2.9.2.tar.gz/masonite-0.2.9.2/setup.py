758f52bfbb1504fdf5ac46a9ac8e3621490ca0aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:34 +0000

    Add package masonite/0.2.9.2/masonite-0.2.9.2.tar.gz

---
+++ packages/masonite/masonite-0.2.9.2.tar.gz/masonite-0.2.9.2/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+setup(
+    name='masonite',
+    packages=['masonite',
+              'masonite.auth',
+              'masonite.cashier',
+              'masonite.extensions',
+              'masonite.facades',
+             ],  # this must be the same as the name above
+    version='0.2.9.2',
+    install_requires=[
+        'validator.py'
+    ],
+    description='The core for the python framework',
+    author='Joseph Mancuso',
+    author_email='idmann509@gmail.com',
+    url='https://github.com/josephmancuso/masonite',  # use the URL to the github repo
+    download_url='',
+    keywords=['python web framework', 'python3'],  # arbitrary keywords
+    classifiers=[],
+)
