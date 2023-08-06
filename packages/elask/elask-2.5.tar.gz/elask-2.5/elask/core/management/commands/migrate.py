eb5fdbad56892e70ce3c0548f47273c547351976
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:17 +0000

    Add package elask/2.5/elask-2.5.tar.gz

---
+++ packages/elask/elask-2.5.tar.gz/elask-2.5/elask/core/management/commands/migrate.py
@@ -0,0 +1,7 @@
+from elask.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        raise NotImplementedError("Not yet implemented")
