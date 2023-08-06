26441735b79f2ee204ff15256ad9684d7b173c4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:47 +0000

    Add package xy-speech/11.0.0/xy_speech-11.0.0.tar.gz

---
+++ packages/xy-speech/xy_speech-11.0.0.tar.gz/xy_speech-11.0.0/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+setup(
+  name = 'xy_speech',
+  packages = ['xy_speech'],
+  version = '11.0.0',
+  description = 'Speech Recognition',
+  long_description='Speech Recognition,voice2text,text2voice',
+  author = 'KingHS',
+  author_email = '382771946@qq.com',
+  keywords = ['pip3', 'speech', 'python3','python','Speech Recognition'],
+  license='MIT',
+  install_requires=[
+        'pyaudio',
+        'wave',
+        'xy_pinyin',
+        'requests'
+    ],
+)
