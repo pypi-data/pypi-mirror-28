d9917a480820b79a27c92283f1ba4fbf5435e430
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:24 +0000

    Add package pymemcache-client/0.3.1/pymemcache_client-0.3.1-py3-none-any.whl

---
+++ packages/pymemcache-client/pymemcache_client-0.3.1-py3-none-any.whl/pymemcache_client/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#  @copyright 2018 TUNE, Inc. (http://www.tune.com)
+#  @namespace pymemcache-client
+
+__title__ = 'pymemcache-client'
+__version__ = '0.3.1'
+__version_info__ = tuple(__version__.split('.'))
+
+__author__ = 'jefft@tune.com'
+__license__ = 'MIT License'
+
+__python_required_version__ = (3, 0)
+
+from .pymemcache_client import PymemcacheClient
