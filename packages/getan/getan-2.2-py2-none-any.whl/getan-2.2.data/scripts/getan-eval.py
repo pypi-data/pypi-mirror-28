10b3fd1ddaf9f3e2d150fed9dbcc2e676fe65ea9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:53 +0000

    Add package getan/2.2/getan-2.2-py2-none-any.whl

---
+++ packages/getan/getan-2.2-py2-none-any.whl/getan-2.2.data/scripts/getan-eval.py
@@ -0,0 +1,69 @@
+#!python
+# -*- coding: utf-8 -*-
+#
+# (c) 2013, 2014 by Björn Ricks <bjoern.ricks@intevation.de>
+#
+# This is Free Software licensed under the terms of GPLv3 or later.
+# For details see LICENSE coming with the source of 'getan'.
+
+import codecs
+import locale
+import sys
+
+from datetime import date, datetime, timedelta
+from optparse import OptionParser
+
+from getan.template import render
+
+
+def main():
+    parser = OptionParser()
+    parser.add_option("-d", "--database", dest="database",
+                      help="getan database",  metavar="DATABASE")
+    parser.add_option("-t", "--template", dest="template", metavar="TEMPLATE",
+                      help="name of getan template")
+    parser.add_option("-u", "--user", dest="user", help="name of user")
+    parser.add_option("-p", "--project", dest="project",
+                      help="key of output project")
+    parser.add_option("-w", "--week", type="int", dest="week",
+                      help="week of year")
+    parser.add_option("-y", "--year", type="int", dest="year", help="year")
+    parser.add_option("-c", "--lastweek", dest="lastweek",
+                      help="entries of last working week",
+                      action="store_true")
+    parser.add_option("-m", "--empty", dest="empty",
+                      help="show projects without an entries",
+                      action="store_true")
+    parser.add_option("--encoding", dest="encoding",
+                      help="encoding of output", metavar="ENCODING")
+
+    (options, args) = parser.parse_args()
+
+    if options.lastweek:
+        week = (datetime.now() - timedelta(7)).isocalendar()[1]
+        year = int(date.today().strftime("%Y"))
+    else:
+        year = options.year
+        week = options.week
+
+    template_name = options.template or "wochenbericht"
+
+    if not options.encoding:
+        encoding = locale.getdefaultlocale()[1] or "utf-8"
+
+    Writer = codecs.getwriter(encoding)
+    sys.stdout = Writer(sys.stdout)
+
+    user = None
+    if options.user:
+        user = options.user.decode(encoding)
+
+    print render(database=options.database, user=user,
+                 template=template_name, year=year, week=week,
+                 project=options.project, empty_projects=options.empty)
+
+
+if __name__ == '__main__':
+    main()
+
+# vim:set ts=4 sw=4 si et sta sts=4 :
---
+++ packages/getan/getan-2.2-py2-none-any.whl/getan-2.2.data/scripts/zeitsort.py
@@ -0,0 +1,49 @@
+#!/home/bernhard/hacking/getan/getan-env/bin/python
+# -*- coding: utf-8 -*-
+#
+# zeitsort
+# --------
+# (c) 2008 by Sascha L. Teichmann <sascha.teichmann@intevation.de>
+#
+# Simple script which sorts lines of zeiterfassung.txt files by date.
+#
+# This is Free Software licensed under the terms of GPLv3 or later.
+# For details see LICENSE coming with the source of 'getan'.
+#
+
+import sys
+import re
+
+from datetime import date
+
+DATE = re.compile("(\d\d)\.(\d\d)\.(\d\d\d\d)")
+
+def date_cmp(a, b):
+    ma = DATE.search(a)
+    mb = DATE.search(b)
+    if not ma and not mb: return cmp(a, b)
+    if ma and not mb: return -1
+    if not ma and mb: return +1
+    da = date(int(ma.group(3)), int(ma.group(2)), int(ma.group(1)))
+    db = date(int(mb.group(3)), int(mb.group(2)), int(mb.group(1)))
+    return cmp(da, db)
+
+def main():
+    all = []
+    while True:
+        line = sys.stdin.readline()
+        if not line: break
+        if not DATE.search(line):
+            # handle multi lines
+            if not all: all.append(line)
+            else:       all[-1] += line
+        else:
+            all.append(line)
+    all.sort(date_cmp)
+    sys.stdout.write(''.join(all))
+    sys.stdout.flush()
+
+if __name__ == '__main__':
+    main()
+
+# vim:set ts=4 sw=4 si et sta sts=4 fenc=utf8:
