9ad66c5d2366ab164402f7ad7dc88a130086fba8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:27 +0000

    Add package pyfancy/2.4.4/pyfancy-2.4.4.tar.gz

---
+++ packages/pyfancy/pyfancy-2.4.4.tar.gz/pyfancy-2.4.4/setup.py
@@ -0,0 +1,58 @@
+"""A setuptools based setup module.
+
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup
+
+# For Markdown README
+# https://stackoverflow.com/a/26737672/5951529
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except(IOError, ImportError):
+    long_description = open('README.md').read()
+
+setup(
+    # Although 'package_data' is the preferred approach, in some case you may
+    # need to place data files outside of your packages. See:
+    # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
+    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
+    name = "pyfancy",
+    description = "Simple terminal formatting in Python",
+    version = "2.4.4",
+    author = "pyfancy",
+    author_email = "arodebaugh@icloud.com",
+    url = "https://github.com/ilovecode1/Pyfancy-2",
+    license = "MIT",
+    keywords = "pyfancy ansi color colour terminal",
+    data_files=[('', ['README.md'])],
+    long_description=long_description,
+     classifiers=[  # Optional
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 4 - Beta',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        # Pick your license as you wish
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    install_requires = ["pygments", "pypandoc"]
+)
