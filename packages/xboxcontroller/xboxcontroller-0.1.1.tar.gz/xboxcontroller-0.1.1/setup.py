ca215d464d6eb7560a9ccbfbe8fad26bef7fc740
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:47 +0000

    Add package xboxcontroller/0.1.1/xboxcontroller-0.1.1.tar.gz

---
+++ packages/xboxcontroller/xboxcontroller-0.1.1.tar.gz/xboxcontroller-0.1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='xboxcontroller',
+      version='0.1.1',
+      description='Python library for using Xbox 360 controllers in linux-based systems.',
+      url='http://github.com/ricgby/xboxcontroller',
+      author='Ricgby',
+      author_email='ricardobustosglz@gmail.com',
+      license='MIT',
+      packages=['xboxcontroller'],
+      zip_safe=False)
