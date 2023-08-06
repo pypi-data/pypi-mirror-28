39c12c1480cd95f1905a054f13a5e4dbb1c70a1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:07 +0000

    Add package django-collectd-rest/0.1.11/django-collectd-rest-0.1.11.tar.gz

---
+++ packages/django-collectd-rest/django-collectd-rest-0.1.11.tar.gz/django-collectd-rest-0.1.11/tests/test_all.py
@@ -0,0 +1,5 @@
+from tests.graph import GraphTest
+from tests.granularity import GraphGranularityTest
+from tests.group import GraphGroupTest
+
+__all__ = ['GraphTest', 'GraphGranularityTest', 'GraphGroupTest']
