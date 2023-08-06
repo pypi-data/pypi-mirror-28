437a81354f73dc92ba768c3756f4b1c800e9bd8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:22 +0000

    Add package vxfreeswitch/0.1.20/vxfreeswitch-0.1.20.tar.gz

---
+++ packages/vxfreeswitch/vxfreeswitch-0.1.20.tar.gz/vxfreeswitch-0.1.20/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="vxfreeswitch",
+    version="0.1.20",
+    url='http://github.com/praekelt/vumi-freeswitch-esl',
+    license='BSD',
+    description="A Freeswitch eventsocket transport for Vumi.",
+    long_description=open('README.rst', 'r').read(),
+    author='Praekelt Foundation',
+    author_email='dev@praekeltfoundation.org',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'vumi',
+        'confmodel>=0.2.0',
+        'Twisted>=13.1.0',
+        'eventsocket==0.1.5',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: System :: Networking',
+    ],
+)
---
+++ packages/vxfreeswitch/vxfreeswitch-0.1.20.tar.gz/vxfreeswitch-0.1.20/vxfreeswitch/__init__.py
@@ -0,0 +1,7 @@
+"""Voice server transport."""
+
+from .voice import VoiceServerTransport
+
+__version__ = "0.1.20"
+
+__all__ = ['VoiceServerTransport']
