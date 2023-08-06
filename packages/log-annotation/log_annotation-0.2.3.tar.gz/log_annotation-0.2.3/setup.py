c716d4469bc8be6254449ad267dd61b4279b15c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:27 +0000

    Add package log-annotation/0.2.3/log_annotation-0.2.3.tar.gz

---
+++ packages/log-annotation/log_annotation-0.2.3.tar.gz/log_annotation-0.2.3/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='log_annotation',
+    version='0.2.3',
+    packages=['log_annotation'],
+    url='',
+    license='Apache 2.0',
+    author='Eduard Feliu Baena',
+    author_email='eduard@adasiaholdings.com',
+    description='annotation for greylog'
+)
