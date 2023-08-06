390c6aeb6a39dd285518aed5456a2c658ebe0165
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:09 +0000

    Add package django-tokenauth/0.0.6/django_tokenauth-0.0.6-py2.py3-none-any.whl

---
+++ packages/django-tokenauth/django_tokenauth-0.0.6-py2.py3-none-any.whl/tokenauth/settings.py
@@ -0,0 +1,14 @@
+from django.conf import settings
+
+# How long a token should be valid for, in seconds.
+TOKEN_DURATION = getattr(settings, "TOKENAUTH_TOKEN_DURATION", 30 * 60)
+
+# Where to redirect after link operations.
+LOGIN_URL = getattr(settings, "TOKENAUTH_LOGIN_URL", settings.LOGIN_URL)
+
+# Where to redirect after login or logout.
+LOGIN_REDIRECT = getattr(settings, "TOKENAUTH_LOGIN_REDIRECT", settings.LOGIN_REDIRECT_URL)
+LOGOUT_REDIRECT = getattr(settings, "TOKENAUTH_LOGOUT_REDIRECT", settings.LOGOUT_REDIRECT_URL or settings.LOGIN_REDIRECT_URL)
+
+
+DEFAULT_FROM_EMAIL = getattr(settings, "TOKENAUTH_DEFAULT_FROM_EMAIL", settings.DEFAULT_FROM_EMAIL)
