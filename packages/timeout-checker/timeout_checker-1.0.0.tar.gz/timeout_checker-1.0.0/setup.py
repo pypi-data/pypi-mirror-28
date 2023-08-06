ca7421f06348a4b79eac785103e53beefe9e02c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:32 +0000

    Add package timeout-checker/1.0.0/timeout_checker-1.0.0.tar.gz

---
+++ packages/timeout-checker/timeout_checker-1.0.0.tar.gz/timeout_checker-1.0.0/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+	name = 'timeout_checker',
+	packages = ['timeout_checker'],
+	version = '1.0.0',
+	description = 'Package for codes timeout',
+	author = 'Raihat Zaman Neloy',
+	author_email = 'raihatneloy1992@gmail.com',
+	url = 'https://github.com/raihatneloy/timeout',
+	download_url = 'https://github.com/raihatneloy/timeout/archive/1.0.0.tar.gz',
+	keywords = ['timeout', 'codes-timeout'],
+	classifiers = []
+)
