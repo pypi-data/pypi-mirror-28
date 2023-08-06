2e5b83dac1f32be5de700d04cc6f8d45834d3a30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:07 +0000

    Add package onegov.form/0.30.0/onegov.form-0.30.0.tar.gz

---
+++ packages/onegov.form/onegov.form-0.30.0.tar.gz/onegov.form-0.30.0/onegov/form/utils.py
@@ -0,0 +1,88 @@
+import re
+
+from decimal import Decimal
+from hashlib import md5
+from unidecode import unidecode
+
+_unwanted_characters = re.compile(r'[^a-zA-Z0-9]+')
+_html_tags = re.compile(r'<.*?>')
+
+
+def as_internal_id(label):
+    clean = unidecode(label).strip(' ').lower()
+    clean = _unwanted_characters.sub('_', clean)
+
+    return clean
+
+
+def get_fields_from_class(cls):
+    fields = []
+
+    for name in dir(cls):
+        if not name.startswith('_'):
+            field = getattr(cls, name)
+
+            if hasattr(field, '_formfield'):
+                fields.append((name, field))
+
+    fields.sort(key=lambda x: (x[1].creation_counter, x[0]))
+
+    return fields
+
+
+def extract_text_from_html(html):
+    return _html_tags.sub('', html)
+
+
+class decimal_range(object):
+    """ Implementation of Python's range builtin using decimal values instead
+    of integers.
+
+    """
+
+    def __init__(self, start, stop, step=None):
+        if step is None and start <= stop:
+            step = '1.0'
+        elif step is None and start >= stop:
+            step = '-1.0'
+
+        self.start = self.current = Decimal(start)
+        self.stop = Decimal(stop)
+        self.step = Decimal(step)
+
+        assert self.step != Decimal('0')
+
+    def __repr__(self):
+        if self.start <= self.stop and self.step == Decimal('1.0'):
+            return "decimal_range('{}', '{}')".format(self.start, self.stop)
+        elif self.start >= self.stop and self.step == Decimal('-1.0'):
+            return "decimal_range('{}', '{}')".format(self.start, self.stop)
+        else:
+            return "decimal_range('{}', '{}', '{}')".format(
+                self.start, self.stop, self.step
+            )
+
+    def __eq__(self, other):
+        if not isinstance(other, self.__class__):
+            return False
+
+        return (self.start, self.stop, self.step)\
+            == (other.start, other.stop, other.step)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        result, self.current = self.current, self.current + self.step
+
+        if self.step > 0 and result >= self.stop:
+            raise StopIteration
+
+        if self.step < 0 and result <= self.stop:
+            raise StopIteration
+
+        return result
+
+
+def hash_definition(definition):
+    return md5(definition.encode('utf-8')).hexdigest()
---
+++ packages/onegov.form/onegov.form-0.30.0.tar.gz/onegov.form-0.30.0/onegov/form/validators.py
@@ -0,0 +1,154 @@
+import importlib
+import humanize
+
+from mimetypes import types_map
+from onegov.form import _
+from onegov.form.core import with_options
+from onegov.form.errors import InvalidFormSyntax, DuplicateLabelError
+from stdnum.exceptions import ValidationError as StdnumValidationError
+from wtforms import ValidationError
+
+
+class Stdnum(object):
+    """ Validates a string using any python-stdnum format.
+
+    See `<https://github.com/arthurdejong/python-stdnum>`_.
+
+    """
+    def __init__(self, format):
+        module = '.'.join(p for p in format.split('.') if p)
+        self.format = importlib.import_module('stdnum.' + module)
+
+    def __call__(self, form, field):
+        # only do a check for filled out values, to check for the existance
+        # of any value use DataRequired!
+        if not field.data:
+            return
+
+        try:
+            self.format.validate(field.data)
+        except StdnumValidationError:
+            raise ValidationError(field.gettext('Invalid input.'))
+
+
+class FileSizeLimit(object):
+    """ Makes sure an uploaded file is not bigger than the given number of
+    bytes.
+
+    Expects an :class:`onegov.form.fields.UploadField` instance.
+
+    """
+
+    message = _(
+        "The file is too large, please provide a file smaller than {}."
+    )
+
+    def __init__(self, max_bytes):
+        self.max_bytes = max_bytes
+
+    def __call__(self, form, field):
+        if field.data:
+            if field.data.get('size', 0) > self.max_bytes:
+                message = field.gettext(self.message).format(
+                    humanize.naturalsize(self.max_bytes)
+                )
+                raise ValidationError(message)
+
+
+class WhitelistedMimeType(object):
+    """ Makes sure an uploaded file is in a whitelist of allowed mimetypes.
+
+    Expects an :class:`onegov.form.fields.UploadField` instance.
+    """
+
+    whitelist = {
+        'application/excel',
+        'application/vnd.ms-excel',
+        'application/msword',
+        'application/pdf',
+        'application/zip',
+        'image/gif',
+        'image/jpeg',
+        'image/png',
+        'image/x-ms-bmp',
+        'text/plain',
+    }
+
+    message = _("Files of this type are not supported.")
+
+    def __init__(self, whitelist=None):
+        if whitelist is not None:
+            self.whitelist = whitelist
+
+    def __call__(self, form, field):
+        if field.data:
+            if field.data['mimetype'] not in self.whitelist:
+                raise ValidationError(field.gettext(self.message))
+
+
+class ExpectedExtensions(WhitelistedMimeType):
+    """ Makes sure an uploaded file has one of the expected extensions. Since
+    extensions are not something we can count on we look up the mimetype of
+    the extension and use that to check.
+
+    Expects an :class:`onegov.form.fields.UploadField` instance.
+
+    Usage::
+
+        ExpectedFileType('*')  # no check, really
+        ExpectedFileType('pdf')  # makes sure the given file is a pdf
+    """
+
+    def __init__(self, extensions):
+        mimetypes = set(
+            types_map.get('.' + ext.lstrip('.'), None) for ext in extensions)
+        super().__init__(whitelist=mimetypes)
+
+
+class ValidFormDefinition(object):
+    """ Makes sure the given text is a valid onegov.form definition. """
+
+    message = _("The form could not be parsed.")
+    email = _("Define at least one required e-mail field ('E-Mail * = @@@')")
+    syntax = _("The syntax on line {line} is not valid.")
+    duplicate = _("The field '{label}' exists more than once.")
+    reserved = _("'{label}' is a reserved name. Please use a different name.")
+
+    def __init__(self, require_email_field=True, reserved_fields=None):
+        self.require_email_field = require_email_field
+        self.reserved_fields = reserved_fields or set()
+
+    def __call__(self, form, field):
+        if field.data:
+            # XXX circular import
+            from onegov.form import parse_form
+
+            try:
+                form = parse_form(field.data)()
+            except InvalidFormSyntax as e:
+                field.widget = with_options(
+                    field.widget, **{'data-highlight-line': e.line}
+                )
+                raise ValidationError(
+                    field.gettext(self.syntax).format(line=e.line)
+                )
+            except DuplicateLabelError as e:
+                raise ValidationError(
+                    field.gettext(self.duplicate).format(label=e.label)
+                )
+            except AttributeError:
+                raise ValidationError(field.gettext(self.message))
+            else:
+                if self.require_email_field:
+                    if not form.has_required_email_field:
+                        raise ValidationError(field.gettext(self.email))
+
+                if self.reserved_fields:
+                    for formfield_id, formfield in form._fields.items():
+                        if formfield_id in self.reserved_fields:
+
+                            raise ValidationError(
+                                field.gettext(self.reserved).format(
+                                    label=formfield.label.text
+                                )
+                            )
