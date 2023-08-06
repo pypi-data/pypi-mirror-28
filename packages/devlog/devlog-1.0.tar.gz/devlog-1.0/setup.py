e5dd4c2f8eaa813341039f61b2d1aab9c2eafb68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:24 +0000

    Add package devlog/1.0/devlog-1.0.tar.gz

---
+++ packages/devlog/devlog-1.0.tar.gz/devlog-1.0/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+
+setup(
+    name='devlog',
+    packages=['devlog'],
+    version='1.0',
+    description='A simple command line developer log',
+    author='Kacha Mukabe',
+    author_email='kmukabe@gmail.com',
+    url='https://www.github.com/kachamukabe/devlog',
+    download_url='https://github.com/kachaMukabe/devlog/archive/v1.0.tar.gz',
+    keywords=['tag'],
+    classifiers=[],
+)
