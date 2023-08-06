e443fd07ae5d3579b56616cb83a83acbefd09c1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:19 +0000

    Add package pyttest/0.1/pyttest-0.1.tar.gz

---
+++ packages/pyttest/pyttest-0.1.tar.gz/pyttest-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'pyttest',
+  packages = ['pyttest'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A random test lib',
+  author = 'Phillis',
+  author_email = 'phillis.test@gmail.com',
+  url = 'https://github.com/philloooo/pyttest', # use the URL to the github repo
+  download_url = 'https://github.com/philloooo/pyttest/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['testing'], # arbitrary keywords
+  classifiers = [],
+)
