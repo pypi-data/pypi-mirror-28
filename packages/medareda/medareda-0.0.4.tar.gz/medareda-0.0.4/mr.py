7dc19d54e7eccde8dc66f03978242c196c1e408a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:37 +0000

    Add package medareda/0.0.4/medareda-0.0.4.tar.gz

---
+++ packages/medareda/medareda-0.0.4.tar.gz/medareda-0.0.4/mr.py
@@ -0,0 +1,11 @@
+# test package
+
+def version():
+    return '0.0.4'
+
+def revStr(st):
+    return st[::-1]
+ 
+def addstr(st1,st2):
+    return st1 + ":" + st2
+
---
+++ packages/medareda/medareda-0.0.4.tar.gz/medareda-0.0.4/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+  name = 'medareda',
+  packages = ['medareda'], # this must be the same as the name above
+  py_modules = ['mr'],
+  version = '0.0.4',
+  description = 'MedaReda Test package',
+  author = 'Mark Hallett',
+  author_email = 'pp@markhallett.co.uk',
+  url = 'https://github.com/dev-area/medareda',
+  download_url = 'https://github.com/dev-area/medareda/tarball/0.0.4',
+  keywords = ['test', 'medareda', 'version0.0.4' ], 
+  classifiers = [],
+)
