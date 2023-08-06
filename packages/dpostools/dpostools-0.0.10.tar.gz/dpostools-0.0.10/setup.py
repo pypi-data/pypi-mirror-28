7bdf3065bc1c5dfc7bee89a04c9be3a76f1b2cda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:42 +0000

    Add package dpostools/0.0.10/dpostools-0.0.10.tar.gz

---
+++ packages/dpostools/dpostools-0.0.10.tar.gz/dpostools-0.0.10/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='dpostools',
+    version='0.0.10',
+    packages=['dpostools', 'dpostools.tests', 'dpostools.yamls'],
+    url='https://github.com/BlockHub/blockhubdpostools.git',
+    license='MIT',
+    author='Charles',
+    author_email='karel@blockhub.nl',
+    description='generic toolkit for interacting with DPOS chains'
+)
