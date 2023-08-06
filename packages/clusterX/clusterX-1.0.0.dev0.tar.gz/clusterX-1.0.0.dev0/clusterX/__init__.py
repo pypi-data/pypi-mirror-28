385818b409e211628fa355485fd496f77a457c4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:39 +0000

    Add package clusterX/1.0.0.dev0/clusterX-1.0.0.dev0.tar.gz

---
+++ packages/clusterX/clusterX-1.0.0.dev0.tar.gz/clusterX-1.0.0.dev0/clusterX/__init__.py
@@ -0,0 +1,8 @@
+# Copyright 2015-2018 HU
+# (see accompanying license files for details).
+
+"""CELL (aka clusterX)"""
+
+import numpy as np
+__version__ = '1.0.0.dev0'
+
---
+++ packages/clusterX/clusterX-1.0.0.dev0.tar.gz/clusterX-1.0.0.dev0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='clusterX',
+      version='1.0.0.dev0',
+      description='Cluster expansion for large-parent-cell materials',
+      url='http://sol.physik.hu-berlin.de/cell/',
+      author='CELL Developers',
+      author_email='srigamonti@physik.hu-berlin.de',
+      packages=find_packages()
+)
