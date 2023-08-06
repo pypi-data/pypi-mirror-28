da373790dfb68f5a8060480f492708731b5f7e82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:09 +0000

    Add package quilt/2.9.1/quilt-2.9.1.tar.gz

---
+++ packages/quilt/quilt-2.9.1.tar.gz/quilt-2.9.1/quilt/nodes.py
@@ -0,0 +1,124 @@
+"""
+Nodes that represent the data in a Quilt package.
+"""
+
+import pandas as pd
+from six import iteritems, string_types
+
+from .tools import core
+
+
+class Node(object):
+    """
+    Abstract class that represents a group or a leaf node in a package.
+    """
+    def __init__(self):
+        # Can't instantiate it directly
+        assert self.__class__ != Node.__class__
+
+    def _class_repr(self):
+        """Only exists to make it easier for subclasses to customize `__repr__`."""
+        return "<%s>" % self.__class__.__name__
+
+    def __repr__(self):
+        return self._class_repr()
+
+    def __setattr__(self, name, value):
+        if name.startswith('_') or isinstance(value, Node):
+            super(Node, self).__setattr__(name, value)
+        else:
+            raise AttributeError("{val} is not a valid package node".format(val=value))
+
+class DataNode(Node):
+    """
+    Represents a dataframe or a file. Allows accessing the contents using `()`.
+    """
+    def __init__(self, package, node, data=None):
+        super(DataNode, self).__init__()
+        self._package = package
+        self._node = node
+        self.__cached_data = data
+
+    def __call__(self):
+        return self._data()
+
+    def _data(self):
+        """
+        Returns the contents of the node: a dataframe or a file path.
+        """
+        if self.__cached_data is None:
+            self.__cached_data = self._package.get_obj(self._node)
+        return self.__cached_data
+
+class GroupNode(DataNode):
+    """
+    Represents a group in a package. Allows accessing child objects using the dot notation.
+    Warning: calling _data() on a large dataset may exceed local memory capacity in Python (Only
+    supported for Parquet packages).
+    """
+
+    def __repr__(self):
+        pinfo = super(GroupNode, self).__repr__()
+        group_info = '\n'.join(name + '/' for name in sorted(self._group_keys()))
+        data_info = '\n'.join(sorted(self._data_keys()))
+        return '%s\n%s\n%s' % (pinfo, group_info, data_info)
+
+    def _items(self):
+        return ((name, child) for name, child in iteritems(self.__dict__)
+                if not name.startswith('_'))
+
+    def _data_keys(self):
+        """
+        every child key referencing a dataframe
+        """
+        return [name for name, child in self._items() if not isinstance(child, GroupNode)]
+
+    def _group_keys(self):
+        """
+        every child key referencing a group that is not a dataframe
+        """
+        return [name for name, child in self._items() if isinstance(child, GroupNode)]
+
+    def _keys(self):
+        """
+        keys directly accessible on this object via getattr or .
+        """
+        return [name for name in self.__dict__ if not name.startswith('_')]
+
+    def _add_group(self, groupname):
+        child = GroupNode(self._package, core.GroupNode({}))
+        setattr(self, groupname, child)
+
+class PackageNode(GroupNode):
+    """
+    Represents a package.
+    """
+
+    def _class_repr(self):
+        finfo = self._package.get_path()
+        return "<%s %r>" % (self.__class__.__name__, finfo)
+
+    def _set(self, path, value):
+        assert isinstance(path, list) and len(path) > 0
+
+        if isinstance(value, pd.DataFrame):
+            core_node = core.TableNode(hashes=[], format=core.PackageFormat.default.value)
+        elif isinstance(value, string_types):
+            core_node = core.FileNode(hashes=[])
+        else:
+            assert False, "Unexpected value: %r" % value
+
+        node = self
+        for key in path[:-1]:
+            assert not key.startswith('_')
+            child = getattr(node, key, None)
+            if not isinstance(child, GroupNode):
+                child = GroupNode(self._package, core.GroupNode({}))
+                setattr(node, key, child)
+
+            node = child
+
+        key = path[-1]
+        assert not key.startswith('_')
+        data_node = DataNode(self._package, core_node, value)
+        setattr(node, key, data_node)
---
+++ packages/quilt/quilt-2.9.1.tar.gz/quilt-2.9.1/quilt/tools/util.py
@@ -0,0 +1,103 @@
+"""
+Helper functions.
+"""
+import gzip
+import os
+
+from appdirs import user_config_dir, user_data_dir
+from six import BytesIO, string_types, Iterator
+
+APP_NAME = "QuiltCli"
+APP_AUTHOR = "QuiltData"
+BASE_DIR = user_data_dir(APP_NAME, APP_AUTHOR)
+CONFIG_DIR = user_config_dir(APP_NAME, APP_AUTHOR)
+
+
+class FileWithReadProgress(Iterator):
+    """
+    Acts like a file with mode='rb', but displays a progress bar while the file is read.
+    """
+    def __init__(self, path_or_fd, progress_cb):
+        if isinstance(path_or_fd, string_types):
+            self._fd = open(path_or_fd, 'rb')
+            self._need_to_close = True
+        else:
+            self._fd = path_or_fd
+            self._need_to_close = False
+
+        self._progress_cb = progress_cb
+
+    def read(self, size=-1):
+        """Read bytes and update the progress bar."""
+        buf = self._fd.read(size)
+        self._progress_cb(len(buf))
+        return buf
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        """Read the next line and update the progress bar."""
+        buf = next(self._fd)
+        self._progress_cb(len(buf))
+        return buf
+
+    def tell(self):
+        """Get the file position."""
+        return self._fd.tell()
+
+    def seek(self, offset, whence=0):
+        """Set the new file position."""
+        self._fd.seek(offset, whence)
+
+    def close(self):
+        """Close the file."""
+        if self._need_to_close:
+            self._fd.close()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, type, value, traceback): # pylint:disable=W0622
+        self.close()
+
+
+def file_to_str(fname):
+    """
+    Read a file into a string
+    PRE: fname is a small file (to avoid hogging memory and its discontents)
+    """
+    data = None
+    # rU = read with Universal line terminator
+    with open(fname, 'rU') as fd:
+        data = fd.read()
+    return data
+
+def gzip_compress(data):
+    """
+    Compress a string. Same as gzip.compress in Python3.
+    """
+    buf = BytesIO()
+    with gzip.GzipFile(fileobj=buf, mode='wb') as fd:
+        fd.write(data)
+    return buf.getvalue()
+
+def sub_dirs(path, invisible=False):
+    """
+    Child directories (non-recursive)
+    """
+    dirs = [x for x in os.listdir(path) if os.path.isdir(os.path.join(path, x))]
+    if not invisible:
+        dirs = [x for x in dirs if not x.startswith('.')]
+
+    return dirs
+
+def sub_files(path, invisible=False):
+    """
+    Child files (non-recursive)
+    """
+    files = [x for x in os.listdir(path) if os.path.isfile(os.path.join(path, x))]
+    if not invisible:
+        files = [x for x in files if not x.startswith('.')]
+
+    return files
