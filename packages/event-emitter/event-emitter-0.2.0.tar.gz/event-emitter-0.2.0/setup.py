5f90f35cf24e0f10ec29908cd06c67239d7b6bb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:28 +0000

    Add package event-emitter/0.2.0/event-emitter-0.2.0.tar.gz

---
+++ packages/event-emitter/event-emitter-0.2.0.tar.gz/event-emitter-0.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='event-emitter',
+    packages=['haydn'],
+    version='0.2.0',
+    description='Javascript style event emitter for Python3',
+    author='Jason Lee',
+    url='https://bitbucket.org/haydn_team/event_emitter/',
+    download_url='https://bitbucket.org/haydn_team/event_emitter.git'
+)
