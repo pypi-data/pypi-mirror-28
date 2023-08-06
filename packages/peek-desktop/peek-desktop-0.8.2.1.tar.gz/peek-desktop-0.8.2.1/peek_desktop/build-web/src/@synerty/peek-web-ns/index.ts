c2e9c85aa1cda3c84a2db4da5577cbe3145db05c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:14 +0000

    Add package peek-desktop/0.8.2.1/peek-desktop-0.8.2.1.tar.gz

---
+++ packages/peek-desktop/peek-desktop-0.8.2.1.tar.gz/peek-desktop-0.8.2.1/peek_desktop/build-web/src/@synerty/peek-web-ns/index.ts
@@ -0,0 +1,6 @@
+export {DeviceInfoService} from "./device-info.interface";
+export {DeviceInfoWebService} from "./device-info-web.service";
+export {PeekCoreConfigService} from "./peek-core-config.service";
+export {PeekModuleFactory} from "./peek-module.factory";
+export {PeekViewBrokerService} from "./peek-view-broker.service";
+export {PeekComponent} from "./peek-component-ts";
