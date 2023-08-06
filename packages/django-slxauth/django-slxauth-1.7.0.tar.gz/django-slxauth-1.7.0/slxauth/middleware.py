d5fd7f3b11f973fd140e30e655f8da9be22293fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:19 +0000

    Add package django-slxauth/1.7.0/django-slxauth-1.7.0.tar.gz

---
+++ packages/django-slxauth/django-slxauth-1.7.0.tar.gz/django-slxauth-1.7.0/slxauth/middleware.py
@@ -0,0 +1,46 @@
+import re
+
+from django.contrib.auth import logout
+from django.utils import translation
+
+from slxauth.utils import get_authenticator
+
+
+class TokenAuthMiddleware(object):
+    def __init__(self, get_response):
+        self.get_response = get_response
+        # One-time configuration and initialization.
+
+    def __call__(self, request):
+        # Code to be executed for each request before
+        # the view (and later middleware) are called.
+
+        access_token = None
+
+        if 'HTTP_AUTHORIZATION' in request.META:
+            m = re.search('Bearer (\\S+)', request.META['HTTP_AUTHORIZATION'])
+            if m:
+                access_token = m.group(1)
+        else:
+            access_token = request.COOKIES.get('access_token')
+            #expiration_time = request.COOKIES.get('expiration_time')
+            #refresh_token = request.COOKIES.get('refresh_token')
+
+        if access_token:
+            if not request.user.is_authenticated or access_token != request.user.access_token:
+                auth = get_authenticator()
+                auth.login_using_token(request, access_token)
+        else:
+            logout(request)
+
+        if request.user.is_authenticated:
+            translation.activate(request.user.language)
+            request.session[translation.LANGUAGE_SESSION_KEY] = request.user.language
+
+        response = self.get_response(request)
+
+        # Code to be executed for each request/response after
+        # the view is called.
+
+        return response
+
