eedb60007d548eb498b83ac289190f7892319bdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:09 +0000

    Add package setup-env/0.0.2/setup-env-0.0.2.tar.gz

---
+++ packages/setup-env/setup-env-0.0.2.tar.gz/setup-env-0.0.2/setup.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python3
+
+from distutils.core import setup
+
+setup(name="setup-env",
+      description="Setup development evnironment tools.",
+      version="0.0.2",
+      author="Chenyao2333",
+      author_email="louchenyao@gmail.com",
+      url="https://github.com/Chenyao2333/setup-env",
+      packages=["setup_env"],
+      package_data={"setup_env": ["scripts/*"]},
+      python_requires = ">=3"
+)
