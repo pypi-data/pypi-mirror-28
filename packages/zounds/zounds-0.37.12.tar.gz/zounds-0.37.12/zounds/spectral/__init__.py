55b2f13c3dd48c7cdac82bcaaf44e8b0385ed95c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:52:53 +0000

    Add package zounds/0.37.12/zounds-0.37.12.tar.gz

---
+++ packages/zounds/zounds-0.37.12.tar.gz/zounds-0.37.12/zounds/spectral/__init__.py
@@ -0,0 +1,24 @@
+"""
+The spectral module contains classes that aid in dealing with frequency-domain
+representations of sound
+"""
+
+from sliding_window import \
+    SlidingWindow, OggVorbisWindowingFunc, WindowingFunc, HanningWindowingFunc
+
+from spectral import \
+    FFT, DCT, DCTIV, MDCT, BarkBands, Chroma, BFCC, SpectralCentroid, \
+    SpectralFlatness, FrequencyAdaptiveTransform, FrequencyWeighting
+
+from tfrepresentation import FrequencyDimension, ExplicitFrequencyDimension
+
+from weighting import AWeighting
+
+from frequencyscale import \
+    LinearScale, LogScale, FrequencyBand, FrequencyScale, GeometricScale, \
+    ExplicitScale, Hertz
+
+from frequencyadaptive import FrequencyAdaptive
+
+from functional import \
+    fft, stft, apply_scale, frequency_decomposition, phase_shift, rainbowgram
