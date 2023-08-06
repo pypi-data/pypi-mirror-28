8c52c979e0c194a8a8f023af0aef7079e9484a8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:52:23 +0000

    Add package genomeqaml-gui/0.1.1/genomeqaml-gui-0.1.1.tar.gz

---
+++ packages/genomeqaml-gui/genomeqaml-gui-0.1.1.tar.gz/genomeqaml-gui-0.1.1/README.md
@@ -0,0 +1,11 @@
+# GenomeQAML GUI
+:camel: GUI wrapper for the OLC GenomeQAML
+
+See __https://github.com/OLC-LOC-Bioinformatics/GenomeQAML__ for details.
+
+## External Dependencies
+
+- [Mash (v2.0 or greater)](https://github.com/marbl/mash)
+- [Prodigal (>=2.6.2)](https://github.com/hyattpd/Prodigal)
+
+Both of these need to be downloaded and included on your $PATH.
---
+++ packages/genomeqaml-gui/genomeqaml-gui-0.1.1.tar.gz/genomeqaml-gui-0.1.1/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name="genomeqaml-gui",
+    version="0.1.1",
+    packages=find_packages(),
+    author="Forest Dussault",
+    author_email="forest.dussault@inspection.gc.ca",
+    url="https://github.com/forestdussault/GenomeQAML_GUI",
+    scripts=['qamlgui.py'],
+    install_requires=['GenomeQAML']
+)
