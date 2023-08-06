b2c26ce2496b28dc9e374927c293f6c847f97e03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:07 +0000

    Add package nu_isp/0.2/nu_isp-0.2.zip

---
+++ packages/nu_isp/nu_isp-0.2.zip/nu_isp-0.2/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name = 'nu_isp',
+    packages = ['nu_isp'],
+    scripts = [],
+    version = '0.2',
+    description = 'Nuvoton ISP',
+    author = 'jcliu,ccma',
+    author_email = 'ccma@nuvoton.com',
+    keywords = ['Nuvoton'],
+    classifiers = [],
+)
