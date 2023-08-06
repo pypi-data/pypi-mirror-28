ef8862d2a911f474dd1993681f0d6abc43b1e090
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:09 +0000

    Add package emmlej-dummy/0.0.1/emmlej-dummy-0.0.1.tar.gz

---
+++ packages/emmlej-dummy/emmlej-dummy-0.0.1.tar.gz/emmlej-dummy-0.0.1/setup.py
@@ -0,0 +1,18 @@
+# coding: Latin-1
+# Copyright © 2018 The Things Network
+# Use of this source code is governed by the
+# MIT license that can be found in the LICENSE file.
+
+from setuptools import setup
+
+setup(name="emmlej-dummy",
+      version="0.0.1",
+      description="Dummy Package for Testing",
+      long_description = "Dummy package to test upload to Pypi with Travis",
+      url = "https://github.com/emmlejeail/dummy-package",
+      author="Emmanuelle Lejeail",
+      author_email="manu.lejeail@gmail.com",
+      license="MIT",
+      packages=["src"],
+      install_requires=[],
+      zip_safe=False)
