03de0329afd4ed84ae6d0f192f139b0a380d199c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:58 +0000

    Add package dicom2nifti/1.2.20/dicom2nifti-1.2.20.tar.gz

---
+++ packages/dicom2nifti/dicom2nifti-1.2.20.tar.gz/dicom2nifti-1.2.20/dicom2nifti/__init__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+dicom2nifti
+
+@author: abrys
+"""
+
+# noinspection PyUnresolvedReferences
+from dicom2nifti.settings import disable_validate_sliceincrement, \
+    disable_validate_orientation, \
+    disable_validate_orthogonal, \
+    disable_validate_slicecount, \
+    disable_validate_multiframe_implicit, \
+    enable_validate_orientation, \
+    enable_validate_orthogonal, \
+    enable_validate_slicecount, \
+    enable_validate_sliceincrement, \
+    enable_validate_multiframe_implicit
+# noinspection PyUnresolvedReferences
+from dicom2nifti.convert_dicom import dicom_series_to_nifti
+# noinspection PyUnresolvedReferences
+from dicom2nifti.convert_dir import convert_directory
+
+# Setup the logger correctly
+import logging
+import sys
+
+logger = logging.getLogger(__name__)
+handler = logging.StreamHandler(sys.stdout)
+handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s %(message)s'))
+logger.addHandler(handler)
+logger.setLevel(logging.WARNING)
