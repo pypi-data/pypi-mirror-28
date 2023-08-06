4584a1f268c50771792b5600b5a60d001b79448b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:24:33 +0000

    Add package unipy-logger/0.1.6/unipy_logger-0.1.6.tar.gz

---
+++ packages/unipy-logger/unipy_logger-0.1.6.tar.gz/unipy_logger-0.1.6/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='unipy_logger',
+      keywords='log, json, uniplaces',
+      version='0.1.6',
+      description='enforce uniplaces log standards and respect uniplaces logging interface.',
+      url='https://github.com/uniplaces/unipy_logger',
+      author='Raffaello De Pieri',
+      author_email='raffaello.pieri@uniplaces.com',
+      license='MIT',
+      packages=['unipy_logger'],
+      install_requires=['logbook'],
+      zip_safe=False)
