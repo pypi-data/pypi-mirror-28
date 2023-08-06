fc7f463a12a5aca27c8d488566e796821374581c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:03:22 +0000

    Add package cypher2/1.0.1/cypher2-1.0.1.tar.gz

---
+++ packages/cypher2/cypher2-1.0.1.tar.gz/cypher2-1.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='cypher2',
+    version='1.0.01',
+    description='Retrieve Music Lyrics',
+    author='Timothy James Dobbins',
+    author_email='tmthyjames@gmail.com',
+    url='https://github.com/tmthyjames/cypher',
+    install_requires=['requests', 'beautifulsoup4'],
+    scripts=['cypher2'] 
+)
