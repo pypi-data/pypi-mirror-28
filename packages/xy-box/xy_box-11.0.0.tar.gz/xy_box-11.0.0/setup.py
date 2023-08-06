2d66d69a789eaa00d96a2b018e6b81d9e0f4b08e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:39 +0000

    Add package xy-box/11.0.0/xy_box-11.0.0.tar.gz

---
+++ packages/xy-box/xy_box-11.0.0.tar.gz/xy_box-11.0.0/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+setup(
+  name = 'xy_box',
+  packages = ['xy_box'],
+  package_data = {'xy_box':['boxes/*']},
+  version = '11.0.0',
+  description = 'xiaoyuan box',
+  long_description='xiaoyuan box',
+  author = 'KingHS',
+  author_email = '382771946@qq.com',
+  keywords = ['pip3', 'python3','python','box'],
+  license='MIT',
+  install_requires=[
+    # 'tkinter',
+  ]
+)
