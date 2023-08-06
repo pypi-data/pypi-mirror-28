062a04293957b664cbae72a1b03d87b62c1a47fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:12 +0000

    Add package arboreal/0.1/arboreal-0.1.tar.gz

---
+++ packages/arboreal/arboreal-0.1.tar.gz/arboreal-0.1/arboreal/__init__.py
@@ -0,0 +1,2 @@
+def wip():
+    return 'This package is a work in progress'
---
+++ packages/arboreal/arboreal-0.1.tar.gz/arboreal-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='arboreal',
+      version='0.1',
+      description='Python package to modularly create decision trees',
+      url='http://github.com/ankur-gupta/arboreal',
+      author='Ankur Gupta',
+      author_email='ankur@perfectlyrandom.org',
+      license='MIT',
+      packages=['arboreal'],
+      zip_safe=False)
