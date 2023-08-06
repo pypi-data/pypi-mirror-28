71c5d55b35286b8e11cecdaf09d5b7ba85e04cc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:16 +0000

    Add package blazar-dashboard/0/blazar-dashboard-0.tar.gz

---
+++ packages/blazar-dashboard/blazar-dashboard-0.tar.gz/blazar-dashboard-0/setup.py
@@ -0,0 +1,19 @@
+"""A setuptools based setup module.
+
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+setup(
+    name='blazar-dashboard',
+    version='0'
+)
