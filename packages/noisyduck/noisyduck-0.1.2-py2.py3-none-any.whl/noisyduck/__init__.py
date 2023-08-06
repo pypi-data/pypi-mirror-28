3bbcf67850a3527b12ed8de8e3a7f3c1b24c7fce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:24 +0000

    Add package noisyduck/0.1.2/noisyduck-0.1.2-py2.py3-none-any.whl

---
+++ packages/noisyduck/noisyduck-0.1.2-py2.py3-none-any.whl/noisyduck/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for Noisy Duck."""
+
+__author__ = """Nathan Wukie"""
+__email__ = 'nathan.wukie@gmail.com'
+__version__ = '0.1.2'
+
+from . import annulus
+from . import filter
+
