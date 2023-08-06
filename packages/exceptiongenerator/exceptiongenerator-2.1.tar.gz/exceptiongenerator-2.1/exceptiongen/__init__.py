9664eff4b9a05264b9e168c49bc264c4a7a2f28a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:04:26 +0000

    Add package exceptiongenerator/2.1/exceptiongenerator-2.1.tar.gz

---
+++ packages/exceptiongenerator/exceptiongenerator-2.1.tar.gz/exceptiongenerator-2.1/exceptiongen/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+def exception_generator(name, parent, message=''):
+    def __init__(self, **debugvars):
+        lines = [message]
+        if len(debugvars) > 0:
+            lines.append('Printing debug variables:')
+        for n, v in debugvars.items():
+            lines.append(f'{n}: {v}')
+        Exception.__init__(self, '\n'.join(lines))
+    return type(name, (parent,), {'__init__': __init__})
