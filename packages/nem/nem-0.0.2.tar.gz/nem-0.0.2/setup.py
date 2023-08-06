b30dd935fe2629c062cb1f50e09857180f33c7d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:03:10 +0000

    Add package nem/0.0.2/nem-0.0.2.tar.gz

---
+++ packages/nem/nem-0.0.2.tar.gz/nem-0.0.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+setup(
+  name = 'nem',
+  packages = ['nem'], # this must be the same as the name above
+  version = '0.0.2',
+  description = 'Access library for AEMO / NEM goverement data',
+  author = 'Michael Wheeler',
+  author_email = 'michael@michael-wheeler.org',
+  url = 'https://github.com/theskorm/nemweb2', # use the URL to the github repo
+  download_url = 'https://github.com/TheSkorm/nemweb2/archive/0.0.2.tar.gz', # I'll explain this in a second
+  keywords = ['aemo', 'nem', 'gov', 'au'], # arbitrary keywords
+  classifiers = [],
+  install_requires = [
+        "beautifulsoup4",
+        "boto3",
+        "arrow"
+  ]
+)
