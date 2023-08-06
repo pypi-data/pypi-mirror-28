7b6ff9164d022855b91043c5850b16ee18148e12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:41 +0000

    Add package astrobject/0.5.9/astrobject-0.5.9.tar.gz

---
+++ packages/astrobject/astrobject-0.5.9.tar.gz/astrobject-0.5.9/astrobject/__init__.py
@@ -0,0 +1,18 @@
+
+#
+# This is the astrobject library
+#
+
+
+__version__ = "0.5.9"
+
+from baseobject   import *
+from photometry   import get_image, get_photopoint
+
+# don't invert collections and collection
+#from collections import *
+#from collection  import *
+#from instruments import *
+
+
+
