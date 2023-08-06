06a3edb2ad3a5e6c9556e35f39579df50986f4d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:56 +0000

    Add package pnestlist/1.0.0/pnestlist-1.0.0.tar.gz

---
+++ packages/pnestlist/pnestlist-1.0.0.tar.gz/pnestlist-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup 
+
+setup(
+	name        = 'pnestlist',
+	version     = '1.0.0',
+	py_modules  = ['pnestlist'],
+	author      = 'Akshat1294',
+	author_mail = 'akshatdeep1294@gmail.com',
+	description = 'A simple printer of nested lists',
+)
